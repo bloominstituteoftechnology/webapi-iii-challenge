1e36d5c624bd4ac0a4045bf4d2a2bd67a8f44144
perryahern <pahern@gmail.com>
2018-02-12 16:42:43 -0500

    get by id working added status codes improved post, added error checking

--- server.js
+++ server.js
@@ -8,23 +8,48 @@ server.use(bodyParser.json());   // tell the server to use bodyParser
 
 const users = {};
 let id = 0;
+const STATUS_USER_ERROR = 422;
+const STATUS_SUCCESS = 200;
 
 server.get('/users', (req, res) => {
   if (id === 0) {
-    res.status = 422;
+    res.status = STATUS_USER_ERROR;
     res.json({ error: 'No users added, cannot display' });
     return;
   }
 
-  res.status = 200;
+  res.status = STATUS_SUCCESS;
   res.json({ users });
   return;
 });
 
+server.get('/users/:id', (req, res) => {
+  const findId = req.params.id;
+  // let foundName = '';
+  const foundName = Object.keys(users)[findId];
+  console.log(Object.keys(users));
+  if (!foundName) {
+    res.status = STATUS_USER_ERROR;
+    res.json({ error: `User with ID ${findId} not found`});
+    return;
+  }
+  res.status = STATUS_SUCCESS;
+  res.json({ foundName });
+  return;
+});
+
 server.post('/users', (req, res) => {
-  users[req.body.name] = id;
+  const name = req.body.name;
+  if (!name) {
+    res.status = STATUS_USER_ERROR;
+    res.json({ error: 'Name must be specified' });
+    return;
+  }
+  users[name] = id;
+  res.status = STATUS_SUCCESS;
   res.send(id + '');
   id++;
+  return;
 })
 
 server.listen(PORT, error => {
