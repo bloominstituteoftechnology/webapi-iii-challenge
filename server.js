23d8a330a1eedca757c438a30ad4fca847e0fadd
Guelmis Cortina <cguelmis@yahoo.com>
2018-02-12 17:55:56 -0500

    get request to user with matching id

--- server.js
+++ server.js
@@ -2,26 +2,55 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const server = express();
+server.use(bodyParser.json());
+
 const STATUS_USER_ERROR = 422;
 const PORT = 3000;
 
-const users = [];
-
-server.use(bodyParser.json());
+const users = [
+	{
+		name: 'Hailie',
+		id: 0
+	},
+	{
+		name: 'Guelmis',
+		id: 1
+	}
+];
 
 server.get('/users', (req, res) => {
-	res.send(users);
+	res.json(users);
 });
 
-server.post('/users', (req, res) => {
-	const user = req.body.user;
-	if (!user) {
+server.get('/users/:id', (req, res) => {
+	const { id } = req.params;
+	const { name } = req.body;
+	console.log('name ->', name);
+	let foundUser = null;
+	users.forEach(user => {
+		if (user.id == id) foundUser = user;
+	});
+	if (!foundUser) {
 		res.status(STATUS_USER_ERROR);
-		res.json({ error: "Must provide user information" });
+		res.json({ error: "User not found by that id" });
+	} else {
+		res.send(foundUser);
+	}
+});
+
+let userId = 2;
+
+server.post('/users', (req, res) => {
+	const { name } = req.body;
+	const newUser = { name, id: userId };
+	if (!name) {
+		res.status(STATUS_USER_ERROR);
+		res.send({ error: "Must provide user information (name)" });
 		return;
 	}
-	users.push(user);
-	res.json({ users: users });
+	users.push(newUser);
+	userId++;
+	res.send({ users: users });
 });
 
 server.listen(PORT, err => {
