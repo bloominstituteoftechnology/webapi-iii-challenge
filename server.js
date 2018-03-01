06307a7c8b7880ae605a1d26942c089ed48c2ed7
LordOrbnauticus <haywood.d.johnson@gmail.com>
2018-03-01 03:05:50 -0600

    post test

--- server.js
+++ server.js
@@ -2,31 +2,43 @@ const bodyParser = require('body-parser');
 const express = require('express')
 const cors = require('cors');
 
+const app = express();
+
 const STATUS_APPROVED = 200;
 const STATUS_USER_ERROR = 422;
 
-server.use(express);
-server.use(bodyParser.json());
-
+app.use(express);
+app.use(bodyParser.json());
 
 let users = {};
+let userid = 0;
 
-server.post('/users', (req, res) => {
-    const username = req.body.username;
-    if (!username) {
+app.get('/users', (req, res) => {
+    res.send(users);
+});
+
+app.post('/users', (req, res) => {
+    userid++;
+    const name = req.body.name;
+    const newUser = {
+        id: userid,
+        username: users
+    }
+    if (!name) {
         res.status(STATUS_USER_ERROR);
         res.json({ err: `Must provide a username` });
-        return;
+    } else {
+    users.push(newUser);
+    res.send(users);
     }
 });
 
-server.get( '/users', (req, res) => {
+// app.get()
 
-});
 
-const port = process.env.PORT || 3000;
-server.listen(port, () => {
-    console.log(`Server is listening on on http://localhost${ port }`);
+const port = process.env.PORT || 3030;
+app.listen(port, () => {
+    console.log(`App is listening on on http://localhost:${ port }`);
 });
 
 
