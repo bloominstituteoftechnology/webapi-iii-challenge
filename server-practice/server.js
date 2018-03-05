d31c633ce81a4c4f42084f25e4f4cb32c3b225c4
german gonzalez <gcomlnk@gmail.com>
2018-03-05 16:42:08 -0500

    POST end-point

--- server-practice/server.js
+++ server-practice/server.js
@@ -6,35 +6,33 @@ const PORT = 3030;
 const STATUS_SUCCESS = 200;
 const STATUS_USER_ERROR = 422;
 
-server.use((req,res, next) => {
-  console.log("here's the request: ", req);
-  next();
+server.use((req, res, next) => {
+    next();
 });
 
 server.use(bodyParser.json());
 
 let idCounter = 0;
-let users = {};
+let users = [];
 
-server.post('/', (req, res) => {
-  const { body: { name } } = req;
-  idCounter++;
-  users[idCounter] = name;
-  res.status(STATUS_SUCCESS);
-  res.send({users: { id: idCounter, name }});
+server.post('/users', (req, res) => {
+    const {name} = req.body;
+    res.status(STATUS_SUCCESS);
+    const usersObj = { name, id: ++idCounter };
+    users.push(usersObj);
+    res.send(users);
 });
 
 server.get('/users', (req, res) => {
-  console.log(req.body, res.send)
-  res.status(STATUS_SUCCESS);
-  res.send(users);
-})
+    res.status(STATUS_SUCCESS);
+    res.send(users);
+});
 
 
 server.listen(PORT, (err) => {
-	if (err) {
-    console.log(`There was an error starting the server: ${err}`);
-  } else {
-    console.log(`Server is listening on port ${PORT}`)
-  }
+    if (err) {
+        console.log(`There was an error starting the server: ${err}`);
+    } else {
+        console.log(`Server is listening on port ${PORT}`)
+    }
 });
