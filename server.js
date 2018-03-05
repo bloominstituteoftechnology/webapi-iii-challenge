4305655881d9a18de01d8a2a59cec576e2e02b5d
rawalhome@gmail.com <rawalhome@gmail.com>
2018-03-05 14:29:40 -0600

    Dani Tacheny + Punit Rawal: Get works

--- server.js
+++ server.js
@@ -1,2 +1,38 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+
+
+const server = express();
+const PORT =  3030;
+const STATUS_SUCCESS = 200;
+const STATUS_ERROR = 413;
+
+let counter = 3;
+const pets = {
+  1: "Rigby",
+  2: "Tag",
+  3: "Schatzi",
+  4: "Riley"
+};
+
+server.get("/", (req, res) => {
+  res.status(STATUS_SUCCESS);
+  res.send(pets);
+  res.error(STATUS_ERROR);
+});
+
+server.get("/:id/", (req, res) => {
+  const id = req.params.id;
+  res.status(STATUS_SUCCESS);
+  res.send(pets[id]);
+  // res.error(STATUS_ERROR);
+})
+
+
+
+server.listen(PORT, (err) =>{
+  if(err) {
+    console.log("Error starting server");
+} else {
+    console.log("Server listening on port: ", PORT);
+}});
