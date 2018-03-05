3debe7ea24e6201b41d366f88e173dd7bee9bae1
Arkoma <aaron@adburk.net>
2018-03-05 14:09:54 -0600

    initial commit
    - set up package.json and package-lock.json files
    
    - set up initial server.js file to handle post request.

---
+++ server-practice/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const server = express();
+const PORT = 3030;
+const STATUS_SUCCESS = 200;
+const STATUS_USER_ERROR = 422;
+
+server.use((req,res, next) => {
+  console.log("here's the request: ", req);
+  next();
+});
+
+server.use(bodyParser.json());
+
+let idCounter = 0;
+
+server.post('/', (req, res) => {
+  const { body: { user } } = req;
+  idCounter++;
+  users[idCounter] = user;
+  res.status(STATUS_SUCCESS);
+  res.send({ id: idCounter });
+});
+
+
+server.listen(PORT, (err) => {
+	if (err) {
+    console.log(`There was an error starting the server: ${err}`);
+  } else {
+    console.log(`Server is listening on port ${PORT}`)
+  }
+});
