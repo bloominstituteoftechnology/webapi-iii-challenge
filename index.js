63d6f86465658f82161f3d239689968543a29ebf
das41236 <davsoudry@gmail.com>
2018-03-05 14:54:47 -0600

    GET complete

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const server = express();
+const PORT = 3000;
+
+let idCounter = 4;
+const users = {
+    1:'GGG',
+    2: 'RRR',
+    3: 'YYY',
+    4: 'ggg'
+};
+
+server.use(bodyParser.json());
+
+server.get('/users/', (req, res) => {
+    res.status(200);
+    res.send(users);
+})
+
+server.get('/users/:id', (req, res) => {
+    const {
+        id
+    } = req.params;
+    res.status(200);
+    res.send(users[id]);
+})
+
+server.get('/search', (req, res) => {
+    let user = [];
+    Object.keys(users).forEach((id => {
+        if (users[id].toLowerCase() === req.query.name.toLowerCase()) {
+            user.push(id);
+        };
+    }));
+    res.status(200);
+    res.send(user);
+})
+
+
+
+
+
+server.listen(PORT, (err) => {
+    if (err) {
+        console.log(`There was an error starting the server: ${err}`);
+    } else {
+        console.log(`Server is listening on port ${PORT}`);
+    }
+});
