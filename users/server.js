ca8303b79070c2303114ac15d5928fda02c04752
ashleijo <shuryjo@gmail.com>
2018-03-05 15:34:54 -0500

    adding users route to send array of users

--- users/server.js
+++ users/server.js
@@ -4,3 +4,31 @@ const bodyParser = require('body-parser');
 const server = express();
 const PORT = 3000;
 const STATUS_SUCCESS = 200;
+
+let idCounter = 2;
+const users = [
+  {
+    id: 1,
+    name: "Bob",
+    age: 20
+  },
+  {
+    id: 2,
+    name: "Sue",
+    age: 30
+  }
+];
+
+server.get("/users", (req, res) => {
+
+  
+  res.send(users);
+})
+
+server.listen(PORT, err => {
+  if (err) {
+    console.log("Error" + err)
+  } else {
+    console.log(`server is listening on ${PORT}`);
+  }
+})
