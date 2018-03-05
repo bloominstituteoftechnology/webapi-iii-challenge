f44c23c377503f6cc71af54d40834fbfee3eb15e
Kevin <16568166+sirkevinc@users.noreply.github.com>
2018-03-05 16:22:18 -0500

    Added GET for /users

--- server.js
+++ server.js
@@ -4,10 +4,32 @@ const bodyParser = require("body-parser");
 const server = express();
 const PORT = 3030;
 
+const users = {
+  1: "Joe",
+  2: "John",
+  3: "Jerry",
+};
+
+server.get("/users", (req, res) => {
+  res.status(200);
+  res.send(users);
+})
+
+server.post("/", (req, res) => {
+  const { user } = req.body;
+  if(!user) {
+    res.status(422);
+    res.json({ error: 'Must provide user'})
+    return;
+  }
+  users.push(user);
+  res.json({ users });
+});
+
 server.listen(PORT, err => {
   if (err) {
     console.log(`ERROR! ${err}`);
   } else {
-    console.loog(`SERVER! ${PORT}`);
+    console.log(`SERVER! ${PORT}`);
   }
 });
