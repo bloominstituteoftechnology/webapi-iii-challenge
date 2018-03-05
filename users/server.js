96bf38483fdb67615300e61551056f8155c98a88
ashleijo <shuryjo@gmail.com>
2018-03-05 15:56:28 -0500

    adding server function to get users by id

--- users/server.js
+++ users/server.js
@@ -8,21 +8,34 @@ const STATUS_SUCCESS = 200;
 let idCounter = 2;
 const users = [
   {
-    id: 1,
+    id: 0,
     name: "Bob",
     age: 20
   },
   {
-    id: 2,
+    id: 1,
     name: "Sue",
     age: 30
   }
 ];
 
 server.get("/users", (req, res) => {
+  /*if (req.query.name) {
+    const matchingUsers = users.filter(user => {
+      user.name === req.query.name
+    });
+    res.status(STATUS_SUCCESS);
+    res.send(matchingUsers);
+  } else {*/
+    res.status(STATUS_SUCCESS)
+    res.send(users);
+  //}
+})
 
-  
-  res.send(users);
+server.get("/users/:id", (req, res) => {
+  const { id } = req.params;
+  res.status(STATUS_SUCCESS);
+  res.send(users[id]);
 })
 
 server.listen(PORT, err => {
