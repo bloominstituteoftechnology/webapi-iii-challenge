8fca3cf08e36ceb07c3c63e9b2cd1d97e6e1f36f
Peter Gray <peter@petergraycreative.com>
2018-03-05 15:13:43 -0600

    Add Search get handling
    Returns matching user, not case sensitive

--- server.js
+++ server.js
@@ -18,17 +18,33 @@ const users = {
 
 server.use(bodyParser.json());
 
-server.get("/users", (req, res) => {
-    res.status(200);
-    res.send(users);
-})
-
-server.get("/users/:id", (req, res) => {
-    const { id } = req.params;
-    res.status(200);
-    res.send({ id: users[id] });
+server.get('/users', (req, res) => {
+  res.status(200);
+  res.send(users);
 });
 
+server.get('/users/:id', (req, res) => {
+  const { id } = req.params;
+  res.status(200);
+  res.send({ id: users[id] });
+});
+
+server.get('/search', (req, res) => {
+  if (req.query.name) {
+    let user = null;
+    let result = [];
+    Object.keys(users).forEach(id => {
+      if (users[id].toLowerCase() === req.query.name.toLowerCase()) {
+        result.push(users[id]);
+      }
+    });
+    res.status(200);
+    res.send(result);
+  } else {
+    res.status(200);
+    res.send(users);
+  }
+});
 
 server.post('/', (req, res) => {
   const user = req.body;
