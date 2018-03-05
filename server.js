d1f92d926e5011a76548b2e25e5377cc72ea54a4
Nathan Daniel <natdf@icloud.com>
2018-03-05 16:16:09 -0600

    WIP of search functionality

--- server.js
+++ server.js
@@ -1,29 +1,58 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const server = express();
 const PORT = 3030;
 
+let idCounter = 5;
 const users = {
-  1: "Joe",
-  2: "John",
-  3: "Jerry",
+  1: 'Joe',
+  2: 'John',
+  3: 'Jerry',
+  4: 'Joe',
+  5: 'Joe',
 };
 
-server.get("/users", (req, res) => {
+let results = [];
+
+server.get('/users', (req, res) => {
   res.status(200);
   res.send(users);
-})
+});
 
-server.post("/", (req, res) => {
-  const { user } = req.body;
-  if(!user) {
-    res.status(422);
-    res.json({ error: 'Must provide user'})
+server.get('/users/:id/', (req, res) => {
+  const id = req.params.id;
+  res.status(200);
+  res.send(users[id]);
+});
+
+server.get('/search', (req, res) => {
+  if (req.query.name) {
+    let user = null;
+    Object.keys(users).forEach(id => {
+      if (users[id] === req.query.name) {
+        results.push(user[id]);
+      }
+    });
+    res.status(200);
+    res.send(results);
+  } else {
+    res.status(200);
+    res.send(users);
+  }
+});
+
+server.post('/users', (req, res) => {
+  const user = req.body.user;
+  if (!user) {
+    res.status(402);
     return;
   }
-  users.push(user);
-  res.json({ users });
+
+  idCounter++;
+  users[idCounter] = user;
+  res.status(200);
+  res.send({ id: idCounter });
 });
 
 server.listen(PORT, err => {
