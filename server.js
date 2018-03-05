49526cee30690b3778172bce0a01526fb9dbdfb3
Jonathan Matthew Brunt <jonbrunt@Jonathans-MacBook-Pro.local>
2018-03-05 14:51:33 -0600

    Add GET method that is id specific

--- server.js
+++ server.js
@@ -20,6 +20,17 @@ server.get("/users", (req, res) => {
   res.send(users);
 });
 
+server.get("/users/:id", (req, res) => {
+  const { id } = req.params;
+  res.status(SUCCESS);
+  let friend = null;
+  users.forEach(val => {
+    if (val.id === id)
+    friend = val; 
+  });
+  res.send(friend);
+});
+
 server.listen(PORT, (err) => {
   if (err) {
     console.log("Something broke: ", err);
