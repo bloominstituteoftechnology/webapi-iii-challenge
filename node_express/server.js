520330fdb761a699e5de0fb6ed1c57ccf2a15606
shobanavv <shobanavv@gmail.com>
2018-03-05 15:02:58 -0800

    Search and Get works

--- node_express/server.js
+++ node_express/server.js
@@ -4,50 +4,63 @@ const bodyParser = require('body-parser');
 const server = express();
 const PORT = 3030;
 
+let idCounter = 3;
 const users = {
     1: 'Shobana',
     2: 'Tiffany',
-    3: 'Ivan'
+    3: 'Ivan',
+    4: 'shobana',
+    5: 'tiffany',
+    6: 'tiffany',
 };
 
 //middleware receives the request.
-server.use((req,res, next) => {
-    console.log("Got a request", req);
-    next();
+server.use(bodyParser.json());
+
+server.get("/", (req, res) => {
+    res.status(200);
+    res.send(users);
+});
+
+server.get('/search?', (req,res) => {
+  if (req.query.name){
+    let user = [];
+    Object.keys(users).forEach((id => {
+        if(users[id].toLowerCase() === req.query.name.toLowerCase() ) {
+          user.push(users[id]);
+        };
+    }));
+    res.status(200);
+    res.send(user);
+  } else {
+    res.status(200);
+    res.send(users);
+  }
 });
 
 // get req with id
-server.get('/:id/', (req,res )=> {
-  if(err) {
-    console.log(`Error occured ${err}`);
-  } else { 
-    if(req.query.name) {
-        let user = null;
-        Object.keys(users).forEach((id  =>{
-            if(users[id] === req.query.name) {
-                user = id;
-            }
-        }));
-        res.status(200);
-        res.send(user);
-    } else {
-        res.status(200);
-        res.send(users);
-    }
-
-  });
-}
-
-server.get('/', (req,res) => {
-    res.status(200);
-    res.send(users);
-
+server.get("/:id/", (req, res) => {
+const id = req.params;
+ res.status(200);
+ res.send(users[id]);
 });
 
-server.listen(PORT, (err) => {
+
+//post req with users
+server.post("/users/", (req, res) => {
+const user= req.body;
+idCounter++;
+users[idCounter] = name.name;
+res.send(users);
+});
+
+
+
+server.listen(PORT, err => {
     if(err) {
-        console.log(`There was an error ${err}`);
+        console.log("There was an error");
     } else {
-        console.log(`Server is listening on PORT number ${PORT}`)
+        console.log(`Server is listening on PORT number ${PORT}`);
     }
 });
+
