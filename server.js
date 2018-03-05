0f2a5cc9c7851c89d3656720cdba64df949c4651
Peter Gray <peter@petergraycreative.com>
2018-03-05 14:47:19 -0600

    Add post method to server.js
    Add, server listener, user dummy data, and post method.

--- server.js
+++ server.js
@@ -1,2 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
+const server = express();
+const PORT = 3030;
+
+server.listen(PORT, err => {
+  if (err) console.log(`Error is ${err}`);
+  else console.log(`Listening on port: ${PORT}`);
+});
+
+idCounter = 3;
+const users = {
+  1: 'Bob',
+  2: 'Bob2',
+  3: 'Bob3',
+};
+
+server.post('/', (req, res) => {
+  const user = req.body;
+
+  idCounter++;
+  users[idCounter] = user;
+  res.status(200);
+  res.send({ id: idCounter });
+});
