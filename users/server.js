7d9c75887deb4b8f1140efb608d2a9594edfce36
Smithface <kylewibblesmith@gmail.com>
2018-03-05 16:52:47 -0500

    added .post request

--- users/server.js
+++ users/server.js
@@ -5,7 +5,6 @@ const server = express();
 const PORT = 3000;
 const STATUS_SUCCESS = 200;
 
-let idCounter = 2;
 const users = [
   {
     id: 0,
@@ -18,19 +17,20 @@ const users = [
     age: 30
   }
 ];
+let idCounter = users.length;
 
 server.use(bodyParser.json());
 
 server.get("/users", (req, res) => {
   res.status(STATUS_SUCCESS);
   res.send(users);
-})
+});
 
 server.get("/users/:id", (req, res) => {
   const { id } = req.params;
   res.status(STATUS_SUCCESS);
   res.send(users[id]);
-})
+});
 
 server.get("/search", (req, res) => {
   if (req.query.name) {
@@ -42,14 +42,22 @@ server.get("/search", (req, res) => {
   } else {
     res.send("DIDN'T WORK");
   }
-})
+});
 
 server.delete("/users/:id", (req, res) => {
   const { id } = req.params;
   users.splice(id, 1);
   res.status(STATUS_SUCCESS);
   res.send(users);
-})
+});
+
+server.post("/users", (req, res) => {
+  const user = {...req.body, id: idCounter};
+  users[idCounter] = user;
+  let idCounter = users.length;
+  res.status(STATUS_SUCCESS);
+  res.send(users);
+});
 
 server.listen(PORT, err => {
   if (err) {
