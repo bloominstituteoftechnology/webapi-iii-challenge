fa10125b633ab87029191a4bac0b06999d889742
german gonzalez <gcomlnk@gmail.com>
2018-03-05 19:26:04 -0500

    search functionality

--- server-practice/server.js
+++ server-practice/server.js
@@ -7,35 +7,30 @@ const STATUS_SUCCESS = 200;
 const STATUS_USER_ERROR = 422;
 
 server.use((req, res, next) => {
-  next();
+    next();
 });
 
 server.use(bodyParser.json());
 
-let idCounter = 3;
-let users = [
-  { "id": 1, "name": "German"},
-  { "id": 2, "name": "Aaron"},
-  { "id": 3, "name": "Dylan"},
-];
+let idCounter = 0;
+let users = [];
 
 server.get('/users', (req, res) => {
-  console.log('req.query: ', req.query);
-  console.log('users: ', users);
-  console.log('users[1]: ', users[1]);
-  if (req.query.name) {
-    let user = null;
-    Object.keys(users).forEach((id => {
-      if (users[id] === req.query.name) {
-        user = id;
-      };
-    }));
-    res.status(STATUS_SUCCESS);
-    res.send(user);
-  } else {
-    res.status(STATUS_USER_ERROR);
-    res.send(users);
-  }
+    res.status(STATUS_SUCCESS).send(users);
+});
+
+server.get('/search', (req, res) => {
+    if (req.query.name) {
+        let user = [];
+        Object.keys(users).forEach((id => {
+            if (users[id].name.toUpperCase().includes(req.query.name.toUpperCase())) {
+                user.push(users[id]);
+            }
+        }));
+        res.status(STATUS_SUCCESS).send(user);
+    } else {
+        res.status(STATUS_SUCCESS).send(users);
+    }
 });
 
 server.post('/users', (req, res) => {
@@ -46,37 +41,27 @@ server.post('/users', (req, res) => {
     res.send(users);
 });
 
-server.get('/users', (req, res) => {
-    res.status(STATUS_SUCCESS);
-    res.send(users);
-});
-
 server.get('/users/:id', (req, res) => {
     const {id} = req.params;
     const foundUser = users.find((user) => user.id == id);
-
     if (foundUser) {
         res.status(STATUS_SUCCESS).send({foundUser});
         console.log('foundUser =>', foundUser);
     } else {
         res.status(STATUS_USER_ERROR)
     }
-
 });
 
 server.delete('/users/:id', (req, res) => {
     const {id} = req.params;
     const foundUser = users.find((user) => user.id == id);
-
     if (foundUser) {
-        const userRemoved = {...foundUser};
         users = users.filter(user => user.id != id);
         res.status(STATUS_SUCCESS).json({users});
     } else {
         res.status(STATUS_USER_ERROR);
     }
 });
-
 
 server.listen(PORT, (err) => {
     if (err) {
