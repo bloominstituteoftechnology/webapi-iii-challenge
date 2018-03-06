9abc534df2d06e2234afe0995a1330f83fdaa1f3
Igor Yermak <igor.yermak@gmail.com>
2018-03-05 20:55:05 -0500

    search query, POST and GET id

--- server.js
+++ server.js
@@ -17,10 +17,40 @@ server.get("/", (req, res) => {
   res.send(users);
 })
 
+server.get('/:id', (req, res) => {
+  const { id } = req.paramsl
+  res.status(200);
+  res.send(users[id]);
+});
+
+server.get("/", (req, res) => {
+  if (req.query.name) {
+    const user = users.find(target => target.name ===
+      req.query.name)
+
+  
+  }
+  res.status(200)
+  res.send(user);
+ 
+});
+
+server.post("/", (req, res) => {
+  const {
+    user
+  } = req.body;
+  users[idCounter] = user;
+  idCounter++;
+  res.status(200);
+  res.send(idCounter + "");
+
+})
+
 server.listen(PORT, err => {
   if (err) {
-    console.log(`There was an error starting the server: ${err}`);
+    console.log(`There was an error starting the server: ${err}`); 2
   } else {
     console.log(`Server is listening on ${PORT}`);
   }
 });
+
