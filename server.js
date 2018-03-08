dbe0ae51031641ab5f8a3931f83c696af9be4034
Charlie Sparks <csparks@antioch.edu>
2018-03-07 17:46:38 -0800

    4/5 objectives completed, running into a strange error with with my testing for one, though

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const port = 3000
+const USER_ERROR = 422;
 
 const server = express();
 server.use(bodyParser.json())
@@ -14,6 +15,33 @@ server.get("/", (req, res) => {
 
 server.get("/users", (req, res) => {
     res.send(users);
+})
+
+server.get("/users/:id", (req, res) => {
+    const id = req.params.id;
+    if (users.indexOf(id) > -1){
+        res.send(`${id}`)
+    } else {
+        res.status(USER_ERROR);
+        res.send("User not found");
+    }
+})
+
+server.get("/search", (req, res) => {
+    const name = req.query.name;
+    const result = [];
+    
+    if (!name){
+        res.send("No match found");
+        return;
+    } else {
+        users.forEach(person => {
+            if (person.toLowerCase() === name.toLowerCase()){
+                result.push(person);
+            }
+        })
+        res.send(result);
+    }
 })
 
 server.post("/users", (req, res) => {
