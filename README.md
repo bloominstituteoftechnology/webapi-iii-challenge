81923173284a9855a0bb162506d395d2dc81a681
Glenn-David Daniel <d2rd@outlook.com>
2018-03-10 22:06:20 -0800

    Refactored 'basses' object reducing it to 1 key/value pair.  Queries on 'make' and 'id' work

--- README.md
+++ README.md
@@ -21,10 +21,10 @@
 [x] Create a new folder and run `npm init` to create your `package.json` file.
 [x] Install npm packages: `npm i --save express body-parser`
 [ ] Create a server that makes the following routes function properly.
-[ ] * [POST] `/users` This route should save a new user to the server. (This is just in memory and will not persist if you restart the server.)
-[ ] * [GET] `/users` This route will return an array of all users.
-[ ] * [GET] `/users/:id` This route will return the user with the matching `id` property.
-[ ] * [GET] `/search?name=<query>` The query parameter passed to this route should specify the name of the user you are searching for.  Return an array of all users whose names match the name provided.  This search should not be case sensitive.
-[ ] * [DELETE] `/users/:id` This route should delete the specified user.
+[ ] * [POST] `/basses` This route should save a new user to the server. (This is just in memory and will not persist if you restart the server.)
+[ ] * [GET] `/basses` This route will return an array of all basses.
+[ ] * [GET] `/basses/:id` This route will return the user with the matching `id` property.
+[ ] * [GET] `/search?name=<query>` The query parameter passed to this route should specify the name of the user you are searching for.  Return an array of all basses whose names match the name provided.  This search should not be case sensitive.
+[ ] * [DELETE] `/basses/:id` This route should delete the specified user.
 
 Your user objects can take any form.  Just ensure that they have an `id` property.  You can generate this `id` property on the server any way you like.
--- server.js
+++ server.js
@@ -4,55 +4,67 @@ const server = express();
 const PORT = 3040;
 const STATUS_SUCCESS = 200;
 const STATUS_USER_ERROR = 422;
+
 let idCounter = null;
-let basses = 
+
+/*
+let bassList = 
 [
+  {
+    id: 0,
+    make: "Tobias",
+    model: "Toby",
+    strings: 5,
+    color: "Brown Sunburst",
+    purchaseLocation: "Bananas At Large, San Rafael",
+    purchasePrice: 850,
+    currentValue: 1500
+    },
     {
-        "id": 0,
-        "make": "Tobias",
-        "model": "Toby",
-        "strings": 5,
-        "color": "Brown Sunburst",
-        "purchaseLocation": "Bananas At Large, San Rafael",
-        "purchasePrice": 850,
-        "currentValue": 1500
-       },
-       {
-        "id": 1,
-        "make": "Fender",
-        "model": "Jazz Bass",
-        "strings": 4,
-        "color": "Blue",
-        "purchaseLocation": "Private",
-        "purchasePrice": 300,
-        "currentValue": 2300
-       },
+    id: 1,
+    make: "Fender",
+    model: "Jazz Bass",
+    strings: 4,
+    color: "Blue",
+    purchaseLocation: "Private",
+    purchasePrice: 300,
+    currentValue: 2300
+    },
   {
-    "id": 2,
-    "make": "Padula",
-    "model": "MVP-Buzz-5",
-    "strings": 5,
-    "color": "Brown Sunburst",
-    "purchaseLocation": "Guitar Center Hollywood",
-    "purchasePrice": 1800,
-    "currentValue": 2800
-  },
+    id: 2,
+    make: "Padula",
+    model: "MVP-Buzz-5",
+    strings: 5,
+    color: "Brown Sunburst",
+    purchaseLocation: "Guitar Center Hollywood",
+    purchasePrice: 1800,
+    currentValue: 2800
+    },
   {
-    "id": 3,
-    "make": "Hohner",
-    "model": "",
-    "strings": 4,
-    "color": "",
-    "purchaseLocation": "",
-    "purchasePrice": 0,
-    "currentValue": 0
+    id: 3,
+    make: "Hohner",
+    model: "",
+    strings: 4,
+    color: "",
+    purchaseLocation: "",
+    purchasePrice: 0,
+    currentValue: 0
   }
  ]
+ */
+let basses = 
+  {
+    0: "Tobias",
+    1: "Fender",
+    2: "Padula",
+    3: "Hohner"
+  }
+ 
 server.use((req, res, next) => {
     console.log("Request received, what about it? ", req);
     next();
 });
-server.use(bodyParser.json());
+server.use(bodyParser.json());  //calls bodyParser to handle inputs
 
 
 
@@ -72,13 +84,22 @@ server.get("/", (req, res) => {
     }
   });
 
-  //QUERY REQUEST
+//QUERY REQUEST ON id
 server.get("/:id/", (req, res) => {
   const {
     id
   } = req.params;
   res.status(STATUS_SUCCESS);
   res.send(basses[id])
+});
+
+//QUERY REQUEST ON make
+server.search("/:make/", (req, res) => {
+  const {
+    bass
+  } = req.query.make;
+  res.status(STATUS_SUCCESS);
+  res.send(bass[make])
 });
 
 //POST request
@@ -93,12 +114,23 @@ server.post("/", (req, res) => {
     res.send({ id: idCounter});
 })
 
+//DELETE request
+server.post("/", (req, res) => {
+  const {
+    bass 
+    }= req.body;
+
+    idCounter++;
+    basses[idCounter] = bass;
+    res.status(STATUS_SUCCESS);
+    res.send({ id: idCounter});
+})
 
   //TELL THE SERVER TO LISTEN TO 'PORT'
   server.listen(PORT,(err) => {
     if(err) {
-      console.log('There was an error starting the server: ${err}');
+      console.log(`There was an error starting the server: ${err}`);
     } else {
-      console.log('Server is listening on port ${PORT}');
+      console.log(`Server is listening on port ${PORT}`);
     }
   });
