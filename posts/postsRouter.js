743d680607957e8b5161ca85706fca71876d79ee
zackhitch <zackhitch@users.noreply.github.com>
2018-04-04 15:37:55 -0700

    Completed userRouter.js, WIP postsRouter.js - ZH

---
+++ posts/postsRouter.js
@@ -0,0 +1,7 @@
+const express = require('express');
+
+const router = express.Router();
+
+const userDb = require('../data/helpers/userDb.js');
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,13 +1,36 @@
 const express = require('express');
-const morgan = require('morgan');
+// const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
 
+const usersRouter = require('./users/usersRouter.js');
+const postsRouter = require('./posts/postsRouter.js');
+// const tagsRouter = require('./tags/tagsRouter.js');
+
+// const userDb = require('./data/helpers/userDb.js');
+// const postDb = require('./data/helpers/postDb.js');
+// const tagDb = require('./data/helpers/tagDb.js');
+
 const server = express();
 
+// custom middleware [m1, m2, mn] -> [request handlers]
+function logger(req, res, next) {
+  //next points to the next middleware
+  console.log(`requesting: ${req.url}`);
+  console.log('params: ', req.params);
+  // res.send('done');
+  // req.url = `${req.url}/1`; //changes the url of the request.
+  next();
+}
+
 server.use(helmet());
+// server.use(morgan());
 server.use(cors());
 server.use(express.json());
+server.use(logger);
+
+server.use('/api/users', usersRouter);
+server.use('/api/posts', postsRouter);
 
 const port = 5012;
 server.listen(port, () => console.log('API running on port 5012'));
