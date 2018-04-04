e0f700d45a263fe2ed87f6b85a2436a6f825e80a
kait-schorr <kaitlynschorr@gmail.com>
2018-04-04 15:36:06 -0500

    Implemented get for tags

--- server.js
+++ server.js
@@ -6,11 +6,13 @@ const db = require("./data/dbConfig.js");
 
 const userRouter = require("./users/userRouter.js");
 const postRouter = require("./posts/postRouter.js");
+const tagRouter = require("./tags/tagRouter.js");
 
 server.use(express.json());
 
 server.use("/api/posts/", postRouter);
 server.use("/api/users", userRouter);
+server.use("/api/tags", tagRouter);
 
 const port = 5000;
 server.listen(port, () => {
