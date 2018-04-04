a3c8b9d42349add8f73131807f1e528e7f87f599
Carlos Andrade <carlos.andrade@outlook.com>
2018-04-04 18:40:39 -0400

    we see the light

--- data/Routers/tag.js
+++ data/Routers/tag.js
@@ -5,19 +5,66 @@ const router = express.Router()
 const db = require('../helpers/tagDb');
 
 
-// router.get('/:id', (req, res)=> {
-//     const { id } = req.params;
-//     db.get(id)
-//     .then(user => {
-//         res.json(user)
-//     })
-//     .catch(error => {
-//         res.status(500).json(console.error( "Error is your face ", error ));
-//     })
+router.get('/', (req, res)=> {
 
-// })
+    db.get()
+    .then(tags => {
+        res.status(200).json(tags)
+    })
+    .catch(error => {
+        res.status(500).json(console.error( "Error getting tags list ", error ));
+    })
 
+})
 
+router.get('/:id', (req, res)=> {
+    const { id } = req.params;
+    db.get(id)
+    .then(tag => {
+        res.status(200).json(tag)
+    })
+    .catch(error => {
+        res.status(500).json(console.error( "Error getting tag ", error ));
+    })
+
+})
+
+router.post('/add', (req, res) => {
+    const tag = req.body
+
+    db.insert(tag)
+    .then(tag=> {
+        res.status(200).json(tag)
+    })
+    .catch(error => {
+        res.status(500).json(console.error( "Error posting tag ", error))
+    })
+})
+
+router.put('/:id/update', (req, res) => {
+    const { id } = req.params;
+    const tag = req.body
+
+    db.update(id, tag)
+    .then(tagged => {
+        res.status(200).json(tagged)
+    })
+    .catch(error => {
+        res.status(500).json(console.error( "Error updating tag", error))
+    })
+})
+
+router.delete('/:id/delete', (req, res) => {
+    const { id } = req.params;
+    
+    db.remove(id)
+    .then(removed => {
+        res.status(200).json(removed)
+    })
+    .catch(error => {
+        res.status(500).json(console.error( "Error deleting tag", error))
+    })
+})
 
 
 
