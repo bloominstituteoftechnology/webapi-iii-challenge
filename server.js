039af91741118f94501f4d3756507e2dd93f2ca4
Kevin Jolley <30558808+Serrowxd@users.noreply.github.com>
2018-04-04 16:33:42 -0500

    Blank Object?
    server.get is returning a blank object when attempting to return the users object.

--- server.js
+++ server.js
@@ -1,4 +1,32 @@
+// Server Imports
 const express = require('express');
-const db = require('./data/dbConfig.js')
+const postDb = require('./data/helpers/postDb.js');
+const tagDb = require('./data/helpers/tagDb.js');
+const userDb = require('./data/helpers/userDb.js');
 
+// Server
 const server = express();
+
+// Middleware
+server.use(express.json());
+
+// Server Code
+server.get('/', (req, res) => { // API Check
+  res.json({ api: 'Running..' });
+});
+
+server.get('/api/users/', (req, res) => { // Grab all Users
+
+  userDb
+    .get()
+    .then(response => {
+      res.json(users);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+// Port
+const port = 5000;
+server.listen(port, () => console.log('API Running on port 5000'));
