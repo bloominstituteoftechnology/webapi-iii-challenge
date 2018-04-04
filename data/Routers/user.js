6eea21ae6685a3038f7eb1c36efa73377ca993d8
Carlos Andrade <carlos.andrade@outlook.com>
2018-04-04 18:19:02 -0400

    completed users router

--- data/Routers/user.js
+++ data/Routers/user.js
@@ -4,32 +4,79 @@ const router = express.Router()
 
 const db = require('../helpers/userDb');
 
+router.get('/', (req, res)=> {
+
+    db.get()
+    .then(user => {
+        res.status(200).json(user)
+    })
+    .catch(error => {
+        res.status(500).json(console.error( "Error getting users list ", error ));
+    })
+
+})
 
 router.get('/:id', (req, res)=> {
     const { id } = req.params;
     db.get(id)
     .then(user => {
-        res.json(user)
+        res.status(200).json(user)
     })
     .catch(error => {
-        res.status(500).json(console.error( "Error is your face ", error ));
+        res.status(500).json(console.error( "Error getting user ", error ));
     })
 
 })
 
 router.get('/:id/posts', (req, res) => {
-    const { userId } = req.params;
+    const { id }  = req.params;
 
-    db.getUserPosts(userId)
+    db.getUserPosts(id)
     .then(posts => {
-        res.json(posts)
+        res.status(200).json(posts)
+      
     })
     .catch(error => {
-        res.status(500).json(console.error( "Error is your life ", error))
+        res.status(500).json(console.error( "Error pulling up posts ", error))
     })
 })
 
+router.post('/add', (req, res) => {
+    const user = req.body
 
+    db.insert(user)
+    .then(user=> {
+        res.status(200).json(user)
+    })
+    .catch(error => {
+        res.status(500).json(console.error( "Error posting ", error))
+    })
+})
+
+router.put('/:id/update', (req, res) => {
+    const { id } = req.params;
+    const user = req.body
+
+    db.update(id, user)
+    .then(updated => {
+        res.status(200).json(updated)
+    })
+    .catch(error => {
+        res.status(500).json(console.error( "Error updating ", error))
+    })
+})
+
+router.delete('/:id/delete', (req, res) => {
+    const { id } = req.params;
+    
+    db.remove(id)
+    .then(removed => {
+        res.status(200).json(removed)
+    })
+    .catch(error => {
+        res.status(500).json(console.error( "Error deleting ", error))
+    })
+})
 
 
 
