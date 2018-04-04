a6cc10eb918e61455aa22bc12122da1d304e0f89
astro11 <aquila.strother@asu.edu>
2018-04-04 18:46:08 -0400

    adding routes WIP

---
+++ data/users/userRouter.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+// const db = require('../data/dbConfig.js');
+
+// handles routes that start with: /api/users
+
+router.get('/', (req, res) => {
+
+});
