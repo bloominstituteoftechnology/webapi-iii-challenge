ec2ff494ff0941db148346e4999447dd075e7e68
mjd02 <mostdarryl@gmail.com>
2018-04-04 18:19:33 -0400

    completed posts

--- server.js
+++ server.js
@@ -1,35 +1,83 @@
 const express = require('express');
 const userDB = require('./data/helpers/userDb.js');
+const postDB = require('./data/helpers/postDb.js');
 
 const app = express();
 app.use(express.json());
 
+app.get('/api/posts/tags/:id',(req,res)=>{
+  postDB.getPostTags(req.params.id).then(response=>{
+    res.status(200).json(response);
+  })
+  .catch(err=>{
+    res.status(500).json(err);
+  });
+});
+
+app.get('/api/posts/:id',(req,res)=>{
+  postDB.get(req.params.id).then(response=>{
+    res.status(200).json(response);
+  })
+  .catch(err=>{
+    res.status(500).json(err);
+  });
+});
+
+app.post('/api/posts',(req,res)=>{
+  postDB.insert(req.body).then(response=>{
+    res.status(201).json(response);
+  })
+  .catch(err=>{
+    res.status(500).json(err);
+  });
+});
+
+app.put('/api/posts/:id',(req,res)=>{
+  postDB.update(req.params.id,req.body).then(response=>{
+    res.status(200).json(response);
+  })
+  .catch(err=>{
+    res.status(500).json(err);
+  });
+});
+
+app.delete('/api/posts/:id',(req,res)=>{
+  postDB.remove(req.params.id)
+  .then(response=>{
+    res.status(200).json(response);
+  })
+  .catch(err=>{
+    res.status(500).json(err);
+  });
+});
 
 app.get('/api/users',(req,res)=>{
   userDB.get().then(response=>{
-    res.json(response);
+    res.status(200).json(response);
   })
   .catch(err=>{
-    res.json(err);
+    res.status(500).json(err);
   });
 });
 
 app.put('/api/users/:id',(req,res)=>{
   userDB.update(req.params.id,req.body)
   .then(response=>{
-    res.json(response);
+    res.status(200).json(response);
   })
   .catch(err=>{
-    res.json(err);
+    res.status(500).json(err);
   });
 });
 
+
+
 app.post('/api/users',(req,res)=>{
   userDB.insert(req.body).then(response=>{
-    res.json(response);
+    res.status(200).json(response);
   })
   .catch(err=>{
-    res.json(err);
+    res.status(500).json(err);
   });
 });
 
