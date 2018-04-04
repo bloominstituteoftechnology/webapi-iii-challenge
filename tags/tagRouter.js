ab2cdb37085c314faf7005dcf5bd2cbdaa92edfa
Justin Kaseman <justinkaseman@live.com>
2018-04-04 16:18:04 -0700

    Finish tags CRUD

--- tags/tagRouter.js
+++ tags/tagRouter.js
@@ -19,4 +19,29 @@ router.get('/:id', (req, res) => {
     .catch(error => res.status(500).json(error));
 })
 
+router.post('/', (req, res) => {
+    const tag = req.body;
+
+    db.insert(tag)
+    .then(posts => res.status(200).json(posts))
+    .catch(error => res.status(500).json(error));
+})
+
+router.put('/:id', (req, res) => {
+    const id = req.params.id;
+    const updatedTag = { ...req.body, id};
+
+    db.update(id, updatedTag)
+    .then(posts => res.status(200).json(posts))
+    .catch(error => res.status(500).json(error));
+})
+
+router.delete('/:id', (req, res) => {
+    const id = req.params.id;
+
+    db.remove(id)
+    .then(posts => res.status(200).json(posts))
+    .catch(error => res.status(500).json(error));
+})
+
 module.exports = router;
