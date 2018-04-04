d35faeeddead806ecf0ed8f8c55aba66a5d96266
Liz McKenzie <lizmacbot@lizs-imac.attlocal.net>
2018-04-04 13:54:27 -0700

    completed insert in userRouter

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -4,27 +4,45 @@ const router = express.Router();
 
 const db = require("../data/helpers/userDb.js");
 
-
 router.get("/", (req, res) => {
-    db
-        .get()
-        .then(users => {
-            res.json(users);
-        })
-        .catch(error => {
-            res.status(500).json(error);
-        })
-})
+  db
+    .get()
+    .then(users => {
+      res.json(users);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
 
 router.get("/:id", (req, res) => {
+  db
+    .get(req.params.id)
+    .then(users => {
+      res.json(users);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+// something here and less than 128ch.
+router.push("/", (req, res) => {
+  const { user } = req.body;
+  if (!user) {
+    res.status(400).json({ error: "Name Required" });
+  } else if (user.length > 128) {
+    res.status(400).json({ error: "Max length 128 characters" });
+  } else {
     db
-        .get(req.params.id)
-        .then(users => {
-            res.json(users);
-        })
-        .catch(error => {
-            res.status(500).json(error);
-        })
-})
+      .insert()
+      .then(user => {
+        res.json(user);
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+  }
+});
 
 module.exports = router;
