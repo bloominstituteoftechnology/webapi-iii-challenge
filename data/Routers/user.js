c56286481a645dbf6998bf9aa31dd5b6346e75ed
Carlos Andrade <carlos.andrade@outlook.com>
2018-04-04 17:08:31 -0400

    got get good

--- data/Routers/user.js
+++ data/Routers/user.js
@@ -8,8 +8,8 @@ const db = require('../helpers/userDb');
 router.get('/:id', (req, res)=> {
     const { id } = req.params;
     db.get(id)
-    .then(users => {
-        console.log()
+    .then(user => {
+        res.json(user)
     })
     .catch(error => {
         res.status(500).json(console.error( "Error is your face ", error ));
