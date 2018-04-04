52ac062c3810444ebb519fa6e113a2e6b186b096
Jeremy Jones <jeremyjones@Macintosh-58b035f2f989.local>
2018-04-04 14:20:27 -0700

    setup complete

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
-const cors = rquire('cors');
+const cors = require('cors');
+
+// const userRouter = require('./users/userRoutes.js');
 
 const server = express();
 
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
+
+const port = 5000;
+server.listen(port, () => console.log('API running on port 5000'));
