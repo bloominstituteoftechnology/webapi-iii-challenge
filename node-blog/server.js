da49016f7083961ffcd26fe4dbe96283051a1871
happy <christopherstone@Christophers-MacBook-Pro.local>
2018-04-04 16:35:32 -0400

    Stone Chris | Node Blog
    Stone Chris | Node Blog

--- node-blog/server.js
+++ node-blog/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const server = express();
-
+// const db = require('../data/helpers');
 const port = 5000;
 
 server.listen(port , () => console.log('API Running on port 5000'));
+
+server.get('/', function(req, res){
+    res.send({api: 'Running...'})
+});
 
