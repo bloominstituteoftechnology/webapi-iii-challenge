cf5aa40f7df4ce46e7e2ca3705fba2a0f192fd8a
RiaClay <sherria.rydyn@gmail.com>
2018-04-04 17:59:32 -0500

    finished the GET for users, tags, and posts. Working on POST.

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const morgan = require('morgan');
+// const morgan = require('morgan');
 const helmet = require('helmet');
 
-// const userRouter = require('/.users/userRouter.js');
-// const postRouter = require('./posts/postRouter.js');
-// const tagRouter = require('./tag/tagRouter.js');
+const userRouter = require('./users/userRouter.js');
+const postRouter = require('./posts/postRouter.js');
+const tagRouter = require('./tags/tagRouter.js');
 
 const server = express();
 
@@ -14,18 +14,20 @@ function logger (req, res, next) {
     next();
 }
 
-const port = 5000;
-server.listen(port, () => console.log("API is now running on localhost: 5000"));
-
-// server.use(helmet());
+server.use(helmet());
 // server.use(morgan('dev'));
-// server.use(express.json());
-// server.use(logger);
-// server.use('/api/users', userRouter);
-// server.use('/api/posts', postRouter);
-// server.use('/api/tags', tagRouter);
+server.use(express.json());
+server.use(logger);
+
+server.use('/api/users', userRouter);
+server.use('/api/posts', postRouter);
+server.use('/api/tags', tagRouter);
 
 server.get('/', (req, res) => {
     res.json({ api: "Connected....." });
 })
+
+const port = 5000;
+server.listen(port, () => console.log("API is now running on localhost: 5000"));
+
 
--- users/userRouter.js
+++ users/userRouter.js
@@ -2,7 +2,40 @@ const express = require('express');
 
 const router = express.Router();
 
-const userDB = require('../data/helpers/userDb.js');
+const db = require('../data/helpers/userDb.js');
+
+
+
+router.get('/', (req, res) => {
+   db
+
+     .get()
+     .then(users => {
+         res.json(users);
+     })
+     .catch(error => {
+         res.status(500).json({ error: "A list of users could not be retrieved." });
+     })
+})
+
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    
+    db
+
+     .get(id)
+     .then(user => {
+         if (user === undefined) {
+             res.status(404).json({ error: "User ID ${id} could not be found"});
+         } else {
+             res.status(200).json(user)
+         }       
+     })
+     .catch(error => {
+         res.status(500).json({ error: "Could not retrieve user information." });
+     })
+})
+
 
 module.exports = router;
 
