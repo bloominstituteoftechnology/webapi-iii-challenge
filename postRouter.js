c29fc94cfdecc9a5d59bedc97ceac13abca22cf8
runranron <williamrpelton@gmail.com>
2018-04-04 17:51:06 -0500

    William Pelton WIP

---
+++ postRouter.js
@@ -0,0 +1,55 @@
+const postRouter = require('express').Router();
+
+const postDb = require('./data/helpers/postDb');
+
+postRouter.get('/', (req, res) => {
+    postDb.get().then(posts => {
+        res.json(posts);
+    }).catch(error => {
+        res.status(500).json({error: "The posts could not be retrieved."});
+    })
+});
+
+postRouter.get('/:id', (req, res) => {
+    const { id } = req.params;
+    postDb.get(id).then(post => {
+        res.json(post);
+    }).catch(error => {
+        res.status(500).json({error: "This post could not be retrieved."})
+    })
+});
+
+postRouter.post('/', (req, res) => {
+    postDb.insert(req.body).then(response => {
+        res.json({ ...response, ...req.body});
+    }).catch(error => {
+        res.status(500).json(error);
+    })
+});
+
+postRouter.delete('/:id', (req, res) => {
+    const { id } = req.params;
+
+    postDb
+        .get(id)
+        .then(response => {
+            const post = response;
+            if(!post) {
+                res.status(404).json({ message: "The post with the specified ID does not exist." });
+                return;
+            }
+        postDb
+            .remove(id)
+            .then(response => {
+                res.json(user);
+            })
+            .catch(error => {
+                res.status(500).json({ error: "The post could not be removed" });
+            });
+    })
+    .catch(error => {
+        res.status(500).json({ error: "The user post not be removed" });
+    })
+});
+
+module.exports = postRouter;
---
+++ userRouter.js
@@ -0,0 +1,106 @@
+const userRouter = require('express').Router();
+
+const userDb = require('./data/helpers/userDb');
+
+
+userRouter.get('/', (req, res) => {
+    userDb.get().then(users => {
+        res.json(users);
+    }).catch(error => {
+        res.status(500).json('{error: "The users information could not be retrieved."}');
+    })
+});
+
+userRouter.get('/:id', (req, res) => {
+    const { id } = req.params;
+
+    userDb
+        .get(id)
+        .then(user => {
+            if(!user) {
+                res.status(404).json({error: 'Your hobbit was not found.'});
+                return;
+            }
+            res.status(200).json(user);
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        });
+});
+
+userRouter.get('/:id/posts', (req, res) => {
+    const { id } = req.params;
+
+    userDb
+        .getUserPosts(id)
+        .then(posts => {
+            res.status(200).json(posts);
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        });
+});
+
+userRouter.post('/', (req, res) => {
+    const name = req.body.name;
+
+    if(!name) {
+        res.json('User must have a name.');
+        return;
+    }
+
+    userDb.insert(req.body).then(response => {
+        res.json({ ...response, ...req.body});
+    }).catch(error => {
+        res.status(500).json(error);
+    })
+});
+
+userRouter.delete('/:id', (req, res) => {
+    const { id } = req.params;
+
+    userDb
+        .get(id)
+        .then(response => {
+            const user = response;
+            if(!user) {
+                res.status(404).json({ message: "The user with the specified ID does not exist." });
+                return;
+            }
+        userDb
+            .remove(id)
+            .then(response => {
+                res.json(user);
+            })
+            .catch(error => {
+                res.status(500).json({ error: "The user could not be removed" });
+            });
+    })
+    .catch(error => {
+        res.status(500).json({ error: "The user could not be removed" });
+    })
+});
+
+userRouter.put('/:id', (req, res) => {
+    const { id } = req.params;
+    const update = req.body;
+
+    userDb
+        .update(id, update)
+        .then(count => {
+            if(count > 0) {
+                userDb.get(id).then(updatedUser => {
+                    res.status(200).json(updatedUser);
+                });
+            } else {
+                res
+                    .status(404)
+                    .json({ message: 'The user with the specified ID does not exist.'});
+            }
+        })
+    .catch(error => {
+        res.status(500).json(error);
+    })
+});
+
+module.exports = userRouter;
