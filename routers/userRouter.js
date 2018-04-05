72938a95dbe07a7bf45351016d727003524677b0
Jacob Henry <jacobiswinning@gmail.com>
2018-04-05 16:16:52 -0600

    added put, delete

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -29,56 +29,59 @@ router.get("/:id", (req, res) => {
 });
   
 
-// router.post('/', (req, res) => {
-//     const user = req.body;
-//     db.insert(user)
-//     .then(response => {
-//         res.status(201).json(response)
-//     })
-//     .catch(error => {
-//         res.status(500).json({ error: "The users information could not be retrieved." })
-//     })
-// })
+router.post('/', (req, res) => {
+    const user = req.body;
+    db.insert(user)
+    .then(response => {
+        res.status(201).json(response)
+    })
+    .catch(error => {
+        res.status(500).json({ error: "The users information could not be retrieved." })
+    })
+})
 
-// router.delete('/:id', (req, res) => {
-//     const { id } = req.params;
-//     let user;
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    //const user = req;
+    db.remove(id)
+        .then(response => {
+            res.status(200).json(user);
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        })
 
-//     db.findById(id)
+   
+})
 
-//     .then(response => {
-//         user = {...response[0]};
-        
-//         db.remove(id)
-//         .then(response => {
-//             res.status(200).json(user);
-//         })
-//         .catch(error => {
-//             res.status(500).json(error);
-//         })
-//     })
-//     .catch(error => {
-//         res.status(500).json(error);
-//     })
-// })
-
-// router.put('/:id', (req, res) => {
-//     const { id } = req.params;
-//     const update = req.body;
-
-//     db.update(id, update)
-//     .then(count => {
-//         if (count > 0) {
-//             db.findById(id).then(updatedUsers => {
-//                 res.status(200).json(updatedUsers[0])
-//             })
-//         } else {
-//             res.status(404).json({ message: "The user with the specified ID does not exist." })
-//         }
-//     })
-//     .catch(error => {
-//         res.status(500).json(error);
-//     });
-// });
+router.put('/:id', (req, res) => {
+  const { id } = req.params;
+  const user = req.body;
+  if (!user.name) {
+    res.status(400).json({ error: 'Name is Required' });
+  } else if (user.name.length > 128) {
+    res.status(400).json({ error: 'The Max length is 128 characters' });
+  } else {
+    db
+      .update(id, user)
+      .then(count => {
+        if (count > 0) {
+          db
+            .get(id)
+            .then(user => {
+              res.json(user);
+            })
+            .catch(error => {
+              res.status(500).json(error);
+            });
+        } else {
+          res.status(404).json({ errorMessage: 'User not found' });
+        }
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+  }
+});
 
 module.exports = router;
