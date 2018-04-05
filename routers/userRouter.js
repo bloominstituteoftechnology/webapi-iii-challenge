03f9ba98d48129e55c04a8401db33d8b2a6f45fb
Liz McKenzie <elizabethbraggisgreat@gmail.com>
2018-04-05 16:57:47 -0400

    Added stuff to stuff to make stuff

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -68,4 +68,32 @@ router.get('/:id/posts', (req, res) => {
       res.status(500).json(error);
     });
 });
+
+router.put('/:id', (req, res) => {
+  const { id } = req.params;
+  const user = req.body;
+  if (!user.name) {
+    res.status(400).json({ error: 'Name Required' });
+  } else if (user.name.length > 128) {
+    res.status(400).json({ error: 'Max length 128 characters' });
+  } else {
+    db
+      .update(id, user)
+      .then(count => {
+        if (count > 0) {
+          db
+            .get(id)
+            .then(user => {
+              res.json(user);
+            })
+            .catch(error => {
+              res.status(500).json(error);
+            });
+        } else {
+          res.status(404).json({ errorMessage: 'User not found' });
+        }
+      })
+      .catch(error => {});
+  }
+});
 module.exports = router;
