6eb5f618b885b7ae2569b7c0b0e2050536ab3e53
charletta <charlettabullard12@outlook.com>
2018-04-05 16:23:22 -0400

    Added to the postRouter.js and restructured server.js again...still bugs WIP

--- server.js
+++ server.js
@@ -1,32 +1,16 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-
-// const postDb = require('./data/helpers/postDb.js');
-// const tagDb = require('./data/helpers/tagDb.js');
-// const userDb = require('./data/helpers/userDb.js');
 
 const userRouter = require('./users/userRouter.js');
 const postRouter = require('./posts/postRouter.js');
 const tagRouter = require('./tags/tagRouter.js');
 
 const server = express();
-var helmet = require('helmet')
-
-function logger(req, res, next) {
-  console.log(`requesting: ${req.url}`);
-
-  next();
-}
 
 //middleware
 server.use(express.json());
-app.use(helmet())
-server.use(logger);
-
 server.use('/api/users', userRouter);
 server.use('/api/posts', postRouter);
 server.use('/api/tags', tagRouter);
 
 const port = 5000;
 server.listen(port, () => console.log('API Running on port 5000'));
-
