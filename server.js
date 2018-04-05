e93234513fb52a1bc215394114ad7592380ae788
Justin Kaseman <justinkaseman@live.com>
2018-04-04 19:53:10 -0700

    Tag Uppercasing Middleware

--- server.js
+++ server.js
@@ -7,16 +7,18 @@ const postRouter = require('./posts/postRouter.js');
 const server = express();
 
 function uppercasemachine (req, res, next) {
-    res.send(req.body.toUpperCase());
+    if ((!!req.body.tag)) {
+        req.body = { "tag": `${req.body.tag.toUpperCase()}`}
+    }
     next();
 }
 
-//server.use(uppercasemachine);
-
 server.use(express.json());
+server.use(uppercasemachine);
 server.use('/api/posts', postRouter);
 server.use('/api/tags', tagRouter);
 server.use('/api/users', userRouter);
+
 
 const port = 5000;
 server.listen(port, () => console.log('API launched on port 5000'));
