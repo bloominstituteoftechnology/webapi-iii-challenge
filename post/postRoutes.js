7ba86ad8795bc6e65de794f5dff03e0486b3aaa4
Megan Williamson <meganawilliamson@gmail.com>
2018-04-05 15:33:31 -0700

    post endpoints complete

---
+++ post/postRoutes.js
@@ -0,0 +1,105 @@
+const express = require('express');
+
+const router = express.Router();
+
+const db = require('../data/helpers/postDb.js');
+
+//endpoint for /api/posts
+
+//GET REQUEST
+router.get('/', (req, res) => {
+  db
+    .get()
+    .then((posts) => {
+      res.status(200).json(posts);
+    })
+    .catch((error) => {
+      res.status(500).json({ error: 'The posts could not be retrieved' });
+    });
+});
+
+router.get('/:postId/', (req, res) => {
+  const { postId } = req.params;
+
+  db
+    .get(postId)
+    .then((post) => {
+      res.status(200).json(post);
+    })
+    .catch((error) => {
+      res.status(500).json({ error: 'The posts could not be retrieved' });
+    });
+});
+
+router.get('/:postId/tags', (req, res) => {
+  const { postId } = req.params;
+
+  db
+    .getPostTags(postId)
+    .then((tags) => {
+      res.status(200).json(tags);
+    })
+    .catch((error) => {
+      res.status(500).json({ error: 'The users posts could not be retrieved' });
+    });
+});
+
+// //GET DELETE
+
+router.delete('/:postId', (req, res) => {
+  const { postId } = req.params;
+
+  db
+    .remove(postId)
+    .then((post) => {
+      if (post > 0) {
+        res.status(200).json({ message: 'Post deleted successfully!' });
+      } else {
+        res.status(404).json({ error: 'Post not found' });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({ error: 'The Post could not be retrieved' });
+    });
+});
+
+// //GET POST
+
+router.post('/', (req, res) => {
+  const post = req.body;
+
+  if (!req.body.text || !req.body.userId) {
+    res.status(400).json({ errorMessage: 'UserId and text required' });
+    return;
+  }
+
+  db
+    .insert(post)
+    .then((post) => {
+      res.status(201).json({ message: 'Created a new post successfully ' });
+    })
+    .catch((error) => {
+      res.status(500).json({ error: 'The new post could not be created' });
+    });
+
+  // res.json({q:2});
+});
+
+// //GET PUT
+
+router.put('/:postId', (req, res) => {
+  const { postId } = req.params;
+  const updatedPost = req.body;
+
+  db
+    .update(postId, updatedPost)
+    .then((updates) => {
+      res.status(200).json(updatedPost);
+    })
+    .catch((error) => {
+      res.status(500).json({ error: 'The new users could not be retrieved' });
+    });
+  // res.json({q:2});
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,21 +1,18 @@
 const express = require('express');
-const usersRoutes = require('./users/usersRoutes');
-const db = require('./data/helpers/postDb.js');
 
+const usersRoutes = require('./users/usersRoutes.js');
+const postRoutes = require('./post/postRoutes.js');
+const tagsRoutes = require('./tags/tagRoutes.js');
 
 const server = express();
 
-server.use(express.json()); // parser
+server.use(express.json());
 server.use('/api/users', usersRoutes);
-
-// Endpoints
-// /api/users/:id/posts
-
-// TEST BELOW
-// server.get('/', (req,res) => {
-//     res.json({q:1});
-// });
+server.use('/api/posts', postRoutes);
+server.use('/api/tags', tagsRoutes);
 
 const port = 5000;
 
-server.listen(port, () => {console.log("Server running on port 5000")});
+server.listen(port, () => {
+  console.log('Server running on port 5000');
+});
