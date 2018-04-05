5d6f77b6382ae03dbf624899de077d95642f57b2
nourmhassan <nour-pop1@hotmail.com>
2018-04-05 12:18:42 -0700

    userRouter

--- server.js
+++ server.js
@@ -1,24 +1,36 @@
 const express = require ('express');
 const helmet = require ('helmet');
-const morgan = require ('morgan');
+// const morgan = require ('morgan');
 const cors = require ('cors');
 const bodyParser = require ('body-parser');
 
+
+const userRouter = require('./users/userRouter.js');
+const postRouter = require('./users/postRouter.js');
+const tagRouter = require('./users/tagRouter.js');
+
+
 const server = express();
+
+
+function logger(req, res, next) {
+    next();
+}
+
+server.use(cors());
+server.use(helmet());
+server.use(express.json());
+server.use(logger);
+
+server.use('/api/users', userRouter);
+server.use('/api/posts', postRouter);
+server.use('/api/tags', tagRouter);
+
+
+
 server.get('/' ,( req ,res )=> {
     res.josn ({ api : 'running...'});
 } );
-
-
-
-
-
-
-
-
-
-
-
 
 
 
