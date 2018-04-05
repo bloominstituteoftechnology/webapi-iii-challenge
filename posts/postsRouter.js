6116da50379798b68b551069694d2e3812eea1d0
zackhitch <zackhitch@users.noreply.github.com>
2018-04-04 17:20:41 -0700

    Completed postsRouter, todo: tasksRouter, stretch - ZH

--- posts/postsRouter.js
+++ posts/postsRouter.js
@@ -2,6 +2,95 @@ const express = require('express');
 
 const router = express.Router();
 
-const userDb = require('../data/helpers/userDb.js');
+const postDb = require('../data/helpers/postDb.js');
+
+// /api/posts
+router.get('/', (req, res) => {
+  postDb
+    .get()
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  postDb
+    .get(id)
+    .then(post => {
+      res.status(200).json(post);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+router.get('/:id/tags', (req, res) => {
+  const { id } = req.params;
+  postDb
+    .getPostTags(id)
+    .then(tags => {
+      res.status(200).json(tags);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+router.post('/', (req, res) => {
+  postDb
+    .insert(req.body)
+    .then(response => {
+      res.status(200).json(response);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+router.put('/:id', (req, res) => {
+  const { id } = req.params;
+  const update = req.body;
+  postDb
+    .update(id, update)
+    .then(count => {
+      if (count > 0) {
+        postDb
+          .get(id)
+          .then(post => {
+            res.status(200).json(post[0]);
+          })
+          .catch();
+      } else {
+        res.status(404).json({ message: 'post at that id doesnt exist' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  postDb
+    .get(id)
+    .then(response => {
+      post = { ...response[0] };
+      postDb
+        .remove(id)
+        .then(response => {
+          res.status(200).json(post);
+        })
+        .catch(error => {
+          res.status(404).json(error);
+        });
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
 
 module.exports = router;
