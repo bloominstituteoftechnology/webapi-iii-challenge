2dfd0f55bcdd3fdfee57e971216d1b449cfd0915
Unknown <DragonRydrz@me.com>
2018-04-05 18:23:59 -0400

    base project completed.

--- routers/postRouter.js
+++ routers/postRouter.js
@@ -1,7 +1,173 @@
-const express = require("express");
+const express = require('express');
 
 const router = express.Router();
 
-const db = require("../data/helpers/userDb.js");
+const db = require('../data/helpers/postDb.js');
+
+router.get('/', (req, res) => {
+  db
+    .get()
+    .then(posts => {
+      res.json(posts);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+// router.get('/:id', (req, res) => {
+//   console.log('inside it');
+//   const { id } = req.params;
+//   const postIds = [];
+
+//   db
+//     .get()
+//     .then(posts => {
+//       posts.forEach(post => {
+//         postIds.push(post.id);
+//       });
+//       if (postIds.includes(id)) {
+//         db
+//           .get(id)
+//           .then(post => {
+//             console.log('inside the postssssss');
+
+//             res.json(post);
+//           })
+//           .catch(error => {
+//             res.status(500).json(error);
+//           });
+//       }
+//     })
+//     .catch(error => res.status(500).json({ error }));
+// });
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  const postIDs = [];
+
+  db.get().then(posts => {
+    posts.forEach(post => {
+      postIDs.push(post.id);
+    });
+
+    if (postIDs.includes(Number(id))) {
+      db
+        .get(id)
+        .then(post => {
+          res.json(post);
+        })
+        .catch(error => {
+          res.status(500).json(error);
+        });
+    } else {
+      res.status(404).json({ message: 'Post does not exist with that ID.' });
+    }
+  });
+});
+
+// something here and less than 128ch. no duplicates.
+router.post('/', (req, res) => {
+  const post = req.body;
+  console.log(post);
+  const postsList = [];
+  db
+    .get()
+    .then(posts => {
+      posts.forEach(post => {
+        postsList.push(post.text);
+      });
+      if (!post.text) {
+        res.status(400).json({ error: 'Text Required' });
+      } else if (post.text.length > 128) {
+        res.status(400).json({ error: 'Max length 128 characters' });
+      } else if (postsList.includes(post.text)) {
+        res.status(400).json({ error: 'Post already exists' });
+      } else {
+        db
+          .insert(post)
+          .then(post => {
+            res.json(post);
+          })
+          .catch(error => {
+            console.log('in the catch');
+            res.status(500).json(error);
+          });
+      }
+    })
+    .catch(error => res.status(500).json({ error }));
+});
+
+router.get('/:id/tags', (req, res) => {
+  const { id } = req.params;
+
+  db
+    .getPostTags(id)
+    .then(tags => {
+      if (!tags[0]) {
+        res
+          .status(200)
+          .json({ message: 'This post does not contain any tags.' });
+      } else {
+        res.json(tags.map(tag => tag.tag));
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+router.put('/:id', (req, res) => {
+  const { id } = req.params;
+  const post = req.body;
+  if (!post.text) {
+    res.status(400).json({ error: 'Text Required' });
+  } else if (post.text.length > 128) {
+    res.status(400).json({ error: 'Max length 128 characters' });
+  } else {
+    db
+      .update(id, post)
+      .then(count => {
+        if (count > 0) {
+          db
+            .get(id)
+            .then(post => {
+              res.json(post);
+            })
+            .catch(error => {
+              res.status(500).json(error);
+            });
+        } else {
+          res.status(404).json({ errorMessage: 'Post not found' });
+        }
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+  }
+});
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  db
+    .remove(id)
+    .then(count => {
+      if (count > 0) {
+        db
+          .get()
+          .then(post => {
+            res.json(post);
+          })
+          .catch(error => {
+            res.status(500).json(error);
+          });
+      } else {
+        res.status(404).json({ errorMessage: 'Post not found' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
 
 module.exports = router;
--- routers/tagRouter.js
+++ routers/tagRouter.js
@@ -1,7 +1,125 @@
-const express = require("express");
+const express = require('express');
 
 const router = express.Router();
 
-const db = require("../data/helpers/userDb.js");
+const db = require('../data/helpers/tagDb.js');
+
+router.get('/', (req, res) => {
+  db
+    .get()
+    .then(tags => {
+      res.json(tags);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  const tagsArray = [];
+
+  db
+    .get()
+    .then(tags => {
+      const lookingForTag = tags.filter(tag => tag.id === Number(id));
+      if (lookingForTag[0]) {
+        res.json(lookingForTag[0]);
+      } else {
+        console.log('error 1');
+        res
+          .status(404)
+          .json({ message: 'The tag you are looking for, does not exist.' });
+      }
+    })
+    .catch(error => {
+      console.log('error 2');
+      res.status(500).json(error);
+    });
+});
+
+// something here and less than 128ch. no duplicates.
+router.post('/', (req, res) => {
+  const tag = req.body;
+  const tagsList = [];
+  db
+    .get()
+    .then(tags => {
+      tags.forEach(tag => {
+        tagsList.push(tag.tag);
+      });
+      if (!tag.tag) {
+        res.status(400).json({ error: 'Tag Required' });
+      } else if (tag.tag.length > 128) {
+        res.status(400).json({ error: 'Max length 128 characters' });
+      } else if (tagsList.includes(tag.tag)) {
+        res.status(400).json({ error: 'Tag already exists' });
+      } else {
+        db
+          .insert(tag)
+          .then(tag => {
+            res.json(tag);
+          })
+          .catch(error => {
+            console.log('in the catch');
+            res.status(500).json(error);
+          });
+      }
+    })
+    .catch(error => res.status(500).json({ error }));
+});
+
+router.put('/:id', (req, res) => {
+  const { id } = req.params;
+  const tag = req.body;
+  if (!tag.tag) {
+    res.status(400).json({ error: 'Tag Required' });
+  } else if (tag.tag.length > 128) {
+    res.status(400).json({ error: 'Max length 128 characters' });
+  } else {
+    db
+      .update(id, tag)
+      .then(count => {
+        if (count > 0) {
+          db
+            .get(id)
+            .then(tag => {
+              res.json(tag);
+            })
+            .catch(error => {
+              res.status(500).json(error);
+            });
+        } else {
+          res.status(404).json({ errorMessage: 'Tag not found' });
+        }
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+  }
+});
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  db
+    .remove(id)
+    .then(count => {
+      if (count > 0) {
+        db
+          .get()
+          .then(tag => {
+            res.json(tag);
+          })
+          .catch(error => {
+            res.status(500).json(error);
+          });
+      } else {
+        res.status(404).json({ errorMessage: 'Tag not found' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
 
 module.exports = router;
