30d4244183262130c2ec79749e60a009aecf9fa7
Liz McKenzie <elizabethbraggisgreat@gmail.com>
2018-04-05 16:35:51 -0400

    added getpostbyID

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,10 +1,10 @@
-const express = require("express");
+const express = require('express');
 
 const router = express.Router();
 
-const db = require("../data/helpers/userDb.js");
+const db = require('../data/helpers/userDb.js');
 
-router.get("/", (req, res) => {
+router.get('/', (req, res) => {
   db
     .get()
     .then(users => {
@@ -15,7 +15,7 @@ router.get("/", (req, res) => {
     });
 });
 
-router.get("/:id", (req, res) => {
+router.get('/:id', (req, res) => {
   db
     .get(req.params.id)
     .then(users => {
@@ -27,7 +27,7 @@ router.get("/:id", (req, res) => {
 });
 
 // something here and less than 128ch. no duplicates.
-router.post("/", (req, res) => {
+router.post('/', (req, res) => {
   const user = req.body;
   console.log(user);
   const usersList = [];
@@ -38,11 +38,11 @@ router.post("/", (req, res) => {
         usersList.push(user.name);
       });
       if (!user.name) {
-        res.status(400).json({ error: "Name Required" });
+        res.status(400).json({ error: 'Name Required' });
       } else if (user.name.length > 128) {
-        res.status(400).json({ error: "Max length 128 characters" });
+        res.status(400).json({ error: 'Max length 128 characters' });
       } else if (usersList.includes(user.name)) {
-        res.status(400).json({ error: "User already exists" });
+        res.status(400).json({ error: 'User already exists' });
       } else {
         db
           .insert(user)
@@ -50,7 +50,7 @@ router.post("/", (req, res) => {
             res.json(user);
           })
           .catch(error => {
-            console.log("in the catch");
+            console.log('in the catch');
             res.status(500).json(error);
           });
       }
@@ -58,4 +58,14 @@ router.post("/", (req, res) => {
     .catch(error => res.status(500).json({ error }));
 });
 
+router.get('/:id/posts', (req, res) => {
+  db
+    .getUserPosts(req.params.id)
+    .then(posts => {
+      res.json(posts.map(post => post.text));
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
 module.exports = router;
