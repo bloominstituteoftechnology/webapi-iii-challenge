d33602f2c8096bbfc7ee3bf0f5b90dc973eb89aa
Shaun Kelly <Shaun@redskelly.local>
2018-04-04 23:07:13 -0400

    finishing endpoints for userRouter, also meeting schema guidelines tbd

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
 
 const postRouter = require('./posts/postRouter');
 const tagRouter = require('./tags/tagRouter');
@@ -6,8 +8,26 @@ const userRouter = require('./users/userRouter');
 
 const server = express();
 
-// middleware
+// custom middleware
+const logger = (req, res, next) => {
+  console.log('Serving up params: ', req.params);
 
+  next();
+}
+
+// middleware
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+server.use(logger);
+
+server.use('/api/posts', postRouter);
+server.use('/api/tags', tagRouter);
+server.use('/api/users', userRouter);
+
+server.get('/', (req, res) => {
+  res.json({ api: 'Running...' });
+});
 
 const port = 5000;
 server.listen(port, () => console.log('API Running on port 5000'));
--- users/userRouter.js
+++ users/userRouter.js
@@ -4,5 +4,70 @@ const router = express.Router();
 
 const db = require('../data/helpers/userDb');
 
+router.get('/', (req,res) => {
+  
+  db.get().then(user => {
+      res.status(200).json(user);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    })
+})
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  
+  db.get(id)
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    })
+})
+
+router.get('/:id/posts', (req, res) => {
+  const { id } = req.params;
+  
+  db.getUserPosts(id)
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    })
+})
+
+router.post('/', (req, res) => {
+  const user = req.body;
+
+  db.insert(user)
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    })
+})
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+
+  db.get(id)
+    .then(response => {
+      user = { ...response[0] };
+
+      db.remove(id)
+        .then(response => {
+          res.status(200).json(user);
+        })
+        .catch(error => {
+          res.status(500).json(error);
+        });
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+})
 
 module.exports = router;
