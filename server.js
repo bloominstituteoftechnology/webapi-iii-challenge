603b684f959f6306593ead8979c2cbebf32ad9e3
Cesar Mejia Leiva <cmejia@gmail.com>
2018-04-05 07:33:58 -0700

    client installed

--- server.js
+++ server.js
@@ -9,15 +9,23 @@ const routerTag = require('./tags/routerTag');
 
 const server = express();
 
+function tagToUpperCase(req, res, next) {
+  console.log('req.body.tag', req.body.tag);
+  if (req.body.tag) req.body.tag = req.body.tag.toUpperCase();
+  console.log('req.body.tag', req.body.tag);
+  next();
+}
+
 // middleware use
 server.use(morgan('dev'));
 server.use(helmet());
 server.use(express.json());
 server.use(CORS());
+server.use(tagToUpperCase);
 
 server.use('/api/users', routerUser);
 server.use('/api/posts', routerPost);
 server.use('/api/tags', routerTag);
 
-const port = 5000;
-server.listen(port, () => console.log('API Running on port 5000'));
+const port = 5001;
+server.listen(port, () => console.log('API Running on port 5001'));
