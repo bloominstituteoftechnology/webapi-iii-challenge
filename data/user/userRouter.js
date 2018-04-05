2be2f7a4c6fae75a14ffd4f4ccc62b75fb2f3842
Cristina Laryea <crispad06@gmail.com>
2018-04-05 16:31:40 -0400

    gets are working

--- data/user/userRouter.js
+++ data/user/userRouter.js
@@ -8,8 +8,49 @@ router.get('/', (req, res) => {
     db
     .get()
     .then(users => {
-        res.json(users);
+        res.status(200).json(users);
+    })
+    .catch(error => {
+        res.status(500).json(error);
     });
 });
+
+
+router.get('/:id/posts', (req, res) => {
+    const { id } = req.params;
+    db
+    .getUserPosts(id)
+    .then(posts => {
+        res.status(200).json(posts);
+    })
+    .catch(error => {
+        res.status(500).json(error);
+    });
+});
+
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    db
+    .get(id)
+    .then(user => {
+        res.status(200).json(user);
+    })
+    .catch(error => {
+        res.status(500).json(error);
+    });
+});
+
+router.post('/', function(req, res) {
+    const users = req.body;
+    db
+    .insert(users)
+    .then(response => {
+        res.status(201).json(response);
+    })
+    .catch(error => {
+        res.status(500).json({ error: 'No post addded.'});
+    });
+});
+
 
 module.exports = router;
