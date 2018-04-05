3598a47e7380a6ea7dc55f7714a0a9cfa98fcbf5
Edward Manda <lilkups@yahoo.com>
2018-04-05 00:26:30 -0500

    postRouter added and working

--- server.js
+++ server.js
@@ -4,16 +4,15 @@ const helmet = require('helmet');
 const cors = require('cors');
 
 const userRouter = require('./users/userRouter.js');
-//const postRouter = require('./users/userRouter.js');
+const postRouter = require('./posts/postRouter.js');
 //const tagRouter = require('./users/userRouter.js');
 
 const server = express();
 
 // custom middleware [m1, m2, mn] -> [request handlers]
 function logger(req, res, next) {
-    console.log('url: ', req.url);
-    if(req.url === '/api/users/:id/orders'){
-        console.log('body: ', req.body);
+    if(req.url === '/api/tags/'){
+        console.log('tags: ', req.url);
     }
     
     // next points to the next middleware
@@ -28,8 +27,8 @@ server.use(express.json());
 server.use(logger);
 
 server.use('/api/users', userRouter);
-//server.use('/api/posts', userRouter);
-//server.use('/api/tags', userRouter);
+server.use('/api/posts', postRouter);
+//server.use('/api/tags', tagRouter);
 
 server.get('/', function(req, res) {
   res.json({ api: 'Running...' });
--- users/userRouter.js
+++ users/userRouter.js
@@ -6,91 +6,173 @@ const db = require('../data/helpers/userDb.js');
 
 // handles routes that start with: /api/users
 
-router.get('/', (req, res) => {
+// get
+router.get(`/`, (req, res) => {
   db
     .get()
     .then(users => {
-      res.json(users);
-    })
-    .catch(error => {
-      res.status(500).json(error);
-    });
-});
-
-router.get('/:id/orders', (req, res) => {
-  res.send(`viewing orders for user with id ${req.params.id}`);
-});
-
-router.get('/:id', (req, res) => {
-  const { id } = req.params;
-
-  db
-    .get(id)
-    // .findById(userid)
-    .then(users => {
-      res.json(users);
-    })
-    .catch(error => {
-      res.status(500).json(error);
-    });
-});
-
-router.post('/', (req, res) => {
-  const user = req.body;
-  db
-    .insert(user)
-    .then(response => {
-      res.status(201).json(response);
-    })
-    .catch(error => {
-      res.status(500).json({
-        error: 'There was an error while saving the user to the database',
-      });
-    });
-});
-
-router.delete('/:id', (req, res) => {
-  const { id } = req.params;
-  let user;
-
-  db
-    .get(id)
-    .then(response => {
-      user = { ...response };
-
-      db
-        .remove(id)
-        .then(response => {
-          res.status(200).json(user);
-        })
-        .catch(error => {
-          res.status(500).json(error);
-        });
-    })
-    .catch(error => {
-      res.status(500).json(error);
-    });
-});
-
-router.put('/:id', (req, res) => {
-  const { id } = req.params;
-  const update = req.body;
-
-  db
-    .update(id, update)
-    .then(count => {
-      if (count > 0) {
-        db.get(id).then(updatedUsers => {
-          res.status(200).json(updatedUsers);
-        });
-      } else {
+      if(users.length > 0){ // checks if any users were found
+        res
+          .status(200)
+          .json(users);
+      }else{
         res
           .status(404)
-          .json({ message: 'The user with the specified ID does not exist.' });
+          .json({ message: `No users found!` }); // no users found
       }
     })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: `The users could not be retrieved.` }); // database error
+    });
+});
+
+// get
+router.get(`/:id/posts`, (req, res) => {
+  const { id } = req.params;
+  db
+    .getUserPosts(id)
+    .then(posts => {
+      if(posts.length > 0){ // checks if any posts were found
+        res
+          .status(200)
+          .json(posts);
+      }else{
+        res
+          .status(404)
+          .json({ message: `No posts found!` }); // no posts found
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: `The posts could not be retrieved.` }); // database error
+    });
+});
+
+//get by id
+router.get(`/:id`, (req, res) => {
+  const { id } = req.params;
+  db
+    .get(id)
+    .then(user => {
+      if(Object.keys(user).length > 0){
+        res
+          .status(200)
+          .json(user); 
+      }else{
+        res
+        .status(404)
+        .json({ message: `The user does not exist.` }); // user doesn't exist
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: `The user information could not be retrieved. Internal server error!` }); // database error
+    });
+});
+
+//post
+router.post(`/`, (req, res) => {
+  const newUser =  req.body !== undefined ? req.body : {};
+
+  // check if name is set
+  if(newUser.name === undefined){
+    res 
+      .status(400)
+      .json({ errorMessage: `Please provide a name for the user.` })
+      return;
+  }
+
+  //insert it in the database
+  db
+    .insert(newUser)
+    .then(response => {
+      db
+        .get(response.id)
+        .then(user => {
+          if(Object.keys(user).length > 0){
+            res
+              .status(200)
+              .json(user);
+          }else{
+            res
+              .status(404)
+              .json({ message: `The user was not created.` }); // user doesn't exist, so was not created
+          }
+        });
+    }).catch(err => {
+      res
+        .status(500)
+        .json({error: `There was an error while saving the user to the database. Internal server error`});
+    });
+});
+
+//delete
+router.delete(`/:id`, (req, res) => {
+  const { id } = req.params;
+  db
+    .get(id)
+    .then(response => {
+      if(Object.keys(response).length > 0){
+        // make a copy of the user
+        const user = { ...response };
+        db
+          .remove(id)
+          .then(count => {
+            res
+              .status(200)
+              .json(user); // send the user deleted back with the response
+          });
+      }else{
+        res
+          .status(404)
+          .json({ message: `The user was not deleted.` }); // user doesn't exist?? somehow!!
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: `The user could not be removed. Internal server error!` }); // database error
+    });  
+});
+
+//put
+router.put(`/:id`, (req, res) => {
+  const { id } = req.params;
+  const userUpdates = req.body ? req.body : {};
+
+  // update
+  db
+    .update(id, userUpdates)
+    .then(count => {
+        if(count > 0){
+          // update done
+          db
+          .get(id)
+          .then(updatedUser => {
+            if(Object.keys(updatedUser).length > 0){
+              res
+                .status(200)
+                .json(updatedUser); 
+            }else{
+              res
+                .status(400) // check if this is correct?
+                .json({ message: `Error encountered` }); // update occured but an error happened
+            }
+          });
+        }else{
+          res
+            .status(404)
+            .json({ message: `The user was not updated.` }); // nothing was updated
+        }
+    })
     .catch(error => {
-      res.status(500).json(error);
+      res 
+        .status(500)
+        .json({ error: `The user information could not be modified.` }); // database error
     });
 });
 
