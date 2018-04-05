64f9e38dc1f9ced7c77894d7692d44fa32a7772b
Omoniyi Alimi <omoniyialimi@Omoniyis-MacBook-Pro.local>
2018-04-05 18:57:19 -0400

    Port 5000 working

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const helmet = require('helmet');
 const morgan = require('morgan')
-const db = require('./data/db.js');
+const db = require('./data/dbConfig.js');
 
 const server = express();
 
-const db = require('/date/db.js')
+
 
 //middleware
 server.use(morgan('dev'));
 server.use(helmet());
 server.use(express.json());
+
+const port = 5000;
+server.listen(port, () => console.log('API Running on port 5000'));
