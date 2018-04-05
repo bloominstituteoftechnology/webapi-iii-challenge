061929baea582820d05efd69471709d6b8844fd0
charletta <charlettabullard12@outlook.com>
2018-04-05 18:34:27 -0400

    Revised router pages...still bugs not able to view in postman nor browser...WIP

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -7,54 +7,12 @@ const db = require('../data/helpers/postDb.js');
 router.get('/', (req, res) => {
   db
     .get()
-    .then(posts => res.status(200).json(posts))
-    .catch(error => res.status(500).json(error));
-});
-
-router.get('/:id/tags', (req, res) => {
-  const { id } = req.params;
-
-  db
-    .getPostTags(id)
-    .then(posts => res.status(200).json(posts))
-    .catch(error => res.status(500).json(error));
-});
-
-router.get('/:id', (req, res) => {
-  const { id } = req.params;
-
-  db
-    .get(id)
-    .then(posts => res.status(200).json(posts))
-    .catch(error => res.status(500).json(error));
-});
-
-router.post('/', (req, res) => {
-  const post = req.body;
-
-  db
-    .insert(post)
-    .then(posts => res.status(200).json(posts))
-    .catch(error => res.status(500).json(error));
-});
-
-router.put('/:id', (req, res) => {
-  const id = req.params.id;
-  const updatedPost = { post, id };
-
-  db
-    .update(id, updatedPost)
-    .then(posts => res.status(200).json(posts))
-    .catch(error => res.status(500).json(error));
-});
-
-router.delete('/:id', (req, res) => {
-  const id = req.params.id;
-
-  db
-    .remove(id)
-    .then(posts => res.status(200).json(posts))
-    .catch(error => res.status(500).json(error));
+    .then(posts => {
+      res.json(posts);
+    })
+    .catch(error => {
+      res.status(500).json({ error: 'Unable to retrieve posts.' });
+    });
 });
 
 module.exports = router;
