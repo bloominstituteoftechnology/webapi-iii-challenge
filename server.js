c09212557752fd85bf84bba14976dde097f803fa
mjd02 <mostdarryl@gmail.com>
2018-04-06 18:13:04 -0400

    completed

--- server.js
+++ server.js
@@ -1,9 +1,50 @@
 const express = require('express');
 const userDB = require('./data/helpers/userDb.js');
 const postDB = require('./data/helpers/postDb.js');
+const tagDB = require('./data/helpers/tagDb.js');
 
 const app = express();
 app.use(express.json());
+
+
+app.delete('/api/tags/:id',(req,res)=>{
+  tagDB.remove(req.params.id).then(response=>{
+    res.status(200).json(response);
+  })
+  .catch(err=>{
+    res.status(500).json(err);
+  });
+});
+
+
+app.put('/api/tags/:id',(req,res)=>{
+  tagDB.update(req.params.id,req.body).then(response=>{
+    res.status(200).json(response);
+  })
+  .catch(err=>{
+    res.status(500).json(err);
+  });
+});
+
+
+app.post('/api/tags',(req,res)=>{
+  tagDB.insert(req.body).then(response=>{
+    res.status(200).json(response);
+  })
+  .catch(err=>{
+    res.status(500).json(err);
+  });
+});
+
+app.get('/api/tags/:id',(req,res)=>{
+  tagDB.get(req.params.id).then(response=>{
+    res.status(200).json(response);
+  })
+  .catch(err=>{
+    res.status(500).json(err);
+  });
+});
+
 
 app.get('/api/posts/tags/:id',(req,res)=>{
   postDB.getPostTags(req.params.id).then(response=>{
