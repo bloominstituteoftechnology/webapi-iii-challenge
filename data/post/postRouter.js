748dbcac0e858bc13c8e0941d089d16b9962e01c
Cristina Laryea <crispad06@gmail.com>
2018-04-06 10:56:09 -0400

    postRouters, done

--- data/post/postRouter.js
+++ data/post/postRouter.js
@@ -42,4 +42,73 @@ router.get('/:id', (req, res) => {
         });
 });
 
+router.get('/:id/tags', (req, res) => {
+    const { id } = req.params;
+
+    postdb
+        .getPostTags(id)
+        .then(tags => {
+            res.status(200).json(tags);
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        });
+});
+
+router.post('/', (req, res) => {
+    const posts = req.body;
+
+    postdb
+    .insert(posts)
+    .then(response => {
+        res.status(201).json(response);
+    })
+    .catch(error => {
+        res.status(500).json({ error: 'No post added.' });
+    });
+});
+
+router.put('/:id', (req, res) => {
+    const { id } = req.params;
+    const update = req.body;
+
+    postdb
+        .update(id, update)
+        .then(count => {
+            if (count > 0) {
+                postdb  
+                    .get(id)
+                    .then(update => {
+                        res.status(200).json(update);
+                    });
+            }else {
+                res.status(404).json({ message: 'Post at that id does not exist.' });
+            }
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        });
+});
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+
+    postdb 
+        .get(id)
+        .then(response => {
+            post = {...response[0] };
+            postdb
+            .remove(id)
+            .then(response => {
+                res.status(200).json(error);
+            })
+            .catch(error => {
+                res.status(404).json(error);
+            });
+        })
+            .catch(error => {
+                res.status(500).json(error);
+            });
+});
+
 module.exports = router;
