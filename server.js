4f149853462cc48793d80f9ceaa9d088178f70f1
SaaSha <saasha.pina@gmail.com>
2018-04-05 23:54:33 -0400

    SaaSha | W.I.P | Update

--- server.js
+++ server.js
@@ -3,26 +3,32 @@ const express = require('express')
 const server = express();
 
 const helmet = require('helmet');
-const morgan = require('morgan');
 const cors = require('cors')
 
 const userRoutes = require('./userRoutes.js');
-//const tagRoutes = require('./tagRoutes');
-//const postRoutes = require('./postRoutes.js');
+// const tagRoutes = require('./tagRoutes');
+// const postRoutes = require('./postRoutes.js');
 
+//custom middle ware
+function logger(req, res, next) {
+    console.log( 'epic response here');
 
-server.use(helmet);
-server.use(morgan);
-server.use(cors);
+    next();
+}
+//use middle ware
+server.use(helmet());
+server.use(cors());
 server.use(express.json());
 
 server.use('/api/users', userRoutes);
 // server.use(tagRoutes);
 // server.use(postRoutes);
 
+// initial route
 server.get('/', function(req,res) {
     res.json({ api: 'running...'})
 })
 
 
-server.listen(5000, console.log('API listening on port 5000'))
+
+server.listen(5000, console.log('API listening on port 5000'));
--- userRoutes.js
+++ userRoutes.js
@@ -1,16 +1,16 @@
 const express = require('express');
 
-const userDb = require('./data/helpers/userDb.js')
+const userDb = require('./data/helpers/userDb.js');
 
-const router = express.Router
+const router = express.Router();
 
 //CRUD operations for User /api/users
 
-router.get('/', (request,response) => { 
+router.get('/', (req,res) => { 
     
     userDb
     .get() // get the data from (data)
-    .then(user => {  //send the data in json
+    .then(users => {  //send the data in json
         res.json(users);
     }) //send the user to client
     .catch( error => {
@@ -18,15 +18,47 @@ router.get('/', (request,response) => {
     })
 })
 
-router.get('/id', (req, res) => {
+router.get('/:id', (req, res) => {
     const { id } = req.params;
     userDb
     .getUserPosts(id) //get users post by id
     .then( user => {
-        res.json(user[0]);
+        res.json(user);
     })
     .catch(error => {
         res.status(500).json(error);
     })
 })
 
+router.post('/', (req,res) => {
+    const user = req.body;
+
+    userDb
+    .insert(user)
+    .then ( response => {
+        res.status(201).json(response);
+    })
+    .catch(error => {
+        res.status(500).json({error: "Error while saving to db"})
+    });
+    
+})
+
+router.put('/:id', (req, res) => {
+
+    userDb
+    .update(id, update)
+    .then( response => {
+
+    })
+    .catch()
+})
+
+router.delete('/:id', (req, res) => {
+
+    userDb
+    .remove(id)
+    .then()
+    .catch()
+})
+module.exports = router;
