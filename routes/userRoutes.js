1fac26be30c4d2fe7a568e1c6e7075b8c1bf3b6f
nickbrenn <nbren@tutanota.com>
2018-05-02 16:21:26 -0400

    Enabled getting posting putting and deleting of users

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -15,4 +15,117 @@ router.get("/", (req, res) => {
     });
 });
 
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+  db
+    .get(id)
+    .then(user => {
+      if (user) {
+        res.json(user);
+      } else {
+        res
+          .status(404)
+          .json({ message: "The user with the specified ID does not exist." });
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: "The user information could not be retrieved." });
+    });
+});
+
+router.post("/", (req, res) => {
+  db
+    .insert(req.body)
+    .then(user => {
+      db
+        .get(user.id)
+        .then(newUser => {
+          res.status(200).json(newUser);
+        })
+        .catch(err =>
+          res
+            .status(500)
+            .json({ error: "The user with the specified ID does not exist." })
+        );
+    })
+    .catch(err => {
+      if (!req.body.hasOwnProperty("name")) {
+        res.status(400).json({
+          errorMessage: "Please provide a name for the user."
+        });
+      } else {
+        res.status(500).json({
+          error: "There was an error while saving the user to the database."
+        });
+      }
+    });
+});
+
+router.delete("/:id", (req, res) => {
+  const id = req.params.id;
+  db
+    .get(id)
+    .then(user => {
+      if (user) {
+        db
+          .get(id)
+          .then(user => {
+            // user = { ...users };
+            db.remove(id).then(response => {
+              res.status(200).json({ ...user });
+            });
+          })
+          .catch(err =>
+            res.status(500).json({ error: "The user could not be removed." })
+          );
+      } else {
+        res
+          .status(404)
+          .json({ message: "The user with the specified ID does not exist." });
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: "The user information could not be retrieved." });
+    });
+});
+
+router.put("/:id", (req, res) => {
+  const id = req.params.id;
+  db
+    .get(id)
+    .then(user => {
+      if (!req.body.hasOwnProperty("name")) {
+        res.status(400).json({
+          errorMessage: "Please provide a name for the user."
+        });
+      } else if (user) {
+        db
+          .update(id, req.body)
+          .then(user => {
+            db.get(id).then(user => {
+              res.status(200).json({ ...user });
+            });
+          })
+          .catch(err =>
+            res
+              .status(500)
+              .json({ error: "The user information could not be modified." })
+          );
+      } else {
+        res
+          .status(404)
+          .json({ message: "The user with the specified ID does not exist." });
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: "The user information could not be retrieved." });
+    });
+});
+
 module.exports = router;
