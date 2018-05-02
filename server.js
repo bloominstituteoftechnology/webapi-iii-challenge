a6e08241902fdb562b56ad5c8429deef9663c809
Harrison Brock <harrison@harrisonbrock.com>
2018-05-02 15:34:07 -0400

    Get users by id done

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const cors = require('cors');
 // const helmet require('helmet');
 const server = express();
 
-const userDb = require('./data/helpers/userDb')
+const userDb = require('./data/helpers/userDb');
 
-server.use(express.json())
-server.use(cors())
+server.use(express.json());
+server.use(cors());
 
-server.use(express.json())
-server.use(cors())
+server.use(express.json());
+server.use(cors());
 // server.use(helmet());
 
 server.get('/api/users', (req, res) => {
@@ -19,7 +19,18 @@ server.get('/api/users', (req, res) => {
 .catch(() => res.status(500).send({ error: 'Error fetching users' }))
 });
 
+server.get('/api/users/:id', (req, res) => {
+    const id = req.params.id;
+    userDb
+    .get(id)
+    .then((response) => response.length === 0
+    ? res.status(200).send({error: `User not found`})
+    : res.status(200).send(response))
+.catch(() => res.status(500).send({ error: 'Error fetching users' }))
+});
+
+
 
 
   
-server.listen(5000, console.log('Listening'))
+server.listen(5000, console.log('Listening'));
