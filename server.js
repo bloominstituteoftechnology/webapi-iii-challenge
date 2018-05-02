133655817133980a96896021248832b53e985379
ktan114 <22802321+ktan114@users.noreply.github.com>
2018-05-02 15:56:02 -0400

    Added express for reading HTTP request, set up initial GET, connect server to a localhost

---
+++ server.js
@@ -0,0 +1,16 @@
+// Imports dependencies
+const express = require('express');
+
+// Add middleware
+const server = express();
+
+// GET method
+server.get('/', (req,res) => {
+    console.log('Got a server set up');
+    res.send('Got a server set up');
+})
+
+// Server attached to a port
+
+const port = 5000;
+server.listen(port, () => {`Server is listening in localhost: ${port} `})
