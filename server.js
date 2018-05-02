cc93116100275c3057751cd5a608c5a267965267
Devon Bacon <devon.bacon@gmail.com>
2018-05-02 15:56:17 -0700

    Added function to delete data in server.js

--- server.js
+++ server.js
@@ -57,4 +57,24 @@ server.post('/api/:target', (req, res) => {
   });
 });
 
+// delete specific data
+server.delete('/api/:target/:id', (req, res) => {
+  data[req.params.target]
+    .remove(req.params.id)
+    .then(id => {
+      if (id) {
+        res.json(id);
+      } else {
+        res.status(404).json({
+          err: "The data with the specified ID does not exist."
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: "The data could not be removed"
+      });
+    });
+});
+
 server.listen(5000, console.log('\n== API Running on port 5000 ==\n'));
