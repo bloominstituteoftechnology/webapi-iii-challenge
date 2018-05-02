44fd57a12ec0994297ab0d3c0185d1daa3109107
EricGomez101 <eric_gomez_1@hotmail.com>
2018-05-02 16:36:28 -0700

    i have set up one route handler so far

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 //import all dependency's here:
-const express = require('express');
-const server = express();
-const cors = require('cors');
-const helmet = require('helmet');
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import userDb from './data/helpers/userDb.js';
+import postsDb from './data/helpers/postDb.js';
+import tagsDb from './data/helpers/tagDb.js';
+
 const port = 5000;
+const server = express();
 
 // create custom middleware here:
 
@@ -13,9 +17,38 @@ server.use(helmet());
 server.use(cors());
 server.use(express.json());
 
+// add route handlers:
+
 // create route handlers here:
-server.get('/', (req, res) => {
-  res.json({message: 'welcome!'});
+server.get('/api/users', (req, res) => {
+  userDb.get()
+    .then(users => {
+      postsDb.get()
+        .then(posts => {
+          users.map(user => {
+            user.posts = [];
+            posts.map((post) => {
+              if(post.userId === user.id){
+                user.posts.push(post);
+              }
+            })
+          })
+          res.json(users);
+        })
+    });
+})
+
+server.get('/api/posts', (req, res) => {
+  postsDb.get()
+    .then(posts => {
+      res.json(posts);
+    });
+})
+server.get('/api/tags/', (req, res) => {
+  tagsDb.get()
+    .then(tags => {
+      res.json(tags);
+    })
 })
 server.listen(port, () => {
   console.dir(`server listening on port ${port}`);
