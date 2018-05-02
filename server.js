66c4ab4d9ce5f11fa6b02f71257855c94f992fd5
Devon Bacon <devon.bacon@gmail.com>
2018-05-02 13:25:10 -0700

    Added start script and started crafting posts get request

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 const express = require('express');
-
 const db = require('./data/dbConfig');
 
 const server = express();
+server.use(express.json());
 
-server.listen(5000, console.log('\n== API Running on port 500 ==\n'));
+// get all posts
+server.get('/api/posts', (req, res) => {
+  db
+  .get()
+  .then(posts => {
+    res.json(posts);
+  })
+  .catch(err => {
+    res.status(500).json({
+      error: "The posts information could not be retrieved."
+    });
+  });
+});
+
+server.listen(5000, console.log('\n== API Running on port 5000 ==\n'));
