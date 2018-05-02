23ca390d59d4c1d4b607cf0aea25adca48756375
Velsu <riftmage50@gmail.com>
2018-05-03 00:39:18 +0200

    Added Tags along with Router

--- server.js
+++ server.js
@@ -5,11 +5,13 @@ const cors = require("cors");
 
 const usersRouter = require("./data/routers/usersRouter.js");
 const postsRouter = require("./data/routers/postsRouter.js");
+const tagsRouter = require("./data/routers/tagsRouter.js");
 
 const app = express();
 app.use(bodyParser.json());
 app.use("/users", usersRouter);
 app.use("/posts", postsRouter);
+app.use("/tags", tagsRouter);
 
 app.get("/", function(req, res) {
   res.json({ server: "running" });
