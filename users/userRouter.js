b5de160f8c9a19a407ceb68baff7f81f45d5fc10
2940cristian <2940cristian@gmail.com>
2018-05-02 13:41:23 -0700

    Remove and update added, users CRUD complete, will add errors and check later

--- users/userRouter.js
+++ users/userRouter.js
@@ -23,13 +23,37 @@ router.get('/users/:id', (req, res) => {
 })
 
 router.post('/users/', (req, res) => {
-    const id = req.params.id;
     newUser = req.body;
     userDb.insert(newUser).then(response => {
         res.status(200).json(response);
     }).catch(err => {
         res.status(500).json({
             error: "There was en error posting a new user to the database"
+        })
+    })
+})
+
+router.put('/users/:id', (req, res) => {
+    const id = req.params.id;
+    const userUpdate = req.body;
+
+    userDb.update(id, userUpdate).then(response => {
+        res.status(200).json({
+            Sucess: "User has been updated"
+        })
+    }).catch(err => {
+        res.status(500).json({
+            error: "User update failed"
+        })
+    })
+})
+
+router.delete('/users/:id', (req, res) => {
+    const id = req.params.id;
+
+    userDb.remove(id).then(response => {
+        res.status(200).json({
+            Deleted: "Post has been deleted from the database"
         })
     })
 })
