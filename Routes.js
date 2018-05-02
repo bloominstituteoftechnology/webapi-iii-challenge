d7998d5eda90d528435bcb08b67f28a08ce293c7
axhon <axhonruiz@gmail.com>
2018-05-02 09:33:25 -1000

    getting all users

---
+++ Routes.js
@@ -0,0 +1,26 @@
+import express, { Router } from 'express';
+
+import db from './data/helpers/userDb';
+
+const mainRouter = Router({ mergeParams: true });
+
+// this router will 3 different routers
+//  users, posts, tags
+
+// /users
+const userRoute = express.Router();
+
+userRoute.get('/', async (req, res) => {
+  const response = await db.get();
+  res.json(response);
+});
+
+mainRouter.use('/users/', userRoute);
+
+// /posts
+mainRouter.use('/posts', () => {});
+
+// /tags
+mainRouter.use('/tags', () => {});
+
+export { mainRouter };
--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 import express from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import { mainRouter } from './Routes';
 
 const app = express();
+
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(express.json());
+
+app.use('/api', mainRouter);
 
 app.listen(3000, () => console.log('hey'));
