c33ebf20a7cbb846542b5f12c72e9357e578c6cf
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2018-05-02 13:14:35 -0700

    added route for user

--- Routes/userRoute.js
+++ Routes/userRoute.js
@@ -9,4 +9,32 @@ userRoute.get('/', async (req, res) => {
   res.json(response);
 });
 
+userRoute.get('/:id', async (req, res) => {
+  const response = await db.get(req.params.id);
+  console.log(response);
+  if (response.length > 0) {
+    res.status(404).json({msg: 'User not found'});
+    return;
+  } 
+  res.json(response[0]);
+});
+
+userRoute.post('/', async (req, res) => {
+  const userInformation = await db.insert(req.body);
+  console.log(userInformation);
+  res.json(userInformation);
+});
+
+userRoute.put('/:id', async (req, res) => {
+  const userInformation = await db.update(req.params.id, req.body);
+  console.log(userInformation);
+  res.json(userInformation);
+});
+
+userRoute.delete('/:id', async (req, res) => {
+  const userInformation = await db.remove(req.params.id);
+  console.log(userInformation);
+  res.json(userInformation);
+})
+
 export default userRoute;
