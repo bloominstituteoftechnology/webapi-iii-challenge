0da0a21ebec175b7634ff5eddd5a1fb0d46845ad
Velsu <riftmage50@gmail.com>
2018-05-03 00:26:06 +0200

    Added Posts API along with router

--- server.js
+++ server.js
@@ -4,10 +4,12 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const usersRouter = require("./data/routers/usersRouter.js");
+const postsRouter = require("./data/routers/postsRouter.js");
 
 const app = express();
 app.use(bodyParser.json());
 app.use("/users", usersRouter);
+app.use("/posts", postsRouter);
 
 app.get("/", function(req, res) {
   res.json({ server: "running" });
