ff9927a3a6beaeb084521971ade6652bbdc10cdb
Justin M <jekm321@gmail.com>
2018-05-02 16:37:40 -0400

    making progress?

---
+++ server.js
@@ -0,0 +1,30 @@
+const userDb = require('./data/helpers/userDb');
+const postDb = require('./data/helpers/postDb');
+const tagDb = require('./data/helpers/tagDb');
+const userRoutes = require('./users/userRoutes');
+
+const express = require('express');
+const server = express();
+
+errorHandler = (err, req, res, next) => {
+    if (err) {
+        if (err.errno === 19) {
+            res.stats(400).json({ msg: 'Please provide all required fields' });
+        } else {
+            res.stats(500).json({ error: 'something bad happened' });
+        }
+    }
+}
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.json({ api: 'running' });
+})
+
+server.use('/api/users', userRoutes);
+
+server.use(errorHandler);
+
+const port = 5000;
+server.listen(port, () => console.log(`\n==API Running on port ${port} ==\n`));
