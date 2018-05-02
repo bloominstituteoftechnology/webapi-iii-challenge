0584fd43e5835f9025da61f08c331f259550de15
2940cristian <2940cristian@gmail.com>
2018-05-02 12:53:35 -0700

    Seting up routes for each api

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express = require("express");
-const db = require('./data/dbConfig.js')
+const postDb = require("./data/helpers/postDb.js");
+const tagDb = require("./data/helpers/tagDb.js");
+const userDb = require("./data/helpers/userDb.js");
 
 const server = express();
 
 server.use(express.json());
+
+server.get('/api/users', (req, res) => {
+    userDb.get().then(user => {
+        res.json(user);
+    }).catch(err => {
+        res.status(500).json({
+            error: "The user information could not be found"
+        })
+    })
+})
 
 server.listen(5000, () => console.log("Listening on port 5000"));
