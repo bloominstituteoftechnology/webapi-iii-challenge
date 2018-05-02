926612a55322e19baa266ea3cd44c78f68ab3906
cgoez <goez2001@gmail.com>
2018-05-02 16:03:02 -0700

    Change structure to support subroutes

---
+++ data/users/userRoutes.js
@@ -0,0 +1,2 @@
+// Node Modules
+const express = require("express");
--- server.js
+++ server.js
@@ -1,22 +1,29 @@
 // Node Modules
-const postDb = require("./data/helpers/postDb");
-const tagDb = require("./data/helpers/tagDb");
-const userDb = require("./data/helpers/userDb");
-
 const express = require("express");
 const cors = require("cors");
+const helmet = require("helmet");
+
+// Import from ./users/
+const postRoutes = require("./posts/postRoutes");
+const tagRoutes = require("./tags/tagRoutes");
+const userRoutes = require('./users/userRoutes');
 
 const server = express();
 
+// Middleware
 server.use(express.json());
+server.use(helmet());
 server.use(cors());
+
+// Route Handlers
+server.use('/api/users', userRoutes)
+server.use('/api/tags', tagRoutes)
+server.use('/api/posts', postRoutes)
 
 // Server running
 server.get("/", (req, res) => {
   res.send("API is running!");
 });
 
-// USERS
-server.get('/')
 
-server.listen(5000, () => console.log('\n== Server running on port 5000 ==\n'))
+server.listen(5000, () => console.log("\n== Server running on port 5000 ==\n"));
