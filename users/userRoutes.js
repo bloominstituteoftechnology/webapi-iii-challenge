31d06d7083373e56c6d52e448d50511a6fcb3fb9
Justin Hong <jhong2009@gmail.com>
2018-05-02 14:20:56 -0700

    in userRoutes.js: set up GET to fetch a user by id

--- users/userRoutes.js
+++ users/userRoutes.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const userDB = require("../data/helpers/userDB");
 const router = express.Router();
-// routes for "/api/users"
-// need get, insert, update, remove, getUserPosts route handlers
 
+// routes for "/api/users"
 // DB schema
 // id: number, no need to provide it when creating users, the database will generate it.
 // name: up to 128 characters long, required.
 
 // ROUTE HANDLERS /users/userRoutes
+
+// GET: list of all users
 router.get("/", (req, res) => {
 	userDB
 		.get()
@@ -16,6 +17,26 @@ router.get("/", (req, res) => {
 			res.json(users);
 		})
 		.catch(err => {
+			res.status(500).json({ error: err });
+		});
+});
+
+// GET: user by id
+router.get("/:id", (req, res) => {
+	// define id
+	const { id } = req.params;
+
+	userDB
+		.get(id)
+		.then(user => {
+			if (user) {
+				res.status(200).json(user);
+			} else {
+				res.status(404).json({ message: "user not found" });
+			}
+		})
+		.catch(err => {
+			// res.status(404).json({ message: "user not found" });
 			res.status(500).json({ error: err });
 		});
 });
