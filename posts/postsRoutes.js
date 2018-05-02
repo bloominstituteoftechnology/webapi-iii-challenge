f333be1b68b511dbb2fae2c9669f4c170efb8743
Harrison Brock <harrison@harrisonbrock.com>
2018-05-02 18:00:05 -0400

    Added get post by id

--- posts/postsRoutes.js
+++ posts/postsRoutes.js
@@ -12,4 +12,15 @@ router.get('/', (req, res) => {
         .catch(() => res.status(500).send({ error: 'Error fetching posts' }))
 });
 
+//get post by id
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    postDb
+        .get(id)
+        .then((response) => response.length === 0
+            ? res.status(200).send({error: `Post not found`})
+            : res.status(200).send(response))
+        .catch(() => res.status(500).send({ error: 'Error fetching post' }))
+});
+
 module.exports = router;
