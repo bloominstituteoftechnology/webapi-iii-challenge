2f875cda67a392f9a950122fe33da0806f4ab5b1
David Pok <david.pok@gmail.com>
2018-05-02 15:55:54 -0700

    my poor attempt

---
+++ server.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const helmet = require('helmet');
+// const db = require('./data/dbConfig');
+const db = require('./data/helpers/userDb');
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+
+
+//TEST TO SEE IF DEFAULT IS WORKING
+server.get('/', (req, res) => {
+    res.send('Api running');
+  });
+  
+//GETTING USERS
+server.get('/api/users', (req, res) => {
+    db.get()
+      .then(users => {
+          res.json(users);
+      })
+      .catch(err => {
+          res.status(500).json({ error: err });
+      });
+});
+
+//GETTING USER POSTS
+server.get('/api/users/:id', (req, res) => {
+    const userId = req.params.id
+    db.getUserPosts(userId)
+      .then(users => {
+//POSSIBLY ADD res MESSAGE IF NO USER FOUND
+// if (users.length === 0) {
+//     res.status(404).json({ message: 'user not found' });
+//   } else {
+//     res.json(users[0]);
+//   }
+// SOMETHING LIKE THIS ^^^
+          res.json(users);
+      })
+      .catch(err => {
+          res.status(500).json({ error: err });
+      });
+});
+
+//DELETE USERS
+server.delete('/api/users', (req, res) => {
+    const { id } = req.params;
+    // let user;
+    db
+      .get(id)
+      .then(foundUser => {
+          user = { ...foundUser[0] };
+
+    db
+      .remove(id)
+      .then(response => {
+          res.status(200).json(user);
+      });
+      })
+      
+      .catch(err => {
+          res.status(500).json({ error: err });
+      })
+});
+
+const port = 5000;
+server.listen(port, () => console.log(`\n== API Running on port ${port} ==\n`));
