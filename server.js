1e016d8edae0cb6356f2095bb93f25174ae33d30
Justin Hong <jhong2009@gmail.com>
2018-05-02 13:31:43 -0700

    in server.js: set up a server using express, helmet, and routes

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const helmet = require("helmet");
+const server = express();
+
+// middleware
+server.use(helmet());
+server.use(express.json());
+
+// import sub-applications
+const userRoutes = require("./users/userRoutes");
+const tagRoutes = require("./tags/tagRoutes");
+const postRoutes = require("./posts/postRoutes");
+
+// test
+server.get("/", (req, res) => {
+	res.send("API running");
+});
+
+// server at port 5000
+server.listen(5000, () => {
+	console.log("\n== API Running on port 5000 ==\n");
+});
