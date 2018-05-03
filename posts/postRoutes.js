c2bf8c7b9f8946c540f2b8a18535559cd31cb28b
Justin Hong <jhong2009@gmail.com>
2018-05-03 11:43:23 -0700

    in postRoutes.js: set up GET request to fetch a post by id

--- posts/postRoutes.js
+++ posts/postRoutes.js
@@ -21,4 +21,22 @@ router.get("/", (req, res) => {
 		});
 });
 
+// GET - get a posts by id
+router.get("/:id", (req, res) => {
+	const { id } = req.params;
+
+	postDB
+		.get(id)
+		.then(post => {
+			if (post) {
+				res.status(200).json(post);
+			} else {
+				res.status(404).json({ message: "id not found" });
+			}
+		})
+		.catch(err => {
+			res.status(500).json({ error: err });
+		});
+});
+
 module.exports = router;
