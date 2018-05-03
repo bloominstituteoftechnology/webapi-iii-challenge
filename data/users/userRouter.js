4801ce5494174de5db0b94664ae9ff7af801c384
cgoez <goez2001@gmail.com>
2018-05-03 13:58:38 -0700

    userRouter GET user by id

--- data/users/userRouter.js
+++ data/users/userRouter.js
@@ -6,6 +6,7 @@ const router = express.Router();
 
 const db = require("../helpers/userDb");
 
+// GET all users; root
 router.get("/", (res, req) => {
   db
     .get()
@@ -13,8 +14,22 @@ router.get("/", (res, req) => {
       res.json(users);
     })
     .catch(error => {
-      res.statusCode(500).json({
+      res.status(500).json({
         error: "There was an error while retrieving users."
+      });
+    });
+});
+
+// GET user from id
+router.get("/:id", (req, res) => {
+  db
+    .get(id)
+    .then(users => {
+      res.json(users);
+    })
+    .catch(error => {
+      res.status(500).json({
+        error: "There was an error while retrieving the specified user."
       });
     });
 });
