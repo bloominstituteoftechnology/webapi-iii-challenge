57782e0f6c888a05317af36454c6e7470f151fdf
imsdhk <jappleleaf@gmail.com>
2018-05-03 18:13:57 -0400

    finished the Crud,retrieve the list of posts for a user,retrieve the list of tags for a post, implemented the routes

---
+++ userRoutes.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const router = express.Router();
+const userDb = require('./data/helpers/userDb');
+
+//get -- user
+router.get('/', (req, res) => {
+    userDb
+    .get()
+    .then(response => {
+        res.status(200).json({ response })
+    })
+    .catch(err => {
+        res.status(500).json({ Error: err })
+    })
+})
+
+//post -- users 
+router.post('/', (req, res) => {
+    const userInfo = req.body;
+    console.log(userInfo);
+    userDb
+    .insert(userInfo)
+    .then(response => {
+        res.status(201).json({ userInfo })
+    })
+    .catch(err => {
+        res.status(500).json({ Error: err })
+    })
+})
+
+//delete -- users
+router.delete('/:id', (req, res) => {
+    const id = req.params.id;
+    userDb
+    .remove(Number(id))
+    .then(response => {
+        res.status(200).json({ response })
+    })
+    .catch(err => {
+        res.status(500).json({ Error: err })
+    })
+})
+
+//put -- user 
+router.put('/:id', (req, res) => {
+    const id = req.params.id;
+    const user = req.body;
+    userDb.update(id, user)
+    .then(response => {
+        res.status(200).json({ user })
+    })
+    .catch(err => {
+        res.status(500).json({ Error: err })
+    })
+})
+
+module.exports = router;
