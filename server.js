333a99f7e8619e951009b2109761c53c0711353a
Jacob Leon Lyerla <umasour@yahoo.com>
2018-05-03 14:25:15 -0700

    added middleware
    added a logger middleware so i can test it, can't get uppercase middleware to work.

--- server.js
+++ server.js
@@ -9,13 +9,27 @@ const routerUser = require('./Routers/routerUser')
 const routerTag = require('./Routers/routerTag')
 const server = express();
 
+// function toUpper(req,res,next){
+//     console.log(req.body)
+//     if (req.body.tag !== undefined){
+//     req.body = {"tag":`${req.body.tag.toUpperCase()}`}
+//     console.log(req.body)
+// }
+//     next();
+// }
+logger =(req,res,next)=>{
+
+    console.log('tags:', req.url);
+    next()
+}
+
 server.use(helmet())
 server.use(cors());
 server.use(express.json());
 
 server.use('/users', routerUser)
 server.use('/posts',routerPost)
-server.use('/tags', routerTag)
+server.use('/tags',logger, routerTag)
 server.get('/', (req, res)=>{
     res.send('api is running')
 })
