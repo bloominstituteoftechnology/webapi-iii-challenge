4198e3eec964b493bb915ff75cd53dd25ce2ff58
Michal Chmielniak <michal@Michals-MacBook-Pro.local>
2018-05-03 13:35:53 -0400

    added userRouter.js with get

---
+++ data/router/userRouter.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const router = express.Router();
+
+const db = require('../data/helpers/userDb.js');
+
+router.get('/', (req, res) => {
+  db
+    .get()
+    .then(users => {
+      res.json(users);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+router.get('/:id', (req, res) => {
+  db
+    .get(req.params.id)
+    .then(users => {
+      res.json(users);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
