df0e6ec2c135d31d6b1db7e09795b3ce26c81096
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2018-05-03 15:32:29 -0700

    copied over data to edit in order to connect endpoint in API

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,31 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
+//implment code to connect to the /api/posts endpoint and show list of posts
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       posts: [],
+//       title: '',
+//       contents: '',
+//     }
+//   }
+
+//   componentDidMount() {
+//     this.listPosts();
+//   }
+//   listPosts = () => {
+//     axios.get(`http://localhost:5000/api/posts`)
+//       .then(response => {
+//         this.setState({ posts: response.data });
+//       })
+//       .catch(err => {
+//         console.log(err);
+//       });
+//   }
 
 class App extends Component {
   render() {
