5190ab58f8332f5a5b46ed533fadac3079b6169d
tanking <tanking@github.com>
2018-05-03 18:12:03 -0400

    Fixed comments in the route modules

--- userRoute.js
+++ userRoute.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const userDb = require('./data/helpers/userDb');
 
-// Middleware
+// add middleware
 const router = express.Router();
 
 // --------USER--------
