b4776feb1ec133904a402d6df6ee34eefdfd8c07
ktan114 <22802321+ktan114@users.noreply.github.com>
2018-05-03 17:47:29 -0400

    Route for api/users

--- server.js
+++ server.js
@@ -1,100 +1,21 @@
 // Imports node modules
 const express = require('express');
 
-const userDb = require('./data/helpers/userDb');
 const postDb = require('./data/helpers/postDb');
 const tagDb = require('./data/helpers/tagDb');
 
+const userRoutes = require('./userRoute');
 // Server setup
 const server = express();
 
 
 // Add middleware
 server.use(express.json());
+server.use('/api/users', userRoutes)
 
 // GET method to send data to initial page
 server.get('/', (req,res) => {
     res.send('Got a server set up');
-})
-
-// --------USER--------
-
-// GET method for user
-server.get('/api/users', (req, res) => {
-
-    userDb
-    .get()
-    .then(response => {
-        res.status(200).json({ response });
-    })
-    .catch(err => {
-        res.status(500).json({ Error: err });
-    })
-}) 
-
-// POST method for user
-server.post('/api/users', (req, res) => {
-    const userInfo = req.body;
-    
-    userDb
-    .insert(userInfo)
-    .then(response => {
-        res.status(201).json({ userInfo })
-    })
-    .catch( err => {
-        res.status(500).json({ Error: err })
-    })
-})
-
-// DELETE method for user
-server.delete('/api/users/:id', (req, res) => {
-    const id = req.params.id;
-    let userDeleted;
-
-    userDb
-    .get(id)
-    .then(user => {
-        userDeleted = { ...user }
-    })
-
-    userDb
-    .remove(id)
-    .then(response => {
-        console.log(userDeleted)
-        res.status(200).json({ userDeleted })
-    })
-    .catch(err => {
-        res.status(500).json({ Error: err })
-    })
-})
-
-// PUT method for user
-server.put('/api/users/:id', (req, res) => {
-    const id = req.params.id;
-    const user = req.body;
-
-    userDb
-    .update(id, user)
-    .then(response => {
-        res.status(200).json({ user })
-    })
-    .catch(err => {
-        res.status(500).json({ Error: err })
-    })
-})
-
-// Retrieve List of Post for a User
-server.get('/api/users/:id', (req, res) => {
-    const id = req.params.id;
-
-    userDb
-    .getUserPosts(id)
-    .then(response => {
-        res.send({ response })
-    })
-    .catch(err => {
-        res.json({ Error: err })
-    })
 })
 
 // --------POST--------
