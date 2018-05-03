90e3a2312cad60fa2bfd043a727e799a476282ce
Michal Chmielniak <michal@Michals-MacBook-Pro.local>
2018-05-03 13:29:27 -0400

    added dependencies and postRouter.js

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+
+const userRouter = require("./routers/userRouter.js");
+const postRouter = require("./routers/postRouter.js");
+const tagRouter = require("./routers/tagRouter.js");
+
+const server = express();
+
+const logger = (req, res, next) => {
+  console.log(`Requesting: ${req.url}`);
+  console.log(`Body being passed: ${req.body}`);
+
+  next();
+};
+
+server.use(express.json());
+server.use(logger);
+server.use(helmet());
+server.use(cors());
+
+server.use("/api/users", userRouter);
+server.use("/api/posts", postRouter);
+server.use("/api/tags", tagRouter);
+
+const port = 5000;
+
+server.listen(port, () => {
+  console.log("Listening on Port 5000");
+});
