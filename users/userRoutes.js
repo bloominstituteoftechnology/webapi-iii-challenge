e9497c3b4068c65ecc8cb4015377936f9e02169d
totsuya <supertatortots@gmail.com>
2018-05-03 18:51:06 -0400

    added another .get to routes and nothing seems to be broken

--- users/userRoutes.js
+++ users/userRoutes.js
@@ -14,3 +14,16 @@ router.get('/', (req, res, next) => {
     res.status(500).json({ error: error})
   })
 })
+
+router.get('/:id', (req, res, next) => {
+  const  id  = req.params;
+
+  userDb
+  .get(id)
+  .then(users => {
+    res.json(users);
+  }) 
+  .catch (error => {
+    res.status(500).json({error: error});
+  });
+});
