8c22a511662e8f0a93eb35c8151ea1a50626c282
Mark Wong <fmarkwong@gmail.com>
2018-05-03 15:53:05 -0700

    capitalize tags

--- server.js
+++ server.js
@@ -8,6 +8,15 @@ server.listen(5000, () => console.log('\nServer listening on port 5000\n'));
 server.use(express.json());
 // future reference, this is for x-www-form-urlendcoded form data
 // server.use(express.urlencoded({ extended: true }));
+//
+
+const captalizeTags = (req, resp, next) => {
+  if (req.body.tag) {
+    req.body.tag = req.body.tag.toUpperCase();
+  }
+  next();
+}
+server.use(captalizeTags);
 
 server.use('/api/users', userRoutes);
 server.use('/api/posts', postRoutes);
