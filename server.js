09ffaf9f1b2daefcdab30b2cc4d00e4b15219c1f
odax <nr10@albion.edu>
2018-05-03 18:58:43 -0400

    working on stuff

--- server.js
+++ server.js
@@ -1,43 +1,21 @@
 const express = require('express');
 const userDb = require('./data/helpers/userDb.js');
 // const tagDb = require('./data/helpers/tagDb.js');
-// const postDb = require('./data/helpers/postDb.js');
+const postDb = require('./data/helpers/postDb.js');
 
 const server = express();
 
 server.use(express.json());
 
-//Home
+//Homepage
 server.get('/', (req, res) => {
     res.send('Anti-Homepage');
 });
 
 server.listen(5000, () => console.log('\n== API Running on port 5000 ==\n'));
-
-// User handlers /users
-// User Post ================================================================================================================
-server.post("/users", (req, res) => {
-    const newUser = req.body;
-    if (!newUser.name) {
-      res.status(400).json({
-        errorMessage: "Please provide the name of the user!"
-      });
-    }
-    userDb
-      .insert(newUser)
-      .then(user => {
-        res.status(201).json(user);
-      })
-      .catch(err => {
-        res.status(500).json({
-          error: "There was an error while saving the user to the database!"
-        });
-      });
-  });
-
-// User Get All =====================================================================================================================
-//when client uses .get --> db.find -----------------------------------------------------------------------
-server.get('/users', (req, res) => {
+//============================================USER===========================================// userDb 
+//User get all, get all of the users
+  server.get('/users', (req, res) => {
     userDb.get().then(users => {
         res.json(users);
     })
@@ -45,242 +23,200 @@ server.get('/users', (req, res) => {
         res.status(500).json({error: 'could not retrieve user information!'});
         process.abort();
     })
+});
+
+// User get id, get the user of the specified id
+server.get('/users/:id', (req, res) => {
+    const id = req.params.id;
+    userDb
+    .get(id)
+    .then(user => {
+        if (user.length === 0) {
+            res.status(404).json({ message: 'The user with the specified ID does not exist.'})
+        } else {
+            res.json(user);
+        }
+    })
+    .catch(err => {
+        res.status(500).json({error: 'The user information could not be found'});
+        process.abort();
+    });
+});
+
+//User delete, delete user
+server.delete('/users/:id', (req, res) => {
+    const { id } = req.params;
+    let user;
+    userDb
+        .get(id)
+        .then(founduser => {
+            user = { ...founduser };
+            })
+        userDb.remove(id).then (response => {
+        if (id === 'undefined') {
+            res.status(404).json({ message: 'The user with the specified ID does not exist.'})
+        } else {
+            res.status(200).json(user);
+        }
+    })
+    .catch(err => {
+        res.status(500).json({error: 'The user could not be removed.'});
+        process.abort();
+        })
+});
+
+//User update, update username
+server.put('/users/:id', function(req, res) {
+    const { id } = req.params;
+    const update = req.body;
+if (!update.name) {
+    res.status(400).json({
+      errorMessage: "Please the name for the user."
+    });
+  }
+    userDb.update(id, update)
+    .then(count => {
+        if (count > 0) {
+            res.status(200).json({ msg: 'user updated successfully' })
+        } else {
+            res.status(404).json({ msg: 'user not found'});
+        }
+    })
+    .catch(err => {
+        res.status(500).json(err)
+    })
+});
+
+// User Post, create new user
+server.post("/users", (req, res) => {
+  const newUser = req.body;
+  if (!newUser.name) {
+    res.status(400).json({
+      errorMessage: "Please provide the name of the user!"
+    });
+  }
+  userDb
+    .insert(newUser)
+    .then(user => {
+      res.status(201).json(user);
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: "There was an error while saving the user to the database!"
+      });
+    });
+});
+
+// User get Posts, finds posts of a specific user
+server.get('/users/:id/posts', (req, res) => {
+  const id = req.params.id;
+  userDb
+  .getUserPosts(id)
+  .then(posts => {
+      if (posts.length === 0) {
+          res.status(404).json({ message: 'That user does not have any posts'})
+      } else {
+          res.json(posts);
+      }
 })
+  .catch(err => {
+      res.status(500).json({error: 'The users posts could not be found'});
+      process.abort();
+  });
+});
 
+//=============================================Posts===========================================// postDb
+//posts get all, get all posts of all users
+server.get('/posts', (req, res) => {
+  postDb.get().then(posts => {
+      res.json(posts);
+  })
+  .catch(err => {
+      res.status(500).json({error: 'could not retrieve post information!'});
+      process.abort();
+  })
+});
 
+// posts get id, get the post of the specified id
+server.get('/posts/:id', (req, res) => {
+  const id = req.params.id;
+  postDb
+  .get(id)
+  .then(post => {
+      if (post.length === 0) {
+          res.status(404).json({ message: 'The post with the specified ID does not exist.'})
+      } else {
+          res.json(post);
+      }
+  })
+  .catch(err => {
+      res.status(500).json({error: 'The post information could not be found'});
+      process.abort();
+  });
+});
 
-// const express = require('express');
-// const db = require('./data/db.js');
+//posts remove, delete a post based on it's ID ======Trying to resolve this... status 500****************
+server.delete('/posts/:id', (req, res) => {
+  const { id } = req.params.id;
+  let posts;
+  postDb
+      .get(id)
+      .then(foundposts => {
+          posts = { ...foundposts };
+          })
+      postDb.remove(id).then (response => {
+      if (id === 'undefined') {
+          res.status(404).json({ message: 'The post with specified ID could not be found'})
+      } else {
+          res.status(200).json(posts);
+      }
+  })
+  .catch(err => {
+      res.status(500).json({error: 'The post could not be removed.'});
+      process.abort();
+      })
+});
 
-// const server = express();
-
-// function greeter(name) {
-//     return function(req, res, next) {
-//         console.log(`hello ${name}`);
-//         console.log(req.query);
-
-//         if (req.query.pass === 'mellon') {
-//             next();
-//         } else {
-//             res.send('YOU SHALL NOT PASS!!!');
-//         }
-//     };
-// }
-
-// function logger(msg) {
-//     return function(req, res, next) {
-//         console.log(`\n= ${msg}: ${req.url}`);
-
-//         next();
-//     }
-// }
-
-// server.use(greeter('Nalee'));
-// server.use(logger('loading'));
-// server.use(express.json());
-
-// //Home
-// server.get('/', (req, res) => {
-//     res.send('This is the homepage');
-// });
-
-// //when client uses .post --> db.insert --------------------------------------------------------------------
-// server.post("/api/posts", (req, res) => {
-//     const newPost = req.body;
-//     if (!newPost.title || !newPost.contents) {
-//       res.status(400).json({
-//         errorMessage: "Please provide title and contents for the post."
-//       });
-//     }
-//     db
-//       .insert(newPost)
-//       .then(post => {
-//         res.status(201).json(post);
-//       })
-//       .catch(err => {
-//         res.status(500).json({
-//           error: "There was an error while saving the post to the database"
-//         });
-//       });
+// //update one post of a user
+// server.put('/users/:id', function(req, res) {
+//   const { id } = req.params;
+//   const update = req.body;
+// if (!update.name) {
+//   res.status(400).json({
+//     errorMessage: "Please the name for the user."
 //   });
+// }
+//   userDb.update(id, update)
+//   .then(count => {
+//       if (count > 0) {
+//           res.status(200).json({ msg: 'user updated successfully' })
+//       } else {
+//           res.status(404).json({ msg: 'user not found'});
+//       }
+//   })
+//   .catch(err => {
+//       res.status(500).json(err)
+//   })
+// });
 
-// //when client uses .get --> db.find -----------------------------------------------------------------------
-// server.get('/api/posts', (req, res) => {
-//     db.find().then(posts => {
-//         res.json(posts);
-//     })
-//     .catch(err => {
-//         res.status(500).json({error: 'could not retrieve post information'});
-//         process.abort();
-//     })
-// })
-
-// //when client uses .get --> db.findbyid ----------------------------------------------------------------------
-// server.get('/api/posts/:id', (req, res) => {
-//     const id = req.params.id;
-//     db
-//     .findById(id)
-//     .then(post => {
-//         if (post.length === 0) {
-//             res.status(404).json({ message: 'The post with the specified ID does not exist.'})
-//         } else {
-//             res.json(post);
-//         }
-//     })
-//     .catch(err => {
-//         res.status(500).json({error: 'The post information could not be found'});
-//         process.abort();
+// // add a post to a user
+// server.post("/users", (req, res) => {
+// const newUser = req.body;
+// if (!newUser.name) {
+//   res.status(400).json({
+//     errorMessage: "Please provide the name of the user!"
+//   });
+// }
+// userDb
+//   .insert(newUser)
+//   .then(user => {
+//     res.status(201).json(user);
+//   })
+//   .catch(err => {
+//     res.status(500).json({
+//       error: "There was an error while saving the user to the database!"
 //     });
+//   });
 // });
 
-// //when client uses .delete --> db.remove ------------------------------------------------------------------------------------------
-// server.delete('/api/posts/:id', (req, res) => {
-//     const { id } = req.params;
-//     let post;
-//     db
-//         .findById(id)
-//         .then(foundpost => {
-//             post = { ...foundpost[0] };
-//             })
-//         db.remove(id).then (response => {
-//         if (id === 'undefined') {
-//             res.status(404).json({ message: 'The post with the specified ID does not exist.'})
-//         } else {
-//             res.status(200).json(post);
-//         }
-//     })
-//     .catch(err => {
-//         res.status(500).json({error: 'The post could not be removed.'});
-//         process.abort();
-//         })
-// })
-
-// //when client uses .put --> db.update ------------------------------------------------------------
-// server.put('/api/posts/:id', function(req, res) {
-//     const { id } = req.params;
-//     const update = req.body;
-// //below 200 is the default status you don't need to write it
-// // if count = 0 then it doesn't fail, it just doesn't find the correct ID
-// if (!update.title || !update.contents) {
-//     res.status(400).json({
-//       errorMessage: "Please provide title and contents for the post."
-//     });
-//   }
-//     db.update(id, update)
-//     .then(count => {
-//         if (count > 0) {
-//             res.status(200).json({ msg: 'updated successfully' })
-//         } else {
-//             res.status(404).json({ msg: 'post not found'});
-//         }
-//     })
-//     .catch(err => {
-//         res.status(500).json(err)
-//     })
-// })
-
-// server.listen(5000, () => console.log('\n== API Running on port 5000 ==\n'));
-
-
-
-
-
-
-
-
-//below is code from node-express-mini
-//---------------------------------------------------------------------------------
-// const express = require('express');
-
-// const db = require('./data/db');
-
-// const server = express();
-
-// server.use(express.json());
-
-// server.get('/', (req, res) => {
-//     res.send('Api Running');
-// });
-
-
-// //route handlers
-// // get all data
-// server.get('/api/users', (req, res) => {
-//     //get the users
-//     //.then should return an array of all of the users (see github repo)
-//     db.find().then(users => {
-//         res.json(users);
-//     }).catch(err => {
-//         res.status(500).json({ error: err });
-//     });//do something with the error}) //the bros
-
-//     //return the users
-// });
-
-// // /api/users/2
-// server.get('/api/users/:id', (req, res) => {
-//     const id = req.params.id;
-//     console.log('params', req.params);
-//     db
-//     .findById(id)
-//     .then(users => {
-//         if (users.length === 0) {
-//             res.status(404).json({ message: 'user not found'});
-//         } else {
-//         res.json(user);
-//         }
-//     })
-//     .catch(err => {
-//         res.status(500).json({ error: err });
-//     });//do something with the error}) //the bros
-// });
-
-// //.post --> insert
-// server.post('/api/users', (req, res) => {
-//     // const id = req.params.id;
-//     db.insert(req.body).then(users => {
-//         res.status(201).json(users);
-//     })
-//     .catch(err => {
-//         res.status(500).json({ error: err});
-//     })
-// })
-
-// //delete
-// server.delete('/api/users', function(req, res) {
-//     const { id } = req.query;
-//     let user;
-//     db
-//         .findById(id)
-//         .then(foundUser => {
-//             user = { ...foundUser[0] };
-        
-//         db.remove(id).then(response => {
-//             res.status(200).json(user);
-//         });
-//         })
-//         .catch(err => {
-//             res.status(500).json({ erro: err });
-//         });
-// });
-
-// //update
-// server.put('/api/users/:id', function(req, res) {
-//     const { id } = req.params;
-//     const update = req.body;
-// //below 200 is the default status you don't need to write it
-// // if count = 0 then it doesn't fail, it just doesn't find the correct ID
-//     db.update(id, update)
-//     .then(count => {
-//         if (count > 0) {
-//             res.status(200).json({ msg: 'updated successfully' })
-//         } else {
-//             res.status(404).json({ msg: 'user not found'});
-//         }
-//     })
-//     .catch(err => {
-//         res.status(500).json(err)
-//     })
-// })
-
-// server.listen(5000, () => console.log('\n== API Running on port 5000 ==\n'));
+// //get all tags off of a post
