2b91cdafb6246b9d214eedcfc315e3700552d622
Devon Bacon <devon.bacon@gmail.com>
2018-05-03 13:41:26 -0700

    Finished functionality of userRouter.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,18 @@
 const express = require('express');
-
-const db = require('../data/dbConfig');
+const data = require('../data/helpers/userDb');
 
 const router = express.Router();
+
+router.get('/:id/posts', (req, res) => {
+  data.getUserPosts(req.params.id)
+    .then(posts => {
+      res.json(posts);
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: "The information could not be retrieved."
+      });
+    })
+});
 
 module.exports = router;
