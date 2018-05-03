1b6592e522268c09abc01f81f8451917e75ecec9
Devon Bacon <devon.bacon@gmail.com>
2018-05-03 13:41:38 -0700

    Made server.js more readable

--- server.js
+++ server.js
@@ -6,74 +6,76 @@ const data = {
 }
 
 const server = express();
+const userRoutes = require('./routes/userRoutes');
 server.use(express.json());
+server.use('/api/users', userRoutes);
 
 // get all data pertaining to target param
 server.get('/api/:target', (req, res) => {
   data[req.params.target]
-  .get()
-  .then(target => {
-    res.json(target);
-  })
-  .catch(err => {
-    res.status(500).json({
-      error: "The information could not be retrieved."
+    .get()
+    .then(target => {
+      res.json(target);
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: "The information could not be retrieved."
+      });
     });
-  });
 });
 
 // get specific data
 server.get('/api/:target/:id', (req, res) => {
   data[req.params.target]
-  .get(req.params.id)
-  .then(target => {
-    if (target) {
-      res.json(target);
-    } else {
-      res.status(404).json({
-        err: "The data with the specified ID does not exist."
-      })
-    }
-  })
-  .catch(err => {
-    res.status(500).json({
-      error: "The information could not be retrieved."
+    .get(req.params.id)
+    .then(target => {
+      if (target) {
+        res.json(target);
+      } else {
+        res.status(404).json({
+          err: "The data with the specified ID does not exist."
+        })
+      }
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: "The information could not be retrieved."
+      });
     });
-  });
 });
 
 // add new data
 server.post('/api/:target', (req, res) => {
   data[req.params.target]
-  .insert(req.body)
-  .then(objId => {
-    res.status(201).json(objId);
-  })
-  .catch(err => {
-    res.status(500).json({
-      error: "There was an error while saving the post to the database"
+    .insert(req.body)
+    .then(objId => {
+      res.status(201).json(objId);
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: "There was an error while saving the post to the database"
+      });
     });
-  });
 });
 
 // update data by id
 server.put('/api/:target/:id', (req, res) => {
   data[req.params.target]
-  .update(req.params.id, req.body)
-  .then(found => {
-    if (found) {
-      res.status(200).json(req.body);
-    } else {
-      res.status(404).json({
-        err: "The post with the specified ID does not exist."
+    .update(req.params.id, req.body)
+    .then(found => {
+      if (found) {
+        res.status(200).json(req.body);
+      } else {
+        res.status(404).json({
+          err: "The post with the specified ID does not exist."
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: "The post information could not be modified."
       });
-    }
-  })
-  .catch(err => {
-    res.status(500).json({
-      error: "The post information could not be modified."
     });
-  });
 });
 
 // delete specific data
