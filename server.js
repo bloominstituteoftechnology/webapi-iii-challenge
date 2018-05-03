46c131057a562d8a936486ace87b55925c36cf0a
Eric Gomez <eric_gomez_1@hotmail.com>
2018-05-03 12:54:18 -0700

    finished setting up my userRoute middleware
    its not working on my end but the code seems right to me

--- server.js
+++ server.js
@@ -1,51 +1,18 @@
-//import all dependency's here:
 const express = require('express');
-const cors = require('cors');
 const helmet = require('helmet');
-const userDb = require('./data/helpers/userDb');
-const postDb = require('./data/helpers/postDb');
-const tagsDb = require('./data/helpers/tagDb');
-const userRoute = require('./userRoute');
-
+const cors = require('cors');
 const port = 5000;
+const userRoutes = require('./RouteHandlers/userRoutes');
+
 const server = express();
 
-// "start": "nodemon server.js --exec babel-node --presets es2015,stage-2"
-// create custom middleware here:
-
-
-// apply middleware here:
-server.use(helmet());
-server.use(cors());
 server.use(express.json());
+server.use(cors());
+server.use(helmet());
 
-// add route handlers:
-server.use('/api/users', userRoute);
+server.use('/api/users', userRoutes);
 
-// create route handlers here:
 
-// server.post('/api/users', (req, res) => {
-//   const {name} = req.body;
-//   userDb.insert(name)
-//     .then(obj => {
-//       userDb.get(obj.id)
-//         .then(user => {
-//           res.json(user);
-//         })
-//     })
-// })
-server.get('/api/posts', (req, res) => {
-  postsDb.get()
-    .then(posts => {
-      res.json(posts);
-    });
-})
-server.get('/api/tags/', (req, res) => {
-  tagsDb.get()
-    .then(tags => {
-      res.json(tags);
-    })
-})
 server.listen(port, () => {
   console.dir(`server listening on port ${port}`);
 })
