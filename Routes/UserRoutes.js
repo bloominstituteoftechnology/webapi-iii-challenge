192e40b34b8584a0c98f0c54508cc7da40772226
Yasin Shuman <email@yasin.io>
2018-05-02 21:37:11 -0700

    began work on routes but not really

--- Routes/UserRoutes.js
+++ Routes/UserRoutes.js
@@ -4,12 +4,16 @@ const router = express.Router();
 
 const db = require("../data/helpers/userDb");
 //this code is wrong
-router.get("/:id", (req, res, next) => {
+router.get("/", (req, res, next) => {
   const { id } = req.query;
   let user;
   db.get(id).then(foundUser => {
     res.json(foundUser);
   });
 });
+
+//router.put
+//router.post
+//router.delete
 
 module.exports = router;
