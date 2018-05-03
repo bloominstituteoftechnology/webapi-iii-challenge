eb71005a9c6f5be51a50f055c61cba8b99b49227
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2018-05-03 14:03:32 -0700

    userRoutes.js is now working; tested CRUD in Postman

--- Users/userRoutes.js
+++ Users/userRoutes.js
@@ -4,30 +4,88 @@ const db = require('../data/helpers/userDb');
 const router = express.Router();
 
 
-//GET (retrieve post)
+//GET (retrieve user) //Postman test ok: http://localhost:5000/api/users 
+router.get('/', (req, res) => {
+    db
+    .get()
+    .then(posts => {
+        res.status(200).json(posts);
+    })
+    //If there's an error in retrieving the user from the database
+    .catch(err => {
+        res.status(500).json({ errorMsg: 'User Info could not be found.' })
+    });
+});
 
-//If there's an error in retrieving the post from the database
+//GET (retrieve user by id) //Postman test ok: http://localhost:5000/api/users/2
+router.get('/:id', (req, res) => {
+    const id = req.params.id; 
+    db
+    .get(id)
+    .then(posts => {
+        if (posts.length === 0) {
+            res.status(404).json({ errorMsg: 'The specified UserID does not exist.' })
+        } else {
+            res.json(posts);
+        }
+    })
+    //If there's an error in retrieving the user from the database
+    .catch(err => {
+        res.status(500).json({ errorMsg: 'User Info could not be found.' })
+    });
+});
 
-
-//GET (retrieve post by id)
-
-//If there's an error in retrieving the post from the database
-
-
-//POST (add post)
-
-//If there's an error in retrieving the post from the database
-
-
-//DELETE (delete post)
-
-//If there's an error in retrieving the post from the database
-
-
-//PUT (update post)
-
-//If there's an error in retrieving the post from the database
-
+//POST (add user) //Postman test ok: http://localhost:5000/api/users (added Froto Puggins, id 10)
+router.post('/', (req, res) => {
+    const {name} = req.body;
+    const newPost = {name};
+        if (req.body.length === 0) {
+            res.status(404).json({ errorMsg: 'User with specified userID does not exist.' })
+        } else 
+        db
+        .insert(newPost)
+        .then(post => {
+            res.status(201).json(post);
+        })
+        //If there's an error in retrieving the user from the database
+        .catch(err => {
+            res.status(500).json({ errorMsg: 'Error saving user to database.' })
+        });
+});
+    
+//DELETE (delete user) //Postman test ok: http://localhost:5000/api/users/11 (userID 11 removed)
+router.delete('/:id', (req, res) => {
+    const id = req.params.id;
+    if(!db.get(id)) {
+        res.status(404).json({ errorMsg: 'User with specified userID does not exist.' })
+    } else
+    db.remove(id)
+    .then(remove => {
+        res.status(201).json(remove);
+    })
+    //If there's an error in retrieving the user from the database
+    .catch(err => {
+        res.status(500).json({ errorMsg: 'Sorry, User could not be removed.' })
+    });
+});
+    
+//PUT (update user) //Postman test ok: http://localhost:5000/api/users/10 (name modified to FROTO PUGGINS PUGGINGTON)
+router.put('/:id', (req, res) => {
+    const {name} = req.body;
+    const id = req.params.id;
+    if(!db.get(id)) {
+        res.status(404).json({ errorMsg: 'User with the specified userID does not exist.' })
+    } else 
+    db.update(id, req.body)
+    .then(improve => {
+        res.status(200).json(improve);
+    })
+    //If there's an error in retrieving the user from the database
+    .catch(err => {
+        res.status(500).json({ errorMsg: 'User info could not be modified' })
+    });
+});
+    
 
 //module export
 module.exports = router;
