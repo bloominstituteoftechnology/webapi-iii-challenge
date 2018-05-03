d9af4107cd79eaebd399cb919ef81d865fb42f48
sadrak105 <sadrak10@gmail.com>
2018-05-03 17:18:44 -0400

    server listening

--- server.js
+++ server.js
@@ -7,3 +7,6 @@ const server = express();
 server.use(express.json());
 server.use(cors());
 server.use(helmet());
+
+const port = 8000;
+server.listen(port, () => console.log(`\n== API Running on port ${port} ==\n`));
