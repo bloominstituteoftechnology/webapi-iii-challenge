f9dd6df1db01e1a6e7df166aaddd372f7171e3e1
MINE <Mine@MINEs-MacBook-Pro-2.local>
2018-05-03 15:17:31 -0700

    Add Middleware

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 // yarn init -y
 // yarn install
+// yarn add express
+// yarn add helmet
 // npm i knex
 // npm i sqlite3
 // yarn add nodemon --dev
 
 // import your node modules
 const express = require('express');
+// const morgan = require("morgan");
+// const helmet = require("helmet");
 // yarn add helmet -> npm i helmet
 // const helmet = require('helmet');
 
@@ -14,3 +18,28 @@ const server = express();
 
 const db = require('./data/dbConfig.js');
 
+// const userRouter = require("./users/userRouter.js");
+// const postRouter = require("./posts/postRouter.js");
+// const tagRouter = require("./tags/tagRouter.js");
+
+function toUpperCase(req, res, next) {
+    console.log("In middleware: ", req.body);
+    if (req.body.tag) {
+        req.body.tag = req.body.tag.toUpperCase();
+    }
+    next();
+}
+
+// server.use(helmet());
+server.use(express.json());
+// server.use(morgan());
+server.use(toUpperCase);
+
+// server.use("/api/posts/", postRouter);
+// server.use("/api/users/", userRouter);
+// server.use("/api/tags/", tagRouter);
+
+const port = 8000;
+server.listen(port, () => {
+    console.log("API Running");
+});
