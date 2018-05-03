e9f616a8a49777f53cd00ccd339a4ac8d7c3253a
Yasin Shuman <email@yasin.io>
2018-05-03 15:10:34 -0700

    added babel dependencies

--- Routes/UserRoutes.js
+++ Routes/UserRoutes.js
@@ -5,11 +5,12 @@ const router = express.Router();
 const db = require("../data/helpers/userDb");
 //this code is wrong
 router.get("/", (req, res, next) => {
-  const { id } = req.query;
-  let user;
-  db.get(id).then(foundUser => {
-    res.json(foundUser);
-  });
+  // const { id } = req.query;
+  // let user;
+  // db.get(id).then(foundUser => {
+  //   res.json(foundUser);
+  // });
+  res.send("hello");
 });
 
 //router.put
