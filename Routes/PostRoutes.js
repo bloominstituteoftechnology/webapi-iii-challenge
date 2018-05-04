d2a0581dd482e20871e176e9f58c1c6f07a8644e
Yasin Shuman <email@yasin.io>
2018-05-03 21:53:33 -0700

    fixed some typos

--- Routes/PostRoutes.js
+++ Routes/PostRoutes.js
@@ -1,19 +1,19 @@
-import express from 'express';
-import PostController from '../Controllers/PostController';
+import express from "express";
+import PostController from "../Controllers/PostController";
 
 const PostRouter = express.Router();
 const {
-	getpost,
-	getPostTags,
-	createPost,
-	updatePost,
-	removePost,
+  getPost,
+  getPostTags,
+  createPost,
+  updatePost,
+  removePost
 } = PostController;
 
-PostRouter.get('/:id', getPost);
-PostRouter.get('/:id/tags', getPostPosts);
-PostRouter.Post('/', createPost);
-PostRouter.put('/:id', updatePost);
-PostRouter.delete('/:id', removePost);
+PostRouter.get("/:id", getPost);
+PostRouter.get("/:id/tags", getPostTags);
+PostRouter.post("/", createPost);
+PostRouter.put("/:id", updatePost);
+PostRouter.delete("/:id", removePost);
 
 export default PostRouter;
--- server.js
+++ server.js
@@ -1,17 +1,12 @@
-
-import express from 'express';
-import UserRouter from './Routes/UserRoutes';
-import PostRouter from './Routes/PostRoutes';
-import TagRouter from './Routes/TagRoutes';
-
-const userRoutes = require('./Routes/userRoutes');
+import express from "express";
+import UserRouter from "./Routes/UserRoutes";
+import PostRouter from "./Routes/PostRoutes";
+import TagRouter from "./Routes/TagRoutes";
 
 const app = express();
 app.use(express.json);
-app.use('/api/users', UserRouter);
-app.use('/api/posts', PostRouter);
-app.use('/api/tags', TagRouter);
+app.use("/api/users", UserRouter);
+app.use("/api/posts", PostRouter);
+app.use("/api/tags", TagRouter);
 
-// server.use(errorHandler);
-server.listen(3333, () => console.log('\n== API running on port 3333 ==\n'));
-
+app.listen(3333, () => console.log("\n== API running on port 3333 ==\n"));
