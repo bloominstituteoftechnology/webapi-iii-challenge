703f065708b455d439af9cd210280d43513b29c4
MINE <Mine@MINEs-MacBook-Pro-2.local>
2018-05-03 17:10:23 -0700

    Modified TagRouter code

--- tags/tagRouter.js
+++ tags/tagRouter.js
@@ -6,87 +6,73 @@ const db = require("../data/helpers/tagDb.js");
 
 // GET/Request Tags
 router.get("/", (req, res) => {
-  db
-    .get()
-    .then(tags => {
-      res.json(tags);
-    })
-    .catch(error => {
-      res
-        .status(500)
-        .json({ message: "There was an error getting tags from the server." });
-    });
+    db
+        .get()
+        .then(tags => {
+            res.json(tags);
+        })
+        .catch(error => {
+            res.status(500).json({ message: "There was an error getting tags from the server." });
+        });
 });
 
 // GET/Request Specific Tag
 router.get("/:tagId", (req, res) => {
-  db
-    .get(req.params.tagId)
-    .then(tag => {
-      res.json(tag);
-    })
-    .catch(error => {
-      res
-        .status(500)
-        .json({ message: "There was an error getting tags from the server." });
-    });
+    db
+        .get(req.params.tagId)
+        .then(tag => {
+            res.json(tag);
+        })
+        .catch(error => {
+            res.status(500).json({ message: "There was an error getting tags from the server." });
+        });
 });
 
 // POST/Create New Tag
 router.post("/", (req, res) => {
-  let newTag = req.body;
-  db
-    .insert(newTag)
-    .then(tagId => {
-      const { id } = tagId;
-      db.get(id).then(tag => {
-        res.json(tag);
-      });
-    })
-    .catch(error => {
-      res
-        .status(500)
-        .json({ message: "There was an error posting tags to the server." });
-    });
+    let newTag = req.body;
+    db
+        .insert(newTag)
+        .then(tagId => { 
+        const { id } = tagId;
+            db.get(id).then(tag => {
+                res.json(tag);
+            });
+        })
+        .catch(error => {
+            res.status(500).json({ message: "There was an error posting tags to the server." });
+        });
 });
 
 // DELETE/Remove Tag
 router.delete("/:tagId", (req, res) => {
-  db
-    .remove(req.params.tagId)
-    .then(num => {
-      if (num < 1) {
-        res.status(404).json({ message: "There is no tag with that id." });
-      } else {
-        res.json({
-          message: `Tag with the id ${req.params.tagId} successfully deleted.`
+    db
+        .remove(req.params.tagId)
+        .then(num => {
+            if (num < 1) {
+                res.status(404).json({ message: "There is no tag with that id." });
+            } else {
+                res.json({ message: `Tag with the id ${req.params.tagId} successfully deleted.` });
+            }
+        })
+        .catch(error => {
+            res.status(500).json({ message: "There was an error deleting tag from the server." });
         });
-      }
-    })
-    .catch(error => {
-      res
-        .status(500)
-        .json({ message: "There was an error deleting tag from the server." });
-    });
 });
 
 // PUT/Updating Tag
 router.put("/:tagId", (req, res) => {
-  db
-    .update(req.params.tagId, req.body)
-    .then(count => {
-      if (count < 1) {
-        res.status(404).json({ message: "There was no tag by that ID" });
-      } else {
-        db.get(req.params.tagId).then(tag => {
-          res.json(tag);
-        });
-      }
-    })
-    .catch(error => {
-      res
-        .status(500)
-        .json({ message: "There was an error updating tags on the server." });
+    db
+        .update(req.params.tagId, req.body)
+        .then(count => {
+            if (count < 1) {
+                res.status(404).json({ message: "There was no tag by that ID" });
+            } else {
+                db.get(req.params.tagId).then(tag => { res.json(tag);});
+            }
+        })
+        .catch(error => {
+            res.status(500).json({ message: "There was an error updating tags on the server." });
     });
 });
 
