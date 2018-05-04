34817b0392f1e985ad82b2737d1065882b951409
Nedu Robert <nedumrobert@gmail.com>
2018-05-04 16:19:38 +0100

    added App component

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,59 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Route } from 'react-router-dom';
+import axios from 'axios';
+
+import Users from './Components/Users/Users';
+import User from './Components/User/User';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+      name: '',
+    };
+  }
+
+  componentDidMount() {
+    this.getUsers();
+  }
+
+  getUsers = () => {
+    axios
+      .get('http://localhost:5000/api/users')
+      .then(res => {
+        this.setState({ users: res.data });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  handleNewUser = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  addUser = e => {
+    const user = {
+      name: this.state.name,
+    };
+    axios
+      .post('http://localhost:5000/api/users', user)
+      .then(newUser => {
+        console.log(newUser);
+        this.setState({ name: '' });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    return <div className="App">
+        <Route exact path="/users" render={props => <Users {...props} users={this.state.users} handleNewUser={this.handleNewUser} addUser={this.addUser} state={this.state} />} />
+        <Route exact path="/users/:id" render={props => <User {...props} users={this.state.users} getUsers={this.getUsers} />} />
+      </div>;
   }
 }
 
