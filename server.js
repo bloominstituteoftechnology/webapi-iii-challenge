6b838885056251f3255ac7f7bda9fa49e1d384cf
Anthony <anthony@Anthonys-MacBook-Pro.local>
2018-05-05 17:32:43 -0400

    added route handlers and set up server file

--- server.js
+++ server.js
@@ -1,39 +1,27 @@
 // import your node modules
 const cors = require('cors');
-const postDb = require('./data/helpers/post.Db');
-const tabDb = require('./data/helpers/tagDb.Db');
-const userDb = require('./data/helpers/userDb.Db');
-
+const postDb = require('./data/helpers/postDb');
+const tabDb = require('./data/helpers/tagDb');
+const userDb = require('./data/helpers/userDb');
+const dbConfig = require('./data/dbConfig.js')
 const express = require('express');
-const db = require('./data/db.js');
+const helmet = require('helmet');
+const port = 5000;
 const server = express();
 
+const userRouter = require('./RouteHandlers/userRouter');
+const tagRouter = require('./RouteHandlers/tagRouter');
+const postRouter = require('./RouteHandlers/postRouter');
 
-// Adding Middleware:
+//Middleware
+server.use(helmet());
 server.use(express.json());
 
-// Route Handlers
-// Post a Post 
 
-server.post('/api/posts', (request, response) => {
-    const id = request.params.id;
-    let title = request.body.title;
-    let contents = request.body.contents;
-    let post = { title, contents };
+// Routes
+server.use('/api/users', userRouter);
+server.use('/api/tags', tagRouter);
+server.use('/api/posts', postRouter);
 
-    db
-        .insert(post)
-        .then(post => {
-            response.status(201);
-            response.json(post);
-        })
-        .catch(error => {
-            if (!title || !contents) {
-                response.status(400);
-                response.json({ errorMessage: 'Please provide title and contents.' })
-            }
-            response.status(500);
-            response.json({ error: 'There was an error while saving the post to the database.' })
-        })
 
-});
+server.listen(5000, () => console.log('Listening!'))
