27d75b119fb423402b13d2739b84302b2d7bfc8b
jesuarva <jean.ariza@jesuarva.com>
2018-06-01 01:23:20 +0200

    ADD DELETE/:id to all 3 endpoints via the routerFactory function

--- Routers/routerFactory.js
+++ Routers/routerFactory.js
@@ -14,7 +14,7 @@ function routerFactory(router, db, errorStatus, errorMessage) {
   });
 
   router.get("/:id", (req, res) => {
-    const {id} = req.params;
+    const { id } = req.params;
     console.log(id);
 
     db
@@ -26,6 +26,20 @@ function routerFactory(router, db, errorStatus, errorMessage) {
       .catch(e => {
         console.log("error", e);
         res.status(errorStatus).json(errorDb);
+      });
+  });
+
+  router.delete("/:id", (req, res) => {
+    const { id } = req.params;
+    console.log(id);
+    db
+      .remove(id)
+      .then(response => {
+        console.log("response", response);
+        res.status(200).json(response);
+      })
+      .catch(e => {
+        console.log("error", e);
       });
   });
 }
--- Routers/users.js
+++ Routers/users.js
@@ -6,5 +6,7 @@ const routerFactory = require("./routerFactory");
 
 routerFactory(users, db, 500, "Can not get users");
 
+// Add ligic for "db.getUserPosts"
+
 module.exports = users;
 
