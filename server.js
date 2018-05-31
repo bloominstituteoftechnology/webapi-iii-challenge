06cbcd732d8667717747aba3a66ccd14cd6e030c
maxwashington <mnwashington18@gmail.com>
2018-05-31 17:21:52 -0500

    Updated server

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const postDB = require("./data/helpers/postDb");
 const tagDB = require("./data/helpers/tagDB");
 const userDB = require("./data/helpers/userDB");
@@ -12,5 +13,13 @@ const tagRoutes = require("./Tags/TagRoutes")
 const postRoutes = require("./Posts/PostRoutes")
 
 //MiddleWare
+
+server.use(cors())
+server.use(express.json())
+server.use("/api/user" , userRoutes)
+server.use("/api/post" , postRoutes)
+server.use("/api/tags" , tagRoutes)
+
+
 
 server.listen(port, () => console.log(`*** Server Running on Port ${port} ***`))
