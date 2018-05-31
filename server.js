4d098595418a0302bb04ca40514dc19afdcfacaa
A99 <A99@99s-macbook-pro.home>
2018-06-01 00:32:31 +0200

    Added posts functionality

--- server.js
+++ server.js
@@ -129,4 +129,118 @@ server.delete('/api/users/:id', (req, res) => {
 
 
 
+
+
+
+// Posts API
+
+server.get('/api/posts', (req, res) => {
+    posts
+        .get()
+        .then(posts => {
+            res.json({ posts });
+        })
+        .catch(error => {
+            sendUserError(500, `There was an error processing your request`, res);
+            return;
+        });
+});
+
+server.get('/api/posts/:id', (req, res) => {
+    const { id } = req.params;
+    posts
+        .get(id)
+        .then(post => {
+            res.json(post);
+        })
+        .catch(error => {
+            sendUserError(500, `There was an error processing your request`, res);
+            return;
+        });
+});
+
+server.get('/api/posts/:id/tags', (req, res) => {
+    const { id } = req.params;
+    posts
+        .getPostTags(id)
+        .then(tags => {
+            if(tags.length === 0) {
+                sendUserError(404, `The post with the ID ${id} has no tags.`, res);
+                return;
+            }
+            res.json({ tags });
+        })
+        .catch(error => {
+            sendUserError(500, `There was an error processing your request`, res);
+            return;
+        });
+});
+
+server.post('/api/posts', (req, res) => {
+    const { text, userId } = req.body;
+    if(!text || !userId) {
+        sendUserError(400, `Must provide a userId and text for a new post`, res);
+        return;
+    }
+    posts
+        .insert({text, userId})
+        .then(response => {
+            res.status(201).json(response);
+        })
+        .catch(error => {
+            sendUserError(500, `There was an error processing your request`, res);
+            return;
+        });
+});
+
+server.put('/api/posts/:id', (req, res) => {
+    const { id } = req.params;
+    const { text } = req.body;
+    if (!text) {
+        sendUserError(400, `Post must have text`, res);
+        return;
+    }
+    posts
+        .update(id, {text})
+        .then(post => {
+            if(post === 0) {
+                sendUserError(404, `The post with the ID ${id} does not exist.`, res);
+                return;
+            }
+            posts
+                .get(id)
+                .then(post => {
+                    res.json(post);
+                })
+                .catch(error => {
+                    sendUserError(500, `There was an error processing your request`, res);
+                    return;
+                })
+        })
+        .catch(error => {
+            sendUserError(500, `There was an error processing your request`, res);
+            return;
+        });
+});
+
+server.delete('/api/posts/:id', (req, res) => {
+    const { id } = req.params;
+    posts
+        .remove(id)
+        .then(response => {
+            if(response === 0) {
+                sendUserError(404, `The user with the ID ${id} does not exist.`, res);
+                return;
+            }
+            res.json({ success: `User with ID ${id} removed`});
+        })
+        .catch(error => {
+            sendUserError(500, `There was an error processing your request`, res);
+            return;
+        });
+});
+
+
+
+
 server.listen(port, () => console.log(`Server is running on port ${port}`));
