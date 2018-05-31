10e8cf3117c3a945082ffb0cba25cafa11786f63
Andrew Kolumbic <andrewkolumbic@Andrews-Mac-mini.local>
2018-05-31 12:22:33 -0700

    server up and running

--- server.js
+++ server.js
@@ -1,2 +1,17 @@
 const express = require('express');
 const cors = require('cors');
+const posts = require('./data/helpers/postDb.js');
+const users = require('./data/helpers/userDb.js');
+const tags = require('./data/helpers/tagDb.js');
+
+const port = 5555;
+const server = express();
+server.use(cors({ origin: "http://localhost:3000" }));
+
+server.get('/api/users');
+server.get('/api/posts');
+server.get('/api/tags');
+
+
+
+server.listen(port, () => console.log(`Server running on port ${port}`));
