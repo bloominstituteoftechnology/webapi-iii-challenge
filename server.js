ae12c6703cb379f6684e89e87d90f2ab393db820
Lavell Burton <taylormade800@gmail.com>
2018-05-31 14:44:49 -0700

    mvp done

--- server.js
+++ server.js
@@ -14,8 +14,6 @@ const sendUserError = (status, message, res) => {
     return;
 }
 
-
-
 server.get('/api/users', (req, res) => {
     users.get()
         .then(users => {
@@ -26,7 +24,77 @@ server.get('/api/users', (req, res) => {
         });
 });
 
+server.get('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+    users.get(id)
+        .then(response => {
+            res.json({ response });
+        })
+        .catch(error => {
+            res.json({ error });
+        });
+});
+
+server.get('/api/user/posts/:id', (req, res) => {
+    const { id } = req.params;
+    users.getUserPosts(id)
+        .then(users => {
+            res.json({ users });
+        })
+        .catch(error => {
+            res.json({ error });
+        });
+})
+
+server.get('/api/post/tags/:id', (req, res) => {
+    const { id } = req.params;
+    posts.getPostTags(id)
+        .then(posts => {
+            res.json({ posts });
+        })
+        .catch(error => {
+            res.json({ error });
+        });
+})
 
 
+// server.post('/api/users', (req, res) => {
+//     const { name } = req.body;
+//     users.insert(name)
+//         .then(user => {
+//             res.json( user );
+//         })
+//         .catch(error => {
+//             console.log(error);
+//         })
+// })
 
-server.listen(port, () => console.log(`Server running on port ${port}`));
+server.delete('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+    users.remove(id)
+        .then(response => {
+            if (response === 1) {
+                res.send('hello you have deleted');
+            } else {
+                res.send('sorry cant find that');
+            }
+        })
+        .catch(error => {
+            console.log(error);
+        })
+})
+
+// server.put('/api/users/:id', (req, res) => {
+//     const { id } = req.params;
+//     const { name } = req.body;
+//     users.update(id, name)
+//         .then(response => {
+//             res.json(response);
+//         })
+//         .catch(error => {
+//             console.log(error);
+//         })
+// })
+
+
+server.listen(port, () => console.log(`Server running on port, ${port}`));
