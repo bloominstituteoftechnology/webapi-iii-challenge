7854e5c63e3c36e1e8650c6d22da35b253511a31
Andrew Kolumbic <andrewkolumbic@Andrews-Mac-mini.local>
2018-05-31 13:28:31 -0700

    all gets done

--- server.js
+++ server.js
@@ -1,16 +1,55 @@
 const express = require('express');
 const cors = require('cors');
-const posts = require('./data/helpers/postDb.js');
+const db = require('./data/dbConfig.js');
 const users = require('./data/helpers/userDb.js');
+const posts = require('./data/helpers/postDb.js');
 const tags = require('./data/helpers/tagDb.js');
 
 const port = 5555;
 const server = express();
+server.use(express.json());
 server.use(cors({ origin: "http://localhost:3000" }));
 
-server.get('/api/users');
-server.get('/api/posts');
-server.get('/api/tags');
+server.get('/', (req, res) => {
+        res.json('Howdy!')
+});
+
+//Users
+server.get('/api/users/', (req, res) => {
+    users
+    .get()
+    .then(response => {
+        console.log(response)
+        res.json(response)
+    })
+    .catch(error => {
+        res.json(error)
+    })
+})
+
+//Posts
+server.get('/api/posts', (req, res) => {
+    posts
+    .get()
+    .then(response => {
+        res.json(response)
+    })
+    .catch(error => {
+        res.json(error)
+    })
+});
+
+//Tags
+server.get('/api/tags', (req, res) => {
+    tags
+    .get()
+    .then(response => {
+        res.json(response)
+    })
+    .catch(error => {
+        res.json(error)
+    })
+});
 
 
 
