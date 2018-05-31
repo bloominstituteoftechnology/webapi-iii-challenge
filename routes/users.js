a3763e22d11d90d397d2d16350628e4f9f02ed10
bbenefield89 <bsquared18@gmail.com>
2018-05-31 15:33:52 -0400

    feat(routes/users.js): remove existing user DELETE:/:id

--- routes/users.js
+++ routes/users.js
@@ -46,8 +46,21 @@ router.put('/:id', (req, res) => {
       if (!name) {
         return error(res, 400, 'Please provide a new name when attempting to update your name');
       }
-      error(res, 500, 'Could not process your request. If this issue persists, please contact the owner of the site')
+      error(res, 500, 'Could not process your request. If this issue persists, please contact the owner of the site');
     });
+});
+
+// remove
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  db.remove(id)
+    .then(data => {
+      if (!data) {
+        return error(res, 404, 'Sorry, could not find that user. Perhaps try another one?');
+      }
+      res.json(data);
+    })
+    .catch(err => error(res, 500, 'Could not process your request. If this issue persists, please contact the owner of the site'));
 })
 
 /*************************
