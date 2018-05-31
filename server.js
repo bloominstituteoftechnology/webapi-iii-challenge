b77f96be4fc80d2aef45674c5270820e90cd803e
MacBook <LonnieDavis@MacBookPros-MacBook-Pro.local>
2018-05-31 16:31:06 -0500

    latest changes

--- server.js
+++ server.js
@@ -9,9 +9,30 @@ server.use(express.json());
 
 
 //database helpers
-// const posts = require('./data/helpers/postDb.js')
-// const users = require('./data/helpers/userDb.js')
-// const tags = require('./data/helpers/tagDb.js')
-// server.get('/api/users')
-// server.get('/api/posts')
-// server.get('/api/tags')
+
+const posts = require('./data/helpers/postDb.js');
+const users = require('./data/helpers/userDb.js');
+const tags = require('./data/helpers/tagDb.js');
+
+server.get('/api/users');
+server.get('/api/posts');
+server.get('/api/tags');
+
+const sendUserError = (status, message, res) => {
+    res.status(status).json({ errorMessage: message });
+    //return;
+};
+
+server.get('/api/users', (req, res) => {
+    db
+    .find()
+    .then(Promise => {
+        res.json({ users });
+    })
+    .catch(error => {
+        sendUserError(500, 'The posts information could not be retrieved.', res);
+        return;
+    });
+});
+
+server.listen(port, () => console.log(`Server running on port ${port}`));
