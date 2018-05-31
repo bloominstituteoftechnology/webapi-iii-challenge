9cdd8d2d1318bcb31a3ec513c0e40f0c5ababd1c
Bradiowave <divebombwalker@gmail.com>
2018-05-31 12:11:55 -0700

    made basic get methods for users,posts,&tags

--- server.js
+++ server.js
@@ -10,8 +10,34 @@ const server = express();
 server.use(express.json());
 server.use(cors({ origin: 'http://localhost:3000' }));
 
-// server.get('/api/posts')
-// server.get('/api/users')
-// server.get('/api/tags')
+server.get('/api/users', (req, res) => {
+    users.get()
+        .then(users => {
+            res.json({ users })
+        })
+        .catch(err => {
+            res.json({ err })
+        })
+});
+
+server.get('/api/posts', (req, res) => {
+    posts.get()
+        .then(posts => {
+            res.json({ posts })
+        })
+        .catch(err => {
+            res.json({ err })
+        })
+});
+
+server.get('/api/tags', (req, res) => {
+    tags.get()
+        .then(tags => {
+            res.json({ tags })
+        })
+        .catch(err => {
+            res.json({ err })
+        })
+});
 
 server.listen(port, () => console.log(`Server running on port ${port}`));
