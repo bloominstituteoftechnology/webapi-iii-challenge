a9cb07f17c97ff4994669cdc6561791a667b0ec5
kelly <kelfro@fastmail.com>
2018-05-31 16:31:32 -0700

    routed the userlist in react

--- client/src/App.js
+++ client/src/App.js
@@ -1,52 +1,10 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import { Route } from 'react-router-dom';
 import './App.css';
-import { Table } from 'reactstrap';
-import axios from 'axios';
+import Userlist from './Userlist.js';
 
-class App extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			users: []
-		}
-	}
-componentDidMount() {
-	this.gatherUsers();
-}
-
-gatherUsers = () => {
-	axios.get('http://localhost:5555/api/users')
-		.then(response => {
-			console.log(response)
-			this.setState({ users: response.data.users });
-		})
-		.catch(error => console.log(error));
-}
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-  	</header>
-	<div>
-		{this.state.users.map(user => {
-			return (
-				<Table striped>
-					<tbody>
-						<tr>
-							<td>{user.name}</td>
-						</tr>
-					</tbody>
-				</Table>
-			);
-		})}
-	</div>
-      </div>
-    );
-  }
-}
+const App = () => (
+	<div><Route exact path='/' component={ Userlist }></Route></div>
+)
 
 export default App;
---
+++ client/src/Userlist.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import logo from './logo.svg';
+import './App.css';
+import { Table } from 'reactstrap';
+import axios from 'axios';
+
+class Userlist extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			users: []
+		}
+	}
+componentDidMount() {
+	this.gatherUsers();
+}
+
+gatherUsers = () => {
+	axios.get('http://localhost:5555/api/users')
+		.then(response => {
+			console.log(response)
+			this.setState({ users: response.data.users });
+		})
+		.catch(error => console.log(error));
+}
+
+  render() {
+    return (
+	<div>
+		{this.state.users.map(user => {
+			return (
+				<Table striped>
+					<tbody>
+						<tr>
+							<td>{user.name}</td>
+						</tr>
+					</tbody>
+				</Table>
+			);
+		})}
+	</div>
+    );
+  }
+}
+
+export default Userlist;
