45764112e70eb27e4bcd5d7475f92b438302ba50
kelly <kelfro@fastmail.com>
2018-05-31 16:03:54 -0700

    created react app to display list of users

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,49 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Table } from 'reactstrap';
+import axios from 'axios';
 
 class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			users: []
+		}
+	}
+componentDidMount() {
+	this.gatherUsers();
+}
+
+gatherUsers = () => {
+	axios.get('http://localhost:5555/api/users')
+		.then(response => {
+			console.log(response)
+			this.setState({ users: response.data.users });
+		})
+		.catch(error => console.log(error));
+}
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+  	</header>
+	<div>
+		{this.state.users.map(user => {
+			return (
+				<Table striped>
+					<tbody>
+						<tr>
+							<td>{user.name}</td>
+						</tr>
+					</tbody>
+				</Table>
+			);
+		})}
+	</div>
       </div>
     );
   }
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-//const cors = require('cors');
+const cors = require('cors');
 const posts = require('./data/helpers/postDb.js');
 const users = require('./data/helpers/userDb.js');
 const tags = require('./data/helpers/tagDb.js');
@@ -7,7 +7,7 @@ const tags = require('./data/helpers/tagDb.js');
 const port = 5555;
 const server = express();
 server.use(express.json());
-//server.use(cors({ origin: 'http://localhost:3000' }));
+server.use(cors({ origin: 'http://localhost:3000' }));
 
 server.get('/', (req, res) => {
 	res.send('Hello, Kelly');
