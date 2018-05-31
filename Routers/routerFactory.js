a597a54e828f8119e1ca418582cd6fb216fd7a83
jesuarva <jean.ariza@jesuarva.com>
2018-06-01 01:04:31 +0200

    ADD GET/:id to all 3 endpoints via the routerFactory function

--- Routers/routerFactory.js
+++ Routers/routerFactory.js
@@ -12,6 +12,22 @@ function routerFactory(router, db, errorStatus, errorMessage) {
         res.status(errorStatus).json(errorDb);
       });
   });
+
+  router.get("/:id", (req, res) => {
+    const {id} = req.params;
+    console.log(id);
+
+    db
+      .get(id)
+      .then(response => {
+        console.log("response", response);
+        res.status(200).json(response);
+      })
+      .catch(e => {
+        console.log("error", e);
+        res.status(errorStatus).json(errorDb);
+      });
+  });
 }
 
 module.exports = routerFactory;
