6ff0fce32cbc55ce2536d31cae992f9b83ef0bf8
Cassandra Lamendola <cass.lamendola@gmail.com>
2018-05-31 10:54:54 -0700

    intial setup and planning of server
    - add dependencies
    - plan of action for setting up server
    - replace yarn.lock for package.lock

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const cors = require('cors');
+
+// database helpers
+const posts = require('./data/helpers/postDb.js');
+//tags
+//users
+
+//server.get() for each one
+
+//server.listen
