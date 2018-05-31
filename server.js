09673749e42ad554c453601c08a0f0d3ff5ece62
jaklundun <govi.escudero@gmail.com>
2018-05-31 14:45:00 -0700

    Finished MPV minus POST endpoints.

--- server.js
+++ server.js
@@ -10,22 +10,85 @@ const server = express(); // variable server calls express
 // server.use(cors({origin: 'http://localhost:xxxx'}));
 server.use(express.json()); //extending middleware into our server
 
-// Returns an array of all the post objects contained in the database.
-// When the client makes a GET request to /api/posts:
-// If there's an error in retrieving the posts from the database:
-// cancel the request.
-// respond with HTTP status code 500.
-// return the following JSON object: { error: "The posts information could not be retrieved." }.
-
 server.get('/api/users', (req, res) => {
     users.get()
     .then(posts => {
         res.json(posts);
     })
 });
-// server.get('/api/users')
-// server.get('/api/posts')
-// server.get('/api/tags')```
+
+server.get('/api/users/:id', (req, res)=> {
+    const { id } = req.params;
+    users.get(id)
+        .then(response => {
+            res.json(response);
+        })
+        .catch(error=> {
+            console.log(error);
+        })
+});
+
+server.get('/api/users/post/:id', (req, res) => {
+    const { id } = req.params;
+    users.getUserPosts(id)
+        .then(users => {
+            res.json({users});
+        })
+        .catch(error => {
+            res.json({error});
+        });
+})
+
+server.get('/api/post/tags/:id', (req, res) => {
+    const { id } = req.params;
+    posts.getPostTags(id)
+        .then(posts => {
+            res.json({ posts })
+        })
+        .catch(error => {
+            res.json({ error })
+        });
+});
+
+
+// server.post('/api/users', (req, res) => {
+//     const { name } = req.body;
+//     users.insert(name)
+//     .then(user => {
+//         res.json( user )
+//     })
+//     .catch(error => {
+//         res.json( {errorMessage: error} )
+//     })
+// })
+
+server.delete('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+    users.remove(id)
+        .then(response => {
+            if (response === 1) {
+                res.json("Delete successful");
+            } else {
+                res.json("No user with that Id exists. Nothing to delete.");
+            }
+        })
+        .catch(error => {
+            console.log(error);
+        })
+})
+
+// server.put('/api/users/:id', (req, res) => {
+//     const { id } = req.params;
+//     const { name } = req.body;
+//     users.update(id, name)
+//         .then(response => {
+//             res.json(response)
+//         })
+//         .catch(error => {
+//             console.log(error);
+//         })
+// });
+
 
 //Set up
     // server object: we have inialized it with our express server
