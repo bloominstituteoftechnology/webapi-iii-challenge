1e9df7e52f6499c97cc3d97e07d68792dd622589
Iqra Javed <iqrajaved@Iqras-MacBook-Air.local>
2018-05-31 13:45:04 -0500

    setup imports & variables & enabled server

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
-// express
-// cors
-
+const express = require('express');
+const cors = requite('cors');
 // database helpers
-// const posts = require('./data/helpers/postDb.js')
-// const users = require('./data/helpers/userDb.js')
-// const tags = require('./data/helpers/userDb.js')
+const posts = require('./data/helpers/postDb.js');
+const users = require('./data/helpers/userDb.js');
+const tags = require('./data/helpers/tagDb.js');
+
+const port = 5555;
+const server = express();
+server.use(express.json());
+server.use(cors());
+
+
 // server.get('/api/users')
 // server.get('/api/posts')
 // server.get('/api/tags')
