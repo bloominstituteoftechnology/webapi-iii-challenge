d7d3cf242f8de4e1acca105427d80d83875cccfd
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-06-01 04:42:22 +0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+// express
+// cors
+
+// database helpers
+// const posts = require('./data/helpers/postDb.js')
+// const users = require('./data/helpers/userDb.js')
+// const tags = require('./data/helpers/tagDb.js')
+// server.get('/api/users')
+// server.get('/api/posts')
+// server.get('/api/tags')
+
