5e87660ae1a1a591fe8d639fdd87681007dbf3e3
Zach Campbell <beazlebubbles@gmail.com>
2018-05-31 14:09:07 -0600

    Implemented CRUD for users (Imported previous project code and adapted)

--- server.js
+++ server.js
@@ -9,6 +9,103 @@ const server = express();
 server.use(express.json());
 server.use(cors({ origin: 'http://localhost:3000' }));
 
+server.post('/api/users', (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    res.status(400).json({ errorMessage: "Please provide a name for the user." });
+  }
+  else {
+    users
+      .insert({ name })
+      .then(response => {
+        users.get(response.id)
+          .then(user => {
+            res.status(201).json({ user });
+          });
+      })
+      .catch(error => {
+        res.status(500).json({ errorMessage: "There was an error while saving the post to the database" });
+      });
+  }
+});
 
+
+server.get('/api/users', (req, res) => {
+  users.get().then(users => {
+    res.json({ users });
+  })
+  .catch(error => {
+    res.status(500).json({ errorMessage: "The user could not be retrieved." });
+  });
+});
+
+server.get('/api/users/:id', (req, res) => {
+  const id = req.params.id;
+  users
+    .get(id)
+    .then(users => {
+      console.log(users);
+      if (users) {
+        res.json({ users });
+      }
+      else {
+        res.status(404).json({ errorMessage: "The user with the specified ID does not exist." });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ errorMessage: "The user could not be retrieved." });
+    })
+});
+
+
+server.put('/api/users/:id', (req, res) => {
+  const { name } = req.body;
+  const id = req.params.id;
+  if (!name) {
+    res.status(400).json({ errorMessage: "Please provide a name for the user." });
+  }
+  else {
+    users
+      .update(id, { name })
+      .then(success => {
+        if (success) {
+          users.get(id)
+            .then(user => {
+              res.json({ user });
+            });
+        }
+        else {
+          res.status(404).json({ errorMessage: "The user with the specified ID does not exist." });
+        }
+      })
+      .catch(error => {
+        res.status(500).json({ errorMessage: "The user information could not be retrieved." });
+      })
+  }
+});
+
+server.delete('/api/users/:id', (req, res) => {
+  const id = req.params.id;
+
+  let deletedUser;
+
+  users.get(id)
+    .then((user) => deletedUser = user);
+
+  users
+    .remove(id)
+    .then(success => {
+      console.log(success);
+      if (success) {
+        res.json({ deletedUser });
+      }
+      else {
+        res.status(404).json({ errorMessage: "The user with the specified ID does not exist." });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ errorMessage: "The user could not be removed" });
+    })
+});
 
 server.listen(port, () => console.log(`Server running on port ${port}`));
