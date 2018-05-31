a7da0da1fa48925f6fb9964e711a5c4b2010f71a
Iqra Javed <iqrajaved@Iqras-MacBook-Air.local>
2018-05-31 12:53:37 -0500

    initialized project installed dependencies created server.js

---
+++ server.js
@@ -0,0 +1,10 @@
+// express
+// cors
+
+// database helpers
+// const posts = require('./data/helpers/postDb.js')
+// const users = require('./data/helpers/userDb.js')
+// const tags = require('./data/helpers/userDb.js')
+// server.get('/api/users')
+// server.get('/api/posts')
+// server.get('/api/tags')
