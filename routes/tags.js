46045b9ad3c1ed995fcc7597e9f39234c2f25025
bbenefield89 <bsquared18@gmail.com>
2018-05-31 17:13:03 -0400

    feat(routes/tags): update existing tag PUT:/:id

--- routes/tags.js
+++ routes/tags.js
@@ -25,7 +25,7 @@ router.post('/', (req, res) => {
       res.json(data);
     })
     .catch(err => error(res, 500, 'Could not process your request. If this issue persists, please contact the owner of the site'));
-})
+});
 
 /*************************
 ** ROUTE /:id **
@@ -41,6 +41,25 @@ router.get('/:id', (req, res) => {
       res.json(data);
     })
     .catch(err => error(res, 500, 'Could not process your request. If this issue persists, please contact the owner of the site'));
+});
+
+// update
+router.put('/:id', (req, res) => {
+  const { id } = req.params;
+  const { tag } = req.body;
+  db.update(id, { tag })
+    .then(data => {
+      if (!data) {
+        return error(res, 404, 'This tag no longer exists... or maybe it never did?');
+      }
+      res.json(data);
+    })
+    .catch(err => {
+      if (!tag) {
+        return error(res, 400, 'Please provide a new name before attempting to update a tag');
+      }
+      error(res, 500, 'Could not process your request. If this issue persists, please contact the owner of the site')
+    });
 });
 
 module.exports = router;
