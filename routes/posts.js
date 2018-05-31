a31c832ef22452d17f642f6785417edadf95fff5
bbenefield89 <bsquared18@gmail.com>
2018-05-31 16:24:32 -0400

    feat(routes/posts): get a posts tags GET:/:id/tags

--- routes/posts.js
+++ routes/posts.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const db = require('../data/helpers/postDb');
 const error = require('./helpers/error');
 
-// GET http://localhost:5000/api/posts/1
+// GET http://localhost:5000/api/posts/100/tags
 
 /*************************
 ** ROUTE /:id **
@@ -12,8 +12,24 @@ const error = require('./helpers/error');
 // get
 router.get('/:id', (req, res) => {
   const { id } = req.params;
-  db.get(id)
+  db.get()
     .then(data => res.json(data))
+    .catch(err => error(res, 500, 'Could not process your request. If this issue persists, please contact the owner of the site'));
+});
+
+/*************************
+** ROUTE /:id/tags **
+*************************/
+// get
+router.get('/:id/tags', (req, res) => {
+  const { id } = req.params;
+  db.getPostTags(id)
+    .then(data => {
+      if (data.length === 0) {
+        return error(res, 404, 'Either this post doesn\'t exist, or it has yet to be tagged by the author');
+      }
+      res.json(data)
+    })
     .catch(err => error(res, 500, 'Could not process your request. If this issue persists, please contact the owner of the site'));
 })
 
