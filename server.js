6ecae3863a3a266d42db052d12ca3aec245ad1d2
Steve <shalomlandsman@gmail.com>
2018-05-31 21:14:16 -0400

    mvp

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ const sendUserError = (status, message, res) => {
     return;
 };
 
+//get all users or single user with id query:
 server.get('/api/users', (req, res) => {
     if (!req.query) {
         users
@@ -33,13 +34,14 @@ server.get('/api/users', (req, res) => {
             .then(user => {
                 res.json(user);
             })
-            .catch( error => {
+            .catch(error => {
                 sendUserError(500, "The users information could not be retrieved.", res);
                 return;
             });
     }
 });
 
+//list of posts for user:
 server.get('/api/users/posts', (req, res) => {
     const { id } = req.query;
     users
@@ -47,12 +49,13 @@ server.get('/api/users/posts', (req, res) => {
         .then(posts => {
             res.json(posts);
         })
-        .catch( error => {
+        .catch(error => {
             sendUserError(500, "The users information could not be retrieved.", res);
             return;
         });
 });
 
+//add user:
 server.post('/api/users', (req, res) => {
     const { name } = req.body;
     if (!name) {
@@ -69,11 +72,10 @@ server.post('/api/users', (req, res) => {
         });
 });
 
+//edit user name:
 server.put('/api/users/:id', (req, res) => {
     const { name } = req.body;
     const id = req.params.id;
-    console.log(id);
-    console.log(name);
     users
         .update(id, { name })
         .then(user => {
@@ -88,6 +90,7 @@ server.put('/api/users/:id', (req, res) => {
         });
 });
 
+//delete user:
 server.delete('/api/users/:id', (req, res) => {
     const id = req.params.id;
     users
@@ -103,6 +106,158 @@ server.delete('/api/users/:id', (req, res) => {
             sendUserError(500, "The user could not be updated.", res);
         });
 });
+
+//get posts or post (using query):
+server.get('/api/posts', (req, res) => {
+    const { id } = req.query;
+    posts
+        .get(id)
+        .then(user => {
+            res.json(user);
+        })
+        .catch(error => {
+            sendUserError(500, "The users posts could not be retrieved.", res);
+            return;
+        });
+});
+
+//list of tags for post:
+server.get('/api/posts/:id', (req, res) => {
+    const id = req.params.id;
+    posts
+        .getPostTags(id)
+        .then(tags => {
+            if (tags.length === 0) {
+                sendUserError(404, 'The post with the specified ID does not exist.', res);
+                return;
+            }
+            res.json(tags);
+        })
+        .catch(error => {
+            sendUserError(500, "The users posts could not be retrieved.", res);
+        });
+});
+
+//add new post:
+server.post('/api/posts', (req, res) => {
+    const { text, userId } = req.body;
+    if (!text || !userId) {
+        sendUserError(400, "Please provide text.", res);
+        return;
+    }
+    posts
+        .insert({ text, userId })
+        .then(post => {
+            res.json(post);
+        })
+        .catch(error => {
+            sendUserError(500, "The post could not be created.", res);
+            return;
+        });
+});
+
+//edit post:
+server.put('/api/posts/:id', (req, res) => {
+    const { text } = req.body;
+    const id = req.params.id;
+    posts
+        .update(id, { text })
+        .then(post => {
+            if (post.length === 0) {
+                sendUserError(404, 'The post with the specified ID does not exist.', res);
+                return;
+            }
+            res.json(post);
+        })
+        .catch(error => {
+            sendUserError(500, "The post could not be updated.", res);
+        });
+});
+
+//delete post:
+server.delete('/api/posts/:id', (req, res) => {
+    const id = req.params.id;
+    posts
+        .remove(id)
+        .then(post => {
+            if (post.length === 0) {
+                sendUserError(404, 'The post with the specified ID does not exist.', res);
+                return;
+            }
+            res.json(post);
+        })
+        .catch(error => {
+            sendUserError(500, "The post could not be removed.", res);
+        });
+});
+
+//get all tags or tag by id using query
+server.get('/api/tags', (req, res) => {
+    const { id } = req.query;
+    tags
+        .get(id)
+        .then(posts => {
+            res.json(posts);
+        })
+        .catch(error => {
+            sendUserError(500, "The requested tags could not be retrieved.", res);
+            return;
+        });
+});
+
+//add tag
+server.post('/api/tags', (req, res) => {
+    const { tag } = req.body;
+    if (!tag) {
+        sendUserError(400, "Please provide tag.", res);
+        return;
+    }
+    tags
+        .insert({ tag })
+        .then(result => {
+            res.json(result);
+        })
+        .catch(error => {
+            sendUserError(500, "The tag could not be created.", res);
+            return;
+        });
+});
+
+//update tag:
+server.put('/api/tags/:id', (req, res) => {
+    const { tag } = req.body;
+    const id = req.params.id;
+    tags
+        .update(id, { tag })
+        .then(result => {
+            if (result.length === 0) {
+                sendUserError(404, 'The tag with the specified ID does not exist.', res);
+                return;
+            }
+            res.json(result);
+        })
+        .catch(error => {
+            sendUserError(500, "The tag could not be updated.", res);
+        });
+});
+
+//delete tag:
+server.delete('/api/tags/:id', (req, res) => {
+    const id = req.params.id;
+    tags
+        .remove(id)
+        .then(result => {
+            if (result.length === 0) {
+                sendUserError(404, 'The tag with the specified ID does not exist.', res);
+                return;
+            }
+            res.json(result);
+        })
+        .catch(error => {
+            sendUserError(500, "The tag could not be removed.", res);
+        });
+});
+
 /*server.get('/api/users', (req,res) =>{
     const { id } = req.query;
     users
