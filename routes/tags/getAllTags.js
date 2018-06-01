576d8e2de2378df26427eb807e1ec2036f4aaffd
Danny Vail <dannyv@dannys-mbp.attlocal.net>
2018-06-01 03:32:11 -0400

    Created GET request to pull all tags

--- routes/tags/getAllTags.js
+++ routes/tags/getAllTags.js
@@ -1,5 +1,11 @@
 const tags = require('../../data/helpers/tagDb');
 
 module.exports = (req, res) => {
-
+  tags.get()
+    .then(tags => {
+      res.status(200).json(tags);
+    })
+    .catch(error => {
+      res.status(500).json({ error: 'The list of tags could not be retrieved.' });
+    });
 };
