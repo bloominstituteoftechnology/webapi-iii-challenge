25a90b700d2b65e68fb08fbbaff803f48230a931
Danny Vail <dannyv@dannys-mbp.attlocal.net>
2018-06-01 03:33:55 -0400

    Created GET request for a single tag

--- routes/tags/getSingleTag.js
+++ routes/tags/getSingleTag.js
@@ -1,5 +1,17 @@
 const tags = require('../../data/helpers/tagDb');
 
 module.exports = (req, res) => {
+  const { id } = req.params;
 
+  tags.get(id)
+    .then(tag => {
+      if(tag){
+        res.status(200).json(tag);
+      } else {
+        res.status(404).json({ error: 'The tag with the specified ID does not exist.' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ error: 'The tag could not be retrieved.' });
+    });
 };
