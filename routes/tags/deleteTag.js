45207a5924fb42583c120dbb4ba90cc94ad9d25f
Danny Vail <dannyv@dannys-mbp.attlocal.net>
2018-06-01 03:57:01 -0400

    Created DELETE request to remove tag

--- routes/tags/deleteTag.js
+++ routes/tags/deleteTag.js
@@ -1,5 +1,18 @@
 const tags = require('../../data/helpers/tagDb');
+const getUpdatedTags = require('./getAllTags');
 
 module.exports = (req, res) => {
+  const { id } = req.params;
 
+  tags.remove(id)
+    .then(recordsDeleted => {
+      if(recordsDeleted > 0){
+        getUpdatedTags(req, res);
+      } else {
+        res.status(404).json({ error: 'The tag with the specified ID does not exist.' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ error: 'The tag could not be removed.' });
+    });
 };
