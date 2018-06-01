f5027d2cc11e0e3cbf256d5c74ea1cb246e136be
Danny Vail <dannyv@dannys-mbp.attlocal.net>
2018-06-01 03:07:40 -0400

    Created DELETE request to remove a post

--- routes/posts/deletePost.js
+++ routes/posts/deletePost.js
@@ -1,5 +1,18 @@
 const posts = require('../../data/helpers/postDb');
+const getUpdatedPosts = require('./getAllPosts');
 
-module.exports = () => {
+module.exports = (req, res) => {
+  const { id } = req.params;
 
+  posts.remove(id)
+    .then(recordsDeleted => {
+      if (recordsDeleted > 0) {
+        getUpdatedPosts(req, res);
+      } else {
+        res.status(404).json({ error: 'The post with the specified ID does not exist.' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ error: 'The post could not be removed.' });
+    });
 };
