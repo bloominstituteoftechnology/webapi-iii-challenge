25de61d5be29b7228b7cc1a3632c1248989c90d4
Danny Vail <dannyv@dannys-mbp.attlocal.net>
2018-06-01 03:24:45 -0400

    Created GET request to pull a single post - Reset database

--- routes/posts/getSinglePost.js
+++ routes/posts/getSinglePost.js
@@ -1,5 +1,16 @@
 const posts = require('../../data/helpers/postDb');
 
-module.exports = () => {
-
+module.exports = (req, res) => {
+  const { id } = req.params;
+  posts.get(id)
+    .then(post => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res.status(404).json({ error: 'The post with the specified ID does not exist.' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ error: 'The post could not be retrieved.' });
+    });
 };
