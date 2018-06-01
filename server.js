88382ed8827320f64bc133c87d9db67e5008cd55
Danny Vail <dannyv@dannys-mbp.attlocal.net>
2018-06-01 14:19:32 -0400

    Enable Cors

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const server = require('express')();
 const port = 5000;
 const routes = require('./routes');
+const cors = require('cors');
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  methods: ['POST', 'GET', 'PUT', 'DELETE']
+};
 
+server.use(cors(corsOptions));
 server.use('/', routes);
 
 server.listen(port, () => {
