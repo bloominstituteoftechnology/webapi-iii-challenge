fc7a08b1b11f40ba7fd3b3b4f9fa59b219d4c1f9
sagardesai90 <saagardesai@gmail.com>
2018-06-01 12:52:07 -0500

    got the users .get to work

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const db = require('./data/helpers/postDb.js');
-const db1 = require('./data/helpers/tagDb.js');
-const db2 = require('./data/helpers/userDb.js');
+const posts = require('./data/helpers/postDb.js');
+const tags = require('./data/helpers/tagDb.js');
+const users = require('./data/helpers/userDb.js');
 const cors = require('cors');
 const port = 5555;
 const server = express();
@@ -22,18 +22,56 @@ Server Requests Below
  * 
  *********/
 
+/***********
+ USERS 
+ **********/
  //GET
-server.get(`/api/users`, (_, res) => {
-    db2
+server.get(`/api/users`, (req, res) => {
+    users
     .get()
-    .then(res => console.log(res))
-    .catch(error => console.log(error))
+    .then(users => {
+        res.json({ users })
+    })
+    .catch(error => {
+        res.status(500);
+        res.json({error: "The users information could not be retrieved."})
+    })
 });
+
+server.get('/api/users/:id', (req, res) => {
+    const {id} = req.params;
+    if(req.params.id === undefined){
+        res.status(404)
+        res.json({ message: "The user with the specified ID does not exist." })
+    } else {
+        console.log(id);
+        users
+        .get(id)
+        .then(message => {
+            res.json({message})
+        })
+        .catch(error => {
+            res.status(500)
+            res.json({ error: "The user information could not be retrieved." })
+        })
+    }
+})
 
 
 //POST
-//  server.post(`api/${users}/:id`, (req, res) => {
-//      const
-//  });
+ server.post(`/api/users`, (req, res) => {
+    const { name } = req.body
+    if (name) {
+
+        users
+        .insert({name})
+        .then(res => {
+            res.status(201).json({name})
+        })
+        .catch(error => {
+            res.status(500).json({message: "Please provide title and contents for the post."});
+        })
+    }
+ });
 
 server.listen(port, () => console.log(`Server is running on port ${port}`));
