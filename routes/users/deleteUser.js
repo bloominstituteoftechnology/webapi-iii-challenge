67125251e1ea2de6d2705153f563cb9c9899a3e9
Danny Vail <dannyv@dannys-mbp.attlocal.net>
2018-05-31 22:27:53 -0400

    Created DELETE request for removing a user

--- routes/users/deleteUser.js
+++ routes/users/deleteUser.js
@@ -1,5 +1,18 @@
 const users = require('../../data/helpers/userDb');
+const getUpdatedUsers = require('./getAllUsers');
 
 module.exports = (req, res) => {
+  const { id } = req.params;
 
+  users.remove(id)
+    .then(recordsDeleted => {
+      if(recordsDeleted > 0){
+        getUpdatedUsers(req, res);
+      } else {
+        res.status(500).json({ error: 'The user with the specified ID does not exist.' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ error: 'The user could not be removed.' });
+    });
 };
