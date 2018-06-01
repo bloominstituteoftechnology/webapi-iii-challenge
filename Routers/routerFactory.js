f61f7b7a38e50d0a372e987fa3efbdf5cc5d48ec
jesuarva <jean.ariza@jesuarva.com>
2018-06-01 02:40:50 +0200

    ADD POST to all 3 endpoints via the routerFactory function

--- Routers/routerFactory.js
+++ Routers/routerFactory.js
@@ -42,6 +42,59 @@ function routerFactory(router, db, errorStatus, errorMessage) {
         console.log("error", e);
       });
   });
+
+  router.post("/", areArgumentsValid, insertDb);
+
+  function insertDb(req, res) {
+    const { ...obj } = req.obj;
+    const path = req.baseUrl;
+    console.log(obj);
+    db
+      .insert(obj)
+      .then(response => {
+        console.log("response", response);
+        res.status(200).json(`New ${path.substring(1)} added!`);
+      })
+      .catch(e => {
+        console.log("error", e);
+      });
+  }
+
+  function areArgumentsValid(req, res, next) {
+    console.log(req.baseUrl);
+    const { name, userId, text, tag } = req.body;
+    console.log(name, userId, text, tag);
+
+    switch (req.baseUrl) {
+      case "/users":
+        // do something
+        if (name) {
+          req.obj = { name };
+          next();
+        } else {
+          res.status(400).json("Please provide a name");
+        }
+        break;
+      case "/posts":
+        // do something
+        if (userId && text) {
+          req.obj = { userId, text };
+          next();
+        } else {
+          res.status(400).json("Please provide a userId and text");
+        }
+        break;
+      case "/tags":
+        // do something
+        if (tag) {
+          req.obj = { tag };
+          next();
+        } else {
+          res.status(400).json("Please provide a tag");
+        }
+        break;
+    }
+  }
 }
 
 module.exports = routerFactory;
