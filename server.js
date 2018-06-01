a5e3ae5b1f1f366d1666ffc2ea9077983df02070
AmyShackles <amyshackles@gmail.com>
2018-05-31 21:46:36 -0700

    Realized too late that delete was more complicated
    - Have now successfully configured it so that when you delete a user, it also removes all posts by that user.
    - Unfortunately, I deleted all of the users that had tags before I realized that not many of the users had tags, so I can't confirm or deny that my delete posts also deletes all tags from the posts.
    - Can confirm that tag delete does work.

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ server.get('/api/users/:id/posts', (req, res) => {
     userDB
         .getUserPosts(id)
         .then(posts => {
-            if (posts) {
+            if (posts.length > 0) {
                 res.json({ posts })
             } else {
                 sendUserError(404, "The specified user does not exist", res)
@@ -117,10 +117,10 @@ server.get('/api/posts/:id/tags', (req, res) => {
     postDB
         .getPostTags(id)
         .then(tag => {
-            if (tag) {
+            if (tag.length > 0) {
                 res.json({ tag })
             } else {
-                sendUserError(404, "The specified ID does not exist", res)
+                sendUserError(404, "This tag does not exist", res)
             }
         })
         .catch(error => {
@@ -174,6 +174,135 @@ server.post('/api/tags', (req, res) => {
             sendUserError(500, "There was a error while saving the tag.", res)
         })}
 })
+server.put('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+    const { name } = req.body;
+    userDB
+        .get(id)
+        .then(user => {
+        if (!user) {
+            sendUserError(404, "The user with the specified ID does not exist", res)
+        } else {
+    userDB
+        .update(id, { name })
+        .then(user => {
+            if (!name) {
+                sendUserError(400, "Please provide a name for the user.", res)
+            } else {
+                userDB
+                    .get(id)
+                    .then(user => {
+                        res.json({ user })
+                    });
+        }})
+        .catch(error => {
+            sendUserError(500, "The user information could not be modified.", res)
+        })
+    }})
+})
+server.put('/api/posts/:id', (req, res) => {
+    const { id } = req.params
+    const { userID, text } = req.body;
+    postDB
+        .update(id, { userID, text})
+        .then(post => {
+            if (!userID || !text) {
+                sendUserError(400, "Please provide a userID and text.", res)
+            } else if (post === 0) {
+                sendUserError(404, "The post with the specified ID could not be found", res)
+            } else {
+                postDB
+                    .get(id)
+                    .then(post => {
+                        res.json({ post })
+                    });
+            }})
+            .catch(error => {
+                sendUserError(500, "The post information could not be modified.", res)
+            })
+})
+server.put('/api/tags/:id', (req, res) => {
+    const { id } = req.params;
+    const { tag } = req.body;
+    tagDB 
+        .update(id, { tag })
+        .then(tags => {
+            if (!tag) {
+                sendUserError(400, "Please provide a tag.", res)
+            } else if (!tags) {
+                sendUserError(404, "The tag with the specified ID could not be found.", res)
+            } else {
+                tagDB
+                    .get(id)
+                    .then(tags => {
+                        res.json({ tags })
+                    });
+            }})
+            .catch(error => {
+                sendUserError(500, "The tag information could not be modified.", res)
+            })
 
+})
+server.delete('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+    let deletedUser;
+    let deletedUserPosts;
+    userDB
+        .get(id)
+        .then((user) => deletedUser = user)
+    userDB
+        .getUserPosts(id)
+        .then((postsByUser) => deletedUserPosts = postsByUser)
+        .catch(error => {
+            sendUserError(500, "The tags of the post could not be removed.")
+        })   
+    userDB
+        .remove(id, deletedUserPosts)
+        .then(user => {
+            if (user === 0) {
+                sendUserError(404, "The user with the specified ID does not exist.", res)
+            } else {
+                res.json({ deletedUser });
+            }})
+            .catch(error => {
+                sendUserError(500, "The user could not be removed", res)
+            })
+});
+server.delete('/api/posts/:id', (req, res) => {
+    const { id } = req.params;
+    let deletedPost;
+    let deletedPostTags;
+    postDB
+        .get(id)
+        .then((post) => deletedPost = post)
+    postDB
+        .getPostTags(id)
+        .then((postTag) => deletedPostTags = postTag)
+    postDB
+        .remove(id, deletedPostTags)
+        .then(postRemoved => {
+            if (postRemoved === 0) {
+                return sendUserError(404, "No post was found with that specific ID.", res)
+            } else {
+                res.json({ success: 'Post removed' })
+            }})
+            .catch(error => {
+                return sendUserError(500, "There was an error deleting the post from the database.", res)
+            })
+});
+server.delete('/api/tags/:id', (req, res) => {
+    const { id } = req.params;
+    tagDB
+        .remove(id)
+        .then(removedTag => {
+            if (removedTag === 0) {
+                return sendUserError(404, "No tag was found with that specific ID.", res)
+            } else {
+                res.json({ success: 'Tag removed' })
+        }})
+        .catch(error => {
+            return sendUserError(500, "There was an error deleting the tag from the database.", res)
+        })
+})
 
 server.listen(port, () => console.log(`Server running on port ${port}`));
