9570c4e5334d814d7b252ca3341a93e7f30e14e0
Iqra Javed <iqrajaved@Iqras-MacBook-Air.local>
2018-06-01 00:26:03 -0500

    implemented GET & POST in usersRoutes.js

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const cors = require('cors');
-
-
 const port = 5555;
 const server = express();
 
+// middleware
 server.use(express.json());
 server.use(cors());
 
+// sub-applications
 const usersRoutes = require('./users/usersRoutes');
-const postsRoutes = require('./posts/postsRoutes');
-const tagsRoutes = require('./tags/tagsRoutes');
+// const postsRoutes = require('./posts/postsRoutes');
+// const tagsRoutes = require('./tags/tagsRoutes');
 
-server.listen(port, () => console.log(`Magic Happening on port${port}`));
+// route handlers
+server.use('/users', usersRoutes);
+// server.use('/api/tags', tagsRoutes);
+// server.use('/api/posts', postsRoutes);
+
+
+
+server.listen(port, () => console.log(`Magic Happening on port ${port}`));
--- users/usersRoutes.js
+++ users/usersRoutes.js
@@ -1,12 +1,11 @@
-const users = require('../data/helpers/userDb.js');
 const express = require('express');
-const server = express();
+const usersDB = require('../data/helpers/userDb.js');
+const router = express.Router();
 
-server.use(express.json());
+// GET REQUEST 
+router.get('/', (req, res) => {
 
-server.get('/api/users', (req, res) => {
-
-    users.get()
+    usersDB.get()
     .then(users => {
         res.json(users)
     })
@@ -14,3 +13,31 @@ server.get('/api/users', (req, res) => {
         res.status(500).json({errorMessage: 'There was an error while retrieving the list of users.'})
     })
 })
+
+
+// POST REQUEST 
+router.post('/', (req, res) => {
+
+    const { name } = req.body;
+    if (name.length < 1 || name.length > 128) {
+        res.status(400).json({errorMessage: 'error'})
+        return;
+    }
+
+    usersDB
+        .insert({ name })
+        .then(response => {
+            res.status(201).json({ response });
+            return;
+    })
+        .catch(error => {
+        res.status(500).json({errorMessage: 'There was an error saving the user to the database'})
+        return;
+        });
+});
+
+
+
+
+
+module.exports = router;
