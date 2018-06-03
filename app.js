8321007469961241494644fefbaedf1c00ab4ba2
bbenefield89 <bsquared18@gmail.com>
2018-06-03 11:43:09 -0400

    feat(app.js): add env port variable

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 
 const app = express();
 const api = require('./routes');
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
