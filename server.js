70542caea6b26e3fc969d179663a205fb0e5fc49
CMVNK <Kopecky12112@gmail.com>
2018-06-06 14:56:54 -0700

    corrected a typo in 02-users.js
    updated checklist table in readme
    
    added error handling to server on what I have so far

--- server.js
+++ server.js
@@ -18,7 +18,11 @@ server.get('/api/users', (req, res)=>{
     userDb
         .get()
         .then(users=>{
+            if(users.length===0){
+                sendUserError(404, "Users could not be found", res);
+            } else{
             res.json({ users });
+            }
         })
         .catch(err =>{
             sendUserError(500, "There was an error in retrieving users information");
@@ -29,7 +33,15 @@ server.get('/api/posts', (req, res) =>{
     postDb
         .get()
         .then(posts=>{
+            if(posts.length===0){
+                sendUserError(404, "Posts could not be found", res);
+            } else{
             res.json({ posts })
+            }
+        })
+        .catch(err =>{
+            sendUserError(500, "Post information could not be retrieved", res)
+
         })
 })
 
@@ -37,8 +49,45 @@ server.get('/api/tags', (req, res) =>{
     tagDb
         .get()
         .then(tags =>{
+            if(tags.length===0){
+                sendUserError(404, "Posts could not be found", res);
+            } else{
             res.json({tags})
+            }
         })
+        .catch(err =>{
+            sendUserError(500, "Tag information could not be retrieved", res)
+        })
+})
+
+server.post('/api/users', (req, res) =>{
+    const { name } = req.body;
+    if(!name){
+        sendUserError(400, "Must include name", res)
+    }
+    userDb
+        .insert(name)
+        .then(res =>{
+            res.status(201).json(res);
+        })
+        .catch(err =>{
+            sendUserError(500, "User could not be saved", res);
+        })
+})
+
+server.post('/api/posts', (req, res) =>{
+    const { text } = req.body;
+        if(!text){
+            sendUserError(400, "Must include text", res);
+        }
+    postDb
+        .insert(text) 
+        .then(res => {
+            res.status(201).json(res);
+        })
+        .catch(err =>{
+            sendUserError(500, "Post could not be saved", res);
+        });
 })
 
 server.listen(port, () =>{ console.log(`Server is listening on ${port}`)});
