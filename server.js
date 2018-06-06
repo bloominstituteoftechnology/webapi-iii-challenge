9c2db204f3f3b3125f141ba53ec776343028a837
JSNavey <jsnavey@gmail.com>
2018-06-06 19:04:27 -0400

    implemented get by id

--- server.js
+++ server.js
@@ -12,13 +12,13 @@ const server = express();
 server.use(express.json());
 server.use(cors({ origin: 'http://localhost:3000'}));
 
-//--- all end point starts here ---
+//----------- all end point starts here -----------//
 
 server.get('/', (reg, res) => {
-    res.send('Hello from Server from Port 5000 (^_^)');
+    res.send('Hello from Server Port 5000 (^_^)');
 })
 
-//--- users end point ---
+//----------- users end point -----------//
 
 server.get('/api/users', (req, res) => {
     users
@@ -31,7 +31,19 @@ server.get('/api/users', (req, res) => {
         })
 })
 
-//--- posts end point ---
+server.get('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+    users
+        .get(id)
+        .then(users => {
+            res.json({ users })
+        })
+        .catch(error => {
+            console.error(error);
+        })
+})
+
+//----------- posts end point -----------//
 
 server.get('/api/posts', (req, res) => {
     posts
@@ -44,11 +56,35 @@ server.get('/api/posts', (req, res) => {
         })
 })
 
-//---tags end point ---
+server.get('/api/posts/:id', (req, res) => {
+    const { id } = req.params;
+    posts
+        .get(id)
+        .then(posts => {
+            res.json({ posts })
+        })
+        .catch(error => {
+            console.error(error);
+        })
+})
+
+//----------- tags end point -----------//
 
 server.get('/api/tags', (req, res) => {
     tags
         .get()
+        .then(tags => {
+            res.json({ tags })
+        })
+        .catch(error => {
+            console.error(error);
+        })
+})
+
+server.get('/api/tags/:id', (req, res) => {
+    const { id } = req.params;
+    tags
+        .get(id)
         .then(tags => {
             res.json({ tags })
         })
