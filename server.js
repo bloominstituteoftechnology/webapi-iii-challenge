de15d1eef2f3d31f15fcf696ed33f4883d206106
JSNavey <jsnavey@gmail.com>
2018-06-06 18:40:33 -0400

    draft the end point with get() to fetch the data from helper
    able to see the data of each file in postman

--- server.js
+++ server.js
@@ -1,13 +1,61 @@
 const express = require('express');
 const cors = require('cors');
 
-//need db here
+//const db = require('./data/db.js) --> if there's only one db.js in data folder
+//but it's becoz we have helper folder where keep the data so we like below.
+const users = require('./data/helpers/userDb.js');
+const posts = require('./data/helpers/postDb.js');
+const tags = require('./data/helpers/tagDb.js');
 
 const port = 5000;
 const server = express();
 server.use(express.json());
 server.use(cors({ origin: 'http://localhost:3000'}));
 
-//end point here
+//--- all end point starts here ---
+
+server.get('/', (reg, res) => {
+    res.send('Hello from Server from Port 5000 (^_^)');
+})
+
+//--- users end point ---
+
+server.get('/api/users', (req, res) => {
+    users
+        .get()
+        .then(users => {
+            res.json({ users })
+        })
+        .catch(error => {
+            console.error(error);
+        })
+})
+
+//--- posts end point ---
+
+server.get('/api/posts', (req, res) => {
+    posts
+        .get()
+        .then(posts => {
+            res.json({ posts })
+        })
+        .catch(error => {
+            console.error(error);
+        })
+})
+
+//---tags end point ---
+
+server.get('/api/tags', (req, res) => {
+    tags
+        .get()
+        .then(tags => {
+            res.json({ tags })
+        })
+        .catch(error => {
+            console.error(error);
+        })
+})
+
 
 server.listen(port, () => console.log(`Server is running on port ${port}`));
