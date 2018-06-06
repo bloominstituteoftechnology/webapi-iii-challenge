d9f1c665617771dee73b51c5d44ff90f017cbf6d
Usman Javed <ujaved931@gmail.com>
2018-06-07 02:04:01 +0500

    Finished CRUD for postDb implemented get, post, delete, put working on userDb now

--- server.js
+++ server.js
@@ -1,27 +1,34 @@
 const cors = require('cors');
 const express = require('express');
-const userDb = require('./data/helpers/userDb');
-const postDb = require('./data/helpers/postDb');
-const tagDb = require('./data/helpers/tagDb');
+
+
+
 
 const port = 5454;
 const server = express();
 server.use(express.json());
 server.use(cors());
 
+// message at localhost:5454
 const userError = (status, message, res) => {
     res.status(status).json({ errorMessage: message });
     return;
 }
 
+// importing routes
+
+const userRoutes = require('./users/userRoutes')
+const tagRoutes = require('./tags/tagRoutes')
+const postRoutes = require('./posts/postRoutes')
+
 server.get('/', (req, res) => {
-    // 1st argL route where a resource can be interacted with
-    // 2nd arg: call back to deal with sending responsevs, and handling incoming
     res.send('Hello from express')
 
 });
 
-
+server.use('/users', userRoutes);
+server.use('/tags', tagRoutes);
+server.use('/posts', postRoutes);
 
 
 server.listen(port, ()=> console.log(`Server running on port ${port}`));
