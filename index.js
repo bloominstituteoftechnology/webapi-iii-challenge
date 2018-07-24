924cb5a0af91b602ab65bd8cef2dc37dead668c4
DirupT <iamcooled@gmail.com>
2018-07-24 14:24:32 -0500

    added get post by id to server

--- index.js
+++ index.js
@@ -96,4 +96,14 @@ server.get('/api/posts', (req, res) => {
         .catch(err => res.status(500).json({ error: 'The posts information could not be retrieved.' }));
 })
 
+server.get('/api/posts/:id', (req, res) => {
+    posts
+        .get(req.params.id)
+        .then(post => {
+            if (post.length === 0) return res.status(404).json({ message: "The post with the specified ID does not exist." });
+            res.status(200).json(post);
+        })
+        .catch(err => res.status(500).json({ error: 'The post information could not be retrieved.' }));
+})
+
 server.listen(8000, () => console.log('API is running on port 8000'));
