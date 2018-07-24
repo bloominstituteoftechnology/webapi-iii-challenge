d3f716394cd5b550b695eb7bb1fc16177c7eda0d
DirupT <iamcooled@gmail.com>
2018-07-24 14:07:39 -0500

    added update user by id to server

--- index.js
+++ index.js
@@ -33,7 +33,6 @@ server.get('/api/users/:id', (req, res) => {
 })
 
 server.get('/api/users/posts/:id', (req, res) => {
-    console.log(req.params.id);
     users
         .getUserPosts(req.params.id)
         .then(posts => {
@@ -41,6 +40,26 @@ server.get('/api/users/posts/:id', (req, res) => {
             res.status(200).json(posts);
         })
         .catch(err => res.status(500).json({ error: 'The user information could not be retrieved.' }))
+})
+
+server.put('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+    const { name } = req.body;
+
+    if (!name) return res.status(400).json({ errorMessage: "Please provide a name for the user." });
+    users
+        .update(id, { name })
+        .then(response => {
+            if (response === 0) return res.status(404).json({ message: 'The user with the specified ID does not exist.' });
+            users
+                .get(id)
+                .then(user => {
+                    if (user.length === 0) return res.status(404).json({ message: "The user with the specified ID does not exist." })
+                    res.status(200).json(user);
+                })
+                .catch(err => res.status(500).json({ error: 'The post information could not be retrieved.' }));
+        })
+        .catch(err => res.status(500).json({ error: "The users information could not be modified." }));
 })
 
 server.listen(8000, () => console.log('API is running on port 8000'));
