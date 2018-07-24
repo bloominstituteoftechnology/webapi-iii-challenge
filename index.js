17f4690d446e16afa78175573c2f026dca71a642
DirupT <iamcooled@gmail.com>
2018-07-24 13:50:19 -0500

    added post user to server

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ const tags = require('./data/helpers/tagDb');
 const server = express();
 server.use(express.json());
 
+server.post('/api/users', (req, res) => {
+    const { name } = req.body;
+    if (!name) return res.status(400).json({ errorMessage: "Please provide a name for the user." });
+    users
+        .insert({ name })
+        .then(users => res.status(201).json(users))
+        .catch(err => res.status(500).json({ error: 'There was an error while saving the user to the database.' }))
+})
+
 server.get('/api/users', (req, res) => {
     users
         .get()
