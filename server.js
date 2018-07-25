1a4e4c7ee820004d80d1706d92a6e58f17d43d28
Wdifulvio523 <w.difulvio523@gmail.com>
2018-07-25 17:02:34 -0400

    Successfully able to insert, delete and update users, posts, and tags

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const server = express();
 server.use(express.json());
 
 
-//USERS
-//all users
+// USERS
+// all users
 server.get('/users', (req, res) => {
     userDb
     .get()
@@ -24,7 +24,9 @@ server.get('/users', (req, res) => {
             .json({ error: "The user information could not be retrieved."});
 });
 });
-//user by id
+
+
+// user by id
 server.get('/users/:id', (req, res) => {
     userDb
     .get(req.params.id)
@@ -46,9 +48,73 @@ server.get('/users/:id', (req, res) => {
 });
 
 
+//Insert User
+server.post('/users', (req, res) => {
+    const {name} = req.body;
+    if (!name) {
+        res.status(400).json({errorMessage: "Please provide name for the user."})
+        return;
+    }
+    userDb
+        .insert({
+            name
+        })
+        .then(response => {
+            res.status(201).json({name});
+        })
+        .catch(error => {
+            res.status(500).json({error: "There was an error while saving the user to the database" })
+        })
+});
 
 
-//posts
+//Update User
+server.put('/users/:id', (req, res) => {
+    const {id} = req.params;
+    const {name} = req.body;
+    if (!name) {
+        res.status(404).json({errorMessage: "Please provide name for the user."})
+    }
+    userDb
+    .update(id, {name})
+    .then(response => {
+        if (response == 0) {
+            res.status(404).json({message: "The user with the specified ID does not exist."})
+            return;
+        }
+        res.status(200).json({name})
+    })
+    .catch(error => {
+        res.status(500).json({error: "The user information could not be modified."})
+        return;
+    });
+});
+
+
+//Delete User
+server.delete('/users/:id', (req, res) => {
+    const {id} = req.params;
+    userDb
+    .remove(id)
+    .then(response => {
+        if (response === 0) {
+            res
+            .status(404)
+            .json({message: "The user with the specified ID does not exist."})
+        }
+        res
+        .json({message:'User removed from system!'})
+    })
+        .catch(error => {
+            res
+            .status(500)
+            .json({error: "The user could not be removed"})
+        })
+});
+
+
+// POSTS
+// All posts
 server.get('/posts', (req, res) => {
     postDb
     .get()
@@ -63,6 +129,7 @@ server.get('/posts', (req, res) => {
             .json({ error: "The posts information could not be retrieved."});
 });
 });
+
 
 //post by ID
 server.get('/posts/:id', (req, res) => {
@@ -85,7 +152,75 @@ server.get('/posts/:id', (req, res) => {
 });
 });
 
-//tags
+
+// Insert post 
+server.post('/posts', (req, res) => {
+    const {text, userId} = req.body;
+    if (!text || !userId) {
+        res.status(400).json({errorMessage: "Please provide text and userId for the post."})
+        return;
+    }
+    postDb
+        .insert({
+            text, userId
+        })
+        .then(response => {
+            res.status(201).json({text, userId});
+        })
+        .catch(error => {
+            res.status(500).json({error: "There was an error while saving the post to the database" })
+        })
+});
+
+//Update post
+server.put('/posts/:id', (req, res) => {
+    const {id} = req.params;
+    const {text, userId} = req.body;
+    if (!text || !userId) {
+        res.status(400).json({errorMessage: "Please provide text and userId for the post."})
+        return;
+    }
+    postDb
+    .update(id, {text, userId})
+    .then(response => {
+        if (response == 0) {
+            res.status(404).json({message: "The post with the specified ID does not exist."})
+            return;
+        }
+        res.status(200).json({text, userId})
+    })
+    .catch(error => {
+        res.status(500).json({error: "The post information could not be modified."})
+        return;
+    });
+});
+
+
+//Delete Post
+server.delete('/posts/:id', (req, res) => {
+    const {id} = req.params;
+    postDb
+    .remove(id)
+    .then(response => {
+        if (response === 0) {
+            res
+            .status(404)
+            .json({message: "The post with the specified ID does not exist."})
+        }
+        res
+        .json({message:'Post removed from system!'})
+    })
+        .catch(error => {
+            res
+            .status(500)
+            .json({error: "The post could not be removed"})
+        })
+});
+
+
+
+// TAGS
+// All tags
 server.get('/tags', (req, res) => {
     tagDb
     .get()
@@ -101,7 +236,8 @@ server.get('/tags', (req, res) => {
 });
 });
 
-//tag by id
+
+// tag by id
 server.get('/tags/:id', (req, res) => {
     tagDb
     .get(req.params.id)
@@ -120,6 +256,70 @@ server.get('/tags/:id', (req, res) => {
             .status(500)
             .json({ error: "The tag information could not be retrieved."});
 });
+});
+
+
+//Insert tag
+server.post('/tags', (req, res) => {
+    const {tag} = req.body;
+    if (!tag) {
+        res.status(400).json({errorMessage: "Please provide tag for the post."})
+        return;
+    }
+    tagDb
+        .insert({
+            tag
+        })
+        .then(response => {
+            res.status(201).json({tag});
+        })
+        .catch(error => {
+            res.status(500).json({error: "There was an error while saving the tag to the database" })
+        })
+});
+
+//Update tag
+server.put('/tags/:id', (req, res) => {
+    const {id} = req.params;
+    const {tag} = req.body;
+    if (!tag) {
+        res.status(400).json({errorMessage: "Please provide tag for the tag."})
+        return;
+    }
+    tagDb
+    .update(id, {tag})
+    .then(response => {
+        if (response == 0) {
+            res.status(404).json({message: "The tag with the specified ID does not exist."})
+            return;
+        }
+        res.status(200).json({tag})
+    })
+    .catch(error => {
+        res.status(500).json({error: "The tag information could not be modified."})
+        return;
+    });
+});
+
+//Delete tag
+server.delete('/tags/:id', (req, res) => {
+    const {id} = req.params;
+    tagDb
+    .remove(id)
+    .then(response => {
+        if (response === 0) {
+            res
+            .status(404)
+            .json({message: "The tag with the specified ID does not exist."})
+        }
+        res
+        .json({message:'tag removed from system!'})
+    })
+        .catch(error => {
+            res
+            .status(500)
+            .json({error: "The tag could not be removed"})
+        })
 });
 
 server.listen(8000, () => console.log("Yo, your API us running on port 8000"));
