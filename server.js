62c667ec4f4b01c6d3f9409c933c162a71725564
Arthur Pisakhov <arthurpisakhov1@gmail.com>
2018-07-25 16:56:56 -0400

    GET post and tag

--- server.js
+++ server.js
@@ -7,16 +7,35 @@ const morgan = require('morgan');
 
 const server = express();
 
-server.use(express.json());
-server.use(morgan('tiny'))
+// middlware
+server.use(express.json()); 
+server.use(morgan('tiny'));
 
-server.get('/users', async (req, res) => {
+server.get('/users', async (req, res) => { // GET user
     try {
-        const users = await userDb.get()
+        const users = await userDb.get();
         res.status(200).json(users)
     } catch (error) {
-        res.status(500).send({ error: 'Users information could not be retrieved.' })
-    }
-})
+        res.status(500).send({ error: 'The users information could not be retrieved.' })
+    };
+});
+
+server.get('/posts', async (req, res) => { // GET post
+    try {
+        const users = await postDb.get();
+        res.status(200).json(users)
+    } catch (error) {
+        res.status(500).send({ error: 'The posts information could not be retrieved.' })
+    };
+});
+
+server.get('/tags', async (req, res) => {  // GET tag
+    try {
+        const users = await tagDb.get();
+        res.status(200).json(users)
+    } catch (error) {
+        res.status(500).send({ error: 'The tags information could not be retrieved.' })
+    };
+});
 
 server.listen(8000, () => console.log('API running on port 8000'));
