4d38df9b8ec130b56903c76a91a032be5307dd24
Allen Hai <allenhai03@gmail.com>
2018-07-25 14:27:07 -0500

    add POST handler for /users

--- server.js
+++ server.js
@@ -9,11 +9,27 @@ const server  = express()
 server.use(express.json())
 server.use(morgan('tiny'))
 
+server.post('/users', async (req, res) => {
+  if (!req.body || !req.body.name)
+    res.status(400).json({ message: "you need to provide a name for the user" })
+
+  const { name } = req.body
+  
+  try {
+    const { id } = await userDb.insert({ name }) 
+    if (id) {
+      res.status(200).json({ id, name })
+    }
+  } catch(e) {
+    res.status(500).json({ error: "couldn't add user to db" })
+  }
+})
+
 server.get('/users', async (req, res) => {
   try {
     const users = await userDb.get()
     res.status(200).json(users)
-  } catch(e){
+  } catch(e) {
     res.status(500).json({ error: "couldn't retrieve users" })
   }
 })
