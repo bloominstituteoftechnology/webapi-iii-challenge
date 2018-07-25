7423ee0d742d77c2c874d741ac23a6fd1f11e5c7
Luis <luis@Luiss-MacBook-Air.local>
2018-07-25 13:13:20 -0700

    created endpoint to get all users

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 const express = require('express')
 const helmet = require('helmet')
-const morgan = require('morgan')
+const logger = require('morgan')
 const postDb = require('./data/helpers/postDb')
 const tagDb = require('./data/helpers/tagDb')
 const userDb = require('./data/helpers/userDb')
 
 const server = express()
 
+// adding middleware
 server.use(helmet())
-server.use(morgan('short'))
-
+server.use(logger('short'))
 server.use(express.json())
+
+// endpoints for GET method
+server.get('/api/users', (req, res) => {
+  userDb
+    .get()
+    .then((users) => res.status(200).json(users))
+    .catch((err) =>
+      res
+        .status(500)
+        .json({ error: 'The users information could not be retrieved.' })
+    )
+})
+
+server.use(function (req, res) {
+  res.status(404).send("Ain't nobody got time for that!")
+})
 
 server.listen(8000, () => console.log('\n=== API running... ===\n'))
