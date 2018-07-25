dc3b5aad0cda7e3395d2c44f2ae5674ed509de1f
TangledTessellations <mcevoya@oregonstate.edu>
2018-07-25 16:07:42 -0700

    Add global error messages and finish put handler for posts

--- index.js
+++ index.js
@@ -12,12 +12,36 @@ server.listen(8000, () => console.log('API running on port 8000'))
 server.use(express.json())
 server.use(helmet())
 
+const m = {
+    SUCCESS: 200,
+    MISSING_INFORMATION: {
+        code: 400,
+        msg: "Please include all required information"
+    },
+    INVALID_USER_ID: {
+        code: 400,
+        msg: "No User by that ID"
+    },
+    INVALID_POST_ID: {
+        code: 400,
+        msg: "No post by that ID"
+    },
+    INTERNAL_SERVER_ERROR: {
+        code: 500,
+        msg: "Internal Server Error"
+    }
+
+}
+
+
+// ******************************  Posts ********************************************
+
 server.get('/posts', async (req, res) => {
     try{
         const posts = await post.get()
-        res.status(200).json(posts)
+        res.status(m.SUCCESS).json(posts)
     }catch(err){
-        res.status(500).json({ "error": "The posts information could not be retrieved"})
+        res.status(m.INTERNAL_SERVER_ERROR.code).json({ "error": m.INTERNAL_SERVER_ERROR.msg })
     }
 })
 
@@ -25,63 +49,55 @@ server.get('/posts/:id', async (req,res) => {
     let { id } = req.params
     try{
         const postIn = await post.get(id)
-        res.status(200).json(postIn)
+        res.status(m.SUCCESS).json(postIn)
     }catch(err){
-        res.status(400).json({ "error": "The posts information for that ID could not be found"})
+        res.status(m.INVALID_POST_ID.code).json({ "error": m.INVALID_POST_ID.msg})
     }
 })
 
 server.post('/posts', async (req, res) => {
     const { text, userId } = req.body
-    console.log("text", text, "userID", userId);
-    let userIn = await user.get(userId)
-    
-    if(!text || !userId){
-        res.status(400).json({ "error": "Please include some text and the user ID of an existing user"})
-    }
-    
-    if(!userIn){
-        res.status(400).json({ "error": "No user exists with that ID"})
-    }
-    
+    let error = m.MISSING_INFORMATION
+
     try{
+        if(!text || !userId){ throw Error() }   // Error is currently missing information, throw if missing information
+        error = m.INVALID_USER_ID
+        let userIn = await user.get(userId)     // Error is now invalid user error
+        if(!userIn){ throw Error() }
         const postOut = {...req.body}
+        error = m.INTERNAL_SERVER_ERROR           // ID checks out, change error to internal server error in case that goes wrong
         const response = await post.insert(postOut)
-        res.status(200).json(response)
+        res.status(m.SUCCESS).json(response)
     }catch(err){
-        res.status(500).json({ "error": "Error inserting post into database"})
+        res.status(error.code).json({ "error": error.msg})
     }
 })
 
-// server.put('/posts/:id', async (req, res) => {
-//     const { title, contents } = req.body
-//     const { id } = req.params
-//     let post = await db.findById(id);
+server.put('/posts/:id', async (req, res) => {
+    const updated = {...req.body}
+    const { id } = req.params
+    let error = m.INVALID_USER_ID
 
-//     if(!title || !contents){
-//         res.status(400).json({ "errorMessage": "Please provide title and contents for the post." })
-//     }
-//     if(post.length < 1){
-//         res.status(404).json({ "errorMessage": "The post with the specified ID does not exist." })
-//     }
+    try{
+        let postIn = await post.get(id)
+        error = m.INTERNAL_SERVER_ERROR     // If Id checks out, change error to server error
+        await post.update(id, updated); 
+        res.status(SUCCESS).json(updated)
+    }catch(err) {
+        res.status(error.code).json({ "error": error.msg })
+    }
+})
 
-//     try{
-//         post = post[0];
-//         post["title"] = title;
-//         post["contents"] = contents;
-//         post["updated_at"] = Date.now();
-//         await db.update(id, post);  
-//         res.status(200).send(post)
-//     }catch(err) {
-//         res.status(500).json({ error: "The post information could not be modified." })
-//     }
-// })
 
+
+
+
+// ******************************  Users ********************************************
 
 server.get('/users', async (req, res) => {
     try{
         const users = await user.get()
-        res.status(200).json(users)
+        res.status(m.SUCCESS).json(users)
     }catch(err){
         res.status(500).json({ "error": "Problem retrieving users"})
     }
@@ -92,12 +108,12 @@ server.get('/users', async (req, res) => {
 
 
 
-
+// ******************************  Tags ********************************************
 
 server.get('/tags', async (req, res) => {
     try{
         const tags = await tag.get()
-        res.status(200).json(tags)
+        res.status(m.SUCCESS).json(tags)
     }catch(err){
         res.status(500).json({ "error": "Problem retrieving tags"})
     }
