b6347ae741f62c2ae167c838aa4077961e935358
Wdifulvio523 <w.difulvio523@gmail.com>
2018-07-25 15:55:53 -0400

    Successfully created a get. request to verify that server was running

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require("express");
 
 
-
-
-
-
-
 const server = express();
 server.use(express.json());
+
+
+
+server.get('/', (requestAnimationFrame, res) => {
+    res.send('<p>Hello</p>');
+});
+
+
+
+
+
+
 
 server.listen(8000, () => console.log("Yo, your API us running on port 8000"));
