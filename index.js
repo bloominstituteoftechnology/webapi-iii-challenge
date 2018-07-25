85aba7cab65237f880a09f1eb7350520dfece91e
DirupT <iamcooled@gmail.com>
2018-07-25 15:02:03 -0500

    converted to async/await for tags

--- index.js
+++ index.js
@@ -159,65 +159,69 @@ server.delete('/api/posts/:id', async (req, res) => {
 
 // END OF POSTS
 
-server.post('/api/tags', (req, res) => {
+server.post('/api/tags', async (req, res) => {
     const { tag } = req.body;
     if (!tag) return res.status(400).json({ errorMessage: "Please provide a tag." });
-    tags
-        .insert({ tag })
-        .then(tags => res.status(201).json(tags))
-        .catch(err => res.status(500).json({ error: 'There was an error while saving the tag to the database.' }));
+    try {
+        const response = await tags.insert({ tag });
+        return res.status(201).json(response);
+    } catch (err) {
+        return res.status(500).json({ error: 'There was an error while saving the tag to the database.' });
+    }
 })
 
-server.get('/api/tags', (req, res) => {
-    tags
-        .get()
-        .then(tags => res.status(200).json(tags))
-        .catch(err => res.status(500).json({ error: 'The tags information could not be retrieved.' }));
+server.get('/api/tags', async (req, res) => {
+    try {
+        const response = await tags.get();
+        return res.status(200).json(response);
+    } catch (err) {
+        return res.status(500).json({ error: 'The tags information could not be retrieved.' });
+    }
 })
 
-server.get('/api/tags/:id', (req, res) => {
-    tags
-        .get(req.params.id)
-        .then(tag => {
-            if (tag.length === 0) return res.status(404).json({ message: "The tag with the specified ID does not exist." });
-            res.status(200).json(tag);
-        })
-        .catch(err => res.status(500).json({ error: 'The tag information could not be retrieved.' }));
+server.get('/api/tags/:id', async (req, res) => {
+    try {
+        const response = await tags.get(req.params.id);
+        if (!response) return res.status(404).json({ message: "The tag with the specified ID does not exist." });
+        res.status(200).json(response);
+    } catch (err) {
+        return res.status(500).json({ error: 'The tag information could not be retrieved.' });
+    }
 })
 
-server.put('/api/tags/:id', (req, res) => {
+server.put('/api/tags/:id', async (req, res) => {
     const { id } = req.params;
     const { tag } = req.body;
     if (!tag) return res.status(400).json({ errorMessage: "Please provide a tag." });
-    tags
-        .update(id, { tag })
-        .then(response => {
-            if (response === 0) return res.status(404).json({ message: 'The tag with the specified ID does not exist.' });
-            tags
-                .get(id)
-                .then(tag => {
-                    if (tag.length === 0) return res.status(404).json({ message: "The tag with the specified ID does not exist." });
-                    res.status(200).json(tag);
-                })
-                .catch(err => res.status(500).json({ error: 'The tag information could not be retrieved.' }));
-        })
-        .catch(err => res.status(500).json({ error: "The tags information could not be modified." }));
+    try {
+        const updateResponse = await tags.update(id, { tag });
+        if (updateResponse === 0) return res.status(404).json({ message: 'The tag with the specified ID does not exist.' });
+        try {
+            const findResponse = await tags.get(id);
+            if (!findResponse) return res.status(404).json({ message: "The tag with the specified ID does not exist." });
+            return res.status(200).json(findResponse);
+        } catch (err) {
+            return res.status(500).json({ error: 'The tag information could not be retrieved.' });
+        }
+    } catch (err) {
+        return res.status(500).json({ error: "The tags information could not be modified." });
+    }
 })
 
-server.delete('/api/tags/:id', (req, res) => {
-    tags
-        .get(req.params.id)
-        .then(tag => {
-            if (tag.length === 0) return res.status(404).json({ message: "The tag with the specified ID does not exist." });
-            res.status(200).json(tag);
-            tags
-                .remove(req.params.id)
-                .then(response => {
-                    if (response === 0) return res.status(404).json({ message: "The tag with the specified ID does not exist." });
-                })
-                .catch(err => res.status(500).json({ error: "The tag could not be removed" }));
-        })
-        .catch(err => res.status(500).json({ error: "The tag information could not be retrieved." }))
+server.delete('/api/tags/:id', async (req, res) => {
+    try {
+        const findResponse = await tags.get(req.params.id);
+        if (!findResponse) return res.status(404).json({ message: "The tag with the specified ID does not exist." });
+        try {
+            const removeResponse = await tags.remove(req.params.id);
+            if (removeResponse === 0) return res.status(404).json({ message: "The tag with the specified ID does not exist." });
+            return res.status(200).json(findResponse);
+        } catch (err) {
+            return res.status(500).json({ error: "The tag could not be removed" });
+        }
+    } catch (err) {
+        return res.status(500).json({ error: "The tag information could not be retrieved." });
+    }
 })
 
 server.listen(8000, () => console.log('API is running on port 8000'));
