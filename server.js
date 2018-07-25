2cb15172f50dbd2e2e7508b9250a32395604da45
emaquino44 <prexy44@yahoo.com>
2018-07-25 14:09:32 -0700

    server setup and api running

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
+// import your node modules
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
 
+
+const server = express();
+const port = 5000;
+
+//middleware
+server.use(express.json());
+server.use(helmet());
+server.use(cors({ origin: 'http://localhost:3000' }));
+
+server.get('/', (req, res) => {
+
+  res.send('Hello World');
+});
+
+
+
+
+// add your server code starting here
+server.listen(5000, () => console.log('API is running'));
