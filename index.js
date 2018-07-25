f0f68013f83ee3afd9d60c80c3bca9115cff659e
Dil Sombayeva <dilsombayeva@KevinsMacBook2.attlocal.net>
2018-07-25 14:13:23 -0700

    CRUD for tags is done

--- index.js
+++ index.js
@@ -103,13 +103,25 @@ server.get('/api/posts', (req, res) => {
 server.get('/api/posts/:id', (req, res) => {
     const { id } = req.params;
     posts.get(id).then(p => {
-        if(!p) {
+        if(p.length === 0) {
             res.status(404).json({ error: 'The post with specified ID does not exist.' });
         }
         res.status(200).json(p)
     })
     .catch(err => {
         res.status(500).json({ error: 'The posts information could not be retrieved.'})
+    })
+});
+server.get('/api/posts/:id/tags', (req, res) => {
+    const { id } = req.params;
+    posts.getPostTags(id).then(p => {
+        if(p.length === 0) {
+            res.status(404).json({ error: 'The list of tags with specified post ID does not exist.' });
+        }
+        res.status(200).json(p)
+    })
+    .catch(err => {
+        res.status(500).json({ error: 'The post tags information could not be retrieved.'})
     })
 });
 server.post('/api/posts', (req, res) => {
@@ -155,7 +167,69 @@ server.put('/api/posts/:id', (req, res) => {
 })
 // ***** post end *******
 
-// ***** tag start *****
+// ***** tag start ******
+server.get('/api/tags', (req, res) => {
+    tags.get().then(u => {
+        res.status(200).json(u)
+    })
+    .catch(err => {
+        res.status(500).json({ error: 'The tags information could not be retrieved'})
+    })
+});
+server.get('/api/tags/:id', (req, res) => {
+    const { id } = req.params;
+    tags.get(id).then(u => {
+        if(!u) {
+            res.status(404).json({ error: 'The tag with specified ID does not exist.' });
+        }
+        res.status(200).json(u)
+    })
+    .catch(err => {
+        res.status(500).json({ error: 'The tag information could not be retrieved.'})
+    })
+});
+
+server.post('/api/tags', (req, res) => {
+    const { tag } = req.body;
+    if(!tag){
+        res.status(400).json({ error: 'Please provide tag.' });
+        res.end(); // <-- needed or not?
+    }
+    tags.insert({ tag }).then(u => {
+        res.status(201).json(u);
+    })
+    .catch(err => {
+        res.status(500).json({ error: 'There was an error while saving the tag to the database.' })
+    })
+});
+server.delete('/api/tags/:id', (req, res) => {
+    const { id } = req.params;
+    tags.remove(id).then(u => {
+        if(!u) {
+            res.status(404).json({ error: 'The tag with the specified ID does not exist.' })
+        }
+        res.status(200).json(u);
+    })
+    .catch(err => {
+        res.status(500).json({ error: 'The tag could not be removed.' })
+    })
+})
+server.put('/api/tags/:id', (req, res) => {
+    const { id } = req.params;
+    const { tag } = req.body;
+    tags.update(id, { tag }).then(u => {
+        if(!tag) {
+            res.status(400).json({ error: 'Please provide tag name' })
+        } else if(!u) {
+            res.status(404).json({ error: 'The tag with specified ID does not exist' })
+        } else {
+            res.status(200).json(u);
+        }
+    })
+    .catch(err => {
+        res.status(500).json({ error: 'The tag information could not be modified' })
+    })
+})
 // ***** tag end *******
 
 server.listen(port, () => console.log(`Server is listening on port ${port}`))
