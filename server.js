9109f9813a9522c353f88c132031bd9d9ffc5358
Vu Cao <cdhvu@yahoo.com>
2018-07-25 13:24:58 -0700

    Created endpoints for GET with id

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const server= express();
 //use middleware
 server.use(express.json());
 
-server.get('/users', async (req,res) => {
+server.get('/users/', async (req,res) => {
     try {
         const response = await userDb.get();
         res.status(200).json(response);
@@ -18,7 +18,22 @@ server.get('/users', async (req,res) => {
     }
 })
 
-server.get('/posts', async (req,res) => {
+server.get('/users/:id', async (req,res) => {
+    try {
+        const id = req.params.id;
+        const response = await userDb.get(id);
+        
+        if (!response) {
+            return res.status(404).send({ message: "The user with the specified ID does not exist." })
+        }
+
+        res.status(200).json(response);
+    } catch (error) {
+        res.status(500).send({error: 'Users information could not be retrieved.'})
+    }
+})
+
+server.get('/posts/', async (req,res) => {
     try {
         const response = await postDb.get();
         res.status(200).json(response);
@@ -27,9 +42,39 @@ server.get('/posts', async (req,res) => {
     }
 })
 
-server.get('/tags', async (req,res) => {
+server.get('/posts/:id', async (req,res) => {
+    try {
+        const id = req.params.id;
+        const response = await postDb.get(id);
+        
+        if (!response) {
+            return res.status(404).send({ message: "The post with the specified ID does not exist." })
+        }
+
+        res.status(200).json(response);
+    } catch (error) {
+        res.status(500).send({error: 'Posts information could not be retrieved.'})
+    }
+})
+
+server.get('/tags/', async (req,res) => {
     try {
         const response = await tagDb.get();
+        res.status(200).json(response);
+    } catch (error) {
+        res.status(500).send({error: 'Tags information could not be retrieved.'})
+    }
+})
+
+server.get('/tags/:id', async (req,res) => {
+    try {
+        const id = req.params.id;
+        const response = await tagDb.get(id);
+        
+        if (!response) {
+            return res.status(404).send({ message: "The tag with the specified ID does not exist." })
+        }
+
         res.status(200).json(response);
     } catch (error) {
         res.status(500).send({error: 'Tags information could not be retrieved.'})
