9c1f9fdafe23a84fdc61ec84c2ba4fa034ff0cf8
Vu Cao <cdhvu@yahoo.com>
2018-07-25 14:15:46 -0700

    Created enpoint for Get userPosts and  postTags

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const server= express();
 //use middleware
 server.use(express.json());
 
+//endpoint for GET users
 server.get('/users/', async (req,res) => {
     try {
         const response = await userDb.get();
@@ -18,6 +19,7 @@ server.get('/users/', async (req,res) => {
     }
 })
 
+//endpoint for GET user with id
 server.get('/users/:id', async (req,res) => {
     try {
         const id = req.params.id;
@@ -33,6 +35,7 @@ server.get('/users/:id', async (req,res) => {
     }
 })
 
+//endpoint for GET posts
 server.get('/posts/', async (req,res) => {
     try {
         const response = await postDb.get();
@@ -42,21 +45,20 @@ server.get('/posts/', async (req,res) => {
     }
 })
 
+//endpoint for GET posts with id
 server.get('/posts/:id', async (req,res) => {
+    const id = req.params.id;
+    
     try {
-        const id = req.params.id;
         const response = await postDb.get(id);
-        
-        if (!response) {
-            return res.status(404).send({ message: "The post with the specified ID does not exist." })
-        }
-
         res.status(200).json(response);
     } catch (error) {
+        console.log(error)
         res.status(500).send({error: 'Posts information could not be retrieved.'})
     }
 })
 
+//endpoint for GET tags
 server.get('/tags/', async (req,res) => {
     try {
         const response = await tagDb.get();
@@ -66,9 +68,11 @@ server.get('/tags/', async (req,res) => {
     }
 })
 
+//endpoint for GET tags with id
 server.get('/tags/:id', async (req,res) => {
+    const id = req.params.id;
+
     try {
-        const id = req.params.id;
         const response = await tagDb.get(id);
         
         if (!response) {
@@ -78,6 +82,29 @@ server.get('/tags/:id', async (req,res) => {
         res.status(200).json(response);
     } catch (error) {
         res.status(500).send({error: 'Tags information could not be retrieved.'})
+    }
+})
+
+//endpoint for GET userposts
+server.get('/users/:id/posts', async (req,res) => {
+    const id = req.params.id;
+
+    try {
+        const response = await userDb.getUserPosts(id);
+        res.status(200).json(response);
+    } catch (error) {
+        res.status(500).send({message: 'Users information could not be retrieved.', error: error.message})
+    }
+})
+
+server.get('/posts/:id/tags', async (req,res) => {
+    const id = req.params.id;
+
+    try {
+        const response = await postDb.getPostTags(id);
+        res.status(200).json(response);
+    } catch (error) {
+        res.status(500).send({message: 'Users information could not be retrieved.', error: error.message})
     }
 })
 
