3de19d337e39e8de7fb8f2cdbe191266a1b0af4d
Mariana Cerda <raymond_grc@yahoo.com>
2018-07-25 15:49:34 -0500

    Add get method to users

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const UserDb = require("./data/helpers/userDb.js");
 
 server.use(express.json());
 
-
+//posts
 server.get("/api/posts/", (req, res) => {
   postDb
         .get()
@@ -30,6 +30,30 @@ server.get("/api/posts/:id", (req, res) => {
             return res.status(500).send({Message: "Server Error"});
         });
 });
-
-
+//users
+server.get("/api/users/", (req, res) => {
+  UserDb
+    .get()
+    .then(users => {
+      res.json(users)
+    })
+    .catch(error => {
+      return res.status(500).send({Message: "Server Error"});
+    })
+})
+server.get("/api/users/:id", (req, res) => {
+  const { id } = req.params;
+  UserDb
+    .get(id)
+    .then(users => {
+      if (users == undefined){
+        return res.status(200).send({Message: "User does not exist"});
+      }
+      res.json(users)
+    })
+    .catch(error => {
+      return res.status(500).send({Message: "Server Error"});
+    })
+})
+//tags
 server.listen(8000, () => {console.log("Server is listening on port 8000")})
