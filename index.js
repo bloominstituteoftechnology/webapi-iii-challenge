251b067ea1524cbe7ea7b8857e59a728c2098a60
Blake Fletcher <blakefletcher@Blakes-MacBook-Air.local>
2018-07-25 12:34:03 -0400

    Put posts complete, stuck on put users and tags

--- index.js
+++ index.js
@@ -194,4 +194,120 @@ server.delete('/api/tags/:id', (req, res) => {
         })
 })
 
+// PUT users, posts, tags
+
+{/*server.put('/api/users/:id', (req, res) => {
+    const {id} = req.params;
+    const {name} = req.body;
+    
+    if (!name) {
+        res.status(400).json({error: "Please provide a name for the user."});
+      return; 
+      }
+
+    users
+        .update(id, {name})
+        .then(response => {
+            if ( response > 0 ) {
+                users
+                    .get(id)
+                    .then(users => {
+                        res.status(200).json({users})
+                    })
+            } else {
+               res.status(404).json({message: "user not found"}) 
+            }
+        })
+        .catch(err => {
+            res.status(500).json({message: "The user with the specified ID does not exist."})
+        })
+
+})*/}
+
+server.put('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+    const { name } = req.body;
+    if (!name) {
+        sendUserError(400, `User must have a name`, res);
+        return;
+    }
+    users
+        .update(id, {name})
+        .then(user => {
+            if(user.length === 0) {
+                sendUserError(404, `The user with the ID ${id} does not exist.`, res);
+                return;
+            }
+            users
+                .get(id)
+                .then(user => {
+                    res.json({user});
+                })
+                .catch(error => {
+                    sendUserError(500, `There was an error processing your request`, res);
+                    return;
+                })
+        })
+        .catch(error => {
+            sendUserError(500, `There was an error processing your request`, res);
+            return;
+        });
+});
+
+server.put('/api/posts/:id', (req, res) => {
+    const {id} = req.params
+    const { userId, text } = req.body
+  
+    if (!userId || !text) {
+      res.status(400).json({error: "Please provide userId and text for the post."});
+    return; 
+    }
+  
+    posts
+        .update(id, {userId, text})
+        .then(response => {
+            if ( response > 0 ) {
+                posts
+                    .get(id)
+                    .then(posts => {
+                        res.status(200).json({posts})
+                    })
+            } else {
+               res.status(404).json({message: "The post with the specified ID does not exist."}) 
+            }
+        })
+        .catch(err => {
+            res.status(500).json({message: "The post could not be modified."})
+        })
+  
+  })
+
+  server.put('/api/tags/:id', (req, res) => {
+    const {id} = req.params
+    const { tag } = req.body
+  
+    if (!tag ) {
+      res.status(400).json({error: "Please provide the tag."});
+    return; 
+    }
+  
+    tags
+        .update(id, {tag})
+        .then(response => {
+            if ( response > 0 ) {
+                tags
+                    .get(id)
+                    .then(tags => {
+                        res.status(200).json({tags})
+                    })
+            } else {
+               res.status(404).json({message: "The tag with the specified ID does not exist."}) 
+            }
+        })
+        .catch(err => {
+            res.status(500).json({message: "The tag could not be modified."})
+        })
+  
+  })
+
 server.listen(port, () => console.log(`Server is listening to port ${port}`));
