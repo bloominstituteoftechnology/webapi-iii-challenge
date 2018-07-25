5d20e0899fbfe5b09f2393d6b9a950aac12532e1
Mariana Cerda <raymond_grc@yahoo.com>
2018-07-25 15:27:31 -0500

    Add get method to posts, posts by ID

--- index.js
+++ index.js
@@ -9,4 +9,27 @@ const UserDb = require("./data/helpers/userDb.js");
 server.use(express.json());
 
 
+server.get("/api/posts/", (req, res) => {
+  postDb
+        .get()
+        .then(posts => {
+            res.json(posts);
+        })
+        .catch(error => {
+            return res.status(500).send({Message: "Server Error"});
+        });
+});
+server.get("/api/posts/:id", (req, res) => {
+  const { id } = req.params;
+  postDb
+        .get(id)
+        .then(posts => {
+            res.json(posts);
+        })
+        .catch(error => {
+            return res.status(500).send({Message: "Server Error"});
+        });
+});
+
+
 server.listen(8000, () => {console.log("Server is listening on port 8000")})
