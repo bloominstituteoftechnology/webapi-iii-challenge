9c2c7fb659070277104ea432af001484455b21b4
Wdifulvio523 <w.difulvio523@gmail.com>
2018-07-25 16:14:58 -0400

    successfully retrieved user by id

--- server.js
+++ server.js
@@ -24,6 +24,26 @@ server.get('/users', (req, res) => {
 });
 });
 
+server.get('/users/:id', (req, res) => {
+    userDb
+    .get(req.params.id)
+    .then(user => {
+        if (user.length === 0) {
+            res
+            .status(404)
+            .json({ message: "The user with the specified ID does not exist." });
+        }
+        res
+        .status(200)
+        .json(user);
+    })
+    .catch(err => {
+            res
+            .status(500)
+            .json({ error: "The user information could not be retrieved."});
+});
+});
+
 
 //posts
 server.get('/posts', (req, res) => {
