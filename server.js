2cf38bc291b12f5bc42579244b38a8a04848c52b
emaquino44 <prexy44@yahoo.com>
2018-07-25 14:35:15 -0700

    debugged server issue and created user get endpoint

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const userDb = require('./data/helpers/userDb.js');
 
 
 const server = express();
@@ -12,13 +13,23 @@ server.use(express.json());
 server.use(helmet());
 server.use(cors({ origin: 'http://localhost:3000' }));
 
-server.get('/', (req, res) => {
 
-  res.send('Hello World');
+//router handlers
+server.get('/', (req, res) => {
+  res.send('Hello World you');
 });
 
-
+//User Endpoints - router handlers
+server.get('/api/users', (req, res) => {
+	userDb.get()
+		.then(users => {
+			res.status(200).json({users});
+		})
+		.catch(err => {
+			res.status(500).json({error: "Please provide user name."});
+		});
+})
 
 
 // add your server code starting here
-server.listen(5000, () => console.log('API is running'));
+server.listen(port, () => console.log(`API listening on port ${port}`));
