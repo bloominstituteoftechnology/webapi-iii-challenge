a3602488eb605c676f0f38678ecad370fb3563e7
DirupT <iamcooled@gmail.com>
2018-07-25 14:24:06 -0500

    converted to async/await for users

--- index.js
+++ index.js
@@ -8,76 +8,79 @@ const server = express();
 server.use(express.json());
 server.use(cors());
 
-server.post('/api/users', (req, res) => {
+server.post('/api/users', async (req, res) => {
     const { name } = req.body;
     if (!name) return res.status(400).json({ errorMessage: "Please provide a name for the user." });
-    users
-        .insert({ name })
-        .then(users => res.status(201).json(users))
-        .catch(err => res.status(500).json({ error: 'There was an error while saving the user to the database.' }));
+    try {
+        const response = await users.insert({ name });
+        return res.status(201).json(response);
+    } catch (err) {
+        return res.status(500).json({ error: 'There was an error while saving the user to the database.' });
+    }
 })
 
-server.get('/api/users', (req, res) => {
-    users
-        .get()
-        .then(users => res.status(200).json(users))
-        .catch(err => res.status(500).json({ error: 'The users information could not be retrieved.' }));
+server.get('/api/users', async (req, res) => {
+    try {
+        const response = await users.get();
+        return res.status(200).json(response);
+    } catch (err) {
+        return res.status(500).json({ error: 'The users information could not be retrieved.' });
+    }
 })
 
-server.get('/api/users/:id', (req, res) => {
-    users
-        .get(req.params.id)
-        .then(user => {
-            if (user.length === 0) return res.status(404).json({ message: "The user with the specified ID does not exist." });
-            res.status(200).json(user);
-        })
-        .catch(err => res.status(500).json({ error: 'The user information could not be retrieved.' }));
+server.get('/api/users/:id', async (req, res) => {
+    try {
+        const response = await users.get(req.params.id);
+        if (!response) return res.status(404).json({ message: "The user with the specified ID does not exist." });
+        return res.status(200).json(response);
+    } catch (err) {
+        return res.status(500).json({ error: 'The user information could not be retrieved.' });
+    }
 })
 
-server.get('/api/users/posts/:id', (req, res) => {
-    users
-        .getUserPosts(req.params.id)
-        .then(posts => {
-            if (posts.length === 0) return res.status(404).json({ message: 'This user has no posts!' });
-            res.status(200).json(posts);
-        })
-        .catch(err => res.status(500).json({ error: 'The user information could not be retrieved.' }));
+server.get('/api/users/posts/:id', async (req, res) => {
+    try {
+        const response = await users.getUserPosts(req.params.id);
+        if (response.length === 0) return res.status(404).json({ message: 'This user has no posts!' });
+        return res.status(200).json(response);
+    } catch (err) {
+        return res.status(500).json({ error: 'The user information could not be retrieved.' });
+    }
 })
 
-server.put('/api/users/:id', (req, res) => {
+server.put('/api/users/:id', async (req, res) => {
     const { id } = req.params;
     const { name } = req.body;
-
     if (!name) return res.status(400).json({ errorMessage: "Please provide a name for the user." });
-    users
-        .update(id, { name })
-        .then(response => {
-            if (response === 0) return res.status(404).json({ message: 'The user with the specified ID does not exist.' });
-            users
-                .get(id)
-                .then(user => {
-                    if (user.length === 0) return res.status(404).json({ message: "The user with the specified ID does not exist." });
-                    res.status(200).json(user);
-                })
-                .catch(err => res.status(500).json({ error: 'The user information could not be retrieved.' }));
-        })
-        .catch(err => res.status(500).json({ error: "The users information could not be modified." }));
+    try {
+        const updateResponse = await users.update(id, { name });
+        if (updateResponse === 0) return res.status(404).json({ message: 'The user with the specified ID does not exist.' });
+        try {
+            const findResponse = await users.get(id);
+            if (!findResponse) return res.status(404).json({ message: "The user with the specified ID does not exist." });
+            return res.status(200).json(findResponse);
+        } catch (err) {
+            return res.status(500).json({ error: 'The user information could not be retrieved.' });
+        }
+    } catch (err) {
+        return res.status(500).json({ error: "The users information could not be modified." });
+    }
 })
 
-server.delete('/api/users/:id', (req, res) => {
-    users
-        .get(req.params.id)
-        .then(user => {
-            if (user.length === 0) return res.status(404).json({ message: "The user with the specified ID does not exist." });
-            res.status(200).json(user);
-            users
-                .remove(req.params.id)
-                .then(response => {
-                    if (response === 0) return res.status(404).json({ message: "The user with the specified ID does not exist." });
-                })
-                .catch(err => res.status(500).json({ error: "The user could not be removed" }));
-        })
-        .catch(err => res.status(500).json({ error: "The user information could not be retrieved." }))
+server.delete('/api/users/:id', async (req, res) => {
+    try {
+        const findResponse = await users.get(req.params.id);
+        if (!findResponse) return res.status(404).json({ message: "The user with the specified ID does not exist." });
+        try {
+            const removeResponse = await users.remove(req.params.id);
+            if (removeResponse === 0) return res.status(404).json({ message: "The user with the specified ID does not exist." });
+            return res.status(200).json(findResponse);
+        } catch (err) {
+            return res.status(500).json({ error: "The user could not be removed" });
+        }
+    } catch (err) {
+        return res.status(500).json({ error: "The user information could not be retrieved." });
+    }
 })
 
 // END OF USERS
