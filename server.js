936f2fa74ebba36ce6777fa163359959a84b83e7
Jameson Brown <jameson@Jamesons-MacBook-Pro.local>
2018-07-25 15:24:44 -0600

    Added .post to users, posts, tags

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const postDb = require('./data/helpers/postDb');
 const tagDb = require('./data/helpers/tagDb');
-const userDB = require('./data/helpers/userDB');
+const userDb = require('./data/helpers/userDb');
 
 const server = express();
 server.use(express.json());
@@ -11,11 +11,11 @@ server.get('/', (req, res) => {
 })
 // Users
 server.get('/api/users', (req, res) => {
-    userDB.get()
+    userDb.get()
         .then(response => res.status(200).json(response))
         .catch(err => res.status(500).json({ error: "The users information could not be retrieved." }))
 })
-server.get('/api/users/:id', (req, res) => {
+server.get('/api/user/:id', (req, res) => {
     userDb.get(Number(req.params.id))
         .then (response => {
             if (response.length === 0) {
@@ -24,13 +24,34 @@ server.get('/api/users/:id', (req, res) => {
         })
         .catch (err => res.status(500).json({ error: "The users information could not be retrieved." }))
 })
+server.post('/api/users', (req, res) => {
+    const { name } = req.body;
+    if (!name) {
+        res.status(400).json({ errorMessage: "Please provide a name for the user." })
+    }
+    userDb.insert({ name })
+        .then(response => res.status(201).json({ name }))
+        .catch(err => res.status(500).json({ error: "There was an error while saving the user to the database" }))
+})
+server.delete('/api/user/:id', (req, res) => {
+    userDb.remove(req.params.id)
+        .then(response => {
+            if (response.length === 0) {
+                res.status(404).json({ message: "The user with the specified ID does not exist." })
+            } else {
+                res.status(200).json({ message: "The user has been deleted."})
+            }
+        })
+        .catch(err => res.status(500).json({ error: "The user could not be removed" }))
+})
+
 // Posts
 server.get('/api/posts', (req, res) => {
     postDb.get()
         .then(response => res.status(200).json(response))
         .catch(err => res.status(500).json({ error: "The posts information could not be retrieved." }))
 })
-server.get('/api/posts/:id', (req, res) => {
+server.get('/api/post/:id', (req, res) => {
     postDb.get(Number(req.params.id))
         .then (response => {
             if (response.length === 0) {
@@ -39,13 +60,34 @@ server.get('/api/posts/:id', (req, res) => {
         })
         .catch (err => res.status(500).json({ error: "The posts information could not be retrieved." }))
 })
+server.post('/api/posts', (req, res) => {
+    const { text, userId } = req.body;
+    if (!text || !userId) {
+        res.status(400).json({ errorMessage: "Please provide text and userId for the post." })
+    }
+    postDb.insert({ text, userId })
+        .then(response => res.status(201).json({ text, userId }))
+        .catch(err => res.status(500).json({ error: "There was an error while saving the post to the database." }))
+})
+server.delete('/api/post/:id', (req, res) => {
+    postDb.remove(req.params.id)
+        .then(response => {
+            if (response.length === 0) {
+                res.status(404).json({ message: "The post with the specified ID does not exist." })
+            } else {
+                res.status(200).json({ message: "The post has been deleted."})
+            }
+        })
+        .catch(err => res.status(500).json({ error: "The post could not be removed" }))
+})
+
 // Tags
 server.get('/api/tags', (req, res) => {
     tagDb.get()
         .then(response => res.status(200).json(response))
         .catch(err => res.status(500).json({ error: "The tags information could not be retrieved." }))
 })
-server.get('/api/tags/:id', (req, res) => {
+server.get('/api/tag/:id', (req, res) => {
     tagDb.get(Number(req.params.id))
         .then (response => {
             if (response.length === 0) {
@@ -53,6 +95,26 @@ server.get('/api/tags/:id', (req, res) => {
             } res.status(200).json(response);
         })
         .catch (err => res.status(500).json({ error: "The tags information could not be retrieved." }))
+})
+server.post('/api/tags', (req, res) => {
+    const { tag } = req.body;
+    if (!tag) {
+        res.status(400).json({ errorMessage: "Please provide tag for the post." })
+    }
+    tagDb.insert({ tag })
+        .then(response => res.status(201).json({ tag }))
+        .catch(err => res.status(500).json({ error: "There was an error while saving the tag to the database." }))
+})
+server.delete('/api/tag/:id', (req, res) => {
+    tagDb.remove(req.params.id)
+        .then(response => {
+            if (response.length === 0) {
+                res.status(404).json({ message: "The tag with the specified ID does not exist." })
+            } else {
+                res.status(200).json({ message: "The tag has been deleted."})
+            }
+        })
+        .catch(err => res.status(500).json({ error: "The tag could not be removed" }))
 })
 
 server.listen(8000, () => console.log('API is running on port 8000'))
