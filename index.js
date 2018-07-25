27ed35a8446d9950037df2c74976c94e6d26c275
Dil Sombayeva <dilsombayeva@KevinsMacBook2.attlocal.net>
2018-07-25 12:30:51 -0700

    added first get request for users

--- index.js
+++ index.js
@@ -11,5 +11,18 @@ const server = express();
 server.use(helmet());
 server.use(express.json());
 
+server.get('/', (req, res) => {
+    res.send('<h1>Home page</h1>')
+})
+
+server.get('/api/users', (req, res) => {
+    users.get().then(u => {
+        res.status(200).json(u)
+    })
+    .catch(err => {
+        res.status(500).json({ error: 'The users information could not be retrieved'})
+    })
+})
+
 
 server.listen(port, () => console.log(`Server is listening on port ${port}`))
