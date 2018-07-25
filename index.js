d8d9fc3eaf1b501812429f140ed218af155570e6
Simon <Simon_Freelance@hotmail.com>
2018-07-25 19:02:36 -0400

    Able to make a post request to create new post

--- index.js
+++ index.js
@@ -129,4 +129,27 @@ server.get('/api/posts/:id', async (req, res) => {
         res.status(NOT_FOUND_CODE).json({ error: 'Post cannot be found' });
     }
 })
+
+server.post('/api/posts', async (req, res) => {
+    try {
+        const { userId, text } = req.body;
+        if(userId === undefined || text === undefined) {
+            throw BAD_REQUEST_CODE;
+        }
+        const postResponse = postDb.insert(req.body);
+        res.status(OK_CODE).json(postResponse);
+    } 
+    catch (err) {
+        switch(err) {
+            case BAD_REQUEST_CODE: {
+                res.status(BAD_REQUEST_CODE).json({ errorMessage: 'There needs to be a user ID and text in order to create post'});
+                res.end();
+                return;
+            }
+            default: {
+            res.status(INTERNAL_SERVER_ERROR_CODE).json({error: 'Error creating post'})
+            }
+        }
+    }
+})
 server.listen(8001);
