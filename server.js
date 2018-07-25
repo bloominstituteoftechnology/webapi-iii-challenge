ec9cad0696a9355e88028d50e8a4a8e84c5ebac8
Wdifulvio523 <w.difulvio523@gmail.com>
2018-07-25 16:21:15 -0400

    successfully retrieved users, posts, and tags by id

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ const server = express();
 server.use(express.json());
 
 
-//users
+//USERS
+//all users
 server.get('/users', (req, res) => {
     userDb
     .get()
@@ -23,7 +24,7 @@ server.get('/users', (req, res) => {
             .json({ error: "The user information could not be retrieved."});
 });
 });
-
+//user by id
 server.get('/users/:id', (req, res) => {
     userDb
     .get(req.params.id)
@@ -45,6 +46,8 @@ server.get('/users/:id', (req, res) => {
 });
 
 
+
+
 //posts
 server.get('/posts', (req, res) => {
     postDb
@@ -61,6 +64,27 @@ server.get('/posts', (req, res) => {
 });
 });
 
+//post by ID
+server.get('/posts/:id', (req, res) => {
+    postDb
+    .get(req.params.id)
+    .then(post => {
+        if (post.length === 0) {
+            res
+            .status(404)
+            .json({ message: "The post with the specified ID does not exist." });
+        }
+        res
+        .status(200)
+        .json(post);
+    })
+    .catch(err => {
+            res
+            .status(500)
+            .json({ error: "The post information could not be retrieved."});
+});
+});
+
 //tags
 server.get('/tags', (req, res) => {
     tagDb
@@ -74,6 +98,27 @@ server.get('/tags', (req, res) => {
             res
             .status(500)
             .json({ error: "The tags information could not be retrieved."});
+});
+});
+
+//tag by id
+server.get('/tags/:id', (req, res) => {
+    tagDb
+    .get(req.params.id)
+    .then(tag => {
+        if (tag.length === 0) {
+            res
+            .status(404)
+            .json({ message: "The tag with the specified ID does not exist." });
+        }
+        res
+        .status(200)
+        .json(tag);
+    })
+    .catch(err => {
+            res
+            .status(500)
+            .json({ error: "The tag information could not be retrieved."});
 });
 });
 
