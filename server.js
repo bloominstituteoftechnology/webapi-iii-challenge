3cb625ac87930f44c30a4da0caeef055b778ee1f
Joe Stanfield <jlstan06@hotmail.com>
2018-07-25 18:21:31 -0400

    finished with create post delete and put didnt add middleware

--- server.js
+++ server.js
@@ -25,6 +25,63 @@ server.get('/users/:id', (req, res) => {
     });
 })
 
+server.post('/users', (req, res) => {
+    const { name } = req.body;
+    if (!name)
+    res.status(400).json({ errorMessage: "Give a name fool"});
+    userDb.insert({ name })
+    .then(posts => res.status(201).json({ name}))
+    .catch(err => res.status(400).json({ error: "Error Saving User"}))
+})
+
+server.delete('/users/:id', (req, res) => {
+    const { id } = req.params;
+    userDb.remove(req.params.id)
+    .then(user => {
+        if(user.length === 0) {
+            res.status(404).json({ message: "ID doesn't exist"});
+        }
+        res.status(200).json({message: "Gone forever"});
+    })
+    .catch(error => {
+        res.status(500).json({error: "Error Deleteing User"})
+    });
+})
+
+server.put('/users/:id', (req, res) => {
+    const { name } = req.body;
+    if(!name)
+    res.status(400).json({ errorMessage: "Provide name please"});
+    userDb.update(req.params.id, {name})
+    .then(users => {
+        if(users.length === 0) {
+            res.status(404).json({ message: "ID doesn't exist"});
+        }
+        res.status(200).json({name});
+    })
+    .catch(error => {
+        res.status(500).json({error: "Didnt work"})
+    });
+})
+
+server.get('/users/:id/posts', (req, res) => {
+    userDb.getUserPosts(req.params.id)
+    .then(posts => {
+        if(posts.length === 0) {
+            res.status(404).json({message:"ID doesnt exist"});
+        }
+        res.status(200).json(posts);
+    })
+    .catch(error => {
+        res.status(500).json({error: "User post cant be retrieved"})
+    });
+})
+
+
+
+
+//////////////////////////users
+
 server.get('/post', (req, res) => {
     postDb.get().then(posts => res.status(200).json(posts))
     .catch(err => res.status(500).json({ error: "No post here"})
@@ -33,16 +90,70 @@ server.get('/post', (req, res) => {
 
 server.get('/post/:id', (req, res) => {
     postDb.get(req.params.id)
+    .then(post => {
+        if(post.length === 0) {
+            res.status(404).json({ message: "ID doesn't exist"});
+        }
+        res.status(200).json(post);
+    })
+    .catch(error => {
+        res.status(500).json({ error: "User info could not be got"})
+    });
+})
+
+server.post('/post', (req, res) => {
+    const { userId, text } = req.body;
+    if (!userId || !text)
+    res.status(400).json({ errorMessage: "Give some text fool"});
+    postDb.insert({ userId, text })
+    .then(posts => res.status(201).json({userId, text}))
+    .catch(err => res.status(400).json({ error: "Error Saving post"}))
+})
+
+server.delete('/post/:id', (req, res) => {
+    const { id } = req.params;
+    postDb.remove(req.params.id)
+    .then(post => {
+        if(post.length === 0) {
+            res.status(404).json({message: "That ID dont exist"});
+        }
+        res.status(200).json({message: "Gone forever no turning back now"});
+    })
+    .catch(error => {
+        res.status(500).json({error: "Error Deleting Post"})
+    });
+})
+
+server.put('/post/:id', (req, res) => {
+    const { text } = req.body;
+    if(!text)
+    res.status(400).json({ errorMessage: "Provide text please"});
+    postDb.update(req.params.id, {text})
     .then(posts => {
         if(posts.length === 0) {
             res.status(404).json({ message: "ID doesn't exist"});
         }
-        res.status(200).json(posts);
+        res.status(200).json({text});
     })
     .catch(error => {
-        res.status(500).json({ error: "Post info could not be got"})
+        res.status(500).json({error: "Didnt work"})
     });
 })
+
+server.get('/post/:id/tags', (req, res) => {
+    postDb.getPostTags(req.params.id)
+    .then(tags => {
+        if(tags.length === 0) {
+            res.status(404).json({message:"ID doesnt exist"});
+        }
+        res.status(200).json(tags);
+    })
+    .catch(error => {
+        res.status(500).json({error: "Post tag cant be retrieved"})
+    });
+})
+
+/////////////////////////posts
 
 server.get('/tag', (req, res) => {
     tagDb.get().then(tags => res.status(200).json(tags))
@@ -63,6 +174,50 @@ server.get('/tag/:id', (req, res) => {
     });
 })
 
+server.post('/tag', (req, res) => {
+    const { tag } = req.body;
+    if (!tag)
+    res.status(400).json({ errorMessage: "Give me a tag fool"});
+    tagDb.insert({ tag })
+    .then(posts => res.status(201).json({tag}))
+    .catch(err => res.status(400).json({ error: "Error Saving tag"}))
+})
+
+server.delete('/tag/:id', (req, res) => {
+    const { id } = req.params;
+    postDb.remove(req.params.id)
+    .then(tags => {
+        if(tags.length === 0) {
+            res.status(404).json({message: "That ID dont exist"});
+        }
+        res.status(200).json({message: "Gone forever no turning back now"});
+    })
+    .catch(error => {
+        res.status(500).json({error: "Error Deleting tag"})
+    });
+})
+
+server.put('/tag/:id', (req, res) => {
+    const { tag } = req.body;
+    if(!tag)
+    res.status(400).json({ errorMessage: "Provide tag please"});
+    tagDb.update(req.params.id, {tag})
+    .then(tags => {
+        if(tags.length === 0) {
+            res.status(404).json({ message: "ID doesn't exist"});
+        }
+        res.status(200).json({tag});
+    })
+    .catch(error => {
+        res.status(500).json({error: "Didnt work"})
+    });
+})
+
+
+
+
+
+//////////////////////////tags
 
 
 
@@ -75,4 +230,5 @@ server.get('/tag/:id', (req, res) => {
 
 
 
-server.listen(8000, () => console.log("Api running here"));
+
+server.listen(9000, () => console.log("Api running here"));
