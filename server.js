c2193dc898003f8f84a167051f1084ec391f59d6
chainchompa <chainchompa@gmail.com>
2018-07-25 14:01:17 -0400

    able to view all posts and post tags

--- server.js
+++ server.js
@@ -33,6 +33,49 @@ server.get('/users/:id', (req, res) => {
   })
 })
 
+server.get('/posts', (req, res) => {
+  postDb
+  .get()
+  .then(posts => {
+    res.status(200).json({ posts });
+  })
+  .catch(error => {
+    res.status(500).json({ error: "Could not retreive posts"})
+  })
+})
+
+server.get('/posts/:id', (req, res) => {
+  const { id } = req.params;
+  userDb
+  .getUserPosts(id)
+  .then(post => {
+    if (post.length === 0) {
+      res.status(404).json({ message: "The post with the specified ID does not exist." })
+    } else {
+      res.status(200).json({ post })
+    }
+  })
+  .catch(error => {
+    res.status(500).json({ error: "The post information could not be retrieved." })
+  })
+})
+
+server.get('/posts/:id/tags', (req, res) => {
+  const { id } = req.params;
+  postDb
+  .getPostTags(id)
+  .then(post => {
+    if (post.length === 0) {
+      res.status(404).json({ message: "The post with the specified ID does not exist." })
+    } else {
+      res.status(200).json({ post })
+    }
+  })
+  .catch(error => {
+    res.status(500).json({ error: "The post's tags could not be retrieved." })
+  })
+})
+
 
 
 
