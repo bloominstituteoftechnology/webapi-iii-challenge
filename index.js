7c6a0ef9a9628de155f93750eff97ced4108d553
Simon <Simon_Freelance@hotmail.com>
2018-07-25 17:12:44 -0400

    Imported packages and APIs with middle setup

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const CREATED_CODE = 201;
 const BAD_REQUEST_CODE = 400;
 const NOT_FOUND_CODE = 404;
 const INTERNAL_SERVER_ERROR_CODE = 500;
+
+
+const userDb = require('./data/helpers/userDb');
+const postDb = require('./data/helpers/postDb');
+const tagDb = require('./data/helpers/tagDb');
+const express = require('express');
+
+const server = express();
+server.use(express.json());
