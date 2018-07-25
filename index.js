e21882604c58404d1edc4d23f4075da5232ec750
Kevin Tena <kevten09@gmail.com>
2018-07-25 17:23:11 -0500

    Added Post for users to create user

--- index.js
+++ index.js
@@ -27,4 +27,20 @@ server.get('/users/:id', async (req,res) => {
         res.status(500).json({ message: 'User could not be retrieved.'})
     }
 })
+
+server.post('/users/', async (req, res) => {
+    let user = req.body;
+    if(!('name') in user){
+        res.status(400).send({errorMessage: "Please provide a name for the user."});
+    }
+    
+    try {
+        const newUserId = await userDb.insert(user)
+        res.status(200).json(newUserId);
+    }
+    catch(err) {
+        res.status(500).json({ message: 'User could not be created.'})
+    }
+
+})
 server.listen(8000, () => console.log('API running on port 8000'));
