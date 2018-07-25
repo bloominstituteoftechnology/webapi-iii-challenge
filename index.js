7f416618eaeb3e970b2063b562dcd82912f00a76
Kevin <brackkevin@gmail.com>
2018-07-25 14:38:44 -0500

    Import databases and create 404 to catch rogue requests

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
+// initialize server
 const express = require("express");
-
 const server = express();
+
+// import databases
+const postDb = require("./data/helpers/postDb");
+const tagDb = require("./data/helpers/tagDb");
+const userDb = require("./data/helpers/userDb");
+
+// use middleware
+
+// create endpoints
+
+// catch all 404
+server.use(function(req, res) {
+  res.status(404).send("ERROR: FILE NOT FOUND");
+});
 
 server.listen(8000, () => console.log("\n ==API RUNNING ON PORT 8000== \n"));
