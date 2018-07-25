8e3986e14304336e7fde4da2df491a61a7d843cf
Mariana Cerda <raymond_grc@yahoo.com>
2018-07-25 15:01:51 -0500

    Add server variables

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-const postDb = require("./data/helpers/postDb.js")
+const express = require("express");
+const server = express();
+
+const postDb = require("./data/helpers/postDb.js");
+const tagDb = require("./data/helpers/tagDb.js");
+const UserDb = require("./data/helpers/userDb.js");
+
+
+server.use(express.json());
+
+
+server.listen(8000, () => {console.log("Server is listening on port 8000")})
