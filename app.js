b3af743c8aeb4ea282f59ff078fe6765e8091eb2
LBWright <lwright.js@gmail.com>
2018-07-25 15:47:26 -0700

    posts CRUD functionality completed. Needs tags

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const cors = require('cors');
 
 const userRoutes = require('./api/users');
 const postRoutes = require('./api/posts');
@@ -11,6 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(helmet());
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.status(200).json({ msg: 'API works' });
