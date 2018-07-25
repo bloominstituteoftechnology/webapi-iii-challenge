1523257dca63bbed6e1902f3754609a265dfbaec
chainchompa <chainchompa@gmail.com>
2018-07-25 16:58:08 -0400

    able to add new user, post, and tag

--- server.js
+++ server.js
@@ -60,6 +60,18 @@ server.get('/posts/:id', (req, res) => {
   })
 })
 
+server.get('/tags', (req, res) => {
+  tagDb
+  .get()
+  .then(tags => {
+    res.status(200).json({ tags });
+  })
+  .catch(error => {
+    res.status(500).json({ error: "Could not retreive tags"})
+  })
+})
+
+
 server.get('/posts/:id/tags', (req, res) => {
   const { id } = req.params;
   postDb
@@ -76,7 +88,52 @@ server.get('/posts/:id/tags', (req, res) => {
   })
 })
 
+server.post('/users', (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    res.status(400).json({ errorMessage: "Please provide a name for the user." })
+    return;
+  }
+  userDb
+  .insert({ name })
+  .then(user => {
+    res.status(200).json({ user })
+  })
+  .catch(error => {
+    res.status(500).json({ error: "There was an error while saving the user to the database" })
+  })
+})
 
+server.post('/posts', (req, res) => {
+  const { userId, text } = req.body;
+  if (!text || !userId) {
+    res.status(400).json({ errorMessage: "Please provide a user ID and text for the post." })
+    return;
+  }
+  postDb
+  .insert({ userId, text })
+  .then(post => {
+    res.status(200).json({ post })
+  })
+  .catch(error => {
+    res.status(500).json({ error: "There was an error while saving the post to the database" })
+  })
+})
 
+server.post('/tags', (req, res) => {
+  const { tag } = req.body;
+  if (!tag) {
+    res.status(400).json({ errorMessage: "Please provide a tag." })
+    return;
+  }
+  tagDb
+  .insert({ tag })
+  .then(tag => {
+    res.status(200).json({ tag })
+  })
+  .catch(error => {
+    res.status(500).json({ error: "There was an error while saving the tag to the database" })
+  })
+})
 
 server.listen(8000, () => console.log('API running on port 8000'));
