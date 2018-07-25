9c256ff8324e536634d8d8a70c99156722ef3d13
cmok4290 <cmok4290@gmail.com>
2018-07-25 16:38:50 -0400

    added post get

--- index.js
+++ index.js
@@ -15,4 +15,41 @@ server.get('/', (req, res) => {
     res.status(200).send('Server running...');
 });
 
+server.get('/api/posts', async (req, res) => {
+    try {
+        const posts = await postDb.get();
+        res.status(200).json(posts);
+    } catch(err) {
+        res.status(500).json({error: 'The posts information could not be retrieved.'});
+    }
+});
+
+server.get('/api/posts/:id', async (req, res) => {
+    try {
+        const {id} = req.params;
+        const post = await postDb.get(id);
+        if(post) {
+            res.status(200).json(post);
+        } else {
+            res.status(404).json({error: 'The post with the specified ID does not exist.'});
+        }
+    } catch(err) {
+        res.status(500).json({error: 'The post information could not be retrieved.'});
+    }
+});
+
+server.get('/api/postTags/:id', async (req, res) => {
+    try {
+        const {id} = req.params;
+        const post = await postDb.getPostTags(id);
+        if(post[0]) {
+            res.status(200).json(post);
+        } else {
+            res.status(404).json({error: 'The post tag with the specified ID does not exist.'});
+        }
+    } catch(err) {
+        res.status(500).json({error: 'The post tag information could not be retrieved.'});
+    }
+});
+
 server.listen(port, () => console.log(`Server running @ localhost:${port}`));
