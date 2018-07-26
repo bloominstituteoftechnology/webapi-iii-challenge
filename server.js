120dd5fc455e508c27e6d28ea8029cc5cedf1f5f
CLN <cln@Sags-MacBook-Pro.local>
2018-07-26 18:43:35 -0400

    ,.

--- server.js
+++ server.js
@@ -7,22 +7,92 @@ const userDb = require('./data/helpers/userDb.js');
 const server = express();
 server.use(express.json());
 
-server.get('/yo', (req, res) => {
+server.get('/', (req, res) => {
   res.send('Hello World');
 });
 
 server.get('/api/posts', (req, res) => {
- 
+
   postDb.get()
     .then(posts => {
       res.status(200).json(posts);
     })
     .catch(error => {
       res.status(500)
-        .json({ error: "The posts information could not be retrieved." })
+        .json({ error: "The posts could not be retrieved." })
     })
 
+});
+server.get('/api/posts/:id', (req, res) => {
+  const postId = req.params.id;
+  postDb.get(postId)//req.params.id
+    .then(posts => {
+      if (posts.length === 0) {
+        res.status(404)
+          .json({ error: "missing post." })
+      }
+      res.status(200).json(posts)
+    }).catch(error => {
+      res.status(500)
+        .json({ error: "The posts with the specified postid do not exist." })
+    });
 
+})
+server.get('/api/poststags/:id', (req, res) => {
+  const postId = req.params.id;
+  postDb.getPostTags(postId)//req.params.id
+    .then(posts => {
+      if (posts.length === 0) {
+        res.status(404)
+          .json({ error: "missing post tags." })
+      }
+      res.status(200).json(posts)
+    }).catch(error => {
+      res.status(500)
+        .json({ error: "The posts with the specified postid do not exist." })
+    });
+
+})
+
+
+server.get('/api/users', (req, res) => {
+
+  userDb.get()
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(error => {
+      res.status(500)
+        .json({ error: "The users information could not be retrieved." })
+    })
+
+});
+server.get('/api/userposts/:id', (req, res) => {
+  const userId = req.params.id;
+  userDb.getUserPosts(userId)//req.params.id
+    .then(posts => {
+      if (posts.length === 0) {
+        res.status(404)
+          .json({ error: "missing user." })
+      }
+      res.status(200).json(posts)
+    }).catch(error => {
+      res.status(500)
+        .json({ error: "The posts with the specified userID do not exist." })
+    });
+
+})
+
+server.get('/api/tags', (req, res) => {
+
+  tagDb.get()
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(error => {
+      res.status(500)
+        .json({ error: "The users information could not be retrieved." })
+    })
 
 });
 
