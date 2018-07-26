c91f5c4f750b25ee850c6d07ffaad0b040ed5235
CLN <cln@Sags-MacBook-Pro.local>
2018-07-26 18:45:15 -0400

    Added functionality for adding user,.

--- server.js
+++ server.js
@@ -24,8 +24,8 @@ server.get('/api/posts', (req, res) => {
 
 });
 server.get('/api/posts/:id', (req, res) => {
-  const postId = req.params.id;
-  postDb.get(postId)//req.params.id
+  const id = req.params.id;
+  postDb.get(id)//req.params.id
     .then(posts => {
       if (posts.length === 0) {
         res.status(404)
@@ -38,7 +38,22 @@ server.get('/api/posts/:id', (req, res) => {
     });
 
 })
-server.get('/api/poststags/:id', (req, res) => {
+server.delete('/api/posts/:id', (req, res) => {
+  const { id } = req.params;
+  // posts = posts.filter(p => p.id != id)
+  postDb.remove(id)
+    .then(posts => {
+      if (posts === 0) {
+        res.status(404)
+        .json({ error: "The post with the specified ID does not exist." })
+      }
+      res.status(200).json(posts)
+    }).catch(error => {
+      res.status(500)
+        .json({ error: "error 2." })
+    });
+})
+server.get('/api/posttags/:id', (req, res) => {
   const postId = req.params.id;
   postDb.getPostTags(postId)//req.params.id
     .then(posts => {
@@ -67,6 +82,49 @@ server.get('/api/users', (req, res) => {
     })
 
 });
+server.get('/api/users/:id', (req, res) => {
+  const id = req.params.id;
+  userDb.get(id)
+    .then(posts => {
+      if (posts.length === 0) {
+        res.status(404)
+          .json({ error: "error1." })
+      }
+      res.status(200).json(posts)
+    }).catch(error => {
+      res.status(500)
+        .json({ error: "missing user." })
+    });
+
+});
+server.post('/api/users', (req, res) => {
+  user = req.body
+  userDb.insert(user)
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(error => {
+      res.status(500)
+        .json({ error: "User not added." })
+    })
+
+});
+server.delete('/api/users/:id', (req, res) => {
+  const { id } = req.params;
+  // posts = posts.filter(p => p.id != id)
+  userDb.remove(id)
+    .then(posts => {
+      if (posts === 0) {
+        res.status(404)
+        .json({ error: "The user with the specified ID does not exist." })
+      }
+      res.status(200).json(posts)
+    }).catch(error => {
+      res.status(500)
+        .json({ error: "error 2." })
+    });
+})
+
 server.get('/api/userposts/:id', (req, res) => {
   const userId = req.params.id;
   userDb.getUserPosts(userId)//req.params.id
