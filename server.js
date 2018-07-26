3c3259c688904547c1b4224bd972fe57ba85f2b6
Vu Cao <cdhvu@yahoo.com>
2018-07-26 13:32:51 -0700

    Created endpoints for PUT users, posts, and tags

--- server.js
+++ server.js
@@ -11,15 +11,17 @@ server.use(uppercaseTag);
 
 //middleware to capitalize tags
 function uppercaseTag(req, res, next) {
-    if (req.method === 'GET' && req.url === '/tags') {
+    if (req.method === 'GET' && req.url.includes('/tags')) {
         let tags = res.json;
-
+        
         res.json = function (data) {
             // data(or argument[0]) contains the response body
-            data.forEach(tag => tag.tag = tag.tag.toUpperCase());
+            data.length > 1 
+            ? data.forEach(tag => tag.tag = tag.tag.toUpperCase())
+            : data.tag = data.tag.toUpperCase();
             tags.apply(res, arguments);
         }
-    } else if (req.method === 'GET' && req.url.includes('/posts/')) {
+    } else if (req.method === 'GET' && !req.url.includes('users') && req.url.includes('/posts')) {
         let tags = res.json;
 
         res.json = function (data) {
@@ -33,7 +35,7 @@ function uppercaseTag(req, res, next) {
 }
 
 //endpoint for GET users
-server.get('/users/', async (req, res) => {
+server.get('/users', async (req, res) => {
     try {
         const response = await userDb.get();
         res.status(200).json(response);
@@ -59,7 +61,7 @@ server.get('/users/:id', async (req, res) => {
 })
 
 //endpoint for GET posts
-server.get('/posts/', async (req, res) => {
+server.get('/posts', async (req, res) => {
     try {
         const response = await postDb.get();
         res.status(200).json(response);
@@ -81,7 +83,7 @@ server.get('/posts/:id', async (req, res) => {
 })
 
 //endpoint for GET tags
-server.get('/tags/', async (req, res) => {
+server.get('/tags', async (req, res) => {
     try {
         const response = await tagDb.get();
         res.status(200).json(response);
@@ -132,7 +134,7 @@ server.get('/posts/:id/tags', async (req, res) => {
 })
 
 //endpoint for POST user
-server.post('/users/', async (req, res) => {
+server.post('/users', async (req, res) => {
     if (!req.body.name) {
         return res.status(400).send({ message: 'Please provide name of user.' })
     }
@@ -206,7 +208,6 @@ server.delete('/posts/:id', async (req, res) => {
     }
 })
 
-
 //endpoint for DELETE tag
 server.delete('/users/:id', async (req, res) => {
     const id = req.params.id;
@@ -223,5 +224,70 @@ server.delete('/users/:id', async (req, res) => {
         res.status(500).send({ message: "The tag could not be removed.", error: error.message })
     }
 })
+
+//endpoint for PUT user
+server.put('/users/:id', async (req, res) => {
+    if (!req.body.name) {
+        return res.status(400).send({ message: 'Please provide name of user.' })
+    }
+
+    const id = req.params.id;
+    const user = req.body;
+    try {
+        const response = await userDb.update(id, user);
+        if (response===0) {
+            res.status(404).send({ message: "The user with the specified ID does not exist." });
+        } else {
+            const newUser = await userDb.get(id);
+            res.status(200).json(newUser);
+        }
+    } catch (error) {
+        res.status(500).send({ message: "This user could not be modified.", error: error.message })
+    }
+})
+
+//endpoint for PUT post
+server.put('/posts/:id', async (req, res) => {
+    if ((!req.body.text && !req.body.userId)) {
+        return res.status(400).send({ message: "Please provide userId and text for the post." })
+    }
+
+    const id = req.params.id;
+    const post = req.body;
+
+    try {
+        const response = await postDb.update(id, post);
+        if (response===0) {
+            res.status(404).send({ message: "The post with the specified ID does not exist." });
+        } else {
+            const newPost = await postDb.get(id);
+            res.status(200).json(newPost);
+        }
+    } catch (error) {
+        res.status(500).send({ message: "This post could not be modified.", error: error.message })
+    }
+})
+
+// endpoint for PUT tag
+server.put('/tags/:id', async (req, res) => {
+    if (!req.body.tag) {
+        return res.status(400).send({ message: 'Please provide name of tag.' })
+    }
+
+    const id = req.params.id;
+    const tag = req.body;
+    try {
+        const response = await tagDb.update(id, tag);
+        if (response===0) {
+            res.status(404).send({ message: "The tag with the specified ID does not exist." });
+        } else {
+            const newTag = await tagDb.get(id);
+            res.status(200).json(newTag);
+        }
+    } catch (error) {
+        res.status(500).send({ message: "This tag could not be modified.", error: error.message })
+    }
+})
+
 
 server.listen(8000, () => console.log('\n=== API Running... ===\n'))
