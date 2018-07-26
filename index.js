092719ebfc4a8bc267245ef716120c4b1ceb2c60
Randy Calderon <randycweb@gmail.com>
2018-07-26 15:33:01 -0700

    Added CRUD for tags

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require('express')
+const helmet = require('helmet')
+const morgan = require('morgan')
+
+// Run Server
+
 const server = express();
+
+//imported helpers
 const postDb = require('./data/helpers/postDb');
 const tagDb = require('./data/helpers/tagDb');
 const userDb = require('./data/helpers/userDb');
 
+// Parse objects to json
+
 server.use(express.json())
+
+
+//security
+server.use(helmet())
+
+//logger 
+server.use(morgan('dev'))
 
 //Users
 
@@ -131,4 +147,61 @@ server.delete('/posts/:id', async (req, res) => {
     }
 })
 
+
+//tags
+
+server.get('/tags', async (req, res) => {
+    try {
+        const getTags = await tagDb.get()
+        res.status(200).json(getTags)
+    } catch(err) {
+        res.status(500).json({error: "The post information couldn't be retrieved"})
+    }
+})
+
+server.post('/tags', async (req, res) => {
+    try {
+        const { tag } = req.body
+        if ( tag === undefined) {
+            res.status(400).json({error: "Please provide a name for the user"}) 
+        }
+        const tagInsert = await tagDb.insert({ tag })
+    } catch(err) {
+        res.status(500).json({ error: "There was an error saving the user to the database"})
+    }
+})
+
+server.get('/tags/:id', async (req, res) => {
+    try {
+        const id = req.params.id
+        const getTagId = await tagDb.get(id)
+        res.status(200).json(getTagId)
+    } catch(err) {
+        res.status(400).json({ error: "The tag with this Id doesn't exist"})
+    }
+})
+
+server.put('/tags/:id', async (req, res) => {
+    try {
+        const { tag } = req.body
+        const id = req.params.id
+        const updateTag = await tagDb(id, { tag })
+        res.send(200).json(updateTag)
+    } catch(err) {
+        res.status(400)
+    }   
+})
+
+server.delete('tags/:id', async (req, res) => {
+    try {
+        const id = req.params.id
+        if(removeTag === 0) {
+            res.status(404)
+        }
+        const removeTag = await tagDb.remove(id)
+        res.status(200).json(removeTag)
+    } catch(err) {
+        res.status(500).json({ error: "The post couldn't be removed"})
+    }
+})
 server.listen(8000);
