44eaeee049327a5768506577a176d6753bc9643f
cmok4290 <cmok4290@gmail.com>
2018-07-26 19:11:24 -0400

    fixed switch in error handler

--- routers/indexRouter.js
+++ routers/indexRouter.js
@@ -17,18 +17,22 @@ router.use((err, req, res, next) => {
             res
                 .status(400)
                 .send({success: false, data: undefined, error: 'REQUEST INFORMATION ERROR'});
+            break;
         case 403:
             res
                 .status(403)
                 .send({success: false, data: undefined, error: 'Balance is the key, making things even is the secret to success.'});
+            break;
         case 404:
             res
                 .status(404)
                 .send({success: false, data: undefined, error: 'ID ERROR'});
+            break;
         default:
             res
                 .status(500)
                 .send({success: false, data: undefined, error: 'RESOURCE UNAVAILABLE'});
+            break;
     }
 });
 
