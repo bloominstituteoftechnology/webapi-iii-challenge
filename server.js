035f753e3c7a02025dd84ea5919f4e678d534e5f
Mark Hong <m4rkh0ng@gmail.com>
2018-07-26 17:38:53 -0400

    worked with the Node III challenge to add auth with a specific message during even seconds (but also removed this from applying to all routes, otherwise, that would be kind of difficult to use

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
 
 const postDb = require('./data/helpers/postDb.js');
 const tagDb = require('./data/helpers/tagDb.js');
@@ -8,13 +11,57 @@ const server = express();
 
 server.use(express.json());
 
+//adding middleware
+function atGate(req, res, next) {
+    console.log(`At the gate, about to be eaten`);
+  
+    next();
+}
+
+server.use(atGate);
+
+function auth(req, res, next) {
+    // changing auth depending on the URL or if it applies globally:
+
+    // if (req.url === '/mellon') {
+    // if(Math.floor(Date.now()/1000) % 2 === 0) {
+    if(Math.floor(Date.now()/1000) % 2 === 0 && req.url === '/mellon') {
+      res.status(403).json({ error: 'Balance is the key, making things even is the secret to success' });  
+      return;
+    } else {
+      next();
+    }
+}
+
+server.get('/mellon', auth, (req, res) => {
+    console.log('Gate opening...');
+    console.log('Inside and safe');
+    res.send('Welcome Traveler!');
+});
+
+server.use(cors());
+server.use(helmet());
+server.use(morgan('short'));
+
+function logger (req, res, next) {
+    console.log(
+        `[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get(
+          'Origin'
+        )}`
+    );
+
+    next();
+}
+
+server.use(logger);
+
 // GET | Returns the stored posts / tags / users
 
 server.get('/', (req, res) => {
     res.send({ hello: 'world', project: "blog" });
 });
 
-server.get('/users/', (req, res) => {
+server.get('/users/', auth, (req, res) => {
     userDb.get()
     .then(response => {
         res.status(200).json(response)
@@ -101,7 +148,6 @@ server.get('/tags/:id', (req, res) => {
         .status(500).json({ error: `Couldn't get tag.`})
     })
 })
-
 
 // 
 
