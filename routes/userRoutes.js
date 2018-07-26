cb1b28952de6138cee25b318417bc010b3b826e3
Arthur Pisakhov <arthurpisakhov1@gmail.com>
2018-07-26 17:20:14 -0400

    PUT/DELETE/POST  for posts.

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -39,12 +39,15 @@ router.post('/', async (req, res) => { // POST user
 
 router.put('/:id', async (req, res) => { // PUT user/:id
     if (!req.body || !req.body.name)
-        res.status(400).json({ error: 'Please provide name and bio for the user.'})
+    res.status(400).json({ message: 'Please provide a name for the user.' })
+
+    const id = Number(req.params.id)
     const { name } = req.body
+
     try {
-        const usedID = await userDb.update(req.params.id, { name });
-        if (userID > 0)
-            res.status(200).json({ id, name })
+        const numberOfUpdatedUsers = await userDb.update(id, { name })
+        if (numberOfUpdatedUsers > 0)
+        res.status(200).json({ id, name })
     } catch (error) {
         res.status(500).json({ error: 'Unable to update user.' })
     }
