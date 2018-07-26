0107bfe2657d62d676a6b5cb7689d305b22227f2
Simon <Simon_Freelance@hotmail.com>
2018-07-26 19:13:40 -0400

    Error handling for the posts have been completed

--- api/postRoutes.js
+++ api/postRoutes.js
@@ -13,56 +13,94 @@ router.get('/', async (req, res) => {
     } catch (err) {
     }
 })
-router.get('/:id', async (req, res) => {
+router.get('/:id', async (req, res, next) => {
     try {
     const { id } = req.params;
     const post = await postDb.get(id);
+    console.log(post);
     if(post === undefined) {
-        
+        next({
+            code: codes.NOT_FOUND,
+            message: "There is no post with that id"
+        })
+        return;
     }
     res.status(codes.OK).json(post);
     } catch (err) {
+        next({
+            code: codes.INTERNAL_SERVER_ERROR,
+            message: "Was not able to get post with that id at this time"
+        })
     }
 })
 
-router.post('/', async (req, res) => {
+router.post('/', async (req, res, next) => {
     try {
         const { userId, text } = req.body;
         if(userId === undefined || text === undefined) {
+            next({
+                code: codes.BAD_REQUEST,
+                message: "Either the text, userId or both are not filled out please put that information"
+            })
+            return;
         }
         const postResponse = postDb.insert(req.body);
         res.status(codes.OK).json(postResponse);
     }
     catch (err) {
-        
+        next({
+            code: codes.INTERNAL_SERVER_ERROR,
+            message: "Was not able to create the post"
+        })
     }
 })
-router.put('/:id', async(req, res) => {
+router.put('/:id', async(req, res, next) => {
     try {
         const id = req.params.id;
         const { userId, text } = req.body;
 
         if(userId === undefined || text === undefined) {
+            next({
+                code: codes.BAD_REQUEST,
+                message: "Put the text and userId in order to update the post"
+            })
+            return;
         }
         const updateResponse = await postDb.update(id, req.body);
         if(updateResponse === 0) {
+            next({
+                code: codes.NOT_FOUND,
+                message: "The post with that id could not be found to be updated."
+            })
+            return;
         }
         res.status(codes.OK).json(updateResponse);
     }
     catch(err) {
-        
+        next({
+            code: codes.INTERNAL_SERVER_ERROR,
+            message: "Could not update post"
+        })
     }
 });
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', async (req, res, next) => {
     try {
         const id = req.params.id
         const deleteResponse = await postDb.remove(id);
         if(deleteResponse === 0) {
+            next({
+                code: codes.NOT_FOUND,
+                message: "The post with that id could not be found to be deleted"
+            })
+            return;
         }
         res.status(codes.OK).json(deleteResponse);
     }
     catch (err) {
-        
+        next({
+            code: codes.INTERNAL_SERVER_ERROR,
+            message: "Could not delete post"
+        })
     }
 });
 
