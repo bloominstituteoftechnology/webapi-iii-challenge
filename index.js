e3c405dae216f4a0bbf10b0ec739a45806d59133
Randy Calderon <randycweb@gmail.com>
2018-07-26 06:39:50 -0700

    Added get for userDb

--- index.js
+++ index.js
@@ -6,29 +6,66 @@ const userDb = require('./data/helpers/userDb');
 
 server.use(express.json())
 
-server.get('/posts/:id', async (req, res) => {
-    try {
-        const id = req.params.id
-        console.log(req.params.id)
-        const posts = await postDb.get(id)
-        res.status(200).json(posts);
-    } catch(err) {
+//Users
 
+server.get('/users', async (req, res) => {
+    try {
+        const users = await userDb.get()
+        res.status(200).json(users)
+    } catch(err) {
+        res.status(400).json({error: "Users not found"})
     }
 })
 
-server.get('/post/tag/:id', async (req,res) => {
+server.get('/users/:id', async(req, res) => {
     try {
         const id = req.params.id
-        console.log(id)
+        const userId = await userDb.getUserPosts(id)
+        if (userId === undefined) {
+            res.status(404).json({error: "The user with that id can't be found."})
+        }
+        res.status(200).json(userId)
+    } catch(err) {
+        res.status(404).json({error: 'Users not found.'})
+    }
+})
+
+// Posts 
+
+server.get('/posts', async (req, res) => {
+    try {
+        const posts = await postDb.get()
+        res.status(200).json(posts)
+    } catch(err) {
+        res.status(404).json({error: 'Post could not be found.'})
+    }
+})
+
+
+server.get('/posts/:id', async (req,res) => {
+    try {
+        const id = req.params.id
         const postTag = await postDb.getPostTags(id)
         res.status(200).json(postTag)
     } catch(err) {
-
+        res.status(404).json({ error: 'The post with that id could not be found.'})
     }
 })
 
+server.post('/posts', async (req, res) => {
+    try {
+        const { userId, text } = req.body;
+        if (userId === undefined || text === underfined) {
+            res.status(400).json({error: "UserId or text is missing"})
+        }
+        const post = await postDb.insert(req.body);
+        res.status(200).json(post)
+    } catch(err) {
+        res.status(500).json({error: "Error posting post."})
+    }
+})
 
+// Tags
 
 
 server.listen(8000);
