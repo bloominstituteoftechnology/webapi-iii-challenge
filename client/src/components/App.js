3fdf9c017a982e4e2ecb7cc3267209d2092157ab
Kamry Bowman <kamry.bowman@gmail.com>
2018-07-26 17:05:44 -0600

    Got users displaying

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,17 +1,15 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import axios from 'axios';
-import 'babel-polyfill';
-
 
 export default class App extends Component {
   constructor(props) {
     super(props);
-    this.url =  'http://localhost:8000/api';
+    this.url = 'http://localhost:8000/api';
     this.state = {
       posts: [],
       users: [],
       currentUser: undefined,
-    }
+    };
     this.fetchData = this.fetchData.bind(this);
   }
 
@@ -19,24 +17,30 @@ export default class App extends Component {
     this.fetchData();
   }
 
-  async fetchData() {
-    try {
-      const users = await axios.get(`${this.url}/users/`);
-      this.setState({ users });
-    } catch (err) {
-      console.log(err);
-    }
+  fetchData() {
+    axios
+      .get('http://localhost:8000/api/users')
+      .then((res) => {
+      console.log(res);
+      this.setState({ users: res.data });
+    }).catch((err) => { 
+      console.log(err) 
+    });
   }
-  
+
   render() {
-    const users = this.state.posts.map( user => (
-    <li key={ user.userId }>
-      <a>{ user.name }</a>
-    </li>
+    const users = this.state.users.map(user => (
+      <li key={user.userId}>
+        <a>
+          {user.name}
+        </a>
+      </li>
     ));
     return (
       <div>
-        <h2>Select a user</h2>
+        <h2>
+Select a user
+        </h2>
         {users}
       </div>
     );
