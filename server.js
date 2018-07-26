23319ae2a2b7414a5070b60b1ebdfb9d2ab4d09a
Gabriel Duquette <gabrielduquette@gmail.com>
2018-07-26 00:12:11 -0400

    error messages back in place, users and posts reformatted and tested, length checks added

--- server.js
+++ server.js
@@ -12,24 +12,6 @@ server.use(cors());
 server.use(helmet());
 server.use(morgan('dev'));
 
-const usersNotRetrieved = "The users could not be retrieved.";
-const userNotRetrieved = "The user could not be retrieved.";
-const userNotFound = "The user with the specified ID does not exist.";
-const userPostsNotRetrieved = "The user's posts could not be retrieved.";
-const noPosts = "This user has no posts."
-const missingName = "Please provide a name for this user."
-const userSaveError = "There was an error while saving the user to the database.";
-const userDeleteError = "There was an error while deleting the user from the database.";
-const userEditError = "There was an error while saving the edited user to the database.";
-
-const postsNotRetrieved = "The posts could not be retrieved.";
-const postNotRetrieved = "The post could not be retrieved.";
-const postNotFound = "The post with the specified ID does not exist.";
-const postTagsNotRetrieved = "The post's tags could not be retrieved.";
-const noTags = "This post has no tags."
-const missingIDOrText = "Please provide an ID and/or text for this post."
-const postSaveError = "There was an error while saving the post to the database.";
-
 // ============ Users ============
 
 server.get('/api/users', async (req, res) => {
@@ -37,7 +19,7 @@ server.get('/api/users', async (req, res) => {
     const allUsers = await users.get();
     res.status(200).json(allUsers);
   } catch (error) {
-    res.status(500).json({ message: usersNotRetrieved, error: error.message });
+    res.status(500).json({ message: "Users could not be retrieved.", error: error.message });
   }
 });
 
@@ -45,31 +27,29 @@ server.get('/api/users/:id', async (req, res) => {
   try {
     const user = await users.get(req.params.id);
     if (user === undefined) {
-      res.status(404).json({ message: userNotFound, error: error.message });
-      return;
+      return res.status(404).json({ message: "User does not exist." });
     }
     res.status(200).json(user);
   } catch (error) {
-    res.status(500).json({ message: userNotRetrieved, error: error.message });
+    res.status(500).json({ message: "User could not be retrieved.", error: error.message });
   }
 });
 
 server.get('/api/users/:id/posts', async (req, res) => {
   try {
     const posts = await users.getUserPosts(req.params.id);
-    if (posts.length === 0) {
-      res.status(404).json({ message: noPosts, error: error.message });
-      return;
-    }
     res.status(200).json(posts);
   } catch (error) {
-    res.status(500).json({ message: userPostsNotRetrieved, error: error.message });
+    res.status(500).json({ message: "User's posts could not be retrieved.", error: error.message });
   }
 });
 
 server.post('/api/users', async (req, res) => {
   if (!req.body.name) {
-    res.status(400).json({ message: missingName, error: error.message });
+    res.status(400).json({ message: "Please enter a name." });
+  }
+  if (req.body.name.length > 80) {
+    res.status(400).json({ message: "Name must be less than 80 characters." });
   }
   try {
     const { id } = await users.insert(req.body);
@@ -77,31 +57,33 @@ server.post('/api/users', async (req, res) => {
       const newUser = await users.get(id);
       res.status(201).json(newUser);
     } catch (error) {
-      res.status(404).json({ message: userNotFound, error: error.message });
+      res.status(404).json({ message: "User does not exist.", error: error.message });
     }
   } catch (error) {
-    res.status(500).json({ message: userSaveError, error: error.message });
+    res.status(500).json({ message: "An error occurred while saving the user to the database.", error: error.message });
   }
 });
 
 server.put('/api/users/:id', async (req, res) => {
   if (!req.body.name) {
-    res.status(400).json({ message: missingName, error: error.message });
+    res.status(400).json({ message: "Please enter a name." });
+  }
+  if (req.body.name.length > 80) {
+    res.status(400).json({ message: "Name must be less than 80 characters." });
   }
   try {
     await users.update(req.params.id, req.body);
     try {
       const user = await users.get(req.params.id);
       if (user === undefined) {
-        res.status(404).json({ message: userNotFound, error: error.message });
-        return;
+        res.status(404).json({ message: "User does not exist." });
       }
       res.status(200).json(user);
     } catch (error) {
-      res.status(500).json({ message: userNotRetrieved, error: error.message });
+      res.status(500).json({ message: "User could not be retrieved.", error: error.message });
     }
   } catch (error) {
-    res.status(500).json({ message: userEditError, error: error.message });
+    res.status(500).json({ message: "An error occurred while saving the edited user to the database.", error: error.message });
   }
 });
 
@@ -109,12 +91,11 @@ server.delete('/api/users/:id', async (req, res) => {
   try {
     const user = await users.remove(req.params.id);
     if (user === 0) {
-      res.status(404).json({ message: userNotFound, error: error.message });
-      return;
+      res.status(404).json({ message: "User does not exist." });
     }
     res.status(200).json(user);
   } catch (error) {
-    res.status(500).json({ message: userDeleteError, error: error.message });
+    res.status(500).json({ message: "An error occurred while deleting the user from the database.", error: error.message });
   }
 });
 
@@ -125,20 +106,16 @@ server.get('/api/posts', async (req, res) => {
     const allPosts = await posts.get();
     res.status(200).json(allPosts);
   } catch (error) {
-    res.status(500).json({ message: postsNotRetrieved, error: error.message });
+    res.status(500).json({ message: "Posts could not be retrieved.", error: error.message });
   }
 });
 
 server.get('/api/posts/:id', async (req, res) => {
   try {
     const post = await posts.get(req.params.id);
-    if (post === undefined) {
-      res.status(404).json({ message: postNotFound, error: error.message });
-      return;
-    }
     res.status(200).json(post);
   } catch (error) {
-    res.status(500).json({ message: postNotRetrieved, error: error.message });
+    res.status(500).json({ message: "Post could not be retrieved.", error: error.message });
   }
 });
 
@@ -146,18 +123,22 @@ server.get('/api/posts/:id/tags', async (req, res) => {
   try {
     const tags = await posts.getPostTags(req.params.id);
     if (tags.length === 0) {
-      res.status(404).json({ message: noTags, error: error.message });
-      return;
+      res.status(404).json({ message: "Post has no tags or does not exist." });
     }
     res.status(200).json(tags);
   } catch (error) {
-    res.status(500).json({ message: postTagsNotRetrieved, error: error.message });
+    res.status(500).json({ message: "Post tags could not be retrieved.", error: error.message });
   }
 });
 
 server.post('/api/posts', async (req, res) => {
-  if (!req.body.userId || !req.body.text) {
-    res.status(400).json({ error: missingIDOrText, message: error.message });
+  if (!req.body.text || !req.body.userId) {
+    res.status(400).json({ message: "Please enter some text and/or a user ID." });
+    return;
+  }
+  if (req.body.text.length > 128) {
+    res.status(400).json({ message: "Text must be less than 128 characters." });
+    return;
   }
   try {
     const { id } = await posts.insert(req.body);
@@ -165,11 +146,46 @@ server.post('/api/posts', async (req, res) => {
       const newPost = await posts.get(id);
       res.status(201).json(newPost);
     } catch (error) {
-      res.status(500).json({ message: postNotFound, error: error.message });
+      res.status(404).json({ message: "Post does not exist." });
     }
   } catch (error) {
-    res.status(500).json({ message: postSaveError, error: error.message });
+    res.status(500).json({ message: "An error occurred while saving the post to the database.", error: error.message });
   }
-})
+});
+
+server.put('/api/posts/:id', async (req, res) => {
+  if (!req.body.text || !req.body.userId) {
+    res.status(400).json({ message: "Please enter some text and/or a user ID." });
+  }
+  if (req.body.text.length > 128) {
+    res.status(400).json({ message: "Text must be less than 128 characters." });
+  }
+  try {
+    await posts.update(req.params.id, req.body);
+    try {
+      const post = await posts.get(req.params.id);
+      if (post === undefined) {
+        res.status(404).json({ message: "Post does not exist." });
+      }
+      res.status(200).json(post);
+    } catch (error) {
+      res.status(500).json({ message: "Post could not be retrieved.", error: error.message });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "An error occurred while saving the edited post to the database.", error: error.message });
+  }
+});
+
+server.delete('/api/posts/:id', async (req, res) => {
+  try {
+    const post = await posts.remove(req.params.id);
+    if (post === 0) {
+      res.status(404).json({ message: "Post does not exist." });
+    }
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(500).json({ message: "An error occurred while deleting the post from the database.", error: error.message });
+  }
+});
 
 server.listen(8000, () => console.log('\n=== API running ===\n'));
