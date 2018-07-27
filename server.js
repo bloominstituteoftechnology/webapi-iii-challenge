0371c54cef0e872a59d2a72a7af29e8e928bf12c
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2018-07-27 11:59:07 +0100

    added Database functions and third party middleware

--- server.js
+++ server.js
@@ -2,10 +2,24 @@ const express = require('express');
 const helmet = require("helmet")
 const morgan = require('morgan')
 
+//import Db models
+const postDb = require('./data/helpers/postDb')
+const tagDb = require('./data/helpers/tagDb')
+const userDb = require('./data/helpers/userDb')
+
+//instance of express server
 const server = express()
 
+// add json middleware
+server.use(express.json())
+
+// add helmet  and morgan middleware
+server.use(helmet())
+server.use(morgan('dev'))
+
+
 server.get('/', (req, res) =>{
-    res.send('API IS WORKING')
+    res.send('Welcome to API for POST, TAGS AND USERS')
 })
 
 //POSTS API CRUD
