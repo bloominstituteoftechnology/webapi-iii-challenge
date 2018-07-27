f89637380a12e6a7e344fd8abd70a93e4fd2d5aa
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2018-07-27 13:46:54 +0100

    CRUD TAGs API implemented sucessfully

--- server.js
+++ server.js
@@ -26,12 +26,26 @@ server.get('/', (req, res) =>{
 server.get('/posts',(req, res) => {
     const post = postDb.get()
     post
-        .then(post => {
-            res.status(200).json(post)
+        .then(response => {
+            res.status(200).json(response)
         })
         .catch(err => {
             res.status(500).json({error : `Sorry $(err) Has occurred`})
         })
+
+})
+
+server.get('/posts/:id',(req, res) => {
+    const { id } = req.params
+        const taggedPosts = postDb.getPostTags(id)
+        taggedPosts
+            .then( response => {
+                responds.message = 'Post Sucessful'
+                res.status(201).json(response)
+            })
+            .catch(err => {
+                res.status(500).json( {error : `Post wasn't sucessfull because of ${err}`} )
+            })
 
 })
 
@@ -44,9 +58,9 @@ server.post('/posts',(req, res) => {
     else{
         const posts = postDb.insert(newPost)
         posts
-            .then( responds => {
+            .then( response => {
                 responds.message = 'Post Sucessful'
-                res.status(201).json(responds)
+                res.status(201).json(response)
             })
             .catch(err => {
                 res.status(500).json( {error : `Post wasn't sucessfull because of ${err}`} )
@@ -54,6 +68,7 @@ server.post('/posts',(req, res) => {
     }
 
 })
+
 
 server.put('/posts/:id',(req, res) => {
     const { id } = req.params;
@@ -93,22 +108,71 @@ server.delete('/posts/:id',(req, res) => {
 
 //TAG API CRUD
 
-server.get('/posts',(req, res) => {
+server.get('/tags',(req, res) => {
+    const tags = tagDb.get()
+    tags
+        .then(response => {
+            res.status(200).json(response)
+        })
+        .catch(err => {
+            res.status(500).json({error : `Sorry $(err) Has occurred`})
+        })
 
 })
 
-server.post('/posts',(req, res) => {
+server.post('/tags',(req, res) => {
+    const { userId, text } = req.body
+    const newTag = { userId, text }
+    if(!userId || !text){
+        res.status(400).json({ error : 'Please provide your userId and Text'})
+    }
+    else{
+        const posts = tagDb.insert(newTag)
+        posts
+            .then( response => {
+                responds.message = 'Post Sucessful'
+                res.status(201).json(response)
+            })
+            .catch(err => {
+                res.status(500).json( {error : `Post wasn't sucessfull because of ${err}`} )
+            })
+    }
 
 })
 
-server.post('/posts/:id',(req, res) => {
+server.put('/tags/:id',(req, res) => {
+    const { id } = req.params;
+    const { userId, text } = req.body
+    const newPost = { userId, text }
+
+    if(!userId || !text){
+        res.status(400).json({ error : 'Please provide your userId and Text'})
+    }
+    else {
+        const taggedPost = tagDb.update( id, newPost )
+        taggedPost
+           .then(response => {
+            response.message = 'update was sucessful'
+               res.status(200).json(response)
+           })
+           .catch(err => {
+               res.status(500).json({ error : "there was an error while updating records"})
+           })
+    }
 
 })
-server.put('/posts',(req, res) => {
 
-})
-server.delete('/posts',(req, res) => {
-
+server.delete('/tags/:id',(req, res) => {
+    const { id } = req.params
+    console.log(id)
+    const taggedPost = tagDb.remove(id)
+    taggedPost
+        .then(responds => {
+        res.status(200).json(responds)
+        })
+        .catch(err => {
+        res.status(404).json({ error : "Delete was not sucessful" })
+        })
 })
 
 // USERS API CRUD
