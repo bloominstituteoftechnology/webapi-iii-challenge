b4991055491fdb0962fb0a8e8af034a2b7375333
Gabriel Duquette <gabrielduquette@gmail.com>
2018-07-26 23:22:58 -0400

    add react project

--- server.js
+++ server.js
@@ -13,31 +13,4 @@ server.use(morgan('dev'));
 
 server.use('/api', apiRoutes);
 
-// server.use((err, req, res, next) => {
-//   // save the error to the database
-//   // send an email to the responsible party
-//
-//   switch (err.code) {
-//     case 400:
-//
-//     case 11345:
-//       res.status(403).send({
-//         success: false,
-//         data: undefined,
-//         title: err.message,
-//         description: '',
-//         recovery: 'Please require access to this resource',
-//       });
-//     case 404:
-//
-//     default:
-//       res.status(500).send({
-//         success: false,
-//         data: undefined,
-//         error: err.message,
-//         recovery: '',
-//       });
-//   }
-// });
-
 server.listen(8000, () => console.log('\n=== API running on port 8000 ===\n'));
