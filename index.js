44207e4355fa44ac4ef506ede0441969721d9673
Katie Gorbell <katiegorbell@gmail.com>
2018-08-01 16:28:15 -0500

    get request for users data added

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const express = require('express');
 const server = express();
 
+const postDb = require('./data/helpers/postDb.js');
+const tagDb = require('./data/helpers/tagDb.js');
+const userDb = require('./data/helpers/userDb.js');
+
 server.use(express.json());
+
+server.get('/users', (req, res) => {
+    userDb.get()
+        .then(response =>
+            res.status(200).json(response)
+        )
+        .catch(() => {
+            res.status(500).json({ error: 'The users data could not be retrieved '})
+        })
+})
+
 
 
 server.listen(8000, () => console.log('\n === API running... === \n'))
