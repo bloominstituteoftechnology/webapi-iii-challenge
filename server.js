6afbbda06e4421aa72b8994405bc8b5a36a2eb58
Daniel Chamorro <dan@dcitllc.com>
2018-08-01 18:41:27 -0400

    Added React and dispalying post

--- server.js
+++ server.js
@@ -253,68 +253,68 @@ server.get("/tags/:id", (req, res) => {
 
 //* POST Request tagDb insert()
 server.post("/tags", (req, res) => {
-    if (!req.body.tag) {
-      return res.status(400).json({
-        errorMessage: "Please provide the text for the post."
-      });
-    }
-  
-    tagDb
-      .insert({
-        text: req.body.tag
+  if (!req.body.tag) {
+    return res.status(400).json({
+      errorMessage: "Please provide the text for the post."
+    });
+  }
+
+  tagDb
+    .insert({
+      text: req.body.tag
+    })
+    .then(id => res.status(201).json(id))
+    .catch(err =>
+      res.status(500).json({
+        error: "There was an error while saving the tag to the database"
       })
-      .then(id => res.status(201).json(id))
-      .catch(err =>
-        res.status(500).json({
-          error: "There was an error while saving the tag to the database"
-        })
-      );
-  });
-  
-  //* UPDATE Request postDb update().
-  server.put("/tags/:id", (req, res) => {
-    const { tag } = req.body;
-    const { id } = req.params;
-  
-    if (!tag &&) {
-      res.status(400).json({
-        errorMessage: "Please provide tag and user id for the posts."
-      });
-    }
-    tagDb
-      .update(id, { tag })
-      .then(response => {
-        if (!response) {
-          res
-            .status(404)
-            .json({ message: "The user with the specified ID does not exist." });
-        } else {
-          res.status(200).json({ tag });
-        }
-      })
-      .catch(err =>
-        res.status(500).json({ error: "The user could not be updated" })
-      );
-  });
-  
-  //* DELETE Request postDb remove()
-  server.delete("/tags/:id", (req, res) => {
-    const { id } = req.params;
-  
-    postDb
-      .remove(id)
-      .then(tag => {
-        if (!tag) {
-          res
-            .status(404)
-            .json({ message: "The tag with the specified ID does not exist." });
-        } else {
-          res.status(200).json({ message: "The tag has been deleted." });
-        }
-      })
-      .catch(err =>
-        res.status(500).json({ error: "The tag could not be removed" })
-      );
-  });
+    );
+});
+
+//* UPDATE Request postDb update().
+server.put("/tags/:id", (req, res) => {
+  const { tag } = req.body;
+  const { id } = req.params;
+
+  if (!tag) {
+    res.status(400).json({
+      errorMessage: "Please provide tag and user id for the posts."
+    });
+  }
+  tagDb
+    .update(id, { tag })
+    .then(response => {
+      if (!response) {
+        res
+          .status(404)
+          .json({ message: "The user with the specified ID does not exist." });
+      } else {
+        res.status(200).json({ tag });
+      }
+    })
+    .catch(err =>
+      res.status(500).json({ error: "The user could not be updated" })
+    );
+});
+
+//* DELETE Request postDb remove()
+server.delete("/tags/:id", (req, res) => {
+  const { id } = req.params;
+
+  postDb
+    .remove(id)
+    .then(tag => {
+      if (!tag) {
+        res
+          .status(404)
+          .json({ message: "The tag with the specified ID does not exist." });
+      } else {
+        res.status(200).json({ message: "The tag has been deleted." });
+      }
+    })
+    .catch(err =>
+      res.status(500).json({ error: "The tag could not be removed" })
+    );
+});
 
 server.listen(8000, () => console.log("\n === API Running... ===\n"));
