08deed53e37ba6105427f4366c0f3ce1603d83dc
Daniel Chamorro <dan@dcitllc.com>
2018-08-01 16:23:45 -0400

    dchamorro - Added userDb and userDb GET

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
 const express = require("express");
 const morgan = require("morgan");
-const server = express();
+const userDb = require("./data/helpers/userDb");
 
-// const db = require("./data/db.js");
+const server = express();
 
 server.use(express.json());
 server.use(morgan("dev"));
 
-// add your server code starting here
-
 server.get("/", (req, res) => {
   res.send("Hello World");
+});
+
+//* GET Request
+server.get("/users", (req, res) => {
+  userDb
+    .get()
+    .then(users => res.status(200).json(users))
+    .catch(err =>
+      res
+        .status(500)
+        .json({ error: "The users information could not be retrieved." })
+    );
 });
 
 server.listen(8000, () => console.log("\n === API Running... ===\n"));
