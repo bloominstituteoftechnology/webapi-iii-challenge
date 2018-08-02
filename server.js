1311379fe18a6f0e0af467341caded7618faead8
JacobLayton <jlayjlay3@gmail.com>
2018-08-02 03:40:43 -0700

    Get request added

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const server = express();
 
+const posts = require('./data/helpers/postDb');
+const tags = require('./data/helpers/tagDb');
+const users = require('./data/helpers/userDb');
+
 server.use(express.json());
+
+server.get('/api/users', (req, res) => {
+    users.get()
+        .then(response => {
+            res.json(response)
+        })
+        .catch(() => {
+            res.status(500).json({ error: "The users information could not be retrieved."})
+        })
+});
 
 server.listen(5000, () => console.log('API running on port 5000'));
