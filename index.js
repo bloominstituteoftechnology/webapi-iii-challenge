9b858b5f21cf43623601fd8dd4efd292af69aea6
sean chavez <seanchavez@seanchavezsMBP.home>
2018-08-01 22:50:24 -0700

    Implement POST user endpoint Add 404 message to GET single user endpoint

--- index.js
+++ index.js
@@ -20,11 +20,31 @@ server.get('/api/users', (req, res) => {
 server.get('/api/users/:id', (req, res) => {
   userDb.get(req.params.id)
     .then(response => {
+      if (!response) {
+        res.status(404)
+          .json({ errorMessage: "The user with the specified ID does not exist" });
+      }
       res.status(200).json(response);
     })
     .catch(err => {
       res.status(500)
         .json({ error: "The user information could not be retrieved"});
+    });
+});
+
+server.post('/api/users',(req, res) => {
+  if (!req.body.name) {
+    res.status(400)
+      .json({ errorMessage: "Please provide user name"});
+    return;
+  }
+  userDb.insert(req.body)
+    .then(response => {
+      res.status(201).json(response);
+    })
+    .catch(err => {
+      res.status(500)
+        .json({ error: "There was an error saving user to the database"});
     });
 });
 
