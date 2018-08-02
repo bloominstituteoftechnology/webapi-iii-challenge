4005da2bccb01f6ee0a6e2086339ca18b073f591
Sumayyah <sumayyah@zubairs-air.attlocal.net>
2018-08-02 14:53:39 -0700

    All CRUD points for users and posts endpoints working including get user posts and get posts tags 💖

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ const cors = require('cors');
 server.use(express.json());
 server.use(cors());
 
+// one user has many posts ex: instagram user
+// one posts only has one user
+// user has the posts and the posts has the tags
+// each post can have many tags like hashtags on instagram
+
 server.get('/posts', (req, res) => {
   postDb
     .get()
@@ -35,23 +40,24 @@ server.get('/posts/:id', (req, res) => {
     });
 });
 
-// server.get('/posts/:id/tags', (req, res) => {
-//   const { id } = req.params;
-//   postDb
-//     .get(id)
-//     .then((post) => {
-//       if (post === 0) {
-//         res.status(404).json({ message: 'The post doesnt exist' });
-//       } else {
-//         tagDb.getPostTags(id).then((tag) => {
-//           res.status(201).json(tag);
-//         });
-//       }
-//     })
-//     .catch(() => {
-//       res.status(500).json({ err: 'The posts info cannot be found.' });
-//     });
-// });
+// gets tags from the same posts
+server.get('/posts/:id/tags', (req, res) => {
+  const { id } = req.params;
+  postDb
+    .get(id)
+    .then((post) => {
+      if (post === 0) {
+        res.status(404).json({ message: 'The post doesnt exist' });
+      } else {
+        postDb.getPostTags(id).then((tag) => {
+          res.status(201).json(tag);
+        });
+      }
+    })
+    .catch(() => {
+      res.status(500).json({ err: 'The posts info cannot be found.' });
+    });
+});
 
 server.post('/posts', (req, res) => {
   const post = req.body;
@@ -86,25 +92,23 @@ server.delete('/posts/:id', (req, res) => {
     });
 });
 
+// we're updating the post but who posted it doesn't change
 server.put('/posts/:id', (req, res) => {
   const { id } = req.params;
   const post = req.body;
-  if (!post.userId || !post.text) {
-    res.status(404).json({ errorMessage: 'Please provide user Id and text for the post.' });
-  }
 
+  if (!post.text) {
+    res.status(404).json({ errorMessage: 'Please provide text for the post.' });
+  }
   postDb
-    .get(id)
-    .then((post) => {
-      if (post === 0) {
+    .update(id, post)
+    .then((id) => {
+      if (id === 0) {
         res.status(404).json({
           message: 'The post with this ID does not exist :/'
         });
-      } else {
-        postDb.update(id, post).then((id) => {
-          res.status(200).json(id);
-        });
       }
+      res.status(200).json(id);
     })
     .catch(() => {
       res.status(500).json({ error: 'The post information could not be updated :(' });
@@ -112,13 +116,103 @@ server.put('/posts/:id', (req, res) => {
 });
 
 server.get('/users', (req, res) => {
-  postDb
+  userDb
     .get()
     .then((users) => {
       res.json(users);
     })
     .catch(() => {
       res.status(500).json({ err: 'The user information could not be found :(' });
+    });
+});
+
+server.get('/users/:id', (req, res) => {
+  const { id } = req.params;
+  userDb
+    .get(id)
+    .then((user) => {
+      if (user === 0) {
+        res.status(404).json({ message: 'The user doesnt exist' });
+      } else {
+        res.json(user);
+      }
+    })
+    .catch(() => {
+      res.status(500).json({ err: 'The user info cannot be found.' });
+    });
+});
+
+server.post('/users', (req, res) => {
+  const user = req.body;
+  if (!user.name) {
+    res.status(404).json({ errorMessage: 'Please provide user name.' });
+  }
+  userDb
+    .insert(user)
+    .then((user) => {
+      res.status(201).json(user);
+    })
+    .catch(() => {
+      res.status(500).json({ error: 'There was an error while saving the user to the database' });
+    });
+});
+
+server.delete('/users/:id', (req, res) => {
+  const { id } = req.params;
+  userDb
+    .remove(id)
+    .then((user) => {
+      if (user === 0) {
+        res.status(404).json({ message: 'The user with this specified ID doesnt exist' });
+      } else {
+        res.status(200).json(user);
+      }
+    })
+    .catch(() => {
+      res.status(500).json({
+        error: 'The user could not be deleted :('
+      });
+    });
+});
+
+server.put('/users/:id', (req, res) => {
+  const { id } = req.params;
+  const user = req.body;
+
+  if (!user.name) {
+    res.status(404).json({ errorMessage: 'Please provide a name.' });
+  }
+  userDb
+    .update(id, user)
+    .then((id) => {
+      if (id === 0) {
+        res.status(404).json({
+          message: 'The user with this ID does not exist :/'
+        });
+      }
+      res.status(200).json(id);
+    })
+    .catch(() => {
+      res.status(500).json({ error: 'The user info could not be updated :(' });
+    });
+});
+
+// gets all posts from the same user
+server.get('/users/:id/posts', (req, res) => {
+  const { id } = req.params;
+  userDb
+    .get(id)
+    .then((user) => {
+      if (user === 0) {
+        res.status(404).json({ message: 'The user doesnt exist' });
+      } else {
+        userDb.getUserPosts(id).then((post) => {
+          res.status(201).json(post);
+        });
+      }
+    })
+    .catch(() => {
+      res.status(500).json({ err: 'The users info cannot be found.' });
     });
 });
 
