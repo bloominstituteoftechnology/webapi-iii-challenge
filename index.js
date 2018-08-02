34bd42e423cecde915cd4379c5db5b7d13d95507
sean chavez <seanchavez@seanchavezsMBP.home>
2018-08-01 20:13:36 -0700

    Require DB helpers; Implement GET users endpoint

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const express = require('express');
+const userDb = require('./data/helpers/userDb');
+const postDb = require('./data/helpers/postDb');
+const tagDb = require('./data/helpers/tagDb');
 
 const server = express();
+server.use(express.json());
 
-server.use('/', (req, res) => res.send('this works?'));
+server.get('/api/users', (req, res) => {
+  userDb.get()
+    .then(response => {
+      res.json(response);
+    })
+    .catch(err => {
+      res.status(500)
+        .json({ error: "The users information could not be retrieved"});
+    });
+})
 
 server.listen(8000, () => console.log('API running on port 8000'));
