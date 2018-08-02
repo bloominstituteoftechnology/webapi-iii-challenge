8381329c6929669cc5f7143e15d91258e9161371
JacobLayton <jlayjlay3@gmail.com>
2018-08-02 14:17:22 -0700

    Get reques tby user id added

--- server.js
+++ server.js
@@ -8,13 +8,29 @@ const users = require('./data/helpers/userDb');
 server.use(express.json());
 
 server.get('/api/users', (req, res) => {
-    users.get()
-        .then(response => {
-            res.json(response)
-        })
-        .catch(() => {
-            res.status(500).json({ error: "The users information could not be retrieved."})
-        })
+  users.get()
+    .then(response => {
+      res.json(response)
+    })
+    .catch(() => {
+      res.status(500).json({ error: "The users information could not be retrieved." })
+    })
 });
 
-server.listen(5000, () => console.log('API running on port 5000'));
+server.get('/api/users/:id', (req, res) => {
+  const { id } = req.params;
+  users.get(id)
+    .then(response => {
+      if (response.length < 1) {
+        res.status(404).json({ message: "The user with the specified ID does not exist." })
+      } else {
+        res.json(response)
+      }
+    })
+    .catch(() => {
+      res.status(500).json({ error: "The user information could not be retrieved." })
+    })
+})
+
+
+server.listen(5000, () => console.log('\n === API running on port 5000 === \n'));
