7a8643d40e01220b7600244ac2d4c13c3b72910b
unknown <may.alex43@gmail.com>
2018-08-03 10:59:04 -0400

    Forgot to commit yesterday

--- node-blog/src/App.js
+++ node-blog/src/App.js
@@ -34,7 +34,10 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <Users />
+        <Users 
+          handleData={this.handleData} 
+          name={this.state.userData}  
+        />
       </div>
     );
   }
--- node-blog/src/components/Users.js
+++ node-blog/src/components/Users.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import User from './User';
+
 
 const Users= props => {
     return (
         <div>
-            props.users.map(user =>{
-
-            })
+            {props.name.map(user =>{
+                return (
+                    <User
+                        handleData={props.handleData}
+                        key= {user.id}
+                        name={user.name}
+                        
+                    />
+                );
+            })}
         </div>
     );
 }
