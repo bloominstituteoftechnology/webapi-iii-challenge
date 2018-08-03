c6721bef77d43a4a6102e0035a025f76e60e4632
sean chavez <seanchavez@seanchavezsMBP.home>
2018-08-02 17:00:11 -0700

    Implement GET endpoints to retrieve all posts and single post by id Add cors for later

--- index.js
+++ index.js
@@ -2,20 +2,25 @@ const express = require('express');
 const userDb = require('./data/helpers/userDb');
 const postDb = require('./data/helpers/postDb');
 const tagDb = require('./data/helpers/tagDb');
+const cors = require('cors');
 
 const server = express();
 server.use(express.json());
+server.use(cors());
+
+//============USERS============
 
 server.get('/api/users', (req, res) => {
   userDb.get()
     .then(response => {
-      res.status(200).res.json(response);
+      res.status(200).json(response);
     })
     .catch(err => {
+      console.log(err);
       res.status(500)
         .json({ error: "The users information could not be retrieved"});
     });
-})
+});
 
 server.get('/api/users/:id', (req, res) => {
   userDb.get(req.params.id)
@@ -62,7 +67,58 @@ server.delete('/api/users/:id', (req,res) => {
 });
 
 server.put('/api/users/:id', (req, res) => {
-  if
+  const name = req.body.name;
+  if (!name || name.length > 128) {
+    res.status(400)
+      .json({ message: "Please provide a user name up to 128 characters long"});
+    return;
+  }
+  userDb.update(req.params.id, req.body)
+    .then(response => {
+      if (response === 0) {
+        res.status(404)
+          .json({ message: "The user with the specified ID does not exist" });
+      } else {
+        res.status(200);
+      }
+    })
+    .catch(() => {
+      res.status(500)
+        .json({ error: "The user information could not be modified"});
+    });
+});
+
+//============Posts============
+
+server.get('/api/posts', (req, res) => {
+  postDb.get()
+    .then(response => {
+      res.status(200).json(response);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500)
+        .json({ error: "The posts information could not be retrieved"});
+    });
+});
+
+server.get('/api/posts/:userId', (req, res) => {
+  postDb.get(req.params.userId)
+    .then(response => {
+      if (!response) {
+        res.status(404)
+          .json({ message: "The post with the specified user ID does not exist" });
+      }
+      res.status(200).json(response);
+    })
+    .catch(err => {
+      res.status(500)
+        .json({ error: "The post information could not be retrieved"});
+    });
+});
+
+server.post('/api/posts', (req, res) => {
+
 })
 
 server.listen(8000, () => console.log('API running on port 8000'));
