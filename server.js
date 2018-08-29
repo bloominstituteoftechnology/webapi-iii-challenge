3a0219c8040074780c8c24f54b1d086885e15acd
William <kwontaewoongah@gmail.com>
2018-08-29 15:43:54 -0700

    implement delete user endpoint functionality

--- server.js
+++ server.js
@@ -29,9 +29,13 @@ server.put('/users/:id',(req,res)=>{
         users.update(req.params.id,user).then(count=>{
             count===1?
             users.get(req.params.id).then(user=>res.status(200).json(user)).catch(err=>console.log(err)):
-            req.status(404).json({message: "The post with the specified ID does not exist."})
+            req.status(404).json({message: "The user with the specified ID does not exist."})
         }
-    ).catch(err=>res.status(500).json({ error: "The post information could not be modified." }))
+    ).catch(err=>res.status(500).json({ error: "The user information could not be modified." }))
     }
+})
+server.delete('/users/:id',(req,res)=>{
+    users.remove(req.params.id).then(count=>count>0? res.status(200).json({success:`Deleted ${count} user(s)`}):res.status(404).json({ message: "The user with the specified ID does not exist." })
+    ) .catch(err=>res.status(500).json({error: "The user could not be removed" }))
 })
 server.listen(9000,()=>console.log('Engines firing server starting new horizons venturing.'))
