2f1be4d663a35e3bfbfb3da4720b0e2b375571f1
adamcglee <adamcglee@gmail.com>
2018-08-29 15:32:07 -0700

    finished adding middleware and setting up status codes

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const dbPosts = require('./data/helpers/postDb.js')
 
 const server = express();
 
+function uppercase (req, res, next) {
+    req.body.name = req.body.name.toUpperCase();
+    next();
+}
+
 server.use(express.json());
 
 server.get('/users', (req, res) => {
@@ -21,7 +26,11 @@ server.get('/users', (req, res) => {
 server.get('/users/:id', (req, res) => {
     dbUsers.get(req.params.id)
         .then(users => {
-            res.status(200).json(users);
+            if(users.length > 0) {
+                res.status(200).json(users);                
+            } else {
+                res.status(400).json({ message: "The User with the specified ID could not be retrieved."})
+            }
         }) 
         .catch(err => {
             console.log('error', err);
@@ -29,9 +38,11 @@ server.get('/users/:id', (req, res) => {
         })
 })
 
-server.post('/users', (req, res) => {
-    console.log(req.body)
+server.post('/users', uppercase, (req, res) => {
     const user = req.body;
+    if(!user) {
+        res.status(400).json({ message: "Please provide a name for the User."})
+    }
     dbUsers.insert(user)
         .then(() => {
             dbUsers.get()
@@ -49,15 +60,23 @@ server.delete('/users/:id', (req, res) => {
     const {id} = req.params;
     dbUsers.remove(id)
         .then(count => {
-            res.status(204).end();
+            if(count) {
+                res.status(204).end();
+            } else {
+                res.status(404).json({ message: "No User with this ID was found."})
+            }            
         }) 
         .catch(err => res.status(500).json(err));
 })
 
-server.put('/users/:id', (req, res) => {
+server.put('/users/:id', uppercase, (req, res) => {
         dbUsers.update(req.params.id, req.body)
             .then(user => {
-                res.status(200).json(user)
+                if (user) {
+                    res.status(200).json(user)
+                } else {
+                    res.status(404).json({ message: "No User with this ID was found."})
+                }               
             })
             .catch(err => res.status(500).json({ message: "Update Failed"}))
 })
