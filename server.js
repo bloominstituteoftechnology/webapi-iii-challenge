0fc1de4b3c67e3683a7d785c4748d5ef1225bbc7
francistse23 <wtse.ucsd@gmail.com>
2018-08-29 13:11:37 -0700

    Added Middleware to convert name input to upper case before executing the respective functions. Also added a condition to check character limit. If >128 characters, then system will log a message and ask user to shorten input.

--- server.js
+++ server.js
@@ -10,10 +10,20 @@ server.use(express.json());
 server.use(helmet());
 server.use(morgan());
 
+function upperCase( req, res, next) {
+    const name = req.body.name.toUpperCase();
+    if (name !== name.toUpperCase()) {
+        res.status(400).json({message: "Name is not in upper case"})
+    } else{
+        next();
+    }
+};
+
 server.get("/", (req, res) => {
     res.send("Testing")
 });
 
+//USERS
 server.get("/users", (req, res) => {
     dbUsers.get()
     .then(users => {
@@ -31,19 +41,22 @@ server.get("/users/:id", (req, res) => {
     .catch(err => console.log(err));
 })
 
-server.post("/users", (req,res) => {
-    const user = req.body;
-    if (!user.name){
+server.post("/users", upperCase, (req,res) => {
+    const name = req.body.name;
+    if (!name){
         res.status(400).json({message: "Please enter a name."})
-    } else if (user.name.length>128) {
+    } 
+    else if (name.length > 128) {
         res.status(406).json({message: "Name is too long. Please limit name length to 128 characters or less."})
-    } else {
+    } 
+    else {
         dbUsers.insert(user)
         .then(response => res.status(200).json(response.id))
         .catch(err => console.log(err));
     }
 })
 
+//POSTS
 server.get("/posts", (req, res) => {
     dbPosts.get()
     .then(posts => res.status(200).json(posts))
