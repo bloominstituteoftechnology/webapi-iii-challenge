ea689b305f027d950a35afd68f6824b5119a4ee9
francistse23 <wtse.ucsd@gmail.com>
2018-08-29 12:52:07 -0700

    Added GET method to retrieve user by ID

--- server.js
+++ server.js
@@ -22,10 +22,21 @@ server.get("/users", (req, res) => {
     .catch(err => console.log(err));
 });
 
+server.get("/users/:id", (req, res) => {
+    const {id} = req.params;
+    dbUsers.get(id)
+    .then(response => {
+        res.status(200).json(response)
+    })
+    .catch(err => console.log(err));
+})
+
 server.post("/users", (req,res) => {
     const user = req.body;
     if (!user.name){
         res.status(400).json({message: "Please enter a name."})
+    } else if (user.name.length>128) {
+        res.status(406).json({message: "Name is too long. Please limit name length to 128 characters or less."})
     } else {
         dbUsers.insert(user)
         .then(response => res.status(200).json(response.id))
