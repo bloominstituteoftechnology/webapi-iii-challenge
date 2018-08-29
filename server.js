ea16a3ab63d24d72bade58110b6f85629bbb4265
Charlie Winslow <cwinslow22@yahoo.com>
2018-08-29 15:53:49 -0500

    added get method for users

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const server = express();
+const dbUser = require('./data/helpers/userDb.js');
+
+server.use(express.json());
 
 
 server.get('/', (req, res) => {
-    res.send('Welcome to Node-Blog, and express middleware production');
+    res.send('Welcome to Node-Blog, an express middleware production');
 } );
 
+server.get('/api/users', (req, res) => {
+    dbUser.get()
+    .then(users => {
+        res.status(200).json(users);
+    })
+    .catch(err => {
+        console.error('error', err);
 
-server.listen(8888, () => console.log('/n== API on port 8888 ==/n') );
+        res.status(500).json({ error: 'The users information could not be retrieved.' });
+    })
+})
+
+server.listen(8000, () => console.log('/n== API on port 8k ==/n') );
