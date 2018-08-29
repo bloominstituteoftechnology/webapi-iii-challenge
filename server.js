2ebbf1cc4afe64f7bed70fac0a4c4bd18cd0ac5c
Carey <careycbaldwin@gmail.com>
2018-08-29 16:21:53 -0400

    tried greeter middleware for practice

--- server.js
+++ server.js
@@ -4,9 +4,19 @@ const db = require ('./data/dbConfig.js');
 
 const server = express();
 
+//Middleware here (practice)
+function greeter(req, res, next) {
+    req.name = 'FSW 12: Middleware Enthusiasts';
+    next();
+}
+
 //Initiate the Server at /
 server.get('/',(req, res) => {
     res.send('Initiating Server');
+});
+
+server.get('/hello', greeter, (req,res) =>{
+    res.send(`hello ${req.name}`);
 });
 
 //Start the server
