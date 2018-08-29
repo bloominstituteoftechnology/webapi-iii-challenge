47faebecb88b235f28c91302d93a1450858f5f52
William <kwontaewoongah@gmail.com>
2018-08-29 15:29:05 -0700

    added post endpoint for users

--- server.js
+++ server.js
@@ -12,5 +12,13 @@ server.get('/users',(req,res)=>{
 server.get('/users/:id',(req,res)=>{
     users.get(req.params.id).then(user=>res.status(200).json(user)).catch(err=>res.status(500).json({error:"The users information could not be retrieved"}))
 })
-
+server.post('/users',(req,res)=>{
+    const user=req.body;
+    if (!user) {
+        res.status(400).json({errorMessage:"Please enter a user name"})
+    } else {
+        users.insert(user).then(user=>res.status(201).json(user))
+        .catch(err=>res.status(500).json({ error: "There was an error while saving the user to the database" }))
+    }
+})
 server.listen(9000,()=>console.log('Engines firing server starting new horizons venturing.'))
