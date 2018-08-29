17b2c9316747d617a90b92453cd3a9d89e37cfb9
francistse23 <wtse.ucsd@gmail.com>
2018-08-29 16:04:05 -0700

    Can add and delete Post. Need to implement check for existing userId.

--- server.js
+++ server.js
@@ -34,38 +34,41 @@ server.get("/users/:userId", (req, res) => {
     .then(response => {
         res.status(200).json(response)
     })
-    .catch(err => console.log(err));
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({ message: "Failed to retrieve user with the specific userId." })
+    });
 })
 
 server.get("/users/:userId/posts/:postId", (req,res) => {
     const {userId, postId} = req.params
-    if(userId === postId){
+    if(userId === postId) {
         dbUsers.getUserPosts(userId)
         .then(posts => {res.status(200).json(posts)})
         .catch(err => {
             console.log(err);
-            res.status(500).json({message: "Post with correlated User ID is not found."})
+            res.status(500).json({ message: "Post with correlated User ID is not found." })
         })
-    } else{
-        res.status(400).json({message: "UserId and PostId do not match. Please check and try again."})
+    } else {
+        res.status(400).json({ message: "UserId and PostId do not match. Please check and try again." })
     }
 });
 
 server.post("/users", upperCase, (req,res) => {
     const {name} = req.body;
     const upperName = req.upperName;
-    if (!name){
-        res.status(400).json({message: "Please enter a name."});
+    if(!name) {
+        res.status(400).json({ message: "Please enter a name." });
     } 
-    else if (name.length > 128) {
-        res.status(406).json({message: "Name is too long. Please limit name length to 128 characters or less."});
+    else if(name.length > 128) {
+        res.status(406).json({ message: "Name is too long. Please limit name length to 128 characters or less." });
     } 
     else {
         dbUsers.insert({name: upperName})
-        .then(user => {res.status(200).json(upperName)})
+        .then(user => { res.status(200).json(user) })
         .catch(err => {
             console.log(err)
-            res.status(500).json({message: "Failed to create new user."})
+            res.status(500).json({ message: "Failed to create new user." })
         });
     }
 })
@@ -74,44 +77,72 @@ server.put("/users/:userId", upperCase, (req,res) => {
     const upperName = req.upperName;
     req.body.name = upperName;
     dbUsers.update(req.params.userId, req.body)
-    .then(user => {res.status(200).json(user)})
+    .then(user => { res.status(200).json(user) })
     .catch(err => {
         console.log(err)
-        res.status(500).json({message: "Updated failed."})
+        res.status(500).json({ message: "Updated failed." })
     });
 })
 
 server.delete("/users/:userId", (req,res) => {
     dbUsers.remove(req.params.userId)
-    .then(count => res.status(200).json(count))
+    .then(count => { res.status(200).json({ message: `${count} users deleted.`} )})
     .catch(err => {
         console.log(err);
-        res.status(500).json({message: "Cannot delete user."})
+        res.status(500).json({ message: "Cannot delete user." })
     })
 })
 
 //POSTS
 server.get("/posts", (req, res) => {
     dbPosts.get()
-    .then(posts => res.status(200).json(posts))
+    .then(posts => { res.status(200).json(posts) })
     .catch(err => {
         console.log(err)
-        res.status(500).json({message: "Failed to retrieve post data."})
+        res.status(500).json({ message: "Failed to retrieve post data." })
     });
 });
 
 server.get("/posts/:postId", (req, res) => {
     const {postId} = req.params;
     dbPosts.get(postId)
-    .then(post => {res.status(200).json(post)})
+    .then(post => { res.status(200).json(post) })
     .catch(err => {
         console.log(err);
-        res.status(500).json({message: "Failed to retrieve post(s) with the specific PostId."})
+        res.status(500).json({ message: "Failed to retrieve post(s) with the specific PostId." })
     })
 })
 
-//server.post("/posts", upperCase, (req, res) => {
+server.post("/posts", (req, res) => {
+    const {userId} = req.params;
+    console.log(userId);
+    const post = req.body;
+    if (!post.text) {
+        res.status(400).json({ message: "Cannot be blank. Please enter some text." })
+    } else if(!post.userId) {
+        res.status(400).json({ message: "Field required. UserId must be entered." })
+    } 
+    //else if() {
 
-// });
+    // } 
+    else {
+        dbPosts.insert(post)
+        .then(post => { res.status(200).json(post) })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ message: "Failed to create new post." })
+        })
+    }
+});
+
+server.delete("/posts/:postId", (req, res) => {
+    const {postId} = req.params;
+    dbPosts.remove(postId)
+    .then(count => res.status(200).json({ message: `${count} posts deleted.`} ))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({ message: "Failed to delete post." })
+    });
+});
 
 server.listen(9000, () => console.log("===API on port 9000==="));
