8d2248c7a6b81e188f5644bffdf6dddfa0cb5245
Carey <careycbaldwin@gmail.com>
2018-08-29 17:07:26 -0400

    completed GET request for individual user

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+//pull in helper method to get user data
 const users = require('./data/helpers/userDb');
 
 // const db = require ('./data/dbConfig.js');
@@ -7,22 +8,31 @@ const server = express();
 
 server.use(express.json()); //allows parsing of json data from req.body
 
-//Middleware here (practice)
-function greeter(req, res, next) {
-    req.name = 'FSW 12: Middleware Enthusiasts';
-    next();
+// //Middleware here (practice)
+// function greeter(req, res, next) {
+//     req.name = 'FSW 12: Middleware Enthusiasts';
+//     next();
+// }
+
+
+//Middleware to capitalize user name
+function capitalize(req, res, next) {
+    req.name = req.body.toUpperCase();
+    next;
 }
+
 
 //Initiate the Server at /
 server.get('/',(req, res) => {
     res.send('Initiating Server');
 });
 
-server.get('/hello', greeter, (req,res) =>{
-    res.send(`hello ${req.name}`);
-});
+// //using greeter function (practice)
+// server.get('/hello', greeter, (req,res) =>{
+//     res.send(`hello ${req.name}`);
+// });
 
-//GET request for retrieving all users
+//GET request for retrieving all users needs: 'const users = require('./data/helpers/userDb')' to work
 server.get('/api/users', (req,res) => {
     users
         .get()
@@ -34,6 +44,24 @@ server.get('/api/users', (req,res) => {
         res.status(500).json({message: 'Error getting users'})
     });
 });
+
+//GET request for retrieving individual user
+server.get('/api/users/:id', (req,res) => {
+    const id = req.params.id;
+    users
+    .get(id)
+    .then(user => {
+        if (user===0) {
+            res.status(404).json({message: 'No user corresponding to that identifier'});
+            return;
+        }
+        res.json(user);
+    })
+    .catch(err =>{
+        res.status(500).json({message: 'Error getting user information'})
+    });
+});
+
 
 //Start the server
 server.listen(9000, () => console.log('\n == API on port 9000 == \n'));
