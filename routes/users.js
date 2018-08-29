deebf0e8e4454ac121c040d08c73d80888a37ccb
Ashwin <aswinjeganath@gmail.com>
2018-08-30 03:22:33 +0530

    Add route handler to remove users from db

--- routes/users.js
+++ routes/users.js
@@ -30,22 +30,50 @@ router
     }
   });
 
-router.route('/:id').put(nameUppercaser, async (req, res) => {
-  if (!req.body.name)
-    return res.status(400).json({ message: 'You need to provide a name' });
-  try {
-    let data = await db.update(req.params.id, { name: req.body.name });
-    if (data) {
-      return res
-        .status(200)
-        .json({ message: `${data} record(s) updated successfully` });
+router
+  .route('/:id')
+  .get(async (req, res) => {
+    try {
+      let data = await db.get(req.params.id);
+      if (data) {
+        return res.status(200).json(data);
+      }
+      res
+        .status(404)
+        .json({ message: "The user with the specified ID doesn't exist" });
+    } catch (err) {
+      res.status(500).json(err);
     }
-    res
-      .status(404)
-      .json({ message: "The user with specified id doesn't exist" });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+  })
+  .put(nameUppercaser, async (req, res) => {
+    if (!req.body.name)
+      return res.status(400).json({ message: 'You need to provide a name' });
+    try {
+      let data = await db.update(req.params.id, { name: req.body.name });
+      if (data) {
+        return res
+          .status(200)
+          .json({ message: `${data} record(s) updated successfully` });
+      }
+      res
+        .status(404)
+        .json({ message: "The user with specified id doesn't exist" });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  })
+  .delete(async (req, res) => {
+    try {
+      let count = await db.remove(req.params.id);
+      if (count) {
+        return res.status(200).json({ message: `${count} record(s) removed` });
+      }
+      return res
+        .status(404)
+        .json({ message: "The user with specified id doesn't exist" });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 module.exports = router;
