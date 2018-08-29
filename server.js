05e9de132594b682b25fee77e9bb3c03bc85f39e
Sean <sean47900@gmail.com>
2018-08-29 16:34:26 -0500

    working on get method

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
 
 const express = require('express');
 const server = express();
+const userDb = require('./data/helpers/userDb.js');
+
+
+ server.use(express.json());
+
  server.get('/', (req, res) => {
     res.send('Welcome to Node-Blog');
 } );
+
+server.get('/users', (req, res) => {
+    userDb.find().then(users => {
+      res.status(200).json(users)
+    }).catch(err => {
+      console.error('error', err);
+       res.status(500).json({ message: 'Error retrieving user data'})
+    });
+  });
+
  server.listen(8000, () => console.log('/n== API on port 8000 ==/n') )
