8f7ca8ae7d10a65adaa17da84a6c50fbde32ad23
szincone <sawyerzincone@gmail.com>
2018-08-29 17:10:06 -0600

    POST on PostDB working correctly

--- index.js
+++ index.js
@@ -52,7 +52,29 @@ server.get("/users/:id", (req, res) => {
         .json({ error: "The post information could not be retrieved." });
     });
 });
-// end GET //
+
+// GET USER POSTS REQUEST
+server.get("/users/:id/posts", (req, res) => {
+  const { id } = req.params;
+  userDB
+    .getUserPosts(id)
+    .then(user => {
+      if (user.length === 0) {
+        res
+          .status(404)
+          .json({ message: "The user with the specified ID does not exist." });
+      } else {
+        return res.status(200).json({ user });
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: "The post information could not be retrieved." });
+    });
+});
+
+// end GETS //
 
 // POST REQUEST //
 server.post("/users", nameToUpperCase, async (req, res) => {
@@ -123,30 +145,10 @@ server.put("/users/:id", nameToUpperCase, (req, res) => {
 });
 // end PUT //
 
-// GET USER POSTS REQUEST //
-server.get("/users/:id/posts", (req, res) => {
-  const { id } = req.params;
-  userDB
-    .getUserPosts(id)
-    .then(user => {
-      if (user.length === 0) {
-        res
-          .status(404)
-          .json({ message: "The user with the specified ID does not exist." });
-      } else {
-        return res.status(200).json({ user });
-      }
-    })
-    .catch(err => {
-      res
-        .status(500)
-        .json({ error: "The post information could not be retrieved." });
-    });
-});
-// end GET USER POSTS //
 //////////////================================= END USERDB REQUESTS =================================//////////////
 
 //////////////================================= START POSTDB REQUESTS =================================//////////////
+// GET REQUESTS
 server.get("/posts", (req, res) => {
   postDB
     .get()
@@ -179,7 +181,30 @@ server.get("/posts/:id", (req, res) => {
         .json({ error: "The post information could not be retrieved." });
     });
 });
-// end GET //
+// end GETS //
+
+// POST REQUEST //
+//// MUST BE ID OF EXISTING USER ////
+
+server.post("/posts", async (req, res) => {
+  const post = req.body;
+  if (!post.userId || !post.text) {
+    return res.status(400).json({
+      errorMessage: "Please provide the correct User ID and text."
+    });
+  } else {
+    try {
+      const response = await postDB.insert(post);
+      res.status(201).json({ message: "New post created successfully." });
+    } catch (err) {
+      res.status(500).json({
+        error: "There was an error while saving the post to the database."
+      });
+    }
+  }
+});
+// end POST //
+
 //////////////================================= END POSTDB REQUESTS =================================//////////////
 
 server.listen(8000, () => console.log("\n== API on port 8k ==\n"));
