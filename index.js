5305a27d5ce5282e52958adda42e62c79bd920ec
codejoncode <jonathanjholloway@gmail.com>
2018-08-29 17:46:11 -0500

    So now only the first letter of the name is capitalized and if there is other parts to the name as long as there is a space in between parts those parts will be capitalized

--- index.js
+++ index.js
@@ -5,12 +5,18 @@ const server = express();
 
 server.use(express.json());
 
-//middleware 
-function upperCase(req,res,next) {
-    console.log(req.body)
-    req.upperName = req.body.name.toUpperCase()
-    console.log(req.upperName); 
-    next(); 
+//middleware
+function upperCase(req, res, next) {
+  console.log(req.body.name);
+  let name = req.body.name
+  const newName = name
+    .split(" ")
+    .map(name => name[0].toUpperCase() + name.slice(1, name.length))
+    .join(" ");
+  console.log(newName);
+  req.upperName = newName;
+  console.log(req.upperName);
+  next();
 }
 
 server.get("/api/users", (req, res) => {
@@ -41,56 +47,67 @@ server.get("/api/users/:id", (req, res) => {
     });
 });
 
-server.post("/api/users", upperCase,  (req, res) => {
-  const {name}  = req.body;
-  const send = {name: req.upperName}
-  console.log(send)
-  if(name && name.length <= 128){
+server.post("/api/users", upperCase, (req, res) => {
+  const { name } = req.body;
+  const send = { name: req.upperName };
+  console.log(send);
+  if (name && name.length <= 128) {
     db.insert(send)
       .then(user => {
-        res.status(201).json(send)
-      }).catch(error => {
-        res.status(500).json({error})
+        res.status(201).json(send);
       })
-    } else {
-      res.status(400).json({message: "You need a name and it has to be less than 128 characters"})
+      .catch(error => {
+        res.status(500).json({ error });
+      });
+  } else {
+    res.status(400).json({
+      message: "You need a name and it has to be less than 128 characters"
+    });
   }
 });
 
 server.put("/api/users/:id", upperCase, (req, res) => {
-  const {name}  = req.body;
-  const {id} = req.params; 
-  const send = {name: req.upperName}
-  if(name && name.length <= 128){
+  const { name } = req.body;
+  const { id } = req.params;
+  const send = { name: req.upperName };
+  if (name && name.length <= 128) {
     db.update(id, send)
-    .then(user => {
-      if(user){
-        res.status(200).json(send)
-      } else {
-        res.status(404).json({message: `The user with id ${id} was not found.`})
-      }
-    }).catch(error => {
-      res.status(500).json({error})
-    })
+      .then(user => {
+        if (user) {
+          res.status(200).json(send);
+        } else {
+          res
+            .status(404)
+            .json({ message: `The user with id ${id} was not found.` });
+        }
+      })
+      .catch(error => {
+        res.status(500).json({ error });
+      });
   } else {
-    res.status(400).json({message: "You need a name and it has to be less than 128 characters"})
+    res.status(400).json({
+      message: "You need a name and it has to be less than 128 characters"
+    });
   }
 });
 
 server.delete("/api/users/:id", (req, res) => {
-  const {id} = req.params;
-  console.log(id)
+  const { id } = req.params;
+  console.log(id);
   db.remove(id)
     .then(user => {
-        if(user){
-          console.log(user)
-          res.status(204).end()
-        } else {
-          res.status(404).json({message: `The user with id ${id} was not found.`})
-        }
-    }).catch( error => {
-      res.status(500).json({error})
+      if (user) {
+        console.log(user);
+        res.status(204).end();
+      } else {
+        res
+          .status(404)
+          .json({ message: `The user with id ${id} was not found.` });
+      }
     })
+    .catch(error => {
+      res.status(500).json({ error });
+    });
 });
 
 server.listen(PORT, () => console.log(`\n== API on port ${PORT}==\n`));
