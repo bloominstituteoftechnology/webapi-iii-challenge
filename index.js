0e4eeee87bae0ab32595c68b85c69ab4f3b013ea
MichaelBoegner <cmikeb@gmail.com>
2018-08-29 18:06:28 -0400

    Posts Endpoint handlers get, getPostTags, and remove functional. Troubleshooting insert and update

--- index.js
+++ index.js
@@ -14,54 +14,114 @@ function auth(req, res, next){
     }
 }
 
+//==========================USERDB ENDPOINTS ===============================
+
 server.get('/users/:id', (req, res) => {
     userDb.get(req.params.id)
-        .then(posts => { 
-            res.status(200).json(posts); 
+        .then(users => { 
+            res.status(200).json(users); 
         })
         .catch(err => {
-            res.status(500).json({error: "The posts information could not be retrieved."})
+            res.status(500).json({error: "The users information could not be retrieved."})
         })
 })
 
 server.get('/user-posts/:id', (req, res) => {
     userDb.getUserPosts(req.params.id)
-        .then(posts => { 
-            res.status(200).json(posts); 
+        .then(users => { 
+            res.status(200).json(users); 
         })
         .catch(err => {
-            res.status(500).json({error: "The posts information could not be retrieved."})
+            res.status(500).json({error: "The user's posts could not be retrieved."})
         })
 })
 
 server.post('/users', auth, (req, res) => {
     userDb.insert(req.body)
-        .then(posts => { 
-            res.status(200).json(posts); 
+        .then(users => { 
+            res.status(200).json(users); 
         })
         .catch(err => {
-            res.status(500).json({error: "The posts information could not be retrieved."})
+            res.status(500).json({error: "The user could not be created."})
         })
 })
 
 server.put('/users/:id', auth, (req, res) => {
     userDb.update(req.params.id, req.body)
-        .then(posts => { 
-            res.status(200).json(posts); 
+        .then(users => { 
+            res.status(200).json(users); 
         })
         .catch(err => {
-            res.status(500).json({error: "The posts information could not be retrieved."})
+            res.status(500).json({error: "The user could not be updated."})
         })
 })
 
 server.delete('/users/:id', (req, res) => {
     userDb.remove(req.params.id)
+        .then(users => { 
+            res.status(200).json(users); 
+        })
+        .catch(err => {
+            res.status(500).json({error: "The user could not be deleted."})
+        })
+})
+
+
+//==========================POSTDB ENDPOINTS ===============================
+
+server.get('/posts/:id', (req, res) => {
+    postDb.get(req.params.id)
         .then(posts => { 
             res.status(200).json(posts); 
         })
         .catch(err => {
-            res.status(500).json({error: "The posts information could not be retrieved."})
+            res.status(500).json({error: "The post information could not be retrieved."})
         })
 })
+
+server.get('/posts-tags/:id', (req, res) => {
+    postDb.getPostTags(req.params.id)
+        .then(posts => { 
+            res.status(200).json(posts); 
+        })
+        .catch(err => {
+            res.status(500).json({error: "The post's tags could not be retrieved."})
+        })
+})
+
+server.post('/posts', (req, res) => {
+    postDb.insert(req.body)
+        .then(posts => { 
+            res.status(200).json(posts); 
+        })
+        .catch(err => {
+            res.status(500).json({error: "The post could not be created."})
+        })
+})
+
+server.put('/posts/:id', (req, res) => {
+    postDb.update(req.params.id, req.body)
+        .then(posts => { 
+            res.status(200).json(posts); 
+        })
+        .catch(err => {
+            res.status(500).json({error: "The post could not be updated."})
+        })
+})
+
+server.delete('/posts/:id', (req, res) => {
+    postDb.remove(req.params.id)
+        .then(posts => { 
+            res.status(200).json(posts); 
+        })
+        .catch(err => {
+            res.status(500).json({error: "The post could not be updated."})
+        })
+})
+
+
+
+
+
 
 server.listen(7000);
