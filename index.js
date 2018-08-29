5d025e79cb80975aeabc41e9ea59033b722c5a22
codejoncode <jonathanjholloway@gmail.com>
2018-08-29 15:54:41 -0500

    able to get individual user tested with chrome browser and postman

--- index.js
+++ index.js
@@ -1,32 +1,48 @@
-const express = require('express');
-const PORT = 9000; 
-const db = require("./data/helpers/userDb.js")
+const express = require("express");
+const PORT = 9000;
+const db = require("./data/helpers/userDb.js");
 const server = express();
 
-server.use(express.json())
+server.use(express.json());
 
-
-
-server.get('/api/users', (req,res) => {
+server.get("/api/users", (req, res) => {
   db.get()
     .then(users => {
-        console.log(users)
-        res.status(200).json(users)
-    }).catch(error => {
-        res.status(500).json({error})
+      console.log(users);
+      res.status(200).json(users);
     })
-})
+    .catch(error => {
+      res.status(500).json({ error });
+    });
+});
 
-server.post('/api/users', (req,res) => {
-  res.send("Creating")
-})
+server.get("/api/users/:id", (req, res) => {
+  const { id } = req.params;
+  db.get(id)
+    .then(user => {
+      if (user) {
+        res.status(200).json(user);
+      } else {
+        res
+          .status(404)
+          .json({ message: `The user with id ${id} was not found` });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ error });
+    });
+});
 
-server.put('/api/users/:id',(req,res) => {
-  res.send("Updating")
-})
+server.post("/api/users", (req, res) => {
+  res.send("Creating");
+});
 
-server.delete('/api/users/:id', (req, res) => {
-  res.send("Deleting")
-})
+server.put("/api/users/:id", (req, res) => {
+  res.send("Updating");
+});
 
-server.listen(PORT, () => console.log(`\n== API on port ${PORT}==\n`))
+server.delete("/api/users/:id", (req, res) => {
+  res.send("Deleting");
+});
+
+server.listen(PORT, () => console.log(`\n== API on port ${PORT}==\n`));
