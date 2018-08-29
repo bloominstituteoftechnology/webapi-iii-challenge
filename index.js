cbd52cb5fd724d382a03604fa8cbef6cd9fb411a
szincone <sawyerzincone@gmail.com>
2018-08-29 14:41:46 -0600

    individual GET request on users.db finished

--- index.js
+++ index.js
@@ -24,4 +24,24 @@ server.get("/users", (req, res) => {
     });
 });
 
+server.get("/users/:id", (req, res) => {
+  const id = req.params.id;
+  userDB
+    .get(id)
+    .then(user => {
+      if (user.length === 0) {
+        res
+          .status(404)
+          .json({ message: "The user with the specified ID does not exist." });
+      } else {
+        return res.status(200).json(user);
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: "The post information could not be retrieved." });
+    });
+});
+
 server.listen(8000, () => console.log("\n== API on port 8k ==\n"));
