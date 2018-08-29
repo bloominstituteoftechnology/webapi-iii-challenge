b98d349fcac6ec5949795d3613c3348168b7fa43
Charlie Winslow <cwinslow22@yahoo.com>
2018-08-29 16:35:39 -0500

    added functionality for get for users:id

--- server.js
+++ server.js
@@ -21,4 +21,20 @@ server.get('/api/users', (req, res) => {
     })
 })
 
+server.get('/api/users/:id', (req, res) => {
+    dbUser.get(req.params.id)
+    .then(user => {
+        // console.log(user);
+        if (!user) {
+            res.status(404).json({ message: 'The user with the specified ID does not exist.' });
+            return;
+        }
+        res.status(200).json(user);
+    })
+    .catch(err => {
+        console.error('error', err);
+        res.status(500).json({ error: 'The user information could not be retrieved.'})
+    })
+});
+
 server.listen(8000, () => console.log('/n== API on port 8k ==/n') );
