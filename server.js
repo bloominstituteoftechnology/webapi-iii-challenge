fbbc5715b8a211d9853640fb3bfc78afe21bd497
Sean <sean47900@gmail.com>
2018-08-29 17:07:36 -0500

    can view individual users

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const server = express();
 const userDb = require('./data/helpers/userDb.js');
-
+const postDb = require('./data/helpers/postDb.js');
 
  server.use(express.json());
 
@@ -18,5 +18,18 @@ server.get('/users', (req, res) => {
        res.status(500).json({ message: 'Error retrieving user data'})
     });
   });
+
+  server.get("/users/:id", (req,res) => {
+    const id = req.params.id; 
+    userDb.get(id).then(user => {
+        if(user){
+            res.status(200).json(user)
+        }else {
+            res.status(404).json({error: "User ID does not exist"})
+        }
+    }).catch(err => {
+        res.status(500).status.json({error:"User information could not be retrieved"})
+    })
+}); 
 
  server.listen(8000, () => console.log('/n== API on port 8000 ==/n') )
