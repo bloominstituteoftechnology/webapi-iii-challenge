54e874e100ff9bbec5590e2fce403d4e6f5eaeaf
szincone <sawyerzincone@gmail.com>
2018-08-29 13:53:44 -0600

    server is working with basic test

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = require("express");
-const db = require("./data/dbConfig.js");
 const server = express();
 
+const db = require("./data/dbConfig.js");
+
 server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.send("Working Server");
+});
+
+server.listen(8000, () => console.log("\n++ API on port 8k ==\n"));
