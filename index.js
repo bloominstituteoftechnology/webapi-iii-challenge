a3f843cc78308f2bf12b9373e5cb0e89688bc076
szincone <sawyerzincone@gmail.com>
2018-08-29 14:16:36 -0600

    usersdb GET all request working

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const express = require("express");
 const server = express();
 
-const db = require("./data/dbConfig.js");
+const userDB = require("./data/helpers/userDb.js");
 
 server.use(express.json());
 
-server.get("/", (req, res) => {
-  res.send("Working Server");
+// test request
+// server.get("/", (req, res) => {
+//   res.send("Working Server Test");
+// });
+
+// GET REQUEST
+server.get("/users", (req, res) => {
+  userDB
+    .get()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: "The users information could not be retrieved." });
+    });
 });
 
-server.listen(8000, () => console.log("\n++ API on port 8k ==\n"));
+server.listen(8000, () => console.log("\n== API on port 8k ==\n"));
