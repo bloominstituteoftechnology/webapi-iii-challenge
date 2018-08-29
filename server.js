30e4d425edcf688a97568b822b20e375ccf35d8e
Joseph Stossmeister <joseph.stossmeister@gmail.com>
2018-08-29 16:50:26 -0400

    Started working on posts, realized I didn't add getting a user's posts

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const users = require('./data/helpers/userDb');
+const posts = require('./data/helpers/postDb');
 
 
 const server = express();
@@ -31,6 +32,12 @@ server.get('/api/users/:id', (req, res) => {
         .catch(err => res.status(500).json({ error: "User information retrieval failed." }));
 });
 
+server.get('/api/users/:id/posts', (req, res) => {
+  users.getUserPosts(req.params.id)
+        .then(usrPosts => res.status(201).json(usrPosts))
+        .catch(err => res.status(500).json({ error: "User's Post information retrieval failed." }));
+});
+
 server.post('/api/users', capitalizeUsername, (req, res) => {
   const { name } = req.body;
   users.insert({ name })
@@ -59,5 +66,25 @@ server.put('/api/users/:id', capitalizeUsername, (req, res) => {
         .catch(err => res.status(500).json({ error: `Database Failure on Updating User with id ${id}` }));
 });
 
+//posts endpoints
+server.get('/api/posts', (req, res) => {
+  posts.get()
+});
+
+server.get('/api/posts/:id', (req, res) => {
+
+});
+
+server.post('/api/posts', (req, res) => {
+
+});
+
+server.delete('/api/posts/:id', (req, res) => {
+
+});
+
+server.put('/api/posts/:id', (req, res) => {
+
+});
 
 server.listen(9000, ()=> console.log('SERVER - PORT: 9000 - LISTENING'));
