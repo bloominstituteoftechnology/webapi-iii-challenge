51cab1f1fd69b3443e2711413e454f36cedc4c6c
kamalnrf <kamalnrf@hotmail.com>
2018-08-30 01:18:43 +0530

    Add controllers for update, delete and get user posts

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -33,3 +33,52 @@ exports.insertUser = async (req, res) => {
             })
     }
 }
+
+exports.updateUser = async (req, res) => {
+    try{
+        const updatedUser = await _usersDB.update(req.params.id, req.body)
+        res.status(200)
+            .json({
+                updatedUser
+            })
+    }catch(err){
+        res.status(500)
+            .json({
+                error:"There was a problem in updating the user",
+                errorLog: err
+            })
+    }
+}
+
+exports.deleteUser = async (req, res) => {
+    try{
+        const removeUser = await _usersDB.remove(req.params.id)
+        res.status(200)
+            .json({
+                status: true,
+                removeUser
+            })
+    }catch(err){
+        res.status(500)
+            .json({
+                error: "There was a problem in removing user",
+                errorLog: err
+            })
+    }
+}
+
+exports.getUserPosts = async (req, res) => {
+    try{
+        const userPosts = await _usersDB.getUserPosts(req.params.id)
+        res.status(200)
+            .json({
+                userPosts
+            })
+    }catch(err){
+        res.status(500)
+            .json({
+                error: "There was a retriving user posts",
+                errorLog: err
+            })
+    }
+}
