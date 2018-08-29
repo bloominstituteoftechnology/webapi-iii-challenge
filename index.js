9a2162c98153efdd8734773ae79d78aff9658b7c
Katherine Gilligan <katia.gilligan88@gmail.com>
2018-08-29 15:39:49 -0600

    Added delete and put methods for users

--- index.js
+++ index.js
@@ -43,6 +43,33 @@ server.post("/api/users", (req, res) => {
     } else {res.status(400).json({error: "Please provide a name for the user"})}
 }); 
 
+server.delete("/api/users/:id", (req, res) => {
+    const id = req.params.id; 
+    userDb.remove(id).then(count => {
+        if(count > 0){
+            res.status(200).json({message: "Deleted user successfully!"})
+        }
+    }).catch(err => {
+        res.status(500).json({error: "Database error in deleting user"})
+    })
+}); 
 
+server.put("/api/users/:id", (req, res) => {
+    const id = req.params.id; 
+    const updatedData = req.body; 
+    if(updatedData.name){
+        userDb.update(id, updatedData).then(count => {
+            if(count > 0){
+                res.status(200).json({message:"User information updated"})
+            }else {
+                res.status(404).json({error: "The user with the specified ID does not exist"})
+            }
+        }).catch(err => {
+            res.status(500).json({error: "Error when updating user information in database"})
+        })
+    }else {
+        res.status(400).json({message: "Please provide name in updated data"})
+    }
+})
 
 server.listen(5000); 
