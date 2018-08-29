087243ff23bee10705280f518dc8786e784be12c
Lauren <llwor94@gmail.com>
2018-08-29 01:51:02 -0700

    get and post user methods working

--- server.js
+++ server.js
@@ -13,4 +13,55 @@ server.get('/', (req, res) => {
   res.send('Sup fam')
 })
 
+//user requests 
+
+server.get('/users', async (req, res) => {
+  try {
+    let data = await userdb.get();
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json(error);
+  }
+});
+
+server.get('/users/:id', async (req, res) => {
+  try {
+    let data = await userdb.get(req.params.id);
+    if (data) {
+      return res.status(200).json(data);
+    } else {
+      return res.status(404).json({ error: 'This user does not exist.' })
+    }
+  } catch (err) {
+    res.status(500).json(err)
+  };
+})
+
+server.get('/userPosts/:id', async (req, res) => {
+  try {
+    let data = await userdb.getUserPosts(req.params.id)
+    if (data.length > 0) {
+      return res.status(200).json(data)
+    } else {
+      return res.status(404).json({ error: 'This user does not have any posts.' })
+    }
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
+
+server.post('/users', async (req, res) => {
+  let { name } = req.body;
+  try {
+    if (name) {
+      let data = await userdb.insert(req.body)
+      return res.status(200).jspon(data)
+    } else {
+      return res.status(404).json({ error: 'Please provide a name for this user.' })
+    }
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
+
 server.listen(8000, () => console.log("\n== API on port 8k ==\n"))
