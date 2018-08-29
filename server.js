3180da15b9152ed25f2b4e932e46a42a3f53619e
William <kwontaewoongah@gmail.com>
2018-08-29 15:38:12 -0700

    implemented put endpoint functionality

--- server.js
+++ server.js
@@ -21,4 +21,17 @@ server.post('/users',(req,res)=>{
         .catch(err=>res.status(500).json({ error: "There was an error while saving the user to the database" }))
     }
 })
+server.put('/users/:id',(req,res)=>{
+    const user=req.body;
+    if (!user) {
+        res.status(400).json({errorMessage:"Please enter a user name"});
+    } else {
+        users.update(req.params.id,user).then(count=>{
+            count===1?
+            users.get(req.params.id).then(user=>res.status(200).json(user)).catch(err=>console.log(err)):
+            req.status(404).json({message: "The post with the specified ID does not exist."})
+        }
+    ).catch(err=>res.status(500).json({ error: "The post information could not be modified." }))
+    }
+})
 server.listen(9000,()=>console.log('Engines firing server starting new horizons venturing.'))
