7ba65ae7a65aee2eacb0fd5d34888f1cd2155ee5
jaymzsocool <james.mcraejr@gmail.com>
2018-08-29 18:15:28 -0400

    added get and working on post

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
-
-const server = express('short');
+const dbUsers = require('./data/helpers/userDb.js')
+const dbPosts = require('./data/helpers/postDb.js')
+const server = express();
 
 logger = (req, res, next) => {
     console.log(`${req.method} to ${req.url}`)
@@ -8,11 +9,39 @@ logger = (req, res, next) => {
 }
 
 server.use(logger);
+server.use(express.json());
 
 
 server.get('/', (req, res) =>{
     res.send('running')
 })
+
+server.get('/users', (req, res) => {
+    dbUsers.get()
+        .then(users => {
+            res.status(200).json(users);
+        })
+        .catch(err => {
+            console.error('error', err);
+            res.status(500).json({ error: "The User information could not be retrieved."})
+        });
+});
+
+server.post('/users', (req, res) => {
+    const user = req.body;
+    dbUsers.insert(user)
+        .then(response =>
+            db.findById(response.id).then(user => {
+                res.status(201).json(user);
+        })
+        )        
+        .catch( err => {
+            console.error('error', err);
+            res.status(500).json({ error: "There was an error while saving the User to the Database."})
+        })
+})
+
+
 
 server.listen(5000, () =>     
     console.log(`server is listening on port 5000`));
