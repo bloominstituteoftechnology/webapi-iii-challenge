6f22ef4bed01059905377574d0c20626ab908a54
Jun Hyuk Kim <junhyukkeem@gmail.com>
2018-08-29 11:58:30 -0700

    implement POST method on users, implement middleware to capitalize first letter
    still need to do full name, if applicable

--- index.js
+++ index.js
@@ -7,8 +7,12 @@ const app = express();
 app.use(express.json());
 
 function upperUser(req, res, next){
-	req.body.name = req.body.name[0].toUpperCase() + req.body.name.substr(1, req.body.name.length-1);
-	next();
+	if (!req.body || !req.body.name){
+		res.status(422).json({ message: 'A user needs a name'})
+	} else {
+		req.body.name = req.body.name[0].toUpperCase() + req.body.name.substr(1, req.body.name.length-1);
+		next();
+	}
 }
 
 app.get('/', (req, res) => {
@@ -25,19 +29,8 @@ app.get('/users', async (req, res) => {
 	}
 });
 
-app.get('/posts', async (req, res) => {
-	try {
-		const posts = await postDB.get();
-		res.status(200).json(posts);
-	}
-	catch(err) {
-		res.status(500).json({ message: 'Error getting the data'});
-	}
-});
-
 app.get('/users/:id', async (req, res) => {
 	const { id } = req.params;
-	console.log(id);
 	try {
 		const user = await userDB.get(id);
 		res.status(200).json(user);
@@ -49,7 +42,6 @@ app.get('/users/:id', async (req, res) => {
 
 app.get('/users/:id/posts', async (req, res) => {
 	const { id } = req.params;
-	console.log(id);
 	try {
 		const posts = await userDB.getUserPosts(id);
 		res.status(200).json(posts);
@@ -58,6 +50,39 @@ app.get('/users/:id/posts', async (req, res) => {
 		res.status(500).json({ message: 'Error getting the data'});
 	}
 });
+
+app.get('/posts', async (req, res) => {
+	try {
+		const posts = await postDB.get();
+		res.status(200).json(posts);
+	}
+	catch(err) {
+		res.status(500).json({ message: 'Error getting the data'});
+	}
+});
+
+app.get('/posts/:id', async (req, res) => {
+	const { id } = req.params;
+	try {
+		const post = await postDB.get(id);
+		res.status(200).json(post);
+	}
+	catch(err) {
+		res.status(500).json({ message: 'Error getting the data'});
+	}
+});
+
+app.post('/users', upperUser, async (req,res) => {
+	const { name } = req.body;
+	try {
+		const count = await userDB.insert({name});
+		res.status(201).json({message: 'User successfully added'});
+	}
+	catch(err) {
+		res.status(500).json({ message: 'Something went wrong in our server '})
+	}
+});
+
 
 // app.post('/users', async (req, res) => {
 // 	const user = req.body;
