5b17e0a0b904d9d75deace9e609a61f3b98a8f3a
jordan-massingill <jordan_massingill@hotmail.com>
2018-08-29 17:26:42 -0500

    reached MVP
    Added all CRUD functionality
    Added middleware to uppercase the new names input to the userDb

--- index.js
+++ index.js
@@ -6,13 +6,17 @@ const userDb = require('./data/helpers/userDb.js');
 
 const postDb = require('./data/helpers/postDb.js');
 
+function upperName(req, res, next) {
+  req.body.name = req.body.name.toUpperCase();
+  next();
+}
 
 server.use(express.json());
 
 // configure routing
 
 server.get('/users', (req, res) => {
-  userDb.find().then(users => {
+  userDb.get().then(users => {
     res.status(200).json(users)
   }).catch(err => {
     console.error('error', err);
@@ -21,14 +25,24 @@ server.get('/users', (req, res) => {
   });
 });
 
-server.post('/users', authenticate, async (req, res) => {
-  const user = {
-    "name": req.body.name,
-    "bio": req.body.bio
-  };
-  if (user.name && user.bio) {
+server.get('/users/:id', (req, res) => {
+  userDb.get(parseInt(req.params.id)).then(user => {
+    if (user === undefined) {
+      res.status(404).json({ message: 'Sorry: the user specified does not exist'});
+    }
+    else {
+      res.status(200).json(user);
+    }
+  }).catch(err => {
+    res.status(500).json({ message: 'Sorry, there was an error saving the user'})
+  })
+})
+
+server.post('/users', upperName, async (req, res) => {
+  const user = req.body;
+  if (user.name) {
     try {
-      const response = await db.insert(user);
+      const response = await userDb.insert(user);
       res.status(201).json({ message: "User was successfully added"});
     }
     catch (err) {
@@ -36,8 +50,40 @@ server.post('/users', authenticate, async (req, res) => {
     }
   }
   else {
-    res.status(422).json({ message: "Must have both name and bio to add user"});
+    res.status(422).json({ message: "Must have a name to add user"});
   }
+})
+
+server.put('/users/:id', upperName, (req, res) => {
+  console.log(req.params.id);
+  if (!req.body.name) {
+    res.status(422).json({ message: 'Must have a name to add user.'});
+  }
+  else {
+    userDb.update(req.params.id, req.body).then(count => {
+      if (count !== 1) {
+        res.status(404).send('The user you requested does not exist');
+      }
+      else {
+        res.status(201).json({ message: 'User was successfully updated!'});
+      }
+    }
+  ).catch(err => {
+    res.status(500).json({ message: 'Sorry, there was an issue with the server'});
+  })
+}});
+
+server.delete('/users/:id', (req, res) => {
+  userDb.remove(req.params.id).then(count => {
+    if (count !== 1) {
+      res.status(404).json({ message: 'The user with the specified ID does not exist.' });
+    }
+    else {
+      res.status(200).json({ message: 'The user was successfully deleted' });
+    }
+  }).catch(err => {
+    res.status(500).json({ message: 'The user could not be deleted - please try again'})
+  })
 })
 
 server.listen(8000, ()=>console.log('Hello!'))
