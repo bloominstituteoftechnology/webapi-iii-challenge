79ebeae941b63fc26611b34e63962b199cb4ae3e
William <kwontaewoongah@gmail.com>
2018-08-29 16:03:46 -0700

    implemented middleware to uppercase users name

--- server.js
+++ server.js
@@ -6,13 +6,18 @@ const server=express();
 const users = require('./data/helpers/userDb.js');
 server.use(express.json()).use(cors()).use(morgan('dev')).use(helmet());
 
+function upperCase(req, res, next) {
+    req.body.name=req.body.name.toUpperCase();
+    next();
+  }
+
 server.get('/users',(req,res)=>{
     users.get().then(user=>res.status(200).json(user)).catch(err=>res.status(500).json({error: "The users information could not be retrieved."}))}
     );
 server.get('/users/:id',(req,res)=>{
     users.get(req.params.id).then(user=>res.status(200).json(user)).catch(err=>res.status(500).json({error:"The users information could not be retrieved"}))
 })
-server.post('/users',(req,res)=>{
+server.post('/users',upperCase, (req,res)=>{
     const user=req.body;
     if (!user) {
         res.status(400).json({errorMessage:"Please enter a user name"})
@@ -38,4 +43,5 @@ server.delete('/users/:id',(req,res)=>{
     users.remove(req.params.id).then(count=>count>0? res.status(200).json({success:`Deleted ${count} user(s)`}):res.status(404).json({ message: "The user with the specified ID does not exist." })
     ) .catch(err=>res.status(500).json({error: "The user could not be removed" }))
 })
+
 server.listen(9000,()=>console.log('Engines firing server starting new horizons venturing.'))
