1800c1b7230415b8ade030bc8f8a9bda9e765598
MichaelBoegner <cmikeb@gmail.com>
2018-08-29 17:05:26 -0400

    userDb Endpoints in Place and Functional
    Adding middleware for uppercase user name check

--- index.js
+++ index.js
@@ -1,12 +1,53 @@
 const express = require('express');
-const db = require('./data/helpers/userDb.js');
+const userDb = require('./data/helpers/userDb.js');
+const postDb = require('./data/helpers/postDb');
 const server = express();
 server.use(express.json());
 
 
 
 server.get('/users/:id', (req, res) => {
-    db.get(req.params.id)
+    userDb.get(req.params.id)
+        .then(posts => { 
+            res.status(200).json(posts); 
+        })
+        .catch(err => {
+            res.status(500).json({error: "The posts information could not be retrieved."})
+        })
+})
+
+server.get('/user-posts/:id', (req, res) => {
+    userDb.getUserPosts(req.params.id)
+        .then(posts => { 
+            res.status(200).json(posts); 
+        })
+        .catch(err => {
+            res.status(500).json({error: "The posts information could not be retrieved."})
+        })
+})
+
+server.post('/users', (req, res) => {
+    userDb.insert(req.body)
+        .then(posts => { 
+            res.status(200).json(posts); 
+        })
+        .catch(err => {
+            res.status(500).json({error: "The posts information could not be retrieved."})
+        })
+})
+
+server.put('/users/:id', (req, res) => {
+    userDb.update(req.params.id, req.body)
+        .then(posts => { 
+            res.status(200).json(posts); 
+        })
+        .catch(err => {
+            res.status(500).json({error: "The posts information could not be retrieved."})
+        })
+})
+
+server.delete('/users/:id', (req, res) => {
+    userDb.remove(req.params.id)
         .then(posts => { 
             res.status(200).json(posts); 
         })
