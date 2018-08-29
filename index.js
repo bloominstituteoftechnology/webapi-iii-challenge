4751373d977cd53fda118a35d412332b9cd1f8db
Ashwin <aswinjeganath@gmail.com>
2018-08-30 02:29:18 +0530

    Add dependencies, set up server

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const logger = require('morgan');
+const helmet = require('helmet');
+
+const app = express();
+
+// middleware config
+app.use(cors());
+app.use(logger('short'));
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(5000, () => console.log('\nServer listening on port 5000\n'));
