e19710e918a616879af4cea189d4c9b67849fa5e
Katherine Gilligan <katia.gilligan88@gmail.com>
2018-08-29 14:54:55 -0600

    Added dependencies, pkg.json, and post/get for users

---
+++ index.js
@@ -0,0 +1,48 @@
+
+const express = require('express'); 
+const server =  express(); 
+const userDb = require('./data/helpers/userDb.js')
+
+server.use(express.json());
+
+//User CRUD operations
+
+server.get("/api/users", (req,res) => {
+    userDb.get().then(users => {
+        res.status(200).json(users)
+    }).catch(err => {
+        res.status(500).json({error: "There was an error retrieving the data"})
+    })
+}); 
+
+server.get("/api/users/:id", (req,res) => {
+    const id = req.params.id; 
+    userDb.get(id).then(user => {
+        if(user){
+            res.status(200).json(user)
+        }else {
+            res.status(404).json({error: "A user with the specified ID does not exist"})
+        }
+    }).catch(err => {
+        res.status(500).status.json({error:"The user information could not be retrieved"})
+    })
+}); 
+
+server.post("/api/users", (req, res) => {
+    const data = req.body
+    if (data.name){
+        userDb.insert(data).then(userId => {
+            userDb.get(userId.id).then(user => {
+                res.status(201).json(user)
+            }).catch(err => {
+                res.status(500).json({error: "Error accessing new user from database"})
+            })
+        }).catch(err => {
+            res.status(500).json({error: "There was an error updating the information in the database"})
+        })
+    } else {res.status(400).json({error: "Please provide a name for the user"})}
+}); 
+
+
+
+server.listen(5000); 
