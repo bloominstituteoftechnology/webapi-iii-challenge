527e0773b2a581e62374e6ced3b030cd7fa48c23
LizBaker <lizbaker.pdx@gmail.com>
2018-08-30 16:10:29 -0700

    Added errorhandler middleware, used routing for user routes -working on post endpoints

--- index.js
+++ index.js
@@ -1,80 +1,59 @@
 const express = require('express');
 const server = express();
-const users = require("./data/helpers/userDb.js");
+const routes = require('./routes');
+const posts = require("./data/helpers/postDb");
+const path = require('path');
+const middle = require('./middleware');
+
+
 
 server.use(express.json());
+server.use('/api/users', routes)
 
-//middleware
-
-function uppercase (req, res, next) {
-    req.body.name = req.body.name.charAt().toUpperCase() + req.body.name.slice(1);  
-    next();
-}
-
-
-// routes
-
-server.get("/api/users", (req, res) => {
-    users.get()
-      .then(users => {
-        res.status(200).json(users);
-      })
-      .catch(err => {
-        console.log("error", err);
-        res.status(500).json({ error: "The user information could not be retrieved." });
-      });
-  });
-
-server.get("/api/users/:id", (req, res)=> {
-      users.get(req.params.id)
-      .then(users => {
-        res.status(200).json(users);
-      })
-      .catch(err => {
-        console.log("error", err);
-        res.status(500).json({ error: "The user information could not be retrieved." });
-      });
-  })
-
-server.post("/api/users", uppercase, (req, res)=> {
-    if (req.body.name.length < 128){
-    users.insert(req.body)
-    .then(users => {
-        res.status(200).json(users);
-      })
-      .catch(err => {
-        console.log("error", err);
-        res.status(500).json({ error: "The user information could not be posted." });
-      });}
-      else {
-          res.status(401).json({error: "dats too long"})
-      }
+server.get("/api/posts", (req, res) => {
+  posts
+    .get()
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(err => {
+      console.log("error", err);
+      res
+        .status(500)
+        .json({ error: "The post information could not be retrieved." });
+    });
 });
 
-server.put("/api/users/:id", uppercase, (req, res) => {
-    users.update(req.params.id, req.body)
-      .then(users => {
-        res.status(200).json(users);
-      })
-      .catch(err => {
-        console.log("error", err);
-        res.status(500).json({ message: "error posting data" });
-      });
+server.get('/download', (req, res, next) => {
+    const filePath = path.join(__dirname, 'index.html');
+    res.sendFile(filePath, err => {
+      if (err) {
+        next(err); 
+      } else {
+        console.log('File sent successfully');
+      }
+    });
   });
+
+//   server.use(errorHandler);
+//   function errorHandler(err, req, res, next) {
+//     console.error(err);
   
-server.delete("/api/users/:id", (req, res) => {
-    users.remove(req.params.id)
-      .then(users => {
-        res.status(200).json(users);
-      })
-      .catch(err => {
-        console.log("error", err);
-        res.status(500).json({ message: "error deleting data" });
-      });
-  });
+//     switch (err.statusCode) {
+//       case 404:
+//         res.status(404).json({
+//           message: 'that file aint herrrrrrre',
+//         });
   
-
-
-
+//         break;
+  
+//       default:
+//         res.status(500).json({
+//           message: 'There was an error performing the required operation',
+//         });
+//         break;
+//     }
+//   }
+middle(server);
 
 server.listen(7000, ()=> console.log('~API chillin on 7k~'));
