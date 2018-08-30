97e613699650fb1b3f5edc6a7fe1b345a4591b0d
mkerbleski <mkerbles@gmail.com>
2018-08-30 15:32:20 -0600

    check for valid userId.

--- posts/postRoutes.js
+++ posts/postRoutes.js
@@ -5,8 +5,26 @@ const express = require('express');
 const router = express.Router();
 
 const postDb = require('../data/helpers/postDb.js');
+const userDb = require('../data/helpers/userDb.js');
 
 router.use(express.json());
+
+function isValidUser(req, res, next){
+  userDb.get().then(allUsers => {
+    let filter = allUsers.filter(users => {
+      return users.id == req.body.userId
+    })
+    console.log(filter[0])
+    if(filter[0]) {
+      next();
+    } else {
+      res.status(403).json({message: "userId is not valid. Please try again."})
+    }
+  }).catch(err => {
+    res.status(500).json({message: "error validating userId"})
+  })
+}
+
 
 router.get('/', (req, res) => {
   console.log('posts requested')
@@ -22,17 +40,25 @@ router.get('/:id', (req, res) => {
   })
 });
 
-router.post('/', (req, res) => {
-  //text is required
+router.post('/', isValidUser, (req, res) => {
   //userId is required from existing user
   // if Id present say it will be disgarded
-  postDb.insert(req.body)
-    .then(postId => {
-      res.status(200).json(postId)
-    })
-    .catch(err => {
-      res.status(400).json(err)
-    })
+// console.log('filter', filter)
+  if (!req.body.text) {
+    res.json(400).json({message: "please add text and resubmit request"})
+  } else if (!req.body.userId) {
+    res.json(400).json({message: "please include userId and resubmit request"})
+  } else if (req.body.id){
+    res.status(400).json({message: 'please remove id and submit again.'})
+  } else {
+    postDb.insert(req.body)
+      .then(postId => {
+        res.status(200).json(postId)
+      })
+      .catch(err => {
+        res.status(400).json(err)
+      })
+  }
 });
 
 router.put('/:id', (req, res) => {
