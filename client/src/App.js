71e41d8a03229bdcb5216bc73d7fcc858ac71db3
codejoncode <jonathanjholloway@gmail.com>
2018-08-30 17:09:55 -0500

    able to display all of the users

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios'; 
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      users: []
+    }
+  }
+
+  componentDidMount() {
+    this.fetchUsers();
+  }
+
+  fetchUsers = () => {
+    const promise = axios.get('http://localhost:9000/api/users');
+    promise
+      .then(response => {
+        this.setState({users: response.data})
+      })
+      .catch(error => {
+        console.log(error); 
+      })
+  }
   render() {
+    console.log(this.state.users)
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {this.state.users.map(user => <div key = {user.id}>{user.name}</div>)}
+        
       </div>
     );
   }
