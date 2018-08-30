84f580b121d30e73ee61557180a5e31fed7d4a73
johnoro <officialjohnorourke@gmail.com>
2018-08-30 16:14:29 -0400

    Displaying the list of users now.

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,35 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = { users: [] };
+  }
+
+  URL = 'http://localhost:5000/api';
+
+  componentDidMount() {
+    axios.get(`${this.URL}/users`)
+      .then(res => this.setState({ users: res.data }))
+      .catch(err => console.error(err));
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="users">
+          {this.state.users.map(user => {
+            return (
+              <div className="user" key={user.id}>
+                <h3>{user.name}</h3>
+              </div>
+            );
+          })}
+        </div>
       </div>
     );
   }
