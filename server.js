1f6ff745352db208e616076eb63e07608432d177
Sean <sean47900@gmail.com>
2018-08-30 14:05:49 -0500

    put and delete methods

--- server.js
+++ server.js
@@ -47,4 +47,33 @@ server.post("/users", (req, res) => {
     } else {res.status(400).json({error: "Name required"})}
 }); 
 
+server.put("users/:id", (req, res) => {
+    const id = req.params.id; 
+    const updatedData = req.body; 
+    if(updatedData.name){
+        userDb.update(id, updatedData).then(count => {
+            if(count > 0){
+                res.status(200).json({message:"User information updated"})
+            }else {
+                res.status(404).json({error: "User ID does not exist"})
+            }
+        }).catch(err => {
+            res.status(500).json({error: "Error updating"})
+        })
+    }else {
+        res.status(400).json({message: "Must provide Name"})
+    }
+})
+
+server.delete("users/:id", (req, res) => {
+    const id = req.params.id; 
+    userDb.remove(id).then(count => {
+        if(count > 0){
+            res.status(200).json({message: "Deleted user"})
+        }
+    }).catch(err => {
+        res.status(500).json({error: "Error deleting user"})
+    })
+}); 
+
  server.listen(8000, () => console.log('/n== API on port 8000 ==/n') )
