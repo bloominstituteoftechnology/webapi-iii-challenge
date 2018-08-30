63662b18484cf20af08ae1782b830d6a00449446
William <kwontaewoongah@gmail.com>
2018-08-29 19:43:27 -0700

    refactored from then catch to async await

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const morgan=require('morgan');
 const helmet=require('helmet');
 const server=express();
 const users = require('./data/helpers/userDb.js');
+const posts=require('./data/helpers/postDb.js');
 server.use(express.json()).use(cors()).use(morgan('dev')).use(helmet());
 
 function upperCase(req, res, next) {
@@ -11,37 +12,57 @@ function upperCase(req, res, next) {
     next();
   }
 
-server.get('/users',(req,res)=>{
-    users.get().then(user=>res.status(200).json(user)).catch(err=>res.status(500).json({error: "The users information could not be retrieved."}))}
-    );
-server.get('/users/:id',(req,res)=>{
-    users.get(req.params.id).then(user=>res.status(200).json(user)).catch(err=>res.status(500).json({error:"The users information could not be retrieved"}))
-})
-server.post('/users',upperCase, (req,res)=>{
-    const user=req.body;
-    if (!user) {
-        res.status(400).json({errorMessage:"Please enter a user name"})
-    } else {
-        users.insert(user).then(user=>res.status(201).json(user))
-        .catch(err=>res.status(500).json({ error: "There was an error while saving the user to the database" }))
+server.get('/users',async (req,res)=>{
+    try {
+        const user=await(users.get());
+        res.status(200).json(user);
+    } catch(error) {
+        res.status(500).json({error: "User(s) information could not be retrieved."})
+    }
+});
+server.get('/users/:id',async(req,res)=>{
+    try{
+        const user=await (users.get(req.params.id));
+        user===undefined?res.status(404).json({message:"The user with the specific ID does not exist"}):res.status(200).json(user);
+    } catch(error){
+        res.status(500).json({error:"User(s) information could not be retrieved"});
     }
 })
-server.put('/users/:id',upperCase,(req,res)=>{
+server.post('/users', upperCase, async(req,res)=>{
     const user=req.body;
-    if (!user) {
-        res.status(400).json({errorMessage:"Please enter a user name"});
-    } else {
-        users.update(req.params.id,user).then(count=>{
-            count===1?
-            users.get(req.params.id).then(user=>res.status(200).json(user)).catch(err=>console.log(err)):
-            req.status(404).json({message: "The user with the specified ID does not exist."})
+    if (user) {
+        try {
+            const response=await users.insert(user);
+            res.status(201).json(user);
+        } catch (error) {
+            res.status(500).json({ error: "There was an error while saving the user to the database" });
         }
-    ).catch(err=>res.status(500).json({ error: "The user information could not be modified." }))
+       }   else {
+        res.status(400).json({message: 'A user needs a name'});
     }
 })
-server.delete('/users/:id',(req,res)=>{
-    users.remove(req.params.id).then(count=>count>0? res.status(200).json({success:`Deleted ${count} user(s)`}):res.status(404).json({ message: "The user with the specified ID does not exist." })
-    ) .catch(err=>res.status(500).json({error: "The user could not be removed" }))
+server.put('/users/:id', upperCase, async(req,res)=>{
+    const user=req.body;
+    if (user) {
+        try {
+            const response=await users.update(req.params.id,user);
+            response===1?res.status(200).json(user):res.status(404).json({message:"The user with the specified ID does not exist"});
+        }
+        catch(error) {
+            res.status(500).json({ error: "The user information could not be modified." })     
+        }
+    } else {
+        res.status(400).json({errorMessage:"Please enter a user name"});
+    }
+})
+server.delete('/users/:id',async(req,res)=>{
+    try{
+        const removed=await users.remove(req.params.id);
+        removed===1?res.status(200).json({success: `Deleted user.`}):
+        res.status(404).json({message:"The user with the specified ID does not exist"})
+    } catch(error) {
+        res.status(500).json({error: "The user could not be removed" })
+    }
 })
 
 server.listen(9000,()=>console.log('Engines firing server starting new horizons venturing.'))
