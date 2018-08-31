806c408afdbfec3be29981b965fca0e6bebbf5f0
szincone <sawyerzincone@gmail.com>
2018-08-30 19:55:44 -0600

    cd'd out to update server

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const cors = require("cors");
 const userRoutes = require("../users/userRoutes.js");
 const postRoutes = require("../posts/postRoutes.js");
 
@@ -7,7 +7,7 @@ module.exports = server => {
   // causes express middleware
   // stack to be added to every layer (request function)
   server.use(express.json());
-
+  server.use(cors());
   // must be used when using express Router
   // links url with requests
   server.use("/users", userRoutes);
