3a668ebcdc5cd3fc33ff200f9967eac0918b3c7c
AI2EJAY <JHammett1314@gmail.com>
2018-09-05 17:05:36 -0500

    Having troulbe retreiving the data

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
 const express = require("express");
+const postDb = require("./data/helpers/postDb");
+const userDb = require("./data/helpers/userDb");
 const server = express();
+
+const upercaseChecker = (req, res, next) => {};
 
 server.use(express.json());
 
 server.get("/", (req, res) => {
-    res.send('Hello')
+  res.send("Hello");
 });
 
-server.listen(9000, () => console.log("Up and running"))
+// server.get("/posts", (req, res) => {
+//     console.log(postDb)
+//   postDb
+//     .find()
+//     .then(posts => {
+//       res.status(200).json(posts);
+//     })
+//     .catch(err => {
+//       console.error("error", err);
+//     });
+// });
+
+server.get("/users", (req, res) => {
+  userDb
+    .find()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => {
+      console.error("error", err);
+    });
+});
+
+server.listen(8000, () => console.log("Up and running"));
