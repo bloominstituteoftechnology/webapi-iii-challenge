c0af530592e93df45f87341716f84f135be32f34
Kairi <kairijb@gmail.com>
2018-09-05 17:13:59 -0500

    capitalizes user's name

--- index.js
+++ index.js
@@ -20,15 +20,16 @@ const logger = (req, res, next) => {
 
 const lengthCheck = (req, res, next) => {
     if (req.body.length < 1 || req.body.lengh > 128) {
-        res.status(401).json({message: 'name is required and cannot exceed 128 characters'})
+        res.status(401).json({ message: 'name is required and cannot exceed 128 characters' })
     } else {
+        req.body.toUppercase();
         next();
     }
 }
 
 const textCheck = (req, res, next) => {
     if (req.body.length < 1) {
-        res.status(401).json({message: 'text is required'})
+        res.status(401).json({ message: 'text is required' })
     } else {
         next();
     }
@@ -36,6 +37,7 @@ const textCheck = (req, res, next) => {
 
 server.use(logger)
 server.use(lengthCheck)
+server.use(textCheck)
 
 // configure routing
 server.get('/', (req, res) => {
@@ -51,28 +53,28 @@ server.get('/users/:id', (req, res) => {
     db.find(id).then(posts => {
         res.status(200).json(posts)
     })
-    .catch(err => res.status(404).json({ message: "The user with the specified ID does not exist." }))
+        .catch(err => res.status(404).json({ message: "The user with the specified ID does not exist." }))
 })
 
 server.post('/users', lengthCheck, (req, res) => {
     db.insert(req.body)
-    .then(response => res.status(201).json({message: 'user creation success'}))
-    .catch(err => res.status(500).json({message: 'error creating user'}))
+        .then(response => res.status(201).json({ message: 'user creation success' }))
+        .catch(err => res.status(500).json({ message: 'error creating user' }))
 })
 
 server.delete('/users/:id', (req, res) => {
-    const {id} = req.params
+    const { id } = req.params
 
     db.remove(id)
-    .then(count => res.status(204).end())
-    .catch(err => res.status(500).json({message: 'delete unsuccessful'}))
+        .then(count => res.status(204).end())
+        .catch(err => res.status(500).json({ message: 'delete unsuccessful' }))
 })
 
 server.put('/users/:id', (req, res) => {
-    const {id} = req.params
+    const { id } = req.params
     db.update(id, req.body)
-    .then(users => res.status(200).json(users))
-    .catch(err => res.status(500).json({message: 'user update fail'}))
+        .then(users => res.status(200).json(users))
+        .catch(err => res.status(500).json({ message: 'user update fail' }))
 })
 
 // POSTS
@@ -83,28 +85,28 @@ server.get('/posts/:id', (req, res) => {
     db.find(id).then(posts => {
         res.status(200).json(posts)
     })
-    .catch(err => res.status(404).json({ message: "The post with the specified ID does not exist." }))
+        .catch(err => res.status(404).json({ message: "The post with the specified ID does not exist." }))
 })
 
-server.post('/posts', (req, res) => {
+server.post('/posts', textCheck, (req, res) => {
     db.insert(req.body)
-    .then(response => res.status(201).json({message: 'post creation success'}))
-    .catch(err => res.status(500).json({message: 'error creating posts'}))
+        .then(response => res.status(201).json({ message: 'post creation success' }))
+        .catch(err => res.status(500).json({ message: 'error creating posts' }))
 })
 
 server.delete('/posts/:id', (req, res) => {
-    const {id} = req.params
+    const { id } = req.params
 
     db.remove(id)
-    .then(count => res.status(204).end())
-    .catch(err => res.status(500).json({message: 'delete unsuccessful'}))
+        .then(count => res.status(204).end())
+        .catch(err => res.status(500).json({ message: 'delete unsuccessful' }))
 })
 
 server.put('/posts/:id', (req, res) => {
-    const {id} = req.params
+    const { id } = req.params
     db.update(id, req.body)
-    .then(posts => res.status(200).json(posts))
-    .catch(err => res.status(500).json({message: 'post update fail'}))
+        .then(posts => res.status(200).json(posts))
+        .catch(err => res.status(500).json({ message: 'post update fail' }))
 })
 
 
