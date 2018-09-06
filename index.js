81964ccfc5f52e65812ae5914493c8c8f0182e25
AI2EJAY <JHammett1314@gmail.com>
2018-09-06 11:51:41 -0500

    All CRUD operations working.
    -I still need to go through them and change status codes/ make sure they are schemed correctly.

--- index.js
+++ index.js
@@ -2,20 +2,24 @@ const express = require("express");
 const postDb = require("./data/helpers/postDb.js");
 const userDb = require("./data/helpers/userDb.js");
 const server = express();
-
-const upercaseChecker = (req, res, next) => {
-    word = req.body.name
-  if (word[0] === word[0].toUpperCase()) {
-      next()
-  } else res.status(400).json({error: "Please enter a capitilized/propercase name"})
-};
-
 server.use(express.json());
 
+const upercaseChecker = (req, res, next) => {
+  word = req.body.name;
+  if (word[0] === word[0].toUpperCase()) {
+    next();
+  } else
+    res
+      .status(400)
+      .json({ error: "Please enter a capitilized/propercase name" });
+};
+
+//TEST=========================
 server.get("/", (req, res) => {
   res.send("Hello");
 });
-
+//==============================
+// USER===================================================================
 server.get("/users", (req, res) => {
   userDb
     .get()
@@ -41,17 +45,32 @@ server.get("/users/:id", (req, res) => {
     });
 });
 
-server.post("/users", upercaseChecker , (req, res) => {
-  const postContents = req.body;
+server.get("/users/:id/posts", (req, res) => {
+  const id = req.params.id;
+  userDb
+    .getUserPosts(id)
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(err => {
+      console.error("error", err);
+      res
+        .status(500)
+        .json({ error: "The USER POST data could not be retrieved" });
+    });
+});
+
+server.post("/users", upercaseChecker, (req, res) => {
+  const userContents = req.body;
   const response = userDb.insert(postContents);
   userDb
-    .insert(postContents)
+    .insert(userContents)
     .then(() => {
       res.status(203).json(response);
     })
     .catch(err => {
       console.error("error", err);
-      res.status(422).json({ error: "Unable to create post" });
+      res.status(422).json({ error: "Unable to create user" });
     });
 });
 
@@ -79,21 +98,79 @@ server.delete("/users/:id", (req, res) => {
       res.status(500).json({ error: "UNABLE TO DELETE" });
     });
 });
+// ===================================================================
 
+//POSTS===============================================================
+server.get("/posts", (req, res) => {
+  postDb
+    .get()
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(err => {
+      console.error("error", err);
+      res.status(500).json({ error: "There was an error accessing the posts" });
+    });
+});
+
+server.get("/posts/:id", (req, res) => {
+  id = req.params.id;
+  postDb
+    .get(id)
+    .then(post => {
+      res.status(200).json(post);
+    })
+    .catch(err => {
+      console.error("error", err);
+      res.status(500).json({ error: "Can't gather post" });
+    });
+});
+
+server.post("/posts", (req, res) => {
+  const postContents = req.body;
+  postDb
+    .insert(postContents)
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(err => {
+      console.error("error", err);
+      res.status(500).json({ error: "Something went wrong with your post" });
+    });
+});
+
+server.put("/posts/:id", (req, res) => {
+  const id = req.params.id;
+  const postContents = req.body;
+  postDb
+    .update(id, postContents)
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(err => {
+      console.error("error", err);
+      res.status(500).json({ message: "Unable to UPDATE post" });
+    });
+});
+
+server.delete("/posts/:id", (req, res) => {
+  const id = req.params.id;
+  postDb
+    .remove(id)
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(err => {
+      console.error("error", err);
+      res.status(500).json({
+        message: "There was an error when trying to delete this post"
+      });
+    });
+});
+
+// ===================================================================
 const port = 8000;
 
 server.listen(port, err => {
   console.log(`\n=== The server is up and running on ${port} ===\n`);
 });
-
-// server.get("/posts", (req, res) => {
-//     console.log(postDb)
-//   postDb
-//     .find()
-//     .then(posts => {
-//       res.status(200).json(posts);
-//     })
-//     .catch(err => {
-//       console.error("error", err);
-//     });
-// });
