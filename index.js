104cc703b531136741161f9e6214b3f34fb3352d
AlexanderCaves <ahcaves@gmail.com>
2018-09-06 16:13:52 -0700

    working on endpoints; trouble with db module

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 const express = require('express');
 
+const db = require('../dbConfig.js');
+
 const server = express();
 
-server.listen(7000, () => console.log('\n== API on port 7k ==n\l'));
+//middleware
+
+server.use(express.json());
+
+//routes
+server.get('/users', (req, res) => {
+    db.find()
+        .then(users => {
+            res.status(200).json(users);
+        })
+        .catch(err => {
+            console.error('error', err);
+
+            res.status(500).json({ error: "The user information could not be retrieved." })
+        });
+})
+
+server.listen(7000, () => console.log('\n== API on port 7k ==\n'));
