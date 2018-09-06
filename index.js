b094a60f968e8f132270b7cee9409303bf6046b3
Rachel DiCesare <racheldicesare@missrachelthedeveloper.com>
2018-09-05 22:02:40 -0400

    added posts endpoints

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ server.get('/', (req,res) => {
     res.send('What is this madness?')
 });
 
+// users
+
 server.get('/users' , (req, res) => {
     userDb.get()
     .then(users => {
@@ -72,7 +74,68 @@ server.put('/users/:id', upperName, (req , res) => {
     })
     .catch(err => res.status(500).json({ message: 'update failed'}));  
    })
-   
+
+   // posts
+
+   server.get('/posts', (req, res) => {
+    postDb.get().then(posts => {
+        res.status(200).json(posts);
+    }).catch(err => {
+        console.error('error',err);
+
+        res.status(500).json({ message:'Error getting the posts'});
+    })
+});
+
+server.get('/posts/:id', (req, res) => {
+    postDb.findById(parseInt(req.params.id)).then(post => {
+      console.log(post);
+      if (post.length === 0) {
+        res.status(404).json({  message: 'The post with the specified ID does not exist.' });
+      }
+      else {
+        res.status(200).json(post)
+    }}).catch(err => {
+      console.error('error', err);
+       res.status(500).json({ error: 'The post information could not be retrieved.'})
+    })
+  });
+
+  server.post('/posts', (req, res) => {
+      if(!req.body.title || !req.body.contents){
+          res.status(400).json({errorMessage: 'Please provide title and contents for post.'})
+      }
+      postDb.insert(req.body).then(id => {
+          res.status(201).json(id)
+      }).catch(err => {
+          console.error('error',err);
+          res.status(500).json({error: 'There was an error while saving the post to the database.'})
+      })
+  });
+
+  server.delete('/posts/:id', (req, res) => {
+      const { id } = req.params;
+
+      postDb.remove(id)
+      .then(posts => {
+        if (posts.length === 0) {
+            res.status(404).json({ message: "The post with the specified ID does not exist"});
+        } else {res.status(200).json(posts);
+        }
+      })
+      .catch(err => {
+          res.status(500).json({error: "The post could not be removed"});
+      })
+  });
+
+
+
+  server.put('/posts/:id', (req, res) => {
+    postDb.update(req.params.id, req.body).then(posts => {
+        res.status(200).json(posts)
+    })
+    .catch(err => res.status(500).json({ message: 'update failed'}));
+})
 
 
 
