0a97ecbd2e3ac12386083db5bdb1158bb158064e
Erica Thompson <ericathompson.nc@gmail.com>
2018-09-10 05:05:33 -0700

    Delete Complete

--- server.js
+++ server.js
@@ -38,30 +38,85 @@ server.get('/posts', (req, res) => {
 
 
 
-// server.delete('/api/posts/:id', (req, res) => {
-//     const {id} = req.params;
 
-//         db.findById(id)
-//             .then((post) => {
-//                 res.status(201).json(post);
-//             })
-//             .catch((fail) => {
-//                 console.log(fail);
-//                 res.status(404).json({ message: "The post with the specified ID does not exist."});
-//             });
-  
-//     db.remove(id)
-//         .then((posts) => {
-//             response.json(posts);
+
+
+
+// server.post('/users', (req, res) => {
+//     const {user} = req.body;
+//     const {id} = req.params;
+    
+//     users.insert(user)
+//         .then((user)=> {
+//             const id = user.id;
+
+//             users.insert(id)
+//                 .then((user) => {
+//                     res.status(201).json(user);
+//                 })
+//                 .catch((fail) => {
+//                     console.log(fail);
+//                     res.status(500).json({ error: "There was an error while saving the post to the database." });
+//                 });
 //         })
 //         .catch((fail) => {
 //             console.log(fail);
-//             response.status(500).json({
-//                 error: "The post could not be removed"
-//             });
-//         })
+//             res.status(500).json({ error: "Catch Fail: There was an error while saving the post to the database." });
+//         });
 // });
 
+
+
+
+
+server.delete('/users/:id', (req, res) => {
+    const {id} = req.params;
+
+        users.getUserPosts(id)
+            .then((user) => {
+                res.status(201).json(user);
+            })
+            .catch((fail) => {
+                console.log(fail);
+                res.status(404).json({ message: "The post with the specified ID does not exist."});
+            });
+
+    users.remove(id)
+        .then((user) => {
+            response.json(user);
+        })
+        .catch((fail) => {
+            console.log(fail);
+            response.status(500).json({
+                error: "The post could not be removed"
+            });
+        })
+});
+
+
+server.delete('/posts/:id', (req, res) => {
+    const {id} = req.params;
+
+        posts.getPostTags(id)
+            .then((post) => {
+                res.status(201).json(post);
+            })
+            .catch((fail) => {
+                console.log(fail);
+                res.status(404).json({ message: "The post with the specified ID does not exist."});
+            });
+
+    posts.remove(id)
+        .then((post) => {
+            response.json(post);
+        })
+        .catch((fail) => {
+            console.log(fail);
+            response.status(500).json({
+                error: "The post could not be removed"
+            });
+        })
+});
 
 
 
