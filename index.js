b1bde4ae2ceab8718ac937c0104df286acf7afc4
Kevin Nguyen <kevinn858@gmail.com>
2018-10-03 15:59:10 -0700

    added get with insert

--- index.js
+++ index.js
@@ -29,20 +29,79 @@ server.get('/api/:dbtype', (req, res)=>{
             break;
         case 'users':
             userDb.get()
-                .then(posts =>{
-                    res.status(200).json(posts);
+                .then(users =>{
+                    res.status(200).json(users);
                 })
                 .catch(err => console.error(err));
             break;
         case 'tags':
             tagDb.get()
-                .then(posts =>{
-                    res.status(200).json(posts);
+                .then(tags =>{
+                    res.status(200).json(tags);
                 })
                 .catch(err => console.error(err));
             break;
         default:
-            res.status(400).send(`${dbtype} not found`);
+            res.status(400).send(`URI ${dbtype} not found`);
     }
 });
 
+server.get('/api/:dbtype/:id', (req, res)=>{
+    const {dbtype, id} = req.params;
+    // res.status(200).send(`dbtype:${dbtype}\nid:${id}`);
+    switch (dbtype){
+        case 'posts':
+            postDb.get(id)
+                .then(post =>{
+                    res.status(200).json(post);
+                })
+                .catch(err => console.error(err));
+            break;
+        case 'users':
+            userDb.get(id)
+                .then(user =>{
+                    res.status(200).json(user);
+                })
+                .catch(err => console.error(err));
+            break;
+        case 'tags':
+            tagDb.get(id)
+                .then(tag =>{
+                    res.status(200).json(tag);
+                })
+                .catch(err => console.error(err));
+            break;
+        default:
+            res.status(400).send(`URI ${dbtype} not found`);
+    }
+});
+
+server.post('/api/:dbtype', (req, res)=>{
+    const {dbtype} = req.params;
+    const item = req.body;
+    switch (dbtype){
+        case 'posts':
+            postDb.insert(item)
+                .then(post =>{
+                    res.status(200).json(post);
+                })
+                .catch(err => console.error(err));
+            break;
+        case 'users':
+            userDb.insert(item)
+                .then(user =>{
+                    res.status(200).json(user);
+                })
+                .catch(err => console.error(err));
+            break;
+        case 'tags':
+            tagDb.insert(item)
+                .then(tag =>{
+                    res.status(200).json(tag);
+                })
+                .catch(err => console.error(err));
+            break;
+        default:
+            res.status(400).send(`URI ${dbtype} not found`);
+    }
+})
