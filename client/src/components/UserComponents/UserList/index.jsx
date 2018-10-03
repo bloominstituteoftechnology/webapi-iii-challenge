07df2e37f647c2975ed01493a7434074fe4808e8
decagondev <decagondevelopment@gmail.com>
2018-10-03 23:03:17 +0100

    (STRETCH) : refactored UserList still need to hook it up in App component

---
+++ client/src/components/UserComponents/UserList/index.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "../components.css";
+import "./index.css";
+import { Link } from "react-router-dom";
+
+const UserList = props => {
+  if (!props.users) {
+    return <div>Users are loading...</div>;
+  }
+  // if the users do exist then we can slice them and reverse them so that newest user is shown first
+  const users = props.users.slice().reverse();
+
+  return (
+    <div className="main-container">
+      <h2>Users:</h2>
+      <div className="user-previews-container">
+        {users.map(user => {
+          return (
+            <Link to={`/users/${user.id}`} key={Math.random()}>
+              <div className="user-preview-container">
+                <h3>{user.name}</h3>
+              </div>
+            </Link>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default UserList;
