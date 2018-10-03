ce5323f66d954de07965b64839080c7a4c54ef5d
Eva Li <evalee904@gmail.com>
2018-10-03 17:32:04 -0500

    User endpoint functioning.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const logger = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const port = 5000;
-// const portDb = require('./data/helpers/postDb');
+const portDb = require('./data/helpers/postDb');
 // const tagDb = require('./data/helpers/tagDb');
 const userDb = require('./data/helpers/userDb');
 const server = express();
@@ -31,7 +31,37 @@ server.get('/api/users', (req, res) => {
 })
 
 server.post('/api/users', (req, res) => {
-  console.log(req.body);
+ //console.log(req.body); 
+  const { name } = req.body;
+  const newUser = { name }
+  userDb.insert(newUser)
+    .then(user => {
+      console.log('\n--- New user added ---\n, user');
+      res.status(201).json(user);
+    })
+    .catch(err => res.json({ Error: 'Error adding a new user.'}));
+})
+
+server.put('/api/users/:id', (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+  const editUser = { name }
+  userDb.update(id, editUser)
+    .then(user => {
+      console.log('\n--- User edited ---\n, user');
+      res.status(200).json(user);
+    })
+    .catch(err => res.json({Error: 'Error editting user.'}));
+})
+
+server.delete('/api/users/:id', (req, res) => {
+  const { id } = req.params;
+  userDb.remove(id)
+    .then(removedUser => {
+      console.log('\n--- User removed ---\n, removedUser');
+      res.status(200).json(removedUser);
+    })
+    .catch(err => res.json({Error: 'Error removing user.'}));
 })
 
 
