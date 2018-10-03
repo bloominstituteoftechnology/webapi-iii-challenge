c320ea6f639e6ed17af1c6aafc066b087ec1acfa
decagondev <decagondevelopment@gmail.com>
2018-10-03 22:34:42 +0100

    (STRETCH) : added users to app state and map over the state users

--- client/src/components/App/index.jsx
+++ client/src/components/App/index.jsx
@@ -2,12 +2,24 @@ import React, { Component } from "react";
 import "./index.css";
 
 class App extends Component {
+  state = {
+    users: [
+      { name: "bob" },
+      { name: "dave" },
+      { name: "steve" },
+      { name: "jim" }
+    ]
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1>Node Blog</h1>
-        </header>
+        <h1>Node Blog</h1>
+        <div>
+          {this.state.users.map(user => {
+            return <h3>{user.name}</h3>;
+          })}
+        </div>
       </div>
     );
   }
