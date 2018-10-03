5f950bc8131961bbc73445d88a6588fd36781967
Eva Li <evalee904@gmail.com>
2018-10-03 18:13:50 -0500

    Post endpoint functioning. Day 1 done.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const logger = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const port = 5000;
-const portDb = require('./data/helpers/postDb');
+const postDb = require('./data/helpers/postDb');
 // const tagDb = require('./data/helpers/tagDb');
 const userDb = require('./data/helpers/userDb');
 const server = express();
@@ -21,6 +21,7 @@ server.get('/', (req, res) => {
   res.json('Hi');
 });
 
+//Users
 server.get('/api/users', (req, res) => {
   userDb.get()
     .then(users => {
@@ -62,6 +63,50 @@ server.delete('/api/users/:id', (req, res) => {
       res.status(200).json(removedUser);
     })
     .catch(err => res.json({Error: 'Error removing user.'}));
+})
+
+//Posts
+server.get('/api/posts', (req, res) => {
+  postDb.get()
+    .then(posts => {
+      console.log('\n** posts **', posts);
+      res.json(posts);
+    })
+    .catch(err => res.json({Error: 'Error getting posts.'}));
+})
+
+server.post('/api/posts', (req, res) => {
+//  console.log(req.body); 
+  const { text, userId } = req.body;
+  const newPost = { text, userId }
+  postDb.insert(newPost)
+    .then(post => {
+      console.log('\n--- New post added ---\n, post');
+      res.status(201).json(post);
+    })
+    .catch(err => res.json({ Error: 'Error adding a new post.'}));
+})
+
+server.put('/api/posts/:id', (req, res) => {
+  const { id } = req.params;
+  const { text, userId } = req.body;
+  const editPost = { text, userId }
+  postDb.update(id, editPost)
+    .then(post => {
+      console.log('\n--- Post edited ---\n, post');
+      res.status(200).json(post);
+    })
+    .catch(err => res.json({Error: 'Error editting post.'}));
+})
+
+server.delete('/api/posts/:id', (req, res) => {
+  const { id } = req.params;
+  postDb.remove(id)
+    .then(removedPost => {
+      console.log('\n--- Post removed ---\n, removedPost');
+      res.status(200).json(removedPost);
+    })
+    .catch(err => res.json({Error: 'Error removing post.'}));
 })
 
 
