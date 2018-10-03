4497a50a1cfce48c0604a0c2f216c0621f26e0a6
decagondev <decagondevelopment@gmail.com>
2018-10-03 22:59:48 +0100

    (STRETCH) : refactored User component in to a class based component still need to hook it up with a UserList

--- client/src/components/UserComponents/User/index.jsx
+++ client/src/components/UserComponents/User/index.jsx
@@ -1,5 +1,43 @@
-import React from "react";
+// import React from "react";
 
-const User = props => <div>{props.user}</div>;
+// const User = props => <div>{props.user}</div>;
 
-export default User;
+// export default User;
+
+import React, { Component } from "react";
+import axios from "axios";
+export default class User extends Component {
+  state = {
+    isEditing: false,
+    user: null,
+    name: ""
+  };
+
+  get id() {
+    return this.props.match.params.id;
+  }
+
+  componentDidMount() {
+    axios
+      .get(`http://localhost:8000/api/users/${this.id}`)
+      .then(response => {
+        this.setState({
+          user: response.data[0],
+          name: response.data[0].name
+        });
+      })
+      .catch(error => console.log(error));
+  }
+
+  render() {
+    if (!this.state.user) {
+      return <div className="main-container User">User is loading...</div>;
+    }
+
+    return (
+      <div className="main-container user">
+        <h2>{this.state.name}</h2>
+      </div>
+    );
+  }
+}
