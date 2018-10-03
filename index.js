02468a94fd30e30b000e2ce15f535935644d7fc3
Brock Maraee Rohloff <00AgentB@gmail.com>
2018-10-03 15:44:54 -0600

    Implement initial CRUD Endpoint: Root Server READ Endpoint which sends string: It's Working!

--- index.js
+++ index.js
@@ -3,12 +3,23 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const helmet = require('helmet');
+
+// Database Helpers
 const userDb = require('./data/helpers/userDb');
 const postDb = require('./data/helpers/postDb');
 
 const server = express();
 
-server.use(cors(), morgan('combined'), helmet());
+server.use(express.json(), cors(), morgan('combined'), helmet());
 
+// ===============- SERVER CRUD ENDPOINTS -===============
+
+// #####=- Root Server READ Endpoint -=#####
+server.get('/', (request, response) => {
+    response.status(200).send(`It's working!`);
+})
+
+
+// #####=- Server Port Address and Listen Method -=#####
 port = 9999;
 server.listen(port, () => {console.log(`-=-=-=- Node Blog Server Active on Port ${port} -=-=-=-`)});
