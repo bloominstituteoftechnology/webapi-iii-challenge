40c06a5ae6b0d562426d9e78fd78998c6c8f52bf
Timothy Hoang <timh1203@gmail.com>
2018-10-03 16:43:55 -0400

    Add POST method for users route

--- index.js
+++ index.js
@@ -8,19 +8,35 @@ const port = 8000;
 const server = express();
 server.use(express.json());
 
-// ROUTE HANDLERS
+// MIDDLEWARE
+const upperCaser = (req, res, next) => {
+  req.name = req.body.name.toUpperCase();
+  next();
+}
+
 server.route('/users')
   .get((req, res) => {
     userDb.get()
     .then(data => res.status(200).json(data))
-    .catch(err => res.status(500).json({ error: "The requests for users could not be retrieved." }));
+    .catch(err => res.status(500).json({ error: "The request for users could not be retrieved." }));
+  })
+  .post(upperCaser, (req, res) => {
+    const { name } = req;
+    const newUser = {};
+    newUser.name = name;
+    console.log(newUser);
+      if (!name) return res.status(400).json({ errorMessage: "Please provide a user name." });
+      userDb.insert(newUser)
+        .then(user => res.status(201).json(user))
+        .catch(err => res.status(500).json({ error: "There was an error while saving the post to the database" }));
   })
 
+// POSTS ROUTE HANDLERS
 server.route('/posts')
   .get((req, res) => {
     postDb.get()
     .then(data => res.status(200).json(data))
-    .catch(err => res.status(500).json({ error: "The requests for posts could not be retrieved." }));
+    .catch(err => res.status(500).json({ error: "The request for posts could not be retrieved." }));
   })
 
 // PORT LISTENERS
