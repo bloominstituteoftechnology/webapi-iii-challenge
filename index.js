2f1e63a151040344a24ce1f131aa429bec7a3169
Nicocchi <nicocchilover@gmail.com>
2018-10-03 16:18:35 -0400

    CRUD completed for users

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ const postDb = require('./data/helpers/postDb.js');
 const server = express();
 const port = 9000;
 
-const naming = (req, res, next) => {
-    const name = req.params.name.toUpperCase();
+const nameToUpperCase = (req, res, next) => {
+    let name;
+    if (req.body.name) name = req.body.name.toUpperCase();
+    if (!req.body.name) name = 'NO NAME';
     req.name = name;
     next();
 }
@@ -18,15 +20,15 @@ server.use(logger('combined'), express.json());
 server.get('/api/users', (req, res) => {
     userDb.get()
         .then(user => {
-            res.json(user);
+            res.send(user);
         })
-        .catch(err => res.status(500).json({error: `The users information coud not be retrieved. | ${err}`}));
+        .catch(err => res.status(500).send({error: `The users information coud not be retrieved. | ${err}`}));
 });
 
-server.post('/api/user/:name', naming, (req, res) => {
+server.post('/api/users/', nameToUpperCase, (req, res) => {
     const name  = req.name;
     console.log(name);
-    if ( !name ) return res.status(400).json({error: 'Please provide a name for the user.'});
+    if ( !name ) return res.status(400).send({error: 'Please provide a name for the user.'});
 
     const newUser = { name };
     userDb.insert(newUser)
@@ -34,13 +36,38 @@ server.post('/api/user/:name', naming, (req, res) => {
             const { id } = userId;
             userDb.get(id)
                 .then(user => {
-                    if(!user) {
-                        return res.status(422).json({error: `User does not exist by that id ${userId}`});
-                    }
-                    res.status(201).json(user);
+                    if(!user) return res.status(422).send({error: `User does not exist by that id ${userId}`});
+
+                    res.status(201).send(user);
                 });
         })
-        .catch(err => res.status(500).json({error: `There was an error while saving the suer to the database. | ${err}`}));
+        .catch(err => res.status(500).send({error: `There was an error while saving the user to the database. | ${err}`}));
 });
+
+server.put('/api/users/:id', nameToUpperCase, (req, res) => {
+    const { id } = req.params;
+    const  name  = req.name;
+    console.log(name);
+    if ( !name ) return res.status(400).send({error: 'Please provide a name for the user.'});
+
+    const newUser = { name };
+    userDb.update(id, newUser)
+        .then(user => {
+            if(!user) return res.status(422).send({error: `User does not exist by that id ${userId}`});
+            res.status(200).send(user);
+        })
+        .catch(err => res.status(500).send({error: `The user could not be modified. | ${err}`}));
+});
+
+server.delete('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+    console.log(id);
+    userDb.remove(id)
+        .then(rmvdUser => {
+            if(!rmvdUser) return res.status(404).send({error: `The user with the ID of ${id} does not exist.`});
+            res.status(200).send({message: 'The user deleted successfully'});
+        })
+        .catch(err => res.status(500).send({error: `The user could not be removed. | ${err}`}))
+})
 
 server.listen(port, () => console.log(`\n=== API running on port ${port} ===\n`));
