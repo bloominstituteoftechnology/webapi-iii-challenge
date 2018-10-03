fef640d069b1b423afb173d56cba6c6637626968
Vera Simon <ghjkl>
2018-10-03 12:37:09 -0700

    Node Blog - Day I
    It mightve helped if I added express and cors as well >.>
    Simple listener set up on ":8080/" returning a json object

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Hello world!");
+const express = require('express');
+const cors = require('cors');
+
+const server = express();
+server.use(cors());
+server.use(express.json());
+
+
+server.get('/', (req, res) => {
+    res.status(200).json({"message": "I AM ROOT!"});
+});
+
+
+const port = 8080;
+server.listen(port, () => console.log(`\n~~~ Server listening on port ${port} ~~~\n`));
