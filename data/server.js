ee45a9f60eab13d927e4e4b0719b5c9aa80e3ff4
Csillag61 <csillagok@gmail.com>
2018-10-03 16:55:57 -0400

    Custom mw, user endpoints created

---
+++ data/server.js
@@ -0,0 +1,119 @@
+const express = require("express");
+const cors = require("cors");
+const posts = require("./data/helpers/postDb");
+const users = require("./data/helpers/userDb");
+const tags = require(".data/helpers/tagDb");
+const port = 3333;
+const server = express();
+server.use(express.json());
+server.use(cors({}));
+
+const errorHelper = (status, message, res) => {
+  res.status(status.json({ error: message }));
+};
+
+const nameCheck = (req, res, next) => {
+  const { name } = req.body;
+  if (!name) {
+    errorHelper(404, "Name must be added", res);
+    next();
+  }
+};
+
+server.get("/api/users", (req, res) => {
+  users
+    .get()
+    .then(getUsers => {
+      res.json(getUsers);
+    })
+    .catch(err => {
+      return errorHelper(500, "Internal Server Error", res);
+    });
+});
+server.post("/api/users", nameCheck, (req, res) => {
+  const { name } = req.body;
+  users
+  .insert({ name })
+  .then(response => {
+    res.json(response);
+  })
+  .catch(err => {
+      return errorHelper(500, "Internal Server Error", res);
+  });
+});
+
+server.get("/api/users", (req, res) => {
+    users
+        .get()
+        .then(getUsers => {
+            res.json(getUsers);
+        })
+        .catch(err => {
+            return errorHelper(500, "Internal Server Error", res);
+        });
+});
+server.post("/api/users/:id", (req, res) => {
+    const { id } = req.body;
+    users
+        .get( id )
+        .then(user => {
+            if (user ===0){
+                return errorHelper(404, 'No user by that Id in the DB', res);
+            }
+            res.json(user);
+        })
+        .catch(err => {
+            return errorHelper(500, "Internal Server Error", res);
+        });
+});
+
+server.get('/api/users/posts/:userId', (req, res)=>{
+    const {userId} = req.params;
+    .getUserPosts(userId)
+        .then(usersPosts => {
+            if (usersPosts === 0) {
+                return errorHelper(404, 'No user by that Id in the DB', res);
+            }
+            res.json(usersPosts);
+        })
+        .catch(err => {
+            return errorHelper(500, "Internal Server Error", res);
+        });
+});
+server.delete("/api/users/:id", (req, res) => {
+    const { id } = req.params;
+    users
+        .remove(id)
+        .then(userRemoved => {
+            if (userRemoved === 0) {
+                return errorHelper(404, 'No user by that Id in the DB', res);
+            }else{
+            res.json({success: "User Removed"});
+            }
+        })
+        .catch(err => {
+            return errorHelper(500, "Internal Server Error", res);
+        });
+});
+server.put("/api/users/:id", nameCheck, (req, res) => {
+    const { id } = req.params;
+    const {name} = req.body;
+    users
+        .update(id, {name})
+        .then(response => {
+            if (response === 0) {
+                return errorHelper(404, 'No user by that Id in the DB', res);
+            } else {
+                db.find(id).then(user=>{
+                res.json(user);
+            });
+        }
+        })
+        .catch(err => {
+            return errorHelper(500, "Internal Server Error", res);
+        });
+});
+
+
+
+server.listen(port, () => console.log(`Server listening on ${port}`));
