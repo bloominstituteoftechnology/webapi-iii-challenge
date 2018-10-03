a5dbc33c352cfbf2231df40da67e37197e196a71
unknown <cornielleandres@gmail.com>
2018-10-03 17:26:50 -0400

    Add DELETE request handler to server.

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ server.post('/api/users', yell, (req, res) => {
 				})
 				.catch(err => res.status(500).json({ error: 'The newly created user\'s information could not be retrieved.' }));
 		})
-		.catch(err => res.status(500).json({ error: 'There was an error while saving the user to the database' }));
+		.catch(err => res.status(500).json({ error: 'There was an error while saving the user to the database.' }));
 });
 
 server.put('/api/users/:id', yell, (req, res) => {
@@ -82,7 +82,19 @@ server.put('/api/users/:id', yell, (req, res) => {
 				})
 				.catch(err => res.status(500).json({ error: 'The newly updated user\'s information could not be retrieved.' }));
 		})
-		.catch(err => res.status(500).json({ error: 'There was an error while updating the user information in the database' }));
+		.catch(err => res.status(500).json({ error: 'There was an error while updating the user information in the database.' }));
+});
+
+server.delete('/api/users/:id', (req, res) => {
+	const { id } = req.params;
+	userDb.remove(parseInt(id))
+		.then(del => {
+			if (!del) {
+				return res.status(404).json({ error: `The user with the ID ${ id } does not exist.` });
+			}
+			return res.status(200).json({ message: `The user with id ${ id } was successfully deleted.` });
+		})
+		.catch(err => res.status(500).json({ error: 'There was an error while deleting the user in the database.' }));
 });
 
 server.listen(port, () => {
