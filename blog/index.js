83f01eb93b3068ce1ae736e39a65997d651783a0
Alexandra <alexswartz9753@gmail.com>
2018-10-03 19:04:07 -0400

    added request handlers, all of which currently return 500 error

--- blog/index.js
+++ blog/index.js
@@ -20,19 +20,67 @@ server.get('/', (req, res)=> {
 });
 
 server.get('/api/users/:id', (req, res)=> {
-    console.log(req.query);
     userDb.get(req.query)
-     .then(user=> {
-         if (user) {
-             res.status(200).json(user)
-         } else {
-             res.status(404).json('User not found');
-         }
-     })
-     .catch(err=> {
-         res.status(500).json({error: "The user information could not be retrieved."});
-     });
- });
+        .then(users=> {
+            console.log(users);
+            res.status(200).json(users);
+        })
+        .catch(err=> {
+            res.status(500).json({error: "Information could not be retrieved."});
+        })
+});
+
+server.post('/api/users', (req, res)=> {
+    const {name} = req.body;
+    const newUser = {name};
+    userDb.insert(newUser)
+        .then(userId=> {
+            const {id} = userId;
+            userDb.get(id)
+                .then(user=> {
+                    if (!req.body.name) {
+                        res.status(404).json({error: "User not found"});
+                    }
+                    res.status(201).json(user);
+                })
+        })
+        .catch(err=> {
+            res.status(500).json({error: "This user could not be added to the database"});
+        })
+});
+
+server.delete('/api/users/:id', (req, res)=> {
+    const {id} = req.params;
+    userDb.remove(id)
+        .then(users=> {
+            if (!users) {
+                res.status(404).json({message: "The user with the specified ID does not exist."});
+            }
+            res.status(200).json(users);
+        })
+        .catch(err=> {
+            res.status(500).json({error: "The user could not be removed"});
+        })
+});
+
+server.put('/api/users/:id', (req, res)=> {
+    const {id} = req.params;
+    const {name} = req.body;
+    const updatedUser = {name};
+    userDb.update(id, updatedUser)
+        .then(user=> {
+            if (!user) {
+                res.status(404).json({error: "This user does not exist"});
+            } else if (!req.body) {
+                res.status(400).json({error: "Please add a name for this user"});
+            } else {
+                res.status(200).json(user);
+            }
+        })
+        .catch(err=> {
+            res.status(500).json({error: "This information could not be saved to the database"});
+        })
+});
 
 //Listener
 server.listen(port, ()=> console.log(`API running on port ${port}`));
