1ce706ab88bcd17e941de8a68835510f77172eac
unknown <cornielleandres@gmail.com>
2018-10-03 17:50:38 -0400

    Create front end skeleton with react.

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+	render() {
+		return (
+			<div className = 'App'>
+				<header>
+					<h1>Node Blog</h1>
+				</header>
+			</div>
+		);
+	}
+}
