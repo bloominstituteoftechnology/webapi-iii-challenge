3b0e0bec417da2ee1ecfc441548c3b8c91d92150
Evan Carlstrom <evan.carlstrom@gmail.com>
2018-10-03 19:17:17 -0400

    adding and removing users working properly

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const port = 8000;
 
 const db = require('./data/helpers/userDb.js');
 
+server.use(express.json());
+
 server.get('/', (req, res) => {
   res.send('Server test.');
 });
@@ -22,12 +24,36 @@ server.get('/users', (req, res) => {
 
 server.get('/users/:id', (req, res) => {
   const { id } = req.params;
-  db.getUserPosts(id).then(post => {
-    console.log('\n** posts by specified user **', post);
-    res.json(post);
+  db.getUserPosts(id)
+    .then(post => {
+      console.log('\n** posts by specified user **', post);
+      res.json(post);
   })
   .catch(err => console.error(err));
 });
 
+// add a user
+
+server.post('/users', (req, res) => {
+  const { id, name } = req.body;
+  const newUser = { id, name };
+  db.insert(newUser)
+    .then(user => {
+      res.status(201).json(user);
+    })
+    .catch(err => console.error(err));
+});
+
+// delete a user
+
+server.delete('/users/:id', (req, res) => {
+  const { id } = req.params;
+  db.remove(id)
+    .then(removeUser => {
+      console.log(removedUser);
+      res.status(200).json(removedUser);
+    })
+    .catch(err => console.error(err));
+});
 
 server.listen(port, () => console.log('Server listening on port 8000.'));
