01174131b196ac7ff1b4096f524b94a16a3edc45
m-j-hacker <mjhacker@gmail.com>
2018-10-03 16:16:56 -0500

    Got users list to populate

--- index.js
+++ index.js
@@ -5,14 +5,32 @@ const server = express();
 const logger = require('morgan');
 const cors = require('cors');
 
+const userDb = require('./data/helpers/userDb.js');
+const postDb = require('./data/helpers/postDb.js');
+
 const caps = (req, res, next) => {
-    req.user = req.params.user.toUpperCase();
+    req.user = req.params.users.toUpperCase();
     next();
 }
+
+server.use(logger('combined'));
+server.use(cors());
+server.use(express.json());
 
 server.get('/', (req, res) => {
     res.send('Howdy Pardner!');
 });
+
+server.get('/users', (req, res) => {
+    userDb.get()
+    .then(users => {
+        console.log('***USERS***', users);
+        res.status(200).json(users);
+    })
+    .catch(() => res.status(500).json({ error: "The users could not be retrieved."}));
+});
+
+
 
 server.get('/users/:user', caps, (req, res) => {
     res.send(`${req.user}`);
