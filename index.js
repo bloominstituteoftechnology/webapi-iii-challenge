da383ef85cf6afce1d3c416358517b101a5dcf8b
m-j-hacker <mjhacker@gmail.com>
2018-10-03 15:19:30 -0500

    Began importing needed middleware in index.js

--- index.js
+++ index.js
@@ -2,9 +2,21 @@ const express = require('express');
 
 const server = express();
 
+const logger = require('morgan');
+const cors = require('cors');
+
+const caps = (req, res, next) => {
+    req.user = req.params.user.toUpperCase();
+    next();
+}
+
 server.get('/', (req, res) => {
     res.send('Howdy Pardner!');
 });
+
+server.get('/users/:user', caps, (req, res) => {
+    res.send(`${req.user}`);
+})
 
 const port = 9001;
 server.listen(port, () => console.log(`The server is running on port ${port}, m'lady`));
