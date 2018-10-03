8ffa09b67240bc665134d36ab8d8632ec176b86b
decagondev <decagondevelopment@gmail.com>
2018-10-03 19:50:19 +0100

    (MVP) : listen to port 8000 and give a startup message from the server

--- index.js
+++ index.js
@@ -12,3 +12,6 @@ server.add(express.json());
 server.add(cors());
 server.add(helmet());
 server.add(morgan("default", "dev"));
+
+// listen to port 8000 and give a startup message from the server
+server.listen(8000, () => console.log("API listening on port 8000"));
