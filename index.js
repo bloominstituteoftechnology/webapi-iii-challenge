c6ea84b0c7862559c19ad2c1d4ddd31e2677c819
unknown <cornielleandres@gmail.com>
2018-10-03 15:26:08 -0400

    Add GET request handling to server.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { postDb, tagDb, userDb } = require('./data/helpers');
 
 const server = express();
 const port = 5000;
@@ -7,8 +8,22 @@ const port = 5000;
 server.use(express.json());
 server.use(cors());
 
-server.get('/', (req, res) => {
-	res.send('Testing. Hello world.');
+server.get('/api/users/', (req, res) => {
+	userDb.get()
+		.then(users => res.status(200).send(users))
+		.catch(err => res.status(500).json({ error: 'The users information could not be retrieved.' }));
+});
+
+server.get('/api/users/:id', (req, res) => {
+	const { id } = req.params;
+	userDb.get(id)
+		.then(user => {
+			if (!user) {
+				return res.status(404).json({ message: `The user with the ID ${ id } does not exist.` });
+			}
+			return res.status(200).send(user);
+		})
+		.catch(err => res.status(500).json({ error: 'The users information could not be retrieved.' }));
 });
 
 server.listen(port, () => {
