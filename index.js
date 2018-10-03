471035a7a29842ddeb95a60594448954617e269d
Michael Agard <mdagard@gmail.com>
2018-10-03 19:22:26 -0400

    add express.json() put for addpost

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const logger = (req, res, next) => {
   console.log(`${Date.now()} ${req.method} made to ${req.url}`);
   next();
 };
-server.use(logger);
+server.use(logger, express.json());
 
 // const formatUserName = (req, res, next) => {
 //   req.name = req.params.name.toUpperCase();
@@ -18,26 +18,25 @@ server.get('/', (req, res) => {
   res.send('<h1><a href="https://github.com/michaelagard/Node-Blog" style="text-decoration:none; color:black"><code>Node-Blog Node.js Server</code></a></h1>');
 }); // root server endpoint fluff
 
-server.get('/api/user/:id', (req, res) => {
+server.get('/api/users/:id', (req, res) => {
   userDB.get(req.params.id)
     .then(posts => {
       res.json(posts);
     })
 }) // supply user ID, recieve user object
 
-server.get('/api/user/posts/:id', (req, res) => {
+server.get('/api/users/posts/:id', (req, res) => {
   userDB.getUserPosts(req.params.id)
     .then(posts => {
       res.json(posts);
     })
 }) // supply user ID, recieve user posts
 
-server.put('/api/user/addpost', (req, res) => {
-  userDB.insert(post)
-    .then(posts => {
-      res.json(posts);
-    })
-})
+server.post('/api/users', (req, res) => {
+  userDB.insert(req.body)
+    .then(({ id }) => res.json({ message: 'success!' }))
+    .catch(err => console.log(err));
+});
 
 server.listen(port, () => {
   console.log(`### Node-Blog server started on ${port} ###`);
