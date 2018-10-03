deb73a2f404fd3c252d569d3a5af74ecfd051bb0
BrianARuff <thelonewolf198927@gmail.com>
2018-10-03 18:01:08 -0400

    delete functionality added

--- index.js
+++ index.js
@@ -1,63 +1,92 @@
-const express = require('express');
+const express = require("express");
 
 // middle ware pull-ins
-const cors = require('cors'); // port-to-port interaction
-const logger = require('morgan'); // http request logger
-const helmet = require('helmet'); // encrypt res header...
+const cors = require("cors"); // port-to-port interaction
+const logger = require("morgan"); // http request logger
+const helmet = require("helmet"); // encrypt res header...
 
 // server helper functions
-const userdb = require('./data/helpers/userDb');
-const postdb = require('./data/helpers/postDb');
+const userdb = require("./data/helpers/userDb");
+const postdb = require("./data/helpers/postDb");
 
 // init server
 const server = express();
 
 // middleware usage
-server.use(express.json(), cors(), logger(':method :url :status :response-time ms'), helmet());
+server.use(
+  express.json(),
+  cors(),
+  logger(":method :url :status :response-time ms"),
+  helmet()
+);
 
 // route handlers
 
 /* ======= USER ROUTES ======= */
-server.get('/', (req, res) => {
+server.get("/", (req, res) => {
   res.send(`
     <h1>Root Page</h1>
     <li><a href="http://localhost:${process.env.PORT}/users">All Users</a></li>
     <li><a href="http://localhost:${process.env.PORT}/posts">All Posts</a></li>
-  `)
-})
+  `);
+});
 
-server.get( '/users', (req, res) => {
+server.get("/users", (req, res) => {
   userdb
     .get()
     .then(users => res.status(200).json(users))
     .catch(err => console.log(err));
 });
 
-server.get('/users/:id', (req, res) => {
+server.get("/users/:id", (req, res) => {
   userdb
     .get(req.params.id)
     .then(user => res.status(200).send(user))
     .catch(err => res.status(500).send(err));
 });
 
+server.post("/users", (req, res) => {
+  const { name } = req.body;
+  const newUser = { name };
+  userdb
+    .insert(newUser)
+    .then(user => {
+      userdb
+        .get(user.id)
+        .then(foundUser => res.status(200).send(foundUser))
+        .catch(err => res.status(500).send(err));
+    })
+    .catch(err => res.status(500).send(err));
+});
+
+server.delete("/users/:id", (req, res) => {
+  const { id } = req.params;
+  userdb
+    .remove(id)
+    .then(user =>
+      res.status(200).send(`User with id ${id} deleted sucessfully!`)
+    )
+    .catch(err => res.status(500).send(err));
+});
+
 /* ======= POST ROUTES ======= */
-server.get('/posts', (req, res) => {
+server.get("/posts", (req, res) => {
   postdb
     .get()
     .then(posts => res.status(200).send(posts))
     .catch(err => res.status(500).send(err));
 });
 
-server.get('/posts/:id', (req, res) => {
+server.get("/posts/:id", (req, res) => {
   postdb
     .get(req.params.id)
     .then(post => res.status(200).send(post))
     .catch(err => res.status(500).send(err));
-})
+});
 
-server.post('/posts', (req, res) => {
-  const {text, userId} = req.body;
-  const newPost = {text, userId};
+server.post("/posts", (req, res) => {
+  const { text, userId } = req.body;
+  const newPost = { text, userId };
   console.log(req.body);
   postdb
     .insert(newPost)
@@ -69,23 +98,16 @@ server.post('/posts', (req, res) => {
     })
     .catch(err => {
       console.log(err);
-      res.status(500).send(err)
+      res.status(500).send(err);
     });
-})
+});
 
-server.post('/users', (req, res) => {
-  const {name} = req.body;
-  const newUser = {name};
-  userdb
-    .insert(newUser)
-    .then(user => {
-      userdb
-        .get(user.id)
-        .then(foundUser => res.status(200).send(foundUser))
-        .catch(err => res.status(500).send(err));
-    })
+server.delete("/posts/:id", (req, res) => {
+  const { id } = req.params;
+  postdb.remove(id)
+    .then(post => res.status(200).send(`Post at id ${id} removed successfully.`))
     .catch(err => res.status(500).send(err));
-})
+});
 
 // route handler listner
 server.listen(process.env.PORT, () => console.log(`Port:${process.env.PORT}`));
