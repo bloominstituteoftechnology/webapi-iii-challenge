0d736e03fefd882a44d01d38f9fa3beb0f18d2d6
Evan Carlstrom <evan.carlstrom@gmail.com>
2018-10-03 18:57:36 -0400

    get methods for general users and posts by specific user working properly

--- index.js
+++ index.js
@@ -2,8 +2,32 @@ const express = require('express');
 const server = express();
 const port = 8000;
 
+const db = require('./data/helpers/userDb.js');
+
 server.get('/', (req, res) => {
   res.send('Server test.');
 });
+
+// get users
+
+server.get('/users', (req, res) => {
+  db.get().then(users => {
+    console.log('\n** users **', users);
+    res.json(users);
+  })
+  .catch(err => console.error(err)); // adding specific messages later
+});
+
+// get user posts by specific ID
+
+server.get('/users/:id', (req, res) => {
+  const { id } = req.params;
+  db.getUserPosts(id).then(post => {
+    console.log('\n** posts by specified user **', post);
+    res.json(post);
+  })
+  .catch(err => console.error(err));
+});
+
 
 server.listen(port, () => console.log('Server listening on port 8000.'));
