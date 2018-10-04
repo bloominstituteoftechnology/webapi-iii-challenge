87d5f6d478d9a54d9d2e73839f989cb327078bc4
Brock Maraee Rohloff <00AgentB@gmail.com>
2018-10-04 13:51:01 -0600

    Implement CRUD Endpoint: READ Individual User Endpoint which retrieves a single user object and sends the object to the client

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ server.get('/', (request, response) => {
     response.status(200).send(`It's working!`);
 })
 
+// #####=- READ All Users Endpoint -=#####
 server.get('/users', (request, response) => {
 
 // Unique Error Message
@@ -29,6 +30,18 @@ userDb.get()
 .catch(() => response.status(500).send(error500))
 })
 
+// #####=- READ Individual User Endpoint -=#####
+server.get('/users/:userId', (request, response) => {
+
+const userId = request.params.userId;
+
+// Unique Error Message
+const error500 = { errorMessage: "Unable to retrieve users." };
+
+userDb.get(userId)
+.then(user => response.status(200).send(user))
+.catch(() => response.status(500).send(error500))
+})
 
 // #####=- Server Port Address and Listen Method -=#####
 port = 9999;
