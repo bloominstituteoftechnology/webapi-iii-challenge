2fb36053b912197622af5c077742f4896bd30d1e
Michael Agard <mdagard@gmail.com>
2018-10-03 21:47:06 -0400

    added error handling for all 4 user cases

--- index.js
+++ index.js
@@ -18,23 +18,28 @@ server.get('/', (req, res) => {
   res.send('<h1><a href="https://github.com/michaelagard/Node-Blog" style="text-decoration:none; color:black"><code>Node-Blog Node.js Server</code></a></h1>');
 }); // root server endpoint fluff
 
+// get user
 server.get('/api/users/:id', (req, res) => {
   userDB.get(req.params.id)
-    .then(posts => {
-      res.json(posts);
-    });
+    .then(user => {
+      console.log(user);
+      if (!user) { return res.json({ message: `no user by that id` }) }
+      res.json(user);
+    })
+    .catch(err => console.log(err));
 }); // supply user ID, recieve user object
 
 server.get('/api/users/posts/:id', (req, res) => {
   userDB.getUserPosts(req.params.id)
     .then(posts => {
       res.json(posts);
-    });
+    })
+    .catch(err => console.log(err));
 }); // supply user ID, recieve user posts
 
 server.post('/api/users', formatUserName, (req, res) => {
   if (!req.body.name) { return res.json({ message: `need name` }) }
-  if (req.body.name > 128) { return res.json({ message: `go shorter on name` }) }
+  if (req.body.name.length > 128) { return res.json({ message: `go shorter on name` }) }
   userDB.insert(req.body)
     .then(({ id }) => res.json({ message: `Good job, returned ID ${id}` }))
     .catch(err => console.log(err));
@@ -42,15 +47,21 @@ server.post('/api/users', formatUserName, (req, res) => {
 
 server.put('/api/users/:id', formatUserName, (req, res) => {
   if (!req.body.name) { return res.json({ message: `need name` }) }
-  if (req.body.name > 128) { return res.json({ message: `go shorter on name` }) }
+  if (req.body.name.length > 128) { return res.json({ message: `go shorter on name` }) }
   userDB.update(req.params.id, req.body)
-    .then(count => res.json({ message: `Way to go, returned count ${count}` }))
+    .then(count => {
+      if (!count) { return res.json({ message: `no user by that id` }) }
+      res.json({ message: `Way to go, returned count ${count}` })
+    })
     .catch(err => console.log(err));
 });
 
 server.delete('/api/users/:id', (req, res) => {
   userDB.remove(req.params.id)
-    .then(removed => res.json({ message: `Way to go, removed count ${removed}` }))
+    .then(removed => {
+      if (!removed) { return res.json({ message: `no user by that id` }) }
+      res.json({ message: `Way to go, removed count ${removed}` })
+    })
     .catch(err => console.log(err));
 })
 
