9f0fb5629d10307b99428c94bec67c02126f1c9e
Alexandra <alexswartz9753@gmail.com>
2018-10-04 11:52:54 -0400

    moved index.js and package.json files into main directory

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
-const userDb = require('../data/helpers/userDb.js');
-const postDb = require('../data/helpers/postDb.js');
+const userDb = require('./data/helpers/userDb.js');
+const postDb = require('./data/helpers/postDb.js');
 
 const server = express();
 
@@ -14,16 +14,28 @@ const port = 9000;
 //Middlewares
 server.use(morgan('combined'));
 
-//Routes
+//User Database Routes
 server.get('/', (req, res)=> {
     res.send('Welcome to Node Blog :)')
 });
 
-server.get('/api/users/:id', (req, res)=> {
-    userDb.get(req.query)
+server.get('/api/users', (req, res)=> {
+    userDb.get()
         .then(users=> {
             console.log(users);
-            res.status(200).json(users);
+            res.status(200).json({users});
+        })
+        .catch(err=> {
+            res.status(500).json({error: "Information could not be retrieved"});
+        })
+});
+
+server.get('/api/users/:id', (req, res)=> {
+    console.log(req.params.id);
+    userDb.get(req.params.id)
+        .then(user=> {
+            console.log(user);
+            res.status(200).json({user});
         })
         .catch(err=> {
             res.status(500).json({error: "Information could not be retrieved."});
@@ -31,6 +43,7 @@ server.get('/api/users/:id', (req, res)=> {
 });
 
 server.post('/api/users', (req, res)=> {
+    console.log(req.body);
     const {name} = req.body;
     const newUser = {name};
     userDb.insert(newUser)
@@ -41,7 +54,7 @@ server.post('/api/users', (req, res)=> {
                     if (!req.body.name) {
                         res.status(404).json({error: "User not found"});
                     }
-                    res.status(201).json(user);
+                    res.status(201).json({user});
                 })
         })
         .catch(err=> {
@@ -50,13 +63,14 @@ server.post('/api/users', (req, res)=> {
 });
 
 server.delete('/api/users/:id', (req, res)=> {
+    console.log(req.params);
     const {id} = req.params;
     userDb.remove(id)
-        .then(users=> {
-            if (!users) {
+        .then(user=> {
+            if (!user) {
                 res.status(404).json({message: "The user with the specified ID does not exist."});
             }
-            res.status(200).json(users);
+            res.status(200).json({user});
         })
         .catch(err=> {
             res.status(500).json({error: "The user could not be removed"});
@@ -64,21 +78,33 @@ server.delete('/api/users/:id', (req, res)=> {
 });
 
 server.put('/api/users/:id', (req, res)=> {
+    console.log(req.params, req.body);
     const {id} = req.params;
     const {name} = req.body;
     const updatedUser = {name};
     userDb.update(id, updatedUser)
-        .then(user=> {
-            if (!user) {
+        .then(updatedUser=> {
+            if (!updatedUser) {
                 res.status(404).json({error: "This user does not exist"});
             } else if (!req.body) {
                 res.status(400).json({error: "Please add a name for this user"});
             } else {
-                res.status(200).json(user);
+                res.status(200).json({updatedUser});
             }
         })
         .catch(err=> {
             res.status(500).json({error: "This information could not be saved to the database"});
+        })
+});
+
+//Posts Routes
+server.get('/api/posts', (req, res)=> {
+    postDb.get()
+        .then(posts=> {
+            res.status(200).json({posts});
+        })
+        .catch(err=> {
+            res.status(500).json({error: "Information could not be retrieved"});
         })
 });
 
