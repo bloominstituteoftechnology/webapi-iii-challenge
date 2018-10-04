6360602a74af55e500ef1da3e080cce818f4070b
m-j-hacker <mjhacker@gmail.com>
2018-10-04 15:56:48 -0500

    Moved user http functions to userRoutes and imported them into index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const userRoutes = require('./users/userRoutes.js');
+
 const express = require('express');
 
 const server = express();
@@ -8,33 +10,19 @@ const cors = require('cors');
 const userDb = require('./data/helpers/userDb.js');
 const postDb = require('./data/helpers/postDb.js');
 
-// function upper(obj) {
-//     for (var name in obj) {
-//         if (typeof obj["name"] === 'string') {
-//             obj["name"] = obj["name"].toUpperCase();
-//         }
-//         if (typeof obj["name"] === 'object') {
-//             upper(obj["name"]);
-//         }
-//     }
-//     return obj;
-// }
-
- const caps = (req, res, next) => {
-     console.log(req.body);
-    req.body.name = req.body.name.toUpperCase();
-    next();
- }
-
 server.use(logger('combined'));
 server.use(cors());
 server.use(express.json());
+
+server.use('/users', userRoutes);
 
 server.post('/posts', (req, res) => {
     console.log(req.body);
     const { text, postedBy } = req.body;
     const newPost = { text, postedBy };
     postDb.insert(newPost)
+    .then()
+    .catch()
 
 })
 
@@ -42,68 +30,7 @@ server.get('/', (req, res) => {
     res.send('Howdy Pardner!');
 });
 
-server.get('/users', (req, res) => {
-    userDb.get()
-    .then(users => {
-        console.log('***USERS***', users);
-        res.status(200).json(users);
-    })
-    .catch(() => res.status(500).json({ error: "The users could not be retrieved."}));
-});
 
-server.post('/users', caps, (req, res) => {
-    const { name } = req.body;
-    const newUser = { name };
-    userDb.insert(newUser)
-        .then(userId => {
-            const { id } = userId;
-            userDb.get(id)
-            .then(user => {
-                if (!user) {
-                    return res.status(404).json({ message: "The user with the specified ID does not exist." });
-                } else
-                return res.status(201).json(user);
-            })
-            .catch(() => res.status(500).json({ error: "There was an error while saving the user."}));
-        })
-        .catch(() => res.status(400).json({ error: "Please provide a name for the user" }));
-});
-
-server.get('/users/:id', (req, res) => {
-    const id = req.params.id;
-    userDb.getUserPosts(id)
-    .then(user => {
-        res.status(200).json(user)
-    })
-    .catch(err => {
-        res.send(err);
-    });
-});
-
-server.delete('/users/:id', (req, res) => {
-    const id = req.params.id;
-    userDb.remove(id)
-    .then(removedUser => {
-        console.log(removedUser);
-        if (removedUser === 0) {
-            return res.status(404).json({ message: "The user with the specified ID does not exist"})
-        } else
-        return res.status(200).json(removedUser);
-    })
-    .catch(() => res.status(500).json({ error: "The user could not be removed" }));
-});
-
-server.put('/users/:id', caps, (req, res) => {
-    const { id } = req.params;
-    const { name } = req.body;
-    const newUser = { name };
-    userDb.update(id, newUser)
-    .then(user => {
-        console.log('user = ', user);
-        res.status(200).json(user);
-    })
-    .catch(() => res.status(500).json({ error: "The post information could not be modified."}));
-});
 
 const port = 9001;
 server.listen(port, () => console.log(`The server is running on port ${port}, m'lady`));
---
+++ users/userRoutes.js
@@ -0,0 +1,74 @@
+const express = require('express');
+
+const router = express.Router();
+
+const caps = (req, res, next) => {
+    console.log(req.body);
+   req.body.name = req.body.name.toUpperCase();
+   next();
+}
+
+router.get('/', (req, res) => {
+    userDb.get()
+    .then(users => {
+        console.log('***USERS***', users);
+        res.status(200).json(users);
+    })
+    .catch(() => res.status(500).json({ error: "The users could not be retrieved."}));
+});
+
+router.post('/', caps, (req, res) => {
+    const { name } = req.body;
+    const newUser = { name };
+    userDb.insert(newUser)
+        .then(userId => {
+            const { id } = userId;
+            userDb.get(id)
+            .then(user => {
+                if (!user) {
+                    return res.status(404).json({ message: "The user with the specified ID does not exist." });
+                } else
+                return res.status(201).json(user);
+            })
+            .catch(() => res.status(500).json({ error: "There was an error while saving the user."}));
+        })
+        .catch(() => res.status(400).json({ error: "Please provide a name for the user" }));
+});
+
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    userDb.getUserPosts(id)
+    .then(user => {
+        res.status(200).json(user)
+    })
+    .catch(err => {
+        res.send(err);
+    });
+});
+
+router.delete('/:id', (req, res) => {
+    const id = req.params.id;
+    userDb.remove(id)
+    .then(removedUser => {
+        console.log(removedUser);
+        if (removedUser === 0) {
+            return res.status(404).json({ message: "The user with the specified ID does not exist"})
+        } else
+        return res.status(200).json(removedUser);
+    })
+    .catch(() => res.status(500).json({ error: "The user could not be removed" }));
+});
+
+router.put('/:id', caps, (req, res) => {
+    const { id } = req.params;
+    const { name } = req.body;
+    const newUser = { name };
+    userDb.update(id, newUser)
+    .then(user => {
+        console.log('user = ', user);
+        res.status(200).json(user);
+    })
+    .catch(() => res.status(500).json({ error: "The post information could not be modified."}));
+});
+
+module.exports = router;
