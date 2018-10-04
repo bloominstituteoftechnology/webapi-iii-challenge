058d21ae94bcdc916f00ebb0e1c388d1ee03e618
Caleb Kirkwood <calebkirkwood@calebs-mbp.attlocal.net>
2018-10-04 19:12:04 -0400

    Added user router, and added next() to checkName middleware

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const logger = require('morgan');
 const cors = require('cors');
 
 const port = 9000;
-const userDb = require('./data/helpers/userDb');
+const userRoutes = require('./routes/userRoutes');
+const postRoutes = require('./routes/postRoutes');
+
 const postDb = require('./data/helpers/postDb');
 
 const server = express();
@@ -11,126 +13,16 @@ const server = express();
 ///////////////
 // Middlewares
 ///////////////
-const capitalize = (req, res, next) => {
-  if (req.body.name) {
-    req.name = req.body.name.toUpperCase();
-    console.log(req.name);
-  }
-  next();
-};
-
-const checkName = (req, res, next) => {
-  if (!req.body.name || req.body.name.length > 128) {
-    return res.status(400).send({
-      errorMessage:
-        'Please provide a name for the user that is less than 128 characters.',
-    });
-  }
-};
 
 server.use(logger('combined'));
-server.use(express.json());
+// server.use(express.json());
 
 ///////////////
 // Routes
 ///////////////
 
 // ####### Users #######
-server.get('/api/users', (req, res) => {
-  userDb
-    .get()
-    .then((users) => {
-      console.log('** users **\n', users);
-      res.status(200).json(users);
-    })
-    .catch((err) => {
-      console.log(err);
-      res
-        .status(500)
-        .json({ error: 'The users information could not be retrieved.' });
-    });
-});
-
-server.post('/api/users', checkName, capitalize, (req, res) => {
-  // console.log('req.body', req.body);
-  const name = req.name;
-  if (!name) {
-    return res.status(400).send({
-      errorMessage: 'Please provide a name for the user.',
-    });
-  }
-  const newUser = { name };
-  userDb.insert(newUser).then((userId) => {
-    // console.log(userId);
-    const { id } = userId;
-    userDb
-      .get(id)
-      .then((user) => {
-        // console.log(user);
-        if (!user) {
-          return res
-            .status(422)
-            .send({ Error: `User does not exist by id ${userId}` });
-        }
-        res.status(201).json(user);
-      })
-      .catch((err) => {
-        console.log(err);
-        res
-          .status(500)
-          .json({ error: 'The users information could not be saved.' });
-      });
-  });
-});
-
-server.delete('/api/users/:id', (req, res) => {
-  const { id } = req.params;
-  userDb
-    .remove(id)
-    .then((remove) => {
-      console.log(remove);
-      if (!remove) {
-        return res.status(404).send({
-          message: 'The user with the specified ID does not exist.',
-        });
-      }
-      res.status(200).send({ message: `User with ID ${id} was removed.` });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({
-        error: 'The user could not be removed',
-      });
-    });
-});
-
-server.put('/api/users/:id', checkName, capitalize, (req, res) => {
-  const { id } = req.params;
-  const name = req.name;
-  if (!name) {
-    return res.status(400).send({
-      errorMessage: 'Please provide a name for the user.',
-    });
-  }
-  const newUser = { name };
-  console.log('newUser', newUser);
-  userDb
-    .update(id, newUser)
-    .then((user) => {
-      console.log('user', user);
-      if (!user) {
-        return res.status(404).send({
-          message: 'The user with the specified ID does not exist.',
-        });
-      }
-      res.status(200).json(newUser);
-    })
-    .catch((err) => {
-      res.status(500).json({
-        error: 'The user information could not be modified.',
-      });
-    });
-});
+server.use('/api/users', userRoutes);
 
 // ####### Posts #######
 server.get('/api/posts', (req, res) => {
---
+++ routes/userRoutes.js
@@ -0,0 +1,126 @@
+const express = require('express');
+
+const router = express.Router();
+const userDb = require('../data/helpers/userDb');
+
+// Middleware
+router.use(express.json());
+
+const capitalize = (req, res, next) => {
+  console.log('capitalize');
+  if (req.body.name) {
+    req.name = req.body.name.toUpperCase();
+    console.log(req.name);
+  }
+  next();
+};
+
+const checkName = (req, res, next) => {
+  // console.log('checkName', req.body);
+  if (!req.body.name || req.body.name.length > 128) {
+    return res.status(400).send({
+      errorMessage:
+        'Please provide a name for the user that is less than 128 characters.',
+    });
+  }
+  next();
+};
+
+// Routes
+router.get('/', (req, res) => {
+  userDb
+    .get()
+    .then((users) => {
+      console.log('** users **\n', users);
+      res.status(200).json(users);
+    })
+    .catch((err) => {
+      console.log(err);
+      res
+        .status(500)
+        .json({ error: 'The users information could not be retrieved.' });
+    });
+});
+
+router.post('/', checkName, capitalize, (req, res) => {
+  console.log('req.body', req.body);
+  const name = req.name;
+  if (!name) {
+    return res.status(400).send({
+      errorMessage: 'Please provide a name for the user.',
+    });
+  }
+  const newUser = { name };
+  userDb.insert(newUser).then((userId) => {
+    // console.log(userId);
+    const { id } = userId;
+    userDb
+      .get(id)
+      .then((user) => {
+        // console.log(user);
+        if (!user) {
+          return res
+            .status(422)
+            .send({ Error: `User does not exist by id ${userId}` });
+        }
+        res.status(201).json(user);
+      })
+      .catch((err) => {
+        console.log(err);
+        res
+          .status(500)
+          .json({ error: 'The users information could not be saved.' });
+      });
+  });
+});
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  userDb
+    .remove(id)
+    .then((remove) => {
+      console.log(remove);
+      if (!remove) {
+        return res.status(404).send({
+          message: 'The user with the specified ID does not exist.',
+        });
+      }
+      res.status(200).send({ message: `User with ID ${id} was removed.` });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: 'The user could not be removed',
+      });
+    });
+});
+
+router.put('/:id', checkName, capitalize, (req, res) => {
+  const { id } = req.params;
+  const name = req.name;
+  if (!name) {
+    return res.status(400).send({
+      errorMessage: 'Please provide a name for the user.',
+    });
+  }
+  const newUser = { name };
+  console.log('newUser', newUser);
+  userDb
+    .update(id, newUser)
+    .then((user) => {
+      console.log('user', user);
+      if (!user) {
+        return res.status(404).send({
+          message: 'The user with the specified ID does not exist.',
+        });
+      }
+      res.status(200).json(newUser);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: 'The user information could not be modified.',
+      });
+    });
+});
+
+module.exports = router;
