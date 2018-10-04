8b8ac14f65184c3e1642466a7b8c3e1d47bb0445
Michael Agard <mdagard@gmail.com>
2018-10-04 18:44:18 -0400

    reorder variables and use cases/move formatUserName to userRoutes.js

--- index.js
+++ index.js
@@ -1,34 +1,39 @@
 const express = require('express');
 
 const configureMiddleware = require('./config/middleware.js');
-const userRoutes = require('./routes/userRoutes')
-
-const postDB = require('./data/helpers/postDb');
-
+const userRoutes = require('./routes/userRoutes.js')
+const postRoutes = require('./routes/postRoutes.js')
 const server = express();
-
 const port = 7100;
-
 
 configureMiddleware(server);
 
+// function validate(req, res, next) {
+//   const name = req.body.name;
+//   if (name) {
+//     next();
+//   } else {
+//     next('u12');
+//   }
+// }
+// declares post/user endpoint routes for all userRoutes scenarios
+
+server.use(express.json());
+server.use('/api/posts', postRoutes);
 server.use('/api/users', userRoutes);
 
 const logger = (req, res, next) => {
   console.log(`${Date.now()} ${req.method} made to ${req.url}`);
   next();
 };
-server.use(logger, express.json());
 
-const formatUserName = (req, res, next) => {
-  req.body.name = req.body.name.toUpperCase();
-  next();
-}; // middleware that takes the body's name and converts it to uppercase
+server.use(logger)
+
 
 server.get('/', (req, res) => {
   res.send('<h1><a href="https://github.com/michaelagard/Node-Blog" style="text-decoration:none; color:black"><code>Node-Blog Node.js Server</code></a></h1>');
 }); // root server endpoint fluff
 
 server.listen(port, () => {
-  console.log(`### Node-Blog server started on ${port} ###`);
+  console.log(`\n### Node-Blog server started on ${port} ###`);
 });
