d8ae0c97f6946dcd4026e6dfd07608cc014c3930
James Cassidy <cassidyjamesw@gmail.com>
2018-10-04 19:02:00 -0400

    day 2 complete

--- index.js
+++ index.js
@@ -1,91 +1,24 @@
 // imports
 const express = require("express");
 const logger = require("morgan");
-const cors = require("cors");
-const helmet = require("helmet");
-const userDB = require("./data/helpers/userDb");
+const configureMiddleware = require("./config/middleware.js");
+const userRoutes = require("./users/userRoutes.js");
+const postRoutes = require("./posts/postRoutes");
 
 // setup server
 const server = express();
 const port = 8000;
 server.use(logger("combined"));
-server.use(cors());
-server.use(helmet());
-server.use(express.json());
-
-// MIDDLEWARES
-
-const upperCase = (req, res, next) => {
-  if (req.body.name) {
-    req.body.name = req.body.name.toUpperCase();
-  }
-  next();
-};
+configureMiddleware(server);
 
 // ROUTES
-
-// get users
+// Homepage
 server.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-server.get("/users", (req, res) => {
-  userDB
-    .get()
-    .then(users => {
-      res.status(200).json(users);
-    })
-    .catch(err => res.status(500).json({ error: "ERROROROR" }));
-});
-
-//get one users posts
-server.get("/users/:id", (req, res) => {
-  userDB
-    .getUserPosts(req.params.id)
-    .then(userposts => {
-      res.status(200).json(userposts);
-    })
-    .catch(err => res.status(500).json({ error: "ERROROROR" }));
-});
-
-//add user
-server.post("/users", upperCase, (req, res) => {
-  const { id, name } = req.body;
-  const newUser = { id: id, name: name };
-  userDB
-    .insert(newUser)
-    .then(user => {
-      res.json(user);
-    })
-    .catch(err => res.status(500).json({ error: "ERROROROR" }));
-});
-
-//delete user
-server.delete("/users/:id", (req, res) => {
-  const { id } = req.params;
-  const foundUser = userDB.getUserPosts(id);
-  if (foundUser) {
-    userDB
-      .remove(id)
-      .then(users => {
-        res.json(users);
-      })
-      .catch(err => res.status(500).json({ error: "ERROROROR" }));
-  }
-});
-
-//edit user
-server.put("/users/:id", upperCase, (req, res) => {
-  userDB
-    .update(req.params.id, req.body)
-    .then(posts =>
-      res.json(posts).catch(err => res.status(500).json({ error: "ERROROROR" }))
-    );
-});
-
-// server.get("/name/:name", yell, (req, res) => {
-//   res.send(req.name);
-// });
+server.use("/users", userRoutes);
+server.use("/posts", postRoutes);
 
 // call server.listen
 server.listen(port, () => {
