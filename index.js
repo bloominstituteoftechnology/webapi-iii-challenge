3fe9a743deec0abd33c7daee3c86cc2395671a2e
Austinlp4 <apendergrast44@gmail.com>
2018-10-04 16:19:28 -0500

    Update on post routes

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const db = require('./data/helpers/userDb');
+const userRoutes = require('./users/usersRoute.js');
+const postsRoutes = require('./posts/postsRoutes.js');
 
 const express = require('express');
 const cors = require('cors');
@@ -6,75 +8,16 @@ const server = express();
 const logger = require('morgan');
 const helmet = require('helmet');
 
-const yell = (req, res, next) => {
-    const newName = req.body.name.toUpperCase();
-    req.name = newName;
-    next();
-};
-
+// Server Invocations
+server.use(express.json());
 server.use(logger('combined'));
 server.use(cors());
-server.use(express.json());
 server.use(helmet());
 
-server.get('/users', (req, res) => {
-    db.get().then(users => {
-        res.json(users);
-    })
-});
 
-server.get('/users/:id', (req, res) => {
-    const { id } = req.params;
-    db.get(id)
-      .then(user => {
-          if(!user) {
-              return res.status(404).send({message: `no user with that id`});
-          }
-          console.log(user.name);
-          res.json(user.name);
-      })
-      .catch(err => console.error(err))
-});
 
-server.post('/users', yell, (req, res) => {
-    const { name } = req.body;
-    const newUser = { name };
-    db.insert(newUser)
-      .then(userId => {
-          const { id } = userId;
-        db.get(id)
-          .then(user => {
-              console.log(req.name);
-              if(!user){
-                  return res.status(400).send({errorMessage: "Please provide name for user"})
-              }
-              res.status(201).json(user)
-          });
-      }) 
-      .catch(err => console.error(err))
-});
-
-server.delete('/users/:id', (req, res) => {
-    const { id } = req.params;
-    db.remove(id)
-      .then(removeUser => {
-         console.log(removeUser);
-         res.status(200).json(removeUser);
-      })
-      .catch(err => console.error(err))
-    res.send(req.params);
-});
-
-server.put('/users/:id', yell, (req, res) => {
-    const { id } = req.params;
-    const { name } = req.body;
-    const newUser = { name };
-    db.update(id, newUser)
-      .then(user => {
-          res.status(200).json(user)
-      })
-      .catch(err => console.error(err));
-});
+server.use('/users', userRoutes);
+server.use('/posts', postsRoutes);
 
 const port = 5000;
 server.listen(port, () => console.log(`API running on port ${port}`));
