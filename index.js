63b592cacb008a23da955fa00d721d8b4e633fe9
Michael Agard <mdagard@gmail.com>
2018-10-03 21:36:09 -0400

    add error checking and post/put/delete endpoints now work

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ const logger = (req, res, next) => {
 };
 server.use(logger, express.json());
 
-// const formatUserName = (req, res, next) => {
-//   req.name = req.params.name.toUpperCase();
-//   next();
-// }
+const formatUserName = (req, res, next) => {
+  req.body.name = req.body.name.toUpperCase();
+  next();
+}
 
 server.get('/', (req, res) => {
   res.send('<h1><a href="https://github.com/michaelagard/Node-Blog" style="text-decoration:none; color:black"><code>Node-Blog Node.js Server</code></a></h1>');
@@ -22,21 +22,37 @@ server.get('/api/users/:id', (req, res) => {
   userDB.get(req.params.id)
     .then(posts => {
       res.json(posts);
-    })
-}) // supply user ID, recieve user object
+    });
+}); // supply user ID, recieve user object
 
 server.get('/api/users/posts/:id', (req, res) => {
   userDB.getUserPosts(req.params.id)
     .then(posts => {
       res.json(posts);
-    })
-}) // supply user ID, recieve user posts
+    });
+}); // supply user ID, recieve user posts
 
-server.post('/api/users', (req, res) => {
+server.post('/api/users', formatUserName, (req, res) => {
+  if (!req.body.name) { return res.json({ message: `need name` }) }
+  if (req.body.name > 128) { return res.json({ message: `go shorter on name` }) }
   userDB.insert(req.body)
-    .then(({ id }) => res.json({ message: 'success!' }))
+    .then(({ id }) => res.json({ message: `Good job, returned ID ${id}` }))
     .catch(err => console.log(err));
 });
+
+server.put('/api/users/:id', formatUserName, (req, res) => {
+  if (!req.body.name) { return res.json({ message: `need name` }) }
+  if (req.body.name > 128) { return res.json({ message: `go shorter on name` }) }
+  userDB.update(req.params.id, req.body)
+    .then(count => res.json({ message: `Way to go, returned count ${count}` }))
+    .catch(err => console.log(err));
+});
+
+server.delete('/api/users/:id', (req, res) => {
+  userDB.remove(req.params.id)
+    .then(removed => res.json({ message: `Way to go, removed count ${removed}` }))
+    .catch(err => console.log(err));
+})
 
 server.listen(port, () => {
   console.log(`### Node-Blog server started on ${port} ###`);
