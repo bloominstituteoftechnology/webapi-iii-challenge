6060790e2cfd3c088035fb87ae5de94fdfc5efc4
Cameron Ray <cameronray@Camerons-MacBook-Pro.local>
2018-10-04 09:56:05 -0700

    User endpoints created

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//This file is "the API"
+
 // Pull in express
 const express = require('express');
 const cors = require('cors');
@@ -11,6 +13,7 @@ server.get('/', (req, res) => {
     res.send('This is the response from the server');
 });
 
+// Get all users
 server.get('/api/users', (req, res) => {
     userDb
         .get()
@@ -18,10 +21,11 @@ server.get('/api/users', (req, res) => {
             res.status(200).json(users);
         })
         .catch(err => {
-            res.status(500).json({errorMessage: 'An error with the server occured'})
+            res.status(500).json({errorMessage: 'An error occured trying to get users'})
         })
 });
 
+// Get a specific user
 server.get('/api/users/:id', (req, res) => {
     const {id} = req.params;
     userDb
@@ -30,10 +34,11 @@ server.get('/api/users/:id', (req, res) => {
             res.status(200).json(user);
         })
         .catch(err => {
-            res.status(500).json({errorMessage: 'An error with the server occured'})
+            res.status(500).json({errorMessage: 'An error occured trying to get a user'})
         })
 });
 
+// Get posts from a specific user
 server.get('/api/users/:id/userPosts', (req, res) => {
     const {id} = req.params;
     userDb
@@ -42,11 +47,56 @@ server.get('/api/users/:id/userPosts', (req, res) => {
             res.status(200).json(posts);
         })
         .catch(err => {
-            res.status(500).json({errorMessage: 'An error with the server occured'})
+            res.status(500).json({errorMessage: `An error occured trying to get a user's posts`})
         })
 })
 
-// Call server.listen w/ a port of your choosing
+//I need to create a method to take a new user name and convert it to uppercase
+
+// Create a new user
+server.post('/api/users/create', (req, res) => {
+    const {name} = req.body;
+    const newUser = {name};
+    userDb
+        .insert(newUser)
+        .then(newUser => {
+            res.status(200).json(newUser);
+        })
+        .catch(err => {
+            res.status(500).json({errorMessage: `An error occured trying to get a user's posts`})
+        })
+})
+
+// Update a user
+server.put('/api/users/:id', (req, res) => {
+    const {id} = req.params;
+    const {name} = req.body;
+    const newName = {name};
+    userDb 
+        .update(id , newName)
+        .then(updatedUser => {
+            res.status(200).json(updatedUser);
+        })
+        .catch(err => {
+            res.status(500).json({errorMessage: `An error occured trying to update a user`})
+        })
+})
+
+//Delete a user
+server.delete('/api/users/:id', (req, res) => {
+    userDb
+        .remove(id)
+        .then(users => {
+            res.status(200).json(users);
+        })
+        .catch(err => {
+            res.status(500).json({errorMessage: `An error occured trying to delete a user`})
+        })
+})
+
+
+
+// Choose a port and call server.listen on that port
 const port = 4000;
 
 server.listen(port, () => 
