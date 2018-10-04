4c0baaaeb79dbc1f724a6fd1f30860c1404bcafc
Akshay Gadkari <akshay.gadkari@gmail.com>
2018-10-04 13:52:56 -0700

    trying to fix POST requests

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
+const udb = require('./data/helpers/userDb.js');
+const pdb = require('./data/helpers/postDb.js');
 
 const server = express();
-server.use(cors());
 server.use(express.json());
+server.use(cors({}));
 
-const udb = require('./data/helpers/userDb.js');
-let userId = 1;
+const errorHelper = (status, message, res) => {
+    res.status(status).json({ error: message });
+};
 
 const lettering = (req, res, next) => {
-    req.user = req.params.user.toUpperCase();
+    req.body.name = req.body.name.toUpperCase();
     next();
 };
 
@@ -33,25 +36,38 @@ server.get('/users', (req, res) => {
 // 	.catch(err => res.status(500).send(err));
 // });
 
+// server.post('/users',lettering, (req, res) => {
+//     console.log(req.body);
+//     const {name} = req.body;
+//     udb.insert({name})
+//     	.then(response => {
+// 	    const {id} = response;
+// 	    // res.status(201).json(response);
+// 	    udb.get(id)
+// 	    	.then(userId => {
+// 	    	    if (name.length >= 128 || !name) {
+// 	    	    	return res.status(400).json({
+// 	    	    	    errorMessage: 'Name must be under 128 characters.'
+// 	    	    	});
+// 	    	    }
+// 	    	       res.status(201).json(userId);
+// 	    	});
+// 	})
+// 	.catch(err => console.error(err));
+// });
+
+
 server.post('/users', (req, res) => {
-    console.log(req.body);
-    const {name} = req.body;
-    const newUser = {name};
-    console.log(newUser);
-    udb.insert(req.body)
-    	.then(insertedUser => {
-	    const {id} = userId;
-	    udb.findById(id)
-		.then(userId => {
-		    if (name.length >= 128 || !name) {
-		    	return res.status(400).json({
-		    	    errorMessage: 'Name must be under 128 characters.'
-		    	});
-		    }
-		    res.status(201).json(userId);
-		});
+    const { name } = req.body;
+    udb
+	.insert({ name })
+	.then(response => {
+	    res.json(response);
 	})
-	.catch(err => console.error(err));
+	.catch(err => {
+	    console.log(res.body);
+	    return errorHelper(500, 'Database boof', res);
+	});
 });
 
 server.delete('/users/:id', (req, res) => {
@@ -79,3 +95,20 @@ const port = 8003;
 server.listen(port, () => {
     console.log(`\n === ${port} === \n`);
 });
+
+
+/*
+  in index.js:
+  const userRoutes = require('./users/userRoutes.js);
+  const configureMiddleware = require('./config/middleware.js);
+  configureMiddleware(server);
+
+  in userRoutes.js:
+  router.use(usersOnlyMiddleware);
+
+  in middleware.js:
+  module.exports = server => {
+  server.use(helmet());
+  server.use(express.json());
+  };
+*/
