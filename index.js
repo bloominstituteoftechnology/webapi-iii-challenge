9576a0595a446eeda2a9a458185a1ad8ea62b716
Brock Maraee Rohloff <00AgentB@gmail.com>
2018-10-04 14:54:14 -0600

    Implement UPDATE Individual Endpoint which receives a user id, through url parameters, and a user name and updates the name of the user with the given id to the given name.

--- index.js
+++ index.js
@@ -18,18 +18,20 @@ server.use(express.json(), cors(), morgan('combined'), helmet());
 
 // USER Errors
 const missingUserName = { errorMessage: "Please provide a name for the user." }
-const unableToCreateUser500 = { errorMessage: "Unable to create user." }
 const userNotFound = { errorMessage: "The user with the specified ID does not exist." }
+const noUsersUpdated = { errorMessage: "No users were updated." }
 const unableToFindSingleUser500 = { errorMessage: "The user information could not be retrieved." }
 const unableToFindUsers500 = { errorMessage: "Unable to retrieve users." }
+const unableToCreateUser500 = { errorMessage: "Unable to create user." }
+const unableToUpdateUser500 = { errorMessage: "Unable to updated user." }
 
 
 // POST Errors
 const missingPostData = { errorMessage: "Please provide an id and text parameter for the post." }
-const unableToCreatePost500 = { errorMessage: "Unable to create user." }
 const postNotFound = { errorMessage: "The post with the specified ID does not exist." }
 const unableToFindSinglePost500 = { errorMessage: "The post information could not be retrieved." }
 const unableToFindPosts500 = { errorMessage: "Unable to retrieve posts." }
+const unableToCreatePost500 = { errorMessage: "Unable to create user." }
 
 
 //// ==========- USER DATABASE CRUD ENDPOINTS -==========
@@ -67,9 +69,6 @@ server.get('/users/:userId', (request, response) => {
 /// #####=- CREATE Individual User Endpoint -=#####
 server.post('/users',  (request, response) => {
 
-    // Unique Error Message
-    const error500 = { errorMessage: "Unable to create user." };
-
     // Request Validation
     const { name } = request.body;
     
@@ -90,6 +89,37 @@ server.post('/users',  (request, response) => {
         .catch(() => response.status(500).send(unableToFindSingleUser500))
     })
     .catch(() => response.status(500).send(unableToCreateUser500))
+})
+
+/// #####=- UPDATE Individual User Endpoint -=#####
+server.put('/users/:userId',  (request, response) => {
+
+    // Request Validation
+    const userId = request.params.userId;
+
+    const { name } = request.body;
+
+    if ( !name ) {
+        response.status(400).send(missingUserName);
+    }
+
+    // Database Promise Method
+    userDb.update(userId, {'name': name})
+    .then( didUpdate => {
+        if (!didUpdate) {
+            response.status(400).send(noUsersUpdated)
+        }
+
+        userDb.get(userId)
+        .then(user => {
+            if (!user) {
+                response.status(400).send(userNotFound)
+            }
+            response.status(200).send(user)
+        })
+        .catch(() => response.status(500).send(unableToFindSingleUser500))
+    })
+    .catch(() => response.status(500).send(unableToUpdateUser500))
 })
 
 //// ==========- POST DATABASE CRUD ENDPOINTS -==========
