2a7f03e44d7303a86200712e40d03377ff92afd1
alejandrok93 <alejandrok93@gmail.com>
2018-10-04 13:53:29 -0700

    Finished Posts CRUD operations

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const helmet = require("helmet");
 const userDb = require("./data/helpers/userDb.js");
 const postDb = require("./data/helpers/postDb.js");
 
+//import routes
+const postsRoutes = require("./routers/PostsRoutes.js");
+
 //instantiate server
 const server = express();
 
@@ -19,12 +22,13 @@ const makeUpperCase = (req, res, next) => {
 
 //use middleware
 server.use(express.json());
+server.use("/api/posts", postsRoutes);
 
 server.get("/", (req, res) => {
   res.send("hello node blog");
 });
 
-//User CRUD uperations
+//Users CRUD uperations
 server.get("/api/users", (req, res) => {
   const promise = userDb.get();
   promise
@@ -106,7 +110,78 @@ server.get("/api/posts", (req, res) => {
     .then(posts => {
       res.status(200).json(posts);
     })
-    .catch(err => res.send(err));
+    .catch(err =>
+      res.status(500).json({ error: "There was an error with that request" })
+    );
+});
+
+server.get("/api/posts/:id", (req, res) => {
+  id = req.params.id;
+  const promise = postDb.get(id);
+  promise
+    .then(post => {
+      if (!post) {
+        res.status(500).json({ error: "There was an error with that request" });
+      } else res.status(200).json(post);
+    })
+    .catch(err =>
+      res.status(500).json({ error: "There was an error with that request" })
+    );
+});
+
+server.post("/api/posts", (req, res) => {
+  const newPost = { text: req.body.text, userId: req.body.userId };
+  console.log(newPost);
+  const promise = postDb.insert(newPost);
+  promise
+    .then(idObj => {
+      if (!idObj) {
+        res.status(500).json({ error: "There was an error creating the post" });
+      } else {
+        postDb
+          .get(idObj.id)
+          .then(post => res.status(200).json(post))
+          .catch(err =>
+            res
+              .status(500)
+              .json({ error: "There was an error creating the user" })
+          );
+      }
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+server.put("/api/posts/:id", (req, res) => {
+  const id = req.params.id;
+  const updatedPost = { text: req.body.text };
+  const promise = postDb.update(id, updatedPost);
+  promise
+    .then(numOfRecords => {
+      if (numOfRecords === 0) {
+        res.status(500).json({ error: "There was an error updating the post" });
+      } else {
+        res.status(200).json({ message: "Post was updated successfully" });
+      }
+    })
+    .catch(err =>
+      res.status(500).json({ error: "There was an error with that request" })
+    );
+});
+
+server.delete("/api/posts/:id", (req, res) => {
+  const id = req.params.id;
+  const promise = postDb.remove(id);
+  promise
+    .then(numOfRecords => {
+      if (numOfRecords === 0) {
+        res.status(500).json({ error: "There was an error removing the post" });
+      } else {
+        res.status(200).json({ message: "Post was removed successfully" });
+      }
+    })
+    .catch(err =>
+      res.status(500).json({ error: "There was an error with that request" })
+    );
 });
 
 //listen on port 80
