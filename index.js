a0f9d5a445657456e2dbc70f08cc1f96fd5af8db
Lndubose <lndubose13@gmail.com>
2018-10-04 11:19:36 -0700

    Added an error handle middleware

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 const express = require('express');
 const server = express();
+const cors = require('cors');
 const Userdb = require('./data/helpers/userDb.js');
 
-server.use(express.json());
+server.use(express.json(), cors() );
 
-const Uppercase = (req, res, next) => {
+const validate = (req, res, next) => {
   if(!req.body.name) {
-    next();
-  }  else {
-    let newName = req.body.name.toUpperCase();
-    req.name = newName;
+    next('u1');
+  } else if (req.body.name.length > 128) {   
+    next('u2');
+  } else {
     next();
   }
+}
+
+const Uppercase = (req, res, next) => {
+  let newName = req.body.name.toUpperCase();
+  req.name = newName;
+  next();
 }
 
 server.get('/', (req, res) => {
@@ -22,29 +29,22 @@ server.get('/', (req, res) => {
       })
       res.status(200).json(response)
     })
-    .catch(err => res.status(400).json({error: "Could not retrieve users."}))
+    .catch(err => res.status(400).json({ error: "Could not retrieve users." }))
 })
 
-server.post('/', Uppercase, (req, res) => {
-  if(!req.body.name) {
-    res.status(400).json({ error: "Need a user." })
-  } else if(req.body.name.length > 129) {
-    res.status(400).json({ error: "Too many characters in name. Needs to be less than 129."})
-  } else {
-    let { name } = req.body;
-    const newUser = { name: req.name }
-    console.log(newUser)
-    Userdb.insert(newUser)
-      .then(response => {
-        const send = {
-          ...response,
-          ...newUser
-        }
-        res.status(201).json(send)
-      
+server.post('/', validate, Uppercase, (req, res) => {
+  let { name } = req.body;
+  const newUser = { name: req.name }
+  Userdb.insert(newUser)
+    .then(response => {
+      const send = {
+        ...response,
+        ...newUser
+      }
+      res.status(201).json(send)
+
     })
     .catch(err => res.status(400).json({ error: "User already exists." }));
-  }
 })
 
 
@@ -52,36 +52,51 @@ server.delete('/:id', (req, res) => {
   const { id } = req.params;
   Userdb.remove(id)
     .then(response => {
-      if(!response) {
+      if (!response) {
         res.status(404).json({ error: "No user with this id." })
       } else {
-        res.status(200).json({ message: "Success!"})
+        res.status(200).json({ message: "Success!" })
       }
     })
     .catch(err => res.status(400).json({ error: "Bad request." }))
 })
 
 
-server.put('/:id', Uppercase, (req, res) => {
-  if(!req.body.name) {
-    res.status(400).json({ error: "Need a new user to update."})
-  } else if(req.body.name.length > 129) {
-    res.status(400).json({ error: "Too many characters in name. Needs to be less than 129."})
-  } else {
-    const { id } = req.params;
-    const { name } = req.body;
-    const newUser = { name: req.name }
-    Userdb.update(id, newUser)
-      .then(response => {
-        if(!response) {
-          res.status(400).json({ error: "Wrong id was given"})
-        } else {
-          res.status(200).json({message: "Success!"})
-        }
-      })
-      .catch(err => res.status(400).json({ error: err }))
-  }
+server.put('/:id', validate, Uppercase, (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+  const newUser = { name: req.name }
+  Userdb.update(id, newUser)
+    .then(response => {
+      if (!response) {
+        res.status(400).json({ error: "Wrong id was given" })
+      } else {
+        res.status(200).json({ message: "Success!" })
+      }
+    })
+    .catch(err => res.status(400).json({ error: err }))
 })
+
+
+const errorHandle = (err, req, res, next) => {
+  const errors = {
+    u1: {
+      title: "Require Name",
+      description: "Name is required.",
+      httpCode: 400
+    },
+    u2: {
+      title: "Require Name",
+      description: "Name is too long, needs to be less than 128 characters.",
+      httpCode: 400
+    }
+  }
+
+  const error = errors[err];
+  res.status(error.httpCode).json(error);
+}
+
+server.use(errorHandle);
 
 port = 7000;
 server.listen(port, () => console.log(`---Ready to blog on ${port}---`));
