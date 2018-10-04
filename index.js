af1176985626e3a90d6cc50d97c74002f41ecd30
Huthman King <Huff@Huthmans-MacBook-Pro.local>
2018-10-04 17:19:55 -0400

    userRoutes created userRoutes installed a few more dependencies userRoutes works with postman

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // pull in express
 const express = require('express');
 const port = 9000;
-const dbUsers = require("./data/helpers/userDb.js");
+const userRoutes = require("./users/userRoutes.js");
+const configMiddleware = require("./config/middleware");
 
 // create server/connect to express
 const server = express();
@@ -10,106 +11,10 @@ server.use(express.json());
 
 // middleware
 
-function uppercase(req, res, next) {
-    req.body.name = req.body.name.charAt().toUpperCase() + req.body.name.slice(1);
-    next();
-  };
+configMiddleware(server);
 
 
-server.get("/api/users",  (req, res) => {
-    dbUsers.get()
-        .then(users => {
-            res.status(200).json(users);
-        })
-        .catch(err => {
-            console.log("error", err);
-            res.status(500).json({
-                error: "The user information could not be retrieved."
-            });
-        });
-});
-server.get("/api/users/:id",  (req, res) => {
-    dbUsers.get(req.params.id)
-        .then(users => {
-            res.status(200).json(users);
-        })
-        .catch(err => {
-            console.log("error", err);
-            res.status(500).json({
-                error: "The user information could not be retrieved."
-            });
-        });
-})
-
-
-
-server.post('/api/users', uppercase, (req, res) => {
-    console.log(req.body);
-    const { name } = req.body;
-    const newUser = { name};
-    dbUsers.insert(newUser)
-    .then(insertedUser => {
-        res.status(201).json({'User created': insertedUser});
-    })
-    .catch(err => {
-        res.send(err);
-    });
-    if (!name) {
-        res.status(400).json({
-            errorMessage: "Please provide a name for the user."
-        })
-    }
-   
-    dbUsers
-        .insert(newUser)
-        .then(userId => {
-            
-            dbUsers.get(userId).then(user => {
-                console.log(user);
-                if (!user) {
-                    return res
-                        .status(422)
-                        .json({
-                            Error: `User does not exist by that ${id}`
-                        });
-                }
-                res.status(201).json(user);
-            });
-        })
-        .catch(err => {
-            console.error(err);
-            res.json({
-                error: "There was an error saving the user to the database"
-            });
-        });
-});
-
-
-server.put("/api/users/:id", uppercase, (req, res) => {
-    dbUsers.update(req.params.id, req.body)
-        .then(users => {
-            res.status(200).json(users);
-        })
-        .catch(err => {
-            console.log("error", err);
-            res.status(500).json({
-                message: "Error: User could not be updating"
-            });
-        });
-});
-
-server.delete("/api/users/:id", (req, res) => {
-    dbUsers.remove(req.params.id)
-        .then(users => {
-            res.status(200).json(users);
-        })
-        .catch(err => {
-            console.log("error", err);
-            res.status(500).json({
-                message: "Error: User could not be deleted"
-            });
-        });
-});
+server.use("/api/users", userRoutes);
 
 
 
