292f04e063eea97a118957bfebddd54c330af621
m-j-hacker <mjhacker@gmail.com>
2018-10-03 23:52:35 -0500

    implemented caps middleware on user put request

--- index.js
+++ index.js
@@ -8,14 +8,35 @@ const cors = require('cors');
 const userDb = require('./data/helpers/userDb.js');
 const postDb = require('./data/helpers/postDb.js');
 
-const caps = (req, res, next) => {
-    req.user = req.params.users.toUpperCase();
+// function upper(obj) {
+//     for (var name in obj) {
+//         if (typeof obj["name"] === 'string') {
+//             obj["name"] = obj["name"].toUpperCase();
+//         }
+//         if (typeof obj["name"] === 'object') {
+//             upper(obj["name"]);
+//         }
+//     }
+//     return obj;
+// }
+
+ const caps = (req, res, next) => {
+     console.log(req.body);
+    req.body.name = req.body.name.toUpperCase();
     next();
-}
+ }
 
 server.use(logger('combined'));
 server.use(cors());
 server.use(express.json());
+
+server.post('/posts', (req, res) => {
+    console.log(req.body);
+    const { text, postedBy } = req.body;
+    const newPost = { text, postedBy };
+    postDb.insert(newPost)
+
+})
 
 server.get('/', (req, res) => {
     res.send('Howdy Pardner!');
@@ -32,9 +53,43 @@ server.get('/users', (req, res) => {
 
 
 
-server.get('/users/:user', caps, (req, res) => {
-    res.send(`${req.user}`);
+server.get('/users/:id', (req, res) => {
+    const id = req.params.id;
+    userDb.getUserPosts(id)
+    .then(user => {
+        res.status(200).json(user)
+    })
+    .catch(err => {
+        res.send(err);
+    });
+});
+
+server.get('/users/:name', caps, (req, res) => {
+    userDb.getUserPosts(req.id)
+    .then(user => {
+        res.status(200).json(user)
+    })
+    .catch(err => {
+        res.send(err);
+    });
+});
+
+server.delete('/users/:id', (req, res) => {
+    const id = req.params.id;
+    userDb.remove(id)
 })
+
+server.put('/users/:id', caps, (req, res) => {
+    const { id } = req.params;
+    const { name } = req.body;
+    const newUser = { name };
+    userDb.update(id, newUser)
+    .then(user => {
+        console.log('user = ', user);
+        res.status(200).json(user);
+    })
+    .catch(() => res.status(500).json({ error: "The post information could not be modified."}));
+});
 
 const port = 9001;
 server.listen(port, () => console.log(`The server is running on port ${port}, m'lady`));
