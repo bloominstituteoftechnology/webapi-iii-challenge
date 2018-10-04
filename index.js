210cac633bb1291d78f9581817b2f39e2b262f39
Benny Oseguera <oseguerabenny27@gmail.com>
2018-10-04 16:06:10 -0700

    Finished Day 2 - Benny Oseguera

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ server.use(express.json());
 const db = require("./data/dbConfig");
 // imports-helpers
 const userDb = require("./data/helpers/userDb");
+const postDb = require("./data/helpers/postDb");
 
 // Middlewares
 
@@ -70,6 +71,77 @@ server.put("/users/:id", (req, res) => {
     .update(id, newUser)
     .then(user => {
       res.status(200).json(user);
+    })
+    .catch(err => console.error(err));
+});
+
+//Get user posts
+server.get("/users/:id/posts", (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+  userDb
+    .getUserPosts(id)
+    .then(userPosts => {
+      res.json(userPosts);
+    })
+    .catch(err => console.error(err));
+});
+
+// ---------------Posts---------------
+
+// Get posts
+server.get("/posts", (req, res) => {
+  postDb
+    .get()
+    .then(userPost => {
+      res.json(userPost);
+    })
+    .catch(err => console.error(err));
+});
+
+// Get post
+server.get("/posts/:id", (req, res) => {
+  const { id } = req.params;
+  postDb
+    .get(id)
+    .then(userPost => {
+      res.json(userPost);
+    })
+    .catch(err => console.error(err));
+});
+
+// Post post (lol)
+server.post("/posts", (req, res) => {
+  const { text, userId } = req.body;
+  const newPost = { text, userId };
+  postDb
+    .insert(newPost)
+    .then(post => {
+      res.json(post);
+    })
+    .catch(err => console.error(err));
+});
+
+// Delete post
+server.delete("/posts/:id", (req, res) => {
+  const { id } = req.params;
+  postDb
+    .remove(id)
+    .then(post => {
+      res.json(post);
+    })
+    .catch(err => console.error(err));
+});
+
+// Put Post
+server.put("/posts/:id", (req, res) => {
+  const { id } = req.params;
+  const { text } = req.body;
+  const newPost = { text };
+  postDb
+    .update(id, newPost)
+    .then(post => {
+      res.json(post);
     })
     .catch(err => console.error(err));
 });
