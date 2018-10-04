60250bf070707658f395061dc1b5236aa0e39108
Kevin Nguyen <kevinn858@gmail.com>
2018-10-04 16:36:43 -0700

    fixed yarn start

--- index.js
+++ index.js
@@ -9,11 +9,12 @@ const configureMiddleware = require('./config/middleware.js');
 //Middlewares
 configureMiddleware(server);
 
+server.get('/', (req, res)=>{
+    res.status(200).json('root sucessfully got');
+});
+
+//have server on a port
 const port = 8000;
 server.listen(port, ()=>{
     console.log(`API running on port ${port}`);
-});
-
-server.get('/', (req, res)=>{
-    res.status(200).json('root sucessfully got');
 });
