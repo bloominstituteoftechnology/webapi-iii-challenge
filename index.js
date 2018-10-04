e97ce6ae6ae0f5be1ab287a75204c6146ee37e73
Brock Maraee Rohloff <00AgentB@gmail.com>
2018-10-04 14:00:57 -0600

    Implement CRUD Endpoint: READ All Posts Endpoint which retrieves an array of posts and sends the array to the client

--- index.js
+++ index.js
@@ -12,35 +12,76 @@ const server = express();
 
 server.use(express.json(), cors(), morgan('combined'), helmet());
 
-// ===============- SERVER CRUD ENDPOINTS -===============
+///// ===============- SERVER CRUD ENDPOINTS -===============
 
-// #####=- Root Server READ Endpoint -=#####
+//// ==========- USER DATABASE CRUD ENDPOINTS -==========
+
+/// #####=- Root Server READ Endpoint -=#####
 server.get('/', (request, response) => {
     response.status(200).send(`It's working!`);
 })
 
-// #####=- READ All Users Endpoint -=#####
+/// #####=- READ All Users Endpoint -=#####
 server.get('/users', (request, response) => {
 
-// Unique Error Message
-const error500 = { errorMessage: "Unable to retrieve users." };
+    // Unique Error Message
+    const error500 = { errorMessage: "Unable to retrieve users." };
 
-userDb.get()
-.then(users => response.status(200).send(users))
-.catch(() => response.status(500).send(error500))
+    // Database Promise Method
+    userDb.get()
+    .then(users => response.status(200).send(users))
+    .catch(() => response.status(500).send(error500))
 })
 
-// #####=- READ Individual User Endpoint -=#####
+/// #####=- READ Individual User Endpoint -=#####
 server.get('/users/:userId', (request, response) => {
 
-const userId = request.params.userId;
+    const userId = request.params.userId;
 
-// Unique Error Message
-const error500 = { errorMessage: "Unable to retrieve users." };
+    // Unique Error Message
+    const error500 = { errorMessage: "Unable to retrieve user." };
 
-userDb.get(userId)
-.then(user => response.status(200).send(user))
-.catch(() => response.status(500).send(error500))
+    // Database Promise Method
+    userDb.get(userId)
+    .then(user => response.status(200).send(user))
+    .catch(() => response.status(500).send(error500))
+})
+
+/// #####=- CREATE Individual User Endpoint -=#####
+server.post('/users',  (request, response) => {
+
+    // Database Promise Method
+    userDb.insert()
+    .then()
+    .catch()
+})
+
+//// ==========- POST DATABASE CRUD ENDPOINTS -==========
+
+/// #####=- READ All Posts Endpoint -=#####
+server.get('/posts', (request, response) => {
+
+    // Unique Error Message
+    const error500 = { errorMessage: "Unable to retrieve posts." };
+
+    // Database Promise Method
+    postDb.get()
+    .then(posts => response.status(200).send(posts))
+    .catch(() => response.status(500).send(error500))
+})
+
+/// #####=- READ Individual Post Endpoint -=#####
+server.get('/users/:postId', (request, response) => {
+
+    const userId = request.params.userId;
+
+    // Unique Error Message
+    const error500 = { errorMessage: "Unable to retrieve post." };
+
+    // Database Promise Method
+    postDb.get(postId)
+    .then(post => response.status(200).send(post))
+    .catch(() => response.status(500).send(error500))
 })
 
 // #####=- Server Port Address and Listen Method -=#####
