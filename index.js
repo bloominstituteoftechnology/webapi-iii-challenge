c48b63b8809262b9df285943cc93f70d847042b1
Alexandra <alexswartz9753@gmail.com>
2018-10-04 14:35:50 -0400

    added handlers for updating posts, adding new posts

--- index.js
+++ index.js
@@ -140,7 +140,26 @@ server.post('/api/posts', (req, res)=> {
 });
 
 //UPDATE an existing post
-
+server.put('/api/posts/:id', (req, res)=> {
+    console.log(req.params);
+    console.log(req.body);
+    const {id} = req.params;
+    const {text} = req.body;
+    const updatedPost = {text}
+    postDb.update(id, updatedPost)
+    .then(updatedPost=> {
+        if (!updatedPost) {
+            res.status(404).json({error: "This post does not exist"});
+        } else if (!req.body) {
+            res.status(400).json({error: "Please add some text to this post"});
+        } else {
+            res.status(200).json({updatedPost});
+        }
+    })
+    .catch(err=> {
+        res.status(500).json({error: "This information could not be saved to the database"});
+    })
+});
 
 //Listener
 server.listen(port, ()=> console.log(`API running on port ${port}`));
