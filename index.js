acd114e385670d685e470aed9b128dead68b21fd
Abdulfatah Ahmad <abdulfatah.a.ahmad@gmx.com>
2018-10-10 17:46:55 -0400

    Added Server, Port and Listner
    #COMMENT
    
    Added server using express
    Added port at 9000
    Added listner to the port using server.listen() method

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 const express = require('express');
 const cors = require('cors');
+
+const server = express();
+
+
+const port = 9000;
+
+//server.listen() method creates a listner on the specified port
+server.listen(port, () => console.log(`API is running on ${port}`));
