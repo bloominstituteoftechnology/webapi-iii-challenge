195a113bc6838d6a1d3c723da3640eb4e4f4fecc
Marious Davis <mlancelot88@gmail.com>
2018-10-10 16:52:26 -0500

    GET request handling added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { postDb, tagDb, userDb } = require("./data/helpers");
 
 const server = express();
 const port = 4000;
@@ -7,8 +8,34 @@ const port = 4000;
 server.use(express.json());
 server.use(cors());
 
-server.get("/", (req, res) => {
-  res.send("Testing 1,2,3. Hello!");
+server.get("/api/users/", (req, res) => {
+  userDb
+    .get()
+    .then(users => res.status(200).send(users))
+    .cathc(err =>
+      res
+        .status(500)
+        .json({ error: "The users information could not be found." })
+    );
+});
+
+server.get("/api/users/:id", (req, res) => {
+  const { id } = req.params;
+  userDb
+    .get(id)
+    .then(user => {
+      if (!user) {
+        return res
+          .status(404)
+          .json({ message: `User ID ${id} does not exist. ` });
+      }
+      return res.status(200).send(user);
+    })
+    .catch(err =>
+      res
+        .status(500)
+        .json({ error: "Users information could not be retrieved." })
+    );
 });
 
 server.listen(port, () => {
