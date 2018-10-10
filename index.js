e47cdd093c22333ab475a1febf305a52164cc1c2
lyang9 <lyang9@buffalo.edu>
2018-10-10 17:03:00 -0400

    server.get for root

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const userDb = require('./data/helpers/userDb');
 
 server.use(express.json());
 
+server.get('/', (req, res) => {
+  res.send('Welcome!');
+});
+
 const port = 5000;
 server.listen(port, () =>
   console.log(`\n=== API running on port ${port} ===\n`)
