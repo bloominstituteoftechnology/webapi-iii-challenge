8f82e95f396763e3971e441883c80575d2c619e9
verydecent <wnjhwng@gmail.com>
2018-10-10 15:22:54 -0700

    Bug fix, needed to add knex and sqlite3

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express');
-const helmet = require('helmet');
-const morgan = require('morgan');
-
 const server = express();
+// const helmet = require('helmet');
+// const cors = require('cors');
+// const morgan = require('morgan');
+const postDb = require('./data/helpers/postDb');
+const userDb = require('./data/helpers/userDb');
+
+// server.use(express.json());
+// server.use(cors());
 
 server.get('/', (req, res) => {
     res.send('Orange e30');
-})
+});
 
 const port = 3400;
-server.listen(port, () => console.log(`API running on Port ${port}`));
+server.listen(port, () => console.log(`\n === Port ${port} === \n`));
