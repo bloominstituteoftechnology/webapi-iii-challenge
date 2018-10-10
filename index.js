df9aaaab3dde60c23eee4e12858d3a0826f7a0c0
lyang9 <lyang9@buffalo.edu>
2018-10-10 17:26:33 -0400

    server.get by userId

--- index.js
+++ index.js
@@ -21,6 +21,20 @@ server.get('/api/users', (req, res) => {
     })
 });
 
+server.get('/api/users/:userId', (req, res) => {
+  const userId = req.params.userId;
+  userDb.get(userId)
+    .then(user => {
+      if (!user) {
+        res.status(404).json({ message: "The user with the specified ID does not exist." });
+      }
+      res.status(200).json(user);
+    })
+    .catch(err => {
+      res.status(500).json({ error: "The user information could not be retrieved.", err });
+    });
+});
+
 const port = 5000;
 server.listen(port, () =>
   console.log(`\n=== API running on port ${port} ===\n`)
