401d3a3b5931fafc5a616f864df788d479928e92
lyang9 <lyang9@buffalo.edu>
2018-10-10 17:13:49 -0400

    server.get for all users

--- index.js
+++ index.js
@@ -11,6 +11,16 @@ server.get('/', (req, res) => {
   res.send('Welcome!');
 });
 
+server.get('/api/users', (req, res) => {
+  userDb.get()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => {
+      res.status(500).json({ error: "The users information could not be retrieved.", err });
+    })
+});
+
 const port = 5000;
 server.listen(port, () =>
   console.log(`\n=== API running on port ${port} ===\n`)
