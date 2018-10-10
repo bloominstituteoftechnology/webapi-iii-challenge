ffa21775aefa93f92d5fdeb4c70bdfee97754151
verydecent <wnjhwng@gmail.com>
2018-10-10 15:26:31 -0700

    get request handle

--- index.js
+++ index.js
@@ -13,5 +13,11 @@ server.get('/', (req, res) => {
     res.send('Orange e30');
 });
 
+server.get('/users', (req, res) => {
+    userDb.get()
+        .then(users => res.status(200).send(users))
+        .catch(err => res.status(500).json({ error: "The user info could not be found"}));
+})
+
 const port = 3400;
 server.listen(port, () => console.log(`\n === Port ${port} === \n`));
