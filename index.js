be55b124f815641fe36abda9219d9d573d7208ff
verydecent <wnjhwng@gmail.com>
2018-10-10 14:52:42 -0700

    Testing server using get request

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const morgan = require('morgan');
 
 const server = express();
 
-server.use(express.json(), cors());
+server.get('/', (req, res) => {
+    res.send('Orange e30');
+})
 
 const port = 3400;
 server.listen(port, () => console.log(`API running on Port ${port}`));
