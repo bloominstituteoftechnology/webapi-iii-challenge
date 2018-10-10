5ce044e9a82befe09ff00ee01217b28cf3b48a2a
Sasha <taylork85@gmail.com>
2018-10-10 17:54:04 -0500

    Added middleware and post endpoint

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ const usersDb = require('./data/helpers/userDb.js');
 const port = 7000;
 
 const server = express();
+server.use(logger);
+
+const upper = (req, res, next) => {
+    const newName = req.params.name.toUpperCase();
+    req.name = newName;
+    next();
+}
 
 server.get('/', (req, res) => {
     res.send("New Blog!");
@@ -13,10 +20,32 @@ server.get('/', (req, res) => {
 server.get('/api/users', (req, res) => {
     usersDb.get()
     .then(users => {
+        console.log(req.params);
         res.status(200).json(users);
     })
     .catch(err => res.send(err));
 })
+
+server.get('/api/users/:id', (req, res) => {
+    const id = req.params.id;
+    usersDb.get(id)
+    .then(user => {
+        res.status(200).json(user);
+    })
+    .catch(err => res.send(err));
+})
+
+server.post('/api/users', upper, (req, res) => {
+    const newUser = req.body.name;
+    usersDb.insert(newUser)
+    .then(id => {
+        usersDb.get(id).then(user => {
+            res.status(201).json(user);
+        })
+    })
+    .catch(err => res.send(err));
+})
+
 
 
 
