aa0f57e0874ca1ce77a3af99b3b84285043daf6b
Abdulfatah Ahmad <abdulfatah.a.ahmad@gmx.com>
2018-10-11 18:06:20 -0400

    Added Custom Middleware
    #Comment
    
    Added CapilalizeNames that turns names to upper case

--- index.js
+++ index.js
@@ -9,14 +9,31 @@ const port = 7000;
 
 //const server = express();
 
-
+//Global middleware
+//express.json returns json objects of the response
 server.use(express.json(), logger('combined'), cors(), helmet());
+
+//Custom middleware
+
+const capitalNames = (req, res, next) => {
+    req.body.name = req.body.name.toUpperCase();
+    next();
+}
 
 //Routes
 
+// server.get('/users', (req, res) => {
+//     res.json('users');
+// });
+
 server.get('/users', (req, res) => {
-    res.send('users');
+    userDb.get()
+        .then(users => res.json(users))
+        .catch(err => res.status(500).json({ error: "The user info could not be found" }));
 });
+
+
+
 //server.listen() method creates a listner on the specified port
 server.listen(port, () => console.log(`API is running on ${port}`));
 
