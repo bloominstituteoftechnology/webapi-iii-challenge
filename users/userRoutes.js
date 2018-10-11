83a36fc5651e8bf3e1ad9674c5055bd44c1d38ab
Joseph Chretien-Golden <joechre@gmail.com>
2018-10-11 15:48:03 -0500

    setting up routes for users

--- users/userRoutes.js
+++ users/userRoutes.js
@@ -1,2 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const userDb = require('../data/helpers/userDb.js');
+const upperCaser = require('../middlewares/middlewares.js').upperCaser;
+
+router.route('/')
