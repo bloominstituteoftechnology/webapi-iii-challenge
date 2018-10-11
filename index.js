40351c2e4b58fd3e1094276bc9612c71577c3d02
MichelleOk <umbralcompendium@gmail.com>
2018-10-11 00:08:40 -0600

    wip

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ console.log("Hello from  index.js!");
 
 const express = require('express');
 const server = express();
-const db = require('./data/dbConfig.js');
+// const helmet = require('helmet');
+const userDb = require('./data/helpers/userDb.js');
 // const cors = require('cors');
 
 server.use(express.json());
@@ -17,39 +18,33 @@ server.get('/', (req, res) => {
 //Routes
 
 //GET all users
-server.get('/api/users', (req, res) => {
-    db.find().then(users => {
-        console.log(users);
-        res.json(users);
-    if(!users) {
-        return res.status(500)}
-        res.json({ error: "The posts information could not be retrived."});
-    })
-    .catch(error => res.send(error));
-});
+// server.get('/users', (req, res) => {
+//     userDb.get().then(users => {
+//         console.log(users);
+//         res.json(users);
+//     if(!users) {
+//         return res.status(500)}
+//         res.json({ error: "The posts information could not be retrived."});
+//     })
+//     .catch(error => res.send(error));
+// });
 
 //GET user by id
-server.get('/api/users/:id', (req, res) => {
-    const { id } = req.params;
-    db.findById(id).then(post => {
-        if(post.length === 0) {
-            return res.status(404).send({ message: "The user with the specified id does not exist." });
-        }
-        res.status(200).json(user);
-    })
-    .catch(error => rs.status(500).send({ error: "The user information could not be retrieved." }));
-});
+// server.get('/users/:id', (req, res) => {
+//     const { id } = req.params;
+//     userDb.get(id).then(user => {
+//         if(user.length === 0) {
+//             return res.status(404).send({ message: "The user with the specified id does not exist." });
+//         }
+//         res.status(200).json(user);
+//     })
+//     .catch(error => res.status(500).send({ error: "The user information could not be retrieved." }));
+// });
 
-//Post new user
-server.post('api/users', (req, res) => {
-    const { name } = req.body;
-    const newUser = { name };
-    db.insert(newUser).then(userId => {
-        const { id } = userId;
-        db.findById(id).then(user => {
-            console.log(user);
-            res.status(201).send(user);
-        });
+// //Post new user
+server.post('/users', (req, res) => {
+    userDb.insert(req.body).then(id => {
+        res.status(201).json(id);
     })
     .catach(error => {
         if(!name) {
@@ -65,4 +60,13 @@ server.post('api/users', (req, res) => {
 
 
 const port = 9000;
-server.listen(port, () => console.log(`API running circles on port ${port}`));
+server.listen(port, () => console.log(`API running fire circles on this port ${port}`));
+
+// const { name } = req.body;
+    // const newUser = { name };
+    // userDb.insert(newUser).then(userId => {
+    //     const { id } = userId;
+    //     userDb.post(id).then(user => {
+    //         console.log(user);
+    //         res.status(201).send(user);
+    //     });
