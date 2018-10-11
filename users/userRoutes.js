1c7837c8ed40a4c3af42eb1b701d25ea54725a96
Joseph Chretien-Golden <joechre@gmail.com>
2018-10-11 16:29:24 -0500

    route post promise configured

--- users/userRoutes.js
+++ users/userRoutes.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const userDb = require('../data/helpers/userDb.js');
 const upperCaser = require('../middlewares/middlewares.js').upperCaser;
 
 router.route('/')
+.post(upperCaser, (req,res) => {
+    const { name } = req
+    const newUser = { name };
+if(!name) return res.status(400).json({errorMessage: "Please provide a user name."});
+userDb.insert(newUser)
+    .then(user =. res.status(201).json(user))
+    .catch(err => res.status(500).json({error: "There was an error while saving the user to teh database"}))
+})
