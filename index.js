5d491b6a923cb81b3770d984b0b5f7d82ea32850
Abdulfatah Ahmad <abdulfatah.a.ahmad@gmx.com>
2018-10-11 16:26:59 -0400

    Added Knex & Sqlite3
    #Comment
    
    Added knex and sqlite to access database

--- index.js
+++ index.js
@@ -7,4 +7,6 @@ const server = express();
 const port = 9000;
 
 //server.listen() method creates a listner on the specified port
-server.listen(port, () => console.log(`API is running on ${port}`));
+//server.listen(port, () => console.log(`API is running on ${port}`));
+
+server.use(express.json());
