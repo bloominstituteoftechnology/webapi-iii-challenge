6fcb631542ceec966d75d1143aaf915a10f4d76e
Emily McClanahan <emily.f.mcclanahan@gmail.com>
2018-10-11 17:01:06 -0500

    all user endpoints in place and working. Started on posts endpoints

--- index.js
+++ index.js
@@ -4,17 +4,21 @@ const cors = require('cors');
 const helmet = require('helmet');
 
 const server = express();
-const port = 8001
+const port = 9000
 
 const userDb = require('./data/helpers/userDb');
+const postDb = require('./data/helpers/postDb');
 
 //** Middleware **//
+const makeCaps = (req, res, next) => {
+    req.body.name = req.body.name.toUpperCase();
+    next();
+};
+server.use(express.json(),logger('tiny'), cors(), helmet());
 
+//** User Routes **//
 
-
-//** Endpoints **//
-
-//get
+//get all
 server.get('/api/users', (req,res) => {
     userDb
     .get()
@@ -23,7 +27,107 @@ server.get('/api/users', (req,res) => {
     })
     .catch(err => res.status(500).json({error: "user not found"}));
 });
+
 //get by id
+server.get('/api/users/:userId', (req, res) => {
+    const userId = req.params.userId;
+    userDb
+    .get(userId)
+    .then(user => {
+        if(!user) {
+            res.status(404).json({message: 'user not found'});
+        } else {
+            res.status(200).send(user);
+        }
+    })
+    .catch(err => {
+        res.status(500).json({error: 'user could not be populated'})
+    });
+});
+
+//post
+server.post('/api/users', makeCaps, (req,res) => {
+    const userInfo = req.body;
+    console.log("userInfo", userInfo);
+    userDb
+    .insert(userInfo)
+    .then(response => {
+        res.status(201).json(response);
+    })
+    .catch(err => {
+        if (err.errno === 19) {
+            res.status(400).json({msg: 'please provide all required fields'})
+        } else {
+            res.status(500).json({error: err});
+        }
+    });
+});
+
+//put
+server.put('/api/users/:userId', function(req, res) {
+    const id = req.params.userId;
+    const update = req.body;
+    userDb
+    .update(id, update)
+    .then(count => {
+        if (count > 0) {
+            userDb.get(id).then(users => {
+                res.status(200).json(users[0]);
+            })
+        } else {
+            res.status(404).json({msg: 'user not found'});
+        }
+    })
+    .catch(err => {
+        res.status(500).json(err)
+    })
+})
+
+//delete
+server.delete('/api/users/:userId', function(req, res){
+    const id = req.params.userId;
+    userDb
+    .get(id)
+    .then(user => {
+        userDb.remove(id)
+        .then(response => {
+            res.status(200).json(user);
+        })
+    })
+    .catch(err =>{
+        res.status(500).json({error:err});
+    });
+});
+
+
+//** Post Routes **//
+
+//get all
+server.get('/api/posts', (req,res) => {
+    postDb
+    .get()
+    .then(posts => {
+        res.json(posts);
+    })
+    .catch(err => res.status(500).json({error: "posts not found"}));
+});
+
+//get by id
+server.get('/api/posts/:postId', (req, res) => {
+    const postId = req.params.postId;
+    postDb
+    .get(postId)
+    .then(post => {
+        if(!post) {
+            res.status(404).json({message: 'post not found'});
+        } else {
+            res.status(200).send(post);
+        }
+    })
+    .catch(err => {
+        res.status(500).json({error: 'post could not be populated'})
+    });
+});
 
 //post
 
