a9dea08d7d9cfa5eefbe9cd41d2da79c4bd736ec
Marious Davis <mlancelot88@gmail.com>
2018-10-11 09:39:14 -0500

    DELETE request handling added. minor syntax corrections

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ server.get("/api/users/:id", (req, res) => {
       if (!user) {
         return res
           .status(404)
-          .json({ error: `User ID ${id} does not exist. ` });
+          .json({ error: `User, ID ${id}, does not exist. ` });
       }
       return res.status(200).json(user);
     })
@@ -111,16 +111,38 @@ server.put("/api/users/:id", capital, (req, res) => {
         })
         .catch(err =>
           res.status(500).json({
-            error:
-              "Newly updated user's information could not be retrieved."
+            error: "Newly updated user's information could not be retrieved."
           })
         );
     })
     .catch(err =>
       res.status(500).json({
         error:
-          "An error has occured while updating user information in the database"
+          "An error has occured while updating user information in the database."
       })
+    );
+});
+
+server.delete("/api/users/:id", (req, res) => {
+  const { id } = req.params;
+  userDb
+    .remove(parseInt(id))
+    .then(del => {
+      if (!del) {
+        return res
+          .status(404)
+          .json({ error: `User, ID ${id}, does not exist.` });
+      }
+      return res
+        .status(200)
+        .json({ message: `User, ID ${id}, was successfully deleted.` });
+    })
+    .catch(err =>
+      res
+        .status(500)
+        .json({
+          error: "An error has occured while deleting the user in the database."
+        })
     );
 });
 
