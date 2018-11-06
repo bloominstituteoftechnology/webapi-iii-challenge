c7f2f2bb4273fc31c9cfe44f28f84a3e9542f616
Carlos Lantigua <clantigua954@gmail.com>
2018-11-06 16:54:55 -0500

    get all posts and get post by ID added, trying to add morgan middleware

--- index.js
+++ index.js
@@ -1,9 +1,44 @@
 const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
+// const cors = require('cors');
 const postDB = require('./data/helpers/postDb');
 const userDB = require('./data/helpers/userDb');
 const tagDB = require('./data/helpers/tagDb');
 
 const server = express();
+server.use(express.json());
+server.use(morgan('dev'));
+server.use(helmet());
+// server.use(cors());
 
-const port = 9000;
-server.listen(port, () => console.log(`The port is OVER ${port}!!`));
+server.get('/', (req, res) => {
+	res.json('alive');
+});
+
+// get all posts
+server.get('/api/posts/', (req, res) => {
+	postDB
+		.get()
+		.then((post) => {
+			res.status(200).json(post);
+		})
+		.catch((err) => err.status(500).json({ message: "Can't retrieve post data!!" }));
+});
+
+// get post by id
+server.get('/api/posts/:id', (req, res) => {
+	const { id } = req.params;
+	postDB.get(id).then((post) => {
+		post.length <= 0 ? res.status(404).json({ message: 'That post was not found' }) : res.status(200).json(post);
+	});
+});
+
+// get post by tag
+// server.get('/posts/search', (req, res) => {
+// 	const { id } = req.query;
+
+// 	id ? tagDB.get(id).then((posts) => res.send(posts)) : debug.find().then((posts) => res.send(posts));
+// });
+// const port = 9000;
+// server.listen(port, () => console.log(`The port is OVER ${port}!!`));
