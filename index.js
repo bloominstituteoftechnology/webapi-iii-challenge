fcf23cf5eb7930fd986aa0d5c970ca0f2a942e38
Lee Len Formento <leebatef@gmail.com>
2018-11-06 13:06:33 -0500

    Imported cors, helmet, logger, database for user. GET user completed

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 // modules
 const express = require('express');
 const helmet = require ('helmet');
-const morgan = require('morgan');
+const cors = require('cors');
+const logger = require('morgan');
+const userDb = require('./data/helpers/userDb.js');
 
 // server
 const server = express();
 
 // middlewares
 server.use(express.json());
+server.use(logger(`combined`));
+server.use(cors());
+server.use(helmet());
+
+server.get('/', (req, res) => {
+    res.send('Blog');
+})
+
+server.get('/api/users', (req, res) => {
+    userDb.get().then(users => {
+        console.log('\n*** user **', users);
+        res.status(200).json(users);
+    })
+    .catch(err => res.status(500).json({ error: "The information of users could not be retrieved. "}))
+})
 
 
 const port = 9000;
