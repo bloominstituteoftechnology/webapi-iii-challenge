8b0419c2320f9b56a97b88d79765533946e6387e
Matthew Wright <matthewbwright@gmail.com>
2018-11-07 14:30:20 -0700

    initial commit

---
+++ api/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+  res.status(200).json({ api: 'running' });
+});
+
+module.exports = server;
