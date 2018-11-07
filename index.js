20a33ea235f37214d44ef627a25d4d5ba2607d39
Lajawanti <lajawanti.dhake@gmail.com>
2018-11-07 16:21:32 -0500

    server.post done

--- index.js
+++ index.js
@@ -8,12 +8,28 @@ const server = express();
 server.use(express.json());
 
 server.get('/users', (request, response) => {
-        console.log(request.body);
         db.get()
           .then(users => {
                 response.status(200).json(users); 
            })
+          .catch(error => {
+                response.status(500).json({error : 'The users data could not be retrieved'})
+           }) 
 })
+
+server.post('/users', (request, response) => {
+    if(request.body.name !== undefined) {
+        db.insert(request.body)
+          .then(userId => {
+                response.status(201).json(userId);
+           })
+          .catch(error => {
+                response.status(500).json({message : 'error creating user', error});
+           }) 
+    } else {
+        response.status(400).json({ errorMessage: "Please provide name for the user." })
+    }
+}) 
 
 
 server.listen(9000, () => {
