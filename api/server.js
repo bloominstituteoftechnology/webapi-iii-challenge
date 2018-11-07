6dff1d00f9ecd9e1d504dfea8cd5010efaed1098
adamhinckley <adamhinckley@mac.com>
2018-11-07 13:51:41 -0700

    get request working

--- api/server.js
+++ api/server.js
@@ -1,13 +1,27 @@
 const express = require("express");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const cors = require("cors");
+const postDB = require("../data/helpers/postDb");
+
 const server = express();
 
 //middleware
 server.use(express.json());
 server.use(helmet());
 server.use(morgan("dev"));
+server.use(cors({ origin: "http://localhost:3000" }));
 
 //endpoints
+server.get("/api/posts", (req, res) => {
+  postDB
+    .get()
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "the post info could not be received" });
+    });
+});
 
 module.exports = server;
