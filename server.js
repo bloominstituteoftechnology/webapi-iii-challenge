f07c2ea2c21c69ccd673337948df4ead57264861
jsmnglng <jsmn.glng@gmail.com>
2018-11-07 12:12:31 -0800

    Project setup

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const userDb = require('./data/helpers/userDb');
+
+const server = express();
+const port = 9000;
+
+server.get('/api/users', (req, res) => {
+  res.send('hi')
+})
+
+server.listen(port, () => console.log(`Listening to port ${port}`));
