21c0c1b3195a41f8b73328da676082a1c7f2e8ab
rytwalker <rytwalker@gmail.com>
2018-11-07 16:27:40 -0500

    Added a GET path to get a single users posts

--- api/server.js
+++ api/server.js
@@ -3,6 +3,9 @@ const userDb = require('../data/helpers/userDb');
 
 const server = express();
 
+// Middleware
+server.use(express.json());
+
 // USER ROUTES
 
 // GET an array of all users
@@ -24,6 +27,21 @@ server.get('/api/users/:id', async (req, res) => {
       res.status(404).json({ message: 'That user does not exist' });
     } else {
       res.status(200).json(user);
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Could not retrieve that user' });
+  }
+});
+
+// GET a single users posts
+server.get('/api/users/:id/posts', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const userPosts = await userDb.getUserPosts(id);
+    if (!userPosts) {
+      res.status(404).json({ message: 'That user does not exist' });
+    } else {
+      res.status(200).json(userPosts);
     }
   } catch (error) {
     res.status(500).json({ error: 'Could not retrieve that user' });
