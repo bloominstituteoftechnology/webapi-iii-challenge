71b9c3dd9f69c0217174e8b3cc5cde44201ba271
BrandonVilaychith <blvilaychith@gmail.com>
2018-11-07 14:53:24 -0600

    Finished GET /api/users

--- server.js
+++ server.js
@@ -1,7 +1,25 @@
 // Imports
 const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const userDb = require('./data/helpers/userDb.js');
 
 // Create server
 const server = express();
 
+// Middleware
+server.use(morgan('dev'));
+server.use(helmet());
+server.use(express.json());
+
+// Routes
+server.get('/api/users', (req, res) => {
+  userDb.get()
+    .then(users => res.status(200).json(users))
+    .catch(error => res.status(500).json({ message: 'There has been an error getting the users.' }));
+});
+
+
+
+// Server export
 module.exports = server;
