7c7791c34715f53e5d69552365287a3875cfd3bf
adamhinckley <adamhinckley@mac.com>
2018-11-07 16:53:39 -0700

    both get requests working again

--- api/server.js
+++ api/server.js
@@ -3,6 +3,7 @@ const helmet = require("helmet");
 const morgan = require("morgan");
 const cors = require("cors");
 const postDB = require("../data/helpers/postDb");
+const userDB = require("../data/helpers/userDb");
 
 const server = express();
 
@@ -13,11 +14,28 @@ server.use(morgan("dev"));
 server.use(cors({ origin: "http://localhost:3000" }));
 
 //endpoints
-server.get("/api/posts", (req, res) => {
-  postDB
+server.get("/api/users", (req, res) => {
+  userDB
     .get()
     .then(posts => {
       res.status(200).json(posts);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "the post info could not be received" });
+    });
+});
+
+server.get("/api/users/:id", (req, res) => {
+  const { id } = req.params;
+  console.log("get with ID", req.params);
+  userDB
+    .get(id)
+    .then(post => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res.status(404).json({ message: "the post with the specified ID does not exist" });
+      }
     })
     .catch(err => {
       res.status(500).json({ message: "the post info could not be received" });
