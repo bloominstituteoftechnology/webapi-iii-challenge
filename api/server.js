1473f8297089049a24ae8a8653938be0028688c8
Drewsup123c <drewsup123c@gmail.com>
2018-11-07 16:12:46 -0700

    modified the endpoints to match

--- api/server.js
+++ api/server.js
@@ -20,7 +20,7 @@ server.get('/', (req, res) => {
 });
 
 //users .gets 
-server.get('/users',(req,res)=>{
+server.get('/api/users',(req,res)=>{
     userDb.get()
         .then(users => {
             res.status(200).json(users)
@@ -30,8 +30,20 @@ server.get('/users',(req,res)=>{
         })
 });
 
+server.get("/api/users/:id",(req,res)=>{
+    const { id } = req.params;
+
+    userDb.get(id)
+        .then(user => {
+            res.status(200).json(user)
+        })
+        .catch(error => {
+            res.status(500).json("Can not get the users ", error)
+        })
+})
+
 //posts .gets
-server.get('/posts',(req,res)=>{
+server.get('/api/posts',(req,res)=>{
     postDb.get()
         .then(posts => {
             res.status(200).json(posts)
@@ -40,6 +52,18 @@ server.get('/posts',(req,res)=>{
             res.status(500).json("can not find the posts ",error)
         })
 });
+
+server.get("/api/posts/:id",(req,res)=>{
+    const { id } = req.params;
+
+    postDb.get(id)
+        .then(user => {
+            res.status(200).json(user)
+        })
+        .catch(error => {
+            res.status(500).json("Can not get the users ", error)
+        })
+})
 
 // server.get('/secret', gatekeeper, (req, res) => {
 //     res.send(req.welcomeMessage);
