93c27645fcae8beb4999c6ff4394a0898ec0c15c
theryanclausen <theryanclausen@gmail.com>
2018-11-07 16:44:54 -0700

    MVP

--- index.js
+++ index.js
@@ -59,10 +59,25 @@ server.post("/api/users/", capitalizer, async (req, res) => {
     }
     const id = await userDB.insert({ name });
     const user = await userDB.get(id.id);
-    await res.json(user);
+    res.status(200).json(user);
   } catch (err) {
     sendError(500, "server error", res);
   }
 });
+
+server.put('/api/users/:id', capitalizer, async (req,res) => {
+    try {
+        const {id} = req.params;
+        const {name} = req.body;
+        const count = await userDB.update(id, {name})
+        const user = await userDB.get(id)
+        if (!count){
+            sendError(404, 'no users updated', res)
+        }
+        res.status(200).json(user)
+    }catch(err){
+        sendError(500, 'server error', res)
+    }
+})
 
 server.listen(port, () => console.log(`we hear you ${port}`));
