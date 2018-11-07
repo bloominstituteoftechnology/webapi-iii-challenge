0fadb5edf17bfc81f71bfdbac8729dae2e0c3844
koennecker <koenneckerb@gmail.com>
2018-11-07 14:31:42 -0700

    Added GET request functionality for the list of users, individual users, and posts by individual users.

--- index.js
+++ index.js
@@ -22,6 +22,64 @@ const upperCase = (req, res, next) => {
   next();
 };
 
+server.get("/api/users", (req, res) => {
+  userDb
+    .get()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => {
+      sendUserError(
+        500,
+        "The users' information could not be retrieved.",
+        res,
+        err
+      );
+    });
+});
+
+server.get("/api/users/:id", (req, res) => {
+  const { id } = req.params;
+  userDb
+    .get(id)
+    .then(user => {
+      if (user) {
+        res.status(200).json(user);
+      } else {
+        res.status(404).json({ error: `User id ${id} not found` });
+      }
+    })
+    .catch(err => {
+      sendUserError(
+        500,
+        "The user's information could not be retrieved.",
+        res,
+        err
+      );
+    });
+});
+
+server.get("/api/users/:id/posts", (req, res) => {
+  const { id } = req.params;
+  userDb
+    .getUserPosts(id)
+    .then(posts => {
+      if (posts.length > 0) {
+        res.status(200).json(posts);
+      } else {
+        res.status(404).json({ error: `User, id ${id} not found` });
+      }
+    })
+    .catch(err => {
+      sendUserError(
+        500,
+        "The user's post information could not be retrieved.",
+        res,
+        err
+      );
+    });
+});
+
 server.listen(port, () => {
   console.log(`\n=== Listening on port ${port} ===\n`);
 });
