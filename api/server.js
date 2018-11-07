067c6f4cc91d11c5efd0d233ec8e1c287fa2db96
TRIF3X <thewizardofcrypto@gmail.com>
2018-11-07 16:59:07 -0600

    server get by userid and id working

--- api/server.js
+++ api/server.js
@@ -1,12 +1,42 @@
 const express = require('express')
 const server = express();
-server.use(express.json())
+const userDb = require('../data/helpers/userDb');
+server.use(express.json());
 
+function allCaps(req, res, next) {
+    const { body } = req;
+    console.log(body.name)
+    next();
+}
 
+server.use(allCaps)
 
+server.get('/user/:id', allCaps, (req, res) => {
+    userDb.get(req.params.id)
+        .then(user => {
+            if(user) {
+                res.status(200).json(user)
+            } else {
+                res.status(404).json({ message: 'user not found' })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'The user ID can not be found', error: err })
+        })
+})
 
-server.get('/', (req, res) => {
-    res.send('what up')
+server.get('/user/posts/:id', (req, res) => {
+    userDb.getUserPosts(req.params.id)  
+        .then(users => {
+            if(users.length === 0 ) {
+                res.status(404).json({ message: 'can not find users posts' })
+                } else {
+                res.status(200).json(users)
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ message: `the posts from ${req.params.id} can not be found` })
+        })    
 })
 
 
