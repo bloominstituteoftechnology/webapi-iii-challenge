5a574068d0ce215098855eefd3bb286c48455aaa
rytwalker <rytwalker@gmail.com>
2018-11-07 16:23:07 -0500

    Added a git request for a single user

--- api/server.js
+++ api/server.js
@@ -15,4 +15,19 @@ server.get('/api/users', async (req, res) => {
   }
 });
 
+// GET a single user
+server.get('/api/users/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const user = await userDb.get(id);
+    if (!user) {
+      res.status(404).json({ message: 'That user does not exist' });
+    } else {
+      res.status(200).json(user);
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Could not retrieve that user' });
+  }
+});
+
 module.exports = server;
