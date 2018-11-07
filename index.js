79f019df63da5ba3bd7e65e102f1dc8fd9c8910f
Hunter315 <hunter.mckay.smith@gmail.com>
2018-11-07 13:08:47 -0700

    delete post endpoints

--- index.js
+++ index.js
@@ -152,6 +152,25 @@ server.get("/api/posts", (req, res) => {
   });
   
   //Delete a post
+  server.delete("/api/posts/:id", (req, res) => {
+    const { id } = req.params;
+    postDb
+      .remove(id)
+      .then(post => {
+        if (post.length < 1) {
+          res.status(404).json({
+            message: "The post with that ID does not exist."
+          });
+        } else {
+          res.status(200).json(post);
+        }
+      })
+      .catch(err =>
+        res.status(500).json({
+          error: "The post could not be removed."
+        })
+      );
+  });
   
   //Update a post
   
