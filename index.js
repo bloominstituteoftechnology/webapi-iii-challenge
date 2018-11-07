4f9cd651e2cdbdb13dedba82eb73b697790ac14e
KHollobaugh <khollobaugh@gmail.com>
2018-11-07 17:52:34 -0500

    all user/post endpoints working except posts/:id

--- index.js
+++ index.js
@@ -20,11 +20,27 @@ server.get('/users', (req, res) => {
     })
 })
 
+server.get('/posts', (req, res) => {
+    postDb.get()
+    .then(posts => res.status(200).json(posts))
+.catch(err => {
+    res.status(500).json({ message: "Couldn't retrieve posts" })
+})
+})
+
 server.get('/users/:id', (req, res) => {
     userDb.get(req.params.id)
     .then(user => res.status(200).json(user))
     .catch(err => {
         res.status(404).json({ message: 'there is no user by that ID'})
+    })
+})
+
+server.get('/posts/:id', (req, res) => {
+    postDb.get(req.params.id)
+    .then(post => res.status(200).json(post))
+    .catch(err => {
+        res.status(404).json({ message: 'there is no post by that ID'})
     })
 })
 
@@ -36,6 +52,14 @@ server.post('/users', (req, res) => {
     })
 })
 
+server.post('/posts', (req, res) => {
+    postDb.insert(req.body)
+    .then(done => res.status(201).json(done))
+    .catch(err => {
+        res.status(400).json({ message: 'did not add new post'})
+    })
+})
+
 server.put('/users/:id', (req, res) => {
     userDb.update(req.params.id, req.body)
     .then(user => res.status(201).json(user))
@@ -44,13 +68,30 @@ server.put('/users/:id', (req, res) => {
     })
 })
 
+server.put('/posts/:id', (req, res) => {
+    postDb.update(req.params.id, req.body)
+    .then(post => res.status(201).json(post))
+    .catch(err => {
+        res.status(400).json({ message: "failed to update the post" })
+    })
+})
+
 server.delete('/users/:id', (req, res) => {
     userDb.remove(req.params.id)
+    .then(count => res.status(200).json(count))
+    .catch(err => {
+        res.status(400).json({ message: "could not delete user"})
+    })
+})
+
+server.delete('/posts/:id', (req, res) => {
+    postDb.remove(req.params.id)
     .then(count => res.status(200).json(count))
     .catch(err => {
         res.status(400).json({ message: "could not delete post"})
     })
 })
+
 
 
 
