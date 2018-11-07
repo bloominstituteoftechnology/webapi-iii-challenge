31e9f2a0bd4b9a2bd4b1091044ab54b9f9105b12
frankbenevento <frankbenevento123@gmail.com>
2018-11-07 12:55:35 -0800

    added get and get by id for the users

--- api/server.js
+++ api/server.js
@@ -1,4 +1,33 @@
 const express = require('express');
+const cors = require('cors');
+const postHelper = require('../data/helpers/postDb')
+const userHelper = require('../data/helpers/userDb')
+
 const server = express();
+
+
+server.use(cors())
+server.use(express.json())
+
+
+server.get('/users',(req,res)=>{
+    userHelper.get()
+        .then(response => res.status(200).json(response))
+        .catch(error=>{console.log("An error occurred fetching user data ", error)});
+})
+server.get('/users/:id',(req,res)=>{
+    const { id } = req.params;
+    userHelper.get(id)
+        .then(response => {
+            
+            if(response){ 
+                res.status(200).json(response)
+            } else {
+                res.status(404).json({errorMessage:"Unable to find the user by id ", id})
+            }
+        })
+        .catch(error=>{console.log("An error occurred fetching user data ", error)});
+})
+
 
 module.exports = server;
