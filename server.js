9b90a1e06e577b0f15abda8e3007e4fd265a1b30
Rahul Desai <rd825@georgetown.edu>
2018-11-07 15:57:43 -0500

    Got DELETE working for users

--- server.js
+++ server.js
@@ -6,9 +6,7 @@ const userDb = require('./data/helpers/userDb');
 const server = express();
 server.use(morgan('dev'));
 
-server.get('/', (req,res) => {
-    res.send({API: "live"});
-})
+server.get('/', (req,res) => res.send({API: "live", "Users": "live", "Posts": "not live"}));
 
 server.get('/api/users', (req, res) => {
     userDb.get().then(users => {
@@ -32,13 +30,29 @@ server.get('/api/users/:id', (req,res) => {
         })
 });
 
-server.post('/api/users/', (req, res) => {
-    userDb.insert(req.body).then(user => {
-        res.status(201).json(post);
+server.post('/api/users/', async (req, res) => {
+    try {
+        const user = await userDb.insert(req.body);
+        db.get(user.id).then(user => res.status(201).json(user)).catch(err => res.status(404).json({message: "The user with the specified ID does not exist."}));
+    }
+    catch (err) {
+        res.status(500).json({error: "There was an error while saving the user to the database", err})
+    }
+})
+
+server.delete('/api/users/:id', (req,res) => {
+    userDb.remove(req.params.id).then(count => {
+        if (count) {
+            res.status(200).json(count);
+        }
+        else {
+            res.status(404).json({message: "The user with the specified ID does not exist."});
+        }
     }).catch(err => {
-        res.status(500).json({error: "There was an error while saving the user to the database"});
+        res.status(500).json({error: "The user could not be removed"});
     })
-});
+})
+
 
 
 server.listen(9000, () => console.log('Server running on port 9000'));
