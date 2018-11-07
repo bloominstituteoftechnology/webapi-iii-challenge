9b643d70dd12f7d40f8da913267fb2e125cecc7b
Hargobind Atwal <ethaipath@gmail.com>
2018-11-07 14:53:01 -0800

    added more middleware

--- api/server.js
+++ api/server.js
@@ -13,7 +13,7 @@ server.use(helmet());
 
 // endpoints
 
-const sendAllUsers = (req, res) => {
+const getAllUsers = (req, res) => {
   db.get()
     .then(data => {
       res.status(200).json(data);
@@ -22,6 +22,32 @@ const sendAllUsers = (req, res) => {
       res.status(500).json({ message: `Internal server error. Could not get users`, error });
     });
 }
+
+const getUser = (req, res) => {
+  db.get(req.params.id)
+    .then(data => {
+      res.status(200).json(data);
+    })
+    .catch(error => {
+      res.status(500).json({ message: `Internal server error. Could not get users`, error });
+    });
+}
+
+const addUser = (req, res) => {
+  db.insert({ name: req.body.name })
+    .then(id => {
+      res.status(200).json(id);
+    })
+    .catch(error => {
+      res.status(500).json({ message: `Internal server error. Could not add user`, error });
+    });
+}
+
+const deleteUser = (req, res) => {
+
+
+}
+
 
 const echo = (req, res) => {
   res.status(201).json({
@@ -33,9 +59,9 @@ const echo = (req, res) => {
 }
 
 // bind user endpoints
-server.get('/api/users', sendAllUsers);
-server.get('/api/users/:id', echo);
-server.post('/api/users', echo);
+server.get('/api/users', getAllUsers);
+server.get('/api/users/:id', getUser);
+server.post('/api/users', addUser);
 server.delete('/api/users/:id', echo);
 server.put('/api/users/:id', echo);
 
