96e06f840bc11083a18cdad70aa706f39a724a86
Carlos Lantigua <clantigua954@gmail.com>
2018-11-07 14:38:06 -0500

    added update and delete functions in server file, need to test

---
+++ api/server.js
@@ -0,0 +1,72 @@
+const express = require('express');
+
+const cors = require('cors');
+const postDB = require('../data/helpers/postDb');
+const userDB = require('../data/helpers/userDb');
+const tagDB = require('../data/helpers/tagDb');
+
+const server = express();
+server.use(express.json());
+server.use(cors());
+
+server.get('/', (req, res) => {
+	res.json('alive');
+});
+
+// get all posts
+server.get('/api/posts/', (req, res) => {
+	postDB
+		.get()
+		.then((post) => {
+			res.status(200).json(post);
+		})
+		.catch((err) => err.status(500).json({ message: "Can't retrieve post data!!" }));
+});
+
+// get post by id
+server.get('/api/posts/:id', (req, res) => {
+	const { id } = req.params;
+	postDB.get(id).then((post) => {
+		post.length <= 0 ? res.status(404).json({ message: 'That post was not found' }) : res.status(200).json(post);
+	});
+});
+
+// create a post
+server.post('/api/posts/', (req, res) => {
+	postDB
+		.insert(req.body)
+		.then((postData) => {
+			res.status(201).json(postData);
+		})
+		.catch((err) => {
+			res.status(500).json({ message: 'error creating post', err });
+		});
+});
+
+// delete a post
+server.delete('/api/posts/:id', (req, res) => {
+	postDB.remove(req.params.id).then((count) => {
+		count
+			? res.status(200).json({ message: 'Post deleted successfully' })
+			: res.status(404).json({ message: 'That post was not found or already deleted' });
+	});
+});
+
+// update a post
+
+server.put('/api/posts/:id', (req, res) => {
+	const { id } = req.perams;
+	const changes = req.body;
+	postDB
+		.update(id, changes)
+		.then((count) => {
+			count
+				? res.status(200).json({ message: 'Post updated successfully' })
+				: res.status(404).json({ message: 'That post was not found' });
+		})
+		.catch((err) => {
+			res.status(500).json({ message: 'error updating post', err });
+		});
+});
+
+module.exports = server;
