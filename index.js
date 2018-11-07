bca804bd59d3bb0d0349906a0b9cca2b8fb19005
koennecker <koenneckerb@gmail.com>
2018-11-07 15:54:39 -0700

    Added GET for posts and individual posts.

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const logger = require("morgan");
 const cors = require("cors");
 const helmet = require("helmet");
 const userDb = require("./data/helpers/userDb");
+const postDb = require("./data/helpers/postDb");
+const tagDb = require("./data/helpers/tagDb");
 
 const server = express();
 server.use(express.json());
@@ -21,6 +23,8 @@ const upperCase = (req, res, next) => {
   req.body.name = req.body.name.toUpperCase();
   next();
 };
+
+// users
 
 server.get("/api/users", (req, res) => {
   userDb
@@ -145,6 +149,45 @@ server.delete("/api/users/:id", (req, res) => {
       res.status(404).json({ error: `User id ${id} not found` });
     }
   });
+});
+
+// posts
+
+server.get("/api/posts", (req, res) => {
+  postDb
+    .get()
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(err => {
+      sendUserError(
+        500,
+        "The posts' information could not be retrieved.",
+        res,
+        err
+      );
+    });
+});
+
+server.get("/api/posts/:id", (req, res) => {
+  const { id } = req.params;
+  postDb
+    .get(id)
+    .then(post => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res.status(404).json({ error: `Post id ${id} not found` });
+      }
+    })
+    .catch(err => {
+      sendUserError(
+        500,
+        "The user's information could not be retrieved.",
+        res,
+        err
+      );
+    });
 });
 
 server.listen(port, () => {
