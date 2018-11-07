0a9fc28804d88aeba620555f39597bcfd1d5aaa9
Yusuf Nafey <yusufnafey@yahoo.com>
2018-11-07 15:23:40 -0800

    initialized

---
+++ index.js
@@ -0,0 +1,3 @@
+const server = require('/api/server.js')
+
+server.listen(8000, () => console.log('server is ALIVE'))
---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const server = express()
+
+server.use(express.json())
+
+server.get('/', (req, res) => {
+    res
+        .status(200)
+        .json({ api: ''})
+})
+
+module.exports = server
