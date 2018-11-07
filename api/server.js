083ec878f1dd323e0ef0cc72d9c0a104e5f6101c
adamhinckley <adamhinckley@mac.com>
2018-11-07 14:17:25 -0700

    get(id) working

--- api/server.js
+++ api/server.js
@@ -3,6 +3,8 @@ const helmet = require("helmet");
 const morgan = require("morgan");
 const cors = require("cors");
 const postDB = require("../data/helpers/postDb");
+const tagDb = require("../data/helpers/tagDb");
+const userDB = require("../data/helpers/userDb");
 
 const server = express();
 
@@ -18,6 +20,23 @@ server.get("/api/posts", (req, res) => {
     .get()
     .then(posts => {
       res.status(200).json(posts);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "the post info could not be received" });
+    });
+});
+
+server.get("/api/posts/:id", (req, res) => {
+  const { id } = req.params;
+  console.log("get with ID", req.params);
+  postDB
+    .get(id)
+    .then(post => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res.status(404).json({ message: "the post with the specified ID does not exist" });
+      }
     })
     .catch(err => {
       res.status(500).json({ message: "the post info could not be received" });
