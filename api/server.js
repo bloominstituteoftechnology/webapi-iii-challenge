64521ca43e3a620344e6a6fac820a68af136fbc1
Edward J <edjeong99@gmail.com>
2018-11-07 17:33:28 -0500

    implement get user

--- api/server.js
+++ api/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const userDb = require("../data/helpers/userDb.js");
+
 
 const server = express();
 
@@ -8,6 +10,13 @@ server.use(express.json()); // built in
 server.use(helmet()); // third party
 server.use(morgan('short')); // third party
 
-
+server.get('/', (req, res) => {
+    userDb.get()
+    .then( users =>
+        res.status(200).json(users))
+    .catch( (error) => 
+        res.status(500).json({ message: 'could not get users', error })
+    )
+  });
 
 module.exports = server;
