2f6ec50b1a54e508987d4186d60694cc97cccd3b
Daniel Ferrer <danfrrr13@gmail.com>
2018-11-07 15:45:24 -0500

    added get functionality for users and for specific user posts

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const db = require('./data/helpers/userDb.js');
+
+const server = express();
+
+server.use(express.json());
+
+server.get('/api/users', (req, res) => {
+    db.get()
+        .then(users => {
+            res.status(200).json(users);
+        })
+        .catch(err => {
+            res.status(500).json({ error: 'Could not retrieve users.', err });
+        })
+});
+
+server.get('/api/users/:id', (req, res) => {
+    db.getUserPosts(req.params.id)
+        .then(posts => {
+            res.status(200).json(posts);
+        })
+        .catch(err => {
+            res.status(500).json({ error: 'Error loading posts.', err });
+        })
+
+})
+
+server.listen(9000, () => console.log('The server is listening at port 9000'));
