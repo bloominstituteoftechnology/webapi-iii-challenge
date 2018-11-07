2f18a5a794a13c315f721794ac5d6d9e4c4d49ad
Yanrong Xu <xuyanrong@icloud.com>
2018-11-07 17:41:05 -0500

    make server listen on port 9000

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 const express = require('express');
 const db = require('./data/dbConfig.js')
+
+const app = express();
+
+app.use(express.json());
+
+
+
+
+
+const port = 9000;
+app.listen(port, () => {
+    console.log(`App listening on port ${port}!`);
+});
