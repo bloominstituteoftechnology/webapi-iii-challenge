1e85d70e61c7168105f984da6596c495f09a3032
KHollobaugh <khollobaugh@gmail.com>
2018-11-07 16:23:47 -0500

    server alive and running

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
-const server = require('./api/server.js');
+const express = require('express');
+const db = require('./data/dbConfig');
+// const cors = require('cors');
+const server = express();
+
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.json('alive');
+})
+
+
+
+server.get('/')
 
 const port = 9000;
 server.listen(port, () => console.log('API running'));
