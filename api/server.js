cf1c318fa426d6676a34e87b42cb49486939bba7
ssmodish <smodish@hotmail.com>
2018-11-07 18:24:45 -0500

    I guess I am on track now

--- api/server.js
+++ api/server.js
@@ -1,10 +1,47 @@
 const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const userDb = require('../data/helpers/userDb');
+const postDb = require('../data/helpers/postDb');
+const tagDb = require('../data/helpers/tagDb');
 
 const server = express();
 
+
+
 // configure middleware
-server.get('/', (req, res) => {
-  res.status(200).json({ api: 'running' });
+server.use(express.json());
+server.use(helmet());
+server.use(morgan('short'));
+
+
+
+// configure endpoints
+// server.get('/', (req, res) => {
+//   res.status(200).json({ api: 'running' });
+// });
+
+// get /users
+server.get('/users', (req, res) => {
+  userDb.get()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => { 
+      res
+      .status(500)
+      .json(err);
+    });
 });
 
-module.exports = server;
+// get /users/:id
+
+// get /users/:id/posts
+
+// get /users/:userId/posts/:postId
+
+
+
+
+
+module.exports = server
