71e7882912a70ef0f36a1b5eb3cec2d35741c88b
Chad Jemmett <chad.jemmett@gmail.com>
2018-11-07 14:56:59 -0700

    can do crud operations for posts

--- api/server.js
+++ api/server.js
@@ -13,7 +13,58 @@ server.get('/posts', (req, res) => {
     .then(posts => {
       res.status(200).json(posts)
     })
-    .catch(error => console.log(error))
+   .catch(error => {
+     res.status(500).json({message: 'there was a problem getting posts'})
+   })
+})
+
+server.get('/users', (req, res) => {
+   userDb.get()
+    .then(users => {
+      res.status(200).json(users)
+    })
+   .catch(error => {
+     res.status(500).json({message: 'there was a problem getting users'})
+   })
+})
+
+server.post('/posts', (req, res) => {
+  const postData = req.body;
+  if(postData.text.length === 0) {
+    res.status(500).json({message: 'no empty strings'})
+  } else {
+    postDb.insert(postData).then(newPost => {
+      postDb.get(newPost.id).then(post => {
+        res.status(201).json(post);
+      })
+    })
+     .catch(error => {
+       res.status(500).json({message: 'there was a problem creating post'})
+   })
+  }
+
+
+})
+
+server.delete('/posts/:id', (req, res) => {
+    postDb.remove(req.params.id).then(count => {
+      if(count){
+        res.status(200).json({message: `${count} post deleted`})
+      } else {
+        res.status(404).json({message: `post not found`})
+      }
+    })
+     .catch(error => {
+       res.status(500).json({message: 'there was a problem deleting post'})
+   })
+})
+
+server.put('/posts/:id', (req, res) => {
+  const {id} = req.params;
+  const changes = req.body;
+  postDb.update(id, changes).then(count => {
+      res.status(200).json({message: `${count} post updated`})
+  })
 })
 
 
