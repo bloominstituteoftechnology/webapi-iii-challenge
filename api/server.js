f0e673e369a31fb0adfd3a37d35170058e6494d3
sean-one <basement365@gmail.com>
2018-11-07 15:25:39 -0800

    able to make a get request for a single post

--- api/server.js
+++ api/server.js
@@ -10,6 +10,21 @@ server.get('/api/users', (req, res) => {
         .then(posts => {
             res.json({ posts })
         })
+        .catch(err => {
+            res.status(500).json({ error: "The information could not be retrieved."})
+        })
+});
+
+server.get('/api/users/:id', (req, res) => {
+    const id = req.params.id;
+    db.get(id)
+        .then(post => {
+            res.json({ post })
+        })
+        .catch(err => {
+            res.status(500).json({ error: "The information could not be retrieved."})
+        })
 })
+
 
 module.exports = server;
