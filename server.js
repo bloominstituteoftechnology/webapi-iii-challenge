b1a86a2f6194875a7d099ef131ff3894615f5d61
BrandonVilaychith <blvilaychith@gmail.com>
2018-11-07 15:42:13 -0600

    DELETE finished

--- server.js
+++ server.js
@@ -58,5 +58,25 @@ server.post('/api/users', (req, res) => {
     );
 });
 
+server.delete('/api/users/:id', (req, res) => {
+  const id = req.params.id;
+  userDb
+    .remove(id)
+    .then(count => {
+      if (count) {
+        res
+          .status(200)
+          .json({ message: `User with the id of ${id} was deleted.` });
+      } else {
+        res.status(404).json({
+          message: 'The user you wish to delete can not be found.'
+        });
+      }
+    })
+    .catch(error =>
+      res.status(500).json({ message: 'There was an error deleting the user.' })
+    );
+});
+
 // Server export
 module.exports = server;
