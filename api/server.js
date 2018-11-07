eae7b178311eddd685bd397d3101ddea1c47ed14
Shawn Antonucci <shawnantonucci2018@gmail.com>
2018-11-07 13:44:39 -0800

    implimented post by post id and post by user

--- api/server.js
+++ api/server.js
@@ -1,23 +1,27 @@
 const express = require('express');
-const userDB = require('../data/helpers/userDb.js');
-// const db = require("../data/dbConfig.js");
+const userDb = require('../data/helpers/userDb.js');
+const postDb = require('../data/helpers/postDb.js');
+
+const gateKeeper = require('../gateKeeper/gateKeeperMiddleWare.js');
 
 const server = express();
 
 server.use(express.json());
 
+// all users
 server.get('/api/users', (req, res) => {
-    userDB.get().then(user => {
+    userDb.get().then(user => {
         res.status(200).json(user);
     }).catch(error => {
         res.status(500).json({ error: "The user information could not be retrieved." });
     });
 });
 
+// user by id
 server.get("/api/users/:id", (req, res) => {
     const { id } = req.params;
 
-    userDB.get(id).then(user => {
+    userDb.get(id).then(user => {
         if (user.length !== 0) {
             res.status(200).json(user);
         } else {
@@ -25,6 +29,46 @@ server.get("/api/users/:id", (req, res) => {
         }
     }).catch(error => {
         res.status(500).json({ error: "The user information could not be retrieved." });
+    });
+});
+
+// all posts
+server.get('/api/posts', (req, res) => {
+    postDb.get().then(post => {
+        res.status(200).json(post);
+    }).catch(error => {
+        res.status(500).json({ error: "The user information could not be retrieved." });
+    });
+});
+
+// posts by user id
+server.get("/api/posts/:id", gateKeeper, (req, res) => {
+    const { id } = req.params;
+    const userId = id;
+
+    userDb.getUserPosts(userId).then(post => {
+        if (post.length !== 0) {
+            res.status(200).json(post);
+        } else {
+            res.status(404).json({ message: "The post with the specified ID does not exist." });
+        }
+    }).catch(error => {
+        res.status(500).json({ error: "The post information could not be retrieved." });
+    });
+});
+
+// posts by post id
+server.get("/api/post/:id", (req, res) => {
+    const { id } = req.params;
+
+    postDb.get(id).then(post => {
+        if (post.length !== 0) {
+            res.status(200).json(post);
+        } else {
+            res.status(404).json({ message: "The post with the specified ID does not exist." });
+        }
+    }).catch(error => {
+        res.status(500).json({ error: "The post information could not be retrieved." });
     });
 });
 
