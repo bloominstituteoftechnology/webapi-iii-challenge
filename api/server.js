3346b6a851d619b8eed34dd06aa3e7b1f1d23fca
sean-one <basement365@gmail.com>
2018-11-07 14:54:05 -0800

    server up and running

--- api/server.js
+++ api/server.js
@@ -1,5 +1,15 @@
+//https://github.com/LambdaSchool/Node-Blog/pull/360
+
 const express = require('express');
+const db = require('../data/helpers/postDb.js');
 
 const server = express();
+
+server.get('/api/users', (req, res) => {
+    db.get()
+        .then(posts => {
+            res.json({ posts })
+        })
+})
 
 module.exports = server;
