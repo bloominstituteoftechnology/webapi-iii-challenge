4ba36a026df861a6f59059eeacec124d8855634b
frankbenevento <frankbenevento123@gmail.com>
2018-11-07 15:08:39 -0800

    basic crud ep for the users

--- api/server.js
+++ api/server.js
@@ -23,10 +23,46 @@ server.get('/users/:id',(req,res)=>{
             if(response){ 
                 res.status(200).json(response)
             } else {
-                res.status(404).json({errorMessage:"Unable to find the user by id ", id})
+                res.status(404).send({errorMessage:"Unable to find the user by id ", id})
             }
         })
-        .catch(error=>{console.log("An error occurred fetching user data ", error)});
+        .catch(error=>{console.log("An error occurred fetching user data by id ", error)});
+})
+server.delete('/users/:id',(req,res)=>{
+    const { id } = req.params;
+    userHelper.remove(id)
+        .then(response =>{
+            if(response > 0){
+                res.status(200).json({message:"Success: data has been deleted for", id})
+            } else {
+                res.status(404).send({message:"No data found for deletion", id})
+            }
+        })
+        .catch(error=>{console.log("An error occurred deleting user data ", error)});
+})
+server.post('/users',(req,res)=>{
+    if(req.body.name && req.body.name.length < 129){
+        userHelper.insert(req.body)
+            .then(response=>{
+                res.status(201).json(response)
+            })
+            .catch(error=>{console.log("An error occurred creating user data ", error)});
+    } else  {
+        res.status(400).send({message:"Name is required and cannot exceed 128chars "})
+    }
+})
+server.put('/users/:id',(req,res)=>{
+    const { id } = req.params;
+
+    if(req.body.name && req.body.name.length < 129){
+        userHelper.update(id,req.body)
+        .then(response=>{
+            res.status(200).json(response)
+        })
+        .catch(error=>{console.log("An error occurred updating user data ", error)});
+    } else  {
+        res.status(400).send({message:"Name is required and cannot exceed 128chars "})
+    }    
 })
 
 
