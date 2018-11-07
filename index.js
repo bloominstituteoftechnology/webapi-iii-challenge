7d9e29df35d6680b967330b0da1b5dadd9f749ce
Brandon Strellis <bstrellis@gmail.com>
2018-11-07 17:37:04 -0500

    Added Get and Get by id functionality for users.

--- index.js
+++ index.js
@@ -11,7 +11,27 @@ server.get('/', (req, res) => {
    console.log('something');
 });
 
+server.get('/users', (req, res) => {
+    dbUser.get()
+        .then(users => {
+            res.status(200).json(users);
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'The users cannot be retrieved.'});
+        });
+});
 
+server.get('/users/:id', async (req, res) => {
+    try {
+        console.log('req.params', req.params);
+        const { id } = req.params;
+        const user = await dbUser.get(id);
+        res.status(200).json(user);
+    } catch(err) {
+        res.status(500).json({ message: error });
+    } 
+
+});
 
 const port = 9000;
 
