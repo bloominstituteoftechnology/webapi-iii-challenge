f6535ce45dcd386331cbcfcbe952debf4ca8bdf3
Brian G <brian.gregorius@gmail.com>
2018-11-07 17:22:40 -0500

    completed CRUD functionality

--- server/server.js
+++ server/server.js
@@ -10,7 +10,6 @@ const sendErrorMsg = (errCode, msg, res) => {
     res.json({ Error: msg });
 }
 
-
 //get users
 server
     .get('/', (req, res) => {
@@ -21,7 +20,7 @@ server.get('/api/users', (req, res) => {
     users
         .get()
         .then(user => {
-            res.json(user);
+            res.status(200).json(user);
         })
         .catch(err => {
             return sendErrorMsg(500, 'Users information could not be retrieded', res)
@@ -46,5 +45,60 @@ server
             });
     });
 
+// new user
+server
+    .post('/api/users', (req, res) => {
+        const { name } = req.body;
+
+        users
+            .insert({ name })
+            .then(response => {
+                res.status(200).json({ name: `${req.body.name}` })
+            })
+            .catch(err => {
+                return sendErrorMsg(500, 'Name is not added to DB'.res)
+            });
+
+    });
+
+//delete user
+server
+    .delete('/api/users/:id', (req, res) => {
+        const { id } = req.params;
+
+        users
+            .remove(id)
+            .then(user => {
+                if (user[0]) {
+                    return sendErrorMsg(404, `The name with ${id} does not exist`)
+                } else {
+                    res.status(200).json({ message: `User has been removed` })
+                }
+            })
+            .catch(err => {
+                return sendErrorMsg(500, 'The user could not be removed', res);
+            });
+    });
+
+//update user
+
+server
+    .put('/api/users/:id', (req, res) => {
+        const { id } = req.params;
+        const changes = req.body;
+
+        users
+            .update(id, changes)
+            .then(userId => {
+                if (!req.body.name) {
+                    return sendErrorMsg(400, 'Please provide a name to be posted')
+                } else {
+                    res.status(200).json(req.body)
+                }
+            })
+            .catch(err => {
+                return sendErrorMsg(500, 'The name could not be modified');
+            });
+    });
 
 module.exports = server;
