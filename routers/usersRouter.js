c7314bae62b404410c7bdebf8195ca17d7e652fb
sarahtennis <sarahtennis@Sarahs-Air.fios-router.home>
2018-11-08 17:21:12 -0500

    PUT user name by user id

--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -45,46 +45,81 @@ router.get('/:id/posts', (req, res) => {
 // name is empty or greater than 128 characters --> error
 // after user inserted, cannot find by user id --> error
 router.post('/create-new', (req, res) => {
-    if (req.body.name && (req.body.name.split().length > 128)) {
+    if (req.body.name && (req.body.name.length < 128)) {
         userDb.insert(req.body)
             .then(id => {
                 return userDb.get(id.id);
             })
             .then(user => {
                 if (user) {
-                    res.status(200).json(user);
+                    res.status(201).json(user);
                 } else {
-                    res.status(404).json({ error: `User not found, error in user creation.` })
+                    res.status(404).json({ error: `Error retrieving new user.` })
                 }
             })
             .catch(err => {
                 res.status(500).json({ error: 'Error in user creation.' })
             })
     } else {
-        res.status(400).json({ error: 'Name must be between 1 and 128 characters long.'})
+        res.status(400).json({ error: 'Name must be between 1 and 128 characters long.' })
     }
 });
 
-// DELETE user by user id
+// ***** DELETE user by user id *****
 // user at id does not exist --> error
 // user successfully deleted --> respond success message
-router.delete('/:id/delete', (req, res) => {
+router.delete('/:id', (req, res) => {
     userDb.get(req.params.id)
         .then(user => {
-            if(user) {
+            if (user) {
                 return userDb.remove(req.params.id);
             } else {
-                res.status(404).json({ error: `The user with id ${req.params.id} does not exist.`})
+                res.status(404).json({ error: `The user with id ${req.params.id} does not exist.` })
             }
         })
         .then(recordsDeleted => {
-            if(recordsDeleted){
-                res.status(200).json({ message: `User successfully deleted.`})
+            if (recordsDeleted) {
+                res.status(200).json({ message: `User successfully deleted.` })
+            } else {
+                res.status(500).json({ error: `Failed to delete the user with id ${req.params.id}` })
             }
         })
         .catch(err => {
-            res.status(500).json({ error: `Failed to delete the user with id ${req.params.id}.`})
+            res.status(500).json({ error: `Failed to delete the user with id ${req.params.id}.` })
         });
 })
+
+// ***** PUT update user by user id *****
+router.put('/:id', (req, res) => {
+    if (req.body.name) {
+        userDb.get(req.params.id)
+            .then(user => {
+                if (user) {
+                    return userDb.update(req.params.id, req.body);
+                } else {
+                    return res.status(404).json({ error: `The user with id ${req.params.id} does not exist.` });
+                }
+            })
+            .then(recordsUpdated => {
+                if (recordsUpdated) {
+                    return userDb.get(req.params.id);
+                } else {
+                    return res.status(500).json({ error: `Failed to update the user with id ${req.params.id}.` });
+                }
+            })
+            .then(user => {
+                if (user) {
+                    res.status(200).json(user);
+                } else {
+                    res.status(404).json({ error: `The user with id ${req.params.id} does not exist.` });
+                }
+            })
+            .catch(err => {
+                res.status(500).json({ error: `Failed to update the user with id ${req.params.id}.` });
+            })
+    } else {
+        res.status(400).json({ error: 'Name must be between 1 and 128 characters long.' });
+    }
+});
 
 module.exports = router;
