e7c8e8fd92a1c69d4fa89e33ac79d0e10391ea97
David247365 <ds051128@gmail.com>
2018-11-07 19:12:19 -0600

    working on middleware

--- server.js
+++ server.js
@@ -7,8 +7,18 @@ const port = 9000;
 const server = express();
 server.use(express.json());
 
-server.get("/", (req, res) => {
-  console.log("Up and ready");
+server.get("/api/users", (req, res) => {
+  users
+    .get()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: "The users information can not be retrieved",
+        error: err
+      });
+    });
 });
 
 server.listen(port, () => console.log(`\nServer listening on port ${port}`));
