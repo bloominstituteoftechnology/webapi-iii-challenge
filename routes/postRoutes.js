02c3c7ed4a723a806de2b85d9072aa418f3c0c25
samsisle <samko9522@gmail.com>
2018-11-07 23:05:45 -0800

    Add GET single post request handler

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -18,4 +18,13 @@ router.get('/', async (_, res) => {
   }
 });
 
+router.get('/:id', async (req, res) => {
+  try {
+    const post = await postDb.get(req.params.id);
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(500).json({ error: 'The post could not be retrieved from the database.' });
+  }
+});
+
 module.exports = router;
