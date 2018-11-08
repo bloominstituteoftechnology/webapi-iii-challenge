f7f287b918c5ccdc0e354b1d624dce36580b8a43
pedro <pedroluismontesinosrivera@gmail.com>
2018-11-07 21:54:27 -0500

    retrieving every post in the database

--- data/api/server.js
+++ data/api/server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const helmet = require('helmet');
 const morgan = require('morgan');
 
+const postDb = require('../helpers/postDb');
+const userDb = require('../helpers/userDb');
+
 const server = express();
 
 server.use(express.json());
@@ -10,6 +13,17 @@ server.use(morgan('short'));
 
 server.get('/', (req, res) => {
     res.status(200).json({ api: 'running' });
+});
+
+server.get('/api/posts', (req, res) => {
+    postDb
+        .get()
+        .then(post => {
+            res.status(200).json(post);
+        })
+        .catch(error => {
+            res.status(500).json({error: 'cannot be retrieved', 'error': error})
+        });
 });
 
 module.exports = server;
