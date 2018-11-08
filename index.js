7c427ce9c0cd605a62ce5223afeb5ede2c5c6c76
pedro <pedroluismontesinosrivera@gmail.com>
2018-11-08 18:49:44 -0500

    made port dynamic

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const server = require('./data/api/server');
 
-const port = 9000;
+const port = process.env.PORT || 9000;
 server.listen(port, () => console.log(`\nAPI running on port ${port}\n`));
