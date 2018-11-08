0e39050543719b6b3c3fd9ce2f907c3838335440
JacobABrennan <jacobabrennan@gmail.com>
2018-11-08 18:03:59 -0500

    move reset.css to utilities

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
-import './reset.css';
+import './utilities/reset.css';
 import './client.css';
 import {UserList, UserViewer} from './user.js';
 
