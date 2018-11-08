b263c588db52852b325c789510b477bcc134570b
ssmodish <smodish@hotmail.com>
2018-11-08 18:19:50 -0500

    MVP finally!

--- api/server.js
+++ api/server.js
@@ -17,11 +17,16 @@ server.use(morgan('short'));
 
 // add custom middleware
 // function toUpperCaseMiddleware
+function toUpperCaseMiddleware(req, res, next){
+  req.body.name = req.body.name.toUpperCase();
+
+  next();
+}
 
 
 // configure endpoints
 
-// get /users
+// GET /users
 server.get('/users', (req, res) => {
   userDb.get()
     .then(users => {
@@ -34,7 +39,7 @@ server.get('/users', (req, res) => {
     });
 });
 
-// get /users/:id
+// GET /users/:id
 server.get('/users/:userId', (req, res) => {
   const { userId } = req.params;
 
@@ -49,7 +54,7 @@ server.get('/users/:userId', (req, res) => {
     })
 })
 
-// get /users/:id/posts
+// GET /users/:id/posts
 server.get('/users/:userId/posts', (req, res) => {
   const { userId } = req.params;
 
@@ -64,7 +69,7 @@ server.get('/users/:userId/posts', (req, res) => {
     })
 })
 
-// get /users/:userId/posts/:postId
+// GET /posts/:postId
 server.get('/posts/:postId', (req, res) => {
   const { postId } = req.params;
 
@@ -80,7 +85,7 @@ server.get('/posts/:postId', (req, res) => {
 })
 
 
-// get /posts
+// GET /posts
 server.get('/posts', (req, res) => {
   postDb.get()
     .then(posts => {
@@ -90,6 +95,103 @@ server.get('/posts', (req, res) => {
       res
         .status(500)
         .json(err);
+    });
+});
+
+
+// POST /users
+server.post('/users', toUpperCaseMiddleware, (req, res) => {
+  const { name } = req.body;
+  const newUser = { name };
+
+  if(!newUser){
+    res
+      .status(400)
+      .json({ errorMessage: "Ya gotta give me a name here" });
+  } else {
+    userDb.insert(newUser)
+      .then(newUserRes => {
+        res.status(201).json({ "post": "New User Added!" });
+      })
+      .catch(err => {
+        res.send(err);
+      });
+  };
+});
+
+
+// POST /posts
+server.post('/posts', toUpperCaseMiddleware, (req, res) => {
+  const { text, userId } = req.body;
+  const newPost = { text, userId };
+
+  if(!newPost.text || !newPost.userId){
+    res
+      .status(400)
+      .json({ errorMessage: "Ya gotta give me a message and who said it." });
+  } else {
+    postDb.insert(newPost)
+      .then(newlyPosted => {
+        res.status(201).json({ "post": "New Post Added!" });
+      })
+      .catch(err => {
+        res.send(err);
+      });
+  }
+})
+
+
+// DELETE /users
+server.delete("/users/:id", (req, res) => {
+  userDb.remove(req.params.id)
+    .then(count => {
+      res.status(201).json(count)
+    })
+    .catch(err => {
+      res.status(500).json({ error: err })
+    })
+})
+
+
+// DELETE /posts
+server.delete("/posts/:id", (req, res) => {
+  postDb.remove(req.params.id)
+    .then(count => {
+      res.status(201).json(count)
+    })
+    .catch(err => {
+      res.status(500).json({ error: err })
+    })
+})
+
+
+
+// PUT /users
+server.put('/users/:id', toUpperCaseMiddleware, (req, res) => {
+  const { id } = req.params;
+  const updatedUser = req.body;
+
+  userDb.update(id, updatedUser)
+    .then(updateUser => {
+      res.status(201).json({ "User Updated": updateUser });
+    })
+    .catch(err => {
+      res.send(err);
+    });
+});
+
+
+// PUT /posts
+server.put('/posts/:id', toUpperCaseMiddleware, (req, res) => {
+  const { id } = req.params;
+  const updatedPost = req.body;
+
+  postDb.update(id, updatedPost)
+    .then(updatePost => {
+      res.status(201).json({ "Post Updated": updatePost });
+    })
+    .catch(err => {
+      res.send(err);
     });
 });
 
