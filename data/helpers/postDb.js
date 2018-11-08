d237f10735a92f12dce0b25a1c63dcd18ef13f2f
ryanboris <ryan.boris@gmail.com>
2018-11-07 22:57:38 -0500

    Adds ability to render a list of users

--- data/helpers/postDb.js
+++ data/helpers/postDb.js
@@ -1,3 +1,4 @@
+
 const db = require('../dbConfig.js');
 
 module.exports = {
@@ -5,15 +6,12 @@ module.exports = {
     let query = db('posts as p');
 
     if (id) {
-      query
-        .join('users as u', 'p.userId', 'u.id')
-        .select('p.text', 'u.name as postedBy')
-        .where('p.id', id);
+      query.join('users as u', 'p.userId', 'u.id').select('p.text', 'u.name as postedBy').where('p.id', id);
 
-      const promises = [query, this.getPostTags(id)]; // [ posts, tags ]
+      const promises = [ query, this.getPostTags(id) ]; // [ posts, tags ]
 
       return Promise.all(promises).then(function(results) {
-        let [posts, tags] = results;
+        let [ posts, tags ] = results;
         let post = posts[0];
         post.tags = tags.map(t => t.tag);
 
@@ -24,24 +22,15 @@ module.exports = {
     return query;
   },
   getPostTags: function(postId) {
-    return db('tags as t')
-      .join('posttags as pt', 't.id', 'pt.tagId')
-      .select('t.tag')
-      .where('pt.postId', postId);
+    return db('tags as t').join('posttags as pt', 't.id', 'pt.tagId').select('t.tag').where('pt.postId', postId);
   },
   insert: function(post) {
-    return db('posts')
-      .insert(post)
-      .then(ids => ({ id: ids[0] }));
+    return db('posts').insert(post).then(ids => ({ id: ids[0] }));
   },
   update: function(id, post) {
-    return db('posts')
-      .where('id', id)
-      .update(post);
+    return db('posts').where('id', id).update(post);
   },
   remove: function(id) {
-    return db('posts')
-      .where('id', id)
-      .del();
-  },
+    return db('posts').where('id', id).del();
+  }
 };
--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 /* eslint-disable no-console */
 const express = require('express');
 const cors = require('cors');
+const postDb = require('./data/helpers/postDb');
+const userDb = require('./data/helpers/userDb');
 
 const server = express();
-
 server.use(express.json(), cors());
 
+server.get('/users', (req, res) => {
+  userDb
+    .get()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => {
+      console.log(err, res.status(500).json({ error: 'Error getting users from database' }));
+    });
+});
 
 server.listen(8000, () => {
   console.log('Running on port 8000');
