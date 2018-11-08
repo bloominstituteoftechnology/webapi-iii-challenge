30c81f441b5b763a1d6a3018270a735b05fc9057
TRIF3X <thewizardofcrypto@gmail.com>
2018-11-08 14:45:06 -0600

    created route for posts

---
+++ Post/postRoutes.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const postDb = require('../data/helpers/postDb.js');
+const router = express.Router();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -2,8 +2,10 @@ const express = require('express')
 const server = express();
 server.use(express.json());
 const userRoutes = require('../User/userRoute.js')
+const postRoutes = require('../Post/postRoutes.js')
 
 server.use('/', userRoutes)
+server.use('/posts', postRoutes)
 
 
 
