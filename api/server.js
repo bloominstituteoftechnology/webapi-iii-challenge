ca700bd7119abaf364542b4534efe1fa1411b712
Edward J <edjeong99@gmail.com>
2018-11-08 17:14:13 -0500

    implement CRUD for post

--- api/server.js
+++ api/server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const userRouter = require('../users/userRouter.js');
+const postRouter = require('../posts/postRouter.js');
+
+
 const configureMiddleware = require('../config/middleware.js');
 const server = express();
 
@@ -7,6 +10,7 @@ const server = express();
 configureMiddleware(server);
 
 server.use('/', userRouter);
+server.use('/post', postRouter);
 
 module.exports = server;
 
