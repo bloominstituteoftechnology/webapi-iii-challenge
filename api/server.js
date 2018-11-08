6a115aeffad0256a1566a0683fdac2c6844bb3f8
sean-one <basement365@gmail.com>
2018-11-08 08:51:58 -0800

    really messy middleware added

--- api/server.js
+++ api/server.js
@@ -8,10 +8,15 @@ const server = express();
 
 server.use(express.json());
 
-//custom middleware
-// function pascalcase(req, res, next) {
-//     console.log(req.body)
-// }
+// custom middleware
+const pascalcase = (req, res, next) => {
+    let name = req.body.name;
+    name = name.split('');
+    name[0] = name[0].toUpperCase();
+    name = name.join('')
+    req.body.name = name;
+    next();
+}
 
 //posts
 server.get('/api/posts', async (req, res) => {
