9abd9cf7afb1025a6194a784841c28bd515666ee
Katie Cleary <katiecleary@airmaster.lan>
2018-11-08 10:17:54 -0500

    added user post functionality

--- api/server.js
+++ api/server.js
@@ -18,7 +18,10 @@ name: up to 128 characters long, required.
 Create GET, POST, PUT, and DELETE functionality for users 
  
   */
- server.get('/api/02-users', (req, res) => {
+
+//----- GET -----
+
+ server.get('/api/users', (req, res) => {
     db.get() //calling find method from db.js file 
     .then(users=> { 
       res.status(200).json(users);
@@ -29,40 +32,27 @@ Create GET, POST, PUT, and DELETE functionality for users
         .json({ error: "The users information could not be retrieved." });
     });//if you 'catch' an error as defined by status 500 - let the client know
 });
-    
+
+//----- POST -----
+
+server.post('/api/users', async (req, res) => {
+    const userData = req.body;
+    if (!userData.name || userData.name==="" ) {
+        const errorMessage = "Please provide name for the user"; 
+        res.status(400).json({ errorMessage});
+        return
+    }   
+    try {
+        await db.insert(userData);
+    } catch (error) {
+            res.status(500).json({ error: "There was an error while saving the post to the database" });
+            return      
+    }
+    res.status(201).json({message: "user was added to database" });
+    return
+});
+
 /*
-//----- GET -----
-
-server.get('/api/02-users', (req, res) => {
-    db.find() //calling find method from db.js file 
-      .then(posts=> { 
-        res.status(200).json(posts);
-      }) // once you find them- communicated by 200 status code, display them
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: "The posts information could not be retrieved." });
-      });//if you 'catch' an error as defined by status 500 - let the client know
-  });
-
-//----- GET -----
-
-server.get('/api/posts/:id', (req, res) => {
-    const { id } = req.params; //pull the id off the request 
-    db.findById(id) //call findbyid method, passing in id from above
-      .then(post => { //then check for ...
-        if (post && post.length) { // status 200 - we found it!
-          res.status(200).json(post);
-        } else { // or oops - if we could retrieve it, we would but it's not here, status 404
-          res.status(404).json({ message: "The post with the specified ID does not exist." });
-        }
-      })
-      .catch(err => {
-        res //if data can't be retrieved ... 
-          .status(500)
-          .json({ error: "The post information could not be retrieved." });
-      });
-  });
 
 //----- POST -----
 
