7f46a049b02ea6dc93024d010af72171d7e2ae4b
Edwin Torres <edwin.torres.jr2@gmail.com>
2018-11-08 16:04:26 -0500

    put request

--- api/server.js
+++ api/server.js
@@ -2,11 +2,12 @@ const express = require("express");
 const helmet = require("helmet");
 const morgan = require("morgan");
 
-const userDB = require("./data/helpers/userDb.js");
+const userDB = require("../data/helpers/userDb.js");
 const server = express();
 
 server.use(helmet());
 server.use(morgan("dev"));
+server.use(express.json());
 
 server.get("/", (req, res) => {
   res.status(200).json({ api: "running" });
@@ -14,33 +15,41 @@ server.get("/", (req, res) => {
 
 server.get("/users", (req, res) => {
   userDB
-    .find()
+    .get()
     .then(users => {
+      console.log(users);
       res.status(200).json(users);
     })
     .catch(err => {
       res
         .status(500)
         .json({ message: "we failed you, can't get the users", error: err });
+      console.error(err);
     });
 });
 
-server.post("/api/users", async (req, res) => {
-  console.log("body", req.body);
-  try {
-    const userData = req.body;
-    const userId = await db.insert(userData);
-    const user = await db.findById(userId.id);
-    res.status(201).json(user);
-  } catch (error) {
-    let message = "error creating the user";
+// server.post("/users", async (req, res) => {
+//   console.log("body", req.body);
+//   try {
+//     res.status(201).json(await userDB.insert(req.body));
+//   } catch (error) {
+//     let message = "error creating the user";
 
-    if (error.errno === 19) {
-      message = "please provide both the name and the bio";
-    }
+//     res.status(500).json({ message, error });
+//   }
+// });
 
-    res.status(500).json({ message, error });
-  }
+server.post("/users", (req, res) => {
+  userDB
+    .insert(req.body)
+    .then(userId => {
+      console.log(userId);
+      res.status(200).json(userId);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+      console.error(err);
+    });
 });
 
 module.exports = server;
