e4f44569ee13f81c86dd26d6ed2a97a7744267e6
ryanboris <ryan.boris@gmail.com>
2018-11-08 02:30:36 -0500

    Adds ability to select user based on ID

--- server.js
+++ server.js
@@ -18,6 +18,18 @@ server.get('/users', (req, res) => {
     });
 });
 
+server.get('/users/:id', (req, res) => {
+  userDb
+    .get(req.params.id)
+    .then(user => {
+      !user ? res.status(404).json({ message: 'ID not found.' }) : res.status(200).json(user);
+    })
+    .catch(e => {
+      console.log(e);
+      res.status(500).json({ error: 'Error accessing database.' });
+    });
+});
+
 server.listen(8000, () => {
   console.log('Running on port 8000');
 });
