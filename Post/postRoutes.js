c0ae0a25f662a46440d21304879a8509968957be
TRIF3X <thewizardofcrypto@gmail.com>
2018-11-08 15:22:27 -0600

    get by post id complete

--- Post/postRoutes.js
+++ Post/postRoutes.js
@@ -15,13 +15,20 @@ router.get('/:id', (req, res) => {
         })
 })
 
-
-
-
-
-
-
-
+router.get('/tags/:id', (req, res) => {
+    const { id } = req.params
+    postDb.getPostTags(id)
+        .then(post => {
+            if(post.length === 0) {
+                res.status(404).json({ message: 'invalid tags id' })
+            } else {
+            res.status(200).json(post)
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'The posts tags by ID can not be found'})
+        })
+})
 
 
 
