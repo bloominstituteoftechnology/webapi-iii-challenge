6be7519d23c8f9b35ba67c51c5a70b1eb0fc0fe0
Riley-Brown <riley@riley.gg>
2018-11-08 18:25:46 -0500

    Added route for posts

--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
 /* ======= IMPORTS ====== */
 const express = require("express");
-const postDb = require("../data/helpers/postDb");
 const users = require("./users.js");
+const posts = require("./posts.js");
 
 /* ======= SERVER CONFIG ====== */
 const server = express();
@@ -9,10 +9,15 @@ const server = express();
 /* ======= MIDDLEWARE ====== */
 server.use(express.json());
 
+/* ======= ROOT ====== */
 server.get("/", (req, res) => {
   res.json({ Hello: "testing" });
 });
 
+/* ======= POSTS ROUTE ====== */
+server.use("/api/posts/", posts);
+
+/* ======= USERS ROUTE ====== */
 server.use("/api/users/", users);
 
 module.exports = server;
