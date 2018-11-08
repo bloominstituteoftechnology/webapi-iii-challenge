63a5b2ace206b6fa4dcd21f551b8434b18b64da1
Edward J <edjeong99@gmail.com>
2018-11-08 16:56:25 -0500

    a few edit

--- api/server.js
+++ api/server.js
@@ -1,17 +1,10 @@
 const express = require('express');
-const helmet = require('helmet');
-const morgan = require('morgan');
-const userDb = require("../data/helpers/userDb.js");
-const upperCase = require("../middleware/upperCase.js");
 const userRouter = require('../users/userRouter.js');
-
+const configureMiddleware = require('../config/middleware.js');
 const server = express();
 
-server.use(express.json()); // built in
-server.use(helmet()); // third party
-server.use(morgan('short')); // third party
-// server.use(upperCase);
 
+configureMiddleware(server);
 
 server.use('/', userRouter);
 
