3498c535e85e98b928382523c1e06e9a11fd8a31
sean-one <basement365@gmail.com>
2018-11-07 19:29:42 -0800

    add a new user works

--- api/server.js
+++ api/server.js
@@ -1,30 +1,80 @@
 //https://github.com/LambdaSchool/Node-Blog/pull/360
 
 const express = require('express');
-const db = require('../data/helpers/postDb.js');
+const dbPost = require('../data/helpers/postDb.js');
+const dbUser = require('../data/helpers/userDb.js');
 
 const server = express();
 
-server.get('/api/users', (req, res) => {
-    db.get()
+server.use(express.json());
+
+
+//posts
+server.get('/api/posts', (req, res) => {
+    dbPost.get()
         .then(posts => {
             res.json({ posts })
         })
         .catch(err => {
-            res.status(500).json({ error: "The information could not be retrieved."})
+            res.status(500).json({ error: "The posts could not be retrieved."})
         })
 });
 
-server.get('/api/users/:id', (req, res) => {
+server.get('/api/posts/:id', (req, res) => {
     const id = req.params.id;
-    db.get(id)
+    dbPost.get(id)
         .then(post => {
             res.json({ post })
         })
         .catch(err => {
-            res.status(500).json({ error: "The information could not be retrieved."})
+            res.status(500).json({ error: "The posts could not be retrieved." })
         })
 })
+
+
+
+//users
+server.get('/api/users', (req, res) => {
+    dbUser.get()
+    .then(user => {
+        res.json({ user })
+    })
+    .catch(err => {
+        res.status(500).json({ error: "The users could not be retrieved."})
+    })
+})
+
+server.get('/api/users/:id', (req, res) => {
+    const id = req.params.id;
+    dbUser.get(id)
+    .then(user => {
+        res.json({ user })
+    })
+    .catch(err => {
+        res.status(500).json({ error: "The information could not be retrieved."})
+    })
+});
+
+server.post('/api/users', async (req, res) => {
+    const name = req.body;
+    try {
+        const new_user_id = await dbUser.insert(name);
+        const user = await dbUser.get(new_user_id.id);
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json({ message: "Unable to add name."})
+    }
+});
+
+
+
+// server.post('/api/users', async (req, res) => {
+//     try {
+
+//     } catch (error) {
+//         res.status(500).json({ message: ''})
+//     }
+// })
 
 
 module.exports = server;
