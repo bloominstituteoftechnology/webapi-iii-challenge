c38839fa1d5c06377c82976c1a3b062f702acd73
kyranmccann <kyran.mccann@gmail.com>
2018-11-08 10:47:48 -0800

    updating port

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const server = require('./server');
 
-const port = 9000
+// const port = 9000;
+// server.listen(port, () => console.log(`\nIt Lives!\n`)); 
 
-server.listen(port, () => console.log(`\nIt Lives!\n`)); 
+const port = process.env.PORT || 9000;
+server.listen(port, () => console.log(`\nIt Lives!\n`));
