9f717cb3fc644775316939aec916c53150f657fa
Brandon Strellis <bstrellis@gmail.com>
2018-11-08 17:22:41 -0500

    Added Post to postsRoutes. Returns inserted id and all posts.

--- posts/postsRoutes.js
+++ posts/postsRoutes.js
@@ -15,4 +15,30 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.get('/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const post = await dbPost.get(id);
+        res.status(200).json(post);
+        // refactor this to check validity of id
+        // if (post.text === '') {
+        // } else {
+        //     res.status(404).json({ message: `Post ${id} has not been created yet.`});
+        // }
+    } catch(err) {
+        res.status(404).json({ message: err });
+    }
+});
+
+router.post('/', async (req, res) => {
+    try {
+        const postData = req.body;
+        const id = await dbPost.insert(postData);
+        const posts = await dbPost.get()
+        res.status(200).json({id, posts});
+    } catch(err) {
+        res.status(404).json({ message: err });
+    }
+});
+
 module.exports = router;
