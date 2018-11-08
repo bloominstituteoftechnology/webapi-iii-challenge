4360eaffcb9a91351d21d1504d4f4f8a49ae7b51
TRIF3X <thewizardofcrypto@gmail.com>
2018-11-08 15:17:20 -0600

    get post by id complete

--- Post/postRoutes.js
+++ Post/postRoutes.js
@@ -4,7 +4,16 @@ const router = express.Router();
 
 
 
-
+router.get('/:id', (req, res) => {
+    const { id } = req.params
+    postDb.get(id)
+        .then(post => {
+                res.status(200).json(post)
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'The post ID can not be found'})
+        })
+})
 
 
 
