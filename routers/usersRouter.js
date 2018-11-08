dbdae141c523a37bd497de1a16d7eca532aa99e5
sarahtennis <sarahtennis@Sarahs-Air.fios-router.home>
2018-11-08 16:23:54 -0500

    POST new user

--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -40,4 +40,29 @@ router.get('/:id/posts', (req, res) => {
         });
 });
 
+// ***** POST new user *****
+// after user created, find by id --> responds with new user object
+// name is empty or greater than 128 characters --> error
+// after user inserted, cannot find by user id --> error
+router.post('/create-new', (req, res) => {
+    if (req.body.name && (req.body.name.split().length > 128)) {
+        userDb.insert(req.body)
+            .then(id => {
+                return userDb.get(id.id);
+            })
+            .then(user => {
+                if (user) {
+                    res.status(200).json(user);
+                } else {
+                    res.status(404).json({ error: `User not found, error in user creation.` })
+                }
+            })
+            .catch(err => {
+                res.status(500).json({ error: 'Error in user creation.' })
+            })
+    } else {
+        res.status(400).json({ error: 'Name must be between 1 and 128 characters long.'})
+    }
+});
+
 module.exports = router;
