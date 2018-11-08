b354546a2931d40aaa668dac4e430c8c1e12241d
David247365 <ds051128@gmail.com>
2018-11-07 19:55:11 -0600

    created endpoint for individual users

--- server.js
+++ server.js
@@ -7,6 +7,15 @@ const port = 9000;
 const server = express();
 server.use(express.json());
 
+// Middleware
+
+const uppercaseMiddleware = (req, res, next) => {
+  req.body.name = req.body.name.toUpperCase();
+  next();
+};
+
+// Getting all users
+
 server.get("/api/users", (req, res) => {
   users
     .get()
@@ -15,9 +24,27 @@ server.get("/api/users", (req, res) => {
     })
     .catch(err => {
       res.status(500).json({
-        message: "The users information can not be retrieved",
+        message: "Users can not be retrieved",
         error: err
       });
+    });
+});
+
+// Getting Individual Users
+
+server.get("/api/users/:id", (req, res) => {
+  const { id } = req.params;
+  users
+    .get(id)
+    .then(user => {
+      if (!user) {
+        res.status(404).json({ message: "User does not exist" });
+      } else {
+        res.status(200).json(user);
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Could not retrieve user" });
     });
 });
 
