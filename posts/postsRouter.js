f62336811426ff30c4bf867799c5b5472b02be51
Stublink <chrisrey95@gmail.com>
2018-11-08 15:42:50 -0500

    :sparkles: posts CRUD endpoints and functionality

--- posts/postsRouter.js
+++ posts/postsRouter.js
@@ -13,4 +13,55 @@ router.get('/', async (req, res) => {
     }
 })
 
+
+router.get('/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const post = await postDb.get(id)
+        
+        res.status(200).json(post);
+    } catch(error) {
+        res.status(500).json(error);
+    }
+});
+
+router.post('/', async (req, res) => {
+    try {
+        const { body } = req;
+        const postId = await postDb.insert(body)
+        res.status(201).json(postId);
+    } catch(error) {
+        error.errno === 19 ?
+            res.status(406).json('post already exists')
+            :res.status(500).json(error);
+    }
+});
+
+router.delete('/delete/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const count = await postDb.remove(id);
+        
+        count
+            ? res.status(200).json({message: `${count} post's deleted`})
+            : res.status(404).json({message: 'post not found'})
+    } catch(error) {
+        res.status.apply(500).json(error);
+    }
+})
+
+router.put('/edit/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const count = await postDb.update(id, req.body);
+
+        count
+            ? res.status(200).json({message: `${count} post's edited`})
+            : res.status(404).json({message: 'post not found'})
+
+    } catch(error) {
+        res.status.apply(500).json(error);
+    }
+})
+
 module.exports = router;
