214448ff92fb2b733e596f3fcfe5d29f1893280c
Matthew Wright <matthewbwright@gmail.com>
2018-11-07 17:10:04 -0700

    attempts to hook up front and back ends

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
+import axios from 'axios';
+
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      users: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get('http://localhost:9000/api/users')
+      .then(response => {
+        console.log('response', response.data);
+        this.setState({ users: response.data });
+      })
+      .catch(err => console.log(err));
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header className="header">
+          <img />
+          {this.state.users.map(user => (
+            <div key={user.id}>
+              <h2 className="character">{user.name}</h2>
+            </div>
+          ))}
         </header>
       </div>
     );
