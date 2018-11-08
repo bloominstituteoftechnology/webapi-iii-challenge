fe8d706383f4301c5e7d266fe6750b607f1324df
DaftBeowulf <ajmboosh@gmail.com>
2018-11-07 22:06:17 -0600

    implemented UPDATE functionality

--- index.js
+++ index.js
@@ -36,7 +36,49 @@ server.get('/api/users/:id', (req, res) => {
         res.status(500)
         .json({message: "The user info could not be retrieved."})
     })
+})
 
+//POST
+server.post('/api/users', async (req, res) => {
+const {name} = req.body;
+if (!name) {
+    res.status(400)
+    .json({message: "Please provide a name for the new user."})
+} else {
+    try {
+        const userInfo = req.body;
+        const userId  =await userdb.insert(userInfo);
+        res.status(201).json(userId);
+    } catch (error) {
+        res.status(500).json({error: "An error occurred while saving this user."})
+    }
+}
+})
+
+//UPDATE
+server.put('/api/users/:id', (req, res) => {
+    const {id} = req.params;
+    const changes = req.body;
+
+    if (!changes.name) {
+        res.status(400)
+        .json({error: "Please provide the updated user's name."})
+    } else {
+        userdb.update(id, changes)
+        .then(count => {
+            if (count) {
+            res.status(200)
+            .json(count);
+            } else {
+                res.status(404)
+                .json({message: "The user with the specified ID does not exist."})
+            }
+        })
+        .catch(error => {
+            res.status(500)
+            .json({error: "The user info could not be modified."})
+        })
+    }
 })
 
 server.listen(7000, ()=>console.log('\nServer is listening on port 7000\n'));
