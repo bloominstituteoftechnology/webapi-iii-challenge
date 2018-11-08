dd42b3fde63b698f9103619746714e564b7a6952
Hunter315 <35641380+Hunter315@users.noreply.github.com>
2018-11-08 10:39:18 -0700

    using a router for users

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const userDb = require("./data/helpers/userDb.js");
 const postDb = require("./data/helpers/postDb.js");
 const server = express();
 
+const userRouter = require('./users/userRouter.js');
+
 // MIDDLEWARES
 
 const allCaps = (req, res, next) => {
@@ -22,108 +24,12 @@ const allCaps = (req, res, next) => {
 
 server.use(morgan("dev"), cors(), helmet(), express.json());
    
- 
 //=============== USER ENDPOINTS =============== //
+server.use('/api/users', userRouter);
 
-//Get all users
-server.get("/api/users", (req, res) => {
-    userDb
-      .get()
-      .then(users => {
-        res.json(users);
-      })
-      .catch(err =>
-        res.status(500).json({
-          error: "The users information could not be retrieved."
-        })
-      );
-  });
-  
-  //Get posts of a specific user
-  server.get("/api/users/:id/posts", (req, res) => {
-    userDb
-      .getUserPosts(req.params.id)
-      .then(user => {
-        if (user.length > 0) {
-          res.json(user);
-        } else
-          res.status(404).json({
-            message: "The user with the specified ID does not exist."
-          });
-      })
-      .catch(err =>
-        res
-          .status(500)
-          .json({ error: "The user information could not be retrieved." })
-      );
-  });
-  
-  //Add a new User
-  server.post("/api/users", allCaps, (req, res) => {
-    const newUser = req.body;
-    if (newUser.name.length > 128) {
-        return res.status(411).json({
-         user, message: " The user name must be under 129 characters."
-        });
-      }
-    userDb
-      .insert(newUser)
-      .then(user => {
-        res.status(201).json({user, message: "The user was added successfully"});
-      })
-      .catch(err => {
-        res.status(500).json({
-          error: "There was an error while saving the user to the database."
-        });
-      });
-  });
-  
-  //delete a user
-  server.delete("/api/users/:id", (req, res) => {
-    const { id } = req.params;
-    userDb
-      .remove(id)
-      .then(user => {
-        if (user.length < 1) {
-          res.status(404).json({
-            message: "The user with the specified ID does not exist."
-          });
-        } else {
-          res.status(200).json({user, message: "The user was deleted successfully"});
-        }
-      })
-      .catch(err =>
-        res.status(500).json({
-          error: "The user could not be removed."
-        })
-      );
-  });
-  
-  //Update a user
-  server.put("/api/users/:id", (req, res) => {
-    const { id } = req.params;
-    const newUser = req.body;
-    userDb
-      .update(id, newUser)
-      .then(user => {
-        if (user.length < 1) {
-          res.status(404).json({
-            message: "The user with the specified ID does not exist."
-          });
-        } else {
-          res.status(200).json({user, message: "The user was updated successfully"});
-        }
-      })
-      .catch(err =>
-        res.status(500).json({
-          error: "The user information could not be modified.."
-        })
-      );
-  });
-  
-  //=============== POST ENDPOINTS =============== //
+//=============== POST ENDPOINTS =============== //
 
-  //Get all posts
+//Get all posts
 server.get("/api/posts", (req, res) => {
     postDb
       .get()
@@ -137,76 +43,76 @@ server.get("/api/posts", (req, res) => {
       );
   });
   
-  //Add a new post
-  server.post("/api/posts/", (req, res) => {
-    const { text, userId } = req.body;
-    const newPost = { text, userId };
-    if (!text || !userId) {
-      return res.status(400).json({ error: "Please provide text to your post." });
-    }
-    postDb
-      .insert(newPost)
-      .then(post => {
-        res.status(201).json(post);
-      })
-      .catch(err => {
-        res.status(500).json({
-          error: "There was an error saving your post."
-        });
+//Add a new post
+server.post("/api/posts/", (req, res) => {
+  const { text, userId } = req.body;
+  const newPost = { text, userId };
+  if (!text || !userId) {
+    return res.status(400).json({ error: "Please provide text to your post." });
+  }
+  postDb
+    .insert(newPost)
+    .then(post => {
+      res.status(201).json(post);
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: "There was an error saving your post."
       });
-  });
-  
-  //Delete a post
-  server.delete("/api/posts/:id", (req, res) => {
-    const { id } = req.params;
-    postDb
-      .remove(id)
-      .then(post => {
-        if (post.length < 1) {
-          res.status(404).json({
-            message: "The post with that ID does not exist."
-          });
-        } else {
-          res.status(200).json(post);
-        }
+    });
+});
+
+//Delete a post
+server.delete("/api/posts/:id", (req, res) => {
+  const { id } = req.params;
+  postDb
+    .remove(id)
+    .then(post => {
+      if (post.length < 1) {
+        res.status(404).json({
+          message: "The post with that ID does not exist."
+        });
+      } else {
+        res.status(200).json(post);
+      }
+    })
+    .catch(err =>
+      res.status(500).json({
+        error: "The post could not be removed."
       })
-      .catch(err =>
-        res.status(500).json({
-          error: "The post could not be removed."
-        })
-      );
-  });
-  
-  //Update a post
-  server.put("/api/posts/:id", (req, res) => {
-    const { id } = req.params;
-    const { text, userId } = req.body;
-    const newPost = { text, userId };
-    if (!text || !userId) {
-      return res
-        .status(400)
-        .json({ error: "Please provide a userId and text for the post." });
-    }
-    postDb
-      .update(id, newPost)
-      .then(post => {
-        if (post.length < 1) {
-          res.status(404).json({
-            message: "The post with the specified ID does not exist."
-          });
-        } else {
-          res.status(200).json(post);
-        }
+    );
+});
+
+//Update a post
+server.put("/api/posts/:id", (req, res) => {
+  const { id } = req.params;
+  const { text, userId } = req.body;
+  const newPost = { text, userId };
+  if (!text || !userId) {
+    return res
+      .status(400)
+      .json({ error: "Please provide a userId and text for the post." });
+  }
+  postDb
+    .update(id, newPost)
+    .then(post => {
+      if (post.length < 1) {
+        res.status(404).json({
+          message: "The post with the specified ID does not exist."
+        });
+      } else {
+        res.status(200).json(post);
+      }
+    })
+    .catch(err =>
+      res.status(500).json({
+        error: "The post information could not be modified.."
       })
-      .catch(err =>
-        res.status(500).json({
-          error: "The post information could not be modified.."
-        })
-      );
-  });
-  
-  
-  // call server.listen w/ a port of your choosing
-  server.listen(port, () => {
-    console.log(`\n === API running on port ${port} ===\n`);
-  });
+    );
+});
+
+
+// call server.listen w/ a port of your choosing
+server.listen(port, () => {
+  console.log(`\n === API running on port ${port} ===\n`);
+});
