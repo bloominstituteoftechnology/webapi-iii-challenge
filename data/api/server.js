49b6723b4a3aa17ceca5c8cf5f93a091aa8021af
pedro <pedroluismontesinosrivera@gmail.com>
2018-11-08 00:46:34 -0500

    added the ability to update and delete users

--- data/api/server.js
+++ data/api/server.js
@@ -1,29 +1,85 @@
-const express = require('express');
-const helmet = require('helmet');
-const morgan = require('morgan');
+const express = require("express");
+const server = express();
+const upperCaseUser = require('../middleware/upperCaseUsers');
 
 const postDb = require('../helpers/postDb');
 const userDb = require('../helpers/userDb');
 
-const server = express();
-
 server.use(express.json());
-server.use(helmet()); 
-server.use(morgan('short')); 
 
 server.get('/', (req, res) => {
     res.status(200).json({ api: 'running' });
 });
 
-server.get('/api/posts', (req, res) => {
-    postDb
-        .get()
-        .then(post => {
-            res.status(200).json(post);
+server.get('/api/users', (req, res) => {
+    userDb.get()
+        .then(user => {
+            res.status(200).json(user);
         })
         .catch(error => {
-            res.status(500).json({error: 'cannot be retrieved', 'error': error})
+            res.status(500).json({error: 'users cannot be retrieved', error: error});
         });
-});
+})
+
+server.get('/api/users/:id', (req, res) => {
+    const id = req.params.id;
+
+    userDb.get(id)
+        .then(user => {
+            res.status(200).json(user);
+        })
+        .catch(error => {
+            res.status(500).json({error: 'user cannot be retrieved', error: error});
+        })
+})
+
+server.post('/api/users', upperCaseUser, (req, res) => {
+    if(req.body.name){
+        userDb.insert(req.body)
+            .then(user => {
+                res.status(201).json(user);
+            })
+            .catch(error => {
+                res.status(500).json({error: 'user cannot be added', error: error})
+            })
+    }else{
+        res.status(400).json({errorMessage: 'Please provide a name'})
+    }
+})
+
+server.put('/api/users/:id', upperCaseUser, (req, res) => {
+    const id = req.params.id;
+    if(req.body.name){
+        userDb.update(id, req.body)
+            .then(count => {
+                if(count === 1){
+                    res.status(200).json({message: 'The user has been updated'})
+                }else{
+                    res.status(404).json({message: 'The user does not exist'})
+                }
+            })
+            .catch(error => {
+                res.status(500).json({ error: "The user information could not be modified.", 'error': error });
+            })
+    }else{
+        res.status(400).json({ message: "Please provide name" })
+    }
+})
+
+server.delete('/api/users/:id', (req, res) => {
+    const id = req.params.id;
+
+    userDb.remove(id)
+        .then(count => {
+            if(count === 1){
+                res.status(200).json({message: 'the user has been deleted'});
+            }else{
+                res.status(404).json({message: 'the user does not exist'})
+            }
+        })
+        .catch(error => {
+            res.status(500).json({error: 'the user cannot be deleted', error: error})
+        })
+})
 
 module.exports = server;
