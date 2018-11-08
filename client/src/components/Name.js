0e01ef18039c9477857315d72c8689db4b548441
Rahul Desai <rd825@georgetown.edu>
2018-11-08 08:59:06 -0500

    Some UX improvements

--- client/src/components/Name.js
+++ client/src/components/Name.js
@@ -5,7 +5,7 @@ const Name = props => {
     return (
         <Route render={({history}) => (
             <div onClick={() => { history.push(`/${props.user.id}`) }}>
-                <h2>{props.user.name}</h2>
+                <h2 className='name'>{props.user.name}</h2>
             </div>
             )} 
         />
