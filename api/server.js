a1b3f1d8820b12f8bd496655cb8e3df875548c32
Edward J <edjeong99@gmail.com>
2018-11-08 13:30:38 -0500

    add routers

--- api/server.js
+++ api/server.js
@@ -2,55 +2,18 @@ const express = require('express');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const userDb = require("../data/helpers/userDb.js");
-
+const upperCase = require("../middleware/upperCase.js");
+const userRouter = require('../users/userRouter.js');
 
 const server = express();
 
 server.use(express.json()); // built in
 server.use(helmet()); // third party
 server.use(morgan('short')); // third party
+// server.use(upperCase);
 
-server.get('/', (req, res) => {
-    userDb.get()
-    .then( users =>
-        res.status(200).json(users))
-    .catch( (error) => 
-        res.status(500).json({ message: 'could not get users', error })
-    )
-  });
 
-  server.post('/add', (req, res) => {
-    const newUser = req.body;
-
-  userDb.insert(newUser)
-  .then( id =>
-      res.status(200).json(id))
-  .catch( (error) => 
-      res.status(500).json({ message: 'could not add user', error })
-  )
-});
-
-server.put('/edit/:id', (req, res) => {
-    const {id} = req.params;
-    const editUser = req.body;
-
-  userDb.update(id, editUser)
-  .then( id =>
-      res.status(200).json(id))
-  .catch( (error) => 
-      res.status(500).json({ message: 'could not add user', error })
-  )
-});
-
-  server.delete('/delete/:id', (req, res) => {
-      const {id} = req.params;
-
-    userDb.remove(id)
-    .then( count =>
-        res.status(200).json(count))
-    .catch( (error) => 
-        res.status(500).json({ message: 'could not delete user', error })
-    )
-  });
+server.use('/', userRouter);
 
 module.exports = server;
+
---
+++ middleware/upperCase.js
@@ -0,0 +1,15 @@
+module.exports = (req, res, next) => {
+
+    if(req.body.name){
+        req.body.name = req.body.name.toUpperCase();
+        next();
+    }
+    else{
+        res.status(500).json({message:'name is required'})
+    }
+};
+
+
+
+
+  rem
