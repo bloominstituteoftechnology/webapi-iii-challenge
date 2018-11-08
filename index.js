3e9eb895300b0e56465222f647f10ccf05183dc5
KHollobaugh <khollobaugh@gmail.com>
2018-11-08 17:14:46 -0500

    added postRouter

--- index.js
+++ index.js
@@ -2,24 +2,22 @@ const express = require('express');
 const postDb = require('./data/helpers/postDb');
 const userDb = require('./data/helpers/userDb');
 const cors = require('cors');
-
-
 const toUpperCase = require('./config/middleware/toUpperCase');
-// const postRouter = require('./data/posts/postRouter');
+const postRouter = require('./data/posts/postRouter');
 // const configureMiddleware = require('./config/middleware/middleware');
-
 const server = express();
+
 server.use(express.json());
 // server.use(helmet());
 // server.use(morgan('short'));
-
 server.use(cors());
-
 // configureMiddleware(server);
 
 server.get('/', (req, res) => {
     res.json('alive');
 })
+
+server.use('/posts', postRouter)
 
 server.get('/users', (req, res) => {
         userDb.get()
@@ -29,15 +27,6 @@ server.get('/users', (req, res) => {
     })
 })
 
-// server.user('/posts', postRouter)
-
-server.get('/posts', (req, res) => {
-    postDb.get()
-    .then(posts => res.status(200).json(posts))
-.catch(err => {
-    res.status(500).json({ message: "Couldn't retrieve posts" })
-})
-})
 
 
 server.get('/users/:id', (req, res) => {
@@ -56,27 +45,12 @@ server.get('/users/:id/all', (req, res) => {
     })
 })
 
-server.get('/posts/:id', (req, res) => {
-    postDb.get(req.params.id)
-    .then(post => res.status(200).json(post))
-    .catch(err => {
-        res.status(404).json({ message: 'there is no post by that ID'})
-    })
-})
 
 server.post('/users', toUpperCase, (req, res) => {
     userDb.insert(req.body)
     .then(done => res.status(201).json(done))
     .catch(err => {
         res.status(400).json({ message: 'did not add new user'})
-    })
-})
-
-server.post('/posts', (req, res) => {
-    postDb.insert(req.body)
-    .then(done => res.status(201).json(done))
-    .catch(err => {
-        res.status(400).json({ message: 'did not add new post'})
     })
 })
 
@@ -88,14 +62,6 @@ server.put('/users/:id', toUpperCase, (req, res) => {
     })
 })
 
-server.put('/posts/:id', (req, res) => {
-    postDb.update(req.params.id, req.body)
-    .then(post => res.status(201).json(post))
-    .catch(err => {
-        res.status(400).json({ message: "failed to update the post" })
-    })
-})
-
 server.delete('/users/:id', (req, res) => {
     userDb.remove(req.params.id)
     .then(count => res.status(200).json(count))
@@ -103,16 +69,6 @@ server.delete('/users/:id', (req, res) => {
         res.status(400).json({ message: "could not delete user"})
     })
 })
-
-server.delete('/posts/:id', (req, res) => {
-    postDb.remove(req.params.id)
-    .then(count => res.status(200).json(count))
-    .catch(err => {
-        res.status(400).json({ message: "could not delete post"})
-    })
-})
-
-
 
 
 const port = 9000;
