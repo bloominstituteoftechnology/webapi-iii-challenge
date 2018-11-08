6903c743255de5003b348195537f4eb02e837ff0
Elvis Ibarra <ielvisd@gmail.com>
2018-11-07 17:10:50 -0800

    Fixes Uppercase

--- middleware/upperCaseThat.js
+++ middleware/upperCaseThat.js
@@ -1,8 +1,11 @@
 module.exports = (req, res, next) => {
   // next points to the next middleware/route handler in the queue
 
-  console.log('req.body', req.body);
-  newUpperCaseName = req.body.name.toUpperCase();
+  newUpperCaseName = req.body.name
+    .toLowerCase()
+    .split(' ')
+    .map(s => s.charAt(0).toUpperCase() + s.substring(1))
+    .join(' ');
   req.body.name = newUpperCaseName;
 
   next(); // continue to the next middleware
