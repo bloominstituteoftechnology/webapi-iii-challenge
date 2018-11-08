c3f74f0bfe7826793a00550d99fa690e2cf0ebcb
Lajawanti <lajawanti.dhake@gmail.com>
2018-11-08 11:26:50 -0500

    MVP Done

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 //all required
 const express = require('express');
-const db = require('./data/helpers/userDb.js')
+const morgan = require('morgan');
+
+//database
+const userdb = require('./data/helpers/userDb.js');
+const postdb = require('./data/helpers/postDb.js');
 
 const server = express();
  
 //inbuild middleware
 server.use(express.json());
+server.use(morgan('short'));
 
 //custom middleware
 const upperCase = (request, response, next) => {
@@ -14,9 +19,9 @@ const upperCase = (request, response, next) => {
     next();
 };
  
-//get
+//users get
 server.get('/users', (request, response) => {
-        db.get()
+    userdb.get()
           .then(users => {
                 response.status(200).json(users); 
            })
@@ -25,10 +30,21 @@ server.get('/users', (request, response) => {
            }) 
 })
 
-//post
+//users get
+server.get('/users/:id', (request, response) => {
+    userdb.get(request.params.id)
+          .then(users => {
+                response.status(200).json(users); 
+           })
+          .catch(error => {
+                response.status(500).json({error : 'The users data could not be retrieved'})
+           }) 
+})
+
+//users post
 server.post('/users', upperCase, (request, response) => {
     if(request.body.name !== undefined) {
-        db.insert(request.body)
+    userdb.insert(request.body)
           .then(userId => {
                 response.status(201).json(userId);
            })
@@ -40,31 +56,104 @@ server.post('/users', upperCase, (request, response) => {
     }
 }) 
 
-//delete
+//delete users
 server.delete('/users/:id', (request, response) => {
-    db.remove(request.params.id)
-      .then(count => {
-            response.status(200).json(count);
-       })
-      .catch(error => {
-            response.status(500).json({message : 'error deleting user'})
-       })
-})
-
-//put
-server.put('/users/:id', upperCase, (request, response) => {
-    if(request.body.name !== undefined) {
-        db.update(request.params.id, request.body)
+    userdb.remove(request.params.id)
           .then(count => {
-                if(count) {
-                    response.status(200).json(count);
-                } else {
-                    response.status(404).json({ message: "The user with the specified ID does not exist." })
-                }
+              response.status(200).json(count);
            })
           .catch(error => {
-                response.status(500).json({ error: "The user information could not be modified." })
+              response.status(500).json({message : 'error deleting user'})
            })
+})
+
+//users put
+server.put('/users/:id', upperCase, (request, response) => {
+    if(request.body.name !== undefined) {
+        userdb.update(request.params.id, request.body)
+              .then(count => {
+                  if(count) {
+                        response.status(200).json(count);
+                  } else {
+                        response.status(404).json({ message: "The user with the specified ID does not exist." })
+                  }
+               })
+              .catch(error => {
+                   response.status(500).json({ error: "The user information could not be modified." })
+               })
+    } else {
+        response.status(400).json({ errorMessage: "Please provide name for the user." })
+    }
+})
+
+//*********************************************************************** */
+//posts get
+server.get('/posts', (request, response) => {
+    postdb.get()
+          .then(posts => response.status(200).json(posts))
+          .catch(error => response.status(500).json({error : 'The users data could not be retrieved'})) 
+})
+
+//posts get by id
+server.get('/posts/:id', (request, response) => {
+    postdb.get(request.params.id)
+          .then(posts => response.status(200).json(posts))
+          .catch(error => response.status(500).json({error : 'The users data could not be retrieved'})) 
+})
+
+// GET all posts by a specific user
+server.get('/users/:id/posts', (request, response) => {
+     console.log(request.params.id);
+     userdb.getUserPosts(request.params.id)
+           .then(posts => {
+                if(posts.length < 1) {
+                     response.status(200).json(`no post found for User ID : ${request.params.id}`)
+                }
+                response.status(200).json(posts);
+            })
+           .catch(error => response.status(500).json(error));
+})
+
+//posts post
+server.post('/posts', (request, response) => {
+    if(request.body.text !== undefined && request.body.userId !== undefined) {
+    postdb.insert(request.body)
+          .then(postId => {
+                response.status(201).json(postId);
+           })
+          .catch(error => {
+                response.status(500).json({message : 'error creating user', error});
+           }) 
+    } else {
+        response.status(400).json({ errorMessage: "Please provide text and userId for the post." })
+    }
+}) 
+
+//delete post
+server.delete('/posts/:id', (request, response) => {
+    postdb.remove(request.params.id)
+          .then(count => {
+              response.status(200).json(count);
+           })
+          .catch(error => {
+              response.status(500).json({message : 'error deleting user'})
+           })
+})
+
+//post put
+server.put('/posts/:id', upperCase, (request, response) => {
+    if(request.body.text !== undefined && request.body.userId !== undefined) {
+        postdb.update(request.params.id, request.body)
+              .then(count => {
+                  if(count) {
+                        response.status(200).json(count);
+                  } else {
+                        response.status(404).json({ message: "The user with the specified ID does not exist." })
+                  }
+               })
+              .catch(error => {
+                   response.status(500).json({ error: "The user information could not be modified." })
+               })
     } else {
         response.status(400).json({ errorMessage: "Please provide name for the user." })
     }
