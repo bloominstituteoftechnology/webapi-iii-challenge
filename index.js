ccb947ef76d014e8475dbf21ccfacaa5f42646b5
frankbenevento <frankbenevento123@gmail.com>
2018-11-08 11:02:55 -0800

    testing

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const server=require('./api/server.js');
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 server.listen(port,()=>console.log('************LISTENING ON PORT 8080************'))
