bec082f5ff64d81acd0a619fb1a2b83135918716
ssmodish <smodish@hotmail.com>
2018-11-08 11:12:05 -0500

    PCR push

--- api/server.js
+++ api/server.js
@@ -15,11 +15,11 @@ server.use(helmet());
 server.use(morgan('short'));
 
 
+// add custom middleware
+// function toUpperCaseMiddleware
+
 
 // configure endpoints
-// server.get('/', (req, res) => {
-//   res.status(200).json({ api: 'running' });
-// });
 
 // get /users
 server.get('/users', (req, res) => {
@@ -29,19 +29,70 @@ server.get('/users', (req, res) => {
     })
     .catch(err => { 
       res
-      .status(500)
-      .json(err);
+        .status(500)
+        .json(err);
     });
 });
 
 // get /users/:id
+server.get('/users/:userId', (req, res) => {
+  const { userId } = req.params;
+
+  userDb.get(userId)
+    .then(user => {
+      res.status(200).json(user)
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json(err);
+    })
+})
 
 // get /users/:id/posts
+server.get('/users/:userId/posts', (req, res) => {
+  const { userId } = req.params;
+
+  userDb.getUserPosts(userId)
+    .then(posts => {
+      res.status(200).json(posts)
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json(err);
+    })
+})
 
 // get /users/:userId/posts/:postId
+server.get('/posts/:postId', (req, res) => {
+  const { postId } = req.params;
+
+  postDb.get(postId)
+    .then(post => {
+      res.status(200).json(post)
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json(err);
+    })
+})
+
+
+// get /posts
+server.get('/posts', (req, res) => {
+  postDb.get()
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(err => { 
+      res
+        .status(500)
+        .json(err);
+    });
+});
 
 
 
-
-
-module.exports = server
+module.exports = server;
