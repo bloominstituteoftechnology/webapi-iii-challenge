7eba9e2e8b1b2fd892dc11400104b00975235ef1
Carlos Lantigua <clantigua954@gmail.com>
2018-11-07 19:13:08 -0500

    got users to post in react

--- api/server.js
+++ api/server.js
@@ -13,20 +13,9 @@ server.get('/', (req, res) => {
 	res.json('alive');
 });
 
-// get all posts
-server.get('/api/posts/all', (req, res) => {
-	postDB
-		.get()
-		.then((post) => {
-			res.status(200).json(post);
-		})
-		.catch((err) => res.status(500).json({ message: "Can't retrieve post data!!" }));
-	userDB;
-});
-// test success
-
-// get user by id
-server.get('/api/user/:id', (req, res) => {
+////////////USER SECTION////////////////
+// get all users
+server.get('/api/user/', (req, res) => {
 	const { id } = req.params;
 	userDB
 		.get(id)
@@ -37,23 +26,24 @@ server.get('/api/user/:id', (req, res) => {
 });
 // test success ex. localhost:9000/api/user/1 ==> "id": 1, "name": "Frodo Baggings"
 
+server.get('/api/user/:id', (req, res) => {
+	const { id } = req.params;
+	userDB
+		.get(id)
+		.then((user) => {
+			res.status(200).json(user);
+		})
+		.catch((err) => res.status(500).json({ message: "can't revieve user data!!" }));
+});
+
 // get all user posts
-server.get(`/api/posts/user/`, (req, res) => {
+server.get(`/api/user/:id`, (req, res) => {
 	const userId = req.query.userId;
 	userDB.getUserPosts(userId).then((user) => {
 		user.length <= 0 ? res.status(404).json({ message: 'That user was not found' }) : res.status(200).json(user);
 	});
 });
 // test success ex. localhost:9000/api/posts/user/?userId=1 ==> return of posts by Froto
-
-// get post by id
-server.get('/api/posts/:id', (req, res) => {
-	const { id } = req.params;
-	postDB.get(id).then((post) => {
-		post.length <= 0 ? res.status(404).json({ message: 'That post was not found' }) : res.status(200).json(post);
-	});
-});
-// test success
 
 // create a user and uppercase their name
 server.post('/api/user', upperCase, (req, res) => {
@@ -67,33 +57,6 @@ server.post('/api/user', upperCase, (req, res) => {
 		});
 });
 //test success
-
-// delete a post by id
-server.delete('/api/posts/:id', (req, res) => {
-	postDB.remove(req.params.id).then((count) => {
-		count
-			? res.status(200).json({ message: 'Post deleted successfully' })
-			: res.status(404).json({ message: 'That post was not found or already deleted' });
-	});
-});
-//test success
-
-// update a post by id
-server.put('/api/posts/:id', (req, res) => {
-	const { id } = req.params;
-	const changes = req.body;
-	postDB
-		.update(id, changes)
-		.then((count) => {
-			count
-				? res.status(200).json({ message: 'Post updated successfully' })
-				: res.status(404).json({ message: 'That post was not found' });
-		})
-		.catch((err) => {
-			res.status(500).json({ message: 'error updating post', err });
-		});
-});
-// test success
 
 // update user by id and uppercase name
 server.put('/api/user/:id', upperCase, (req, res) => {
@@ -126,4 +89,56 @@ server.delete('/api/user/:id', (req, res) => {
 			res.status(500).json({ message: 'error, could not delete user', err });
 		});
 });
+// test successful
+
+//////////////POST SECTION///////////////
+
+// get all posts
+server.get('/api/posts/all', (req, res) => {
+	postDB
+		.get()
+		.then((post) => {
+			res.status(200).json(post);
+		})
+		.catch((err) => res.status(500).json({ message: "Can't retrieve post data!!" }));
+	userDB;
+});
+// test success
+
+// get post by id
+server.get('/api/posts/:id', (req, res) => {
+	const { id } = req.params;
+	postDB.get(id).then((post) => {
+		post.length <= 0 ? res.status(404).json({ message: 'That post was not found' }) : res.status(200).json(post);
+	});
+});
+// test success
+
+// delete a post by id
+server.delete('/api/posts/:id', (req, res) => {
+	postDB.remove(req.params.id).then((count) => {
+		count
+			? res.status(200).json({ message: 'Post deleted successfully' })
+			: res.status(404).json({ message: 'That post was not found or already deleted' });
+	});
+});
+//test success
+
+// update a post by id
+server.put('/api/posts/:id', (req, res) => {
+	const { id } = req.params;
+	const changes = req.body;
+	postDB
+		.update(id, changes)
+		.then((count) => {
+			count
+				? res.status(200).json({ message: 'Post updated successfully' })
+				: res.status(404).json({ message: 'That post was not found' });
+		})
+		.catch((err) => {
+			res.status(500).json({ message: 'error updating post', err });
+		});
+});
+// test success
+
 module.exports = server;
