15d87692b65e7a4829de3024c51f4db516c7b0a4
Phil Hart <phil@Phils-iMac.local>
2018-11-14 15:49:48 -0700

    GET by ID up and working

--- server.js
+++ server.js
@@ -26,6 +26,9 @@ const upperCase = (req, res, next) => {
 
 //endpoints
 
+
+
+// Get all users
 server.get("/api/users", (req, res) => {
     users
       .get()
@@ -33,9 +36,67 @@ server.get("/api/users", (req, res) => {
         res.status(200).json(users);
       })
       .catch(err => {
-        res.status(500).json({ message: "the post info could not be received" });
+        res.status(500).json({ message: "the post info could not be received", err });
       });
   });
+
+
+  // Get users by ID
+
+  server.get("/api/users/:id", (req, res) => {
+      const { id } = req.params;
+      users
+        .get(id)
+        .then(users => {
+            if (users) {
+                res.status(200).json(users)
+            } else {
+                res.status(404).json({ message: "the post with the specified ID does not exist" })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ message: "the post info could not be received", err });
+        });
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
