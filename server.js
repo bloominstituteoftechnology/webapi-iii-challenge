adf3eef89dc8c91961276aa2d9602ea0dff60089
jarheadm249 <36177264+jarheadm249@users.noreply.github.com>
2018-11-14 17:24:14 -0500

    Put working

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const users = require('./data/helpers/userDb');
 const posts = require('./data/helpers/postDb');
 const tags = require('./data/helpers/tagDb');
+const db = require('./data/helpers/userDb');
 const port = 8000;
 
 const server = express();
@@ -91,6 +92,25 @@ server.delete('/api/users/:id', (req, res) => {
     return errorMsg(500, 'Try Again', res);
   })
 })
+
+server.put('/api/users/:id', middlewareNameCheck, (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+  users
+    .update(id, { name })
+    .then(response => {
+      if (response === 0) {
+        return errorMsg(404, 'Cannot find that user');
+      } else {
+        res.json({ success: 'User Updated' });
+      }
+    })
+  .catch(err => {
+    return errorMsg(500, 'Try Again', res)
+  })  
+})
+
+
 
 
 
