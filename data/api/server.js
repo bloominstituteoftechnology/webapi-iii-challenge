6e8b87c4124a616b5427ee62100ea1786f440c9a
Lambda_School_Loaner_27 <lambda_school_loaner_27@Lambda-School-Loaner-27s-MacBook-Pro.local>
2018-11-15 16:16:44 -0600

    set up index file that starts the server

--- data/api/server.js
+++ data/api/server.js
@@ -1,3 +1,9 @@
+module.exports = {
+  listen: (port, callback) => {
+    console.log('ran the server');
+    callback();
+  }
+}
 const express = require('express');
 const start = express();
 start.use(express.json());
@@ -5,6 +11,3 @@ start.use(express.json());
 start.get('/', (req, res) => {
   res.send('Server is operational')
 });
-
-const port = 5500;
-start.listen(port, () => console.log(`Server is running on port ${port}`));
