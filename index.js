c73207ca80ba6a524500fa8e34f66598ad083038
Angelo Deleon <rudy209@comcast.net>
2018-11-15 14:38:59 -0800

    Added uppercase middleware!

--- index.js
+++ index.js
@@ -1,67 +1,73 @@
 const express = require("express");
 const server = express();
 server.use(express.json());
+const uppercase = require("./middleware/uppercase.js");
+
 
 const postDB = require("./data/helpers/postDb.js");
 const userDB = require("./data/helpers/userDb.js");
 const tagDB = require("./data/helpers/tagDb.js");
 
+
 // # R00T
 server.get("/", (req, res) => {
-  res.status(200).send("<h1>THIS IS THE <em>root directory</em></h1>");
+    res.status(200).send("<h1>THIS IS THE <em>root directory</em></h1>");
 });
 
 // # GET ALL POSTS
 server.get("/posts/all", (req, res) => {
-  postDB
-    .get()
-    .then(post => {
-      res.status(200).json(post);
-    })
-    .catch(err => res.status(500).json({ message: "Cannot retrieve posts" }));
-  userDB;
+    postDB
+        .get()
+        .then(post => {
+            res.status(200).json(post);
+        })
+        .catch(err => res.status(500).json({ message: "Cannot retrieve posts" }));
+    userDB;
 });
 
 // # GET POSTS BY USERID
 server.get("/posts/user/:id", (req, res) => {
-  const { id } = req.params;
-  userDB.getUserPosts(id).then(user => {
-    res.status(200).json(user);
-  });
+    const { id } = req.params;
+    userDB.getUserPosts(id).then(user => {
+        res.status(200).json(user);
+    });
 });
 
 // # GET POST BY ID
 server.get("/posts/:id", (req, res) => {
-  const { id } = req.params;
-  postDB.get(id).then(post => {
-    res.status(200).json(post);
-  });
+    const { id } = req.params;
+    postDB.get(id).then(post => {
+        res.status(200).json(post);
+    });
 });
 
-// server.post("/posts", async (req, res) => {
-//   try {
-//     const postData = req.body;
-//     const postId = await postDB.insert(postData);
-//     console.log("post:", postData.title);
-//      res.status(201).json(postId);
-//   } catch (error) {
-//     res.status(500).json({ message: "error creating post" });
-//   }
-// });
+// # CREATE A  USER WITH UPPERCASE MIDDLEWARE!!!
+server.post("/user", uppercase, (req, res) => {
+    userDB
+        .insert(req.body)
+        .then(userData => {
+            res.status(201).json(userData);
+        })
+        .catch(err => {
+            res
+                .status(500)
+                .json({ message: "There was an error creating the user", err });
+        });
+});
 
 // # GET TAGS BY P0ST ID
 server.get("/posts/tags/:id", (req, res) => {
-  const { id } = req.params;
-  tagDB.getPostTags(id).then(posts => {
-    res.status(200).json(posts);
-  });
+    const { id } = req.params;
+    tagDB.getPostTags(id).then(posts => {
+        res.status(200).json(posts);
+    });
 });
 
 // # DELETE POST BY ID
 server.delete("/posts/:id", (req, res) => {
-  postDB.remove(req.params.id).then(post => {
-    res.status(200).json({ message: "Post deleted successfully" });
-  });
+    postDB.remove(req.params.id).then(post => {
+        res.status(200).json({ message: "Post deleted successfully" });
+    });
 });
 
 
