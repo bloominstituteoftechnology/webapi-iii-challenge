6f86004ea3074e74856382ce1a1437c826fc1e42
Wanda <doubleuevans@gmail.com>
2018-11-15 13:18:18 -0600

    user get and getbyId and delete; middleware (not working)

--- data/api/server.js
+++ data/api/server.js
@@ -5,8 +5,7 @@ const morgan = require('morgan');
 
 const userDb = require('../helpers/userDb.js');
 
-
-//const gatekeeper = require('../gatekeeper/middleware.js');
+const nameUpper = require('../gatekeeper/middleware.js');
 
 const server = express();
 
@@ -15,9 +14,9 @@ server.use(express.json());
 server.use(helmet());
 server.use(morgan('dev'));
 
-//custom here
+//custom middleware here
 //global middleware
-//server.use(gatekeeper);
+server.use(nameUpper);
 
 // configure endpoints (route handlers are middleware!!)
 server.get('/', (req, res) => {
@@ -35,7 +34,7 @@ server.get('/api/users', (req, res) => {
     })
 });
 
-//get user by id
+//get user by id - not required
 server.get('/api/users/:id', (req, res) => {
   const { id } = req.params; //id is a parameter
 
@@ -51,5 +50,42 @@ server.get('/api/users/:id', (req, res) => {
       res.status(500).json({ error: "The user information could not be retrieved."})
     })
 });
+
+//add user
+server.post('/api/users', nameUpper, (req, res) => {
+  userDb.insert(req.body)
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "error adding user", err })
+    })
+})
+
+//delete user
+server.delete('/api/users/:id', (req, res) => {
+  userDb.remove(req.params.id)
+    .then(count => {
+      res.status(200).json(count)
+    }).catch (err => {
+      res.status(500).json({ message: 'error deleting user '})
+    })
+})
+
+//update username
+/* server.put('/api/users/:id', (req, res) => {
+  const { id } = req.params;
+  const change = req.body;
+
+  userDb.update(id, change)
+    .then(count => {
+      if(count) {
+        res.status(200).json({ message: `${count} user updated`})
+      } else {
+        res.status(404).json({ message: "user not found" })
+      }
+    })
+}) */
+
 
 module.exports = server;
