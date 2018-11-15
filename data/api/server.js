66469313640f2509c9eda2bf0edbe1d792e0cd95
Lambda_School_Loaner_27 <lambda_school_loaner_27@Lambda-School-Loaner-27s-MacBook-Pro.local>
2018-11-15 16:47:06 -0600

    added helmet and morgan

--- data/api/server.js
+++ data/api/server.js
@@ -1,13 +1,19 @@
-module.exports = {
-  listen: (port, callback) => {
-    console.log('ran the server');
-    callback();
-  }
-}
 const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
 const start = express();
+
+// middleware
+// ORDER MATTERS! they will execute top to bottom
 start.use(express.json());
+start.use(helmet());
+start.use(morgan('dev'));
+
+// endpoints (route handlers are middleware!!)
 
 start.get('/', (req, res) => {
-  res.send('Server is operational')
+  res.status(200).json({ api: 'Server is operational'});
 });
+
+module.exports = start;
