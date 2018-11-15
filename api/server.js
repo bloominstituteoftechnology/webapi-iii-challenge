611f07128387c33fd2cd485c7c091dbfa0b3565f
mikaelacurrier <mikaela.currier@gmail.com>
2018-11-15 16:53:36 -0500

    Removed tag router

--- api/server.js
+++ api/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 
 
-const tagRouter = require('./tag/tagRouter');
 const userRouter = require('./user/userRouter');
 const postRouter = require('./post/postRouter');
 
@@ -10,14 +9,12 @@ const server = express();
 
 const configureMiddleware = require('./config/middleware');
 
-
-
 configureMiddleware(server);
+
 
 server.get('/', (req, res) => {
     res.status(200).json({ api: "it runs" });
 })
-
 // POST DATA
 server.use('/post', postRouter);
 
@@ -25,6 +22,5 @@ server.use('/post', postRouter);
 server.use('/user', userRouter);
 
 // TAG DATA
-server.use('/tag', tagRouter);
 
 module.exports = server;
