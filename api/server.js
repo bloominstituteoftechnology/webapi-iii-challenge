c991f5ba0e4d6adf8c710a7b808ed0345574760d
Troy Amlee <troyamlee19@gmail.com>
2018-11-15 19:50:56 -0600

    [mvp] all CRUD functional, toUpperCase middleware works

--- api/server.js
+++ api/server.js
@@ -7,6 +7,7 @@ const toUpperCase = require ('../middleware/toUpperCase.js');
 
 const server = express();
 
+// server.use(toUpperCase());
 server.use(express.json());
 server.use(helmet());
 server.use(morgan('dev'));
@@ -17,9 +18,9 @@ server.get('/', (req, res) => {
 });
 
 server.get('/users', (req, res) => {
-    dbUsers.find()
+    dbUsers.get()
     .then(users => {
-        res.status(200).json(users.toUpperCase());
+        res.status(200).json(users);
     })
     .catch(err => {
         res.status(500).json({ message: "The users information could not be retrieved!", err })
@@ -28,18 +29,63 @@ server.get('/users', (req, res) => {
 
 server.get('/users/:id', (req, res) => {
     const { id } = req.params;
-
-    dbUsers.getUserPosts(id)
+    dbUsers.get(id)
     .then(user => {
         if (user.length === 0){
             res.status(404).json({ message: "The User with the specified ID does not exist" })
-        }else { res.status(200).json(user.toUpperCase()) }
+        }else { res.status(200).json(user)}
     });
 });
 
-
-server.get('/posts', (req, res) => {
-    
+server.get('/users/:id/posts', (req, res) => {
+    const { id } = req.params;
+    dbUsers.getUserPosts(id)
+    .then(userpost => {
+        if (userpost.length === 0){
+            res.status(404).json({ message: "The User with the specified ID does not exist" })
+        }else { res.status(200).json(userpost) }
+    });  
 });
+
+
+server.post('/users', toUpperCase, async (req, res) => {
+    console.log('Body:', req.body);
+    try {
+        const userData = req.body;
+        const userId = await dbUsers.insert(userData);
+        const user = await dbUsers.get(userId.id);
+        res.status(201).json(user);
+    } catch (error) {
+        res.status(500).json({ message: `error in creating user ${user}`, error })
+    }
+})
+
+server.delete('/users/:id', (req, res) => {
+    dbUsers.remove(req.params.id)
+    .then(count => {
+        if (count) {
+         res.status(204).json({ message: `${count} user was deleted` });   
+        } else {res.status(404).json({ message: `404 ${count} user not found` })}
+        
+    })
+    .catch(error => {
+        res.status(500).json( { message: "There was an error deleting user", error } )
+    })
+})
+
+server.put('/users/:id', (req, res) => {
+    dbUsers.update(req.params.id, req.body)
+    .then(count => {
+        if(count){
+        res.status(200).json(`${count} user was updated`)     
+        } else{ res.status(404).json({ message: 'user does not exist' }) }
+       
+    })
+    .catch(error => {
+        res.status(500).json({ message: 'Error Updating The User', error })
+    })
+})
+
+
 
 module.exports = server;
--- middleware/toUpperCase.js
+++ middleware/toUpperCase.js
@@ -1,3 +1,7 @@
 module.exports = (req, res, next) => {
-    
+    console.log(req.body.name.toUpperCase());
+    req.body.name = req.body.name.toUpperCase();
+    // res.send(`${res.body.name}`);
+    // console.log(req.body.name)
+    next();
 }
