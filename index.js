eae8f4eb2cb727e246c15bdb2b07f8f1d74b5afe
Jennifer King <jenbellpgh@gmail.com>
2018-12-03 23:42:35 -0500

    Added middleware: helmet & morgan

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
 
+// Server:
 const server = express();
 const PORT = 5454;
+
+// Middleware:
+server.use( 
+  express.json(),
+  morgan('dev'),
+  helmet()
+);
+
 
 // Route handler endpoint for GET - barebones for now,
 // will customize after server is operational.
