3a1c2f0c584a8f109ea6058ef9fa00bcbdb7cdea
cferrell2 <cferrell12182@gmail.com>
2018-12-03 23:49:06 -0500

    create .get route handlers to retrieve all users as well as individual user

--- server.js
+++ server.js
@@ -1,12 +1,31 @@
+//grab database methods
+const userdb = require('./data/helpers/userDb')
+const postdb = require('./data/helpers/postDb')
+
 //create server
 const express = require('express');
 const server = express();
 server.use(express.json());
 
-//.get route handler
-server.get('/', (req, res) =>{
-    res.json("server up and running!")
+//****USER ROUTE HANDLERS*************
+server.get('/api/users', (req, res) =>{
+    userdb.get()
+    .then(users =>{
+        res.json(users)
+    })
+    .catch(err =>{
+        res.status(500)
+        res.json({error: "Unable to retrieve users"})
+    })
 });
+server.get('/api/users/:id', (req, res) =>{
+    const id = req.params.id;
+    userdb.get(id)
+    .then(user =>{
+        res.json(user)
+    })
+});
+
 
 //listener
 const PORT = 4000;
