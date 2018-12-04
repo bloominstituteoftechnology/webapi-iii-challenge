05f009b0c08575791e7548edb6ce8dd277186034
Rick Fleming <elfeming111@yahoo.com>
2018-12-03 21:45:54 -0700

    Added post endpoint for user

--- index.js
+++ index.js
@@ -31,6 +31,25 @@ server.get('/api/users/:id', (req, res)=>{
     })
 })
 
+server.post('/api/users', (req, res)=>{
+    const newUser = req.body
+    if(newUser.name){
+        userDB.insert(newUser)
+        .then(idObj=>{
+            userDB.get(idObj.id)
+            .then(user=>{
+                res.json(user);
+            })
+        })
+        .catch(error=>{
+            res.status(500).json({error: 'The information could not be retrieved.'})
+        })
+    }
+    else{
+        res.status(400).json({errorMessage: 'Please provide a user name.'})
+    }
+})
+
 server.listen(PORT, err=>{
     console.log(`Server running on port: ${PORT}`);
 });
