82666219a4e5c42b7c5bc8af5ae0b8962dd465a6
Joseph Thompson <joe.thompson838@gmail.com>
2018-12-04 16:40:05 -0500

    finished the CRUD operations for server side

--- server.js
+++ server.js
@@ -1,51 +1,80 @@
 const express = require('express');
 const cors = require('cors');
+const helmet = require('helmet');
+const logger = require('morgan');
+
 const userDB = require('./data/helpers/userDb');
 const postDB = require('./data/helpers/postDb');
+
 const SERVER = express();
 const PORT = 3020;
 
-SERVER.use(cors());
+SERVER.use(cors(), helmet(), logger('dev'));
 SERVER.use(express.json());
 
 // ----------- SETUP USERS END POINTS ----------------
 
+// set up a get for all users to be DRY
+const getUsers = (id, req, res) => {
+
+    if (id) {
+        userDB.get(id)
+          .then(user => {
+            if (user) {
+            res.json({user: user})
+            }
+            else {
+              res.status(404).json({error: "The user you are looking for does not exist"})
+            }
+          })
+          .catch(err => {
+            res.status(500).json({message: "Please try again later, cannot connect to database"})
+          })
+      }
+      else {
+        userDB.get()
+              .then(users => {
+                if (users) {
+                  res.json({users: users})
+                }
+                else {
+                  res.status(404).json({message: "Users does not exist. Please add a user to the Database first."})
+                }
+              })
+              .catch(err => {
+                res.status(500).json({error: "Cannot Get Users From Database at this time, Come back later"})
+              })
+      }
+}
+
 // ~~~~~~~~~~~ GET REQUEST USERS ~~~~~~~~~~~~~~~~~~~~
 SERVER.get('/users', (req,res) => {
-  userDB.get()
-        .then(users => {
-          if (users) {
-            res.json({users: users})
-          }
-          else {
-            res.status(404).json({message: "Users does not exist. Please add a user to the Database first."})
-          }
-        })
-        .catch(err => {
-          res.status(500).json({error: "Cannot Get Users From Database at this time, Come back later"})
-        })
+  return getUsers(null, req,res);
 })
 
 // ~~~~~~~~~~~ GET REQUEST FOR USER (ID) ~~~~~~~~~~~~~~~~~~~~
 SERVER.get('/users/:id', (req, res) => {
   const { id } = req.params;
-
-  userDB.get(id)
-        .then(user => {
-          if (user) {
-          res.json({user: user})
+  return getUsers(id, req, res);
+})
+// ~~~~~~~~~~~ GET REQUEST FOR USER POSTS ~~~~~~~~~~~~~~~~~~~~
+SERVER.get('/users/:id/posts', (req, res) => {
+  const { id } = req.params;
+  userDB.getUserPosts(id)
+        .then(posts => {
+          if(posts.length === 0) {
+            res.status(404).json({message: "This user has no posts or this User does not exist. Please try again"})
           }
           else {
-            res.status(404).json({error: "The user you are looking for does not exist"})
+            res.json({posts})
           }
         })
         .catch(err => {
-          res.status(500).json({message: "Please try again later, cannot connect to database"})
+          res.status(500).json({error: err})
         })
 })
-
 // ~~~~~~~~~~~ POST REQUEST FOR NEW USER ~~~~~~~~~~~~~~~~~~~~
-SERVER.post('/users', (req, res) => {
+SERVER.post('/users', (req, res, next) => {
   const { name } = req.body;
   userDB.insert({name})
         .then(userID => {
@@ -53,6 +82,39 @@ SERVER.post('/users', (req, res) => {
         })
 })
 
+// ~~~~~~~~~~~ DELETE REQUEST FOR USER ~~~~~~~~~~~~~~~~~~~~
+SERVER.delete('/users/:id', (req, res) => {
+  const { id } = req.params
+  userDB.remove(id)
+        .then(deleted => {
+          if(deleted) {
+            res.json({message: "user successfully deleted"})
+          }
+        })
+        .catch(err => {
+          res.status(500).json({error: err})
+        })
+})
+
+// ~~~~~~~~~~~ DELETE REQUEST FOR USER ~~~~~~~~~~~~~~~~~~~~
+SERVER.put('/users/:id', (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  userDB.update(id, {name})
+        .then(count => {
+          if (count) {
+            res.json({message: "user successfully updated!"})
+          }
+          else {
+            res.status(400).json({message: "User was not updated, Try again later."})
+          }
+        })
+        .catch(err => {
+          res.status(500).json({error: err})
+        })
+
+})
 // ----------- SETUP POSTS END POINTS ----------------
 
 // ~~~~~~~~~~~ GET REQUEST POSTS ~~~~~~~~~~~~~~~~~~~~
@@ -85,7 +147,7 @@ SERVER.get('/posts/:id', (req,res) => {
           }
         })
         .catch(err => {
-          res.status(404).json({error: "Please try again, this post does not exist"})
+          res.status(404).json({error: "This post does not exist"})
         })
 })
 
@@ -108,6 +170,37 @@ SERVER.post('/posts', (req, res) => {
         })
 })
 
+// ~~~~~~~~~~~ DELETE REQUEST FOR POST ~~~~~~~~~~~~~~~~~~~~
+SERVER.delete('/posts/:id', (req, res) => {
+  const { id } = req.params;
+  postDB.remove(id)
+        .then(deleted => {
+          if(deleted) {
+            res.json({message: 'Post successfully deleted.'})
+          }
+        })
+        .catch(err => {
+          res.status(500).json({error: err})
+        })
+})
+
+// ~~~~~~~~~~~ UPDATE REQUEST FOR POST ~~~~~~~~~~~~~~~~~~~~
+SERVER.put('/posts/:id', (req, res) => {
+  const { id } = req.params;
+  const { text } = req.body;
+  postDB.update(id, {text})
+        .then(count => {
+          if (count) {
+            res.json({message: "Post has been successfully updated."})
+          }
+          else {
+            res.status(400).json({message: "Please refresh and try again."})
+          }
+        })
+        .catch(err => {
+          res.status(500).json({error: "Server not responding, please try again"})
+        })
+})
 SERVER.listen(PORT, () => {
   console.log(`\n~~~~ Server running on PORT:${PORT} ~~~~\n`)
 })
