8bd29eb0489b2d116acdc63d04cd7d5cf3d5ec9d
hraya <hraya@lssu.edu>
2018-12-03 23:54:52 -0600

    Have get and post
    The GET and POST working for post and user

--- index.js
+++ index.js
@@ -1,10 +1,71 @@
 const express = require('express');
 
+const postDb = require('./data/helpers/postDb.js');
+const userDb =  require ('./data/helpers/userDb.js')
+
+
+
 const server = express();
+server.use(express.json())
 const PORT = 4000;
 
-server.get('/userPosts', (req, res) =>{
-    res.status(200).json({message:"Did this work"})
+server.get('/post', (req, res) =>{
+    postDb.get()
+    .then(posts =>{
+        res.status(200).json(posts)
+    }).catch(err => {
+        res.status(500).json({message:"trouble getting posts"})
+    })
+})
+
+server.get('/post/:id', (req, res) =>{
+    const  { id } = req.params;
+    postDb.get(id)
+    .then(posts =>{
+        res.status(200).json(posts)
+    }).catch(err => {
+        res.status(500).json({message:"trouble getting posts"})
+    })
+})
+
+
+server.get('/user', (req, res) =>{
+    userDb.get()
+    .then(user =>{
+        res.status(200).json(user)
+    }).catch(err => {
+        res.status(500).json({message:"trouble getting users"})
+    })
+})
+
+server.get('/user/:id', (req, res) =>{
+    const  { id } = req.params;
+    postDb.get(id)
+    .then(user =>{
+        res.status(200).json(user)
+    }).catch(err => {
+        res.status(500).json({message:"trouble getting user"})
+    })
+})
+
+server.post('/post', (req, res) =>{
+    const data = req.body;
+    postDb.insert(data)
+    .then(post =>{
+        res.status(201).json(post)
+    }).catch(err =>{
+        res.status(404).json({message:"Could not update Post"})
+    })
+})
+
+server.post('/user', (req, res) =>{
+    const data = req.body;
+    userDb.insert(data)
+    .then(user =>{
+        res.status(201).json(user)
+    }).catch( err =>{
+        res.status(404).json({message:"Could not update User"})
+    })
 })
 
 server.listen(PORT, ()=>{
