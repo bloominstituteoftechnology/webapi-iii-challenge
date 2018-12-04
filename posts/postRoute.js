8c9e6ce82018616f2268efbc793e5b79e4b35cc0
Colin Dismuke <colin.dismuke@gmail.com>
2018-12-03 22:58:14 -0600

    GET post for individual posts functioning

--- posts/postRoute.js
+++ posts/postRoute.js
@@ -3,6 +3,7 @@ const db = require("../data/helpers/postDb");
 
 const router = express.Router();
 
+// Endpoints
 router.get("/", (req, res) => {
   db.get()
     .then(posts => {
@@ -13,4 +14,21 @@ router.get("/", (req, res) => {
     });
 });
 
+router.get("/:postId", (req, res) => {
+  const { postId } = req.params;
+  console.log(req.params);
+  db.get(postId).then(post => {
+    if (post) {
+      res.json(post);
+    } else {
+      res.status(404).json({ message: "Failed to get post." });
+    }
+  });
+});
+
 module.exports = router;
+
+// get(): calling find returns a promise that resolves to an array of all the resources contained in the database. If you pass an id to this method it will return the resource with that id if found.
+// insert(): calling insert passing it a resource object will add it to the database and return an object with the id of the inserted resource. The object looks like this: { id: 123 }.
+// update(): accepts two arguments, the first is the id of the resource to update and the second is an object with the changes to apply. It returns the count of updated records. If the count is 1 it means the record was updated correctly.
+// remove(): the remove method accepts an id as it's first parameter and, upon successfully deleting the resource from the database, returns the number of records deleted.
