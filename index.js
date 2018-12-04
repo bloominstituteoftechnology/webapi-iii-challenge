765263d4bbc6cc5567c0de32f142ed46d9037886
Obed Lorisson <Obed.lorisson@gmail.com>
2018-12-04 01:10:13 -0500

    add knex as dependencies add sqlite3 as dependencies add get to get all the users implement get by id to retrieve individual user

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-console.log("server running")
+//Modules require 
+const express = require('express');
+const db = require('./data/helpers/userDb');
+const server = express();
+
+const PORT = 5050;
+
+//endpoints
+server.get('/api/users',(req,res) =>{
+  db.get()
+  .then(users =>{
+    res.json(users)
+  })
+  .catch(err =>{
+    res
+    .status(500)
+    .json({message:"Unable to retrieve users "})
+  })
+})
+
+server.get('/api/users/:id', (req, res)=>{
+  const { id } = req.params;
+  db.get(id)
+  .then(user =>{
+    console.log('user?',user);
+  })
+  .catch(err =>{
+    res
+    .status(500)
+    .json({message:"The User cannot be retrieved"})
+  })
+})
+
+
+
+
+//listen
+server.listen(PORT, () =>{
+  console.log(`Server running on port ${PORT}`)
+})
