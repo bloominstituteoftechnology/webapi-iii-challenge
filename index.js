c4fb3d8222d69def0dbbddf0423fdf71011b6f7d
Jennifer King <jenbellpgh@gmail.com>
2018-12-04 00:31:52 -0500

    GET list of all users.

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+// Base Requirements:
 const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
+
+// App Requirements:
+const users = require('./data/helpers/userDb');
 
 // Server:
 const server = express();
@@ -13,13 +17,40 @@ server.use(
   helmet()
 );
 
-
 // Route handler endpoint for GET - barebones for now,
 // will customize after server is operational.
 server.get( '/', (req, res) => {
   res.json( {message: "Got it!"} );
 });
 
+//-------- User Info: --------//
+// GET:
+server.get( '/users', (req, res) => {
+  users.get()
+    .then( listUsers => {
+      res.json(listUsers);
+    })
+    .catch( err => {
+      res.status(500).json({ error: "The user information could not be retrieved."});
+    });
+});
+
+server.get( '/users/:id', (req, res) => {
+  const { id } = req.params;
+  users
+    .get(id)
+    .then( listUser => {
+      // Check for empty result
+      if( listUser ){
+        res.json(listUser);
+      } else {
+        res.status(404).json({ message: "The user does not exist."});
+      }
+    })
+    .catch( err => {
+      res.status(500).json({ error: "The user information could not be retrieved."});
+    });
+});
 
 // Listen for incoming requests. Must always be last in file.
 server.listen( PORT, () => {
