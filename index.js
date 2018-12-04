e2816e9f44dcc915b7f404c22297bf9a28880f35
Emmanium <pradoe.ep@gmail.com>
2018-12-04 16:42:29 -0700

    added delete request updated post request

--- index.js
+++ index.js
@@ -38,21 +38,56 @@ server.get('/api/users/:id', (req, res) => {
       res.json(posts)
     })
     .catch(err => {
-      res.status(500).json({message: `Couldn't retrieve posts`})
+      res
+        .status(500)
+        .json({error: `Couldn't retrieve posts`})
     })
 });
 
-//creates a post
+//creates a user
 server.post('/api/users', (req, res) => {
   const user = req.body;
-  userDb.insert(user)
-    .then(userId => {
-      res.json(userId)
+  
+  if (user.name) {
+    userDb.insert(user)
+      .then(userId => {
+        res.json(userId)
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({error: `Couldn't add user`})
+      })
+  } else {
+    res
+      .status(400)
+      .json({errorMessage: 'Please include a username'})
+  }
+});
+
+//deletes a user
+server.delete('/api/users/:id', (req, res) => {
+  const { id } = req.params;
+
+  userDb.remove(id)
+    .then(count => {
+      if (count) {
+        res
+          .json({message: 'Successfully deleted'})
+      } else {
+        res
+          .status(404)
+          .json({message: 'User with specified ID does not exist'})
+      }
     })
     .catch(err => {
-      res.status(500).json({message: `Couldn't add user`})
+      res
+        .status(500)
+        .json({error: 'User could not be removed'})
     })
-})
+});
+
+//updates a user
 
 server.listen(PORT, () => {
   console.log(`Server is listening on PORT ${PORT}`)
