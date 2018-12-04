b79a5311267680475f7ba73e584a16a763952d6d
cferrell2 <cferrell12182@gmail.com>
2018-12-04 00:23:28 -0500

    create .delete route handler to delete a user from database

--- server.js
+++ server.js
@@ -33,15 +33,49 @@ server.get('/api/users/:id', (req, res) =>{
 
 server.post('/api/users', (req,res) =>{
     const user = req.body;
-    userdb.insert(user)
-    .then(id =>{
-        res.json(id)
+    if(user.name){
+        userdb.insert(user)
+        .then(id =>{
+            res.json(id)
+        })
+        .catch(err =>{
+            res.status(500)
+            res.json({error: "Unable to add new user"})
+        })
+    }else{
+        res.status(400)
+        res.json({error: "missing name of user"})
+    }
+});
+
+server.delete('/api/users/:id', (req, res) =>{
+    const id = req.params.id;
+    userdb.get(id)
+    .then(user =>{
+        if(user){
+            userdb.remove(id)
+            .then(count =>{
+                if(count){
+                    res.status(200)
+                    res.json(user)
+                }else{
+                    res.status(404)
+                    res.json({message: "The user with the specified id does not exist"})
+                }
+        
+            })
+        }else{
+            res.status(404)
+            res.json({message: "The user with the specified id does not exist"})
+        }
     })
     .catch(err =>{
         res.status(500)
-        res.json({error: "Unable to add new user"})
+        res.json({error: "Unable to delete user"})
     })
 });
+
+
 
 
 //listener
