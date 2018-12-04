5aa356bb8205b73c83dcb3a1fdb5e5b7af50b7e3
Julie Jonak <juliejonak10@gmail.com>
2018-12-03 22:30:41 -0600

    BOILERPLATE: - index.js: Server started on port, express added - package.json: Created with nodemon and scripts added - yarn.lock: Yarn initialized

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const server = express();
+const PORT = 4100;
+
+//middleware
+
+
+//route handlers
+
+
+//listening
+server.listen(PORT, ()=> {
+    console.log(`Server is alive at ${PORT}`);
+})
