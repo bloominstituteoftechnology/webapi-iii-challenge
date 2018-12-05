9f41de988ad869d290f6beaefdf4a5a4c980a2e0
Colin Dismuke <colin.dismuke@gmail.com>
2018-12-05 15:31:11 -0600

    Built minimal app to display posts

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,28 +1,41 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import { Route, withRouter } from "react-router-dom";
+
+import Nav from "./components/Nav";
+import Posts from "./components/Posts";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:3000/api/posts/")
+      .then(res => {
+        this.setState({ posts: res.data });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Nav />
+        <Route
+          exact
+          path="/"
+          render={props => <Posts {...props} posts={this.state.posts} />}
+        />
       </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
---
+++ blog/src/components/Posts.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+
+import Post from "./Post";
+
+import styled from "styled-components";
+
+const PostsContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  margin-top: 20px;
+`;
+
+const PostContainer = styled.div`
+  flex: 0 1 20%;
+  min-width: 300px;
+  height: 300px;
+  /* background-color: #f6f5f7; */
+  border: 0;
+  border-radius: 3px;
+  box-shadow: 0 -1px 0 #e0e0e0, 0 0 2px rgba(0, 0, 0, 0.12),
+    0 2px 4px rgba(0, 0, 0, 0.24);
+  cursor: pointer;
+  margin: 10px;
+  padding: 15px;
+`;
+
+class Posts extends Component {
+  render() {
+    return (
+      <div>
+        <PostsContainer>
+          {this.props.posts.map(post => {
+            return (
+              <PostContainer>
+                <Post text={post.text} />
+              </PostContainer>
+            );
+          })}
+        </PostsContainer>
+      </div>
+    );
+  }
+}
+
+export default Posts;
