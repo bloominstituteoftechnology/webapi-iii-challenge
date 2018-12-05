4fb2eae966d4a00934b2c32f2ac7cad2b5845a2c
louislee7 <7.louislee@gmail.com>
2018-12-04 22:20:38 -0800

    added CRUD for posts

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const helmet = require('helmet');
 const logger = require('morgan');
 const customMW = require('./data/customMW')
 
-const db = require('./data/helpers/userDb.js')
+const userDb = require('./data/helpers/userDb')
+const postDb = require('./data/helpers/postDb')
 
 const server = express();
 const PORT = 5000;
@@ -16,11 +17,11 @@ server.use(customMW.uppercaseUser);
 
 //route handlers
 
-//POST
-server.post('/users/', (req, res) => {
+//POST new user
+server.post('/api/users/', (req, res) => {
     const newUser = req.body;
     if (newUser.name) {
-        db.insert(newUser)
+        userDb.insert(newUser)
             .then(id => {
                 res.json(id)
             })
@@ -35,9 +36,28 @@ server.post('/users/', (req, res) => {
     }
 })
 
-//GET
-server.get('/users', (req, res) => {
-    db.get()
+//POST new post
+server.post('/api/posts/', (req, res) => {
+    const newPost = req.body;
+    if (newPost.text && newPost.userId) {
+        postDb.insert(newPost)
+            .then(id => {
+                res.json(id)
+            })
+            .catch(err => {
+                res.status(500)
+                    .json({ error: "There was an error while saving the post to the database" })
+            })
+    }
+    else {
+        res.status(400)
+            .json({ errorMessage: "Please provide the text of the post and the user ID." })
+    }
+})
+
+//GET users
+server.get('/api/users', (req, res) => {
+    userDb.get()
         .then(posts => {
             res.json(posts);
         })
@@ -48,10 +68,9 @@ server.get('/users', (req, res) => {
         })
 })
 
-//GET one user's posts
-server.get('/users/:id', (req, res) => {
-    const { id } = req.params;
-    db.getUserPosts(id)
+//GET posts
+server.get('/api/posts', (req, res) => {
+    postDb.get()
         .then(p => {
             if (p) {
                 res.json(p);
@@ -68,13 +87,79 @@ server.get('/users/:id', (req, res) => {
         })
 });
 
-//DELETE
-server.delete('/users/:id', (req, res) => {
+//GET one user's posts
+server.get('/api/users/:id', (req, res) => {
     const { id } = req.params;
-    db.remove(id)
+    userDb.getUserPosts(id)
+        .then(p => {
+            if (p) {
+                res.json(p);
+            }
+            else {
+                res.status(404)
+                    .json({ message: "The user with the specified ID does not exist." })
+            }
+        })
+        .catch(err => {
+            res
+                .status(500)
+                .json({ message: "failed to get user" })
+        })
+});
+
+//GET one post
+server.get('/api/posts/:id', (req, res) => {
+    const { id } = req.params;
+    postDb.get(id)
+        .then(p => {
+            if (p) {
+                res.json(p);
+            }
+            else {
+                res.status(404)
+                    .json({ message: "The user with the specified ID does not exist." })
+            }
+        })
+        .catch(err => {
+            res
+                .status(500)
+                .json({ message: "failed to get user" })
+        })
+});
+
+//PUT user
+server.put('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+    const updatedUser = req.body;
+    if (updatedUser.name) {
+        userDb.update(id, updatedUser)
+            .then(count => {
+                if (count) {
+                    res.json({ message: "The user has been successfully updated." })
+                }
+                else {
+                    res.status(404)
+                        .json({ message: "The post with the specified ID does not exist." })
+                }
+            })
+            .catch(err => {
+                res.status(500)
+                    .json({ error: "The post information could not be modified." })
+            })
+    }
+    else {
+        res.status(400)
+            .json({ errorMessage: "Please provide name for the user." })
+    }
+})
+
+//DELETE user
+server.delete('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+    userDb.remove(id)
         .then(count => {
             if (count) {
-                res.json({ message: "User succesfully deleted."});
+                res.json({ message: "User succesfully deleted." });
             }
             else {
                 res.status(404)
