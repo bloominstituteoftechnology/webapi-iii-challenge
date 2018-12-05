34c06a4116a645dcfc0b6dce3a26fd247cb952e7
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2018-12-05 00:05:19 -0500

    added route to GET user by id added route to GET post by userId

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const helmet = require('helmet');
 const logger = require('morgan');
 
+const userDb = require('./data/helpers/userDb');
+const postDb = require('./data/helpers/postDb');
+
 const server = express();
 const PORT = 4040;
 
@@ -11,7 +14,40 @@ server.use(helmet());
 server.use(logger('dev'));
 
 server.get('/', (req, res) => {
-  res.status(404).json({ message: 'server is running' });
+  res.json({ message: 'server is running' });
+});
+
+// GET user by id
+server.get('/user/:id', (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+  userDb
+    .get(id)
+    .then(user => {
+      if (user) {
+        res.send(user);
+      } else {
+        res.status(404).json({message: 'user does not exist'});
+      }
+    })
+    .catch(err => {
+      res.status(500)
+      .json({ message: 'unable to fullfill request' });
+    });
+});
+
+// GET post by userId
+server.get('/posts/:userId', (req, res) => {
+  const { userId } = req.params
+    postDb
+    .get(userId)
+    .then(posts => {
+      res.send(posts);
+    })
+    .catch(err => {
+      res.status(500)
+      .send({ message: 'unable retrieve posts.' });
+    });
 });
 
 server.listen(PORT, () => {
