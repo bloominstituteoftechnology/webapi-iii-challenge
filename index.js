295da2253c0798e3f5988ff72252baba5ffa1016
Jorlost19 <39849563+Jorlost19@users.noreply.github.com>
2018-12-04 20:52:36 -0600

    Finished .put for users

--- index.js
+++ index.js
@@ -20,5 +20,41 @@ server.get('/api/posts', (req, res)=>{
         .catch(err => res.status(500).json({error: 'We have an unexpected error while retrieving your posts!'}))
 })
 
+server.get('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+    dbUsers.get(id)
+        .then(user => user? res.json(user) : res.status(404).json({error: 'There is no user with the specified ID!'}))
+        .catch(err => res.status(500).json({error: 'We have an unexpected error while retrieving your user!'}))
+})
+
+server.get('/api/posts/:id', (req, res) => {
+    const { id } = req.params;
+    dbPosts.get(id)
+        .then(post => post ? res.json(post) : res.status(404).json({error: 'There is no post with the specified ID!'}))
+        .catch(err => res.status(500).json({error: 'We have an unexpected error while retrieving your post!'}))
+})
+
+server.get('/api/users/:id/posts', (req, res) => {
+    const { id } = req.params;
+    dbUsers.getUserPosts(id)
+        .then(userPosts => userPosts.length ? res.json(userPosts) : res.json({message: 'This user has no post yet!'}))
+        .catch(err => res.status(500).json({error: "Something went wrong retrieving your user's posts"}))
+})
+
+server.post('/api/users', (req, res) => {
+    const { name } = req.body;
+    if(!name){res.status(400).json({error: 'Add a valid Name!'})}
+    dbUsers.insert({ name })
+        .then(idInfo => dbUsers.get(idInfo.id).then(user => resstatus(201).json(user)))
+        .catch(err => res.status(500).json({error: 'Something went wrong trying to add the new user!'}))
+})
+
+server.put('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+    const user = req.body;
+    dbUsers.update(id, user)
+        .then(count => count ? dbUsers.get(id).then(user => res.json(user)) : res.status(404).json({error: 'There is no user with the specified ID!'}))
+        .catch(err => res.status(500).json({error: "Something went wrong updating your user's info"}))
+})
 
 server.listen(PORT, () => console.log(`Server up and running on port: ${PORT}`))
