9c7e65ad5d4a3e6b4730c48d53b80f3f25757bc2
TheGameFreak720 <luis.alvarez@commithub.com>
2018-12-05 18:31:17 -0500

    did get request for all users

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const morgan = require('morgan');
-const db = require('./data/dbConfig.js');
 
+const userDB = require("./data/helpers/userDb");
+const postDB = require("./data/helpers/postDB");
 
 const server = express();
 const PORT = 3000;
 
+server.use(express.json());
 server.use(morgan('dev'));
+
+server.get("/users", (req, res) => {
+    userDB.get()
+      .then(users => {
+        res.json(users);
+      })
+      .catch(err => {
+        res.status(500).json({ error: "Error getting users" });
+      });
+});
+
 
 server.listen(PORT, err => {
     console.log(`Server listening on port ${PORT}`);
