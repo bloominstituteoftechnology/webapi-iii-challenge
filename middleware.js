b03c4ec4030d35ea6d503ca28e755657402fb622
Michael Trew <michaeltrew@Michaels-iMac.attlocal.net>
2018-12-04 23:25:05 -0500

    gatekeeper

--- middleware.js
+++ middleware.js
@@ -1,7 +1,10 @@
-const capitalize = (req,res,next) => {
-  req.body.name =
-    req.body.name.toUpperCase();
-      next();
-}
+const capitalize = (req, res, next) => {
+  const user = req.body.name;
+  if (user = req.body.name.toUpperCase()) {
+    next()
+  } else {
+    res.status(400).json({ message: "name needs to be uppercased" })
+  };
+};
 
 module.exports = capitalize;
