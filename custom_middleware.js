cb0c66d0bcdea390d38dcfb28a1a2ed875ed1d00
Kendra Williams <kmonique.williams@gmail.com>
2018-12-04 23:33:53 -0500

    type error corrected

--- custom_middleware.js
+++ custom_middleware.js
@@ -1,11 +1,15 @@
 const gatekeeper = (req, res, next) => {
    let username = req.body;
-   username.name = JSON.stringify(username.name);
-   if (username.name != username.name.toUpperCase()){
-      username.name = JSON.parse(username.name.toUpperCase());
-      next();
+   if(username.name) {
+      username.name = JSON.stringify(username.name);
+      if (username.name != username.name.toUpperCase()){
+         username.name = JSON.parse(username.name.toUpperCase());
+         next();
+      } else {
+         res.status(400).json({message: "error modifying name"})
+      }
    } else {
-      res.status(400).json({message: "error modifying name"})
+      next();
    }
 }
 
