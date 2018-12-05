83b1d0fa159791f245191de6460c1af1ceff0214
BrandonMoll <brandonmoll85@gmail.com>
2018-12-04 21:04:39 -0600

    GET and GET by ID endpoints working for users

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const logger = require('morgan');
 const server = express();
 
 const PORT = 4000;
+const userDB = require('./data/helpers/userDb')
 
 server.use(
     express.json(),
@@ -13,9 +14,32 @@ server.use(
 )
 
 //endpoints
+server.get('/users', (req, res) => {
+    userDB.get()
+    .then(users => {
+        res.json(users)
+    })
+    .catch(err => {
+        res.status(500).json({message: 'Error getting users'})
+    })
+});
 
+server.get('/users/:id', (req, res) => {
+    const {id} = req.params;
+    userDB.get(id)
+    .then(user => {
+        if(user) {
+            res.json(user)
+        } else {
+            res.status(404).json({message: 'That user ID does not exist'})
+        }
+    })
+    .catch(err => {
+        res.status(500).json({message: 'Error finding that Users Posts'})
+    })
+})
 //listener
 
 server.listen(PORT, () => {
-    console.log('server is running')
+    console.log(`server is listening on port ${PORT}`)
 })
