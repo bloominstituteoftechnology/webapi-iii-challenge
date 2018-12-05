07cff17bb13c7b30aff73a240edaf14e6c7d899d
Willie Yturralde <willieino@hotmail.com>
2018-12-04 19:46:17 -0800

    modified apis

---
+++ customMiddleware.js
@@ -0,0 +1,14 @@
+module.exports.gatekeeper = (req, res, next) => {
+    const password = req.query.password;
+    if (password === 'ferret') {
+        next();
+    } else {
+        res.status(400).json({
+            message: "Your password is incorrect"
+        })
+    }
+}
+
+ 
+ 
+ 
