48077c1836f75900af21e0c990f1116ff1faf576
Michael Trew <michaeltrew@Michaels-iMac.attlocal.net>
2018-12-04 23:25:45 -0500

    edit

--- middleware.js
+++ middleware.js
@@ -1,6 +1,6 @@
 const capitalize = (req, res, next) => {
   const user = req.body.name;
-  if (user = req.body.name.toUpperCase()) {
+  if (user === req.body.name.toUpperCase()) {
     next()
   } else {
     res.status(400).json({ message: "name needs to be uppercased" })
