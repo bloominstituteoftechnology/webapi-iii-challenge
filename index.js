63a93e24fd528e54c7d76f782c8116fbe1d0d7df
Jesse Anderson <jesseanderson@Jesses-MacBook-Pro.local>
2018-12-04 23:36:05 -0600

    PUT method is done

--- index.js
+++ index.js
@@ -13,13 +13,21 @@ const port = 5050;
 
 // ================= CUSTOM MIDDLEWARE =================
 const upperCaseName = (req, res, next) => {
+  //===== CHECK IF THE FIRST LETTER OF THE NAME IS CAPITALIZED =====
+  //   if (req.method === 'POST' || req.method === 'PUT') {
+  //     const { name } = req.body;
+  //     if (name[0] === name[0].toUpperCase()) {
+  //       next();
+  //     } else {
+  //       res.status(400).json({ message: 'Name must be capitalized' });
+  //     }
+  //   } else {
+  //     next();
+  //   }
+  //===== TRANSFORM THE NAME TO BE ALL UPPERCASE AND SEND IT THROUGH =====
   if (req.method === 'POST' || req.method === 'PUT') {
-    const { name } = req.body;
-    if (name[0] === name[0].toUpperCase()) {
-      next();
-    } else {
-      res.status(400).json({ message: 'Name must be capitalized' });
-    }
+    req.body.name = req.body.name.toUpperCase();
+    next();
   } else {
     next();
   }
@@ -80,14 +88,16 @@ server.post('/users', (req, res) => {
   }
 });
 
-//DELETE
+// DELETE
 server.delete('/users/:id', (req, res) => {
   const { id } = req.params;
   userDB
     .remove(id)
     .then(count => {
       if (count) {
-        res.json(count);
+        res.json({
+          message: `User with the ID of ${id} has been deleted successfully`
+        });
       } else {
         res
           .status(404)
@@ -97,6 +107,32 @@ server.delete('/users/:id', (req, res) => {
     .catch(err => {
       res.status(500).json({ error: 'Could not delete' });
     });
+});
+
+// PUT
+server.put('/users/:id', (req, res) => {
+  const { id } = req.params;
+  const user = req.body;
+  if (user.name) {
+    userDB
+      .update(id, user)
+      .then(count => {
+        if (count) {
+          userDB.get(id).then(user => {
+            res.json(user);
+          });
+        } else {
+          res
+            .status(404)
+            .json({ message: `User with an ID of ${id} does not exist` });
+        }
+      })
+      .catch(err => {
+        res.status(500).json({ error: 'The user data could not be found' });
+      });
+  } else {
+    res.status(400).json({ message: 'Please provide a name' });
+  }
 });
 
 // LISTENING
