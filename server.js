12344988ca6bc112aab1929d33ff47e5965f53eb
kammagik <kammagik@yahoo.com>
2018-12-04 19:34:41 -0800

    get users list api

--- server.js
+++ server.js
@@ -1,1 +1,27 @@
 const express = require('express');
+const users = require('./data/helpers/userDb');
+
+const server = express();
+const PORT = 3333;
+
+server.use(express.json());
+
+
+
+server.get('/', (req, res) => {
+    res.json({message: "success"})
+});
+
+server.get('/api/users', (req, res) => {
+    users.get()
+    .then(getUser => {
+        res.status(200).json(getUser);
+    })
+    .catch (err => {
+        res.status(500).json({message: "user not found"})
+    })
+})
+
+server.listen(PORT, err => {
+    console.log(`server is listening on port ${PORT}`)
+});
