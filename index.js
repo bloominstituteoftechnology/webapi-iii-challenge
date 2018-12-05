7d93e15e4551e80c12d3e02a90b7ef5b3eda1051
Rob Salzberg <robsalzberg@gmail.com>
2018-12-04 22:21:58 -0500

    Added user endpoints

--- index.js
+++ index.js
@@ -13,6 +13,39 @@ server.use(express.json());
 server.use(logger('dev'));
 server.use(helmet());
 
+//user endpoints
+
+server.get('/user', (req, res) => {
+    userDB
+      .get()
+      .then(users => {
+        res.json(users);
+      })
+      .catch(err => {
+        res.status(500).json({ error: 'Error getting user' });
+      });
+  });
+  
+  server.get('/user/:id', (req, res) => {
+    const { id } = req.params;
+    userDB
+      .get(id)
+      .then(user => {
+        if (user) {
+          res.json(user);
+        } else {
+          res
+            .status(404)
+            .json({ message: 'User with specified ID is not found' });
+        }
+      })
+      .catch(err => {
+        res.status(500).json({ error: 'Could not get user' });
+      });
+  });
+  
+
+//server listening
 
 server.listen(port, () => {
     console.log(`Server is up and running on port ${port}`);
