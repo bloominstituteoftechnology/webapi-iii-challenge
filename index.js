76e64621c1be59555a44c1cd689bf5c568bf5add
Levi Appenfelder <levi.appenfelder@gmail.com>
2018-12-04 21:04:33 -0700

    GET implemented for ALL Users and Posts

--- index.js
+++ index.js
@@ -5,4 +5,27 @@ const userDB = require('./data/helpers/userDb.js');
 const server = express();
 const NUM = 4444;
 
+server.get('/api/users', (req,res) => {
+    userDB.get()
+    .then((users) => {res.json(users)})
+    .catch(err => {
+        res
+        .status(500)
+        .json({message: 'Unable to get users'})})
+} )
+
+server.get('/api/posts', (req,res) => {
+    postDB.get()
+    .then((posts) => {res.json(posts)})
+    .catch(err => {
+        res
+        .status(500)
+        .json({message: 'Unable to get posts'})
+    })
+})
+
+server.get('/api/users/:id'), (req,res) => {
+    
+}
+
 server.listen(NUM, () => console.log(`listening on port ${NUM}`))
