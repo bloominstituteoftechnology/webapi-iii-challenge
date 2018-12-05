5a7adb7d848d4f7d31c05ca77a3073f0f03826b4
Kyle Baker <k@gnomiq.com>
2018-12-04 20:31:53 -0800

    uppercase middleware

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ const app = express();
 
 app
     .use(bodyParser.json())
+    .use(function(req, res, next) {
+        if (req.body.name) {
+            req.body.name = req.body.name.toUpperCase()
+        }
+
+        next()
+    })
     .use('/users', users)
     .use('/posts', posts);
 
