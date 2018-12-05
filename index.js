d570f8b958b78f029ea871c8025514cc9d0d0827
Abdiel Fernandez <fernandezabdiel@gmail.com>
2018-12-05 00:15:39 -0500

    created the CRUD enpoints for the users

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const server = express();
 const cors = require('cors')
 
 const postDb = require('./data/helpers/postDb')
+const userDB = require('./data/helpers/userDb')
 
 const PORT = 5000;
 
@@ -100,7 +101,85 @@ server.delete('/api/posts/:id', (req, res) => {
 })
 
 //CRUD endpoints for users
+//getting a list of all the users
+server.get('/api/users', (req, res) => {
+  userDB.get()
+    .then( users => {
+      res.status(200).json(users)
+    }).catch( err => {
+      res.status(400).json({message: 'unable to get the users'})
+    })
+})
 
+//getting user by ID
+server.get('/api/users/:id', (req, res) => {
+  const {id} = req.params;
+
+  userDB.get(id)
+    .then( user => {
+      res.status(200).json(user)
+    }).catch( err => {
+      res.status(400).json({message: 'unable to get user'})
+    })
+})
+
+//getting user posts by ID
+server.get('/api/users/posts/:userId', (req, res) => {
+  const user = req.params;
+  userDB.getUserPosts(user.userId)
+    .then( posts => {
+      res.status(200).json(posts)
+    }).catch( err => {
+      res.status(400).json({message: 'unable to get user posts'})
+    })
+})
+
+//creating a new user
+server.post('/api/users', (req, res) => {
+  const user = req.body;
+
+  userDB.insert(user)
+    .then( newUser => {
+      userDB.get(newUser.id)
+        .then( user => {
+          res.status(200).json(user)
+        })
+    }).catch( err => {
+      res.status(400).json({message: 'unable to create a new user'})
+    })
+})
+
+//updating a user
+server.put('/api/users/:id', (req, res) => {
+  const {id} = req.params;
+  const changes = req.body;
+
+  userDB.update(id, changes)
+    .then( count => {
+      if(count) {
+        res.status(200).json({message: 'successfully updated the user'})
+      } else {
+        res.status(500).json({message: 'failed to update the user'})
+      }
+    }).catch( err => {
+      res.status(400).json({message: 'unable to processs an update'})
+    })
+})
+
+server.delete('/api/users/:id', (req, res) => {
+  const { id } = req.params;
+  
+  userDB.remove(id)
+    .then( count => {
+      if (count) {
+        res.status(200).json({message: 'successfully deleted the user'})
+      } else {
+        res.status(500).json({message: 'unable to delete the user'})
+      }
+    }).catch( err => {
+      res.status(400).json({message: 'unable to process a delete'})
+    })
+})
 
 server.listen(PORT, () => {
   console.log(`listening on port ${PORT}`)
