8953659ab86cbbf49aeb7cddbb2fe3d44e07668f
Michael Trew <michaeltrew@Michaels-iMac.attlocal.net>
2018-12-04 22:16:58 -0500

    custom mw added

--- index.js
+++ index.js
@@ -3,9 +3,19 @@ const server = express();
 const PORT = 5000;
 const helmet = require('helmet');
 const morgan = require('morgan');
+const capitalize = require('./middleware.js')
 
 server.use(
   express.json(),
   morgan('tiny'),
   helmet()
 )
+
+server.use(capitalize);
+
+
+
+
+server.listen(port,() => {
+  console.log(`Server is listenening on port ${PORT}`)
+});
---
+++ middleware.js
@@ -0,0 +1,7 @@
+const capitalize = (req,res,next) => {
+  req.body.name =
+    req.body.name.toUpperCase();
+      next();
+}
+
+module.exports = capitalize;
