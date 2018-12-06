4f8f2e863536ac148c0d9771aa1887540da49bbe
Abdiel Fernandez <fernandezabdiel@gmail.com>
2018-12-06 15:13:37 -0500

    refactored the custom middlerware that I created

--- customMW.js
+++ customMW.js
@@ -1,15 +1,16 @@
 const toUpperCase = (req, res, next) => {
   const { name } = req.body;
 
-  let currentName = name.split(' ');
-
-  let UpperCaseName = currentName.map( word => {
-    return word.charAt(0).toUpperCase() + word.slice(1)
-  }).join(' ')
-
-  console.log(UpperCaseName)
-  req.body.name = UpperCaseName
+  if (name) {
+    let currentName = name.split(' ');
+    let UpperCaseName = currentName.map( word => {
+      return word.charAt(0).toUpperCase() + word.slice(1)
+    }).join(' ')
+  
+    console.log(UpperCaseName)
+    req.body.name = UpperCaseName
+  }
   next()
 }
 
-module.exports.toUpperCase = toUpperCase;
+module.exports = toUpperCase;
