1fa830a038d7ccd61faca839328d38af5e270854
vy3191 <venkyafhs@gmail.com>
2018-12-05 19:26:17 -0500

    Added squlite3 Added knex Modified index.js

--- index.js
+++ index.js
@@ -3,11 +3,17 @@ const helmet = require('helmet');
 const logger = require('morgan');
 
 const server = express();
-server.use(express.json());
-
 const PORT = '4500';
 
+server.use(express.json());
+server.use(helmet());
+server.use(logger('dev'));
+
+
+
 server.get('/', (req,res) => {
+     res.status(404)
+        .send({errorMessage: "Not able to get it"})
      console.log('server is running now');
 });
 
