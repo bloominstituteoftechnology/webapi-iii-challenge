f5288f7c686706ed3d89562ffc413a5b48afbd0d
rlmclaughlin <rlmclaughlin@yahoo.com>
2018-12-06 22:05:42 -0600

    added cors and fixed a mistake in index.js. MVP finished

--- index.js
+++ index.js
@@ -2,17 +2,14 @@ const express = require('express')
 const PORT = 4000;
 const helmet = require("helmet");
 const logger = require("morgan");
-
+const cors = require('cors');
 const server = express()
 const userRouter = require("./routers/users")
 const postsRouter = require("./routers/posts")
 
-
-
-server.use(express.json(), logger("tiny"), helmet())
+server.use(express.json(), logger("tiny"), helmet(), cors())
 server.use('/api/users', userRouter)
-server.use('/api/posts', postRouter)
-
+server.use('/api/posts', postsRouter)
 
 server.listen(PORT, () => {
     console.log(`server is up and running on port ${PORT}`)
