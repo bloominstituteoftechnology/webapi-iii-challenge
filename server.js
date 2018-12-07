039bf0235548e8ac2125c44ca36231d9704910da
Andres Rivera <andres.rivera.toro@gmail.com>
2018-12-06 22:52:08 -0500

    Add GET methods and endpoints for users

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const userDb = require('./data/helpers/userDb');
+
+const server = express();
+
+server.use(express.json(), cors(), helmet(), morgan('dev'));
+
+server.get('/api/users', (req, res) => {
+  userDb
+    .get()
+    .then(users => res.json(users))
+    .catch((err) => {
+      res.status(500).json({ error: 'Users could not be retrieved.' });
+    });
+});
+
+server.get('/api/users/:id', (req, res) => {
+  const { id } = req.params;
+  userDb
+    .get(id)
+    .then((user) => {
+      if (user) {
+        res.json(user);
+      } else {
+        res.status(404).json({ message: 'The user with the specified ID does not exist.' });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ error: 'User could not be retrieved.' });
+    });
+});
+
+server.get('/api/users/:id/posts', (req, res) => {
+  const { id } = req.params;
+  userDb
+    .get(id)
+    .then((user) => {
+      if (user) {
+        userDb.getUserPosts(id).then((posts) => {
+          if (posts.length) {
+            res.json(posts);
+          } else {
+            res.status(404).json({ message: 'The user does not have any posts.' });
+          }
+        });
+      } else {
+        res.status(404).json({ message: 'The user with the specified ID does not exist.' });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ error: 'User posts could not be retrieved.' });
+    });
+});
+
+module.exports = server;
