e2bf78b78aa325de236627e717bf4172eeeb6292
Jason Hedrick <jasonleehedrick@gmail.com>
2018-12-06 22:03:06 -0800

    Got the posts to show up properly now. Fixed issue of match.params.id not equating because its a string not an integer whereas user.id is an integer. Posts wherent working because I was waiting on a return from a function which called a promise without properly waiting for that function. Fixed it by moving the axios.get into the UserCard component.

--- client/src/components/UserCard.js
+++ client/src/components/UserCard.js
@@ -1,36 +1,48 @@
 import React from 'react';
+import axios from 'axios';
 
 class UserCard extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            id: -1,
+            id: this.props.match.params.id,
             user: {},
             posts: {}
         }
     }
 
     componentDidMount() {
-        const tempPosts = this.props.getUserPosts(this.props.match.params.id);
-        console.log(tempPosts);
-        this.setState({
-            id: this.props.match.params.id,
-            user: this.props.users.find((user) => {if (user.id === this.props.match.params.id) return user}),
-            posts: tempPosts
+        const user = this.props.users.find((user) => {
+            if(user.id.toString() === this.state.id) {
+                return user
+            }
+        });
+
+        axios.get(`http://localhost:4000/api/users/${this.state.id}/posts`)
+        .then((posts) => {
+            this.setState({
+                user: user,
+                posts: posts.data
+            })
         })
+        .catch((error) => {
+            console.log(`Server returned error of: ${error}`)
+        })
+
+        
         
     }
 
     render() {
-        if(this.id === -1) {
+        if(!this.state.user.name) {
             return <h1>Loading User....</h1>
         }
         else {
             return (
                 <div className='user-card-container'>
-                    <div className='user-card-namet'></div><br />
+                    <div className='user-card-namet'>{this.state.user.name}</div><br />
                     {this.state.posts.map((post) => {
-                        return <div key={post.id}>{post.text}</div>
+                        return <p>{post.text}</p>
                     })}
                 </div>
             )
