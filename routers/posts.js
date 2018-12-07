0011d8e99af9c3e9ec2f3eb5f5ba020746e82ac0
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2018-12-06 23:38:30 -0500

    added create post feature

--- routers/posts.js
+++ routers/posts.js
@@ -3,7 +3,7 @@ const postDb = require('../data/helpers/postDb');
 
 const router = express.Router()
 
-// /api/posts/:userId
+// GET /api/posts/:userId
 router.get('/:userId', (req, res) => {
   const { userId } = req.params
     postDb
@@ -17,30 +17,30 @@ router.get('/:userId', (req, res) => {
     });
 });
 
-// /api/posts/
-// router.post('/', (req, res) => {
-//   const post = req.body;
-//   if (post.text && post.postedBy) {
-//     postDb
-//       .insert(post)
-//       .then(idInfo => {
-//         postDb.get(idInfo.id)
-//         .then(post => {
-//           res
-//             .status(201)
-//             .json(idInfo);
-//         });
-//       })
-//       .catch(err => {
-//         res
-//           .status(500)
-//           .json({ message: 'failed to insert post in db' });
-//       });
-//   } else {
-//     res
-//       .status(400)
-//       .json({ errorMessage: 'please provide text and postedBy for post' });
-//   }
-// });
+// CREATE /api/posts/create
+router.post('/create', (req, res) => {
+  const post = req.body;
+  if (post.text) {
+    postDb
+      .insert(post)
+      .then(idInfo => {
+        postDb.get(idInfo.id)
+        .then(post => {
+          res
+            .status(201)
+            .json(idInfo);
+        });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ message: 'failed to insert post in db' });
+      });
+  } else {
+    res
+      .status(400)
+      .json({ errorMessage: 'please provide text and postedBy for post' });
+  }
+});
 
 module.exports = router;
