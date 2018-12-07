c00f74fe8278da7cd56bf81f02bab960c7775e00
Tommmy Collison <tommy@collison.ie>
2018-12-06 19:56:23 -0800

    get users finally working

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 const express = require('express');
 const server = express();
-const PORT = 6060;
+const PORT = 8080;
+const userDb = require('./data/helpers/userDb')
 
 server.use(express.json());
 
 console.log('let us get started');
 
 server.get('/', (req, res) => {
-    res.send('hi there from inside an initial get function')
+    res.send(`hi there from inside an initial get function, on port ${PORT}`)
+})
+
+server.get('/api/users', (req, res) => {
+    userDb.get()
+    .then(users => {
+        res.json(users)
+    })
+    .catch(err => {
+        res.status(500)
+        res.json(`Huh, can't find those`)
+    })
 })
 
 
+// *>*>*>*>*>*>*>*>*>*>*>*>*>*>*>*>*>*>*>*>*>*>*>*
 server.listen(PORT, () => {
   console.log(`server is alive on port ${PORT}`)
 })
