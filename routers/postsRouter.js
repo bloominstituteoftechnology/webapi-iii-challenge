aed87e3f827b906b0a2036fd61074bc9cae495fd
cferrell2 <cferrell12182@gmail.com>
2018-12-06 23:21:53 -0500

    create .put route handler to update  a specific post within the database

--- routers/postsRouter.js
+++ routers/postsRouter.js
@@ -69,4 +69,32 @@ router.delete('/:id', (req,res) =>{
     })
 });
 
+//Update a specified post
+router.put('/:id', (req,res) =>{
+    const id = req.params.id;
+    const post = req.body;
+    if(post.userId && post.text){
+        postdb.update(id, post)
+        .then(count =>{
+            if(count){
+                postdb.get(id)
+                .then(post =>{
+                    res.json(post)
+                })
+            }else{
+                res.status(404)
+                res.json({error: "There is no post associated with the specified id"})
+            }
+        })
+        .catch(err =>{
+            res.status(500)
+            res.json({error: "Unable to update specified post"})
+        })
+    }else{
+        res.status(400)
+        res.json({error: "Post is missing the userId or the text"})
+    }
+
+});
+
 module.exports = router;
