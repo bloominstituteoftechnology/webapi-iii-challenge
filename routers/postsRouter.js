93172984add4bf492ba8a61bcebd62f70e3d1857
cferrell2 <cferrell12182@gmail.com>
2018-12-06 21:56:41 -0500

    create .get route handler to retrieve a post with the specified id from the database

--- routers/postsRouter.js
+++ routers/postsRouter.js
@@ -5,8 +5,28 @@ const express = require('express')
 const router = express.Router();
 
 //Middleware 
+//...
+
 
 //Route Handlers
+//***POSTS ROUTE HANDLERS *** */
+//Get post with the specified ID
+router.get('/:id', (req,res) =>{
+    const id = req.params.id;
+    postdb.get(id)
+    .then(posts =>{
+        if(posts){
+            res.json(posts)
+        }else{
+            res.status(404)
+            res.json({error: "There is no post associated with the specified id"})
+        }
+    })
+    .catch(err =>{
+        res.status(500)
+        res.json({error: "Unable to retrieve posts"})
+    });
+});
 
 
 module.exports = router;
