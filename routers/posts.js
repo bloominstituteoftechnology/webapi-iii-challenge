4e598118b96046d84340cd4df68b928640119928
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2018-12-06 20:32:43 -0500

    added post insert feature

--- routers/posts.js
+++ routers/posts.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const postDb = require('../data/helpers/postDb');
 
 const router = express.Router()
 
@@ -15,5 +16,31 @@ router.get('/:userId', (req, res) => {
       .send({ message: 'unable retrieve posts.' });
     });
 });
+
+// /api/posts/
+// router.post('/', (req, res) => {
+//   const post = req.body;
+//   if (post.text && post.postedBy) {
+//     postDb
+//       .insert(post)
+//       .then(idInfo => {
+//         postDb.get(idInfo.id)
+//         .then(post => {
+//           res
+//             .status(201)
+//             .json(idInfo);
+//         });
+//       })
+//       .catch(err => {
+//         res
+//           .status(500)
+//           .json({ message: 'failed to insert post in db' });
+//       });
+//   } else {
+//     res
+//       .status(400)
+//       .json({ errorMessage: 'please provide text and postedBy for post' });
+//   }
+// });
 
 module.exports = router;
