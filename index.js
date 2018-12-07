df0f07ac7f6d272845c52b43e4a318ad524c29a0
Jesse Anderson <jesseanderson@Jesses-MacBook-Pro.local>
2018-12-06 20:51:49 -0600

    created userRoutes and postRoutes files and I am now using express.Routes for user endpoints.

--- index.js
+++ index.js
@@ -9,7 +9,12 @@ const postDB = require('./data/helpers/postDb');
 // console.log(postDB);
 
 const server = express();
+
 const port = 5050;
+
+const userRoutes = require('./userRoutes');
+
+const postRoutes = require('./postRoutes');
 
 // ================= CUSTOM MIDDLEWARE =================
 const upperCaseName = (req, res, next) => {
@@ -39,101 +44,106 @@ server.use(logger('dev'));
 server.use(helmet());
 server.use(upperCaseName);
 
+// Routes
+
+server.use('/users', userRoutes);
+server.use('/posts', postRoutes);
+
 // Endpoints
 
 // GET
-server.get('/users', (req, res) => {
-  userDB
-    .get()
-    .then(users => {
-      res.json(users);
-    })
-    .catch(err => {
-      res.status(500).json({ error: 'Error getting user' });
-    });
-});
+// server.get('/users', (req, res) => {
+//   userDB
+//     .get()
+//     .then(users => {
+//       res.json(users);
+//     })
+//     .catch(err => {
+//       res.status(500).json({ error: 'Error getting user' });
+//     });
+// });
 
-server.get('/users/:id', (req, res) => {
-  const { id } = req.params;
-  userDB
-    .get(id)
-    .then(user => {
-      if (user) {
-        res.json(user);
-      } else {
-        res
-          .status(404)
-          .json({ message: 'User with specified ID is not found' });
-      }
-    })
-    .catch(err => {
-      res.status(500).json({ error: 'Could not get user' });
-    });
-});
+// server.get('/users/:id', (req, res) => {
+//   const { id } = req.params;
+//   userDB
+//     .get(id)
+//     .then(user => {
+//       if (user) {
+//         res.json(user);
+//       } else {
+//         res
+//           .status(404)
+//           .json({ message: 'User with specified ID is not found' });
+//       }
+//     })
+//     .catch(err => {
+//       res.status(500).json({ error: 'Could not get user' });
+//     });
+// });
 
 // POST
-server.post('/users', (req, res) => {
-  const user = req.body;
-  if (user.name) {
-    userDB
-      .insert(user)
-      .then(user => {
-        res.json(user);
-      })
-      .catch(err => {
-        res.status(500).json({ error: 'Could not add user' });
-      });
-  } else {
-    res.status(400).json({ message: 'Please make sure a name is entered' });
-  }
-});
+// server.post('/users', (req, res) => {
+//   const user = req.body;
+//   if (user.name) {
+//     userDB
+//       .insert(user)
+//       .then(user => {
+//         res.json(user);
+//       })
+//       .catch(err => {
+//         res.status(500).json({ error: 'Could not add user' });
+//       });
+//   } else {
+//     res.status(400).json({ message: 'Please make sure a name is entered' });
+//   }
+// });
 
 // DELETE
-server.delete('/users/:id', (req, res) => {
-  const { id } = req.params;
-  userDB
-    .remove(id)
-    .then(count => {
-      if (count) {
-        res.json({
-          message: `User with the ID of ${id} has been deleted successfully`
-        });
-      } else {
-        res
-          .status(404)
-          .json({ error: 'user with specified ID does not exist' });
-      }
-    })
-    .catch(err => {
-      res.status(500).json({ error: 'Could not delete' });
-    });
-});
+// server.delete('/users/:id', (req, res) => {
+//   const { id } = req.params;
+//   userDB
+//     .remove(id)
+//     .then(count => {
+//       if (count) {
+//         res.json({
+//           message: `User with the ID of ${id} has been deleted successfully`
+//         });
+//       } else {
+//         res
+//           .status(404)
+//           .json({ error: 'user with specified ID does not exist' });
+//       }
+//     })
+//     .catch(err => {
+//       res.status(500).json({ error: 'Could not delete' });
+//     });
+// });
 
 // PUT
-server.put('/users/:id', (req, res) => {
-  const { id } = req.params;
-  const user = req.body;
-  if (user.name) {
-    userDB
-      .update(id, user)
-      .then(count => {
-        if (count) {
-          userDB.get(id).then(user => {
-            res.json(user);
-          });
-        } else {
-          res
-            .status(404)
-            .json({ message: `User with an ID of ${id} does not exist` });
-        }
-      })
-      .catch(err => {
-        res.status(500).json({ error: 'The user data could not be found' });
-      });
-  } else {
-    res.status(400).json({ message: 'Please provide a name' });
-  }
-});
+// server.put('/users/:id', (req, res) => {
+//   const { id } = req.params;
+//   const user = req.body;
+//   if (user.name) {
+//     userDB
+//       .update(id, user)
+//       .then(count => {
+//         if (count) {
+//           userDB.get(id).then(user => {
+//             res.json(user);
+//           });
+//         } else {
+//           res
+//             .status(404)
+//             .json({ message: `User with an ID of ${id} does not exist` });
+//         }
+//       })
+//       .catch(err => {
+//         res.status(500).json({ error: 'The user data could not be found' });
+//       });
+//   } else {
+//     res.status(400).json({ message: 'Please provide a name' });
+//   }
+// });
 
 // LISTENING
 server.listen(port, () => {
---
+++ userRoutes.js
@@ -0,0 +1,95 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  userDB
+    .get()
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => {
+      res.status(500).json({ error: 'Error getting user' });
+    });
+});
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  userDB
+    .get(id)
+    .then(user => {
+      if (user) {
+        res.json(user);
+      } else {
+        res
+          .status(404)
+          .json({ message: 'User with specified ID is not found' });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ error: 'Could not get user' });
+    });
+});
+
+router.post('/', (req, res) => {
+  const user = req.body;
+  if (user.name) {
+    userDB
+      .insert(user)
+      .then(user => {
+        res.json(user);
+      })
+      .catch(err => {
+        res.status(500).json({ error: 'Could not add user' });
+      });
+  } else {
+    res.status(400).json({ message: 'Please make sure a name is entered' });
+  }
+});
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  userDB
+    .remove(id)
+    .then(count => {
+      if (count) {
+        res.json({
+          message: `User with the ID of ${id} has been deleted successfully`
+        });
+      } else {
+        res
+          .status(404)
+          .json({ error: 'user with specified ID does not exist' });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ error: 'Could not delete' });
+    });
+});
+
+router.put('/:id', (req, res) => {
+  const { id } = req.params;
+  const user = req.body;
+  if (user.name) {
+    userDB
+      .update(id, user)
+      .then(count => {
+        if (count) {
+          userDB.get(id).then(user => {
+            res.json(user);
+          });
+        } else {
+          res
+            .status(404)
+            .json({ message: `User with an ID of ${id} does not exist` });
+        }
+      })
+      .catch(err => {
+        res.status(500).json({ error: 'The user data could not be found' });
+      });
+  } else {
+    res.status(400).json({ message: 'Please provide a name' });
+  }
+});
+
+module.exports = router;
