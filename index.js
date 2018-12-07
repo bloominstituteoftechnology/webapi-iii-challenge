495ac7c27373b0154800fdfeb7613dfb972c5016
Jorlost19 <39849563+Jorlost19@users.noreply.github.com>
2018-12-06 21:18:23 -0600

    implemented router for both resources refactored middleware

--- index.js
+++ index.js
@@ -1,94 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 
-const dbUsers = require('./data/helpers/userDb');
-const dbPosts = require('./data/helpers/postDb');
+const postsRouter = require('./posts-router');
+const usersRouter = require('./users-router');
 
 const server = express();
 const PORT = 5050;
 
-const MW = require('./middleware');
-server.use(express.json(), cors(), MW.toTitleCase);
+server.use(express.json(), cors());
+server.use('/api/posts', postsRouter);
+server.use('/api/users', usersRouter);
 
-server.get('/api/users', (req, res)=>{
-    dbUsers.get()
-        .then(users => users.length ? res.json(users) : res.json({message: "We don't have any user for you right now, please try again later!"}))
-        .catch(err => res.status(500).json({error: 'We have an unexpected error while retrieving your users!'}))
-})
-
-server.get('/api/posts', (req, res)=>{
-    dbPosts.get()
-        .then(posts => posts.length ? res.json(posts) : res.json({message: "We don't have any post for you right now, please try again later!"}))
-        .catch(err => res.status(500).json({error: 'We have an unexpected error while retrieving your posts!'}))
-})
-
-server.get('/api/users/:id', (req, res) => {
-    const { id } = req.params;
-    dbUsers.get(id)
-        .then(user => user? res.json(user) : res.status(404).json({error: 'There is no user with the specified ID!'}))
-        .catch(err => res.status(500).json({error: 'We have an unexpected error while retrieving your user!'}))
-})
-
-server.get('/api/posts/:id', (req, res) => {
-    const { id } = req.params;
-    dbPosts.get(id)
-        .then(post => post ? res.json(post) : res.status(404).json({error: "There is no post with the specified ID"}))
-        .catch(err => res.status(500).json({error: 'Something went wrong retrieving your post!'}))        
-})
-
-server.get('/api/users/:id/posts', (req, res) => {
-    const { id } = req.params;
-    dbUsers.getUserPosts(id)
-        .then(userPosts => userPosts.length ? res.json(userPosts) : res.json({message: 'This user has no post yet!'}))
-        .catch(err => res.status(500).json({error: "Something went wrong retrieving your user's posts"}))
-})
-
-server.post('/api/users', (req, res) => {
-    const { name } = req.body;
-    if(!name){res.status(400).json({error: 'Add a valid Name!'})}
-    dbUsers.insert({ name })
-        .then(idInfo => dbUsers.get(idInfo.id).then(user => res.status(201).json(user)))
-        .catch(err => res.status(500).json({error: 'Something went wrong trying to add the new user!'}))
-})
-
-server.post('/api/posts', (req, res) => {
-    const post  = req.body;
-    dbPosts.insert(post)
-        .then(idInfo => dbPosts.get(idInfo.id)
-            .then(post => post ? res.json(post) : res.status(404).json({error: "There is no post with the specified ID"})))
-        .catch(err => res.status(500).json({error: 'Something went wrong adding your post!'}))
-})
-
-server.put('/api/users/:id', (req, res) => {
-    const { id } = req.params;
-    const user = req.body;
-    dbUsers.update(id, user)
-        .then(count => count ? dbUsers.get(id)
-            .then(user => res.json(user)) : res.status(404).json({error: 'There is no user with the specified ID!'}))
-        .catch(err => res.status(500).json({error: "Something went wrong updating your user's info"}))
-})
-
-server.put('/api/posts/:id', (req, res) => {
-    const { id } = req.params;
-    const post = req.body;
-    dbPosts.update(id, post)
-        .then(count => count ? dbPosts.get(id)
-            .then(post => res.json(post)) : res.status(404).json({error: "There is no post with the specified ID!"}))
-        .catch(err => res.status(500).json({error: "Something went wrong updating your post's info"}))
-})
-
-server.delete('/api/users/:id', (req, res) => {
-    const { id } = req.params;
-    dbUsers.remove(id)
-        .then(count => count ? res.json({message: "User successfully deleted!"}) : res.status(404).json({error: "We couldn't find any user with th specified ID!"}))
-        .catch(err => res.status(500).json({error: "Something went wrong deleting that user!"}))
-})
-
-server.delete('/api/posts/:id', (req, res) => {
-    const { id } = req.params;
-    dbPosts.remove(id)
-        .then(count => count ? res.json({message: "Post successfully deleted!"}) : res.status(404).json({error: "We couldn't find any post with the specified ID!"}))
-        .catch( err => res.status(500).json({error: "Something went wrong deleting that post!"}))
-})
-
-server.listen(PORT, () => console.log(`Server up and running on port: ${PORT}`))
+server.listen(PORT, () => console.log(`Server up and running on port: ${PORT}`));
--- middleware.js
+++ middleware.js
@@ -1,7 +1,4 @@
 module.exports.toTitleCase = (req, res, next) => {
-    const { name } = req.body;
-    if(name) {
-        req.body.name = name.toLowerCase().replace(/\b\w/g, I => I.toUpperCase())
-        next();
-    }
-}
+  req.body.name = req.body.name.toLowerCase().replace(/\b\w/g, I => I.toUpperCase());
+  next();
+};
