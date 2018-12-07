5fd4dbe8664de0f0eadc835524114df3f55c069b
Julie Jonak <juliejonak10@gmail.com>
2018-12-06 21:54:00 -0600

    CONNECT USER ROUTER: - index.js: Required user router file to create routes - user_requests.js: imported userDb methods

--- index.js
+++ index.js
@@ -17,27 +17,30 @@ server.use(
 //route handlers
 
 //GET
-server.get('/users', (req, res) => {
-    userDb.get()
-        .then((users) => {
-            if (users.length > 0) {
-                res.json(users)
-            } else {
-                res
-                .status(404)
-                .json({
-                    message: "There are currently no Hobbits in the Shire. Check back later or go create a new one."
-                })
-            }
-        })
-        .catch(err => {
-            res
-            .status(500)
-            .json({
-                message: "Could not fetch the Hobbits. They're hiding."
-            })
-        })
-})
+
+server.use('/users', userRouter);
+
+// server.get('/users', (req, res) => {
+//     userDb.get()
+//         .then((users) => {
+//             if (users.length > 0) {
+//                 res.json(users)
+//             } else {
+//                 res
+//                 .status(404)
+//                 .json({
+//                     message: "There are currently no Hobbits in the Shire. Check back later or go create a new one."
+//                 })
+//             }
+//         })
+//         .catch(err => {
+//             res
+//             .status(500)
+//             .json({
+//                 message: "Could not fetch the Hobbits. They're hiding."
+//             })
+//         })
+// })
 
 server.get('/posts', (req, res) => {
     postDb.get()
@@ -85,63 +88,63 @@ server.get('/posts/:id', (req, res) => {
         })
 })
 
-server.get('/users/:id', (req, res) => {
-    const {id} = req.params;
-    userDb.get(id)
-        .then(user => {
-            if(user) {
-                res.json(user)
-            } else {
-                res
-                .status(404)
-                .json({
-                    message: "This hobbit does not exist."
-                })
-            }
-        })
-        .catch(err => {
-            res
-            .status(500)
-            .json({
-                message: "Failed to find this hobbit"
-            })
-        })
-})
+// server.get('/users/:id', (req, res) => {
+//     const {id} = req.params;
+//     userDb.get(id)
+//         .then(user => {
+//             if(user) {
+//                 res.json(user)
+//             } else {
+//                 res
+//                 .status(404)
+//                 .json({
+//                     message: "This hobbit does not exist."
+//                 })
+//             }
+//         })
+//         .catch(err => {
+//             res
+//             .status(500)
+//             .json({
+//                 message: "Failed to find this hobbit"
+//             })
+//         })
+// })
 
 
-server.get('/users/:id/posts', (req, res) => {
-    const {id} = req.params;
-    userDb.get(id)
-        .then(user => {
-            if (user) {
-                userDb.getUserPosts(id)
-                    .then(posts => {
-                        if (posts.length > 0) {
-                            res.json(posts);
-                        } else {
-                            res
-                            .status(404)
-                            .json({
-                                message: "This Hobbit has said nothing worth writing down."
-                            })
-                        }
-                    })
-            } else {
-                res
-                .status(404)
-                .json({
-                    message: "This Hobbit does not exist so we cannot quote it."
-                })
-            }
-        })
-        .catch(err=>{
-            res
-            .status(500)
-            .json({
-                message: "Failed to find this Hobbit or its quotes."
-            })
-        })
-})
+// server.get('/users/:id/posts', (req, res) => {
+//     const {id} = req.params;
+//     userDb.get(id)
+//         .then(user => {
+//             if (user) {
+//                 userDb.getUserPosts(id)
+//                     .then(posts => {
+//                         if (posts.length > 0) {
+//                             res.json(posts);
+//                         } else {
+//                             res
+//                             .status(404)
+//                             .json({
+//                                 message: "This Hobbit has said nothing worth writing down."
+//                             })
+//                         }
+//                     })
+//             } else {
+//                 res
+//                 .status(404)
+//                 .json({
+//                     message: "This Hobbit does not exist so we cannot quote it."
+//                 })
+//             }
+//         })
+//         .catch(err=>{
+//             res
+//             .status(500)
+//             .json({
+//                 message: "Failed to find this Hobbit or its quotes."
+//             })
+//         })
+// })
 
 //POST
 
--- routers/user_requests.js
+++ routers/user_requests.js
@@ -1,6 +1,8 @@
 const express = require('express');
 
 const router = express.Router();
+const userDb = require('../data/helpers/userDb');
+
 
 
 router.get('/', (req, res) => {
