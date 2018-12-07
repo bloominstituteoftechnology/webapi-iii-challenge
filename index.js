dce269894e200d0f6e085ecb7bf18c7249763939
Matthew Meitl <darksamus6@gmail.com>
2018-12-06 19:35:20 -0600

    intial server setup

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("hello world");
+const express = require('express');
+
+const server = express();
+const PORT = 4000;
+
+server.get('/', (req, res) => {
+    res.json({message: 'request recieved!'});
+
+});
+
+server.listen(PORT, err => {
+    console.log(`Listening on port ${PORT}`);
+});
