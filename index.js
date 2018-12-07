aebf90a7759d6fc38a2b76e7e0d668a5701572fa
TheGameFreak720 <luis.alvarez@commithub.com>
2018-12-06 21:44:46 -0500

    put middleware in a separate file

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const morgan = require('morgan');
 
 const user = require('./routes/user');
 const post = require('./routes/post');
+const upperCase = require('./middleware');
 
 const server = express();
 const PORT = 3000;
@@ -10,15 +11,7 @@ const PORT = 3000;
 server.use(express.json());
 server.use(morgan('dev'));
 
-server.use((req, res, next) => {
-  const name = req.body.name;
-
-  if (name) {
-      const capitalizedString = name.toUpperCase();
-      req.body.name = capitalizedString;
-  } 
-  next();
-});
+server.use(upperCase);
 
 server.use('/users', user);
 server.use('/posts', post);
