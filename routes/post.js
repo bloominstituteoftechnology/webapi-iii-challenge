99d25a05677d4a1b1ab94437dab0eec2b137d01a
TheGameFreak720 <luis.alvarez@commithub.com>
2018-12-06 21:59:29 -0500

    get for an specific user works

--- routes/post.js
+++ routes/post.js
@@ -14,5 +14,17 @@ router.get("/", (req, res) => {
       });
 });
 
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+
+    userDB.getUserPosts(id)
+        .then(posts => {
+            res.json(posts);
+        })
+        .catch(err => {
+            res.status(500).json({ error: "Error getting posts" });
+        });
+});
+
 
 module.exports = router;
