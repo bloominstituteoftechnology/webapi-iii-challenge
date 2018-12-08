cdeb4cbc877b534072b63caf3f661262b4da1107
Julie Jonak <juliejonak10@gmail.com>
2018-12-07 22:17:11 -0600

    POST ROUTES ADDED: - index.js: Removed post requests and imported routes - post_routes.js: Created router file for all post requests

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const postDb = require('./data/helpers/postDb');
 
 const customMiddleware = require('./Custom_Middleware');
 const userRouter = require('./routers/user_requests');
+const postRouter = require('./routers/post_routes');
 
 const server = express();
 const PORT = 4100;
@@ -20,54 +21,7 @@ server.use(
 
 server.use('/users', userRouter);
 
-server.get('/posts', (req, res) => {
-    postDb.get()
-    .then((posts)=>{
-        if (posts.length > 0) {
-            res.json(posts)
-        } else {
-            res
-            .status(404)
-            .json({
-                message: "There are no quotes. The Hobbits appear to be mute right now."
-            })
-        }
-    })
-    .catch(err => {
-        res
-        .status(500)
-        .json({
-            message: "Could not fetch those quotes. They're hiding."
-        })
-    })
-})
-
-server.get('/posts/:id', (req, res) => {
-    const {id} = req.params;
-    postDb.get(id)
-        .then(posts => {
-            console.log("get posts by id", posts);
-            if(posts.length > 0) {
-                res.json(posts)
-            } else {
-                res
-                .status(404)
-                .json({
-                    message: "This hobbit does not have any notable quotes. Perhaps search for a better one."
-                })
-            }
-        })
-        .catch(err => {
-            res
-            .status(500)
-            .json({
-                message: "Failed to find those quotes."
-            })
-        })
-})
-
-//POST
-
+server.use('/posts', postRouter);
 
 
 //listening
