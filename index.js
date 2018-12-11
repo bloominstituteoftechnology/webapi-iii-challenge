832b956511c24cb2f7ccc81598c5570dcaa6e3e1
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2018-12-10 19:48:05 -0600

    get users and user posts endpoints done

--- index.js
+++ index.js
@@ -7,7 +7,42 @@ const parser = express.json();
 const PORT = 4000;
 
 //Endpoints
+  // Get all users
+server.get('/api/users', (req,res) => {
+  userDb.get()
+    .then(users => {
+      res
+        .status(200)  
+        .json(users)
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({message: "Failed to get users"})
+    })
+})
 
+  //Get user by id
+server.get('/api/users/:id', (req,res) => {
+  const {id} = req.params;
+  userDb.getUserPosts(id) 
+    .then(posts => {
+      if(posts.length > 0) {
+        res
+          .status(200)
+          .json(user)
+      } else {
+        res
+          .status(404)
+          .json({message: "No posts under this user"})
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({message:"User does not exist"})
+      })
+})
 
 //Listening 
 server.listen(PORT, () => {
