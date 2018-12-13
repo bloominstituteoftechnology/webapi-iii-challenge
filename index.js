8f76805fa866527b07639afb54a627893fda4ecb
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2018-12-12 18:13:47 -0600

    delete  user endoint done

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const server = express();
 const parser = express.json();
 const PORT = 4000;
 
+server.use(express.json())
 //Endpoints
   // Get all users
 server.get('/api/users', (req,res) => {
@@ -25,12 +26,13 @@ server.get('/api/users', (req,res) => {
   //Get user by id
 server.get('/api/users/:id', (req,res) => {
   const {id} = req.params;
+  console.log(id);
   userDb.getUserPosts(id) 
     .then(posts => {
       if(posts.length > 0) {
         res
           .status(200)
-          .json(user)
+          .json(posts)
       } else {
         res
           .status(404)
@@ -43,6 +45,51 @@ server.get('/api/users/:id', (req,res) => {
         .json({message:"User does not exist"})
       })
 })
+  
+
+  // Add user
+  server.post('/api/users', (req,res) => {
+      const user = req.body;
+    console.log(user);
+    //logs undefined
+    if(user.name) {
+        userDb.insert(user)
+        .then(userId => {
+            console.log(userId)
+          userDb.get(userId)
+            .then(user => {
+                console.log(user)
+          })
+      })
+        .catch(err => {
+            res
+            .status(500)
+            .json({message: " Failed to add user"})
+      })
+  } else {
+        res
+        .status(400)
+        .json({message: "Missing user name"})
+  }
+})
+
+
+  //Delete user 
+server.delete('/api/users/:id', (req, res) => {
+  const {id} = req.params;
+  userDb.remove(id)
+    .then(count => {
+      res
+        .status(200)
+        .json(count)
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .catch({message: "Failed to delete user"})
+    })
+})
+
 
 //Listening 
 server.listen(PORT, () => {
