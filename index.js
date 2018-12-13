c1777078b367c295121c6c044c7166085ff53bdf
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2018-12-12 21:02:58 -0600

    Get posts and get post by id endpoiint done

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const parser = express.json();
 const PORT = 4000;
 
 server.use(express.json())
-//Endpoints
+//User Endpoints
   // Get all users
 server.get('/api/users', (req,res) => {
   userDb.get()
@@ -45,31 +45,32 @@ server.get('/api/users/:id', (req,res) => {
         .json({message:"User does not exist"})
       })
 })
-  
 
+  //Get user posts Endpoint
+server.eventNames
   // Add user
-  server.post('/api/users', (req,res) => {
-      const user = req.body;
-    console.log(user);
+server.post('/api/users', (req,res) => {
+  const user = req.body;
+  console.log(user);
     //logs undefined
-    if(user.name) {
-        userDb.insert(user)
-        .then(userId => {
-            console.log(userId)
-          userDb.get(userId)
-            .then(user => {
-                console.log(user)
-          })
+  if(user.name) {
+    userDb.insert(user)
+    .then(userId => {
+        console.log(userId)
+      userDb.get(userId)
+        .then(user => {
+            console.log(user)
       })
-        .catch(err => {
-            res
-            .status(500)
-            .json({message: " Failed to add user"})
+    })
+      .catch(err => {
+          res
+          .status(500)
+          .json({message: " Failed to add user"})
       })
   } else {
-        res
-        .status(400)
-        .json({message: "Missing user name"})
+    res
+    .status(400)
+    .json({message: "Missing user name"})
   }
 })
 
@@ -117,6 +118,45 @@ server.put('/api/users/:id', (req, res) => {
       .status()
       .json({message: "New User name missing"})
   }
+})
+
+//Post Endpoints
+  //Get Posts
+  server.get('/api/posts/', (req, res) => {
+    postDb.get()
+      .then(posts => {
+        res
+          .status(200)
+          .json(posts)
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({message: "Failed to get posts"})
+      })
+  })
+
+  //Get post by id
+server.get('/api/posts/:id', (req, res) => {
+  const {id} = req.params;
+  postDb.get(id)
+    .then(post=> {
+      console.log(post)
+      if(post){
+        res
+          .status(200)
+          .json(post)
+      } else {
+        res
+          .status(404)
+          .json({message: "Post not found under that id"})
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({message: "Failed to get Post"})
+    })
 })
 
 //Listening 
