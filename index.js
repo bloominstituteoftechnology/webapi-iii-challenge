559a2171fc7a3416064efe18e69219217c654391
cocoitali <cmburatto@gmail.com>
2018-12-20 10:02:05 -0700

    MVP : GET can fetch a specific id

--- index.js
+++ index.js
@@ -1,32 +1,47 @@
-const express = require('express')
-const cors = require('cors')
-const helmet = require('helmet')
-const logger = require('morgan')
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const logger = require("morgan");
 const PORT = 4001;
-const userDb = require('./data/helpers/userDb')
+const userDb = require("./data/helpers/userDb");
+const server = express();
 
-
-const server = express()
-server.use(express.json(), logger('tiny'), helmet(), cors() ) //express.json allows to parse the body
+server.use(express.json(), logger("tiny"), helmet(), cors()); //express.json allows to parse the body
 //helmet gives more protection to app, logger/morgan logs everything
 
-server.get('/', (req, res) => {
-    res.json({message: 'api is running'})
-})
+server.get("/", (req, res) => {
+  res.json({ message: "api is running" });
+});
 
-server.get('/api/users', (req, res) => {
-    userDb.get()
-        .then(users => {
-            res.json(users)
-        })
-        .catch(err => {
-            res
-            .status(500)
-            .json({error: "The users information could not be retrieved."})
-        })
-})
+server.get("/api/users", (req, res) => {
+  userDb
+    .get()
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: "The users information could not be retrieved." });
+    });
+});
 
+server.get("/api/users/:id", (req, res) => {
+  const { id } = req.params;
+  userDb
+    .get(id)
+    .then(user => {
+      if (user) {
+        res.json(user);
+      } else {
+        res.status(404).json({ message: "user does not exist" });
+      }
+    })
+    .catch(err => {
+      res
+        .status(404)
+        .json({ error: "The user information could not be retrieved." });
+    });
+});
 
-
-
-server.listen(PORT, () => console.log(`API running on port ${PORT}`))
+server.listen(PORT, () => console.log(`API running on port ${PORT}`));
