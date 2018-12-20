db55b61baff594d66592b3c3e6d0127066af9e7c
cocoitali <cmburatto@gmail.com>
2018-12-20 09:57:57 -0700

    MVP : added more dependencies, GET for users

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
-const express = require("express");
+const express = require('express')
+const cors = require('cors')
+const helmet = require('helmet')
+const logger = require('morgan')
 const PORT = 4001;
+const userDb = require('./data/helpers/userDb')
 
 
-const server = express();
-server.use(express.json());
+const server = express()
+server.use(express.json(), logger('tiny'), helmet(), cors() ) //express.json allows to parse the body
+//helmet gives more protection to app, logger/morgan logs everything
 
-server.listen(PORT, () => console.log((`API running on port ${PORT}`)))
+server.get('/', (req, res) => {
+    res.json({message: 'api is running'})
+})
+
+server.get('/api/users', (req, res) => {
+    userDb.get()
+        .then(users => {
+            res.json(users)
+        })
+        .catch(err => {
+            res
+            .status(500)
+            .json({error: "The users information could not be retrieved."})
+        })
+})
+
+
+
+
+server.listen(PORT, () => console.log(`API running on port ${PORT}`))
