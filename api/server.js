f1b6890bad1e2f72f0b97c3209e71a46ade8a979
jbierly1 <james.bierly@gmail.com>
2018-12-21 11:33:11 -0500

    implemented put, delete, and capitalization middleware

--- api/server.js
+++ api/server.js
@@ -10,11 +10,20 @@ const dbUsers = require('../data/helpers/userDb.js');
 
 const dbPosts=require('../data/helpers/postDb.js')
 
+function capitalizeNames(req, res, next) {
+    req.body.name=req.body.name.charAt(0).toUpperCase() + req.body.name.slice(1);
+  
+    next();
+  }
+
 server.use(express.json());
 server.use(helmet());
 server.use(morgan('dev'));
+server.use(capitalizeNames);
 
 module.exports=server;
+
+
 
 //get all users
 
@@ -58,17 +67,82 @@ server.get('/api/users/:id', (req, res)=>{
 )
 
 //create user
+
+server.post('/api/users', (req, res) => {
+	const user = req.body;
+
+	if (user.name) {
+		dbUsers.insert(user)
+			.then(userId => {
+				dbUsers.getById(userId).then(newuser => {
+					user = newpost;
+				});
+			})
+			.then(res.status(201).json(user))
+			.catch(err => {
+				res.status(500).json({
+					error: 'There was an error while saving the user to the database'
+				});
+			});
+	} else {
+		res.status(400).json({
+			errorMessage: 'Please provide a name!!!!'
+		});
+	}
+});
+
 //update user, 
+server.put('/api/users/:id', (req, res) => {
+	const updatedUser = req.body;
+	let { id } = req.params;
+
+	if (updatedUser.name) {
+		//
+		console.log('name' + id);
+		console.log('updated user' + updatedUser.name);
+		dbUsers.update(id, updatedUser).then(number => {
+			console.log(number);
+			if (!number) {
+				res.status(404).json({
+					error: 'The user with the specified ID does not exist'
+				});
+			} else {
+					res.status(200).json({success:"user edited successfully"})
+                    };
+				
+			
+		});
+    }})
+
 
 //delete user
 
+server.delete('/api/users/:id', (req, res) => {
+	let { id } = req.params;
+
+	dbUsers.get(id).then(user => {
+		dbUsers.remove(id)
+			.then(
+				user
+					? res.status(200).json({removed:`${user.name}`})
+					: res.status(404).json({
+							message: 'The user with the specified ID does not exist.'
+					  })
+			)
+			.catch(res.status(500).json({ error: 'The user could not be removed' }));
+        });
+    });
+    
+
+
+//make and implement middleware so that every user is capitalized
 
 
 
 //get all posts
 
 
-//make and implement middleware so that every user is capitalized
+
 
 
 
