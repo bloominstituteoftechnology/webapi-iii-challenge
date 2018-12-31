98d93e9f7b8cf76451ac7cb2aa8a6ba197fac38d
cocoitali <cmburatto@gmail.com>
2018-12-31 00:31:58 -0700

    MVP : created seperate file for mw, created custom Uppercase mw

---
+++ data/mwConfig.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const cors = require('cors')
+const helmet = require('helmet')
+const logger = require('morgan')
+
+module.exports = server => {
+  server.use(express.json(), logger('tiny'), helmet(), cors())
+  server.use((req, res, next) => {
+    if (req.body.name) {
+      req.body.name = req.body.name.toUpperCase()
+    }
+    next()
+  })
+}
