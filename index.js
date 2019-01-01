8fccb3bdba23a1bd4903c34b4bd6482e4f10e364
jbierly1 <james.bierly@gmail.com>
2019-01-01 09:24:40 -0500

    added port

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const server = require('./api/server.js');
-const port =9000;
+const port = process.env.PORT || 9000;
 
-server.listen(port, ()=>console.log(`API running on port ${port}`))
+server.listen(port, () => console.log(`API running on port ${port}`));
