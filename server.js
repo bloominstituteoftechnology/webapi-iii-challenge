48efee1c32f7a27224ff84f9d7a09c690ca99a1e
Grace <gracekang.93@gmail.com>
2019-01-09 14:11:33 -0800

    imports userDb, adds get handling

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const express = require('express');
 const helmet = require('helmet');
 const morgan = require('morgan');
 
+// import helpers
+const userDb = require('./data/helpers/userDb.js');
+
 // init server
 const server = express();
 
@@ -13,5 +16,30 @@ server.use(bodyParser());
 server.use(cors());
 server.use(helmet());
 server.use(morgan('short'));
+
+// get users
+server.get('/api/users/:id', async (req, res) => {
+    const { id } = req.params;
+    if (!id) {
+        res.status(400).json({
+            message: 'Please provide an ID.'
+        })
+    }
+    try {
+        const user = await userDb.get(id);
+        if (user) {
+            res.status(200).json({ user })
+        } else {
+            res.status(404).json({
+                message: 'Cannot find user.'
+            })
+        }
+    } catch(err) {
+        res.status(500).json({
+            message: 'Could not get user info.',
+            error: err
+        });
+    }
+})
 
 module.exports = server; 
