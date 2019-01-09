40ed164c618ea7c163637d6b06f9c1f6951ac6e3
Grace <gracekang.93@gmail.com>
2019-01-09 13:47:05 -0800

    modifies script to start, imports modules to server

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
-// import express
+// import modules
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
 
 // init server
 const server = express();
+
+// apply middleware
+server.use(bodyParser());
+server.use(cors());
+server.use(helmet());
+server.use(morgan('short'));
 
 module.exports = server; 
