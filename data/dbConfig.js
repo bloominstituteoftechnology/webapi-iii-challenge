e0604b4d04581ae0980234f4c42e141babc7439d
Joseph Thompson <joe.thompson838@gmail.com>
2019-01-09 11:15:12 -0500

    Update dbConfig.js

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,4 +1,3 @@
 const knex = require('knex');
 const knexConfig = require('../knexfile.js');
-
-module.exports = knex(knexConfig.development);
+module.exports = knex(knexConfig.process.env.environment);
