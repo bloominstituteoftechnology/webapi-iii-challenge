30efa0b6e700c7aef3816330675b5a6e6a9a5d42
Grace <gracekang.93@gmail.com>
2019-01-09 14:22:51 -0800

    gets users and user by ID

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // import modules
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 const cors = require('cors');
 const express = require('express');
 const helmet = require('helmet');
@@ -11,35 +11,46 @@ const userDb = require('./data/helpers/userDb.js');
 // init server
 const server = express();
 
+// body-parser deprecated?
+server.use(express.json());
+
 // apply middleware
-server.use(bodyParser());
+// server.use(bodyParser());
 server.use(cors());
 server.use(helmet());
 server.use(morgan('short'));
 
 // get users
+server.get('/api/users', async (req, res) => {
+    try {
+        const users = await userDb.get();
+        res.status(200).json(users);
+    } catch (err) {
+        res.status(500).json({
+            message: 'There was an error while retrieving users.',
+            error: err
+        });
+    }
+});
+
+// get user by ID
 server.get('/api/users/:id', async (req, res) => {
     const { id } = req.params;
-    if (!id) {
-        res.status(400).json({
-            message: 'Please provide an ID.'
-        })
-    }
     try {
         const user = await userDb.get(id);
         if (user) {
-            res.status(200).json({ user })
+            res.status(200).json(user)
         } else {
             res.status(404).json({
-                message: 'Cannot find user.'
+                message: 'User not found.'
             })
         }
     } catch(err) {
         res.status(500).json({
-            message: 'Could not get user info.',
+            message: 'There was an error while retrieving the user.',
             error: err
         });
     }
-})
+});
 
 module.exports = server; 
