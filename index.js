6029d92e730370f9bb4f670e1a6b66e6188f09e4
Holden B <holden.bucher.92@gmail.com>
2019-01-09 15:17:19 -0600

    separated server/index and fixed package.json

---
+++ index.js
@@ -0,0 +1,5 @@
+const server = require("./server");
+
+const port = 5000;
+
+server.listen(port, () => console.log(`Server is listening on Port ${port}`));
--- server.js
+++ server.js
@@ -5,11 +5,10 @@ const postRoutes = require("./posts/postRoutes");
 const userRoutes = require("./users/userRoutes");
 
 const server = express();
-const port = 5000;
 
 server.use(express.json());
 server.use(cors());
 server.use("/api/users", userRoutes);
 server.use("/api/posts", postRoutes);
 
-server.listen(port, () => console.log(`Server is listening on Port ${port}`));
+module.exports = server;
