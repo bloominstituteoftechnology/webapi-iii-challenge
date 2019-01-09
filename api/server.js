16d18a70ae0772ecfed40c6dcc1f82b0483795c0
ToMMaaY <thuynh629@gmail.com>
2019-01-09 14:13:34 -0800

    Created a upper case middleware and hooked up the Post endpoint

--- api/server.js
+++ api/server.js
@@ -15,14 +15,42 @@ server.use(express.json());
 server.use(morgan("short"));
 server.use(cors());
 
+function upperCase(req, res, next) {
+  const user = req.body;
+  const { name } = user;
+
+  if (name) {
+    const arr = name.split(" ");
+    const upperCased = arr.map(
+      item => item[0].toUpperCase() + item.slice(1).toLowerCase()
+    );
+    const joined = upperCased.join(" ");
+    res.send(joined);
+  } else {
+    res.status(400).json({ message: "Please include user name" });
+  }
+  next();
+}
+
 // routes
 server.get("/api/users", async (req, res) => {
   try {
     const userList = await userDb.get();
     res.json(userList);
   } catch (err) {
-    res.status(500).json({ message: `the user list cannot be retrieved` });
+    res.status(500).json({ message: `The user list cannot be retrieved` });
   }
 });
 
+server.post("/api/users", upperCase, async (req, res) => {
+  const user = req.body;
+  try {
+    const result = await userDb.insert(user);
+    res.status(201).json({ messege: `User ${user} has been created!` });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "There was an error saving user to the database" });
+  }
+});
 module.exports = server;
