5d4204fdd25a65676ac1b161116e16d1965c7b99
ToMMaaY <thuynh629@gmail.com>
2019-01-10 11:18:41 -0800

    Changed port to dynamaic variable

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const server = require("./api/server.js");
 
-const port = process.env.PORT || 5000
-server.listen(5000, console.log("Server Running"));
+const port = process.env.PORT || 5000;
+server.listen(port, console.log("Server Running"));
