6a64b58c5c421641de9b2372ed889ee112dba1b5
Latifah President <latifahpresident@Latifahs-MacBook-Pro.local>
2019-01-14 08:59:21 -0600

    get route to user posts working

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const cors = require('cors');
 const server = express();
 
 const users = require('./data/helpers/userDb.js');
+const post = require('./data/helpers/postDb.js');
+const tags = require('./data/helpers/tagDb.js');
 
 server.use(morgan('short'));
 server.use(helmet());
@@ -12,7 +14,9 @@ server.use(cors());
 server.use(express.json());
 
 
-const url = '/api/users/'
+const url = '/api/users/';
+const posturl = '/api/post/';
+const tagsurl = '/api/tags/';
 
 server.get(url, async(req, res) => {
     try{
@@ -34,7 +38,22 @@ server.get(`${url}:id`, async(req, res) => {
             res.status(200).json(userData)
         }
     }catch(err){
-        res.status(500).json(`{error: 'user infor could not ne retrieved'}`)
+        res.status(500).json(`{error: 'user info could not be retrieved'}`)
+    }
+});
+
+server.get(`${url}post/:userId`, async(req, res) => {
+    const { userId } = req.params;
+    console.log(userId)
+    try{
+        const userPost = await users.getUserPosts(userId)
+        if(userPost.length === 0) {
+            res.status(404).json(`{error: 'no post by that user has been found'}`)
+        } else {
+            res.status(200).json(userPost)
+        }
+    } catch(err) {
+        res.status(500).json(`{error: 'user post could not be found' }`)
     }
 });
 
@@ -81,6 +100,46 @@ server.delete(`${url}:id`, async(req, res) => {
         }
     }catch(err){
         res.status(500).json(err)
+    }
+});
+
+//Post routes
+
+server.get(`${posturl}`, async(req, res) => {
+    try{
+        const postData = await post.get()
+        res.status(200).json(postData)
+    }catch(err){
+        res.status(500).json(`{error: 'that route does not exist'}`)
+    }
+});
+
+server.get(`${posturl}:id`, async(req, res) => {
+    const { id } = req.params;
+    try{
+        const postData = await post.get(id)
+        if(postData.lenght === 0) {
+            res.status(404).json(`{error: 'That post was not found'}`)
+        } else {
+            res.status(200).json(postData)
+        }
+    }catch(err){
+        res.status(500).json(`{error: 'post could not be retrieved'}`)
+    }
+});
+
+server.post(`${posturl}`, async(req, res) => {
+
+});
+
+//Tags Route
+
+server.get(tagsurl, async(req, res) => {
+    try{
+        const tagsData = await tags.get()
+        res.status(200).json(tagsData)
+    }catch(err){
+        res.status(500).json(`{error: 'that url does not exist'}`)
     }
 });
 
