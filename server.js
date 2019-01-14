eeac5b2e3cac59cda4d56c3c223b4e051aa74d80
Latifah President <latifahpresident@Latifahs-MacBook-Pro.local>
2019-01-13 22:57:03 -0600

    post route added

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ server.use(helmet());
 server.use(cors());
 server.use(express.json());
 
+
 const url = '/api/users/'
 
 server.get(url, async(req, res) => {
@@ -25,13 +26,6 @@ server.get(url, async(req, res) => {
 server.get(`${url}:id`, async(req, res) => {
     const { id } = req.params;
 
-    // users.get(id)
-    //     .then(userData => {
-    //         res.status(200).json(userData)
-    //     })
-    //     .catch(err => {
-    //         res.status(500)
-    //     })
     try{
         const userData = await users.get(id)
         if(userData.length === 0) {
@@ -41,6 +35,21 @@ server.get(`${url}:id`, async(req, res) => {
         }
     }catch(err){
         res.status(500).json(`{error: 'user infor could not ne retrieved'}`)
+    }
+});
+
+server.post(`${url}:id`, async(req, res) => {
+    const { name } = req.body;
+    console.log(name)
+    try{
+        if(name.length > 128) {
+            res.status(404).json(`{error: 'name must be under 128 characters'}`)
+        } else {
+            const post = await users.insert({name})
+            res.status(201).json(post)
+        }
+    } catch(err) {
+        res.status(500).json(`{error: 'there was an error while saving post to database'}`)
     }
 });
 
