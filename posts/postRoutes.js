51e019dd321ea2be5e3c26f84ced81cfffb28ea8
Trai Lynne <trailynne@gmail.com>
2019-01-16 15:49:29 -0500

    Posts: Defined GET route handler for READ operation - added guiding comments and starter code - success => 200 with posts - no posts => 404 with error message - error => 500 with error

--- posts/postRoutes.js
+++ posts/postRoutes.js
@@ -2,10 +2,42 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+const db = require('../data/helpers/postDb');
+
+router.use(express.json());
+
+
+// C - Create
+// Ra - ReadAll
+router.get('/', async (req, res) => {
+    try {
+        const posts = await db.get();
+
+        posts.length > 0 ?
+            res
+                .status(200)
+                .json(posts)
+            :
+            res
+                .status(404)
+                .json({
+                    errorMessage: "No posts found at this time. Try again Later please."
+                });
+    } catch (err) {
+        res
+            .status(500)
+            .json({
+                errorMessage: 'An unknown error occured. Please try again'
+            });
+    }
+
     res
         .status(200)
         .send('Welcome to Posts');
 });
+
+// R1 - ReadOne
+// U - Update
+// D - Destroy
 
 module.exports = router;
--- users/userRoutes.js
+++ users/userRoutes.js
@@ -2,11 +2,20 @@ const express = require('express');
 
 const router = express.Router();
 
+const db = require('../data/helpers/postDb');
+
+router.use(express.json());
+
+
+// C - Create
+// Ra - ReadAll
 router.get('/', (req, res) => {
     res
         .status(200)
         .send('Welcome to Users');
 });
-
+// R1 - ReadOne
+// U - Update
+// D - Destroy
 
 module.exports = router;
