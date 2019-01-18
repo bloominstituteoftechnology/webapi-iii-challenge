9c42c8ccb345d74e7b7cb45a6357ff3076deb7ea
Jesse Theisen <theiseja@gmail.com>
2019-01-17 21:43:57 -0600

    MAVP: Added and tested all GET requests, POST and DELETE work also.

--- server.js
+++ server.js
@@ -30,25 +30,73 @@ const nameCheckMiddleware = (req, res, next) => {
 server.get('/api/users', (req, res) => {
     users
      .get()
-     .then(foundUsers => {
+     .then(foundUsers => {    // This request gets all users contained within the DB.
          res.json(foundUsers);
      })
      .catch(err => {
-         return errorHelper(500, 'Error loading users', res);
+         return errorHelper(500, 'The users could not be retrieved from the DB.', res);
      });
+});
+
+server.get('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+    users
+      .get(id)   
+      .then(user => {   // This request gets a user with a specific ID within the DB.
+        if (user === 0) {
+          return errorHelper(404, 'No user by that Id in the DB', res);
+        }
+        res.json(user);
+      })
+      .catch(err => {
+        return errorHelper(500, 'The user with that ID does not exist in the DB', res);
+      });
+  });
+  
+
+server.get('/api/users/posts/:userId', (req, res) => {
+    const { userId } = req.params;
+    users
+    .getUserPosts(userId)   // This request gets all posts by a specific user within the DB.
+    .then(usersPosts => {
+        if (usersPosts === 0) {
+            return errorHelper(404, 'No posts by that user exist in the DB.', res);
+        }
+        res.json(usersPosts);
+    })
+    .catch(err => {
+        return errorHelper(500, 'Users posts could not be retrieved.');
+    });
 });
 
 server.post('/api/users', nameCheckMiddleware, (req, res) => {
     const { name } = req.body;
     users
     .insert({ name })
-    .then(response => {
+    .then(response => {   // This request creates a new user with a unique ID.
         res.json(response);
     })
     .catch(err => {
         return errorHelper(500, 'placeholder', res);
     });
 });
+
+server.delete('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+    users
+    .remove(id)
+    .then(userRemoved => { // This request deletes a user with a specific ID from the DB.
+        if (userRemoved === 0) {
+            return errorHelper(404, 'No user by that Id exists within the DB.');
+        } else {
+            res.json({ success: 'User Removed' });
+        }
+    })
+    .catch(err => {
+        return errorHelper(500, 'The user could not be removed from the DB.', res);
+    });
+});
+
 
 
 
