fa95db41ed720789919cf5919e533f904ac7c72c
Henry Neal <henry.neal0@gmail.com>
2019-02-13 16:30:48 -0500

    fixed server issue

--- routes/User-router.js
+++ routes/User-router.js
@@ -2,3 +2,27 @@ const express = require('express');
 const router = express.Router();
 
 const Users = require('../data/helpers/userDb');
+const Posts = require('../data/helpers/postDb');
+
+// GET // GET // GET // GET // GET // GET // GET // GET // GET // GET // GET // GET // GET // GET // GET // GET // GET // GET // GET // GET // GET // GET 
+router.get('/', async (req, res) => {
+    try {
+        const users = await Users.find();
+        res.status(200).json(users);
+    }catch (error) {
+        console.log(error);
+        res.status(500).json({message:'error getting the user!'});
+    }
+});
+
+// router.get('/id', async (req, res) => {
+//     try {
+//         const user = await Users.getById
+//     }
+// })
+
+const error = {
+    name: 'wrong name!!'
+}
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
-const helmet = require('helmet');
-const morgan = require('morgan');
 const server = express();
-const postRouter = require('./routes/Post-router');
+const helmet = require('helmet');
+
+const morgan = require('morgan');
+
+// const postRouter = require('./routes/Post-router');
 const userRouter = require('./routes/User-router');
+
+const server = require('./server');
+
+
+
 
 server.use(express.json());
 server.use(helmet());
@@ -12,4 +19,8 @@ server.use(morgan('dev'));
 
 server.use('/api/users', userRouter);
 
-module.exorts = server;
+server.listen(4000, () => {
+    console.log('\n* Server Running on http://localhost:4000 *\n');
+  });
+
+// module.exorts = server;
