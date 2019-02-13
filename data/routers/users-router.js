ddc35d831f874b2178713a80de329039ee264042
Zooheck <davidscottflack@gmail.com>
2019-02-13 17:50:30 -0600

    Implement get all users functionality

--- data/routers/users-router.js
+++ data/routers/users-router.js
@@ -3,4 +3,16 @@ const router = express.Router();
 
 const UserFuncs = require('../helpers/userDb.js')
 
+router.get('/', async (req, res) => {
+    const users = await UserFuncs.get();
+    try {
+        res.status(200).json(users)
+    }
+    catch(err) {
+        console.log(err)
+        res.status(500).json({message: "Error retrieving users"})
+    }
+});
+
+
 module.exports = router;
