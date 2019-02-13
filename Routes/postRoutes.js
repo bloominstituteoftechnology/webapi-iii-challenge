685547a975db61e3bc41228208fe3f959e7149b0
Matt Basile <matthewbasile2@gmail.com>
2019-02-13 16:10:55 -0500

    All CRUD operations functioning except my ADD posts.

--- Routes/postRoutes.js
+++ Routes/postRoutes.js
@@ -15,5 +15,93 @@ router.get('/', (req, res) => {
         res.status(code).json({success: false, message})
     })
 });
+router.get('/:id', (req, res) => {
+    const {id} = req.params;
+    db
+    .getById(id)
+    .then(posts => { 
+        if(posts){
+        res.status(200).json({success: true, posts})
+      } else{
+        res.status(404).json({success:false, message: 'The post with that ID does not exist.'});
+      }})
+      .catch(({}) =>{
+        res.status(500).json({success:false, message: 'The posts information could not be retrieved.'});
+    })
+});
+// Add Posts
+router.post('/', (req, res) => {
+    const { text } = req.body;
+    const newPost= { text };
+    if (!text) {
+      return res
+        .status(400)
+        .json({ errorMessage: "Please provide text for the post." });
+    }
+    db.insert(newPost)
+      .then(postId => {
+        const { id } = postId;
+        db.getById(id).then(post => {
+          console.log(post);
+          if (!post) {
+            return res
+              .status(404)
+              .send({ Error: `A post does not exist by that id ${id}` });
+          }
+          else{
+            res.status(201).json(post);
+          }
+        });
+      })
+      .catch(() => res.status(500).json({success: false, message: "There was an error while saving the post to the database."})
+  )});
+// Delete Single Post
+router.delete('/:id', (req, res) => {
+    const {id} = req.params;
+    db
+    .remove(id)
+    .then(posts => { 
+        if(posts){
+        res.status(204).end();
+      } else{
+        res.status(404).json({success:false, message: 'The post with that ID does not exist.'});
+      }})
+      .catch(({}) =>{
+        res.status(500).json({success:false, message: 'The posts information could not be retrieved.'});
+    })
+});
+// Edit a Post
+router.put("/:id", (req, res) => {
+    const { id } = req.params;
+    const { text } = req.body;
+    const post = { text };
+    console.log(post) //{ title: 'Exzc', contents: 'ent at Lambda School' }
+    if (!text) {
+        return res.status(400).json({success: false, message: "Must provide content for the post"});
+    }
+    db.update(id, post)
+      .then(response => {
+        if(response == 0) {
+            return res.status(404).json({success: false, message: "Post with ID does not exist"});
+        }
+        db.getById(id)
+          .then(post=> {
+              console.log(post)
+              if(post.length === 0) {
+                return res.status(404).json({success: false, message: "Post with ID does not exist"});
+              }
+              res.json({post});
+          })
+  
+      })
+      .catch(message => {
+        return res.status(400).json({success: false, message: message});
+      });
+  });
+
+
+
+
+
 
 module.exports = router
--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -5,9 +5,9 @@ const router = express.Router();
 router.get('/', (req, res) => {
     db
     .get()
-    .then(posts => {
-        if(posts){
-        res.status(200).json({success: true, posts})
+    .then(users => {
+        if(users){
+        res.status(200).json({success: true, users})
       } else{
         res.status(500).json({success:false, message: 'The users information could not be retrieved.'});
       }})
@@ -15,6 +15,103 @@ router.get('/', (req, res) => {
         res.status(code).json({success: false, message})
     })
 });
+router.get('/:id', (req, res) => {
+    const {id} = req.params;
+    db
+    .getById(id)
+    .then(users => { 
+        if(users){
+        res.status(200).json({success: true, users})
+      } else{
+        res.status(404).json({success:false, message: 'The post with that ID does not exist.'});
+      }})
+      .catch(({}) =>{
+        res.status(500).json({success:false, message: 'The users information could not be retrieved.'});
+    })
+});
+// Add users
+router.post('/', (req, res) => {
+    const { name } = req.body;
+    const newUser= { name };
+    if (!name) {
+      return res
+        .status(400)
+        .json({ errorMessage: "Please provide a name for the user." });
+    }
+    db.insert(newUser)
+      .then(userId => {
+        const { id } = userId;
+        db.getById(id).then(user => {
+          console.log(user);
+          if (!user) {
+            return res
+              .status(404)
+              .send({ Error: `A user does not exist by that id ${id}` });
+          }
+          else{
+          res.status(201).json(user);
+          }
+        });
+      })
+      .catch(() => res.status(500).json({success: false, message: "There was an error while saving the user to the database."})
+  )});
+// Delete Single user
+router.delete('/:id', (req, res) => {
+    const {id} = req.params;
+    db
+    .remove(id)
+    .then(users => { 
+        if(users){
+        res.status(204).end();
+      } else{
+        res.status(404).json({success:false, message: 'The post with that ID does not exist.'});
+      }})
+      .catch(({}) =>{
+        res.status(500).json({success:false, message: 'The users information could not be retrieved.'});
+    })
+});
+// Edit a user
+router.put("/:id", (req, res) => {
+    const { id } = req.params;
+    const { text } = req.body;
+    const post = { text };
+    console.log(post) //{ title: 'Exzc', contents: 'ent at Lambda School' }
+    if (!text) {
+        return res.status(400).json({success: false, message: "Must provide content for the post"});
+    }
+    db.update(id, post)
+      .then(response => {
+        if(response == 0) {
+            return res.status(404).json({success: false, message: "Post with ID does not exist"});
+        }
+        db.getById(id)
+          .then(post=> {
+              console.log(post)
+              if(post.length === 0) {
+                return res.status(404).json({success: false, message: "Post with ID does not exist"});
+              }
+              res.json({post});
+          })
+  
+      })
+      .catch(message => {
+        return res.status(400).json({success: false, message: message});
+      });
+  });
 
-
+//Get a Users Posts
+router.get('/posts/:id', (req, res) => {
+    const {id} = req.params;
+    db
+    .getUserPosts(id)
+    .then(users => { 
+        if(users){
+        res.status(200).json({success: true, users})
+      } else{
+        res.status(404).json({success:false, message: 'The user with that ID does not exist.'});
+      }})
+      .catch(({}) =>{
+        res.status(500).json({success:false, message: 'The users information could not be retrieved.'});
+    })
+});
 module.exports = router
