ee84b7b26eb41b80db188b9fe5bda7041ac3f792
Nate Boyette <nateboyette@icloud.com>
2019-02-13 13:20:51 -0800

    Add post endpoint.

--- data/helpers/users/userRouter.js
+++ data/helpers/users/userRouter.js
@@ -8,7 +8,17 @@ router.use(cors());
 
 
 // GET 
-
+router.get("/", async (req, res) => {
+  try {
+    const users = await Users.get(req.query);
+    res.status(200).json(users);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Error retrieving the posts."
+    });
+  }
+});
 
 // POST
 
