5ddfaf9aab8369dd82d04e8f2f33f9bec6b67d79
cacheUp <bradb26@gmail.com>
2019-02-14 00:18:23 +0300

    get a list of all users

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // code away!
+const express = require('express');
+const server = express();
+const PORT = 8080;
+const userDb = require('./data/helpers/userDb')
+
+server.use(express.json());
+
+console.log('let us get started');
+
+server.get('/', (req, res) => {
+    res.send(`hi there from inside an initial get function, on port ${PORT}`)
+})
+
+// get a list of all our users
+
+server.get('/api/users', (req, res) => {
+    userDb.get()
+    .then(users => {
+        res.json(users)
+    })
+    .catch(err => {
+        res.status(500)
+        res.json(`Huh, can't find those`)
+    })
+})
