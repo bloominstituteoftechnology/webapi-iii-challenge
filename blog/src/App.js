34f30e591887b9c0ca19841e42e63c033042e58b
Trai Lynne <trailynne@gmail.com>
2019-02-13 17:29:53 -0500

    Static Users View created

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,25 +1,35 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {
+  Component
+} from 'react';
+
 import './App.css';
+
+import {
+  Route,
+  Link
+} from 'react-router-dom';
+import UsersView from './views/usersView';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='app-container'>
+        <nav>
+          <Link exact to='/'>
+            Home
+          </Link>
+
+          <Link to='/users'>
+            Posts By User
+          </Link>
+        </nav>  
+
+        <div className='content-container'>
+          <Route 
+            path='/users'
+            component={UsersView}
+          />
+        </div>
       </div>
     );
   }
---
+++ blog/src/views/usersView.js
@@ -0,0 +1,11 @@
+import React, {
+    Component
+} from 'react';
+
+class UsersView extends Component {
+    render() {
+        return <h1>Users View</h1>
+    }
+}
+
+export default UsersView;
