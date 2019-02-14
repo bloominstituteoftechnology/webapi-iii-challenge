04fa946f2b3637af382912b86d7b9ea9921240d3
rmejia-tajc <rmejia.tajc@gmail.com>
2019-02-13 17:13:15 -0700

    create custom middleware to uppercase users POST and PUT routes

--- users/users-router.js
+++ users/users-router.js
@@ -6,6 +6,16 @@ const Users = require('./userDb.js');
 const router = express.Router(); // notice the Uppercase "R" in Router
 
 
+// custom middleware
+const nameCheckAndToUpper = (req, res, next) => {
+    if (!req.body.name) {
+        res.status(400).json({ errorMessage: "Please provide a name for the user." });
+      } else {
+        req.body.name = req.body.name.toUpperCase();
+        next();
+      }
+};
+
 
 // GET
 router.get('/', async (req, res) => {
@@ -37,44 +47,55 @@ router.get('/:id', async (req, res) => {
     } 
 });
 
-// POST
-router.post('/', async (req, res) => {
+// GET user posts by id
+router.get('/:id/posts', async (req, res) => {
+    try {
 
-    if (!req.body.name) {
-        res.status(400).json({ errorMessage: "Please provide a name for the user." });
-    } else {    
-        try {
-            const user = await Users.insert(req.body);
-            res.status(201).json(user);
-        } catch (error) {
-            //log error to database
-            console.log(error);
-            res.status(500).json({ error: "There was an error while saving the user to the database" });
+        const user = await Users.getUserPosts(req.params.id);
+
+        if (user.length > 0) {
+            res.status(200).json(user);
+        } else {
+            res.status(404).json({ message: "The user with the specified ID does not exist." });
         }
-    }
+    } catch (error) {
+        // log error to database
+        console.log(error);
+        res.status(500).json({ error: "The user information could not be retrieved." });
+    } 
+});
+
+// POST
+router.post('/', nameCheckAndToUpper, async (req, res) => {
+    
+    try {
+        const user = await Users.insert(req.body);
+        res.status(201).json(user);
+    } catch (error) {
+        //log error to database
+        console.log(error);
+        res.status(500).json({ error: "There was an error while saving the user to the database" });
+    }    
 
 });
 
 // PUT
-router.put('/:id', async (req, res) => {
+router.put('/:id', nameCheckAndToUpper, async (req, res) => {
+     
+    try {
+        const user = await Users.update(req.params.id, req.body);
 
-    if (!req.body.name) {
-        res.status(400).json({ errorMessage: "Please provide a name for the user." });
-    } else {  
-        try {
-            const user = await Users.update(req.params.id, req.body);
-
-            if (user) {
-                res.status(200).json(user);
-            } else {
-                res.status(404).json({ message: "The user with the specified ID does not exist." });
-            }
-        } catch (error) {
-            // log error to database
-            console.log(error);
-            res.status(500).json({ error: "The user information could not be modified." });
+        if (user) {
+            res.status(200).json(user);
+        } else {
+            res.status(404).json({ message: "The user with the specified ID does not exist." });
         }
+    } catch (error) {
+        // log error to database
+        console.log(error);
+        res.status(500).json({ error: "The user information could not be modified." });
     }
+    
 });
 
 //DELETE
