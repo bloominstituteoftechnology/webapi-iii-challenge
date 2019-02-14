c057328ad164676682a62a823c130fa0b716cd82
Zooheck <davidscottflack@gmail.com>
2019-02-14 15:54:18 -0600

    prepare for deployment

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-// code away!
+require('dotenv').config();
+
 const server = require('./server.js')
 
-server.listen(4000, () => {
-    console.log('Server listening on port 4000')
+const port = process.env.PORT || 4000;
+
+server.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
 })
