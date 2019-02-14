62ce398f0a8ea95d9b32858500997385eaa367f9
rmejia-tajc <rmejia.tajc@gmail.com>
2019-02-14 14:10:52 -0700

    prep for heroku deployment

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+require('dotenv').config();
+
 // import server
 const server = require('./server.js');
 
 
-
-// watch for connections on port 6000
-server.listen(6000, () => {
-    console.log( '\n\n***** Server Running on http://localhost:5000 *****\n\n' );
+// a hosting service might assign a port dynamically
+// Environment Variable are system wide
+const port = process.env.PORT || 6000;
+// watch for connections on port 6000 or whatever the environmentals assign
+server.listen(port, () => {
+    console.log( `\n\n***** Server Running on http://localhost:${port} *****\n\n` );
 });
