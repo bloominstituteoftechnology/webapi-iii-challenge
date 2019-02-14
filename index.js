4829f3161f3cc84a5a505bec1eede5a0e6aaec3d
Brandon <Blent1050@yahoo.com>
2019-02-14 15:25:36 -0500

    added deployment files

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-// code away!
+require('dotenv').config();
 const server = require('./server.js');
 
-server.listen(4000, () => {
-    console.log('Server running on http://localhost:4000')
-})
+const port = process.env.PORT || 4000;
+server.listen(port, () => {
+  console.log(`Server running on ${port}`);
+});
