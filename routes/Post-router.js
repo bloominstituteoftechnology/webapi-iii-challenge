3652b8a65436e75af208cce17e0e6a70dbd60c00
Henry Neal <henry.neal0@gmail.com>
2019-02-14 15:48:00 -0500

    update

--- routes/Post-router.js
+++ routes/Post-router.js
@@ -14,7 +14,6 @@ router.get('/', async (req, res) => {
         res.status(500).json({message:'error getting the post!'});
     }
 });
-
 router.get('/:id', async (req, res) => {
     try {
         const post = await Posts.getById(req.params.id);
