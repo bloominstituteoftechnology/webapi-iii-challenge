fd6fe12403772f0f5366688f0eeae34b5f526930
Sheila <s.hopper01110@gmail.com>
2019-02-20 17:01:53 -0500

    Initial Commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const helmet =  require('helmet');
+
+const userR = require('./data/helpers/userRouter.js');
+
+const server = express();
+
+
+
+
+module.exports = server;
