d232c635baeb15e669fb6500ab9c00d3a4d07577
Sheila <s.hopper01110@gmail.com>
2019-02-20 19:03:45 -0500

    POST method works for postsRouter.js

--- data/helpers/postsRouter.js
+++ data/helpers/postsRouter.js
@@ -6,52 +6,93 @@ const postRouter = express.Router();
 
 //GET:
 
-postRouter.get('/', (req, res ) => {
-    db
-    .get()
-    .then(posts => {
-        res.status(200).json({ success: true, posts});
-    })//headers
-    .catch(err => {
-        res.status(500).json({ success: false, message: 'The user information could not be retrieved.'})
-    })
-})
+postRouter.get('/', (req, res) => {
+	db
+		.get()
+		.then((posts) => {
+			res.status(200).json({ success: true, posts });
+		}) //headers
+		.catch((err) => {
+			res.status(500).json({ success: false, message: 'The user information could not be retrieved.' });
+		});
+});
 
 //GET:
 
-postRouter.get('/:id', ( req, res ) => {
-    const { id } = req.params;
+postRouter.get('/:id', (req, res) => {
+	const { id } = req.params;
 
-    db.getById(id)
-    .then(posts => {
-        if (posts) {
-            res.status(201).json({ success: true, posts});
-        }else{
-            res.status(404).json({ success: false, message: 'The post with the specified ID does not exist.'})
-        }
-    })
-    .catch(err => {
-        res.status(500).json({ success: false, error: 'The post information could not be retrieved.'})
+	db
+		.getById(id)
+		.then((posts) => {
+			if (posts) {
+				res.status(201).json({ success: true, posts });
+			} else {
+				res.status(404).json({ success: false, message: 'The post with the specified ID does not exist.' });
+			}
+		})
+		.catch((err) => {
+			res.status(500).json({ success: false, error: 'The post information could not be retrieved.' });
+		});
+});
 
-        })
-    })
+//POST:
 
-        //DELETE: 
+postRouter.post('/', (req, res) => {
+    const { text, user_id } = req.body;
+    
+	if (!text && !user_id ) {
+		res.status(400).json({ error: 'Please provide User Id and text.' });
+	} else {
+		db
+			.insert({ text, user_id })
+			.then((post) => {
+				res.status(201).json(post);
+			})
+			.catch((err) => {
+				res.status(500).json({ error: 'There was an error while saving the post to the database' });
+			});
+	}
+});
 
-postRouter.delete('/:id', ( req, res ) => {
-    const { id } =req.params;
-        db
-        .remove(id)
-        .then(post => {
-            if( post ){
-                res.status(204).end();
-            }else{
-                res.status(404).json({ success: false,  message:'The post with the specified ID does not exist.' })
-                }
-            })
-        .catch(err => {
-                res.status(500).json({ error: 'The post could not be removed' })
-            })
-        })
+//DELETE:
+
+postRouter.delete('/:id', (req, res) => {
+	const { id } = req.params;
+	db
+		.remove(id)
+		.then((post) => {
+			if (post) {
+				res.status(204).end();
+			} else {
+				res.status(404).json({ success: false, message: 'The post with the specified ID does not exist.' });
+			}
+		})
+		.catch((err) => {
+			res.status(500).json({ error: 'The post could not be removed' });
+		});
+});
+
+//PUT:
+
+postRouter.put('/:id', (req, res) => {
+	const { id } = req.params;
+	const changes = req.body;
+
+	db
+		.update(id, changes)
+		.then((userInfoUpdate) => {
+			if (!userInfoUpdate) {
+				res.status(404).json({ success: false, message: 'The user with the specified ID does not exist.' });
+			} else if (!changes.name) {
+				return res.status(400).json({ success: false, message: 'Please provide the name of the  user.' });
+			} else {
+				return res.status(200).json({ success: true, changes });
+			}
+		})
+		.catch((err) => {
+			res.status(500).json({ success: false, error: 'The user information could not be modified.' });
+		});
+});
 
 module.exports = postRouter;
