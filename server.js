ff499776678a743d3232b0e46d6a16ae657c83fa
Jonathan Dorety <kyullog@gmail.com>
2019-03-05 23:04:48 -0500

    Create and apply middleware for user name capitalization

--- server.js
+++ server.js
@@ -4,8 +4,14 @@ const userRouter = require("./routers/userRouter.js");
 const postRouter = require("./routers/postRouter");
 
 const parser = express.json();
+const capitalizer = (req, res, next) => {
+  if (req.body.name) {
+    req.body.name = req.body.name.toUpperCase();
+  }
+  next();
+};
 
-server.use(parser);
+server.use(parser, capitalizer);
 server.use("/user", userRouter);
 server.use("/post", postRouter);
 
