af1f98e9774b8f9d1c9aab44ddaa4617f8f3ef4f
Jonathan Dorety <kyullog@gmail.com>
2019-03-05 21:24:42 -0500

    post get all endpoint works

--- routers/postRouter.js
+++ routers/postRouter.js
@@ -3,8 +3,13 @@ const db = require("../data/helpers/postDb.js");
 
 const postRouter = express.Router();
 
-postRouter.get("/", (req, res) => {
-  res.status(200).json("you did it again");
+postRouter.get("/", async (req, res) => {
+  try {
+    const getPosts = await db.get();
+    res.status(200).json(getPosts);
+  } catch {
+    res.status(500).json({ err: "There was a problem completing the request" });
+  }
 });
 
 module.exports = postRouter;
