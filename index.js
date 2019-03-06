14b0facc622891d590dd1c0a33de62de075c91e4
Taylorbelk1 <belktaylor12@gmail.com>
2019-03-06 16:12:04 -0600

    added basic code to verify the server works and we can reach it

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code away!
+const express = require('express');
+
+const server = express();
+const PORT = 9090;
+
+server.get('/', (req, res) => {
+    res.send('Hello World')
+});
+
+server.listen(PORT, () => {
+    console.log(`server running on port number ${PORT}...`)
+})
