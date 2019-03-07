309d0b6ded2b36944c38d42da97b4ddc9990dc90
Jesse Theisen <theiseja@gmail.com>
2019-03-06 22:25:36 -0600

    Doing some debugging for herkou deployment

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const postRoutes = require('./posts/postRoutes')
 const cors = require("cors");
 const helmet = require("helmet");
 const logger = require("morgan");
-const PORT = process.env.PORT || 9090;
+const PORT = process.env.PORT || 9091;
 const server = express();
 
 server.use(express.json(), logger("tiny"), helmet(), cors());
@@ -25,4 +25,6 @@ server.use('/', (req, res) => {
   res.json('API is running.')
 });
 
-server.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
+server.listen(PORT, () => {
+  console.log('\n*** Server Running on PORT ${PORT} ***\n');
+});
