7eb81516871cc63b315e5229dedf1b9f92062d5f
Mckay <mckaywrigley@gmail.com>
2019-03-08 14:18:34 -0700

    wired up post and user routes with router

---
+++ routes/postRoutes.js
@@ -0,0 +1,15 @@
+// Import Express
+const express = require('express');
+
+// Import Router
+const router = express.Router();
+
+// Import Post db
+const db = require('./data/helpers/postDb.js');
+
+
+// Endpoints
+
+
+// Export Router
+module.exports = router;
---
+++ routes/userRoutes.js
@@ -0,0 +1,15 @@
+// Import Express
+const express = require('express');
+
+// Import Router
+const router = express.Router();
+
+// Import User db
+const db = require('./data/helpers/userDb.js');
+
+
+// Endpoints
+
+
+// Export Router
+module.exports = router;
