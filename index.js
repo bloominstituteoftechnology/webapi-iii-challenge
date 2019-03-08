28df3a4f4e8df6fed4513a07ba665b73bbe86fe4
Mckay <mckaywrigley@gmail.com>
2019-03-08 14:16:09 -0700

    wired up index.js

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// code away!
+// Import Express
+const express = require('express');
+
+
+// Import Routes
+
+// User Routes
+const userRoutes = ('./routes/userRoutes.js');
+
+// Post Routes
+const postRoutes = ('./routes/postRoutes.js');
+
+
+// Initialize Express
+const server = express();
+
+
+// Middleware
+server.use(express.json());
+
+
+// Use Routes
+server.use('/api/users', userRoutes);
+server.use('/api/posts', postRoutes);
+
+
+// Run Server
+server.listen(3000, () => {
+    console.log('server running');
+});
