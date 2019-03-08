cd58cba8bfc3b5818b12c7c7cd3f276268638b2f
Jesse Theisen <theiseja@gmail.com>
2019-03-07 21:02:01 -0600

    Everything is working fine, ended up being a networking issue

--- server.js
+++ server.js
@@ -21,6 +21,13 @@ const errorHelper = (status, message, res) => {
 server.use('/api/users', userRoutes)
 server.use('/api/posts', postRoutes)
 
+server.get('/', (req, res, next) => {
+  res.send(`
+    <h2>Node Blog</h2>
+    <p>Welcome to the Node Blog API</p>
+    `);
+});
+
 server.listen(PORT, () => {
   console.log('\n*** Server Running on PORT ${PORT} ***\n');
 });
