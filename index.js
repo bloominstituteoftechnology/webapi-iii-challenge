3ab92ff5ef22cf0e3e7e58123bc4d19ae28b03da
Taylorbelk1 <belktaylor12@gmail.com>
2019-03-07 18:20:35 -0600

    changed port to be dynamic

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // code away!
+require('dotenv').config();
 const server = require('./server');
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
     console.log(`server running on port number ${PORT}...`)
