be2ee6409ed33d187cae27ac359f239ac7d8cc3e
kingmetri <demetribethel@gmail.com>
2019-03-07 20:45:42 -0500

    Added POST, PUT.

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const server = express();
-
 const cors = require('cors');
 
 const postDb = require('./data/helpers/postDb.js')
-const tabDb = require('./data/helpers/tagDb.js')
-const userDb = require('./data/helpers/userDb.js')
+// const tabDb = require('./data/helpers/tagDb.js')
+// const userDb = require('./data/helpers/userDb.js')
 
 
 
@@ -35,16 +34,49 @@ server.get('/api/posts/:id', (req, res) => {
 
     if(!id) {
         res.status(404);
-        res.json({error: 'This does not exist.'})
+        res.json({error: 'The post with the specified ID does not exist.'})
     }
     postDb.get(id)
     .then(response => {
         res.status(200).json(response)
     })
     .catch(err => {
-        sendServerError({error: 'We just missed it.'})
+        sendServerError({error: 'The post information could not be retrieved.'})
     })
 })
 
+server.post('/api/posts', (req, res) => {
+    const post = req.body;
+
+    if(!post) {
+        res.status(400).json({error: 'Please provide content for your post.'})
+    }
+
+    postDb.insert(post)
+    .then(response => {
+        res.status(200).json(response)
+    })
+    .catch(err => {
+        sendServerError({error: 'There was an error saving your post to the database.'})
+    })
+})
+
+server.put('/api/posts/:id', (req, res) => {
+    const id = req.params.id;
+    const post = req.body;
+
+    if(!id) {
+        res.status(404);
+        res.json({error: 'The post with the specified ID does not exist.'})
+    }
+
+    postDb.update(id, post) 
+    .then(response => {
+        res.status(200).json(response)
+    })
+    .catch(err => {
+        sendServerError({error: 'There was an error saving your changes.'})
+    })
+})
 
 server.listen(8000, () => console.log('API running'))
