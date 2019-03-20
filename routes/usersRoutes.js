2c3277542532778067d263cc04646652ce73bd29
Md Kawsar Hussen <mdkawsarhussen@Mds-MacBook-Pro.local>
2019-03-20 14:53:08 -0400

    server is setup, user and post route is created

---
+++ routes/usersRoutes.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const cors = require("cors");
+const server = express();
+
+// routes
+const postsRoutes = require("../routes/postsRoutes");
+const usersRoutes = require("../routes/usersRoutes");
+
+// middleware - global
+server.use(express.json());
+server.use(morgan("short"));
+server.use(helmet());
+server.use(cors());
+
+
+// routes - users
+server.use("/users", usersRoutes);
+// routes - posts
+server.use("/posts", postsRoutes);
+
+module.exports = server;
