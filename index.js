53080f7bf55d4e960dfceaae0172d2fa433730ab
duckets615 <33557104+duckets615@users.noreply.github.com>
2019-03-20 13:52:44 -0400

    server setup, middlware setup, imports

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+// server setup;
+const express = require('express');
+const server = express();
+server.use(express.json());
+const port = 4000;
 
+// MIDDLEWARE
+const logger = require('morgan');  // logging;
+const helmet = require('helmet'); //  security;
+const cors = require('cors');  // react calls;
+server.use(cors());
+
+
+// import server, db
+const userDb = require('./data/helpers/userDb.js');
+const postDb = require('./data/helpers/postDb.js');
