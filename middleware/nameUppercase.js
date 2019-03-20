8fcef056ebc362682c6a6d0875a52608c16c417c
Md Kawsar Hussen <mdkawsarhussen@Mds-MacBook-Pro.local>
2019-03-20 15:49:56 -0400

    middleware created for uppercase naming

---
+++ middleware/nameUppercase.js
@@ -0,0 +1,10 @@
+const upperCase =(req, res, next) =>{
+    const name = req.body.name;
+    if (name){
+        const capitalized = name.toUppercase();
+        req.body.name = capitalized;
+    }
+    next();
+};
+
+module.exports = upperCase;
