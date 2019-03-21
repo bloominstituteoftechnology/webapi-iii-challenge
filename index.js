0c94b38e4edb7166be9489772124fe85fc82dd45
omar salah <42569856+OmarSalah95@users.noreply.github.com>
2019-03-21 13:31:39 -0700

    Heroku modifications. Added envconfig file, added dotenv dependency and set up env variables

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // code away!
+require('dotenv').config()
 const server = require('./server.js')
-const port = 4000;
+const port = process.env.PORT || 4000;
+const host = process.env.HOST 
 
 server.listen(port, () => {
-    console.log(`Server Is Listening at http://localhost:${port}`)
+    console.log(`Server Is Listening at ${ port !== 4000 ? host : 'http://localhost:' + port}`)
 })
