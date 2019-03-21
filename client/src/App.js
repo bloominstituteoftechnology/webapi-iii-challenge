b904af3409db90e21fc79e707f4db7d24f651cff
Sem8 <mssemmi8@gmail.com>
2019-03-21 16:59:24 -0400

    added dotenv to dependencies for heroku deployment

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,52 @@
-import React, { Component } from 'react';
-import { Route, NavLink } from 'react-router-dom';
+import React, { Component } from "react";
+import { Route, NavLink } from "react-router-dom";
 
-import './App.css';
+import "./App.css";
 
 class App extends Component {
-
   state = {
-    users: []
+    users: [],
+    posts: []
   };
 
   componentDidMount() {
-    this.getUsers()   
-
+    this.getUsers();
   }
 
   getUsers = () => {
-    fetch('http://localhost:4000/api/users')
-    .then(res => res.json())
-    .then(res => this.setState({ users: res }));
-    
-  }
+    fetch("http://localhost:4000/api/users")
+      .then(res => res.json())
+      .then(res => this.setState({ users: res }))
+      .catch(err => console.log(err));
+  };
 
-  renderUser = ({name, id}) => (<div key={id}>
-    <p>{name}</p>
-  </div>
+  getPosts = () => {
+    fetch("http://localhost:4000/api/posts")
+      .then(res => res.json())
+      .then(res => this.setState({ posts: res }))
+      .catch(err => console.log(err));
+  };
+
+  getUserPost = (e, user_id) => {
+    e.preventDefault();
+    const { posts } = this.state;
+    fetch(`http://localhost:4000/api/${user_id}/posts`)
+      .then(res => res.json())
+      .then(this.getPosts)
+      .catch(err => console.log(err));
+  };
+
+  renderUser = ({ name, id, user_id }) => (
+    <div key={id}>
+      <p>{name}</p>
+      {/* <NavLink to='' ><button>Post</button></NavLink>  */}
+      <button onClick={e => this.getUserPost(e, id)}>Post</button>
+    </div>
   );
 
   render() {
     const { users } = this.state;
-    return (
-      <div className="App">
-      {users.map(this.renderUser)}
-        
-      </div>
-    );
+    return <div className="App">{users.map(this.renderUser)}</div>;
   }
 }
 
--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // code away!
+require('dotenv').config();
+
 const server = require('./server.js');
 
-server.listen(4000, () => {
-    console.log('\n Server running on port 4000');
+const port = process.env.PORT || 5000;
+
+server.listen(port, () => {
+    console.log(`\n Server running on port ${port}`);
 });
