2d18ffe1b2d591c4db43615b01a88f30e80f24e7
Sem8 <mssemmi8@gmail.com>
2019-03-21 18:02:11 -0400

    stretch added support for environment variables using .env files

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const cors = require('cors');
+const cors = require("cors");
 
 const usersRouter = require("./users-router");
 const postsRouter = require("./posts-router");
@@ -10,9 +10,23 @@ server.use(express.json());
 server.use(cors());
 
 server.get("/", (req, res) => {
-  res.send(
-    `Navigate to /api/users on the URL to get all the users then all their posts from /api/users/posts`
-  );
+  try {
+    const myMessage =
+      process.env.ENVARSUPPORT || "Environment support from env did not work";
+    res
+      .status(200)
+      .json({
+        myEnvMessage: myMessage,
+        otherMessage:
+          "Navigate to /api/users on the URL to get all the users then all their posts from /api/users/posts"
+      });
+  } catch (error) {
+    console.error("\nERROR", error);
+    res.status(500).json({ error: "Cannot retrieve the shoutouts" });
+  }
+  // res.send(
+  //   "Navigate to /api/users on the URL to get all the users then all their posts from /api/users/posts", "myMessage" myMessage
+  // );
 });
 
 server.use("/api/users", usersRouter);
