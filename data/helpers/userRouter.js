c3ee3b9749b75e966380d51776ca421c5b7ef722
omar salah <42569856+OmarSalah95@users.noreply.github.com>
2019-03-20 20:02:56 -0700

    Finally done with MVP full functionality completed :)

--- data/helpers/userRouter.js
+++ data/helpers/userRouter.js
@@ -3,7 +3,8 @@ const userRouter = require('express').Router();
 const db = require('./userDB.js');
 
 const prepUsername = (req, res, next) => {
-  req.body.name[0].toUpperCase();
+  const username = req.body.name[0].toUpperCase() + req.body.name.slice(1);
+  req.body.name = username;
   next();
 }
 
@@ -17,8 +18,8 @@ userRouter.get('/', async (req,res) => {
 })
 userRouter.get('/:id', async (req, res) => {
     try{
-        const user = await db.getById(id);
-        user
+        const user = await db.getById(req.params.id);
+        user.id
             ? res.status(200).json(user)
             : res.status(404).json({message: "That user doesn't exist"})
     }catch(err){
@@ -34,18 +35,40 @@ userRouter.delete('/:id', async (req, res) => {
             : res.status(404).json ({message: "User not found"})
     }catch(err){
         console.log(err);
-        res.status(500).json({message: `Failed to communicate with server due to${err}`})
+        res.status(500).json({message: `Failed to communicate with server due to \n**${err}**`})
     }
 })
-userRouter.post('/:id', prepUsername, (req, res) => {
-    db.getById(req.params.id)
-        .then(()=>{})
-        .catch(()=>{})
+userRouter.post('/', prepUsername, (req, res) => {
+    const user = req.body
+        db.insert(user)
+            .then(dbUser => {user.name ? res.status(200).json(dbUser) : res.status(400).json({message: "Can NOT add User"})})
+            .catch(err => {res.status(500).json({message: `Failed to communicate with server due to \n**${err}**`})})
 })
 userRouter.put('/:id', prepUsername, (req, res) => {
-    db.getById(req.params.id)
-        .then(()=>{})
-        .catch(()=>{})
+    db.update(req.params.id, req.body)
+        .then(count => {count ? res.status(200).json(count) : res.status(400).json({message: "Can NOT find User"}) })
+        .catch(err => { res.status(500).json({message: `Failed to communicate with server due to \n**${err}**`}) })
 })
 
+userRouter.get('/:id/posts', async (req, res) => {
+    db.getUserPosts(req.params.id)
+    .then(posts => { posts.length > 0 ? res.status(200).json(posts) : res.status(400).json({message: "Can NOT find User"})})
+    .catch(err => { res.status(500).json({message: `Failed to communicate with server due to \n**${err}**`})})
+})
+// I am stupid and did this twice because I added an S to the end point :)
+
+// userRouter.get('/:id/posts', async (req, res) => {
+//     try {
+//       const user = await db.getById(req.params.id);
+//       if (user) {
+//           const posts = await db.getUserPosts(id);
+//           res.status(200).json(posts);
+//     } else {
+//         res.status(404).json({ message: 'The user with the specified ID does not exist.' });
+//       }
+//     } catch (error) {
+//       console.log(error);
+//       res.status(500).json({ error: "The user's posts could not be retrieved." });
+//     }
+//   });
 module.exports = userRouter;
--- knexfile.js
+++ knexfile.js
@@ -7,7 +7,7 @@ module.exports = {
     },
     pool: {
       afterCreate: (conn, done) => {
-        conn.run('PRAGMA foreign_keys = ON', done);
+        conn.run('PRAGMA foreign_keys = OFF', done);
       },
     },
     migrations: {
