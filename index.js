a32bf7b07a53c76a62362cefed8f934dfb18f862
omar salah <42569856+OmarSalah95@users.noreply.github.com>
2019-03-21 14:21:26 -0700

    Heroku again

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ require('dotenv').config()
 const server = require('./server.js')
 
 const port = process.env.PORT || 4000;
-const host = process.env.HOST || 'http://localhost:'
+
 
 server.listen(port, () => {
-    console.log(`Server Is Listening at ${ host ? host : host + port}`)
+    console.log(`Server Is Listening at ${port}`)
 })
