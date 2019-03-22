d38786d476e19f78bd9815ca4e4b80e329acf28e
Eiosx <eiosxenzo@gmail.com>
2019-03-21 23:23:45 -0400

    Working on project. Added initial code for upperCase middleware.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// code away!
+const express = require('express');
+
+
+const server = express();
+const parser = express.json();
+const PORT = 3000;
+
+server.use(parser);
+
+function upperCase(req, res, next){
+    let { name } = req.body;
+    const upperCaseName = name.charAt(0).toUpperCase()+name.slice(1);
+    req.body.name = upperCaseName;
+    next();
+}
+
+
+
+server.listen(PORT, () => {
+    console.log(`The server is listening on port ${PORT}`);
+})
