9c468e3e52a44cd6f33f1e4320aa0d2094e989f0
Mckay <mckaywrigley@gmail.com>
2019-04-24 10:50:31 -0700

    create post

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,18 +1,41 @@
 // Import Express
-const express = require('express');
+const express = require("express");
 
 // Import Router
 const router = express.Router();
 
 // Import Post db
-const db = require('../data/helpers/postDb.js');
-
+const db = require("../data/helpers/postDb.js");
 
 // Endpoints
-router.get('/', (req, res) => {
-    res.send('hello 2');
+
+// Create
+router.post("/", (req, res) => {
+  const { text, user_id } = req.body;
+  if (!text || !user_id) {
+    res
+      .status(400)
+      .json({ errorMessage: "Please provide a title and user id." });
+  }
+  db.insert({ text, user_id })
+    .then(post => {
+      res.status(201).json({ post });
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: "There was an error while saving the post to the database."
+      });
+    });
 });
 
+// Read
+router.get("/", (req, res) => {
+  res.send("hello 2");
+});
+
+// Update
+
+// Destroy
 
 // Export Router
 module.exports = router;
