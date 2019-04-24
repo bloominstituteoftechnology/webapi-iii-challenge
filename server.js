2dfea3529e745956458866ae5b22cb12e2d7009b
jkaddu <jkaddu@yahoo.com>
2019-04-24 16:08:47 -0700

    working on uppercase

--- server.js
+++ server.js
@@ -9,11 +9,20 @@ const userRouter = require('./routers/userRoutes.js')
 
 server.use('./api/posts', postRouter);
 server.use('/api/users', userRouter);
+server.use(uppercase);
 
 server.get('/', (req, res) => {
     res.send(`
         <h2>Challenge III<h2>
     `)
 })
+
+function uppercase(req, res, next) {
+    let {name} = req.body;
+    const upperCaseName = name.charAt(0).toUpperCase()+name.slice(1);
+    req.body.name = upperCaseName;
+
+    next();
+}
 
 module.exports = server;
