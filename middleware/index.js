b06a0c595f5df1b61281ba2f500351a9af09dd06
Dustin Snoap <afrobacon@gmail.com>
2019-05-14 19:15:41 -0400

    Added validateUser middleware

--- middleware/index.js
+++ middleware/index.js
@@ -4,10 +4,12 @@
 */
 const logger = require('./logger')
 const validateUserId = require('./validateUserId')
+const validateUser = require('./validateUser')
 
 const warez = {
     logger,
     validateUserId,
+    validateUser,
 }
 
 module.exports = warez
