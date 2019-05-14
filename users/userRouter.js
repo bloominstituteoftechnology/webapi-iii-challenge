6d627526ec94191e69419b622adee76f83ad4a36
John O'Keefe <jokeefe@weegolobal.com>
2019-05-14 01:58:00 -0400

    POST new user complete

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,60 +1,52 @@
 const express = require('express');
-const db = require('./userDb')
+const db = require('./userDb');
 
 const router = express.Router();
 
 router.post('/', async (req, res) => {
-    // try {
-    //     const user = req.body
-    //     const newUser = await db.insert(user)
+    const user = req.body;
 
-    //     if (!user.name) {
-    //         res.status(403).json({ message: 'a user name is required'})
-    //     }
-    //     if (newUser) {
-    //         res.status(200).json({ message: 'User successfully added to database', newUser})
-    //     }
-    // } catch (error) {
-    //     res.status(500).json({ message: 'There was a problem adding a new user to the database', error })
-    // }
+    if (!user || !user.name) {
+        res.status(400).json({ message: 'A user name is required' });
+    } 
+    else {
+        try {
+            const newUser = await db.insert(user);
+
+            if (newUser) {
+                res.status(200).json({
+                    message: 'User successfully added to database',
+                    newUser
+                });
+            }
+        } catch (error) {
+            res.status(500).json({
+                message:
+                    'There was a problem adding a new user to the database',
+                error
+            });
+        }
+    }
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post('/:id/posts', (req, res) => {});
 
-});
+router.get('/', (req, res) => {});
 
-router.get('/', (req, res) => {
+router.get('/:id', (req, res) => {});
 
-});
+router.get('/:id/posts', (req, res) => {});
 
-router.get('/:id', (req, res) => {
+router.delete('/:id', (req, res) => {});
 
-});
-
-router.get('/:id/posts', (req, res) => {
-
-});
-
-router.delete('/:id', (req, res) => {
-
-});
-
-router.put('/:id', (req, res) => {
-
-});
+router.put('/:id', (req, res) => {});
 
 //custom middleware
 
-function validateUserId(req, res, next) {
+function validateUserId(req, res, next) {}
 
-};
+function validateUser(req, res, next) {}
 
-function validateUser(req, res, next) {
-
-};
-
-function validatePost(req, res, next) {
-
-};
+function validatePost(req, res, next) {}
 
 module.exports = router;
