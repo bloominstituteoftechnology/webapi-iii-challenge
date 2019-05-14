74d4b49058c91e7224f3636a215dccf58cb74bf2
Dustin Snoap <afrobacon@gmail.com>
2019-05-14 19:08:27 -0400

    Added validateUserId middleware

--- middleware/index.js
+++ middleware/index.js
@@ -1,7 +1,13 @@
+/*
+    breaking it up this much is proof of concept,
+    a single folder to hold all custom middle is probably reccomended
+*/
 const logger = require('./logger')
+const validateUserId = require('./validateUserId')
 
 const warez = {
     logger,
+    validateUserId,
 }
 
 module.exports = warez
---
+++ middleware/validateUserId.js
@@ -0,0 +1,15 @@
+const db = require('../users/userDb')
+
+validateUserId = async (req, res, next) => {
+    try {
+        const user = await db.getById(req.params.id)
+        user
+        ?   (req.user = user, next())
+        :   res.status(404).json({message: 'invalid user id'})
+    }
+    catch(err) {
+        res.status(500).json({message: 'server broke trying to process your request'})
+    }
+}
+
+module.exports = validateUserId
