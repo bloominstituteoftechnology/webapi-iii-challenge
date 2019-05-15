0d3581f7f41f3f923cc7e49b94ded0e146f34434
jal9626 <jal9626@gmail.com>
2019-05-14 22:47:36 -0400

    endpoints completed with one custom  middleware typeLogger

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,21 +1,87 @@
-const express = 'express';
+const express = require('express');
+
+const Posts = require('./postDb.js');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-
+// this only runs if the url has /api/hubs in it
+router.get('/', async (req, res) => {
+    try {
+        const posts = await Posts.get(req.query);
+        res.status(200).json(posts);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: 'Error retrieving the posts'
+        });
+    }
 });
 
-router.get('/:id', (req, res) => {
+// /api/posts/:id
+router.get('/:id', async (req, res) => {
+    try {
+        const post = await Posts.getById(req.params.id);
 
+        if (post) {
+            res.status(200).json(post);
+        } else {
+            res.status(404).json({ message: 'post not found; invalid id' })
+        }
+    } catch (err) {
+        res.status(500).json({ message: 'error retrieving post'})
+    }    
 });
 
-router.delete('/:id', (req, res) => {
-
+router.post('/', async (req, res) => {
+    const newPost = req.body;
+    
+    if (newPost) {
+        try {
+            const post = await Posts.insert(req.body);
+            res.status(201).json(post);
+        } catch (error) {
+            console.log(error); 
+                res.status(500).json({
+                    message: 'Error adding the post'
+                });
+        } 
+        } else {
+            res.status(400).json({
+                err: 'text property missing'
+            });
+    }        
 });
 
-router.put('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
+    try {
+        const count = await Posts.remove(req.params.id)
+        if (count > 0) {
+            res.status(200).json({ message: 'The post has been nuked' });
+        } else {
+            res.status(404).json({ message: 'The post could not be found' });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ 
+            message: 'Error removing post' 
+        });
+    }
+});
 
+router.put('/:id', async (req, res) => {
+    try {
+        const post = await Posts.update(req.params.id, req.body);
+        if (post) {
+            res.status(200).json(post);
+        } else {
+            res.status(404).json({ message: 'The post could not be found' });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: 'Error updating the post'
+        });
+    }
 });
 
 // custom middleware
--- server.js
+++ server.js
@@ -1,15 +1,36 @@
-const express = 'express';
+const express = require('express');
+const helmet = require('helmet');
+const logger = require('morgan');
+
+const postsRouter = require('./posts/postRouter.js')
 
 const server = express();
+
+//GLOBAL MIDDLEWARE
+
+//built in middleware
+server.use(express.json());
+
+//third party middleware
+server.use(helmet());
+server.use(logger('dev'));
+
+//custom middleware
+server.use(typeLogger)
+
+
+//router
+server.use('/api/posts', postsRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
 
-//custom middleware
 
-function logger(req, res, next) {
 
+function typeLogger(req, res, next) {
+    console.log(`${req.method} Request`);
+    next();
 };
 
 module.exports = server;
