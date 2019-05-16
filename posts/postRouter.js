937f82e8dc8ea79d905a11de46ca99e3edeac019
John O'Keefe <jokeefe@weegolobal.com>
2019-05-16 16:08:41 -0400

    clean up commit

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -3,26 +3,69 @@ const db = require('./postDb')
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-
+router.get('/', async (req, res) => {
+    try {
+        const allPosts = await db.get()
+        res.status(200).json(allPosts)
+    } catch (error) {
+        res.status(500).json({ message: 'There was an error getting all users from the database'})
+    }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', validatePostId, async (req, res) => {
+    const id = req.post
 
+    try {
+        res.status(200).json(id)
+    } catch (error) {
+        res.status(500).json({ message: 'There was an error getting user by id'})     
+    }    
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', validatePostId, async (req, res) => {
+    const { id } = req.post
 
+    try {
+        const deletedPost = await db.remove(id)
+        res.status(200).json({ message: `Successfully deleted post with id ${id}`, deletedPost})
+    } catch (error) {
+        res.status(500).json({ message: 'There was an error deleting post'})
+    }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', validatePostId, async (req, res) => {
+    const { id, user_id } = req.post
+    const newPost = req.body
 
+    try {
+        const updatedPost = await db.insert(id, { id, ...newPost, user_id})
+        res.status(200).json({ message: `Successfully updated post with id ${id}`, updatedPost})
+    } catch (error) {
+        res.status(500).json({ error: 'There was an error updating post', error})
+    }
 });
 
 // custom middleware
 
-function validatePostId(req, res, next) {
-
+async function validatePostId(req, res, next) {
+    const { id } = req.params;
+    try {
+        const post = await db.getById(id);
+        if (post) {
+            req.post = post;
+        } else {
+            res.status(400).json({ message: 'invalid post id' });
+        }
+    } catch (error) {
+        res.status(500).json({
+            error: 'There was an error validating post by id'
+        });
+    }
+    next();
 };
 
-module.exports = router;
+function errorMessage(err, req, res, next) {
+
+}
+
+module.exports = router
--- users/userRouter.js
+++ users/userRouter.js
@@ -3,31 +3,37 @@ const db = require('./userDb');
 
 const router = express.Router();
 
-router.post('/', async (req, res) => {
+router.post('/', validateUser, async (req, res) => {
     const user = req.body;
 
-    if (!user || !user.name || !Object.keys(user).includes('name')) {
-        res.status(400).json({ message: 'A user name is required' });
-    } else {
-        try {
-            const newUser = await db.insert(user);
+    try {
+        const newUser = await db.insert(user);
 
-            if (newUser) {
-                res.status(200).json({
-                    message: 'User successfully added to database',
-                    newUser
-                });
-            }
-        } catch (error) {
-            res.status(500).json({
-                error: 'There was a problem adding a new user to the database',
-                error
+        if (newUser) {
+            res.status(200).json({
+                message: 'User successfully added to database',
+                newUser
             });
         }
+    } catch (error) {
+        res.status(500).json({
+            error: 'There was a problem adding a new user to the database',
+            error
+        });
     }
 });
 
-router.post('/:id/posts', validateUserId,  async (req, res) => {});
+//TODO: Implement new db method to add posts to users
+// router.post('/:id/posts', validateUserId, validatePost, async (req, res) => {
+//     const { id } = req.params
+//     const { post } = req.body
+
+//     try {
+        
+//     } catch (error) {
+        
+//     }
+// });
 
 router.get('/', async (req, res) => {
     try {
@@ -35,22 +41,23 @@ router.get('/', async (req, res) => {
         res.status(200).json(users);
     } catch (error) {
         res.status(500).json({
-            error: 'There was a problem returning all users from database'
+            error: 'There was a problem getting all users from database'
         });
     }
 });
 
-router.get('/:id', validateUserId,  async (req, res) => {
-    const { id } = req.params
-    
-    try {
-        const user = await db.getById(id)
-        res.status(200).json(user)
-    } catch (error) {
-        res.status(500).json({ error: 'There was an error returning user by id'})
-    }
+router.get('/:id', validateUserId, async (req, res) => {
+    const { id } = req.params;
 
-}); //TODO
+    try {
+        const user = await db.getById(id);
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json({
+            error: 'There was an error returning user by id'
+        });
+    }
+});
 
 router.get('/:id/posts', validateUserId, async (req, res) => {
     const { id } = req.params;
@@ -68,79 +75,106 @@ router.get('/:id/posts', validateUserId, async (req, res) => {
         }
     } catch (error) {
         res.status(500).json({
-            error: 'There was a problem adding a new post to the database',
+            error: 'There was a problem getting all posts from user',
             error
         });
     }
 });
 
-router.delete('/:id', validateUserId,  async (req, res) => {
+router.delete('/:id', validateUserId, async (req, res) => {
     const { id } = req.params;
 
-    if (!id) {
-        res.status(400).json({
-            message: 'User id is required to delete a user'
-        }); // TODO: not displaying message
+    try {
+        const removedUser = await db.remove(id);
+
+        if (removedUser) {
+            res.status(200).json({
+                message: `User with id of ${id} successfully removed`
+            });
+        } else {
+            res.status(404).json({
+                message: `There was no user with id of ${id}`
+            });
+        }
+    } catch (error) {
+        res.status(500).json({
+            error: 'There was a problem removing user',
+            error
+        });
+    }
+});
+
+router.put('/:id', validateUserId, async (req, res) => {
+    const { id } = req.params;
+    const { name } = req.body;
+    const userInfo = req.body;
+
+    if (!name) {
+        res.status(400).json({ message: `A user name is required` });
     } else {
         try {
-            const removedUser = await db.remove(id);
-
-            if (removedUser) {
-                res.status(200).json({
-                    message: `User with id of ${id} successfully removed`
-                });
-            } else {
-                res.status(404).json({
-                    message: `There was no user with id of ${id}`
-                });
-            }
+            const updatedUser = await db.update(id, userInfo);
+            res.status(200).json({ message: 'User info has been updated' });
         } catch (error) {
             res.status(500).json({
-                error: 'There was a problem removing user',
-                error
+                error: `There was a problem updating the user`
             });
         }
     }
 });
 
-router.put('/:id', validateUserId,  async (req, res) => {
-    const { id } = req.params
-    const { name } = req.body
-    const userInfo = req.body
-
-    if (!name) {
-        res.status(400).json({ message: `A user name is required`})
-    }
-    else {
-        try {
-            const updatedUser = await db.update(id, userInfo)
-            res.status(200).json({ message: 'User info has been updated'})
-        } catch (error) {
-            res.status(500).json({ error: `There was a problem updating the user`})
-        }
-    }
-});
-
 //custom middleware
-
 async function validateUserId(req, res, next) {
-    const { id } = req.params
+    const { id } = req.params;
     try {
-        const user = await db.getById(id)
+        const user = await db.getById(id);
         if (user) {
-            req.user = user
-        }
-        else {
-            res.status(400).json({ message: "invalid user id" })
+            req.user = user;
+        } else {
+            res.status(400).json({ message: 'invalid user id' });
         }
     } catch (error) {
-        res.status(500).json({ error: 'There was an error validating user by id'})
+        res.status(500).json({
+            error: 'There was an error validating user by id'
+        });
     }
-    next()
+    next();
 }
 
-function validateUser(req, res, next) {}
+function validateUser(req, res, next) {
+    const { body } = req;
+    const { name } = req.body;
 
-function validatePost(req, res, next) {}
+    if (Object.entries(body).length === 0) {
+        res.status(400).json({
+            message: 'missing user data'
+        });
+    } //Todo: not displaying message when body is missing
+    if (!name) {
+        res.status(400).json({
+            message: 'missing required name field'
+        });
+    } else {
+        next();
+    }
+}
+
+function validatePost(req, res, next) {
+    const { body } = req;
+    const { text } = req.body;
+
+    if (Object.entries(body).length === 0) {
+        res.status(400).json({
+            message: 'missing post data'
+        });
+    } //Todo: not displaying message when body is missing
+    if (!text) {
+        res.status(400).json({
+            message: 'missing required text field'
+        });
+    } else {
+        next();
+    }
+}
 
 module.exports = router;
