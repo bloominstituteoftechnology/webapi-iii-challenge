953eccf2dccc5e2a21643d6162d3964f727dc160
coriagogo <pkcparis@gmail.com>
2019-05-29 18:42:03 -0400

    added stretch - add posts and posts routes

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,27 +1,54 @@
-const express = 'express';
+const express = require('express');
+
+const Posts = require('./postDb.js');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-
+router.get('/', async (req, res) => {
+    try {
+        const posts = await Posts.get(req.query);
+        res.status(200).json(posts);
+    } catch(error) {
+        res.status(500).json({ message: 'Error retrieving posts.' });
+    }
 });
 
-router.get('/:id', (req, res) => {
-
+router.get('/:id', async (req, res) => {
+    try {
+        const individualPost = await Posts.getById(req.params.id);
+        res.status(200).json(individualPost);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: 'Post not found.' });
+    }
 });
 
-router.delete('/:id', (req, res) => {
-
+router.delete('/:id', validatePostId, async (req, res) => {
+    try {
+        res.status(200).json(await Posts.remove(req.params.id));
+    } catch (error) {
+        res.status(500).json({ message: 'Error removing post.' });
+    }
 });
 
-router.put('/:id', (req, res) => {
-
+router.put('/:id', validatePostId, async (req, res) => {
+    try {
+        res.status(200).json(await Posts.update(req.params.id, req.body));
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: 'Error updating user.' });
+    }
 });
 
 // custom middleware
 
 function validatePostId(req, res, next) {
-
+    if(!req.params.id) {
+        res.status(400).json({ message: 'Invalid User ID' });
+      } else {
+          req.post = `${req.params.id}`;
+      }
+      next();
 };
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const userRouter = require('./users/userRouter.js');
+const postRouter = require('./posts/postRouter.js');
 
 const server = express();
 
@@ -8,6 +9,7 @@ server.use(express.json());
 server.use(logger);
 
 server.use('/api/users', userRouter);
+server.use('/api/posts', postRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
