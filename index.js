79fece049e85147ec401a993ccff455a1d1556b1
joerlop <jj.erlich155@gmail.com>
2019-05-30 14:19:21 -0500

    Changes for Heroku

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const server = require("./server");
 
-server.listen(4000, () => {
-  console.log('\n*** Server Running on http://localhost:4000 ***\n');
+const port = process.env.PORT || 5000;
+
+server.listen(port, () => {
+  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
