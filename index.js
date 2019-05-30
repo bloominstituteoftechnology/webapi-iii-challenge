15f0a5cbb0eebfc209e3fb19101dfc7db22ada43
Vijay Das <vijaytdas@gmail.com>
2019-05-30 16:42:00 -0500

    add heroku stuff

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 // code away!
 
+require("dotenv").config();
+
+const port = process.env.PORT  || 3030
+
 const server = require('./server.js');
 
-server.listen(3030, () => {
-  console.log('\n* Server Running on http://localhost:3030 *\n');
+server.listen(port, () => {
+  console.log(`\n* Server Running on http://localhost: ${port}**\n`);
 });
 
