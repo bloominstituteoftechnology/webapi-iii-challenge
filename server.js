e0a68433fcfb734c62c3ee6707a2126e10713b53
kly001 <karenly1961@gmail.com>
2019-06-12 02:11:22 -0400

    postRouter.js

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const server = express();
+const helmet = require ('helmet');
+const logger = require('morgan');
+
+server.use(express.json());
+server.use(helmet());
+server.use(logger('dev'))
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
 
-server.use(express.json());
 
-//custom middleware
 
-function logger(req, res, next) {
 
-};
+
 
 module.exports = server;
