54a1cf3180d84badacd8dfe297c3db142b3cfbeb
kly001 <karenly1961@gmail.com>
2019-06-11 22:18:28 -0400

    Initial Commit

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-const express = 'express';
-
+const express = require('express');
 const server = express();
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
+
+server.use(express.json());
 
 //custom middleware
 
