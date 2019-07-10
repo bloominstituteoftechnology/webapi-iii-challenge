763216a852d959ad5ee60d303497ed1d984791cb
Haja Andriamaro <haja.andri@gmail.com>
2019-07-10 15:13:46 +0200

    connected to post Router

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const userRouter = require('./users/userRouter');
+const postRouter = require('./posts/postRouter')
 // ES 6 version
 // import express, { json } from 'express';
 // import userRouter from './users/userRouter';
@@ -8,6 +9,7 @@ const server = express();
 
 server.use(express.json());
 server.use('/api/users', userRouter);
+server.use('/api/posts', postRouter);
 
 
 server.listen(4000, () => {
