30612abb1b229ec042d020bd8e9a420aea998a53
Haja Andriamaro <haja.andri@gmail.com>
2019-07-10 18:26:35 +0200

    logger middleware working

--- index.js
+++ index.js
@@ -5,11 +5,20 @@ const postRouter = require('./posts/postRouter')
 // import express, { json } from 'express';
 // import userRouter from './users/userRouter';
 
+function logger(req, res, next){
+    console.log('Request method: ', req.method);
+    console.log('Request URL: ', req.url);
+    console.log('Request timestamp: ', new Date());
+    next();
+}
+
 const server = express();
 
 server.use(express.json());
+server.use(logger);
 server.use('/api/users', userRouter);
 server.use('/api/posts', postRouter);
+
 
 
 server.listen(4000, () => {
