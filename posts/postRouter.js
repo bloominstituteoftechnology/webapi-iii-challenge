c3dac810c5e382474b7b3943464310dcf1ae5abc
Cai Martin <cmartin.professional@yahoo.com>
2019-07-10 18:08:09 -0400

    Built skeleton but getting a req.body error

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,27 +1,61 @@
-const express = 'express';
-
+const express = require("express");
+const data = require("./postDb");
+const { logger } = require("../server");
 const router = express.Router();
 
-router.get('/', (req, res) => {
-
+router.get("/", logger, (req, res) => {
+  data
+    .get()
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(error => {
+      error.status(404).json("error: could not find post");
+    });
 });
 
-router.get('/:id', (req, res) => {
-
+router.get("/:id", logger, validatePostId, (req, res) => {
+  data
+    .getById(req.params.id)
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(error => {
+      error.status(404).json("error: could not find post");
+    });
 });
 
-router.delete('/:id', (req, res) => {
-
+router.delete("/:id", logger, validatePostId, (req, res) => {
+  data
+    .remove(req.params.id)
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(error => {
+      error.status(404).json("error: could not find post");
+    });
 });
 
-router.put('/:id', (req, res) => {
-
+router.put("/:id", logger, validatePostId, (req, res) => {
+  console.log(req.body);
+  data
+    .update(req.params.id, req.body)
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(error => {
+      error.status(404).json("error: could not find post");
+    });
 });
 
 // custom middleware
 
 function validatePostId(req, res, next) {
-
-};
+  if (req.params.id) {
+    next();
+  } else {
+    res.status(400).send("Please provide valid info");
+  }
+}
 
 module.exports = router;
