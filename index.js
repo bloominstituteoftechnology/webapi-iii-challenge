57a88c30add1e7dc59a312c36926220685fc2e8f
Haja Andriamaro <haja.andri@gmail.com>
2019-07-10 15:07:59 +0200

    connected to user Router

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const userRouter = require('./users/userRouter');
+// ES 6 version
+// import express, { json } from 'express';
+// import userRouter from './users/userRouter';
 
 const server = express();
 
 server.use(express.json());
+server.use('/api/users', userRouter);
 
 
 server.listen(4000, () => {
