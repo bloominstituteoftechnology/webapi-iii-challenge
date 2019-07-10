6da716fc88a3aff4900030107a45eb0f5f5e16fb
Haja Andriamaro <haja.andri@gmail.com>
2019-07-10 15:31:42 +0200

    POST new user working

--- users/userRouter.js
+++ users/userRouter.js
@@ -2,8 +2,22 @@ const express = require("express");
 const Users = require("./userDb");
 const router = express.Router();
 
-router.post('/', (req, res) => {
-    res.status(200).json({message: 'root end point'})
+router.post('/', async (req, res) => {
+    const { name } = req.body;
+    console.log(name);
+    if (!name){
+        res.status(400).json({ errorMessage: "Please provide the user name." });
+    }
+    else {
+        try {
+            const newUser = await Users.insert({name});
+            //const newUserData =  await Users.getById(newUserId);
+            res.status(200).json(newUser);
+        } 
+        catch (error) {
+            res.status(500).json({ errorMessage: "There was an error while adding the new user" });
+        }
+    } 
 });
 
 router.post('/:id/posts', (req, res) => {
