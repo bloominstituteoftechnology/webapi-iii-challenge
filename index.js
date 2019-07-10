bc12d7840844be8e05c430992f1b4effcea11923
Haja Andriamaro <haja.andri@gmail.com>
2019-07-10 14:55:02 +0200

    started up the server

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// code away!
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+
+server.listen(4000, () => {
+  console.log('\n*** Server Running on http://localhost:4000 ***\n');
+});
