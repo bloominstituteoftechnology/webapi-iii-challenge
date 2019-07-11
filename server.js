679c06983aef3762837aea6f5ceef41a726c9394
Blake Mitchell <mblakeandrew@gmail.com>
2019-07-11 15:08:48 -0700

    logger middleware added and working, validateUser in progress

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
-const express = 'express';
+const express = require('express');
+const time = require("express-timestamp");
+const userDb = require("./users/userDb.js")
+const postDb = require("./posts/postDb.js")
+const validateUser = require("./users/userRouter.js")
 
 const server = express();
-
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
-});
-
+server.use(time.init);
 //custom middleware
 
 function logger(req, res, next) {
-
+  console.log(`method: ${req.method}`)
+  console.log(`url: ${req.path}`)
+  console.log(`time: ${req.timestamp}`); 
+  next();
 };
+server.use(logger);
+server.use('/users', validateUser)
+
+server.get('/users', (req, res) => {
+  res.send(`<h2>Let's write some middleware!</h2>`);
+});
 
 module.exports = server;
