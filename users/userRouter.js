a20a827b372855e6960174423606ba20c4ac2f8b
Ty Lippe <tylippe94@gmail.com>
2019-07-11 18:11:03 -0400

    added get, post, put and delete

--- users/userRouter.js
+++ users/userRouter.js
@@ -6,20 +6,20 @@ const User = require('./userDb.js');
 const Post = require('../posts/postDb.js');
 
 //Post a new user
-router.post('/', validateUser, async, (req, res) => {
+router.post('/', validateUserId, async, (req, res) => {
     const { userName } = req.body;
     
     try {
         const newUser = await User.insert({ userName });
         res.status(201).json(newUser)
     }
-    catch { 
+    catch (err) { 
         res.status(500).json({ errorMessage: 'There was an error while add the new user!' })
     }
 });
 
 //Post a comment on a specific user
-router.post('/:id/posts', validateUser, validatePost, async (req, res) => {
+router.post('/:id/posts', validateUserId, validatePost, async (req, res) => {
     const id = req.user.id;
     const text = req.body.text;
 
@@ -27,29 +27,86 @@ router.post('/:id/posts', validateUser, validatePost, async (req, res) => {
         const newPost = await Post.insert({ text, id })
         res.status(201).json(newPost)
     }
-    catch (error) {
+    catch (err) {
         res.status(500).json({ errorMessage: 'There was an error while saving the post to the database.' })
     }
 });
 
-router.get('/', (req, res) => {
-
+//Get all users
+router.get('/', async (req, res) => {
+    try {
+        const users = await User.get();
+        res,status(200).json(users)
+    }
+    catch (err) {
+        res.status(500).json({ errorMessage: 'The users information could not be retrived.' })
+    }
 });
 
-router.get('/:id', (req, res) => {
-
+//Get user by ID
+router.get('/:id', async (req, res) => {
+    const id = req.user.id;
+    
+    try {
+        const user = await User.getById(id)
+            if(user) {
+                res.status(200).json(user);
+            } else {
+                res.status(404).json({ errorMessage: 'The user with the specified ID does not exist.' })
+            }
+    }
+    catch (err) {
+        res.status(500).json({ errorMessage: 'The user information could not be retrieved.' })
+    }
 });
 
-router.get('/:id/posts', (req, res) => {
+//Get post by ID
+router.get('/:id/posts', validateUserId, async (req, res) => {
+    const id = req.user.id;
 
+    try {
+        const post = await User.getUserPosts(id)
+            if(post && post.length > 0) {
+                res.status(200).json(post)
+            } else {
+                res.status(404).json({ errorMessage: 'The post with the specified user ID does not exist.' })
+            }
+    } catch (err) {
+        res.status(500).json({ errorMessage: 'The posts information could not be retrieved.' })
+    }
 });
 
-router.delete('/:id', (req, res) => {
+//Delete a user
+router.delete('/:id', validateUserId, async (req, res) => {
+    const id = req.user.id;
 
+    try {
+        const deleted = await User.remove(id);
+            if(deleted) {
+                res.status(200).end() 
+            }   else {
+                res.status(400).json({ errorMessage: 'User with that ID could not be deleted' })
+            }
+    } catch (err) {
+        res.status(500).json({ errorMessage: 'The user could not be remvoed.' })
+    }
 });
 
-router.put('/:id', (req, res) => {
+//Update a user
+router.put('/:id', validateUserId, validateUser, async (req, res) => {
+    
+    
+    try {
+        const userName = req.body
+        const newUserId = req.user.id
+        await User.update(newUserId, userName)
 
+        const updatedUser = await User.getById(newUserId)
+        res.status(201).json(updatedUser)
+    }
+    catch (err) {
+        res.status(500).json({ errorMessage: 'The user could not be updated.' })
+    }
 });
 
 //custom middleware
