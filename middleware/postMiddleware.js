92eebfe4317fa0dd4e9b895edd29f3af64469ee8
Haja Andriamaro <haja.andri@gmail.com>
2019-07-11 13:53:08 +0200

    refactored with a dedicated file for postRouter middleware

--- middleware/postMiddleware.js
+++ middleware/postMiddleware.js
@@ -4,8 +4,7 @@ module.exports = {
     validatePostId,
   };
 
-  // custom middleware
-
+  // custom middleware for postRouter
 async function validatePostId (req, res, next) {
     const postId = req.params.id;
     const currentPost = await Posts.getById(postId);
