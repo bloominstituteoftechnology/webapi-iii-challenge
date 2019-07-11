068a620a0703aef217f2bbaf36192bc40d086986
Ty Lippe <tylippe94@gmail.com>
2019-07-11 16:10:10 -0400

    deploying

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const server = require('./server.js');
-const port = process.env.port
+const port = process.env.PORT
 
 server.listen(port, () => {
     console.log(`\n* Server Running on http://localhost:${port} *\n`);
