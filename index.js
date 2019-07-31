7f4addf949e02b154214dacae011fea6f83fd50c
Mariana Huh <mhuh0176@gmail.com>
2019-07-30 18:20:09 -0700

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code away!
+const server = require('./server.js');
+
+const port = 4040;
+server.listen(port, () => {
+  console.log(`\n*** Listening on port ${port} ***\n`);
+});
--- server.js
+++ server.js
@@ -1,15 +1,24 @@
-const express = 'express';
+const express = require('express');
+const postRouter = require('./posts/postRouter');
+const userRouter = require('./users/userRouter');
+const helmet = require('helmet');
+const morgan = require('morgan');
 
 const server = express();
 
+server.use(morgan('dev'));
+server.use(logger);
+server.use(express.json());
+server.use(helmet);
+server.use('/api/posts', postRouter);
+server.use('/api/users', userRouter);
+
 server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
+  res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
-function logger(req, res, next) {
-
-};
+function logger(req, res, next) {}
 
 module.exports = server;
