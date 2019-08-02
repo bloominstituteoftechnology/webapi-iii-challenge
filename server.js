ec40bc1563b4db2acdc4a57f02241c2653c7ea84
Mariana Huh <mhuh0176@gmail.com>
2019-08-01 21:16:49 -0700

    added an h1

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ server.use('/api/posts', postRouter);
 server.use('/api/users', userRouter);
 
 server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
+  res.send(`<h1>Hello Universe!</h1><h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
