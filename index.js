a4a3cffb84ee5feaa5c0ad43f802b0afe03cde74
RyanSWest <west56309@gmail.com>
2019-08-01 22:39:49 -0500

    3rd

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ require('dotenv').config()
 const port = process.env.PORT ? process.env.PORT: 4000
 
 
-server.listen(4000, () => {
+server.listen(port, () => {
     console.log(`\n* Server Running on http://localhost:${port} *\n`);
 });
