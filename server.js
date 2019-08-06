1fd262155200c8039cdb8a4092b001488de86d1a
EvaGurtovaia <eva.gurtovaya@gmail.com>
2019-08-06 04:12:27 +0300

    MVP

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
-const express = 'express';
+const express = require("express");
 
 const server = express();
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
+const userRouter = require("./users/userRouter");
+
+server.use(express.json());
+server.use(logger);
+server.use("/api/users", userRouter);
+
+server.get("/", (req, res) => {
+    res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
 function logger(req, res, next) {
-
-};
+    console.log(
+        `Method: ${req.method}, url: ${
+            req.url
+        }, timestamp: ${new Date().toISOString()}`
+    );
+    next();
+}
 
 module.exports = server;
