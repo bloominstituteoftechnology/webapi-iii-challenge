d8eb6e1688f79b88833cd0e71d10455d6c9b5626
karl2365 <karlbeksa@gmail.com>
2019-08-07 17:21:14 -0400

    almost there

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // code away!
+const express = require('express');
+
+const server = express();
+const postRouter = require('./posts/postRouter')
+const userRouter = require('./users/userRouter')
+const serverRouter = require('./server');
+
+
+server.use(express.json());
+
+server.use('/', serverRouter)
+server.use('/api/users', userRouter);
+server.use('/api/posts', postRouter);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+server.get('/', (req, res) => {
+    res.send(`<h2>Let's write some middleware!</h2>`)
+  });
+
+
+server.listen(8000, () => console.log('\nAPI running on port 8000\n'));
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = 'express';
 
-const server = express();
+const server = require('express').Router();
+
+server.use(logger);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
@@ -9,7 +11,11 @@ server.get('/', (req, res) => {
 //custom middleware
 
 function logger(req, res, next) {
+  const method = req.method;
+  const url = req.url;
+  console.log(`you made a ${method} request to ${url} on [${new Date().toISOString()}]`);
+  next();
+}
 
-};
 
 module.exports = server;
