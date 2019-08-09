47b6de76f33d4a76f0720eb65c2a4b658a0e8106
Jengopockets <jengopockets@gmail.com>
2019-08-09 00:53:43 -0400

    mvp

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,27 +1,69 @@
 const express = 'express';
+const db = require('./postDb');
 
-const router = express.Router();
+const router = require('express').Router();
 
 router.get('/', (req, res) => {
-
+    db.get()
+    .then(posts => {
+        res.status(200).json(posts);
+    })
+    .catch(err => {
+        res.status(500).json({error: 'could not get post'})
+    })
 });
 
-router.get('/:id', (req, res) => {
-
+router.get('/:id',  validatePostId, (req, res) => {
+    const id = req.params.id;
+    db.getById(id)
+        .then(post => {
+            res.status(200).json(post)
+        })
+        .catch(err => {
+            res.status(500).json({error: 'error getting post'})
+        })
 });
 
-router.delete('/:id', (req, res) => {
-
+router.delete('/:id',  validatePostId, (req, res) => {
+    const id = req.params.id;
+    db.remove(id)
+        .then(post => {
+            res.status(204).json({message: `successfully deleted post ${id}`})
+        })
+        .catch(err => {
+            res.status(500).json({error: 'server error, did not delete post'})
+        })
 });
 
-router.put('/:id', (req, res) => {
-
+router.put('/:id',  validatePostId, (req, res) => {
+    const id = req.params.id;
+    const changes = req.body;
+    db.update(id, changes)
+        .then(post => {
+            res.status(204).json({message: `post ${id} succesfully updated`})
+        })
+        .catch(err => {
+            req.status(500).json({error: 'server error, did not update post'})
+        })
 });
 
 // custom middleware
 
 function validatePostId(req, res, next) {
-
+    const id = req.params.id;
+    db.getById(id)
+        .then(user => {
+            if (user) {
+                req.user = user;
+                next();
+            }
+            else {
+                res.status(400).json({message: 'invalid id'});
+            }
+        })
+        .catch (err => {
+            res.status(500).json({error: 'There was an error accessing that user from the database.'})
+        })
 };
 
 module.exports = router;
