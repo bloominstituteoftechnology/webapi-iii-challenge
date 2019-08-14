dd38199c115c9ff3696a7dce094cbd7206e2cd58
Ian Bryant <48700036+IXLives@users.noreply.github.com>
2019-08-13 21:12:29 -0400

    MVP Reached. Stopping here tonight

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
-const express = 'express';
+const express = require('express');
+const helmet = require('helmet')
+const bodyParser = express.json()
 
 const server = express();
+const userRouter = require('./users/userRouter')
+
+server.use(bodyParser)
+server.use(helmet())
+server.use(logger)
+
+server.use('/api/users', userRouter)
+
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
@@ -9,7 +19,10 @@ server.get('/', (req, res) => {
 //custom middleware
 
 function logger(req, res, next) {
-
+  const time = new Date().toLocaleTimeString()
+  const date = new Date().toLocaleDateString()
+  console.log(`${req.method} Request | http://localhost:4000${req.url} | ${date} , ${time}`)
+  next()
 };
 
 module.exports = server;
