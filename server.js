10b2173b057e4f8586fec1498b79e625e353dfa5
cahaug <caleahaug@gmail.com>
2019-08-14 14:54:24 -0700

    server running

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
-const express = 'express';
+const express = require('express');
+// const express = 'express';
 
 const server = express();
+
+server.use(express.json())
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
