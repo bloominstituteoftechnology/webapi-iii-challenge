ec1a018a731bec492bf9b46e811e13ac2fef794b
Ya'Kuana Davis <ydavis@bowdoin.edu>
2019-09-04 19:15:51 -0400

    WIP: Server running.

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
-const express = 'express';
+const express = require('express');
 
 const server = express();
 
+server.use(express.json());
+
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
-});
+}); 
 
 //custom middleware
 
 function logger(req, res, next) {
+  console.log(
+    `[${new Date().toISOString()}] from ${req.method} to ${req.get('Origin')}`
+  ); 
 
+  next(); 
 };
+
+server.use('/', logger);
 
 module.exports = server;
