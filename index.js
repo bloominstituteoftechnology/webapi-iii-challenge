914925f650c832219ccadd9385c063d218c22c4f
Scott Renz <scottrenz@scottrenz.com>
2019-09-04 16:02:53 -0400

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // code away!
+// import server from './api/server';
+const server = require('./server.js');
+
+const port = 8000;
+server.listen(port, () => console.log(`\n** API on port ${port} **\n`));
--- server.js
+++ server.js
@@ -9,7 +9,13 @@ server.get('/', (req, res) => {
 //custom middleware
 
 function logger(req, res, next) {
+  console.log(
+    `[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get(
+      'Origin'
+    )}`
+  );
 
-};
+  next();
+}
 
 module.exports = server;
