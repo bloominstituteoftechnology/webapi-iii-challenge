feeb70dbe22fc7f3970775ab0a233155dd8bd117
Mimi Hoang <miminhoang1@gmail.com>
2019-09-05 10:28:56 -0400

    Testing

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,152 @@
-const express = 'express';
+const express = require("express");
 
 const router = express.Router();
+const Users = require("./userDb");
+const Post = require("../posts/postDb");
 
-router.post('/', (req, res) => {
 
+router.post("/", validateUser, (req, res) => {
+  Users.insert(req.body)
+    .then(user => {
+      res.status(201).json(user);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        message: "Error adding the user."
+      });
+    });
 });
 
-router.post('/:id/posts', (req, res) => {
 
+router.post("/:id/posts", [validateUserId, validatePost], (req, res) => {
+  Post.insert(req.body)
+    .then(post => {
+      res.status(201).json(post);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        message: "Error adding post"
+      });
+    });
 });
 
-router.get('/', (req, res) => {
 
+router.get("/", (req, res) => {
+  Users.get(req.query)
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        message: "Error retrieving users."
+      });
+    });
 });
 
-router.get('/:id', (req, res) => {
 
+router.get("/:id", validateUserId, (req, res) => {
+  Users.getById(req.params.id)
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        message: "Error retrieving user."
+      });
+    });
 });
 
-router.get('/:id/posts', (req, res) => {
 
+router.get("/:id/posts", validateUserId, (req, res) => {
+  Users.getUserPosts(req.params.id)
+    .then(posts => {
+      res.status(201).json(posts);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        message: "Error getting user's posts."
+      });
+    });
 });
 
-router.delete('/:id', (req, res) => {
 
+router.delete("/:id", validateUserId, (req, res) => {
+  Users.remove(req.params.id)
+    .then(count => {
+      if (count > 0) {
+        res.status(200).json({
+          message: "The user has been deleted."
+        });
+      } else {
+        res.status(404).json({
+          message: "The user could not be found."
+        });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        message: "Error deleting the user."
+      });
+    });
 });
 
-router.put('/:id', (req, res) => {
 
+router.put("/:id", [validateUser, validateUserId], (req, res) => {
+  const id = req.params.id;
+  const changes = req.body;
+  Users.update(id, changes)
+    .then(post => {
+      res.status(201).json(post);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        message: "Error updating user"
+      });
+    });
 });
 
 //custom middleware
 
 function validateUserId(req, res, next) {
+  const { id } = req.params;
 
-};
+  Users.getById(id)
+    .then(user => {
+      if (user) {
+        next();
+      } else {
+        res.status(404).json({ message: "No user with given id." });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        message: "Error processing request."
+      });
+    });
+}
 
 function validateUser(req, res, next) {
-
-};
+  if (req.body && Object.keys(req.body).length > 0) {
+    next();
+  } else {
+    next({ message: "Bad request." });
+  }
+}
 
 function validatePost(req, res, next) {
-
-};
+  if (req.body && Object.keys(req.body).length > 0) {
+    next();
+  } else {
+    next({ message: "Bad request." });
+  }
+}
 
 module.exports = router;
