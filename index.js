df7ec41cd8c39d43219ad324d6d8ae6b666dae44
Jordan <jordandoan@hotmail.com>
2019-10-02 08:37:11 -0700

    Added logger

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// code away!
+const express = require('express');
+
+const userDB  = require('./users/userDb');
+const postDB = require('./posts/postDb');
+const userRoutes = require('./users/userRouter');
+const postRoutes = require('./posts/postRouter');
+
+const server = express();
+const port = 5000;
+
+server.use(express.json());
+server.use((req, res, next) => {
+  console.log(
+    `[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get('Origin')}`
+  );
+  next();
+});
+
+server.get('/', (req,res) => {
+  res.send("HOME PAGE!!!");
+})
+server.listen(port, () => {
+  console.log("Server has started!");
+});
