548d99a4931334d66905b3fbc1942474004e5538
Jordan <jordandoan@hotmail.com>
2019-10-02 09:30:14 -0700

    Implemented all crud routes

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const express = require('express');
 
-const userDB  = require('./users/userDb');
-const postDB = require('./posts/postDb');
 const userRoutes = require('./users/userRouter');
 const postRoutes = require('./posts/postRouter');
 
@@ -9,19 +7,17 @@ const server = express();
 const port = 5000;
 
 server.use(express.json());
+// logger
 server.use((req, res, next) => {
   console.log(
     `[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get('Origin')}`
   );
   next();
 });
-server.use((req,res,next) => {
-  req.user = "JORDO";
-  next();
-})
 
-server.use('api/posts/', postRoutes);
-server.use('api/users/', userRoutes);
+
+server.use('/api/posts/', postRoutes);
+server.use('/api/users/', userRoutes);
 
 server.get('/', (req,res) => {
   res.send("HOME PAGE!!!");
