2760e7788d2a2e659e83ae597fcfa49fc7da2de7
Kate Efimova <kate.efimova0@gmail.com>
2019-10-03 10:04:55 -0700

    Done with validate user

--- users/middleware/validateUserId.js
+++ users/middleware/validateUserId.js
@@ -2,11 +2,9 @@ const userDb = require("../userDb");
 
 const validateUserId = async (req, res, next) => {
   const user = await userDb.getById(req.params.id);
-  if (req.body.id && user) {
-    next();
-  } else {
-    res.status(400).json({ message: "Invalid user id" });
-  }
+  req.body.id && user
+    ? next()
+    : res.status(400).json({ message: "Invalid user id" });
 };
 
 module.exports = validateUserId;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const userRouter = express.Router();
 const postRouter = require("../posts/postRouter");
 const server = express();
+
 const userDb = require("./userDb");
 const validateUserId = require("./middleware/validateUserId");
-const userRouter = express.Router();
+const validateUser = require("./middleware/validateUser");
 
 userRouter.get("/", (req, res) => {
   userDb
@@ -29,28 +31,22 @@ userRouter.get("/:id", validateUserId, async (req, res) => {
   }
 });
 
-userRouter.get("/:id/posts", validateUserId, (req, res) => {});
+userRouter.get("/:id/posts", validateUserId, validateUser, (req, res) => {});
 
-userRouter.post("/", (req, res) => {
-  if (!req.body.id || !req.body.name)
-    res.status(400).json({
-      errorMessage: "Please provide title and contents for the user."
-    });
-  else {
-    userDb
-      .insert(req.body)
-      .then(user => {
-        res.status(201).json(user);
-      })
-      .catch(error => {
-        res.status(500).json({
-          error: "There was an error while saving the user to the database"
-        });
+userRouter.post("/", validateUser, (req, res) => {
+  userDb
+    .insert(req.body)
+    .then(user => {
+      res.status(201).json(user);
+    })
+    .catch(error => {
+      res.status(500).json({
+        error: "There was an error while saving the user to the database"
       });
-  }
+    });
 });
 
-userRouter.post("/:id/posts", (req, res) => {});
+userRouter.post("/:id/posts", validateUserId, (req, res) => {});
 
 userRouter.delete("/:id", validateUserId, (req, res) => {
   userDb
@@ -63,30 +59,18 @@ userRouter.delete("/:id", validateUserId, (req, res) => {
     });
 });
 
-userRouter.put("/:id", validateUserId, (req, res) => {
-  if (!req.body.name || !req.body.id) {
-    res.status(400).json({
-      errorMessage: "Please provide title and contents for the user."
+userRouter.put("/:id", validateUserId, validateUser, (req, res) => {
+  userDb
+    .update(req.params.id, req.body)
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({ error: "The user information could not be modified." });
     });
-  } else {
-    userDb
-      .update(req.params.id, req.body)
-      .then(user => {
-        res.status(200).json(user);
-      })
-      .catch(error => {
-        res
-          .status(500)
-          .json({ error: "The user information could not be modified." });
-      });
-  }
 });
-
-//custom middleware
-
-function validateUser(req, res, next) {}
-
-function validatePost(req, res, next) {}
 
 server.use("/:id/posts", userRouter);
 
