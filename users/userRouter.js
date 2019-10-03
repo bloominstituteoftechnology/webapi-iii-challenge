f2923044bda80a7048cc2d626b8a753ef7f03ff8
Kate Efimova <kate.efimova0@gmail.com>
2019-10-03 09:18:14 -0700

    Ternary statements, main user router done

--- users/userRouter.js
+++ users/userRouter.js
@@ -21,13 +21,11 @@ userRouter.get("/:id", async (req, res) => {
   try {
     const user = await userDb.getById(req.params.id);
 
-    if (user) {
-      res.status(200).json(user);
-    } else {
-      res
-        .status(404)
-        .json({ message: "The user with the specified ID does not exist." });
-    }
+    user
+      ? res.status(200).json(user)
+      : res
+          .status(404)
+          .json({ message: "The user with the specified ID does not exist." });
   } catch (error) {
     res
       .status(500)
@@ -37,13 +35,64 @@ userRouter.get("/:id", async (req, res) => {
 
 userRouter.get("/:id/posts", (req, res) => {});
 
-userRouter.post("/", (req, res) => {});
+userRouter.post("/", (req, res) => {
+  if (!req.body.id || !req.body.name)
+    res.status(400).json({
+      errorMessage: "Please provide title and contents for the user."
+    });
+  else {
+    userDb
+      .insert(req.body)
+      .then(user => {
+        res.status(201).json(user);
+      })
+      .catch(error => {
+        res.status(500).json({
+          error: "There was an error while saving the user to the database"
+        });
+      });
+  }
+});
 
 userRouter.post("/:id/posts", (req, res) => {});
 
-userRouter.delete("/:id", (req, res) => {});
+userRouter.delete("/:id", (req, res) => {
+  userDb
+    .remove(req.params.id)
+    .then(user => {
+      user != 1
+        ? res
+            .status(404)
+            .json({ message: "The user with the specified ID does not exist." })
+        : res.status(204).json({});
+    })
+    .catch(error => {
+      res.status(500).json({ error: "The user could not be removed" });
+    });
+});
 
-userRouter.put("/:id", (req, res) => {});
+userRouter.put("/:id", (req, res) => {
+  if (!req.body.name || !req.body.id) {
+    res.status(400).json({
+      errorMessage: "Please provide title and contents for the user."
+    });
+  } else {
+    userDb
+      .update(req.params.id, req.body)
+      .then(user => {
+        !user
+          ? res.status(404).json({
+              message: "The user with the specified ID does not exist."
+            })
+          : res.status(200).json(user);
+      })
+      .catch(error => {
+        res
+          .status(500)
+          .json({ error: "The user information could not be modified." });
+      });
+  }
+});
 
 //custom middleware
 
