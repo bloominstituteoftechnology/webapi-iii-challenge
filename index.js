5b04e84492f330fefcf5d1796bf4db08fe7221b8
Mildred Awuor <otieno.awuor.m@gmail.com>
2019-10-30 16:50:42 +0300

    listening on port 3000

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code away!
+
+const server = require("./server");
+
+server.listen(process.env.PORT || 3000, () => {
+  console.log(" server listening on port " + (process.env.PORT || 3000));
+});
