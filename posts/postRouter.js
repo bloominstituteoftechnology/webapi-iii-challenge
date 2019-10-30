596285e363e34cc1d7f131d2593fbf461462ff8c
jambis <imjaamees@gmail.com>
2019-10-30 15:58:23 -0500

    Server running with logger and postsRouter

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,27 +1,82 @@
-const express = 'express';
+const express = require("express");
+const db = require("./postDb");
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get("/", async (req, res) => {
+  const posts = await db.get();
 
+  try {
+    res.status(200).json(posts);
+  } catch {
+    res
+      .status(500)
+      .json({ error: "There was an error while fetching the posts" });
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", validatePostId, async (req, res) => {
+  const post = await db.getById(req.params.id);
 
+  try {
+    res.status(200).json(post);
+  } catch {
+    res
+      .status(500)
+      .json({ error: "There was an error while fetching the post" });
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", validatePostId, async (req, res) => {
+  const deleted = await db.remove(req.params.id);
 
+  try {
+    if (deleted) {
+      res.status(200).json({ message: "The post was successfully deleted" });
+    }
+  } catch {
+    res
+      .status(500)
+      .json({ error: "There was an error while deleting the post" });
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", validatePostId, async (req, res) => {
+  if (!req.body.text) {
+    return res.status(400).json({ message: "Please provide a text" });
+  }
 
+  const updated = await db.update(req.params.id, {
+    text: req.body.text,
+    user_id: req.user_id
+  });
+
+  try {
+    const post = await db.getById(req.params.id);
+    res.status(202).json(post);
+  } catch {
+    res
+      .status(500)
+      .json({ error: "There was an error while updating the post" });
+  }
 });
 
 // custom middleware
 
-function validatePostId(req, res, next) {
-
-};
+async function validatePostId(req, res, next) {
+  const post = await db.getById(req.params.id);
+  try {
+    if (post) {
+      req.user_id = post.user_id;
+      next();
+    } else {
+      res.status(404).json({ message: "There's no post with that ID" });
+    }
+  } catch {
+    res
+      .status(500)
+      .json({ error: "There was an error while handling the request" });
+  }
+}
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,15 +1,26 @@
-const express = 'express';
-
+const express = require("express");
 const server = express();
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
+const postRouter = require("./posts/postRouter");
+const userRouter = require("./users/userRouter");
+
+server.use(express.json());
+server.use(logger);
+server.use("/api/posts", postRouter);
+server.use("/api/users", userRouter);
+
+server.get("/", (req, res) => {
+  res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
 function logger(req, res, next) {
+  console.log(
+    `[${new Date().toISOString()}] ${req.method} to ${req.originalUrl}`
+  );
 
-};
+  next();
+}
 
 module.exports = server;
