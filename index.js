0b9006a18e54a1dfae893d8c8e57f03197c38afe
Mildred Awuor <otieno.awuor.m@gmail.com>
2019-10-31 16:21:41 +0300

    added .env

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // code away!
 
 const server = require("./server");
+const port = process.env.PORT;
 
-server.listen(process.env.PORT || 3000, () => {
-  console.log(" server listening on port " + (process.env.PORT || 3000));
+server.listen(port, () => {
+  console.log(" server listening on port " + port);
 });
