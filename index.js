f8721e19b2bbbede974b9e6fd6e90b3d94441dd2
RichardEsquivel <rlesquivel@gmail.com>
2019-10-31 11:13:48 -0500

    Commiting refactored code

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code away!
+const express = require('express');
+const userRouter = require('/users/userRouter.js')
+const postsRoutes = require('/posts/postRouter.js');
+
+
+server.listen(8000, () => console.log("Server listens on port 8000"));
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,104 @@
-const express = 'express';
-
+const express = require('express');
 const router = express.Router();
+const Users = require('./userDb');
+const Posts = require('../posts/postDb')
+
+
+router.use(validateUsers)
 
 router.post('/', (req, res) => {
-
+	Users.insert(req.body)
+		.then(newUser => {
+			console.log(newUser)
+			res.status(201).json({ Response: newUser })
+		})
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post('/:id/posts', validateUserIds, validatePosts, (req, res) => {
+
+	const userPost = {
+		user_id: req.body.user_id,
+		text: req.body.text
+	}
+
+	Posts.insert(userPost)
+		.then(newPost => {
+			console.log(newPost)
+			res.status(201).json({ Response: newPost })
+		})
 
 });
 
 router.get('/', (req, res) => {
-
+	Users.get()
+		.then(users => {
+			res.status(200).json({ Response: users })
+		})
 });
 
-router.get('/:id', (req, res) => {
-
+router.get('/:id', validateUserIds, (req, res) => {
+	res.status(200).json({ user: req.user })
 });
 
-router.get('/:id/posts', (req, res) => {
-
+router.get('/:id/posts', validateUserIds, (req, res) => {
+	Users.getUserPosts(req.params.id)
+		.then(userPosts => {
+			res.status(200).json({ Response: userPosts })
+		})
 });
 
-router.delete('/:id', (req, res) => {
-
+router.delete('/:id', validateUserIds, (req, res) => {
+	Users.remove(req.params.id)
+		.then(() => {
+			res.status(200).json({ Success: "That user was successfully deleted!" })
+		})
+		.catch(err => res.status(500).json({ Error: "The database was unable to delete the user." }))
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', validateUserIds, (req, res) => {
 
+	Users.update(req.params.id, req.body)
+		.then(() => {
+			res.status(200).json({ Success: "The user's name was updated correctly!" })
+		})
+		.catch(err => res.status(500).json({ Success: "The server was unable to update the users name.", Error: err }))
 });
 
-//custom middleware
+//Including custom middleware to be utilized for validating user id and that the user has user data and a required name field included.
 
-function validateUserId(req, res, next) {
+function validateUserIds(req, res, next) {
+	const userId = req.params.id;
+	Users.getById(userId)
+		.then(user => {
+			if (user) {
+				req.user = user
+				next()
+			} else {
+				res.status(400).json({ Error: "You provided an invalid user id." })
+			}
+		})
 
 };
 
-function validateUser(req, res, next) {
-
+function validateUsers(req, res, next) {
+	// Check there is a req body and also a name field
+	if (!req.body) {
+		res.status(400).json({ Error: "No user data provided!" });
+	} else if (!req.body.name) {
+		res.status(400).json({ Error: "Required name field not provided." });
+	}
+	next();
 };
 
-function validatePost(req, res, next) {
+function validatePosts(req, res, next) {
+	// checking for the req body and also text field
+	if (!req.body) {
+		res.status(400).json({ Error: "You are missing post data." });
+	} else if (!req.body.text) {
+		res.status(400).json({ Error: "Required text field has not been provided." });
+	}
 
+	next();
 };
 
 module.exports = router;
