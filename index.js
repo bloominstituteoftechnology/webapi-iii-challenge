78f89623c1468d74e1e5cb25ed79190d6f2af363
CandaceRossi <50240571+CandaceRossi@users.noreply.github.com>
2019-10-31 15:43:03 -0400

    built out the server adding the date logger and logger functions working on other middleware

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 // code away!
+
+const port = 8000;
+server.listen(port, () => console.log(`Listening on port ${port}`));
--- server.js
+++ server.js
@@ -1,15 +1,38 @@
-const express = 'express';
-
+const express = "express";
+const helmet = require("helmet");
 const server = express();
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
+server.get("/", (req, res) => {
+  const nameInsert = req.name ? `${req.name}` : "";
+  res.send(`
+      <h1>Welcome ${nameInsert} to user/posts!</h1>
+      `);
 });
 
-//custom middleware
+// const userRouter = require("./routes/userRouter");
+// const postRouter = require("./posts/postRouter");
+
+// server.use("./api-users", userRouter);
+// server.use("./api-posts", postRouter);
+
+function dateLogger(req, res, next) {
+  console.log(new Date().toISOString());
+
+  next();
+}
 
 function logger(req, res, next) {
+  console.log(`${req.method} to ${req.url}`);
 
-};
+  next();
+}
+
+//global middleware
+server.use(helmet());
+server.use(express.json());
+server.use(dateLogger);
+server.use(logger);
+
+//custom middleware
 
 module.exports = server;
