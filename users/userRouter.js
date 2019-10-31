9218a69b416200eddfa686ae84773b3501993c62
Dylan Mestyanek <mestyanekdylan@gmail.com>
2019-10-31 15:04:58 -0500

    Completed all routes, and 3/4 custom middleware.

--- users/userRouter.js
+++ users/userRouter.js
@@ -4,40 +4,77 @@ const db = require("./userDb");
 
 const router = express.Router();
 
+// Add a new user
 router.post('/', validateUser, (req, res) => {
     db.insert(req.body)
-    .then(post => res.status(200).json(post))
+    .then(post => res.status(201).json(post))
     .catch(err => res.status(500).json({ error: "Failed to add new user to database." }))
 });
 
-router.post('/:id/posts', (req, res) => {
-
+// Add a new post to a user
+router.post('/:id/posts', validatePost, (req, res) => {
+    db.insert(req.body)
+    .then(post => res.status(201).json(post))
+    .catch(err => res.status(500).json({ error: "Failed to add new post to database." }))
 });
 
+// Get all users
 router.get('/', (req, res) => {
-
+    db.get()
+    .then(users => res.status(200).json(users))
+    .catch(err => res.status(500).json({ error: "Failed to retrieve all users." }))
 });
 
-router.get('/:id', (req, res) => {
-
+// Get a specific user by ID
+router.get('/:id', validateUserId, (req, res) => {
+    db.getById(req.params.id)
+    .then(user => res.status(200).json(user))
+    .catch(err => res.status(500).json({ error: `Failed to find user with an ID of ${id}.` }))
 });
 
-router.get('/:id/posts', (req, res) => {
+// Get a users posts
+router.get('/:id/posts', validateUserId, (req, res) => {
+    const id = req.params.id;
 
+    db.getById(id)
+    .then(user => res.status(200).json(user))
+    .catch(err => res.status(500).json({ error: `Failed to retrieve posts from user with an ID of ${id}.` }))
 });
 
-router.delete('/:id', (req, res) => {
+// Remove user by ID
+router.delete('/:id', validateUserId, (req, res) => {
+    const id = req.params.id;
 
+    db.remove(id)
+    .then(removed => res.status(200).json(removed))
+    .catch(err => res.status(500).json({ error: `Failed to remove user with ID of ${id}.` }))
 });
 
-router.put('/:id', (req, res) => {
+// Edit user by ID
+router.put('/:id', validateUserId, validateUser, (req, res) => {
+    const id = req.params.id;
+    const updatedInfo = req.body;
 
+    db.update(id, updatedInfo)
+    .then(updated => {
+        db.getById(id)
+        .then(user => res.status(200).json(user))
+    })
+    .catch(err => res.status(500).json({ error: `Failed to update user with ID of ${id}.` }))
 });
 
 //custom middleware
-
 function validateUserId(req, res, next) {
-
+    const id = req.params.id;
+    
+    db.getById(id)
+    .then(user => {
+        if (user){
+            req.user = id;
+            next();
+        } else res.status(400).json({ errorMessage: "Invalid user id." })
+    })
+    .catch(err => res.status(500).json({ error: `Failed to retrieve user with an ID of ${id}.` }))
 };
 
 function validateUser(req, res, next) {
@@ -47,11 +84,18 @@ function validateUser(req, res, next) {
         res.status(400).json({ errorMessage: "Missing user data." })
     } else if (!newUser.name) {
         res.status(400).json({ errorMessage: "Missing required name field." })
-    } else next();
+    } else next();    
 };
 
 function validatePost(req, res, next) {
+    const newPost = req.body;
 
+    if (!newPost) {
+        res.status(400).json({ errorMessage: "Missing post data." })
+    } else if (!newPost.text) {
+        res.status(400).json({ errorMessage: "Missing required text field." })
+    } else next();
 };
+
 
 module.exports = router;
