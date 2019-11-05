12332e8a93555e779b93c3dc2a651333f6aac5ae
MicahJank <MicahJank@users.noreply.github.com>
2019-11-05 12:58:43 -0600

    created the logger middleware function

--- server.js
+++ server.js
@@ -1,15 +1,32 @@
-const express = 'express';
+const express = require('express');
+const userRouter = require('./users/userRouter.js');
+const postRouter = require('./posts/postRouter.js');
 
 const server = express();
+
+const middleware = [
+  logger
+];
+
+server.use(express.json());
+
+server.use(middleware);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
 
+
 //custom middleware
 
 function logger(req, res, next) {
-
+  const date = Date();
+  console.log({
+    request_method: req.method,
+    request_url: req.url,
+    timestamp: Date().toString()
+  });
+  next();
 };
 
 module.exports = server;
