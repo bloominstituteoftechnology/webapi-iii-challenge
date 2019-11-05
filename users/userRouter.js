0d051b01167c3668c0da472a405ab68421024378
MicahJank <MicahJank@users.noreply.github.com>
2019-11-05 17:33:14 -0600

    created the validatepost middleware function

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const User = require('./userDb.js');
+const Post = require('../posts/postDb.js');
 
 const router = express.Router();
 
@@ -79,7 +80,22 @@ function validateUser(req, res, next) {
 };
 
 function validatePost(req, res, next) {
+    const { text } = req.body;
 
+    if(!req.body) {
+        res.status(400).json({ message: 'missing post data' });
+    } else if(!text) {
+        res.status(400).json({ message: 'missing required text field' });
+    } else {
+        Post.insert(req.body)
+            .then(post => {
+                req.post = post;
+                next();
+            })
+            .catch(err => {
+                res.status(500).json({ message: 'there was a problem adding the post to the database' });
+            });
+    };
 };
 
 module.exports = router;
