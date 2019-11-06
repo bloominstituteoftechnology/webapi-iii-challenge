565d0a867f07f7b2ad17d585f04d41debf567437
MicahJank <MicahJank@users.noreply.github.com>
2019-11-06 16:48:35 -0600

    working on heroku deployment

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-// code away!
+require('dotenv').config();
 
 const server = require('./server.js');
 
-server.listen(4000, () => {
-    console.log('server listening on port 4000');
+const port = process.env.PORT;
+
+server.listen(port, () => {
+    console.log(`server listening on port ${port}`);
 });
