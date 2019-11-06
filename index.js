dd6e8265ef000c7f6abb80096a4b5d04f78718ae
AceIsHuman <menoman1995@yahoo.com>
2019-11-05 21:49:26 -0600

    import server and listen on port 5000

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // code away!
+const server = require("./server");
+
+server.listen(5000, () => {
+  console.log("==== Magical things on 5000 =====");
+});
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = 'express';
+const express = require('express');
 
 const server = express();
 
