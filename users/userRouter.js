0a925f15ce036b4a236ad70d5553f2a489e232d3
AceIsHuman <menoman1995@yahoo.com>
2019-11-05 23:14:15 -0600

    create validateUserId middleware

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,39 @@
-const express = 'express';
+const express = require("express");
+const userDb = require("./userDb");
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {});
 
-});
+router.post("/:id/posts", (req, res) => {});
 
-router.post('/:id/posts', (req, res) => {
+router.get("/", (req, res) => {});
 
-});
+router.get("/:id", (req, res) => {});
 
-router.get('/', (req, res) => {
+router.get("/:id/posts", (req, res) => {});
 
-});
+router.delete("/:id", (req, res) => {});
 
-router.get('/:id', (req, res) => {
-
-});
-
-router.get('/:id/posts', (req, res) => {
-
-});
-
-router.delete('/:id', (req, res) => {
-
-});
-
-router.put('/:id', (req, res) => {
-
-});
+router.put("/:id", (req, res) => {});
 
 //custom middleware
 
-function validateUserId(req, res, next) {
+async function validateUserId(req, res, next) {
+  const id = req.params.id;
+  try {
+    const user = await userDb.getById(id);
+    user
+      ? (req.user = user)
+      : res.status(400).json({ message: "Invalid user ID" });
+  } catch (err) {
+    return res.status(500).json({ errorMessage: "Internal Server Error" });
+  }
+  next();
+}
 
-};
+function validateUser(req, res, next) {}
 
-function validateUser(req, res, next) {
-
-};
-
-function validatePost(req, res, next) {
-
-};
+function validatePost(req, res, next) {}
 
 module.exports = router;
