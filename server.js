961b6287735bb98b47e69e642a45395236650c9e
Jashele T <jashele@Jasheles-Air.domain>
2019-11-07 19:25:10 -0700

    added logger custom middleware

--- server.js
+++ server.js
@@ -1,14 +1,29 @@
 const express = require("express");
-const server = express();
 const userRouter = require("./users/userRouter");
 const postRouter = require("./posts/postRouter");
 
+const server = express();
+
+server.use(express.json());
+server.use(logger);
 server.use("/api/users", userRouter);
 server.use("/api/posts", postRouter);
-server.use(express.json());
+
 
 server.get("/", (req, res) => {
-  res.send(`<h2>Hey...</h2>`);
+  res.send(`<h2>Jashele Tillman</h2>`);
 });
+
+// logger logs to the console the following information about each request: request method, request url, and a timestamp
+// this middleware runs on every request made to the API
+
+function logger(req, res, next) {
+  console.log(`
+  ${req.method}
+  ${req.url}
+  ${req.get("host")}
+}`);
+  next();
+}
 
 module.exports = server;
