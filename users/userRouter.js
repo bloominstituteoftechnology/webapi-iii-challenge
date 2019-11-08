80b1629c796892cfd9148b35223c2c1fe4aa34f5
AceIsHuman <menoman1995@yahoo.com>
2019-11-07 19:37:40 -0600

    use middleware on endpoints required

--- users/userRouter.js
+++ users/userRouter.js
@@ -3,19 +3,19 @@ const userDb = require("./userDb");
 
 const router = express.Router();
 
-router.post("/", (req, res) => {});
+router.post("/", validateUser, (req, res) => {});
 
-router.post("/:id/posts", (req, res) => {});
+router.post("/:id/posts", validateUserId, validatePost, (req, res) => {});
 
 router.get("/", (req, res) => {});
 
-router.get("/:id", (req, res) => {});
+router.get("/:id", validateUserId, (req, res) => {});
 
-router.get("/:id/posts", (req, res) => {});
+router.get("/:id/posts", validateUserId, (req, res) => {});
 
-router.delete("/:id", (req, res) => {});
+router.delete("/:id", validateUserId, (req, res) => {});
 
-router.put("/:id", (req, res) => {});
+router.put("/:id", validateUserId, (req, res) => {});
 
 //custom middleware
 
