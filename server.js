4da92d867d1ff93d768d428a6d55e635d8362ba6
Heather-Ridgill <Heather.Ridgill@gmail.com>
2019-11-07 19:10:27 -0800

    I need to be better at committing.. but this is the MVP - i think

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
-const express = 'express';
+const express = require ('express');
+const userRouter = require (`./users/userRouter`);
+const postRouter = require (`./posts/postRouter`);
 
 const server = express();
+
+
+// constmiddlewarer = [
+//   logger
+// ];
+
+server.use(express.json());
+// server.use(middleware);
+
+server.use(`/api/users`, userRouter);
+server.use(`/api/posts`, postRouter);
+
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
@@ -9,7 +23,11 @@ server.get('/', (req, res) => {
 //custom middleware
 
 function logger(req, res, next) {
+console.log({request_method:req.method, request_url:req.url, timestamp: Date().toString()});
+next();
 
 };
+
+
 
 module.exports = server;
