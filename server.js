eafbd0c6263cd970bed348aced41a0413d44348d
Jashele T <jashele@Jasheles-Air.domain>
2019-11-08 15:22:09 -0700

    added comment, mvp complete

--- server.js
+++ server.js
@@ -13,8 +13,9 @@ server.get("/", (req, res) => {
   res.send(`<h2>Jashele Tillman</h2>`);
 });
 
-// logger logs to the console the following information about each request: request method, request url, and a timestamp
-// this middleware runs on every request made to the API
+//----------------------------------------------------------------------------//
+// LOGGER middleware: logs request method, request url, and a timestamp
+//----------------------------------------------------------------------------//
 
 function logger(req, res, next) {
   console.log(`
