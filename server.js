5ed4b8f6a10fa24b2710b57867a106c60b0e5517
Jashele T <jashele@Jasheles-Air.domain>
2019-11-07 17:44:36 -0700

    server is up and running

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
-const express = 'express';
+const express = require('express');
 
 const server = express();
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
+server.use(express.json());
+
+server.get("/", (req, res) => {
+   res.send(` 
+   
+   <h1>Hello.</h1>
+   <p>This is a template.</p>
+   
+   `);
 });
 
-//custom middleware
-
-function logger(req, res, next) {
-
-};
 
 module.exports = server;
