89c71c7a6cd5c1a00bec057dbbabc4a8cfdc3f0a
EvaGurtovaia <eva.gurtovaya@gmail.com>
2019-11-07 22:00:04 -0600

    Beginning

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
-const express = 'express';
+const express = "express";
 
 const server = express();
+const userRouter = require("./users/userRouter");
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
+server.use(express.json());
+server.use(logger);
+server.use("/api/users", userRouter);
+
+server.get("/", (req, res) => {
+    res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
 function logger(req, res, next) {
-
-};
+    console.log(
+        `Method: ${req.method}, url: ${
+            req.url
+        }, timestamp: ${new Date().toISOString()}`
+    );
+    next();
+}
 
 module.exports = server;
