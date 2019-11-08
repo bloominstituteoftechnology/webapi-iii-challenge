1835152e8b54e397eb7596fafa9892aae2a8f6b8
Jashele T <jashele@Jasheles-Air.domain>
2019-11-07 19:04:42 -0700

    issue with function resolved

--- server.js
+++ server.js
@@ -2,14 +2,7 @@ const express = require("express");
 const server = express();
 const userRouter = require("./users/userRouter");
 const postRouter = require("./posts/postRouter");
-const middleware = [
-  express.json(),
-  logger,
-  validateUserId,
-  validateUser,
-  validatePost
-];
-server.use(middleware);
+
 server.use("/api/users", userRouter);
 server.use("/api/posts", postRouter);
 server.use(express.json());
